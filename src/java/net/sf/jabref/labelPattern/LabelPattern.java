5a3fb3948376c5bcfce8b3fe5bcd203578e91904
ulriks <ulriks@users.sourceforge.net>
2003-12-14 13:28:45 +0000

    *** empty log message ***

---
+++ src/java/net/sf/jabref/labelPattern/LabelPattern.java
@@ -0,0 +1,93 @@
+/*
+ * Created on 09-Dec-2003
+ */
+package net.sf.jabref.labelPattern;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ * A small table, where an entry type is associated with a label pattern (an 
+ * <code>ArrayList</code>).
+ * 
+ * A parent LabelPattern can be set. 
+ * @author Ulrik Stervbo (ulriks AT ruc.dk)
+ */
+public class LabelPattern extends Hashtable{
+	/**
+	 * The parent of this LabelPattern.
+	 */
+	protected LabelPattern parent = null;
+	
+	public LabelPattern(){}
+	
+	public LabelPattern(LabelPattern parent){
+		this.parent = parent;
+	}
+	/**
+	 * Sets the parent LabelPattern.
+	 * @param parent a <code>String</code>
+	 */
+	public void setParent(LabelPattern parent){
+		this.parent = parent;
+	}
+	
+	/**
+	 * Get the parent LabelPattern
+	 * @return the parent LabelPattern
+	 */
+	public LabelPattern getParent(){
+		return parent;
+	}
+	
+	public void addLabelPattern(String type, String pattern){
+		put(type, LabelPatternUtil.split(pattern));
+	}
+	
+	
+	/**
+	 * Remove a label pattern from the LabelPattern. No key patterns can be removed from
+	 * the very parent LabelPattern since is thought of as a default. To do this, use
+	 * the  removeKeyPattern(String type, boolean sure)
+	 * @param type a <code>String</code>
+	 */
+	public void removeLabelPattern(String type){
+		if(containsKey(type) && parent != null){
+			remove(type);
+		}
+	}
+	
+	public void removeLabelPattern(String type, boolean sure){
+		
+		if(containsKey(type) && sure){
+			remove(type);
+		}
+	}	
+	/**
+	 * Gets an object for a desired label from this LabelPattern or one of it's parents.
+	 * This method first tries to obtain the object from this LabelPattern via the 
+	 * <code>get</code> method of <code>Hashtable</code>. If this fails, we try the 
+	 * parent.
+	 *  
+	 * @param key a <code>String</code>
+	 * @return the object for the given key
+	 * @throws NullPointerException
+	 */
+	public final ArrayList getValue(String key){
+		Object _obj = get(key); // throws the NullPointerException
+		
+		// Test to see if we found anything
+		if(_obj == null){
+			if(parent != null){
+				_obj = parent.getValue(key);
+			}
+			if(_obj == null){
+				//TODO Throw some error
+				System.err.println("Warning: I could not find the label \'"
+														+ key + "\' and gave up");
+			}
+		}
+		return (ArrayList)_obj;
+	}
+
+}
---
+++ src/java/net/sf/jabref/labelPattern/LabelPatternUtil.java
@@ -0,0 +1,255 @@
+/*
+ * Created on 13-Dec-2003
+ */
+package net.sf.jabref.labelPattern;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.sf.jabref.BibtexDatabase;
+import net.sf.jabref.BibtexEntry;
+import net.sf.jabref.Globals;
+import net.sf.jabref.ImportFormatReader;
+
+/**
+ * 
+ * @author Ulrik Stervbo (ulriks AT ruc.dk)
+ */
+/**
+ * This is the utility class of the LabelPattern package.
+ * @author Ulrik Stervbo (ulriks AT ruc.dk)
+ */
+public class LabelPatternUtil {
+	//this is SO crappy, but i have no idea of converting unicode into a String
+	// the content of the AL is build with the buildLetters()
+	private static ArrayList letters = builtLetters();
+	
+	private static BibtexDatabase _db;
+
+	/**
+	 * This method takes a string of the form [field1]spacer[field2]spacer[field3]...,
+	 * where the fields are the (required) fields of a BibTex entry. The string is split
+	 * into firlds and spacers by recognizing the [ and ].
+	 *  
+	 * @param keyPattern a <code>String</code>
+	 * @return an <code>ArrayList</code> The first item of the list
+	 * is a string representation of the key pattern (the parameter),
+	 * the second item is the spacer character (a <code>String</code>).
+	 */
+	public static ArrayList split(String labelPattern){
+		// A holder for fields of the entry to be used for the key
+		ArrayList _alist = new ArrayList();
+		
+		// Regular expresion for identifying the fields
+		Pattern pi = Pattern.compile("\\[\\w*\\]");
+		// Regular expresion for identifying the spacer
+		Pattern ps = Pattern.compile("\\].()*\\[");
+		
+		// The matcher for the field
+		Matcher mi = pi.matcher(labelPattern);
+		// The matcher for the spacer char
+		Matcher ms = ps.matcher(labelPattern);
+		
+		// Before we do anything, we add the parameter to the ArrayLIst
+		_alist.add(labelPattern);
+    
+		// If we can find the spacer character    
+		if(ms.find()){
+			String t_spacer = ms.group();
+				// Remove the `]' and `[' at the ends
+				// We cant imagine a spacer of omre than one character.
+				t_spacer = t_spacer.substring(1,2);
+				_alist.add(t_spacer);
+		}
+                
+		while(mi.find()){
+			// Get the matched string
+			String t_str = mi.group();
+				int _sindex = 1;
+				int _eindex = t_str.length() -1;
+				// Remove the `[' and `]' at the ends
+				t_str = t_str.substring(_sindex, _eindex);
+			_alist.add(t_str); 
+		}
+		
+		return _alist;
+	}
+	
+	/**
+	 * Generates a BibTeX label according to the pattern for a given entry type, and
+	 * returns the <code>Bibtexentry</code> with the unique label.
+	 * @param table a <code>LabelPattern</code>
+	 * @param database a <code>BibtexDatabase</code>
+	 * @param entryId a <code>String</code>
+	 * @return modified Bibtexentry
+	 */
+	public static BibtexEntry makeLabel(LabelPattern table, BibtexDatabase database, String entryId){
+		_db = database;
+		BibtexEntry _entry = _db.getEntryById(entryId);
+		ArrayList _al;
+		String _spacer, _label;
+		StringBuffer _sb = new StringBuffer();
+
+		try {
+			// get the type of entry
+			String _type = _entry.getField("type").toString();
+			// Get the arrayList corrosponding to the type 		
+			_al = table.getValue(_type);
+			int _alSize = _al.size();
+			// get the pacer (first item is the string version of the key pattern)
+			// the second is the spacer, the third, fourth and so forth are fields
+			_spacer = _al.get(1).toString();
+			
+			for(int i = 2; i < _alSize; i++){
+				// hmmmm..... the fields have different format... 
+				//TODO deal with different format for the fields :-)
+				String _field = _al.get(i).toString();
+				if(_field.equals("author")){
+					_sb.append(firstAuthor(_entry.getField("author").toString()));
+				}
+				if(_field.equals("editor")){
+					_sb.append(firstAuthor(_entry.getField("editor").toString()));
+				}
+				if(_field.equals("firstpage")){
+					_sb.append(firstPage(_entry.getField("pages").toString()));
+				}
+				if(_field.equals("lastpage")){
+					_sb.append(lastPage(_entry.getField("pages").toString()));
+				}
+				// we havent seen any special demands
+				else{
+					_sb.append(_entry.getField(_al.get(i).toString()).toString());
+				}
+				
+				if(i < _alSize - 1){ // It is not the space before the last field
+					_sb.append(_spacer);
+				}
+			}
+		}
+					
+		catch (Exception e) {
+				System.err.println(e);
+		}
+		
+		// here we make sure the key is unique		
+			_label = makeLabelUnique(_sb.toString());
+					
+			_entry.setField(Globals.KEY_FIELD, _label);
+		
+		return _entry;
+	}
+	
+	/**
+	 * This method returns a truely unique label (in the BibtexDatabase), by taking a 
+	 * label and add the letters a-z until a unique key is found.
+	 * @param key a <code>String</code>
+	 * @return a unique label
+	 */
+	public static String makeLabelUnique(String label){
+		// First I tried to make this recursive, but had to give up. I needed to
+		// do too many chacks of different kinds.
+		String _orgLabel = label;
+		String _newLabel = label;
+		int lettersSize = letters.size();
+		
+		for(int i = 0; i < lettersSize; i++){
+			if(isLabelUnique(_newLabel)){
+				// Hurray! the key is unique! lets get outta here
+				break;
+			} 
+			else{
+				// though luck! lets add a new letter...
+				_newLabel = _orgLabel + letters.get(i).toString();
+			}
+		}
+		return _newLabel;
+
+	}
+	
+	/**
+	 * Tests whether a given label is unique.
+	 * @param label a <code>String</code>
+	 * @return <code>true</code> if and only if the <code>label</code> is unique
+	 */
+	public static boolean isLabelUnique(String label){
+		boolean _isUnique = true;
+		BibtexEntry _entry;
+		int _dbSize = _db.getEntryCount();
+		// run through the whole DB and check the key field
+		// if this could be made recursive I would be very happy
+		// it kinda sux that we have to run through the whole db.
+		// The idea here is that if we meet NO match, the _duplicate
+		// field will be true 
+
+		for(int i = 0; i < _dbSize; i++){
+			_entry = _db.getEntryById(String.valueOf(i));
+
+			// oh my! there is a match! we better set the uniqueness to false
+			// and leave this for-loop all together
+			if(_entry.getField(Globals.KEY_FIELD).equals(label)){
+				_isUnique = false;
+				break;
+			} 			
+		}
+
+		return _isUnique;
+
+	}
+	/**
+	 * Gets the last name of the first author/editor
+	 * @param authorField a <code>String</code>
+	 * @return the sur name of an author/editor
+	 */
+	private static String firstAuthor(String authorField){
+		String author = "" ; 
+		// This code was part of 'ApplyRule' in 'ArticleLabelRule'
+		String[] tokens= authorField.split("\\band\\b");
+		if( tokens.length > 0){ // if author is empty
+			if(tokens[0].indexOf(",") > 0)
+				tokens[0] = ImportFormatReader.fixAuthor( tokens[0] ); // convert lastname, firstname to firstname lastname
+		String[] firstAuthor = tokens[0].replaceAll("\\s+"," ").split(" ");
+		// lastname, firstname
+		
+		author += firstAuthor[ firstAuthor.length-1];
+		}		
+		return author;
+	}
+	
+	/**
+	 * Split the pages field into two and return the first one
+	 * @param pages a <code>String</code>
+	 * @return the first page number
+	 */
+	private static String firstPage(String pages){
+		String[] _pages = pages.split("-");
+		return _pages[0];
+	}
+
+	/**
+	 * Split the pages field into two and return the last one
+	 * @param pages a <code>String</code>
+	 * @return the last page number
+	 */
+	private static String lastPage(String pages){
+		String[] _pages = pages.split("-");
+		return _pages[1];
+	}	
+	/**
+	 * I <b>HATE</b> this method!! I looked and looked but couldn't find a way to 
+	 * turn 61 (or in real unicode 0061) into the letter 'a' - crap!
+	 * @return an <code>ArrayList</code> which shouldn't be!!
+	 */
+	private static ArrayList builtLetters(){
+		ArrayList _letters = new ArrayList(); 
+		_letters.add("a"); _letters.add("b"); _letters.add("c"); _letters.add("d");
+		_letters.add("e"); _letters.add("f"); _letters.add("g"); _letters.add("h");
+		_letters.add("i"); _letters.add("j"); _letters.add("k"); _letters.add("l");
+		_letters.add("m"); _letters.add("n"); _letters.add("o"); _letters.add("p");
+		_letters.add("q"); _letters.add("r"); _letters.add("s"); _letters.add("t");
+		_letters.add("u"); _letters.add("v"); _letters.add("x"); _letters.add("y");
+		_letters.add("z");
+		
+		return _letters;
+	}
+}
