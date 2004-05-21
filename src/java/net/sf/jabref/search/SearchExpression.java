7df0ca483d9f7590c2f4af7ad0474e67ee63f406
jzieren <jzieren@users.sourceforge.net>
2004-05-21 18:45:34 +0000

    a search expression that supports specification of fields, regular expressions, and logical operators

---
+++ src/java/net/sf/jabref/search/SearchExpression.java
@@ -0,0 +1,41 @@
+/**
+ * SearchExpression.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package net.sf.jabref.search;
+
+import antlr.RecognitionException;
+import antlr.TokenStreamException;
+import antlr.collections.AST;
+import java.io.StringReader;
+import java.util.Hashtable;
+import java.util.regex.PatternSyntaxException;
+import net.sf.jabref.BibtexEntry;
+import net.sf.jabref.JabRefPreferences;
+import net.sf.jabref.SearchRule;
+
+public class SearchExpression implements SearchRule {
+	private SearchExpressionTreeParser treeParser = new SearchExpressionTreeParser();
+	private AST ast = null;
+	private JabRefPreferences prefs = null;
+	public SearchExpression(JabRefPreferences prefs, Hashtable searchOptions)
+		throws TokenStreamException, RecognitionException, PatternSyntaxException
+	{
+		this.prefs = prefs;
+		// parse search expression
+		SearchExpressionParser parser = new SearchExpressionParser(new SearchExpressionLexer(new StringReader(
+			searchOptions.elements().nextElement().toString()))); // supports only single entry
+		parser.searchExpression(); // this is the "global" rule
+		ast = parser.getAST(); // remember abstract syntax tree
+	}
+	public int applyRule(Hashtable searchStrings, BibtexEntry bibtexEntry) {
+		try {
+			return treeParser.apply(prefs,ast,bibtexEntry);
+		} catch (RecognitionException e) {
+			return 0; // this should never occur
+		}
+	}
+}
+
