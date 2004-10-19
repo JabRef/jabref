ce3cc943a3f3feb2640921c1f1c528e19cb113d8
mortenalver <mortenalver@users.sourceforge.net>
2004-10-19 21:38:51 +0000

    Added copy of aux dir

---
+++ src/java/net/sf/jabref/wizard/auximport/AuxSubGenerator.java
@@ -0,0 +1,252 @@
+/*
+Copyright (C) 2004 R. Nagel
+
+All programs in this directory and
+subdirectories are published under the GNU General Public License as
+described below.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+Further information about the GNU GPL is available at:
+http://www.gnu.org/copyleft/gpl.ja.html
+
+*/
+
+// created by : r.nagel 23.08.2004
+//
+// modified :
+
+package net.sf.jabref.wizard.aux ;
+
+import java.io.* ;
+import java.util.regex.* ;
+import java.util.* ;
+
+import net.sf.jabref.* ;
+import net.sf.jabref.imports.* ;
+
+/**
+ * <p>Title: Latex Aux to Bibtex</p>
+ *
+ * <p>Description: generates a sub-database which contains only bibtex entries
+ * from input aux file</p>
+ *
+ * <p>Copyright: Copyright (c) 2004</p>
+ *
+ * <p>Company: </p>
+ *
+ * @version 1.0
+ * @author r.nagel
+ *
+ * @todo Redesign of dialog structure for an assitent like feeling....
+ *   Now the unknown bibtex entries cannot inserted into the reference database
+ *   without closing the dialog.
+ */
+
+public class AuxSubGenerator
+{
+
+  private HashSet mySet ; // all unique bibtex keys in aux file
+
+  private Vector notFoundList ; // all not solved bibtex keys
+
+  private BibtexDatabase db ; // reference database
+  private BibtexDatabase auxDB ; // contains only the bibtex keys who found in aux file
+
+
+  public AuxSubGenerator(BibtexDatabase refDBase)
+  {
+    mySet = new HashSet(20) ;
+    notFoundList = new Vector() ;
+    db = refDBase ;
+  }
+
+  public final void setReferenceDatabase(BibtexDatabase newRefDB)
+  {
+    db = newRefDB ;
+  }
+
+  /**
+   * parseAuxFile
+   * read the Aux file and fill up some intern data structures
+   *
+   * @param filename String : Path to LatexAuxFile
+   * @return boolean, true = no error occurs
+   */
+  public final boolean parseAuxFile(String filename)
+  {
+    // regular expressions
+    Pattern pattern ;
+    Matcher matcher ;
+
+    // while condition
+    boolean weiter = false ;
+
+    // return value -> default: no error
+    boolean back = true ;
+
+    // the important tag
+    pattern = Pattern.compile( "\\\\citation\\{.+\\}" ) ;
+
+    // input-file-buffer
+    BufferedReader br = null ;
+
+    try
+    {
+      br = new BufferedReader( new FileReader( filename ) ) ;
+      weiter = true ;
+    }
+    catch ( FileNotFoundException fnfe )
+    {
+      System.out.println( "Cannot locate input file! " + fnfe.getMessage() ) ;
+      // System.exit( 0 ) ;
+      back = false ;
+    }
+
+    while ( weiter )
+    {
+      String line ;
+      try
+      {
+        line = br.readLine() ;
+      }
+      catch ( IOException ioe )
+      {
+        line = null ;
+        weiter = false ;
+      }
+
+      if ( line != null )
+      {
+        matcher = pattern.matcher(line) ;
+
+        while ( matcher.find() )
+        {
+          // extract the bibtex-key XXX from \citation{XXX} string
+          int len = matcher.end() - matcher.start() ;
+          if ( len > 11 )
+          {
+            String str = matcher.group().substring( matcher.start() + 10,
+                                                    matcher.end() - 1 ) ;
+            // save key into an set
+            mySet.add( str ) ;
+//            System.out.println("found " +str +" in AUX") ;
+          }
+        }
+      }
+      else weiter = false ;
+    }  // end of while
+
+    if (back) // only close, if open sucessful
+    {
+      try
+      {
+        br.close() ;
+      }
+      catch ( IOException ioe )
+      {}
+    }
+
+    return back ;
+  }
+
+  /**
+   * resolveTags
+   * Try to find an equivalent bibtex entry into reference database for all keys
+   * (found in aux file). This methode will fill up some intern data structures.....
+   */
+  public final void resolveTags()
+  {
+    auxDB = new BibtexDatabase() ;
+    notFoundList.clear();
+
+    Iterator it = mySet.iterator() ;
+
+    // forall bibtex keys (found in aux-file) try to find an equivalent
+    // entry into reference database
+    while (it.hasNext())
+    {
+      String str = (String) it.next() ;
+      BibtexEntry entry = db.getEntryByKey(str);
+
+      if (entry == null)
+      {
+        notFoundList.add(str) ;
+      } else
+      {
+        try
+        {
+          auxDB.insertEntry( entry ) ;
+        }
+        catch (Exception e) {}
+      }
+    }
+  }
+
+  /**
+   * generate
+   * Shortcut methode for easy generation.
+   *
+   * @param auxFileName String
+   * @param bibDB BibtexDatabase - reference database
+   * @return Vector - contains all not resolved bibtex entries
+   */
+  public final Vector generate(String auxFileName, BibtexDatabase bibDB)
+  {
+    setReferenceDatabase(bibDB);
+    parseAuxFile(auxFileName) ;
+    resolveTags();
+
+    return notFoundList ;
+  }
+
+  public BibtexDatabase getGeneratedDatabase()
+  {
+    if (auxDB == null)
+      auxDB = new BibtexDatabase() ;
+
+    return auxDB ;
+  }
+
+  public final int getFoundKeysInAux()
+  {
+    return mySet.size() ;
+  }
+
+  public final int getResolvedKeysCount()
+  {
+    return auxDB.getEntryCount() ;
+  }
+
+  public final int getNotResolvedKeysCount()
+  {
+    return notFoundList.size() ;
+  }
+
+  /** reset used all datastructures */
+  public final void clear()
+  {
+    mySet.clear() ;
+    notFoundList.clear();
+  }
+
+  /** returns a vector off all not resolved bibtex entries found in auxfile */
+  public Vector getNotFoundList()
+  {
+    return notFoundList ;
+  }
+
+}
---
+++ src/java/net/sf/jabref/wizard/auximport/gui/FromAuxDialog.java
@@ -0,0 +1,378 @@
+/*
+Copyright (C) 2004 R. Nagel
+
+All programs in this directory and
+subdirectories are published under the GNU General Public License as
+described below.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+Further information about the GNU GPL is available at:
+http://www.gnu.org/copyleft/gpl.ja.html
+
+*/
+
+// created by : r.nagel 23.08.2004
+//
+// modified :
+
+
+package net.sf.jabref.wizard.aux.gui ;
+
+import java.awt.* ;
+import javax.swing.* ;
+import javax.swing.border.* ;
+import java.awt.event.* ;
+import java.io.File;
+
+
+import net.sf.jabref.* ;
+import net.sf.jabref.wizard.aux.* ;
+
+public class FromAuxDialog
+    extends JDialog
+{
+
+  private JPanel panel1 = new JPanel() ;
+  private BorderLayout borderLayout1 = new BorderLayout() ;
+  private JPanel statusPanel = new JPanel() ;
+  private JPanel jPanel2 = new JPanel() ;
+  private JPanel optionsPanel = new JPanel() ;
+  private JButton okButton = new JButton() ;
+  private JButton cancelButton = new JButton() ;
+  private JButton generateButton = new JButton() ;
+  private TitledBorder titledBorder1 ;
+
+  private JComboBox dbChooser = new JComboBox() ;
+  private JTextField auxFileField ;
+  private JButton browseAuxFileButton ;
+
+  private JList notFoundList ;
+  private JTextArea statusInfos ;
+
+  // all open databases from JabRefFrame
+  private JTabbedPane parentTabbedPane ;
+
+  private boolean okPressed = false ;
+
+  private AuxSubGenerator auxParser ;
+
+
+  public FromAuxDialog( JabRefFrame frame, String title, boolean modal,
+                        JTabbedPane viewedDBs )
+  {
+    super( frame, title, modal ) ;
+
+    parentTabbedPane = viewedDBs ;
+
+    auxParser = new AuxSubGenerator(null) ;
+
+    try
+    {
+      jbInit( frame ) ;
+      pack() ;
+    }
+    catch ( Exception ex )
+    {
+      ex.printStackTrace() ;
+    }
+  }
+
+  private void jbInit( JabRefFrame parent ) throws Exception
+  {
+    panel1.setLayout( borderLayout1 ) ;
+    okButton.setText( Globals.lang( "Ok" ) ) ;
+    okButton.setEnabled(false);
+    okButton.addActionListener( new FromAuxDialog_ok_actionAdapter( this ) ) ;
+    cancelButton.setText( Globals.lang( "Cancel" ) ) ;
+    cancelButton.addActionListener( new FromAuxDialog_Cancel_actionAdapter( this ) ) ;
+    generateButton.setText(Globals.lang("Generate"));
+    generateButton.addActionListener( new FromAuxDialog_generate_actionAdapter( this ) );
+
+    initOptionsPanel(parent) ;
+
+    initStatusPanel() ;
+
+    this.setModal( true ) ;
+    this.setResizable( false ) ;
+    this.setTitle( Globals.lang("AUX file import" )) ;
+    getContentPane().add( panel1 ) ;
+
+    panel1.add( optionsPanel, BorderLayout.NORTH ) ;
+    panel1.add( jPanel2, BorderLayout.SOUTH ) ;
+    jPanel2.add( generateButton, null) ;
+    jPanel2.add( okButton, null ) ;
+    jPanel2.add( cancelButton, null ) ;
+    panel1.add( statusPanel, BorderLayout.CENTER ) ;
+
+    // Key bindings:
+    ActionMap am = statusPanel.getActionMap() ;
+    InputMap im = statusPanel.getInputMap( JComponent.WHEN_IN_FOCUSED_WINDOW ) ;
+    im.put( parent.prefs().getKey( "Close dialog" ), "close" ) ;
+    am.put( "close", new AbstractAction()
+    {
+      public void actionPerformed( ActionEvent e )
+      {
+        dispose() ;
+      }
+    } ) ;
+
+  }
+
+  private void initOptionsPanel(JabRefFrame parent)
+  {
+    // collect the names of all open databases
+    int len = parentTabbedPane.getTabCount() ;
+    for ( int t = 0 ; t < len ; t++ )
+    {
+      dbChooser.addItem( ( String ) parentTabbedPane.getTitleAt( t ) ) ;
+    }
+
+    // panel view
+    GridBagLayout gbl = new GridBagLayout() ;
+    GridBagConstraints con = new GridBagConstraints() ;
+    con.weightx = 0 ;
+    con.insets = new Insets( 5, 10, 0, 10 ) ;
+    con.fill = GridBagConstraints.HORIZONTAL ;
+
+    TitledBorder border = new TitledBorder( BorderFactory.createLineBorder(
+        new Color( 153, 153, 153 ), 2 ),
+                                            Globals.lang( "Options" ) ) ;
+    optionsPanel.setBorder( border ) ;
+    optionsPanel.setLayout( gbl ) ;
+
+    // Database
+    JLabel lab1 = new JLabel( Globals.lang( "Reference database" ) + ":" ) ;
+    lab1.setHorizontalAlignment( SwingConstants.LEFT ) ;
+    gbl.setConstraints( lab1, con ) ;
+    optionsPanel.add( lab1 ) ;
+    con.gridwidth = GridBagConstraints.REMAINDER ;
+    gbl.setConstraints( dbChooser, con ) ;
+    optionsPanel.add( dbChooser ) ;
+
+    // AUX
+    con.gridwidth = 1 ;
+    con.weightx = 0 ;
+    con.insets = new Insets( 5, 10, 15, 10 ) ;
+    con.fill = GridBagConstraints.BOTH ;
+    lab1 = new JLabel( Globals.lang( "LaTeX AUX file" ) + ":" ) ;
+    lab1.setHorizontalAlignment( SwingConstants.LEFT ) ;
+    gbl.setConstraints( lab1, con ) ;
+    optionsPanel.add( lab1 ) ;
+    con.weightx = 1 ;
+    auxFileField = new JTextField( "", 25 ) ;
+    gbl.setConstraints( auxFileField, con ) ;
+    optionsPanel.add( auxFileField ) ;
+    con.weightx = 0 ;
+    con.insets = new Insets( 5, 10, 15, 2 ) ;
+    browseAuxFileButton = new JButton( Globals.lang( "Browse" ) ) ;
+    browseAuxFileButton.addActionListener( new BrowseAction(
+                                             auxFileField,
+                                             parent));
+    gbl.setConstraints( browseAuxFileButton, con ) ;
+    optionsPanel.add( browseAuxFileButton ) ;
+
+  }
+
+  private void initStatusPanel()
+  {
+    GridBagLayout gbl = new GridBagLayout() ;
+    GridBagConstraints con = new GridBagConstraints() ;
+    con.weightx = 0 ;
+    con.insets = new Insets( 5, 10, 0, 10 ) ;
+    con.fill = GridBagConstraints.HORIZONTAL ;
+
+    titledBorder1 = new TitledBorder( BorderFactory.createLineBorder( new Color(
+        153, 153, 153 ), 2 ), Globals.lang( "Results" ) ) ;
+
+    statusPanel.setLayout(gbl);
+    statusPanel.setBorder( titledBorder1 ) ;
+
+    JLabel lab1 = new JLabel( Globals.lang( "Unknown bibtex entries" ) + ":" ) ;
+    lab1.setHorizontalAlignment( SwingConstants.LEFT ) ;
+    con.gridwidth = 1 ;
+    gbl.setConstraints( lab1, con ) ;
+    statusPanel.add( lab1 ) ;
+
+    lab1 = new JLabel( Globals.lang( "Messages" ) + ":" ) ;
+    lab1.setHorizontalAlignment( SwingConstants.LEFT ) ;
+    con.gridwidth = GridBagConstraints.REMAINDER ;
+    gbl.setConstraints( lab1, con ) ;
+    statusPanel.add( lab1 ) ;
+
+
+    notFoundList = new JList() ;
+    JScrollPane listScrollPane = new JScrollPane(notFoundList);
+    listScrollPane.setPreferredSize(new Dimension(250, 120));
+    con.gridwidth = 1 ;
+    con.weightx = 0 ;
+    con.gridheight = 2 ;
+    con.insets = new Insets( 5, 10, 15, 10 ) ;
+    con.fill = GridBagConstraints.BOTH ;
+    gbl.setConstraints(listScrollPane, con);
+    statusPanel.add( listScrollPane) ;
+
+    statusInfos = new JTextArea("", 5, 20) ;
+    statusInfos.setBorder(BorderFactory.createEtchedBorder()); 
+    statusInfos.setEditable(false);
+    con.gridheight = 1 ;
+    gbl.setConstraints(statusInfos, con);
+    statusPanel.add(statusInfos) ;
+
+  }
+
+// ---------------------------------------------------------------------------
+
+  void ok_actionPerformed( ActionEvent e )
+  {
+    okPressed = true ;
+    dispose() ;
+  }
+
+  void Cancel_actionPerformed( ActionEvent e )
+  {
+    dispose() ;
+  }
+
+  void generate_actionPerformed( ActionEvent e )
+  {
+     generateButton.setEnabled(false);
+     BasePanel bp = (BasePanel) parentTabbedPane.getComponentAt(
+                                          dbChooser.getSelectedIndex())  ;
+     notFoundList.removeAll() ;
+     statusInfos.setText(null);
+     BibtexDatabase refBase = bp.getDatabase() ;
+     String auxName = auxFileField.getText() ;
+
+     if (auxName != null)
+       if ((refBase != null) && (auxName.length() > 0))
+       {
+         auxParser.clear();
+         notFoundList.setListData(auxParser.generate( auxName, refBase ) ) ;
+
+         statusInfos.append( Globals.lang("keys_in_database") +" " +refBase.getEntryCount() ) ;
+         statusInfos.append( "\n" +Globals.lang("found_in_aux_file") +" "+auxParser.getFoundKeysInAux());
+         statusInfos.append( "\n" +Globals.lang("resolved") +" " +auxParser.getResolvedKeysCount());
+         statusInfos.append( "\n" +Globals.lang("not_found") +" " +auxParser.getNotResolvedKeysCount());
+
+         okButton.setEnabled(true);
+      }
+
+     // the generated database contains no entries -> no active ok-button
+     if (auxParser.getGeneratedDatabase().getEntryCount() < 1)
+     {
+       statusInfos.append( "\n" +Globals.lang("empty database")) ;
+       okButton.setEnabled( false ) ;
+     }
+
+     generateButton.setEnabled(true);
+  }
+
+
+  public boolean okPressed()
+  {
+    return okPressed ;
+  }
+
+  public BibtexDatabase getGenerateDB()
+  {
+    return auxParser.getGeneratedDatabase() ;
+  }
+
+// ---------------------------------------------------------------------------
+
+  /**
+   * Action used to produce a "Browse" button for one of the text fields.
+   */
+  class BrowseAction extends AbstractAction
+  {
+    private JTextField comp ;
+    private JabRefFrame _frame;
+
+    public BrowseAction( JTextField tc, JabRefFrame frame)
+    {
+      super( Globals.lang( "Browse" ) ) ;
+      _frame = frame ;
+      comp = tc ;
+    }
+
+    public void actionPerformed( ActionEvent e )
+    {
+      String chosen = null ;
+      chosen = Globals.getNewFile( _frame, null, new File( comp.getText() ),
+                                   ".aux",
+                                   JFileChooser.OPEN_DIALOG, false ) ;
+      if ( chosen != null )
+      {
+        File newFile = new File( chosen ) ;
+        comp.setText( newFile.getPath() ) ;
+      }
+    }
+  }
+
+
+}
+
+// ----------- helper class -------------------
+class FromAuxDialog_ok_actionAdapter
+    implements java.awt.event.ActionListener
+{
+  FromAuxDialog adaptee ;
+
+  FromAuxDialog_ok_actionAdapter( FromAuxDialog adaptee )
+  {
+    this.adaptee = adaptee ;
+  }
+
+  public void actionPerformed( ActionEvent e )
+  {
+    adaptee.ok_actionPerformed( e ) ;
+  }
+}
+
+class FromAuxDialog_Cancel_actionAdapter
+    implements java.awt.event.ActionListener
+{
+  FromAuxDialog adaptee ;
+
+  FromAuxDialog_Cancel_actionAdapter( FromAuxDialog adaptee )
+  {
+    this.adaptee = adaptee ;
+  }
+
+  public void actionPerformed( ActionEvent e )
+  {
+    adaptee.Cancel_actionPerformed( e ) ;
+  }
+}
+
+class FromAuxDialog_generate_actionAdapter
+    implements java.awt.event.ActionListener
+{
+  FromAuxDialog adaptee ;
+
+  FromAuxDialog_generate_actionAdapter( FromAuxDialog adaptee )
+  {
+    this.adaptee = adaptee ;
+  }
+
+  public void actionPerformed( ActionEvent e )
+  {
+    adaptee.generate_actionPerformed( e ) ;
+  }
+}
+
+
