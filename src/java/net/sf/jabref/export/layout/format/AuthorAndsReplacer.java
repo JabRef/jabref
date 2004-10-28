b85e30943dc2cf0bd144a8e4500878e1ea2f3210
silla <silla@users.sourceforge.net>
2004-10-28 05:00:55 +0000

    New Formater, replaces authors "ands" for commas and &'s. Usefull for publications listings.

---
+++ src/java/net/sf/jabref/export/layout/format/AuthorAndsReplacer.java
@@ -0,0 +1,46 @@
+/*
+ * Created on 10/10/2004
+ * 
+ */
+package net.sf.jabref.export.layout.format;
+
+import net.sf.jabref.export.layout.LayoutFormatter;
+
+/**
+ * Replaces and's for & (in case of two authors) and ; (in case
+ * of more than two authors).
+ * 
+ * @author Carlos Silla
+ */
+public class AuthorAndsReplacer implements LayoutFormatter {
+
+	/* (non-Javadoc)
+	 * @see net.sf.jabref.export.layout.LayoutFormatter#format(java.lang.String)
+	 */
+	public String format(String fieldText) {
+		String[] authors = fieldText.split(" and ");
+		String s;
+	
+		switch(authors.length) {
+			case 1:
+				//Does nothing;
+				s = new String(authors[0]);			
+			break;
+			case 2:
+				s = new String(authors[0] + " & " + authors[1]);
+			break;
+			default:
+				int i = 0, x = authors.length;
+				StringBuffer sb = new StringBuffer();
+				
+				for(i=0;i<x-2;i++) {
+					sb.append(authors[i] + "; ");
+				}				
+				sb.append(authors[i] + " & " + authors[i+1]);
+				s = new String(sb);				
+			break;		
+		}
+		
+		return s;
+	}
+}
