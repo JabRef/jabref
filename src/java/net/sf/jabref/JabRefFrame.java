4d13bcb3c30714e12df8832812034b9d3292e00c
jzieren <jzieren@users.sourceforge.net>
2004-12-02 13:34:22 +0000

    certain import filters return null on failure. this now shows an error message, as with bibentries.size() == 0

--- src/java/net/sf/jabref/JabRefFrame.java
+++ src/java/net/sf/jabref/JabRefFrame.java
@@ -1854,7 +1854,7 @@ class FetchCiteSeerAction
      */
   public void addBibEntries(ArrayList bibentries, String filename,
                              boolean intoNew) {
-    if (bibentries.size() == 0) {
+  	if (bibentries == null || bibentries.size() == 0) {
       // No entries found. We need a message for this.
       JOptionPane.showMessageDialog(ths, Globals.lang("No entries found. Please make sure you are "
                                                       +"using the correct import filter."), Globals.lang("Import failed"),
