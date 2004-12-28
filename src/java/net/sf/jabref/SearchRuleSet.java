1e34ae735d19478db1293241fbd512e60c3b5327
jzieren <jzieren@users.sourceforge.net>
2004-12-28 22:18:58 +0000

    SearchRuleSet now implements SearchRule code formatted

--- src/java/net/sf/jabref/SearchRuleSet.java
+++ src/java/net/sf/jabref/SearchRuleSet.java
@@ -1,59 +1,51 @@
 /*
-Copyright (C) 2003  Nathan Dunn
+ Copyright (C) 2003  Nathan Dunn
 
-All programs in this directory and
-subdirectories are published under the GNU General Public License as
-described below.
+ All programs in this directory and
+ subdirectories are published under the GNU General Public License as
+ described below.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-USA
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
 
-Further information about the GNU GPL is available at:
-http://www.gnu.org/copyleft/gpl.ja.html
+ Further information about the GNU GPL is available at:
+ http://www.gnu.org/copyleft/gpl.ja.html
 
-*/
+ */
 package net.sf.jabref;
 
-import java.util.Vector ; 
-import java.util.Enumeration ; 
-import java.util.Hashtable; 
+import java.util.*;
 
-public class SearchRuleSet{
+public class SearchRuleSet implements SearchRule {
+    protected Vector ruleSet = new Vector();
 
     public void addRule(SearchRule newRule) {
-        ruleSet.add(newRule) ; 
+        ruleSet.add(newRule);
     }
 
     public void clearRules() {
-        ruleSet.clear() ; 
+        ruleSet.clear();
     }
 
-    // we use a hashtable here, in case we want more resolution later
-    // for implementing searches
-    public int applyRules(Hashtable searchString,BibtexEntry bibtexEntry){
-        int score = 0 ; 
-        Enumeration e = ruleSet.elements() ; 
-        while(e.hasMoreElements()){
-            score += ((SearchRule) e.nextElement()).applyRule(searchString,bibtexEntry); 
+    public int applyRule(Map searchString, BibtexEntry bibtexEntry) {
+        int score = 0;
+        Enumeration e = ruleSet.elements();
+        while (e.hasMoreElements()) {
+            score += ((SearchRule) e.nextElement()).applyRule(searchString,
+                    bibtexEntry);
         }
-
-
-        return score ; 
+        return score;
     }
-
-    protected Vector ruleSet = new Vector() ; 
-    
 }
-
--- src/java/net/sf/jabref/groups/AndOrSearchRuleSet.java
+++ src/java/net/sf/jabref/groups/AndOrSearchRuleSet.java
@@ -1,69 +1,68 @@
 /*
-Copyright (C) 2003 Morten O. Alver, Nizar N. Batada
+ Copyright (C) 2003 Morten O. Alver, Nizar N. Batada
 
-All programs in this directory and
-subdirectories are published under the GNU General Public License as
-described below.
+ All programs in this directory and
+ subdirectories are published under the GNU General Public License as
+ described below.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or (at
+ your option) any later version.
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-USA
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
 
-Further information about the GNU GPL is available at:
-http://www.gnu.org/copyleft/gpl.ja.html
+ Further information about the GNU GPL is available at:
+ http://www.gnu.org/copyleft/gpl.ja.html
 
-*/
+ */
 package net.sf.jabref.groups;
 
+import java.util.*;
+
 import net.sf.jabref.*;
-import java.util.Hashtable;
-import java.util.Enumeration;
 
 /**
- * Subclass of SearchRuleSet that ANDs or ORs between its rules,
- * eturning 0 or 1.
+ * Subclass of SearchRuleSet that ANDs or ORs between its rules, eturning 0 or
+ * 1.
  */
 class AndOrSearchRuleSet extends SearchRuleSet {
 
     private boolean and, invert;
 
     public AndOrSearchRuleSet(boolean and, boolean invert) {
-      this.and = and;
-      this.invert = invert;
+        this.and = and;
+        this.invert = invert;
     }
 
-    public int applyRules(Hashtable searchString,BibtexEntry bibtexEntry){
-        int score = 0 ;
-        Enumeration e = ruleSet.elements() ;
+    public int applyRule(Map searchString, BibtexEntry bibtexEntry) {
+        int score = 0;
+        Enumeration e = ruleSet.elements();
 
-	// We let each rule add a maximum of 1 to the score.
-        while(e.hasMoreElements()){
-            score += (((SearchRule) e.nextElement()).
-		      applyRule(searchString,bibtexEntry) > 0 ? 1 : 0);
+        // We let each rule add a maximum of 1 to the score.
+        while (e.hasMoreElements()) {
+            score += (((SearchRule) e.nextElement()).applyRule(searchString,
+                    bibtexEntry) > 0 ? 1 : 0);
         }
 
-	// Then an AND rule demands that score == number of rules, and
-	// an OR rule demands score > 0.
+        // Then an AND rule demands that score == number of rules, and
+        // an OR rule demands score > 0.
         boolean res;
         if (and)
-          res = (score == ruleSet.size());
+            res = (score == ruleSet.size());
         else
-          res = (score > 0);
+            res = (score > 0);
 
         if (invert)
-          return (res ? 0 : 1);
-        else
-          return (res ? 1 : 0);
+            return (res ? 0 : 1);
+        return (res ? 1 : 0);
     }
 }
