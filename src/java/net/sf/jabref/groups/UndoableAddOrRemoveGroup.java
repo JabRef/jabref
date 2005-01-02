a31b47cf4f5f60805a4304ab3d4cd3f57338fbc4
jzieren <jzieren@users.sourceforge.net>
2005-01-02 10:48:23 +0000

    fixed: undo failed for "remove group, keep subgroups"

--- src/java/net/sf/jabref/groups/UndoableAddOrRemoveGroup.java
+++ src/java/net/sf/jabref/groups/UndoableAddOrRemoveGroup.java
@@ -78,7 +78,7 @@ class UndoableAddOrRemoveGroup extends AbstractUndoableEdit {
         // storing a backup of the whole subtree is not required when children
         // are kept
         m_subtreeBackup = editType != REMOVE_NODE_KEEP_CHILDREN ? editedNode
-                .deepCopy() : new GroupTreeNode(editedNode.getGroup());
+                .deepCopy() : new GroupTreeNode(editedNode.getGroup().deepCopy());
         // remember path to edited node. this cannot be stored as a reference,
         // because the reference itself might change. the method below is more
         // robust.
@@ -131,9 +131,9 @@ class UndoableAddOrRemoveGroup extends AbstractUndoableEdit {
                 GroupTreeNode newNode = m_subtreeBackup.deepCopy();
                 for (int i = childIndex; i < childIndex
                         + m_subtreeRootChildCount; ++i) {
-                    newNode.add((GroupTreeNode) cursor.getChildAt(i));
+                    newNode.add((GroupTreeNode) cursor.getChildAt(childIndex));
                 }
-                cursor.insert(m_subtreeBackup.deepCopy(), childIndex);
+                cursor.insert(newNode, childIndex);
                 break;
             case REMOVE_NODE_AND_CHILDREN:
                 cursor.insert(m_subtreeBackup.deepCopy(), childIndex);
