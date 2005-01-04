95944086aafbd73f67a46fcd2a7f24d68ca4835a
jzieren <jzieren@users.sourceforge.net>
2005-01-04 19:12:50 +0000

    several changes in AbstractGroup interface now using references directly, rather than BibtexKeys

--- src/java/net/sf/jabref/groups/ExplicitGroup.java
+++ src/java/net/sf/jabref/groups/ExplicitGroup.java
@@ -2,43 +2,48 @@ package net.sf.jabref.groups;
 
 import java.util.*;
 
+import javax.swing.undo.AbstractUndoableEdit;
+
 import net.sf.jabref.*;
 
 /**
  * @author zieren
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class ExplicitGroup extends AbstractGroup implements SearchRule {
-	public static final String ID = "ExplicitGroup:";
-	private Set m_entries;
-	private String m_name;
-	
-	public ExplicitGroup(String name) {
-	    m_name = name;
-	    m_entries = new HashSet();
-	}
-	
-	public static AbstractGroup fromString(String s) throws Exception {
+    public static final String ID = "ExplicitGroup:";
+    private final Set m_entries;
+    private final BibtexDatabase m_database;
+
+    public ExplicitGroup(String name, BibtexDatabase db) {
+        super(name);
+        m_entries = new HashSet();
+        m_database = db;
+    }
+
+    public static AbstractGroup fromString(String s, BibtexDatabase db)
+            throws Exception {
         if (!s.startsWith(ID))
             throw new Exception(
                     "Internal error: ExplicitGroup cannot be created from \""
                             + s + "\"");
-	    QuotedStringTokenizer tok = new QuotedStringTokenizer(s.substring(ID
+        QuotedStringTokenizer tok = new QuotedStringTokenizer(s.substring(ID
                 .length()), SEPARATOR, QUOTE_CHAR);
-	    ExplicitGroup newGroup = new ExplicitGroup(tok.nextToken());
-	    while (tok.hasMoreTokens())
-	        newGroup.m_entries.add(Util.unquote(tok.nextToken(),QUOTE_CHAR));
-	    return newGroup;
-	}
-	
-    public SearchRule getSearchRule() {
-        return this;
+        ExplicitGroup newGroup = new ExplicitGroup(tok.nextToken(), db);
+        BibtexEntry[] entries;
+        while (tok.hasMoreTokens()) {
+            entries = db.getEntriesByKey(Util.unquote(tok.nextToken(),
+                    QUOTE_CHAR));
+            for (int i = 0; i < entries.length; ++i)
+                newGroup.m_entries.add(entries[i]);
+        }
+        return newGroup;
     }
 
-    public String getName() {
-        return m_name;
+    public SearchRule getSearchRule() {
+        return this;
     }
 
     public boolean supportsAdd() {
@@ -49,56 +54,67 @@ public class ExplicitGroup extends AbstractGroup implements SearchRule {
         return true;
     }
 
-    public void addSelection(BasePanel basePanel) {
+    public AbstractUndoableEdit addSelection(BasePanel basePanel) {
         BibtexEntry[] bes = basePanel.getSelectedEntries();
-        Object bibtexkey;
-        for (int i = 0; i < bes.length; ++i) {
-            bibtexkey= bes[i].getField("bibtexkey");
-            if (bibtexkey != null) // JZTODO : report if null
-                m_entries.add(bibtexkey);
-        }        
-        // JZTODO undo information
+        if (bes.length == 0)
+            return null; // nothing to do
+
+        HashSet entriesBeforeEdit = new HashSet(m_entries);
+        for (int i = 0; i < bes.length; ++i)
+            m_entries.add(bes[i]);
+
+        return new UndoableChangeAssignment(entriesBeforeEdit, m_entries);
     }
 
-    public void removeSelection(BasePanel basePanel) {
+    public AbstractUndoableEdit removeSelection(BasePanel basePanel) {
         BibtexEntry[] bes = basePanel.getSelectedEntries();
-        Object bibtexkey;
-        for (int i = 0; i < bes.length; ++i) {
-            bibtexkey= bes[i].getField("bibtexkey");
-            if (bibtexkey != null) // JZTODO : report if null
-                m_entries.remove(bibtexkey);
-        }        
-        // JZTODO undo information
+        if (bes.length == 0)
+            return null; // nothing to do
+
+        HashSet entriesBeforeEdit = new HashSet(m_entries);
+        for (int i = 0; i < bes.length; ++i)
+            m_entries.remove(bes[i]);
+
+        return new UndoableChangeAssignment(entriesBeforeEdit, m_entries);
     }
 
-    public int contains(Map searchOptions, BibtexEntry entry) {
-        return m_entries.contains(entry.getField("bibtexkey")) ? 1 : 0;
+    public boolean contains(Map searchOptions, BibtexEntry entry) {
+        return m_entries.contains(entry);
     }
 
     public int applyRule(Map searchStrings, BibtexEntry bibtexEntry) {
-        return contains(searchStrings, bibtexEntry);
+        return contains(searchStrings, bibtexEntry) ? 1 : 0;
     }
 
     public AbstractGroup deepCopy() {
-        ExplicitGroup copy = new ExplicitGroup(m_name);
+        ExplicitGroup copy = new ExplicitGroup(m_name, m_database);
         copy.m_entries.addAll(m_entries);
         return copy;
     }
-    
+
     public boolean equals(Object o) {
         if (!(o instanceof ExplicitGroup))
             return false;
-        ExplicitGroup other = (ExplicitGroup)o;
-        return other.m_name.equals(m_name) && other.m_entries.equals(m_entries);
+        ExplicitGroup other = (ExplicitGroup) o;
+        return other.m_name.equals(m_name) && other.m_entries.equals(m_entries)
+                && other.m_database == m_database;
     }
-    
+
+    /**
+     * Returns a String representation of this group and its entries. Entries
+     * are referenced by their Bibtexkey. Entries that do not have a Bibtexkey
+     * are not included in the representation and will thus not be available
+     * upon recreation.
+     */
     public String toString() {
         StringBuffer sb = new StringBuffer();
-		sb.append(ID + Util.quote(m_name, SEPARATOR, QUOTE_CHAR) + SEPARATOR);
-		for (Iterator it = m_entries.iterator(); it.hasNext();) {
-            sb.append(Util.quote((String) it.next(), SEPARATOR, QUOTE_CHAR)
-                    + SEPARATOR);
+        sb.append(ID + Util.quote(m_name, SEPARATOR, QUOTE_CHAR) + SEPARATOR);
+        String s;
+        for (Iterator it = m_entries.iterator(); it.hasNext();) {
+            s = ((BibtexEntry) it.next()).getCiteKey();
+            if (s != null && !s.equals(""))
+                sb.append(Util.quote(s, SEPARATOR, QUOTE_CHAR) + SEPARATOR);
         }
-		return sb.toString();
+        return sb.toString();
     }
 }
