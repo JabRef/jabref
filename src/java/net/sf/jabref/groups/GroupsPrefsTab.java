de0c74dcb7287fbb8563df2b315026767a49ecae
jzieren <jzieren@users.sourceforge.net>
2005-06-11 12:05:37 +0000

    added groups preferences to general preferences

---
+++ src/java/net/sf/jabref/groups/GroupsPrefsTab.java
@@ -0,0 +1,121 @@
+package net.sf.jabref.groups;
+
+import java.awt.BorderLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.sf.jabref.Globals;
+import net.sf.jabref.JabRefPreferences;
+import net.sf.jabref.PrefsTab;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+// JZTODO lyrics
+public class GroupsPrefsTab extends JPanel implements PrefsTab {
+	private final JCheckBox showIcons = new JCheckBox("Show icons for groups");
+	private final JCheckBox showDynamic = new JCheckBox(
+			"<html>Show dynamic groups in <i>italics</i></html>");
+	private final JCheckBox expandTree = new JCheckBox(
+			"Initially show groups tree expanded");
+	private final JCheckBox autoShow = new JCheckBox(
+			"Automatically show groups interface when switching to a database that contains groups");
+	private final JCheckBox autoHide = new JCheckBox(
+			"Automatically hide groups interface when switching to a database that contains no groups");
+	private JTextField groupingField = new JTextField(20);
+	private JTextField keywordSeparator = new JTextField(2);
+
+	private final JabRefPreferences prefs;
+
+	public GroupsPrefsTab(JabRefPreferences prefs) {
+		this.prefs = prefs;
+		
+		keywordSeparator.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				keywordSeparator.selectAll();
+			}
+			public void focusLost(FocusEvent e) {
+				// deselection is automatic
+			}
+		});
+		
+		FormLayout layout = new FormLayout("9dlu, 500px",
+				"p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, p, 3dlu, " +
+				"p, 3dlu, p");
+		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+		builder.appendSeparator("View");
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+		builder.append(showIcons);
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+		builder.append(showDynamic);
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+		builder.append(expandTree);
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+		builder.append(autoShow);
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+		builder.append(autoHide);
+		builder.nextLine();
+		builder.nextLine();
+		builder.appendSeparator("Dynamic Groups");
+		builder.nextLine();
+		builder.nextLine();
+		builder.nextColumn();
+			// build subcomponent
+			FormLayout layout2 = new FormLayout("left:pref, 2dlu, left:pref", 
+					"p, 3dlu, p");
+			DefaultFormBuilder builder2 = new DefaultFormBuilder(layout2);
+			builder2.append(new JLabel(Globals.lang("Default grouping field") + ":"));
+			builder2.append(groupingField);
+			builder2.nextLine();
+			builder2.nextLine();
+			builder2.append(new JLabel("When adding/removing keywords, separate them by:"));
+			builder2.append(keywordSeparator);
+		builder.append(builder2.getPanel());
+
+		setLayout(new BorderLayout());
+		JPanel panel = builder.getPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		add(panel, BorderLayout.CENTER);
+	}
+
+	public void setValues() {
+		showIcons.setSelected(prefs.getBoolean("groupShowIcons"));
+		showDynamic.setSelected(prefs.getBoolean("groupShowDynamic"));
+		expandTree.setSelected(prefs.getBoolean("groupExpandTree"));
+		groupingField.setText(prefs.get("groupsDefaultField"));
+		autoShow.setSelected(prefs.getBoolean("groupAutoShow"));
+		autoHide.setSelected(prefs.getBoolean("groupAutoHide"));
+		keywordSeparator.setText(prefs.get("groupKeywordSeparator"));
+	}
+
+	public void storeSettings() {
+		prefs.putBoolean("groupShowIcons", showIcons.isSelected());
+		prefs.putBoolean("groupShowDynamic", showDynamic.isSelected());
+		prefs.putBoolean("groupExpandTree", expandTree.isSelected());
+		prefs.put("groupsDefaultField", groupingField.getText().trim());
+		prefs.putBoolean("groupAutoShow", autoShow.isSelected());
+		prefs.putBoolean("groupAutoHide", autoHide.isSelected());
+		prefs.put("groupKeywordSeparator", keywordSeparator.getText());
+	}
+
+	public boolean readyToClose() {
+		return true;
+	}
+
+}
