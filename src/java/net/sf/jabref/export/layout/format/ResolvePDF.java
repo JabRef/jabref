dd0fbfc53b25dfdbe1bda2afbcff97dccbe24a83
coezbek <coezbek@users.sourceforge.net>
2006-08-27 01:52:58 +0000

    Better handling for resolvePDF under Windows.

--- src/java/net/sf/jabref/export/layout/format/ResolvePDF.java
+++ src/java/net/sf/jabref/export/layout/format/ResolvePDF.java
@@ -5,11 +5,28 @@ import net.sf.jabref.Util;
 import net.sf.jabref.Globals;
 import java.io.File;
 
+/**
+ * Will expand the relative PDF path.
+ * 
+ * TODO Search also relative to Bib-file.
+ * 
+ * @author $Author$
+ * @version $Revision$ ($Date$)
+ */
 public class ResolvePDF implements LayoutFormatter {
 
 	public String format(String field) {
 		String dir = Globals.prefs.get("pdfDirectory");
 		File f = Util.expandFilename(field, new String[]{dir, "."});
-		return (f != null ? "file://" + f.getPath() : field);
+		
+		/*
+		 * Stumbled over this while investigating 
+		 * 
+		 * https://sourceforge.net/tracker/index.php?func=detail&aid=1469903&group_id=92314&atid=600306 
+		 */
+		if (Globals.ON_WIN && f != null){
+				field = "file://" + f.getPath().replaceAll("\\\\", "/");
+		}
+		return field;
 	}
 }
