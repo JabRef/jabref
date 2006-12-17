766e2d896000f52dee36985ac0c536859ec7042a
coezbek <coezbek@users.sourceforge.net>
2006-12-17 22:45:26 +0000

    Remove Empty Test.

--- src/java/tests/net/sf/jabref/imports/AllTests.java
+++ src/java/tests/net/sf/jabref/imports/AllTests.java
@@ -8,10 +8,9 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for tests.net.sf.jabref.imports");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(IsiImporterTest.class);
-		suite.addTestSuite(AutoImportTest.class);
-		suite.addTestSuite(BibtexParserTest.class);
 		suite.addTestSuite(OAI2ImportTest.class);
+		suite.addTestSuite(BibtexParserTest.class);
+		suite.addTestSuite(IsiImporterTest.class);
 		//$JUnit-END$
 		return suite;
 	}
