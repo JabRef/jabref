64586c27a0e519c81378e238fbf7a3a6e02ee5c2
mortenalver <mortenalver@users.sourceforge.net>
2010-02-01 17:04:40 +0000

    Added support for KOI8_R character set.

--- src/java/net/sf/jabref/Globals.java
+++ src/java/net/sf/jabref/Globals.java
@@ -112,6 +112,7 @@ public class Globals {
 		// String[]{});
 		new String[] { "ISO8859_1", "UTF8", "UTF-16", "ASCII", "Cp1250", "Cp1251", "Cp1252",
 			"Cp1253", "Cp1254", "Cp1257", "SJIS",
+            "KOI8_R", // Cyrillic
 			"EUC_JP", // Added Japanese encodings.
 			"Big5", "Big5_HKSCS", "GBK", "ISO8859_2", "ISO8859_3", "ISO8859_4", "ISO8859_5",
 			"ISO8859_6", "ISO8859_7", "ISO8859_8", "ISO8859_9", "ISO8859_13", "ISO8859_15" };
