fd61e96ebb711b6f1c177066a2139c75a0a4ec9e
Oliver Kopp <olly98@users.sourceforge.net>
2012-04-29 22:27:23 +0200

    Updated regexp for DOI

--- src/java/net/sf/jabref/Util.java
+++ src/java/net/sf/jabref/Util.java
@@ -3020,8 +3020,8 @@ public static boolean openExternalFileUnknown(JabRefFrame frame, BibtexEntry ent
         return targetName;
     }
     
-    // DOI-regexp provided by http://stackoverflow.com/a/10300246/873282
-    private static final String REGEXP_PLAINDOI = "(10[.][0-9]{4,}[^\\s\"/<>]*/[^\\s\"<>]+)";
+    // DOI-regexp provided by http://stackoverflow.com/a/10324802/873282
+    private static final String REGEXP_PLAINDOI = "(\\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![\"&\\'<>])\\S)+)\\b)";
     private static final String REGEXP_DOI_WITH_HTTP_PREFIX = "[^\\s]+?" + REGEXP_PLAINDOI;
 
     /**
