6e59804446f29661ef264e7f26a62c7488e74941
Simon Harrer <simon.harrer@gmail.com>
2014-03-18 11:16:46 +0100

    Code compiles again after adding catch clause for clone unsupported operation exception.

--- src/java/net/sf/jabref/BasePanel.java
+++ src/java/net/sf/jabref/BasePanel.java
@@ -624,6 +624,8 @@ public class BasePanel extends JPanel implements ClipboardOwner, FileUpdateListe
                                             (database, be, BasePanel.this));
                                 } catch (KeyCollisionException ex) {
                                     Util.pr("KeyCollisionException... this shouldn't happen.");
+                                } catch (CloneNotSupportedException e) {
+                                    throw new RuntimeException("should not happen", e);
                                 }
                             }
                           ce.end();
