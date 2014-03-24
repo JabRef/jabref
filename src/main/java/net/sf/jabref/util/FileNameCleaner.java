2388534ce4171ab94ff991c00b32f796229e2a2b
Simon Harrer <simon.harrer@gmail.com>
2014-03-24 15:55:57 +0100

    Formatting a class

--- src/main/java/net/sf/jabref/util/FileNameCleaner.java
+++ src/main/java/net/sf/jabref/util/FileNameCleaner.java
@@ -18,28 +18,32 @@ package net.sf.jabref.util;
 
 import java.util.Arrays;
 
-public class FileNameCleaner {
-final static int[] illegalChars = {34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47};
-static {
-    Arrays.sort(illegalChars);
-}
-
 /**
- * Replaces illegal characters in given fileName by '_'
- * 
- * @param badFileName the fileName to clean
- * @return a clean filename
+ * Replaces illegal characters in given file paths.
  */
-public static String cleanFileName(String badFileName) {
-    StringBuilder cleanName = new StringBuilder();
-    for (int i = 0; i < badFileName.length(); i++) {
-        int c = (int)badFileName.charAt(i);
-        if (Arrays.binarySearch(illegalChars, c) < 0) {
-            cleanName.append((char)c);
-        } else {
-        	cleanName.append('_');
-        }
+public class FileNameCleaner {
+    final static int[] illegalChars = {34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47};
+
+    static {
+        Arrays.sort(illegalChars);
     }
-    return cleanName.toString();
-}
+
+    /**
+     * Replaces illegal characters in given fileName by '_'
+     *
+     * @param badFileName the fileName to clean
+     * @return a clean filename
+     */
+    public static String cleanFileName(String badFileName) {
+        StringBuilder cleanName = new StringBuilder();
+        for (int i = 0; i < badFileName.length(); i++) {
+            int c = (int) badFileName.charAt(i);
+            if (Arrays.binarySearch(illegalChars, c) < 0) {
+                cleanName.append((char) c);
+            } else {
+                cleanName.append('_');
+            }
+        }
+        return cleanName.toString();
+    }
 }
