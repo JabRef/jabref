3eea40f8302a0f02eb004e7ae7a86179b439e9ef
Simon Harrer <simon.harrer@gmail.com>
2014-09-11 14:57:29 +0200

    Array with illegal chars is sorted already, so no need to sort at compile time. Additional minor refactorings

--- src/main/java/net/sf/jabref/util/FileNameCleaner.java
+++ src/main/java/net/sf/jabref/util/FileNameCleaner.java
@@ -22,11 +22,21 @@ import java.util.Arrays;
  * Replaces illegal characters in given file paths.
  */
 public class FileNameCleaner {
-    final static int[] illegalChars = {34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47};
 
-    static {
-        Arrays.sort(illegalChars);
-    }
+    /**
+     * MUST ALWAYS BE A SORTED ARRAY because it is used in a binary search
+     */
+    private final static int[] ILLEGAL_CHARS = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 34,
+            42, 47,
+            58,
+            60, 62, 63,
+            92,
+            124
+    };
 
     /**
      * Replaces illegal characters in given fileName by '_'
@@ -37,13 +47,18 @@ public class FileNameCleaner {
     public static String cleanFileName(String badFileName) {
         StringBuilder cleanName = new StringBuilder();
         for (int i = 0; i < badFileName.length(); i++) {
-            int c = (int) badFileName.charAt(i);
-            if (Arrays.binarySearch(illegalChars, c) < 0) {
-                cleanName.append((char) c);
+            char c = badFileName.charAt(i);
+            if (isCharLegal(c)) {
+                cleanName.append(c);
             } else {
                 cleanName.append('_');
             }
         }
         return cleanName.toString();
+    }
+
+    private static boolean isCharLegal(char c) {
+        int intValueOfChar = (int) c;
+        return Arrays.binarySearch(ILLEGAL_CHARS, intValueOfChar) < 0;
     }
 }
