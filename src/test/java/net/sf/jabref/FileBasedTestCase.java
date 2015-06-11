f7c87fda64e186082453127298e4cff0fdc076cf
Simon Harrer <simon.harrer@gmail.com>
2015-06-11 16:51:46 +0200

    Add messages to the assertions.

--- src/test/java/net/sf/jabref/FileBasedTestCase.java
+++ src/test/java/net/sf/jabref/FileBasedTestCase.java
@@ -20,8 +20,8 @@ public class FileBasedTestCase {
      * Will check if two paths are the same.
      */
     public static void assertEqualPaths(String path1, String path2) {
-        assertNotNull(path1);
-        assertNotNull(path2);
+        assertNotNull("first path must not be null", path1);
+        assertNotNull("second path must not be null", path2);
         assertEquals(path1.replaceAll("\\\\", "/"), path2.replaceAll("\\\\", "/"));
     }
 
