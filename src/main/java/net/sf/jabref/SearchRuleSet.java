11e6a7ee42e490059ef889754b752d554a8fa182
Simon Harrer <simon.harrer@gmail.com>
2015-06-23 10:47:06 +0200

    Enums can be iterated upon.

--- src/main/java/net/sf/jabref/SearchRuleSet.java
+++ src/main/java/net/sf/jabref/SearchRuleSet.java
@@ -26,7 +26,6 @@
  */
 package net.sf.jabref;
 
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.Vector;
 import java.util.regex.PatternSyntaxException;
@@ -34,7 +33,6 @@ import java.util.regex.PatternSyntaxException;
 public class SearchRuleSet implements SearchRule {
 
     protected final Vector<SearchRule> ruleSet = new Vector<SearchRule>();
-
 
     public void addRule(SearchRule newRule) {
         ruleSet.add(newRule);
@@ -47,19 +45,16 @@ public class SearchRuleSet implements SearchRule {
     @Override
     public int applyRule(Map<String, String> searchString, BibtexEntry bibtexEntry) throws PatternSyntaxException {
         int score = 0;
-        Enumeration<SearchRule> e = ruleSet.elements();
-        while (e.hasMoreElements()) {
-            score += e.nextElement().applyRule(searchString,
-                    bibtexEntry);
+        for (SearchRule searchRule : ruleSet) {
+            score += searchRule.applyRule(searchString, bibtexEntry);
         }
         return score;
     }
 
     @Override
     public boolean validateSearchStrings(Map<String, String> searchStrings) {
-        Enumeration<SearchRule> e = ruleSet.elements();
-        while (e.hasMoreElements()) {
-            if (!e.nextElement().validateSearchStrings(searchStrings)) {
+        for (SearchRule searchRule : ruleSet) {
+            if (!searchRule.validateSearchStrings(searchStrings)) {
                 return false;
             }
         }
