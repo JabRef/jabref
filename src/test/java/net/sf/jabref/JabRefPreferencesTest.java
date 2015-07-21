a61522cbca56d756c2ac172edbfdcc3f73cb1d5a
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-07-21 14:14:48 +0200

    Delete export test.
    Preferences export test cannot work since it exports local system paths which will necessarily be different on every machine.

--- src/test/java/net/sf/jabref/JabRefPreferencesTest.java
+++ src/test/java/net/sf/jabref/JabRefPreferencesTest.java
@@ -4,14 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.google.common.io.Files;
 
 public class JabRefPreferencesTest {
 
@@ -21,23 +16,6 @@ public class JabRefPreferencesTest {
     @Before
     public void setUp() {
         prefs = JabRefPreferences.getInstance();
-    }
-
-    @Test
-    public void testPreferencesExport() throws IOException {
-        File tmpFile = new File("src/test/resources/net/sf/jabref/preferencesTest.xml");
-        File expectedFile = new File("src/test/resources/net/sf/jabref/defaultPreferences.xml");
-
-        prefs.exportPreferences(tmpFile.getAbsolutePath());
-
-        List<String> actual = Files.readLines(tmpFile, Charset.defaultCharset());
-        List<String> expected = Files.readLines(expectedFile, Charset.defaultCharset());
-
-        tmpFile.delete();
-
-        for (int i = 0; i < actual.size(); i++) {
-            assertEquals(expected.get(i), actual.get(i));
-        }
     }
 
     @Test
