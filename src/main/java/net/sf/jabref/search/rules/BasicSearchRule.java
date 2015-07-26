9b010a4e933c90bf7d892c5347729d0875f70d64
Simon Harrer <simon.harrer@gmail.com>
2015-07-26 15:35:52 +0200

    Extract code to parse the query into words in separate method object. Added tests for this new object. A few minor refactorings improving the SentenceAnalyzer code, making it more concise.

--- src/main/java/net/sf/jabref/search/rules/BasicSearchRule.java
+++ src/main/java/net/sf/jabref/search/rules/BasicSearchRule.java
@@ -82,44 +82,60 @@ public class BasicSearchRule implements SearchRule {
         return true; // Matched all words.
     }
 
-    protected List<String> parseQuery(String query) {
-        StringBuffer sb = new StringBuffer();
-        List<String> result = new ArrayList<String>();
-        boolean escaped = false;
-        boolean quoted = false;
-        for (int i = 0; i < query.length(); i++) {
-            int c = query.charAt(i);
-            // Check if we are entering an escape sequence:
-            if (!escaped && (c == '\\')) {
-                escaped = true;
-            } else {
-                // See if we have reached the end of a word:
-                if (!escaped && !quoted && Character.isWhitespace((char) c)) {
-                    if (sb.length() > 0) {
-                        result.add(sb.toString());
-                        sb = new StringBuffer();
-                    }
-                } else if (c == '"') {
-                    // Whether it is a start or end quote, store the current
-                    // word if any:
-                    if (sb.length() > 0) {
-                        result.add(sb.toString());
-                        sb = new StringBuffer();
-                    }
-                    quoted = !quoted;
-                } else {
-                    // All other possibilities exhausted, we add the char to
-                    // the current word:
-                    sb.append((char) c);
-                }
-                escaped = false;
-            }
-        }
-        // Finished with the loop. If we have a current word, add it:
-        if (sb.length() > 0) {
-            result.add(sb.toString());
+    public static List<String> parseQuery(String query) {
+        return new SentenceAnalyzer(query).getWords();
+    }
+
+    public static class SentenceAnalyzer {
+
+        public static final char ESCAPE_CHAR = '\\';
+        public static final char QUOTE_CHAR = '"';
+
+        private final String query;
+
+        public SentenceAnalyzer(String query) {
+            this.query = query;
         }
 
-        return result;
+        public List<String> getWords() {
+            List<String> result = new ArrayList<String>();
+
+            StringBuffer sb = new StringBuffer();
+            boolean escaped = false;
+            boolean quoted = false;
+            for(char c : query.toCharArray()) {
+                // Check if we are entering an escape sequence:
+                if (!escaped && (c == ESCAPE_CHAR)) {
+                    escaped = true;
+                } else {
+                    // See if we have reached the end of a word:
+                    if (!escaped && !quoted && Character.isWhitespace(c)) {
+                        if (sb.length() > 0) {
+                            result.add(sb.toString());
+                            sb = new StringBuffer();
+                        }
+                    } else if (c == QUOTE_CHAR) {
+                        // Whether it is a start or end quote, store the current
+                        // word if any:
+                        if (sb.length() > 0) {
+                            result.add(sb.toString());
+                            sb = new StringBuffer();
+                        }
+                        quoted = !quoted;
+                    } else {
+                        // All other possibilities exhausted, we add the char to
+                        // the current word:
+                        sb.append(c);
+                    }
+                    escaped = false;
+                }
+            }
+            // Finished with the loop. If we have a current word, add it:
+            if (sb.length() > 0) {
+                result.add(sb.toString());
+            }
+
+            return result;
+        }
     }
 }
