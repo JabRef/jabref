1bc7c75864e8cee8bc6765aff4f06c9a42ccd483
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-07-30 11:54:20 +0200

    Move EntryEditorPrefsTab to gui.preftabs

--- src/main/java/net/sf/jabref/gui/preftabs/EntryEditorPrefsTab.java
+++ src/main/java/net/sf/jabref/gui/preftabs/EntryEditorPrefsTab.java
@@ -13,7 +13,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-package net.sf.jabref;
+package net.sf.jabref.gui.preftabs;
 
 import java.awt.BorderLayout;
 import java.awt.Insets;
@@ -22,7 +22,10 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import net.sf.jabref.gui.preftabs.PrefsTab;
+import net.sf.jabref.BasePanel;
+import net.sf.jabref.Globals;
+import net.sf.jabref.JabRefFrame;
+import net.sf.jabref.JabRefPreferences;
 import net.sf.jabref.logic.autocompleter.AutoCompleterFactory;
 import org.xnap.commons.gui.shortcut.EmacsKeyBindings;
 
