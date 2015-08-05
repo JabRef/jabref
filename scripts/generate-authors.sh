f29ef7c52a0e7e546082bf0e724501393420794a
stefan-kolb <stefan-kolb@web.de>
2015-08-05 06:17:44 -0700

    Only display author name for inclusion in About pages

--- scripts/generate-authors.sh
+++ scripts/generate-authors.sh
@@ -12,47 +12,47 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
 	# old manual entries 
 	read -d '' authors <<-"EOF" || true
-	Michel Baylac <>
-	Cyrille d'Haese <>
-	Ellen Reitmayr <>
-	Michael Beckmann <>
-	Oliver Beckmann <>
-	Fedor Bezrukov <>
-	Fabian Bieker <>
-	Aaron Chen <>
-	Fabrice Dessaint <>
-	Nathan Dunn <>
-	Alexis Gallagher <>
-	David Gleich <>
-	Bernd Kalbfuss <>
-	Martin Kähmer <>
-	Ervin Kolenovic <>
-	Krzysztof A. Kościuszkiewicz <>
-	Christian Kopf <>
-	Jeffrey Kuhn <>
-	Uwe Kuehn <>
-	Felix Langner <>
-	Stephan Lau <>
-	Alex Montgomery <>
-	Saverio Mori <>
-	Ambrogio Oliva <>
-	Stephan Rave <>
-	John Relph <>
-	Hannes Restel <>
-	Moritz Ringler <>
-	Rudolf Seemann <>
-	Toralf Senger <>
-	Manuel Siebeneicher <>
-	Mike Smoot <>
-	Ulrich Stärk <>
-	Martin Stolle <>
-	David Weitzman <>
-	John Zedlewski <>
-	Samin Muhammad Ridwanul Karim <>
-	Stefan Robert <>
+	Michel Baylac
+	Cyrille d'Haese
+	Ellen Reitmayr
+	Michael Beckmann
+	Oliver Beckmann
+	Fedor Bezrukov
+	Fabian Bieker
+	Aaron Chen
+	Fabrice Dessaint
+	Nathan Dunn
+	Alexis Gallagher
+	David Gleich
+	Bernd Kalbfuss
+	Martin Kähmer
+	Ervin Kolenovic
+	Krzysztof A. Kościuszkiewicz
+	Christian Kopf
+	Jeffrey Kuhn
+	Uwe Kuehn
+	Felix Langner
+	Stephan Lau
+	Alex Montgomery
+	Saverio Mori
+	Ambrogio Oliva
+	Stephan Rave
+	John Relph
+	Hannes Restel
+	Moritz Ringler
+	Rudolf Seemann
+	Toralf Senger
+	Manuel Siebeneicher
+	Mike Smoot
+	Ulrich Stärk
+	Martin Stolle
+	David Weitzman
+	John Zedlewski
+	Samin Muhammad Ridwanul Karim
+	Stefan Robert
 	EOF
 
 	# %aN = author name, %aE = author email
-	echo -e "$authors\n$(git log --format='%aN <%aE>')" | LC_ALL=C.UTF-8 sort --unique --ignore-case
+	echo -e "$authors\n$(git log --format='%aN')" | LC_ALL=C.UTF-8 sort --unique --ignore-case
 } > AUTHORS
 
