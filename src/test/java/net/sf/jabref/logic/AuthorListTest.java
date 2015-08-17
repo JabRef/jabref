dc978c140f7905aa7604a346baea53a3be6ec0de
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-08-17 16:11:50 +0200

    Fix and disable AuthorList test case
    The parsing logic in AuthorList.Author is broken and also terribly written (Even Intellij complains that it is too complex for its dataflow analyzer). Until somebody has the will to fix this, there is no point in executing this test.

--- src/test/java/net/sf/jabref/logic/AuthorListTest.java
+++ src/test/java/net/sf/jabref/logic/AuthorListTest.java
@@ -1,24 +1,23 @@
 package net.sf.jabref.logic;
 
-import net.sf.jabref.export.layout.format.CreateDocBookAuthors;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AuthorListTest {
 
+    @Ignore
     @Test
     public void authorListTest() {
         String authorString = "Olaf von Nilsen, Jr.";
         AuthorList authorList = AuthorList.getAuthorList(authorString);
         for (int i = 0; i < authorList.size(); i++) {
             AuthorList.Author author = authorList.getAuthor(i);
-            assertEquals("Jr.", author.getFirst());
-            assertEquals("Olaf von Nilsen", author.getLast());
-            assertEquals(null, author.getJr());
-            assertEquals(null, author.getVon());
+            assertEquals("Olaf", author.getFirst());
+            assertEquals("Nilsen", author.getLast());
+            assertEquals("Jr.", author.getJr());
+            assertEquals("von", author.getVon());
         }
-
-        assertEquals("<author><firstname>Jr.</firstname><surname>Olaf von Nilsen</surname></author>", new CreateDocBookAuthors().format(authorString));
     }
 }
