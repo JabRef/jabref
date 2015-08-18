e759a288d2dc9e7cafbcaeb262ae1eb81a75b327
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-08-18 12:39:52 +0200

    Update license headers

--- src/main/java/net/sf/jabref/gui/DuplicateResolverDialog.java
+++ src/main/java/net/sf/jabref/gui/DuplicateResolverDialog.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2003-2011 JabRef contributors.
+/*  Copyright (C) 2003-2015 JabRef contributors.
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
