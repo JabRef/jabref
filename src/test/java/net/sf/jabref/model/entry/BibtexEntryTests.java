dc474a23ec50c17ec1f0e1d0972edc5b8b159bd4
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-08-25 10:11:35 +0200

    Add minimal unit test for BibtexEntry

---
+++ src/test/java/net/sf/jabref/model/entry/BibtexEntryTests.java
@@ -0,0 +1,15 @@
+package net.sf.jabref.model.entry;
+
+
+import net.sf.jabref.JabRefPreferences;
+import org.junit.Test;
+
+public class BibtexEntryTests {
+
+    private BibtexEntry entry;
+
+    @Test
+    public void testDefaultConstructor() {
+        entry = new BibtexEntry();
+    }
+}
