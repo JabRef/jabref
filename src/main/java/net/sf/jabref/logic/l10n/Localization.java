4dff6effde9b1a94fa0ea2e175f412d80c4c0d01
Simon Harrer <simon.harrer@gmail.com>
2015-09-10 14:01:37 +0200

    Log problems when jabref falls back to the default locale of the system instead of using the user-specified locale.

--- src/main/java/net/sf/jabref/logic/l10n/Localization.java
+++ src/main/java/net/sf/jabref/logic/l10n/Localization.java
@@ -16,6 +16,7 @@ public class Localization {
     private static final String RESOURCE_PREFIX = "l10n/JabRef";
     private static final String MENU_RESOURCE_PREFIX = "l10n/Menu";
     private static final String INTEGRITY_RESOURCE_PREFIX = "l10n/IntegrityMessage";
+
     public static final String[] ALL_ENCODINGS = // (String[])
             // Charset.availableCharsets().keySet().toArray(new
             // String[]{});
@@ -72,9 +73,24 @@ public class Localization {
 
     public static void setLanguage(String language, String country) {
         Locale locale = new Locale(language, country);
+
         messages = ResourceBundle.getBundle(RESOURCE_PREFIX, locale, new EncodingControl("UTF-8"));
         menuTitles = ResourceBundle.getBundle(MENU_RESOURCE_PREFIX, locale, new EncodingControl("UTF-8"));
         intMessages = ResourceBundle.getBundle(INTEGRITY_RESOURCE_PREFIX, locale, new EncodingControl("UTF-8"));
+
+        // these checks are required as when the requested resource bundle is NOT found, the default locale is used as a fallback silently.
+        if(!messages.getLocale().equals(locale)) {
+            LOGGER.warn("tried loading <" + RESOURCE_PREFIX + "> for locale <" + locale + "> but had to fall back on default locale <" + Locale.getDefault() + ">");
+        }
+
+        if(!menuTitles.getLocale().equals(locale)) {
+            LOGGER.warn("tried loading <" + MENU_RESOURCE_PREFIX + "> for locale <" + locale + "> but had to fall back on default locale <" + Locale.getDefault() + ">");
+        }
+
+        if(!intMessages.getLocale().equals(locale)) {
+            LOGGER.warn("tried loading <" + INTEGRITY_RESOURCE_PREFIX + "> for locale <" + locale + "> but had to fall back on default locale <" + Locale.getDefault() + ">");
+        }
+
         Locale.setDefault(locale);
         javax.swing.JComponent.setDefaultLocale(locale);
     }
