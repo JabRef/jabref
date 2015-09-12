f47d237551a97cf24803159edbfee8a643ba174b
Tobias Diez <tobiasdiez@gmx.de>
2015-09-12 15:20:54 +0200

    Changes default preferences:  - Fields in table are not colored (required/optional)  - Owner column is not shown  - Slightly increasing row padding in table

--- src/main/java/net/sf/jabref/JabRefPreferences.java
+++ src/main/java/net/sf/jabref/JabRefPreferences.java
@@ -515,7 +515,7 @@ public class JabRefPreferences {
         defaults.put(AUTO_RESIZE_MODE, JTable.AUTO_RESIZE_ALL_COLUMNS);
         defaults.put(PREVIEW_PANEL_HEIGHT, 200);
         defaults.put(ENTRY_EDITOR_HEIGHT, 400);
-        defaults.put(TABLE_COLOR_CODES_ON, Boolean.TRUE);
+        defaults.put(TABLE_COLOR_CODES_ON, Boolean.FALSE);
         defaults.put(NAMES_AS_IS, Boolean.FALSE); // "Show names unchanged"
         defaults.put(NAMES_FIRST_LAST, Boolean.FALSE); // "Show 'Firstname Lastname'"
         defaults.put(NAMES_LAST_FIRST, Boolean.FALSE); // "Show 'Lastname, Firstname'"
@@ -554,8 +554,8 @@ public class JabRefPreferences {
         defaults.put(SIDE_PANE_COMPONENT_NAMES, "");
         defaults.put(SIDE_PANE_COMPONENT_PREFERRED_POSITIONS, "");
 
-        defaults.put(COLUMN_NAMES, "entrytype;author;title;year;journal;owner;timestamp;bibtexkey");
-        defaults.put(COLUMN_WIDTHS, "75;280;400;60;100;100;100;100");
+        defaults.put(COLUMN_NAMES, "entrytype;author;title;year;journal;timestamp;bibtexkey");
+        defaults.put(COLUMN_WIDTHS, "75;280;450;60;100;100;100");
         defaults.put(PersistenceTableColumnListener.ACTIVATE_PREF_KEY,
                 PersistenceTableColumnListener.DEFAULT_ENABLED);
         defaults.put(XMP_PRIVACY_FILTERS, "pdf;timestamp;keywords;owner;note;review");
