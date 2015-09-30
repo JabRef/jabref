3e57c8afb01838f9c69ec884f81025f662d16d18
Simon Harrer <simon.harrer@gmail.com>
2015-09-30 16:20:26 +0200

    A test now guarantees that none of the IEEE abbreviations is already in the internal journals list.

---
+++ src/test/java/net/sf/jabref/logic/journals/ShippedJournalAbbreviationDuplicateTest.java
@@ -0,0 +1,32 @@
+package net.sf.jabref.logic.journals;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class ShippedJournalAbbreviationDuplicateTest {
+
+    @Test
+    public void noDuplicatesInShippedJournalAbbreviations() {
+        JournalAbbreviationRepository repoBuiltIn = new JournalAbbreviationRepository();
+        repoBuiltIn.readJournalListFromResource(Abbreviations.JOURNALS_FILE_BUILTIN);
+
+        JournalAbbreviationRepository repoBuiltInIEEEOfficial = new JournalAbbreviationRepository();
+        repoBuiltInIEEEOfficial.readJournalListFromResource(Abbreviations.JOURNALS_IEEE_OFFICIAL_ABBREVIATION_LIST);
+
+        JournalAbbreviationRepository repoBuiltInIEEEStandard = new JournalAbbreviationRepository();
+        repoBuiltInIEEEStandard.readJournalListFromResource(Abbreviations.JOURNALS_IEEE_STANDARD_ABBREVIATION_LIST);
+
+        for(Abbreviation abbreviation : repoBuiltInIEEEOfficial.getAbbreviations()) {
+            Assert.assertFalse("duplicate name " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getName()).isPresent());
+            Assert.assertFalse("duplicate iso " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getIsoAbbreviation()).isPresent());
+            Assert.assertFalse("duplicate medline " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getMedlineAbbreviation()).isPresent());
+        }
+
+        for(Abbreviation abbreviation : repoBuiltInIEEEStandard.getAbbreviations()) {
+            Assert.assertFalse("duplicate name " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getName()).isPresent());
+            Assert.assertFalse("duplicate iso " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getIsoAbbreviation()).isPresent());
+            Assert.assertFalse("duplicate medline " + abbreviation.toString(), repoBuiltIn.getAbbreviation(abbreviation.getMedlineAbbreviation()).isPresent());
+        }
+    }
+
+}
