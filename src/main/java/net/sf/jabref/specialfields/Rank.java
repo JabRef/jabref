8ec6f562485790a3ea1b9ac04ae309d911301b81
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-10-22 11:08:12 +0200

    fix representing icon for Rank specialfield

--- src/main/java/net/sf/jabref/specialfields/Rank.java
+++ src/main/java/net/sf/jabref/specialfields/Rank.java
@@ -50,7 +50,7 @@ public class Rank extends SpecialField {
 
     @Override
     public Icon getRepresentingIcon() {
-        return IconTheme.getImage("ranking");
+        return IconTheme.JabRefIcon.RANKING.getSmallIcon();
     }
 
     @Override
