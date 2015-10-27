eaf1b5264a08c7e5718d9decb997fd43e968c1a2
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-10-27 11:03:38 +0100

    Add test for correct parsing and serialization of month field special syntax

--- src/test/java/net/sf/jabref/bibtex/BibtexEntryWriterTest.java
+++ src/test/java/net/sf/jabref/bibtex/BibtexEntryWriterTest.java
@@ -92,4 +92,33 @@ public class BibtexEntryWriterTest {
 
         assertEquals(bibtexEntry, actual);
     }
+
+    @Test
+    public void monthFieldSpecialSyntax() throws IOException {
+        String bibtexEntry = "@Article{test," + Globals.NEWLINE +
+                "  Author                   = {Foo Bar}," + Globals.NEWLINE +
+                "  Month                    = mar," + Globals.NEWLINE +
+                "  Number                   = {1}" + Globals.NEWLINE +
+                "}" + Globals.NEWLINE;
+
+        // read in bibtex string
+        ParserResult result = BibtexParser.parse(new StringReader(bibtexEntry));
+
+        Collection<BibtexEntry> entries = result.getDatabase().getEntries();
+        Assert.assertEquals(1, entries.size());
+
+        BibtexEntry entry = entries.iterator().next();
+        Assert.assertEquals("test", entry.getCiteKey());
+        Assert.assertEquals(4, entry.getAllFields().size());
+        Set<String> fields = entry.getAllFields();
+        Assert.assertTrue(fields.contains("month"));
+        Assert.assertEquals("#mar#", entry.getField("month"));
+
+        //write out bibtex string
+        StringWriter stringWriter = new StringWriter();
+        writer.write(entry, stringWriter);
+        String actual = stringWriter.toString();
+
+        assertEquals(bibtexEntry, actual);
+    }
 }
