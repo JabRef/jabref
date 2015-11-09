f56e6893907be22cfcfe8f99aa6d9d93f01cf249
mairdl <mairdl.dev@googlemail.com>
2015-11-09 13:15:49 +0100

    Create the test class of the InspecImporter

---
+++ src/test/java/net/sf/jabref/importer/fileformat/InspecImportTest.java
@@ -0,0 +1,67 @@
+package net.sf.jabref.importer.fileformat;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * This is the test class of the InspecImporter. It test all 4 methodes of the class.
+ *
+ * @author mairdl
+ * @author braunch
+ *
+ */
+public class InspecImportTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link net.sf.jabref.importer.fileformat.InspecImporter#isRecognizedFormat(java.io.InputStream)}.
+     */
+    @Test
+    public void testIsRecognizedFormat() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link net.sf.jabref.importer.fileformat.InspecImporter#importEntries(java.io.InputStream, net.sf.jabref.importer.OutputPrinter)}
+     * .
+     */
+    @Test
+    public void testImportEntries() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.sf.jabref.importer.fileformat.InspecImporter#getFormatName()}.
+     */
+    @Test
+    public void testGetFormatName() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link net.sf.jabref.importer.fileformat.InspecImporter#getCLIId()}.
+     */
+    @Test
+    public void testGetCLIId() {
+        fail("Not yet implemented");
+    }
+
+}
