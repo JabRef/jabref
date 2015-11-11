79bcebdc4ca75b9004c17c5b3a2d885d4599e57d
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2015-11-12 00:18:22 +0100

    Removed parseField and correspondings test. Moved tests related to StringUtil to correct file.

--- src/test/java/net/sf/jabref/util/UtilTest.java
+++ src/test/java/net/sf/jabref/util/UtilTest.java
@@ -17,6 +17,28 @@ import java.util.*;
 import java.util.List;
 public class UtilTest {
 
+    @Test
+    public void testGetPublicationDate() {
+
+        Assert.assertEquals("2003-02", net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, year = {2003}, month = #FEB# }")));
+
+        Assert.assertEquals("2003-03", net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, year = {2003}, month = 3 }")));
+
+        Assert.assertEquals("2003", net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, year = {2003}}")));
+
+        Assert.assertEquals(null, net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, month = 3 }")));
+
+        Assert.assertEquals(null, net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, author={bla}}")));
+
+        Assert.assertEquals("2003-12", net.sf.jabref.util.Util.getPublicationDate(BibtexParser
+                .singleFromString("@ARTICLE{HipKro03, year = {03}, month = #DEC# }")));
+
+    }
 
 
     @Test
