5b66b9868e4c7ca97361ba44c2f2f89131828765
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-11-13 10:37:14 +0100

    Fix several resource leaks in CopacImporterTest

--- src/test/java/net/sf/jabref/importer/fileformat/CopacImporterTest.java
+++ src/test/java/net/sf/jabref/importer/fileformat/CopacImporterTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class CopacImporterTest {
@@ -25,23 +26,26 @@ public class CopacImporterTest {
     public void testIsRecognizedFormat() throws IOException {
 
         CopacImporter importer = new CopacImporter();
-        Assert.assertTrue(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("CopacImporterTest1.txt")));
+        try (InputStream stream1 = CopacImporterTest.class
+                .getResourceAsStream("CopacImporterTest1.txt"); InputStream stream2 = CopacImporterTest.class
+                .getResourceAsStream("CopacImporterTest2.txt"); InputStream stream3 = CopacImporterTest.class
+                .getResourceAsStream("IsiImporterTest1.isi"); InputStream stream4 = CopacImporterTest.class
+                .getResourceAsStream("IsiImporterTestInspec.isi"); InputStream stream5 = CopacImporterTest.class
+                .getResourceAsStream("IsiImporterTestWOS.isi"); InputStream stream6 = CopacImporterTest.class
+                .getResourceAsStream("IsiImporterTestMedline.isi")) {
 
-        Assert.assertTrue(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("CopacImporterTest2.txt")));
+            Assert.assertTrue(importer.isRecognizedFormat(stream1));
 
-        Assert.assertFalse(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("IsiImporterTest1.isi")));
+            Assert.assertTrue(importer.isRecognizedFormat(stream2));
 
-        Assert.assertFalse(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("IsiImporterTestInspec.isi")));
+            Assert.assertFalse(importer.isRecognizedFormat(stream3));
 
-        Assert.assertFalse(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("IsiImporterTestWOS.isi")));
+            Assert.assertFalse(importer.isRecognizedFormat(stream4));
 
-        Assert.assertFalse(importer.isRecognizedFormat(CopacImporterTest.class
-                .getResourceAsStream("IsiImporterTestMedline.isi")));
+            Assert.assertFalse(importer.isRecognizedFormat(stream5));
+
+            Assert.assertFalse(importer.isRecognizedFormat(stream6));
+        }
     }
 
     @Test
@@ -50,34 +54,38 @@ public class CopacImporterTest {
 
         CopacImporter importer = new CopacImporter();
 
-        List<BibtexEntry> entries = importer.importEntries(CopacImporterTest.class
-                .getResourceAsStream("CopacImporterTest1.txt"), new OutputPrinterToNull());
-        Assert.assertEquals(1, entries.size());
-        BibtexEntry entry = entries.get(0);
+        try (InputStream stream = CopacImporterTest.class
+                .getResourceAsStream("CopacImporterTest1.txt")) {
+            List<BibtexEntry> entries = importer.importEntries(stream, new OutputPrinterToNull());
+            Assert.assertEquals(1, entries.size());
+            BibtexEntry entry = entries.get(0);
 
-        Assert.assertEquals("The SIS project : software reuse with a natural language approach", entry.getField("title"));
-        Assert.assertEquals(
-                "Prechelt, Lutz and Universität Karlsruhe. Fakultät für Informatik",
-                entry.getField("author"));
-        Assert.assertEquals("Interner Bericht ; Nr.2/92", entry.getField("series"));
-        Assert.assertEquals("1992", entry.getField("year"));
-        Assert.assertEquals("Karlsruhe :  Universitat Karlsruhe, Fakultat fur Informatik", entry.getField("publisher"));
-        Assert.assertEquals(BibtexEntryTypes.BOOK, entry.getType());
+            Assert.assertEquals("The SIS project : software reuse with a natural language approach", entry.getField("title"));
+            Assert.assertEquals(
+                    "Prechelt, Lutz and Universität Karlsruhe. Fakultät für Informatik",
+                    entry.getField("author"));
+            Assert.assertEquals("Interner Bericht ; Nr.2/92", entry.getField("series"));
+            Assert.assertEquals("1992", entry.getField("year"));
+            Assert.assertEquals("Karlsruhe :  Universitat Karlsruhe, Fakultat fur Informatik", entry.getField("publisher"));
+            Assert.assertEquals(BibtexEntryTypes.BOOK, entry.getType());
+        }
     }
 
     @Test
     public void testImportEntries2() throws IOException {
         CopacImporter importer = new CopacImporter();
 
-        List<BibtexEntry> entries = importer.importEntries(CopacImporterTest.class
-                .getResourceAsStream("CopacImporterTest2.txt"), new OutputPrinterToNull());
-        Assert.assertEquals(2, entries.size());
-        BibtexEntry one = entries.get(0);
+        try (InputStream stream = CopacImporterTest.class
+                .getResourceAsStream("CopacImporterTest2.txt")) {
+            List<BibtexEntry> entries = importer.importEntries(stream, new OutputPrinterToNull());
+            Assert.assertEquals(2, entries.size());
+            BibtexEntry one = entries.get(0);
 
-        Assert.assertEquals("Computing and operational research at the London Hospital", one.getField("title"));
+            Assert.assertEquals("Computing and operational research at the London Hospital", one.getField("title"));
 
-        BibtexEntry two = entries.get(1);
+            BibtexEntry two = entries.get(1);
 
-        Assert.assertEquals("Real time systems : management and design", two.getField("title"));
+            Assert.assertEquals("Real time systems : management and design", two.getField("title"));
+        }
     }
 }
