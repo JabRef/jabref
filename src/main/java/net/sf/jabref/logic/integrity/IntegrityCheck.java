ee83369a6a8d25b6f0451d526aae0e45d2848cbe
Stefan Kolb <stefan-kolb@web.de>
2015-11-16 18:49:19 +0100

    Describe Regex

--- src/main/java/net/sf/jabref/logic/integrity/IntegrityCheck.java
+++ src/main/java/net/sf/jabref/logic/integrity/IntegrityCheck.java
@@ -168,7 +168,19 @@ public class IntegrityCheck {
      * a single dash (as in 7-33) to the double dash used in TEX to denote number ranges (as in 7--33).
      */
     private static class PagesChecker implements Checker {
-        private static final Predicate<String> VALID_PAGE_NUMBER = Pattern.compile("\\A\\d+(?:\\+|\\-{2}\\d+)?(?:,\\d+(?:\\+|\\-{2}\\d+)?)*\\z").asPredicate();
+        private static final String PAGES_EXP = ""
+                + "\\A"                       // begin String
+                + "\\d+"                      // number
+                + "(?:"                       // non-capture group
+                + "\\+|\\-{2}\\d+"            // + or --number (range)
+                + ")?"                        // optional group
+                + "(?:"                       // non-capture group
+                + ","                         // comma
+                + "\\d+(?:\\+|\\-{2}\\d+)?"   // repeat former pattern
+                + ")*"                        // repeat group 0,*
+                + "\\z";                      // end String
+
+        private static final Predicate<String> VALID_PAGE_NUMBER = Pattern.compile(PAGES_EXP).asPredicate();
 
         /**
          * Checks, if the page numbers String conforms to the BibTex manual
