9497bb6afb63bbfd467a17dc15adecaf14f754c1
Simon Harrer <simon.harrer@gmail.com>
2015-11-24 14:46:01 +0100

    Use install4j to build the setup files. This enables bundling of JREs and Win7+ features like pin to taskbar

---
+++ scripts/prepare-install4j.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ensure that downloads directory exists
+if [ ! -d ~/downloads ]; then
+  mkdir ~/downloads
+fi
+
+# ensure that tar archive of install4j exists
+cd ~/downloads
+if [ ! -f install4j_unix_6_0_4.tar.gz ]; then
+  wget --quiet http://download-aws.ej-technologies.com/install4j/install4j_unix_6_0_4.tar.gz
+fi;
+
+# extract tar archive of install4j into the source directory of JabRef
+cd ~/jabref
+tar -xzf ~/downloads/install4j_unix_6_0_4.tar.gz
+
+# fetch JREs
+if [ ! -d ~/.install4j6/jres/ ]; then
+  mkdir -p ~/.install4j6/jres/
+fi
+cd ~/.install4j6/jres/
+wget --quiet -nc http://files.jabref.org/jres/windows-x86-1.8.0_66.tar.gz
+wget --quiet -nc http://files.jabref.org/jres/windows-amd64-1.8.0_66.tar.gz
+wget --quiet -nc http://files.jabref.org/jres/macosx-amd64-1.8.0_66_unpacked.tar.gz
