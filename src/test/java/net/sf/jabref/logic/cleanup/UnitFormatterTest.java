05eb69de1be3e53b9184ac880740678d8abdc30c
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2015-12-01 00:27:20 +0100

    Added tests

---
+++ src/test/java/net/sf/jabref/logic/cleanup/UnitFormatterTest.java
@@ -0,0 +1,20 @@
+package net.sf.jabref.logic.cleanup;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class UnitFormatterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
