f27360bdfdcfc1eb06fa55ae8bf329759b3000f0
Stefan Kolb <stefan-kolb@web.de>
2015-12-10 16:50:57 +0100

    Add test for unknown custom entry types

--- src/test/java/net/sf/jabref/model/database/BibDatabaseTypeDetectionTest.java
+++ src/test/java/net/sf/jabref/model/database/BibDatabaseTypeDetectionTest.java
@@ -3,8 +3,10 @@ package net.sf.jabref.model.database;
 import net.sf.jabref.model.entry.BibLatexEntryTypes;
 import net.sf.jabref.model.entry.BibEntry;
 import net.sf.jabref.model.entry.BibtexEntryTypes;
+import net.sf.jabref.model.entry.CustomEntryType;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -40,6 +42,14 @@ public class BibDatabaseTypeDetectionTest {
     public void detectUndistinguishableAsBibtex() {
         BibEntry entry = new BibEntry("someid", BibtexEntryTypes.ARTICLE);
         entry.setField("title", "My cool paper");
+        Collection<BibEntry> entries = Arrays.asList(entry);
+
+        assertEquals(BibDatabaseType.BIBTEX, BibDatabaseTypeDetection.inferType(entries));
+    }
+
+    @Test
+    public void detectUnknownTypeAsBibtex() {
+        BibEntry entry = new BibEntry("someid", new CustomEntryType("unknowntype", new ArrayList<>(0), new ArrayList<>(0)));
         Collection<BibEntry> entries = Arrays.asList(entry);
 
         assertEquals(BibDatabaseType.BIBTEX, BibDatabaseTypeDetection.inferType(entries));
