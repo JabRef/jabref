ee485f98436d050aa66f0cc5385c057adc373ae4
Stefan Kolb <stefan-kolb@web.de>
2015-12-11 02:00:47 +0100

    Removes unused code

--- src/main/java/net/sf/jabref/external/FindFullText.java
+++ src/main/java/net/sf/jabref/external/FindFullText.java
@@ -101,40 +101,4 @@ public class FindFullText {
             return url;
         }
     }
-
-    public static String loadPage(URL url) throws IOException {
-        Reader in = null;
-        URLConnection uc;
-        HttpURLConnection huc = null;
-        try {
-            uc = url.openConnection();
-            if (uc instanceof HttpURLConnection) {
-                huc = (HttpURLConnection) uc;
-                huc.setInstanceFollowRedirects(false);
-                huc.connect();
-
-                in = new InputStreamReader(huc.getInputStream());
-                StringBuilder sb = new StringBuilder();
-                int c;
-                while ((c = in.read()) != -1) {
-                    sb.append((char) c);
-                }
-                return sb.toString();
-            } else {
-                return null; // TODO: are other types of connection (https?) relevant?
-            }
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-                if (huc != null) {
-                    huc.disconnect();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-    }
 }
