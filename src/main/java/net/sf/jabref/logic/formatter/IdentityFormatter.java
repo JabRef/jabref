a54af0a9727c8cf0784eabe28109f267d1627fa8
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2016-01-26 17:43:58 +0100

    Add initial version of save actions feature

---
+++ src/main/java/net/sf/jabref/logic/formatter/IdentityFormatter.java
@@ -0,0 +1,17 @@
+package net.sf.jabref.logic.formatter;
+
+/**
+ * It may seem useless, but is needed as a fallback option
+ */
+public class IdentityFormatter implements Formatter {
+
+    @Override
+    public String getName() {
+        return "IdentityFormatter";
+    }
+
+    @Override
+    public String format(String value) {
+        return value;
+    }
+}
