efaf99cb45e8800eee6e4e0dec5063d110bd9af4
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-01-31 16:36:15 +0100

    Fixed #751

--- src/main/java/net/sf/jabref/specialfields/SpecialFieldValue.java
+++ src/main/java/net/sf/jabref/specialfields/SpecialFieldValue.java
@@ -102,10 +102,7 @@ public class SpecialFieldValue {
 
     public SpecialFieldAction getAction(JabRefFrame frame) {
         if (this.action == null) {
-            action = new SpecialFieldAction(
-                    frame,
-                    this.field,
- this.getFieldValue().get(),
+            action = new SpecialFieldAction(frame, this.field, this.getFieldValue().orElse(null),
                     // if field contains only one value, it has to be nulled
                     // otherwise, another setting does not empty the field
                     this.field.getValues().size() == 1,
