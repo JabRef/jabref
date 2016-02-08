JabRef/jabref

commit 81f2a687010c4da31f1808133ae03fb3ef8cd0b9

Changes based on suggestions and further cleanup

src/main/java/net/sf/jabref/logic/labelPattern/LabelPatternUtil.java
@@ -433,10 +433,8 @@ public class LabelPatternUtil {
             String entryType = entry.getType();
             // Get the arrayList corresponding to the type
             typeList = metaData.getLabelPattern().getValue(entryType);
-            int typeListSize = typeList.size();
             boolean field = false;
-            for (int i = 1; i < typeListSize; i++) {
-                String typeListEntry = typeList.get(i);
+            for (String typeListEntry : typeList) {
                 if ("[".equals(typeListEntry)) {
                     field = true;
                 } else if ("]".equals(typeListEntry)) {
@@ -883,7 +881,7 @@ public class LabelPatternUtil {
      */
     static String firstAuthor(String authorField) {
         AuthorList authorList = AuthorList.getAuthorList(authorField);
-        if (authorList.size() == 0) {
+        if (authorList.isEmpty()) {
             return "";
         }
         String s = authorList.getAuthor(0).getLast();
@@ -904,7 +902,7 @@ public class LabelPatternUtil {
      */
     static String firstAuthorForenameInitials(String authorField) {
         AuthorList authorList = AuthorList.getAuthorList(authorField);
-        if (authorList.size() == 0) {
+        if (authorList.isEmpty()) {
             return "";
         }
         String s = authorList.getAuthor(0).getFirstAbbr();
@@ -925,7 +923,7 @@ public class LabelPatternUtil {
      */
     static String firstAuthorVonAndLast(String authorField) {
         AuthorList authorList = AuthorList.getAuthorList(authorField);
-        if (authorList.size() == 0) {
+        if (authorList.isEmpty()) {
             return "";
         }
         String vonAuthor = authorList.getAuthor(0).getVon().replaceAll(" ", "");
@@ -969,7 +967,7 @@ public class LabelPatternUtil {
      */
     static String lastAuthorForenameInitials(String authorField) {
         AuthorList authorList = AuthorList.getAuthorList(authorField);
-        if (authorList.size() == 0) {
+        if (authorList.isEmpty()) {
             return "";
         }
         String s = authorList.getAuthor(authorList.size() - 1).getFirstAbbr();

src/main/java/net/sf/jabref/JabRef.java
@@ -25,12 +25,12 @@ import net.sf.jabref.importer.*;
 import net.sf.jabref.importer.fetcher.EntryFetcher;
 import net.sf.jabref.importer.fetcher.EntryFetchers;
 import net.sf.jabref.logic.CustomEntryTypesManager;
-import net.sf.jabref.logic.ProxyPreferences;
-import net.sf.jabref.logic.ProxyRegisterer;
 import net.sf.jabref.logic.journals.Abbreviations;
 import net.sf.jabref.logic.l10n.Localization;
 import net.sf.jabref.logic.labelPattern.LabelPatternUtil;
 import net.sf.jabref.logic.logging.JabRefLogger;
+import net.sf.jabref.logic.net.ProxyPreferences;
+import net.sf.jabref.logic.net.ProxyRegisterer;
 import net.sf.jabref.logic.preferences.LastFocusedTabPreferences;
 import net.sf.jabref.logic.remote.RemotePreferences;
 import net.sf.jabref.logic.remote.client.RemoteListenerClient;

src/main/java/net/sf/jabref/MetaData.java
@@ -274,8 +274,8 @@ public class MetaData implements Iterable<String> {
             sb.append(Globals.NEWLINE).append(Globals.NEWLINE);
             List<String> orderedData = metaData.get(key);
             sb.append("@comment{").append(META_FLAG).append(key).append(':');
-            for (int j = 0; j < orderedData.size(); j++) {
-                sb.append(StringUtil.quote(orderedData.get(j), ";", '\\')).append(';');
+            for (String data : orderedData) {
+                sb.append(StringUtil.quote(data, ";", '\\')).append(';');
             }
             sb.append('}');
 

src/main/java/net/sf/jabref/gui/JabRefFrame.java
@@ -453,7 +453,7 @@ public class JabRefFrame extends JFrame implements OutputPrinter {
     private final AbstractAction manageJournals = new ManageJournalsAction(this);
     private final AbstractAction databaseProperties = new DatabasePropertiesAction();
     private final AbstractAction bibtexKeyPattern = new BibtexKeyPatternAction();
-    private final AbstractAction errorConsole = new ErrorConsoleAction(this, Globals.streamEavesdropper, GuiAppender.cache);
+    private final AbstractAction errorConsole = new ErrorConsoleAction(this, Globals.streamEavesdropper, GuiAppender.CACHE);
 
     private final AbstractAction dbConnect = new GeneralAction(Actions.DB_CONNECT,
             Localization.menuTitle("Connect to external SQL database"),

src/main/java/net/sf/jabref/gui/preftabs/NetworkTab.java
@@ -29,9 +29,9 @@ import com.jgoodies.forms.builder.FormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
 
 import net.sf.jabref.JabRefPreferences;
-import net.sf.jabref.logic.ProxyPreferences;
-import net.sf.jabref.logic.ProxyRegisterer;
 import net.sf.jabref.logic.l10n.Localization;
+import net.sf.jabref.logic.net.ProxyPreferences;
+import net.sf.jabref.logic.net.ProxyRegisterer;
 
 public class NetworkTab extends JPanel implements PrefsTab {
 

src/main/java/net/sf/jabref/logic/logging/GuiAppender.java
@@ -1,6 +1,5 @@
 package net.sf.jabref.logic.logging;
 
-import net.sf.jabref.logic.logging.Cache;
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
@@ -16,7 +15,7 @@ import java.io.Serializable;
 @Plugin(name = "GuiAppender", category = "Core", elementType = "appender", printObject = true)
 public class GuiAppender extends AbstractAppender {
 
-    public static final Cache cache = new Cache();
+    public static final Cache CACHE = new Cache();
 
     protected GuiAppender(String name, Filter filter, Layout<? extends Serializable> layout) {
         super(name, filter, layout);
@@ -41,7 +40,7 @@ public class GuiAppender extends AbstractAppender {
 
     @Override
     public void append(LogEvent event) {
-        cache.add(new String(this.getLayout().toByteArray(event)));
+        CACHE.add(new String(this.getLayout().toByteArray(event)));
     }
 
 }

src/main/java/net/sf/jabref/logic/net/ProxyPreferences.java
@@ -1,15 +1,15 @@
-package net.sf.jabref.logic;
+package net.sf.jabref.logic.net;
 
 import net.sf.jabref.JabRefPreferences;
 
 public class ProxyPreferences {
 
-    private Boolean useProxy;
-    private String hostname;
-    private String port;
-    private Boolean useAuthentication;
-    private String username;
-    private String password;
+    private final Boolean useProxy;
+    private final String hostname;
+    private final String port;
+    private final Boolean useAuthentication;
+    private final String username;
+    private final String password;
 
     public ProxyPreferences(Boolean useProxy, String hostname, String port, Boolean useAuthentication, String username,
             String password) {

src/main/java/net/sf/jabref/logic/net/ProxyRegisterer.java
@@ -1,4 +1,4 @@
-package net.sf.jabref.logic;
+package net.sf.jabref.logic.net;
 
 public class ProxyRegisterer {
 

commit 758831d64b5d7ffbab12e12a1d72e0b2404bce4b

Fix a small bug in citekey generator
Generating a new key would prepend the pattern which resulted for
example in `[author]Weinstein` instead of the expected `Weinstein`.
The reason for the bug was that the list returned by
`getLabelPattern().getValue(entryType)` contains as the first item the
complete pattern.

This bug was introduced in
https://github.com/oscargus/jabref/commit/81f2a687010c4da31f1808133ae03fb3ef8cd0b9

src/main/java/net/sf/jabref/logic/labelPattern/LabelPatternUtil.java
@@ -422,7 +422,6 @@ public class LabelPatternUtil {
      */
     public static void makeLabel(MetaData metaData, BibDatabase dBase, BibEntry entry) {
         LabelPatternUtil.database = dBase;
-        List<String> typeList;
         String key;
         StringBuilder stringBuilder = new StringBuilder();
         boolean forceUpper = false;
@@ -432,7 +431,10 @@ public class LabelPatternUtil {
             // get the type of entry
             String entryType = entry.getType();
             // Get the arrayList corresponding to the type
-            typeList = metaData.getLabelPattern().getValue(entryType);
+            List<String> typeList = new ArrayList<>(metaData.getLabelPattern().getValue(entryType));
+            if(! typeList.isEmpty()) {
+                typeList.remove(0);
+            }
             boolean field = false;
             for (String typeListEntry : typeList) {
                 if ("[".equals(typeListEntry)) {