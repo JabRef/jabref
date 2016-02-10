a3628ee2ed482671f2a9cb2cad4bcee613d16292
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2016-02-10 18:35:18 +0100

    Add enablement feature for save actions

--- src/main/java/net/sf/jabref/exporter/SaveActions.java
+++ src/main/java/net/sf/jabref/exporter/SaveActions.java
@@ -20,28 +20,47 @@ public class SaveActions {
 
     public static final String META_KEY = "saveActions";
 
+    private boolean enabled;
+
     public SaveActions(MetaData metaData) {
         actions = new HashMap<>();
         setAvailableFormatters();
 
         List<String> formatters = metaData.getData(META_KEY);
-        if(formatters==null) {
+        if (formatters == null) {
             // no save actions defined in the meta data
             return;
         } else {
-            for (int i = 0; i < formatters.size(); i += 2) {
-                try {
-                    String field = formatters.get(i);
-                    Formatter formatter = getFormatterFromString(formatters.get(i + 1));
+            parseEnabledStatus(formatters);
 
-                    actions.put(field, formatter);
-                } catch (IndexOutOfBoundsException e) {
-                    // the meta data string in the file is broken. -> Ignore the last item
-                    break;
-                }
-            }
+            parseSaveActions(formatters);
         }
 
+    }
+
+    private void parseSaveActions(List<String> formatters) {
+        //read concrete actions
+        for (int i = 0; i < formatters.size(); i += 2) {
+            try {
+                String field = formatters.get(i);
+                Formatter formatter = getFormatterFromString(formatters.get(i + 1));
+
+                actions.put(field, formatter);
+            } catch (IndexOutOfBoundsException e) {
+                // the meta data string in the file is broken. -> Ignore the last item
+                break;
+            }
+        }
+    }
+
+    private void parseEnabledStatus(List<String> formatters) {
+        //read if save actions should be enabled
+        String enableActions = formatters.get(0);
+        if ("enabled".equals(enableActions)) {
+            enabled = true;
+        } else {
+            enabled = false;
+        }
     }
 
     private void setAvailableFormatters() {
@@ -52,15 +71,24 @@ public class SaveActions {
     }
 
     public BibEntry applySaveActions(BibEntry entry) {
-        for (String key : actions.keySet()) {
-            Formatter formatter = actions.get(key);
-
-            String fieldContent = entry.getField(key);
-            String formattedContent = formatter.format(fieldContent);
-            entry.setField(key, formattedContent);
+        if (enabled) {
+            applyAllActions(entry);
         }
-
         return entry;
+    }
+
+    private void applyAllActions(BibEntry entry) {
+        for (String key : actions.keySet()) {
+            applyActionForField(entry, key);
+        }
+    }
+
+    private void applyActionForField(BibEntry entry, String key) {
+        Formatter formatter = actions.get(key);
+
+        String fieldContent = entry.getField(key);
+        String formattedContent = formatter.format(fieldContent);
+        entry.setField(key, formattedContent);
     }
 
     private Formatter getFormatterFromString(String formatterName) {
