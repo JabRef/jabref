a7f15f57b06fe83c4a53383835dbf235a1206694
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2016-02-22 16:51:23 +0100

    Remove unused imports in SaveActionsPanel

--- src/main/java/net/sf/jabref/gui/dbproperties/SaveActionsPanel.java
+++ src/main/java/net/sf/jabref/gui/dbproperties/SaveActionsPanel.java
@@ -14,8 +14,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.stream.Collectors;
