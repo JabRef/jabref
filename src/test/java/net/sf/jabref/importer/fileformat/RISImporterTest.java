fbe4d2413e34a315a47b2476f451178132d7bc54
Admir Obralija <admir.obralija@mathematik.uni-stuttgart.de>
2016-03-01 16:28:23 +0100

    test RISImporter

--- src/test/java/net/sf/jabref/importer/fileformat/RISImporterTest.java
+++ src/test/java/net/sf/jabref/importer/fileformat/RISImporterTest.java
@@ -2,108 +2,39 @@ package net.sf.jabref.importer.fileformat;
 
 import net.sf.jabref.*;
 
-import net.sf.jabref.importer.OutputPrinterToNull;
-import net.sf.jabref.model.entry.BibEntry;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
 
-/**
- * Test cases for the RISImporter
- *
- * @author $Author: coezbek $
- */
 public class RISImporterTest {
+
+    private RisImporter risImporter;
 
     @Before
     public void setUp() throws Exception {
-        if (Globals.prefs == null) {
-            Globals.prefs = JabRefPreferences.getInstance();
-        }
+        Globals.prefs = JabRefPreferences.getInstance();
+        risImporter = new RisImporter();
     }
 
     @Test
-    public void testIsRecognizedFormat() throws IOException {
-
-        RisImporter importer = new RisImporter();
-        try (InputStream stream = RISImporterTest.class
-                .getResourceAsStream("RisImporterTest1.ris")) {
-            Assert.assertTrue(importer.isRecognizedFormat(stream));
-        }
+    public void testGetFormatName() {
+        Assert.assertEquals(risImporter.getFormatName(), "RIS");
     }
 
     @Test
-    public void testProcessSubSup() {
-
-        HashMap<String, String> hm = new HashMap<>();
-        hm.put("title", "/sub 3/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_3$", hm.get("title"));
-
-        hm.put("title", "/sub   3   /");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_3$", hm.get("title"));
-
-        hm.put("title", "/sub 31/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_{31}$", hm.get("title"));
-
-        hm.put("abstract", "/sub 3/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_3$", hm.get("abstract"));
-
-        hm.put("review", "/sub 31/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_{31}$", hm.get("review"));
-
-        hm.put("title", "/sup 3/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$^3$", hm.get("title"));
-
-        hm.put("title", "/sup 31/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$^{31}$", hm.get("title"));
-
-        hm.put("abstract", "/sup 3/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$^3$", hm.get("abstract"));
-
-        hm.put("review", "/sup 31/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$^{31}$", hm.get("review"));
-
-        hm.put("title", "/sub $Hello/");
-        IsiImporter.processSubSup(hm);
-        Assert.assertEquals("$_{\\$Hello}$", hm.get("title"));
+    public void testGetCLIId() {
+        Assert.assertEquals(risImporter.getCLIId(), "ris");
     }
 
     @Test
-    public void testImportEntries() throws IOException {
-        RisImporter importer = new RisImporter();
-
-        try (InputStream stream = RISImporterTest.class
-                .getResourceAsStream("RisImporterTest1.ris")) {
-            List<BibEntry> entries = importer.importEntries(stream, new OutputPrinterToNull());
-            Assert.assertEquals(1, entries.size());
-            BibEntry entry = entries.get(0);
-            Assert.assertEquals("Editorial: Open Source and Empirical Software Engineering", entry
-                    .getField("title"));
-            Assert.assertEquals(
-                    "Harrison, Warren",
-                    entry.getField("author"));
-
-            Assert.assertEquals("article", entry.getType());
-            Assert.assertEquals("Empirical Software Engineering", entry.getField("journal"));
-            Assert.assertEquals("2001", entry.getField("year"));
-            Assert.assertEquals("6", entry.getField("volume"));
-            Assert.assertEquals("3", entry.getField("number"));
-            Assert.assertEquals("193--194", entry.getField("pages"));
-            Assert.assertEquals("#sep#", entry.getField("month"));
+    public void testIfNotRecognizedFormat() throws IOException {
+        try (InputStream stream = RISImporterTest.class.getResourceAsStream("RisImporterCorrupted.ris")) {
+            Assert.assertFalse(risImporter.isRecognizedFormat(stream));
         }
     }
+
 }
