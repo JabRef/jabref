d11b9679426fc0cba6168b0eb12a6ba21aaea54a
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-03-08 22:34:45 +0100

    More lambdas

--- src/main/java/net/sf/jabref/gui/actions/BaseAction.java
+++ src/main/java/net/sf/jabref/gui/actions/BaseAction.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2003-2011 JabRef contributors.
+/*  Copyright (C) 2003-2016 JabRef contributors.
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -20,6 +20,7 @@ package net.sf.jabref.gui.actions;
  * base frame through runCommand(). runCommand() finds the
  * appropriate BaseAction object, and runs its action() method.
  */
+@FunctionalInterface
 public interface BaseAction {
 
     void action() throws Throwable;
