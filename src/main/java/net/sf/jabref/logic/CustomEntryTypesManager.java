653fad96673ffe02da7ee37e866bbd518b2d62c4
Tobias Diez <tobiasdiez@gmx.de>
2016-03-08 15:28:38 +0100

    Move parseEntryType back to CustomEntryTypesManager

--- src/main/java/net/sf/jabref/logic/CustomEntryTypesManager.java
+++ src/main/java/net/sf/jabref/logic/CustomEntryTypesManager.java
@@ -49,4 +49,23 @@ public class CustomEntryTypesManager {
         // has decreased.
         prefs.purgeCustomEntryTypes(number);
     }
+
+    public static CustomEntryType parseEntryType(String comment) {
+        String rest = comment.substring(CustomEntryType.ENTRYTYPE_FLAG.length());
+        int indexEndOfName = rest.indexOf(':');
+        if(indexEndOfName < 0) {
+            return null;
+        }
+        String fieldsDescription = rest.substring(indexEndOfName + 2);
+
+        int indexEndOfRequiredFields = fieldsDescription.indexOf(']');
+        int indexEndOfOptionalFields = fieldsDescription.indexOf(']', indexEndOfRequiredFields + 1);
+        if (indexEndOfRequiredFields < 4 || indexEndOfOptionalFields < indexEndOfRequiredFields + 6) {
+            return null;
+        }
+        String name = rest.substring(0, indexEndOfName);
+        String reqFields = fieldsDescription.substring(4, indexEndOfRequiredFields);
+        String optFields = fieldsDescription.substring(indexEndOfRequiredFields + 6, indexEndOfOptionalFields);
+        return new CustomEntryType(name, reqFields, optFields);
+    }
 }
