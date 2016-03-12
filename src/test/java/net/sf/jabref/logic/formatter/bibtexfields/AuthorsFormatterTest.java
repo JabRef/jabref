5baaff4595ccc6d93b9caf3152cf1fd587392c37
Oliver Kopp <kopp.dev@gmail.com>
2016-03-12 18:31:44 +0100

    Rewrite AuthorsFormatterTest to be a paramized test. Reenables disabled tests.

--- src/test/java/net/sf/jabref/logic/formatter/bibtexfields/AuthorsFormatterTest.java
+++ src/test/java/net/sf/jabref/logic/formatter/bibtexfields/AuthorsFormatterTest.java
@@ -1,10 +1,16 @@
 package net.sf.jabref.logic.formatter.bibtexfields;
 
-import org.junit.After;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 
+@RunWith(Parameterized.class)
 public class AuthorsFormatterTest {
     private AuthorsFormatter formatter;
 
@@ -13,40 +19,50 @@ public class AuthorsFormatterTest {
         formatter = new AuthorsFormatter();
     }
 
-    @After
-    public void tearDown() {
-        formatter = null;
+    @Parameterized.Parameters(name = "{index}: {1} -> {0}")
+    public static Collection<Object[]> authorList() {
+        return Arrays.asList(
+
+                new Object[] {"Bilbo, Staci D.", "Staci D Bilbo"},
+
+                // First case, where AuthorList is better than the original AuthorsFormatter
+                new Object[] {"Bilbo, Staci D.", "Staci D. Bilbo"},
+
+                new Object[] {"Bilbo, Staci D. and Smith, S. H. and Schwarz, Jaclyn M.",
+                        "Staci D Bilbo and Smith SH and Jaclyn M Schwarz"},
+
+                new Object[] {"Ølver, M. A.", "Ølver MA"},
+
+                new Object[] {
+                        "Ølver, M. A. and Øie, G. G. and Øie, G. G. and Alfredsen, J. Å. Å. and Alfredsen, Jo and Olsen, Y. Y. and Olsen, Y. Y.",
+                        "Ølver MA, GG Øie, Øie GG, Alfredsen JÅÅ, Jo Alfredsen, Olsen Y.Y. and Olsen Y. Y."},
+
+                new Object[] {
+                        "Ølver, M. A. and Øie, G. G. and Øie, G. G. and Alfredsen, J. Å. Å. and Alfredsen, Jo and Olsen, Y. Y. and Olsen, Y. Y.",
+                        "Ølver MA, GG Øie, Øie GG, Alfredsen JÅÅ, Jo Alfredsen, Olsen Y.Y., Olsen Y. Y."},
+
+                new Object[] {"Alver, Morten and Alver, Morten O. and Alfredsen, J. A. and Olsen, Y. Y.",
+                        "Alver, Morten and Alver, Morten O and Alfredsen, JA and Olsen, Y.Y."},
+
+                new Object[] {"Alver, M. A. and Alfredsen, J. A. and Olsen, Y. Y.",
+                        "Alver, MA; Alfredsen, JA; Olsen Y.Y."},
+
+                // Second case, where AuthorList is better than the original AuthorsFormatter
+                new Object[] {"Kolb, Stefan and Lenhard, J{\\\"o}rg and Wirtz, Guido",
+                        "Kolb, Stefan and J{\\\"o}rg Lenhard and Wirtz, Guido"});
     }
 
-    @Test
-    public void returnsFormatterName() {
-        Assert.assertNotNull(formatter.getName());
-        Assert.assertNotEquals("", formatter.getName());
-    }
+
+    @Parameter(value = 0)
+    public String expectedNames;
+
+    @Parameter(value = 1)
+    public String namesToNormalize;
+
 
     @Test
     public void testNormalizeAuthorList() {
-        expectCorrect("Staci D Bilbo", "Bilbo, Staci D.");
-        expectCorrect("Staci D. Bilbo", "Staci D. Bilbo"); // TODO strange behaviour
-
-        expectCorrect("Staci D Bilbo and Smith SH and Jaclyn M Schwarz", "Bilbo, Staci D. and Smith, S. H. and Schwarz, Jaclyn M.");
-
-        expectCorrect("Ølver MA", "Ølver, M. A.");
-
-        expectCorrect("Ølver MA, GG Øie, Øie GG, Alfredsen JÅÅ, Jo Alfredsen, Olsen Y.Y. and Olsen Y. Y.",
-                "Ølver, M. A. and Øie, G. G. and Øie, G. G. and Alfredsen, J. Å. Å. and Alfredsen, Jo and Olsen, Y. Y. and Olsen, Y. Y.");
-
-        expectCorrect("Ølver MA, GG Øie, Øie GG, Alfredsen JÅÅ, Jo Alfredsen, Olsen Y.Y., Olsen Y. Y.",
-                "Ølver, M. A. and Øie, G. G. and Øie, G. G. and Alfredsen, J. Å. Å. and Alfredsen, Jo and Olsen, Y. Y. and Olsen, Y. Y.");
-
-        expectCorrect("Alver, Morten and Alver, Morten O and Alfredsen, JA and Olsen, Y.Y.", "Alver, Morten and Alver, Morten O. and Alfredsen, J. A. and Olsen, Y. Y.");
-
-        expectCorrect("Alver, MA; Alfredsen, JA; Olsen Y.Y.", "Alver, M. A. and Alfredsen, J. A. and Olsen, Y. Y.");
-
-        // TODO: expectCorrect("Kolb, Stefan and J{\\\"o}rg Lenhard and Wirtz, Guido", "Kolb, Stefan and Lenhard, J{\\\"o}rg and Wirtz, Guido");
+        Assert.assertEquals(expectedNames, formatter.format(namesToNormalize));
     }
 
-    private void expectCorrect(String input, String expected) {
-        Assert.assertEquals(expected, formatter.format(input));
-    }
 }
