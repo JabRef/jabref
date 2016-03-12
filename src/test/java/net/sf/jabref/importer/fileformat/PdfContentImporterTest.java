3279d4c8198f65dae1372f5f32297cfea45eac26
Oliver Kopp <kopp.dev@gmail.com>
2016-03-12 12:40:14 +0100

    Improve XMPUtilTest and PdfContentImporterTest
     * Minor code improvements
     * Move encrypted.pdf to subfolder "pdfs/"
     * Move meta information about encrypted.pdf from XMPUtilTest.java to encrypted.txt

--- src/test/java/net/sf/jabref/importer/fileformat/PdfContentImporterTest.java
+++ src/test/java/net/sf/jabref/importer/fileformat/PdfContentImporterTest.java
@@ -4,17 +4,20 @@ import net.sf.jabref.model.entry.BibEntry;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PdfContentImporterTest {
+
     @Test
     public void doesNotHandleEncryptedPdfs() throws IOException {
         PdfContentImporter importer = new PdfContentImporter();
-        List<BibEntry> result = importer.importEntries(PdfContentImporter.class.getResourceAsStream("encrypted.pdf"), null);
-
-        assertEquals(Collections.emptyList(), result);
+        try (InputStream is = PdfContentImporter.class.getResourceAsStream("/pdfs/encrypted.pdf")) {
+            List<BibEntry> result = importer.importEntries(is, null);
+            assertEquals(Collections.emptyList(), result);
+        }
     }
 }
