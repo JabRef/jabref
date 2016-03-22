b9bf04a5de9f1d8fe31e0be10a044e9519fab34d
Oscar Gustafsson <ogib@users.sourceforge.net>
2016-03-22 14:01:57 +0100

    Parameterized tests

--- src/test/java/net/sf/jabref/exporter/ExportFormatTest.java
+++ src/test/java/net/sf/jabref/exporter/ExportFormatTest.java
@@ -5,15 +5,16 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileReader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import com.google.common.base.Charsets;
 
 import net.sf.jabref.Globals;
@@ -23,75 +24,68 @@ import net.sf.jabref.logic.journals.JournalAbbreviationLoader;
 import net.sf.jabref.model.database.BibDatabase;
 import net.sf.jabref.model.entry.BibEntry;
 
+@RunWith(Parameterized.class)
 public class ExportFormatTest {
+
+    public IExportFormat exportFormat;
+    public String exportFormatName;
+    public static BibDatabase database;
+    public static Charset charset;
+    public static MetaData metaData;
+    public static List<BibEntry> entries;
+
+    public ExportFormatTest(IExportFormat format, String name) {
+        exportFormat = format;
+        exportFormatName = name;
+    }
+
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
 
-    @Before
-    public void setUp() {
-        Globals.prefs = JabRefPreferences.getInstance();
-        ExportFormats.initAllExports();
+    @BeforeClass
+    public static void setUp() {
         Globals.journalAbbreviationLoader = new JournalAbbreviationLoader(Globals.prefs);
-
+        database = new BibDatabase();
+        charset = Charsets.UTF_8;
+        metaData = new MetaData();
+        entries = Collections.emptyList();
     }
 
     @Test
     public void testExportingEmptyDatabaseYieldsEmptyFile() throws Exception {
-        BibDatabase database = new BibDatabase();
-        for (Map.Entry<String, IExportFormat> exportFormats : ExportFormats.getExportFormats().entrySet()) {
-            IExportFormat exportFormat = exportFormats.getValue();
-            File tmpFile = testFolder.newFile();
-            tmpFile.deleteOnExit();
-            String filename = tmpFile.getCanonicalPath();
-            List<BibEntry> entries = Collections.emptyList();
-            Charset charset = Charsets.UTF_8;
-            MetaData metaData = new MetaData();
-            exportFormat.performExport(database, metaData, filename, charset, entries);
-            assertTrue(exportFormats.getKey() + " failed -- no file", tmpFile.exists());
-            try (FileReader fileReader = new FileReader(tmpFile)) {
-                char[] buffer = new char[512];
-                assertEquals(exportFormats.getKey() + " failed -- non-empty file", -1, fileReader.read(buffer)); // Empty file
-            }
+        File tmpFile = testFolder.newFile();
+        String filename = tmpFile.getCanonicalPath();
+        exportFormat.performExport(database, metaData, filename, charset, entries);
+        try (FileReader fileReader = new FileReader(tmpFile)) {
+            char[] buffer = new char[512];
+            assertEquals(-1, fileReader.read(buffer)); // Empty file
         }
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testExportingNullDatabaseThrowsNPE() throws Exception {
-        List<BibEntry> entries = Collections.emptyList();
-        Charset charset = Charsets.UTF_8;
-        MetaData metaData = new MetaData();
-        for (Map.Entry<String, IExportFormat> exportFormats : ExportFormats.getExportFormats().entrySet()) {
-            try {
-                IExportFormat exportFormat = exportFormats.getValue();
-                File tmpFile = testFolder.newFile();
-                tmpFile.deleteOnExit();
-                String filename = tmpFile.getCanonicalPath();
-                exportFormat.performExport(null, metaData, filename, charset, entries);
-                fail();
-            } catch (NullPointerException expected) {
-                // Ignore -- this is what we want!
-            }
-        }
+        File tmpFile = testFolder.newFile();
+        String filename = tmpFile.getCanonicalPath();
+        exportFormat.performExport(null, metaData, filename, charset, entries);
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testExportingNullEntriesThrowsNPE() throws Exception {
-        BibDatabase database = new BibDatabase();
-        Charset charset = Charsets.UTF_8;
-        MetaData metaData = new MetaData();
-        for (Map.Entry<String, IExportFormat> exportFormats : ExportFormats.getExportFormats().entrySet()) {
-            try {
-                IExportFormat exportFormat = exportFormats.getValue();
-                File tmpFile = testFolder.newFile();
-                tmpFile.deleteOnExit();
-                String filename = tmpFile.getCanonicalPath();
-                exportFormat.performExport(database, metaData, filename, charset, null);
-                fail();
-            } catch (NullPointerException expected) {
-                // Ignore -- this is what we want!
-            }
+        File tmpFile = testFolder.newFile();
+        String filename = tmpFile.getCanonicalPath();
+        exportFormat.performExport(database, metaData, filename, charset, null);
+    }
+
+    @Parameterized.Parameters(name = "{index}: {1}")
+    public static Collection<Object[]> exportFormats() {
+        Collection<Object[]> result = new ArrayList<>();
+        Globals.prefs = JabRefPreferences.getInstance();
+        ExportFormats.initAllExports();
+        for (IExportFormat format : ExportFormats.getExportFormats().values()) {
+            result.add(new Object[] {format, format.getDisplayName()});
         }
+        return result;
     }
 }
