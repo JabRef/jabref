8c208a0380ecf96f14272f71326a65c7299656f4
Stefan Kolb <stefan-kolb@web.de>
2016-03-22 19:10:50 +0100

    Use static method inside tests

--- src/test/java/net/sf/jabref/importer/fetcher/CrossRefTest.java
+++ src/test/java/net/sf/jabref/importer/fetcher/CrossRefTest.java
@@ -11,25 +11,13 @@ import java.util.Locale;
 import static org.junit.Assert.*;
 
 public class CrossRefTest {
-    private CrossRef fetcher;
-
-    @Before
-    public void setUp() throws Exception {
-        fetcher = new CrossRef();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        fetcher = null;
-    }
-
     @Test
     public void findExactData() throws Exception {
         BibEntry entry = new BibEntry();
         entry.setField("title", "Service Interaction Patterns");
         entry.setField("author", "Barros, Alistair and Dumas, Marlon and Arthur H.M. ter Hofstede");
         entry.setField("year", "2005");
-        assertEquals("10.1007/11538394_20", fetcher.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
+        assertEquals("10.1007/11538394_20", CrossRef.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
     }
 
     @Test
@@ -37,14 +25,14 @@ public class CrossRefTest {
         BibEntry entry = new BibEntry();
         entry.setField("title", "Towards Application Portability in Platform as a Service");
         entry.setField("author", "Stefan Kolb");
-        assertEquals("10.1109/sose.2014.26", fetcher.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
+        assertEquals("10.1109/sose.2014.26", CrossRef.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
     }
 
     @Test
     public void findTitleOnly() throws Exception {
         BibEntry entry = new BibEntry();
         entry.setField("title", "Towards Application Portability in Platform as a Service");
-        assertEquals("10.1109/sose.2014.26", fetcher.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
+        assertEquals("10.1109/sose.2014.26", CrossRef.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
     }
 
     @Test
@@ -52,7 +40,7 @@ public class CrossRefTest {
         BibEntry entry = new BibEntry();
         entry.setField("title", "Towards Application Portability");
         entry.setField("author", "Stefan Kolb and Guido Wirtz");
-        assertEquals("10.1109/sose.2014.26", fetcher.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
+        assertEquals("10.1109/sose.2014.26", CrossRef.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
     }
 
     @Test
@@ -60,6 +48,6 @@ public class CrossRefTest {
         BibEntry entry = new BibEntry();
         entry.setField("title", "Towards Application Portability in Platform as a Service");
         entry.setField("author", "Stefan Kolb and Simon Harrer");
-        assertEquals("10.1109/sose.2014.26", fetcher.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
+        assertEquals("10.1109/sose.2014.26", CrossRef.findDOI(entry).get().getDOI().toLowerCase(Locale.ENGLISH));
     }
 }
