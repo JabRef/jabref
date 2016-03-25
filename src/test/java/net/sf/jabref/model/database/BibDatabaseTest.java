86c3e667519fe7ab17d415ea3373cdd52bbc2df3
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-03-25 01:11:31 +0100

    Added tests for BibDatabase

--- src/test/java/net/sf/jabref/model/database/BibDatabaseTest.java
+++ src/test/java/net/sf/jabref/model/database/BibDatabaseTest.java
@@ -6,15 +6,20 @@ import net.sf.jabref.bibtex.BibtexEntryAssert;
 import net.sf.jabref.importer.ParserResult;
 import net.sf.jabref.importer.fileformat.BibtexParser;
 import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.BibtexString;
+import net.sf.jabref.model.entry.IdGenerator;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 import static org.junit.Assert.*;
@@ -43,7 +48,8 @@ public class BibDatabaseTest {
      */
     @Test
     public void resolveStrings() throws IOException {
-        try (FileReader fr = new FileReader("src/test/resources/net/sf/jabref/util/twente.bib")) {
+        try (FileInputStream stream = new FileInputStream("src/test/resources/net/sf/jabref/util/twente.bib");
+                InputStreamReader fr = new InputStreamReader(stream, StandardCharsets.ISO_8859_1)) {
             ParserResult result = BibtexParser.parse(fr);
 
             BibDatabase db = result.getDatabase();
@@ -65,6 +71,7 @@ public class BibDatabaseTest {
         BibEntry entry = new BibEntry();
         database.insertEntry(entry);
         assertEquals(database.getEntries().size(), 1);
+        assertEquals(database.getEntryCount(), 1);
         BibtexEntryAssert.assertEquals(entry, database.getEntries().get(0));
     }
 
@@ -115,4 +122,62 @@ public class BibDatabaseTest {
         database.removeEntry(null);
     }
 
+    @Test
+    public void insertString() {
+        BibDatabase database = new BibDatabase();
+        assertEquals(Collections.emptySet(), database.getStringKeySet());
+        assertTrue(database.hasNoStrings());
+        BibtexString string = new BibtexString(IdGenerator.next(), "DSP", "Digital Signal Processing");
+        database.addString(string);
+        assertFalse(database.hasNoStrings());
+        assertEquals(database.getStringKeySet().size(), 1);
+        assertEquals(database.getStringCount(), 1);
+        assertTrue(database.getStringValues().contains(string));
+        assertTrue(database.getStringKeySet().contains(string.getId()));
+        assertEquals(string, database.getString(string.getId()));
+    }
+
+    @Test
+    public void insertAndRemoveString() {
+        BibDatabase database = new BibDatabase();
+        BibtexString string = new BibtexString(IdGenerator.next(), "DSP", "Digital Signal Processing");
+        database.addString(string);
+        database.removeString(string.getId());
+        assertTrue(database.hasNoStrings());
+        assertEquals(database.getStringKeySet().size(), 0);
+        assertEquals(database.getStringCount(), 0);
+        assertFalse(database.getStringValues().contains(string));
+        assertFalse(database.getStringKeySet().contains(string.getId()));
+        assertNull(database.getString(string.getId()));
+    }
+
+    @Test
+    public void hasStringLabel() {
+        BibDatabase database = new BibDatabase();
+        BibtexString string = new BibtexString(IdGenerator.next(), "DSP", "Digital Signal Processing");
+        database.addString(string);
+        assertTrue(database.hasStringLabel("DSP"));
+        assertFalse(database.hasStringLabel("VLSI"));
+    }
+
+    @Test(expected = KeyCollisionException.class)
+    public void addSameStringLabelTwice() {
+        BibDatabase database = new BibDatabase();
+        BibtexString string = new BibtexString(IdGenerator.next(), "DSP", "Digital Signal Processing");
+        database.addString(string);
+        string = new BibtexString(IdGenerator.next(), "DSP", "Digital Signal Processor");
+        database.addString(string);
+        fail();
+    }
+
+    @Test(expected = KeyCollisionException.class)
+    public void addSameStringIdTwice() {
+        BibDatabase database = new BibDatabase();
+        String id = IdGenerator.next();
+        BibtexString string = new BibtexString(id, "DSP", "Digital Signal Processing");
+        database.addString(string);
+        string = new BibtexString(id, "VLSI", "Very Large Scale Integration");
+        database.addString(string);
+        fail();
+    }
 }
