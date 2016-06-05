7469ffda37e1f3833f89f675620eecdd32e980b2
Stefan Kolb <stefan-kolb@web.de>
2016-06-05 20:17:57 +0200

    Fix help file links and add test to ensure each link at least exists for english translation.

--- src/main/java/net/sf/jabref/gui/help/HelpFiles.java
+++ src/main/java/net/sf/jabref/gui/help/HelpFiles.java
@@ -16,129 +16,70 @@
 package net.sf.jabref.gui.help;
 
 /**
- * This enum globally defines all help pages with the name of the markdown file in the help repository at github
+ * This enum globally defines all help pages with the name of the markdown file in the help repository at Github.
+ *
  * @see <a href=https://github.com/JabRef/help.jabref.org>help.jabref.org@github</a>
- *
- *
  */
 public enum HelpFiles {
-    COMMAND_LINE(
-            ""),
-
+    COMMAND_LINE(""),
     //Empty because it refers to the TOC/index
-    CONTENTS(
-            ""),
-    ENTRY_EDITOR(
-            "EntryEditorHelp"),
-    STRING_EDITOR(
-            "StringEditorHelp"),
-    SEARCH(
-            "SearchHelp"),
-    GROUP(
-            "GroupsHelp"),
-    CONTENT_SELECTOR(
-            "ContentSelectorHelp"),
-    SPECIAL_FIELDS(
-            "specialFieldsHelp"),
-    LABEL_PATTERN(
-            "LabelPatterns"),
-    OWNER(
-            "OwnerHelp"),
-    TIMESTAMP(
-            "TimeStampHelp"),
-    CUSTOM_EXPORTS(
-            "CustomExports"),
-    CUSTOM_EXPORTS_NAME_FORMATTER(
-            "CustomExports#NameFormatter"),
-    CUSTOM_IMPORTS(
-            "CustomImports"),
-    GENERAL_FIELDS(
-            "GeneralFields"),
-    IMPORT_INSPECTION(
-            "ImportInspectionDialog"),
-    REMOTE(
-            "RemoteHelp"),
-    JOURNAL_ABBREV(
-            "JournalAbbreviations"),
-    REGEX_SEARCH(
-            "ExternalFiles#RegularExpressionSearch"),
-    PREVIEW(
-            "PreviewHelp"),
-    AUTOSAVE(
-            "Autosave"),
-
+    CONTENTS(""),
+    ENTRY_EDITOR("EntryEditorHelp"),
+    STRING_EDITOR("StringEditorHelp"),
+    SEARCH("SearchHelp"),
+    GROUP("GroupsHelp"),
+    CONTENT_SELECTOR("ContentSelectorHelp"),
+    SPECIAL_FIELDS("SpecialFieldsHelp"),
+    LABEL_PATTERN("LabelPatterns"),
+    OWNER("OwnerHelp"),
+    TIMESTAMP("TimeStampHelp"),
+    CUSTOM_EXPORTS("CustomExports"),
+    CUSTOM_EXPORTS_NAME_FORMATTER("CustomExports#NameFormatter"),
+    CUSTOM_IMPORTS("CustomImports"),
+    GENERAL_FIELDS("GeneralFields"),
+    IMPORT_INSPECTION("ImportInspectionDialog"),
+    REMOTE("RemoteHelp"),
+    JOURNAL_ABBREV("JournalAbbreviations"),
+    REGEX_SEARCH("ExternalFiles#RegularExpressionSearch"),
+    PREVIEW("PreviewHelp"),
+    AUTOSAVE("Autosave"),
     //The help page covers both OO and LO.
-    OPENOFFICE_LIBREOFFICE(
-            "OpenOfficeIntegration"),
+    OPENOFFICE_LIBREOFFICE("OpenOfficeIntegration"),
+    FETCHER_ACM("ACMPortalHelp"),
+    FETCHER_ADS("ADSHelp"),
+    FETCHER_CITESEERX("CiteSeerHelp"),
+    FETCHER_DBLP("DBLPHelp"),
+    FETCHER_DIVA_TO_BIBTEX("DiVAtoBibTeXHelp"),
+    FETCHER_DOAJ("DOAJHelp"),
+    FETCHER_DOI_TO_BIBTEX("DOItoBibTeXHelp"),
+    FETCHER_GOOGLE_SCHOLAR("GoogleScholarHelp"),
+    FETCHER_GVK("GVKHelp"),
+    FETCHER_IEEEXPLORE("IEEEXploreHelp"),
+    FETCHER_INSPIRE("INSPIRE"),
+    FETCHER_ISBN_TO_BIBTEX("ISBNtoBibTeXHelp"),
+    FETCHER_MEDLINE("MedlineHelp"),
+    FETCHER_OAI2_ARXIV("arXivHelp"),
+    FETCHER_SPRINGER("SpringerHelp"),
+    FETCHER_SCIENCEDIRECT(""),
+    FETCHER_BIBSONOMY_SCRAPER("");
 
-    FETCHER_ACM(
-            "ACMPortalHelp"),
-
-    FETCHER_ADS(
-            "ADSHelp"),
-
-    FETCHER_CITESEERX(
-            "CiteSeerHelp"),
-
-    FETCHER_DBLP(
-            "DBLPHelp"),
-
-    FETCHER_DIVA_TO_BIBTEX(
-            "DiVAtoBibTeXHelp"),
-
-    FETCHER_DOAJ(
-            "DOAJHelp"),
-
-    FETCHER_DOI_TO_BIBTEX(
-            "DOItoBibTeXHelp"),
-
-    FETCHER_GOOGLE_SCHOLAR(
-            "GoogleScholarHelp"),
-
-    FETCHER_GVK(
-            "GVKHelp"),
-
-    FETCHER_IEEEXPLORE(
-            "IEEEXploreHelp"),
-
-    FETCHER_INSPIRE(
-            "INSPIRE"),
-
-    FETCHER_ISBN_TO_BIBTEX(
-            "ISBNtoBibTeXHelp"),
-    FETCHER_MEDLINE(
-            "MedlineHelp"),
-
-    FETCHER_OAI2_ARXIV(
-            "arXivHelp"),
-
-    FETCHER_SPRINGER(
-            "SpringerHelp"),
-
-    FETCHER_SCIENCEDIRECT(
-            ""),
-
-    FETCHER_BIBSONOMY_SCRAPER(
-            "");
+    private final String pageName;
 
     /**
+     * Sets the URL path part of the help page.
      *
-     * @param pageName The filename of the help page
+     * @param pageName the URL path part of the help page
      */
     HelpFiles(String pageName) {
         this.pageName = pageName;
     }
 
-
-    private final String pageName;
-
-
     /**
-     * Get the filename of a help page
-     * @return The filename of the associated help page
+     * Returns the URL path part of the help page.
+     *
+     * @return the URL path part of the help page
      */
     public String getPageName() {
         return pageName;
     }
-
 }
