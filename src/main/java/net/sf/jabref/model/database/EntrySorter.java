93a9efd9db19a711da26df8e44245d547453372f
Simon Harrer <simon.harrer@gmail.com>
2016-06-13 17:57:34 +0200

    Fix #1430 "review changes" did misinterpret changes (#1489)
    Fix #1430: "review changes" did misinterpret changes

--- src/main/java/net/sf/jabref/model/database/EntrySorter.java
+++ src/main/java/net/sf/jabref/model/database/EntrySorter.java
@@ -15,16 +15,13 @@
 */
 package net.sf.jabref.model.database;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import net.sf.jabref.event.EntryAddedEvent;
-import net.sf.jabref.event.EntryChangedEvent;
-import net.sf.jabref.event.EntryRemovedEvent;
 import net.sf.jabref.model.entry.BibEntry;
 
-import com.google.common.eventbus.Subscribe;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -32,91 +29,22 @@ public class EntrySorter {
 
     private static final Log LOGGER = LogFactory.getLog(EntrySorter.class);
 
-    private final List<BibEntry> set;
+    // guarded by itself
+    private final List<BibEntry> entries;
     private final Comparator<BibEntry> comp;
-    private BibEntry[] entryArray;
-    private boolean changed;
 
-
-    public EntrySorter(List<BibEntry> entries, Comparator<BibEntry> comp) {
-        set = entries;
-        this.comp = comp;
-        changed = true;
-        index();
-    }
-
-    private void index() {
-
-        synchronized (set) {
-
-            // Resort if necessary:
-            if (changed) {
-                Collections.sort(set, comp);
-                changed = false;
-            }
-
-            // Create an array of IDs for quick access, since getIdAt() is called by
-            // getValueAt() in EntryTableModel, which *has* to be efficient.
-
-            int count = set.size();
-
-
-            entryArray = new BibEntry[count];
-            int piv = 0;
-            for (BibEntry entry : set) {
-                entryArray[piv] = entry;
-                piv++;
-            }
-        }
+    public EntrySorter(List<BibEntry> entries, Comparator<BibEntry> comparator) {
+        this.entries = new ArrayList<>(entries);
+        this.comp = comparator;
+        Collections.sort(this.entries, comparator);
     }
 
     public BibEntry getEntryAt(int pos) {
-        synchronized (set) {
-            return entryArray[pos];
-        }
+        return entries.get(pos);
     }
 
     public int getEntryCount() {
-        synchronized (set) {
-            if (entryArray != null) {
-                return entryArray.length;
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    @Subscribe
-    public void listen(EntryAddedEvent EntryAddedEvent) {
-        synchronized (set) {
-            int pos = -Collections.binarySearch(set, EntryAddedEvent.getBibEntry(), comp) - 1;
-            LOGGER.debug("Insert position = " + pos);
-            if (pos >= 0) {
-                set.add(pos, EntryAddedEvent.getBibEntry());
-            } else {
-                set.add(0, EntryAddedEvent.getBibEntry());
-            }
-        }
-    }
-
-    @Subscribe
-    public void listen(EntryRemovedEvent entryRemovedEvent) {
-        synchronized (set) {
-            set.remove(entryRemovedEvent.getBibEntry());
-            changed = true;
-        }
-    }
-
-    @Subscribe
-    public void listen(EntryChangedEvent entryChangedEvent) {
-        synchronized (set) {
-            int pos = Collections.binarySearch(set, entryChangedEvent.getBibEntry(), comp);
-            int posOld = set.indexOf(entryChangedEvent.getBibEntry());
-            if (pos < 0) {
-                set.remove(posOld);
-                set.add(-posOld - 1, entryChangedEvent.getBibEntry());
-            }
-        }
+        return entries.size();
     }
 
 }
