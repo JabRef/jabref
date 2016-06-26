eef80b08026158d9b1210f17b669a52f1cc65681
Tobias Diez <tobiasdiez@gmx.de>
2016-06-26 23:12:42 +0200

    Fix tests

--- src/test/java/net/sf/jabref/logic/journals/AbbreviationsTest.java
+++ src/test/java/net/sf/jabref/logic/journals/AbbreviationsTest.java
@@ -5,11 +5,14 @@ import net.sf.jabref.JabRefPreferences;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AbbreviationsTest {
 
     @Mock JabRefPreferences prefs;
