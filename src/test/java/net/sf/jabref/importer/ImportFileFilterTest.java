40f16313cfb0f544a4ccbe46a70e66a9b1c8e1d0
bartsch-dev <bartsch.dev@gmail.com>
2016-06-28 14:09:57 +0200

    test ImportFileFilter with Mockito (#1522)

--- src/test/java/net/sf/jabref/importer/ImportFileFilterTest.java
+++ src/test/java/net/sf/jabref/importer/ImportFileFilterTest.java
@@ -1,48 +1,25 @@
 package net.sf.jabref.importer;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import net.sf.jabref.importer.fileformat.ImportFormat;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class ImportFileFilterTest {
 
     @Test
     public void nameWithSingleExtensions() {
-        ImportFormat importFormatSingleExtension = new ImportFormat() {
-            @Override
-            protected boolean isRecognizedFormat(BufferedReader input) throws IOException {
-                return false;
-            }
-
-            @Override
-            protected ParserResult importDatabase(BufferedReader input) throws IOException {
-                return null;
-            }
-
-            @Override
-            public String getFormatName() {
-                return "Single Extension";
-            }
-
-            @Override
-            public List<String> getExtensions() {
-                return Collections.singletonList(".abc");
-            }
-
-            @Override
-            public String getDescription() {
-                return null;
-            }
-        };
+        ImportFormat importFormatSingleExtension = mock(ImportFormat.class);
+        when(importFormatSingleExtension.getFormatName()).thenReturn("Single Extension");
+        when(importFormatSingleExtension.getExtensions()).thenReturn(Collections.singletonList(".abc"));
 
         ImportFileFilter importFileFilter = new ImportFileFilter(importFormatSingleExtension);
         assertEquals("Single Extension (*.abc)", importFileFilter.getDescription());
@@ -50,32 +27,9 @@ public class ImportFileFilterTest {
 
     @Test
     public void nameWithMultipleExtensions() {
-        ImportFormat importFormatMultipleExtensions = new ImportFormat() {
-            @Override
-            protected boolean isRecognizedFormat(BufferedReader input) throws IOException {
-                return false;
-            }
-
-            @Override
-            protected ParserResult importDatabase(BufferedReader input) throws IOException {
-                return null;
-            }
-
-            @Override
-            public String getFormatName() {
-                return "Multiple Extensions";
-            }
-
-            @Override
-            public List<String> getExtensions() {
-                return Arrays.asList(".abc", ".xyz");
-            }
-
-            @Override
-            public String getDescription() {
-                return null;
-            }
-        };
+        ImportFormat importFormatMultipleExtensions = mock(ImportFormat.class);
+        when(importFormatMultipleExtensions.getFormatName()).thenReturn("Multiple Extensions");
+        when(importFormatMultipleExtensions.getExtensions()).thenReturn(Arrays.asList(".abc", ".xyz"));
 
         ImportFileFilter importFileFilter = new ImportFileFilter(importFormatMultipleExtensions);
         assertEquals("Multiple Extensions (*.abc, *.xyz)", importFileFilter.getDescription());
