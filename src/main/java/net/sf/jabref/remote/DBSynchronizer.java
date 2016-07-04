dbd8c40064ba641b54b37762dc5bc55ec322a788
Admir Obralija <admir.obralija@mathematik.uni-stuttgart.de>
2016-07-04 17:26:13 +0200

    Fix localization key.

--- src/main/java/net/sf/jabref/remote/DBSynchronizer.java
+++ src/main/java/net/sf/jabref/remote/DBSynchronizer.java
@@ -67,7 +67,6 @@ public class DBSynchronizer {
             synchronizeLocalMetaData();
             synchronizeLocalDatabase(); // Pull remote changes for the case that there where some
         }
-        System.out.println(">>> " + this.metaData.getAsStringMap());
     }
 
     /**
