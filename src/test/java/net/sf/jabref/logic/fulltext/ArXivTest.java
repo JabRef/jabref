915b9e1e04a51fe37d40c3334fec8acb756fa017
Tobias Diez <tobiasdiez@gmx.de>
2016-07-19 21:26:36 +0200

    Fix tests

--- src/test/java/net/sf/jabref/logic/fulltext/ArXivTest.java
+++ src/test/java/net/sf/jabref/logic/fulltext/ArXivTest.java
@@ -36,7 +36,7 @@ public class ArXivTest {
         sliceTheoremPaper.setField("date", "2014-05-09");
         sliceTheoremPaper.setField("abstract", "A general slice theorem for the action of a Fr\\'echet Lie group on a Fr\\'echet manifolds is established. The Nash-Moser theorem provides the fundamental tool to generalize the result of Palais to this infinite-dimensional setting. The presented slice theorem is illustrated by its application to gauge theories: the action of the gauge transformation group admits smooth slices at every point and thus the gauge orbit space is stratified by Fr\\'echet manifolds. Furthermore, a covariant and symplectic formulation of classical field theory is proposed and extensively discussed. At the root of this novel framework is the incorporation of field degrees of freedom F and spacetime M into the product manifold F * M. The induced bigrading of differential forms is used in order to carry over the usual symplectic theory to this new setting. The examples of the Klein-Gordon field and general Yang-Mills theory illustrate that the presented approach conveniently handles the occurring symmetries.");
         sliceTheoremPaper.setField("eprint", "1405.2249v1");
-        sliceTheoremPaper.setField("file", "online:http\\://arxiv.org/pdf/1405.2249v1:pdf");
+        sliceTheoremPaper.setField("file", "online:http\\://arxiv.org/pdf/1405.2249v1:PDF");
         sliceTheoremPaper.setField("eprinttype", "arXiv");
         sliceTheoremPaper.setField("eprintclass", "math-ph");
         sliceTheoremPaper.setField("keywords", "math-ph, math.DG, math.MP, math.SG, 58B99, 58Z05, 58B25, 22E65, 58D19, 53D20, 53D42");
@@ -110,7 +110,7 @@ public class ArXivTest {
         expected.setField("date", "2003-07-07");
         expected.setField("abstract", "Multi-electron production is studied at high electron transverse momentum in positron- and electron-proton collisions using the H1 detector at HERA. The data correspond to an integrated luminosity of 115 pb-1. Di-electron and tri-electron event yields are measured. Cross sections are derived in a restricted phase space region dominated by photon-photon collisions. In general good agreement is found with the Standard Model predictions. However, for electron pair invariant masses above 100 GeV, three di-electron events and three tri-electron events are observed, compared to Standard Model expectations of 0.30 \\pm 0.04 and 0.23 \\pm 0.04, respectively.");
         expected.setField("eprint", "hep-ex/0307015v1");
-        expected.setField("file", "online:http\\://arxiv.org/pdf/hep-ex/0307015v1:pdf");
+        expected.setField("file", "online:http\\://arxiv.org/pdf/hep-ex/0307015v1:PDF");
         expected.setField("eprinttype", "arXiv");
         expected.setField("eprintclass", "hep-ex");
         expected.setField("keywords", "hep-ex");
