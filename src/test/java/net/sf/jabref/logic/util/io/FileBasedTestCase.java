360cd941685fe71d6e195805a513423670a13ec8
Siedlerchr <siedlerkiller@gmail.com>
2016-07-23 13:11:54 +0200

    Checkstyle fix

--- src/test/java/net/sf/jabref/logic/util/io/FileBasedTestCase.java
+++ src/test/java/net/sf/jabref/logic/util/io/FileBasedTestCase.java
@@ -10,11 +10,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import net.sf.jabref.BibtexTestData;
 import net.sf.jabref.Globals;
 import net.sf.jabref.model.database.BibDatabase;
 import net.sf.jabref.model.entry.BibEntry;
 import net.sf.jabref.preferences.JabRefPreferences;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
