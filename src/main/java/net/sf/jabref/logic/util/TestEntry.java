f606ec75df63c8f9a56b25be0d8fcfd90088e96e
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-07-23 12:51:13 +0200

    Always return new entry

--- src/main/java/net/sf/jabref/logic/util/TestEntry.java
+++ src/main/java/net/sf/jabref/logic/util/TestEntry.java
@@ -6,28 +6,24 @@ import net.sf.jabref.model.entry.IdGenerator;
 
 public class TestEntry {
 
-    private static BibEntry entry;
-
-
     public static BibEntry getTestEntry() {
-        if (entry == null) {
-            entry = new BibEntry(IdGenerator.next(), "article");
-            entry.setField(FieldName.AUTHOR, "Smith, Bill and Jones, Bob and Williams, Jeff");
-            entry.setField(FieldName.EDITOR, "Taylor, Phil");
-            entry.setField(FieldName.TITLE, "Title of the test entry");
-            entry.setField(FieldName.NUMBER, "3");
-            entry.setField(FieldName.VOLUME, "34");
-            entry.setField(FieldName.YEAR, "2016");
-            entry.setField(FieldName.PAGES, "45--67");
-            entry.setField(FieldName.MONTH, "July");
-            entry.setField(FieldName.FILE, ":testentry.pdf:PDF");
-            entry.setField(FieldName.JOURNAL, "BibTeX Journal");
-            entry.setField(FieldName.PUBLISHER, "JabRef Publishing");
-            entry.setField(FieldName.ADDRESS, "Trondheim");
-            entry.setField(FieldName.URL, "https://github.com/JabRef");
-            entry.setField(FieldName.ABSTRACT,
-                    "This entry describes a test scenario which may be useful in JabRef. By providing a test entry it is possible to see how certain things will look in this graphical bib-file mananger.");
-        }
+
+        BibEntry entry = new BibEntry(IdGenerator.next(), "article");
+        entry.setField(FieldName.AUTHOR, "Smith, Bill and Jones, Bob and Williams, Jeff");
+        entry.setField(FieldName.EDITOR, "Taylor, Phil");
+        entry.setField(FieldName.TITLE, "Title of the test entry");
+        entry.setField(FieldName.NUMBER, "3");
+        entry.setField(FieldName.VOLUME, "34");
+        entry.setField(FieldName.YEAR, "2016");
+        entry.setField(FieldName.PAGES, "45--67");
+        entry.setField(FieldName.MONTH, "July");
+        entry.setField(FieldName.FILE, ":testentry.pdf:PDF");
+        entry.setField(FieldName.JOURNAL, "BibTeX Journal");
+        entry.setField(FieldName.PUBLISHER, "JabRef Publishing");
+        entry.setField(FieldName.ADDRESS, "Trondheim");
+        entry.setField(FieldName.URL, "https://github.com/JabRef");
+        entry.setField(FieldName.ABSTRACT,
+                "This entry describes a test scenario which may be useful in JabRef. By providing a test entry it is possible to see how certain things will look in this graphical bib-file mananger.");
         return entry;
     }
 }
