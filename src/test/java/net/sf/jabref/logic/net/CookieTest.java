4ef0c39a4000a5244be50b888666acbd17635114
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-07-25 21:13:12 +0200

    Fixed imports...

--- src/test/java/net/sf/jabref/logic/net/CookieTest.java
+++ src/test/java/net/sf/jabref/logic/net/CookieTest.java
@@ -5,7 +5,8 @@ import java.net.URISyntaxException;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class CookieTest {
