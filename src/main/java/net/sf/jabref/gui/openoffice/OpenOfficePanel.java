d5a923c87b6bf7b9d48cfbd5d8b7de41a727c2e6
mairdl <mairdl.dev@googlemail.com>
2016-07-27 15:03:18 +0200

    Rename Open/Libre Office into OpenOffice/LibreOffice

--- src/main/java/net/sf/jabref/gui/openoffice/OpenOfficePanel.java
+++ src/main/java/net/sf/jabref/gui/openoffice/OpenOfficePanel.java
@@ -178,7 +178,7 @@ public class OpenOfficePanel extends AbstractWorker {
         JMenuItem item = new JMenuItem(Localization.lang("OpenOffice/LibreOffice connection"),
                 IconTheme.getImage("openoffice"));
         item.addActionListener(event -> manager.show(getName()));
-        item.setAccelerator(Globals.getKeyPrefs().getKey(KeyBinding.OPEN_OFFICE_LIBRE_CONNECTION));
+        item.setAccelerator(Globals.getKeyPrefs().getKey(KeyBinding.OPEN_OPEN_OFFICE_LIBRE_OFFICE_CONNECTION));
         return item;
     }
 
