6b3fd5c20e1084fcb70645ff97c0c4c6cf4194ae
Stefan Kolb <stefan-kolb@web.de>
2016-08-11 14:31:47 +0200

    Fix user-agent 403 error when checking response type, fix ACM DOI resolution

--- src/main/java/net/sf/jabref/logic/importer/MimeTypeDetector.java
+++ src/main/java/net/sf/jabref/logic/importer/MimeTypeDetector.java
@@ -37,6 +37,7 @@ public class MimeTypeDetector {
     private static Optional<String> getMimeType(String url) {
         try {
             URLConnection connection = new URL(url).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
 
             return Optional.ofNullable(connection.getContentType());
         } catch (IOException e) {
