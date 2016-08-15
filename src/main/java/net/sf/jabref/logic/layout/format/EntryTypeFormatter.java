77af1810a0bbc83c5843bce9a53d723a65e1da20
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2016-08-15 19:47:09 +0200

    Follow-up documentation and fixed comments for #1651

--- src/main/java/net/sf/jabref/logic/layout/format/EntryTypeFormatter.java
+++ src/main/java/net/sf/jabref/logic/layout/format/EntryTypeFormatter.java
@@ -5,18 +5,22 @@ import net.sf.jabref.logic.layout.LayoutFormatter;
 import net.sf.jabref.model.database.BibDatabaseMode;
 import net.sf.jabref.model.entry.BibEntry;
 
-
+/*
+ * Camel casing of entry type string, unknown entry types gets a leading capital
+ *
+ * Example (known): inbook -> InBook
+ * Example (unknown): banana -> Banana
+ */
 public class EntryTypeFormatter implements LayoutFormatter {
 
+    /**
+     * Input: entry type as a string
+     */
     @Override
-    public String format(String fieldText) {
-        // Fancy typesetting
+    public String format(String entryType) {
         BibEntry entry = new BibEntry();
-        entry.setType(fieldText);
+        entry.setType(entryType);
         TypedBibEntry typedEntry = new TypedBibEntry(entry, BibDatabaseMode.BIBLATEX);
         return typedEntry.getTypeForDisplay();
-
     }
-
-
 }
