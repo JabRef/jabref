845c5512df544f56337ec50a49da9a9150c44068
grimes2 <juergen.lange@unitybox.de>
2016-09-02 04:50:35 +0200

    Some more tests

--- src/test/java/net/sf/jabref/logic/integrity/IntegrityCheckTest.java
+++ src/test/java/net/sf/jabref/logic/integrity/IntegrityCheckTest.java
@@ -47,12 +47,16 @@ public class IntegrityCheckTest {
         assertCorrect(createContext("year", "1986"));
         assertCorrect(createContext("year", "around 1986"));
         assertCorrect(createContext("year", "(around 1986)"));
+        assertCorrect(createContext("year", "1986,"));
+        assertCorrect(createContext("year", "1986}%"));
         assertCorrect(createContext("year", "1986(){},.;!?<>%&$"));
         assertWrong(createContext("year", "abc"));
         assertWrong(createContext("year", "86"));
         assertWrong(createContext("year", "204"));
         assertWrong(createContext("year", "1986a"));
         assertWrong(createContext("year", "(1986a)"));
+        assertWrong(createContext("year", "1986a,"));
+        assertWrong(createContext("year", "1986}a%"));
         assertWrong(createContext("year", "1986a(){},.;!?<>%&$"));
     }
 
