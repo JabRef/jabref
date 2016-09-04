11ecf099989eb2e829cf21da2eefae458f6d302f
Christoph <cschwentker@gmail.com>
2016-09-04 18:29:07 +0200

    Fix location field not exported correctly to office 2007 xml (#1909)
    * Fix location field not exported to office 2007 xml
    * Add some test for exporting location and address field
    Address in xml field is now imported as location
    add some javadoc

--- src/main/java/net/sf/jabref/logic/exporter/MSBibExportFormat.java
+++ src/main/java/net/sf/jabref/logic/exporter/MSBibExportFormat.java
@@ -43,7 +43,7 @@ class MSBibExportFormat extends ExportFormat {
 
         try (VerifyingWriter ps = session.getWriter()) {
             try {
-                DOMSource source = new DOMSource(msBibDatabase.getDOM());
+                DOMSource source = new DOMSource(msBibDatabase.getDomForExport());
                 StreamResult result = new StreamResult(ps);
                 Transformer trans = TransformerFactory.newInstance().newTransformer();
                 trans.setOutputProperty(OutputKeys.INDENT, "yes");
