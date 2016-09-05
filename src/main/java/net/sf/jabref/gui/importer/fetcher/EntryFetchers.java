5db873a33ae5be09c6885d2eb808dbf69bfde0b1
Sascha Zeller <zeller.dev@gmail.com>
2016-09-05 19:24:11 +0200

    remove fetchers from web search and add list of id fetchers

--- src/main/java/net/sf/jabref/gui/importer/fetcher/EntryFetchers.java
+++ src/main/java/net/sf/jabref/gui/importer/fetcher/EntryFetchers.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import net.sf.jabref.Globals;
+import net.sf.jabref.logic.importer.IdBasedFetcher;
 import net.sf.jabref.logic.importer.fetcher.ArXiv;
 import net.sf.jabref.logic.importer.fetcher.DiVA;
 import net.sf.jabref.logic.importer.fetcher.GvkFetcher;
@@ -30,13 +31,19 @@ public class EntryFetchers {
         entryFetchers.add(new DOAJFetcher());
         entryFetchers.add(new SpringerFetcher());
 
-        entryFetchers.add(new IdBasedEntryFetcher(new DiVA(Globals.prefs.getImportFormatPreferences())));
-        entryFetchers.add(new IdBasedEntryFetcher(new IsbnFetcher(Globals.prefs.getImportFormatPreferences())));
         entryFetchers.add(new SearchBasedEntryFetcher(new ArXiv()));
         entryFetchers.add(new SearchBasedEntryFetcher(new GvkFetcher()));
     }
 
     public List<EntryFetcher> getEntryFetchers() {
         return Collections.unmodifiableList(this.entryFetchers);
+    }
+
+    public static List<IdBasedFetcher> getIdFetchers() {
+        List<IdBasedFetcher> list = new LinkedList<>();
+        list.add(new IsbnFetcher(Globals.prefs.getImportFormatPreferences()));
+        list.add(new DiVA(Globals.prefs.getImportFormatPreferences()));
+        list.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        return list;
     }
 }
