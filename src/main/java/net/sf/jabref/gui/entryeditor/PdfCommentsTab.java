748dd043bf656f2cc964b16fa72b166bc33e3ed6
Ali <ali.ayan@hotmail.de>
2016-09-05 23:30:55 +0200

    Merge branch 'PdfTab' of https://github.com/ayanai1/jabref into PdfTab
    # Conflicts:
    #	CHANGELOG.md
    #	src/databaseTest/java/net/sf/jabref/shared/DBMSSynchronizerTest.java
    #	src/databaseTest/java/net/sf/jabref/shared/SynchronizationTestSimulator.java
    #	src/jmh/java/net/sf/jabref/benchmarks/Benchmarks.java
    #	src/main/java/net/sf/jabref/BibDatabaseContext.java
    #	src/main/java/net/sf/jabref/gui/BasePanel.java
    #	src/main/java/net/sf/jabref/gui/DuplicateResolverDialog.java
    #	src/main/java/net/sf/jabref/gui/JabRefFrame.java
    #	src/main/java/net/sf/jabref/gui/StringDialog.java
    #	src/main/java/net/sf/jabref/gui/actions/BrowseAction.java
    #	src/main/java/net/sf/jabref/gui/bibtexkeypattern/BibtexKeyPatternDialog.java
    #	src/main/java/net/sf/jabref/gui/entryeditor/EntryEditor.java
    #	src/main/java/net/sf/jabref/gui/entryeditor/PdfCommentsTab.java
    #	src/main/java/net/sf/jabref/gui/importer/ImportFileFilter.java
    #	src/main/java/net/sf/jabref/gui/importer/actions/AppendDatabaseAction.java
    #	src/main/java/net/sf/jabref/gui/maintable/MainTable.java
    #	src/main/java/net/sf/jabref/gui/maintable/MainTableColumn.java
    #	src/main/java/net/sf/jabref/gui/mergeentries/MergeFetchedEntryDialog.java
    #	src/main/java/net/sf/jabref/gui/openoffice/StyleSelectDialog.java
    #	src/main/java/net/sf/jabref/gui/preftabs/PreferencesDialog.java
    #	src/main/java/net/sf/jabref/gui/protectedterms/ProtectedTermsDialog.java
    #	src/main/java/net/sf/jabref/gui/shared/OpenSharedDatabaseDialog.java
    #	src/main/java/net/sf/jabref/gui/shared/SharedDatabaseUIManager.java
    #	src/main/java/net/sf/jabref/gui/util/PositionWindow.java
    #	src/main/java/net/sf/jabref/logic/auxparser/AuxParser.java
    #	src/main/java/net/sf/jabref/logic/bibtexkeypattern/BibtexKeyPatternPreferences.java
    #	src/main/java/net/sf/jabref/logic/bibtexkeypattern/BibtexKeyPatternUtil.java
    #	src/main/java/net/sf/jabref/logic/importer/fileformat/BibtexParser.java
    #	src/main/java/net/sf/jabref/logic/importer/fileformat/PdfContentImporter.java
    #	src/main/java/net/sf/jabref/logic/msbib/BibTeXConverter.java
    #	src/main/java/net/sf/jabref/logic/msbib/MSBibDatabase.java
    #	src/main/java/net/sf/jabref/logic/msbib/MSBibEntry.java
    #	src/main/java/net/sf/jabref/logic/pdf/PdfCommentImporter.java
    #	src/main/java/net/sf/jabref/logic/util/io/RegExpFileSearch.java
    #	src/main/java/net/sf/jabref/logic/xmp/XMPSchemaBibtex.java
    #	src/main/java/net/sf/jabref/model/database/BibDatabase.java
    #	src/main/java/net/sf/jabref/model/entry/BibEntry.java
    #	src/main/java/net/sf/jabref/pdfimport/PdfImporter.java
    #	src/main/java/net/sf/jabref/preferences/JabRefPreferences.java
    #	src/main/java/net/sf/jabref/shared/DBMSProcessor.java
    #	src/main/java/net/sf/jabref/shared/DBMSSynchronizer.java
    #	src/test/java/net/sf/jabref/BibDatabaseContextTest.java
    #	src/test/java/net/sf/jabref/gui/importer/actions/ConvertLegacyExplicitGroupsTest.java
    #	src/test/java/net/sf/jabref/logic/auxparser/AuxParserTest.java
    #	src/test/java/net/sf/jabref/logic/bibtexkeypattern/BibtexKeyPatternUtilTest.java
    #	src/test/java/net/sf/jabref/logic/exporter/BibtexDatabaseWriterTest.java
    #	src/test/java/net/sf/jabref/logic/groups/ExplicitGroupTest.java
    #	src/test/java/net/sf/jabref/logic/groups/GroupTreeNodeTest.java
    #	src/test/java/net/sf/jabref/logic/groups/KeywordGroupTest.java
    #	src/test/java/net/sf/jabref/logic/importer/fileformat/BibtexParserTest.java
    #	src/test/java/net/sf/jabref/logic/integrity/EntryLinkCheckerTest.java
    #	src/test/java/net/sf/jabref/model/database/KeyChangeListenerTest.java
    #	src/test/resources/net/sf/jabref/logic/exporter/MsBibExportFormatTest2.xml

--- src/main/java/net/sf/jabref/gui/entryeditor/PdfCommentsTab.java
+++ src/main/java/net/sf/jabref/gui/entryeditor/PdfCommentsTab.java
@@ -1,9 +1,6 @@
 package net.sf.jabref.gui.entryeditor;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -17,36 +14,32 @@ import javax.swing.JTextArea;
 import net.sf.jabref.gui.BasePanel;
 import net.sf.jabref.gui.JabRefFrame;
 import net.sf.jabref.logic.pdf.PdfCommentImporter;
-import net.sf.jabref.model.entry.BibEntry;
 import net.sf.jabref.model.entry.FieldName;
+import javax.swing.JTextField;
 
 public class PdfCommentsTab extends JPanel {
 
-    private JPanel informationPanel = new JPanel();
+    private final JPanel informationPanel = new JPanel();
 
-    private JList<String> commentList = new JList<>();
+    private final JList<String> commentList = new JList<>();
 
-    private final JScrollPane commentScrollPane = new JScrollPane(commentList);
+    private final JScrollPane commentScrollPane = new JScrollPane();
 
-    private JLabel authorLabel = new JLabel("author",JLabel.CENTER);
+    private final JLabel authorLabel = new JLabel("author", JLabel.CENTER);
 
-    private JTextArea authorArea = new JTextArea("author",2,25);
+    private final JTextArea authorArea = new JTextArea("author", 2, 25);
 
-    private JScrollPane authorScrollPane = new JScrollPane(authorArea);
+    private final JScrollPane authorScrollPane = new JScrollPane();
 
-    private JLabel dateLabel = new JLabel ("date",JLabel.CENTER);
+    private final JLabel dateLabel = new JLabel("date", JLabel.CENTER);
 
-    private JTextArea dateArea = new JTextArea("date",2,25);
+    private final JTextArea dateArea = new JTextArea("date", 2, 25);
 
-    private JScrollPane dateScrollPane = new JScrollPane(dateArea);
+    private final JScrollPane dateScrollPane = new JScrollPane();
 
-    private JLabel siteLabel = new JLabel("site",JLabel.CENTER);
+    private final JLabel pageLabel = new JLabel("page", JLabel.CENTER);
 
-    private JTextArea siteArea = new JTextArea("site",2,25);
-
-    private JScrollPane siteScrollPane = new JScrollPane(siteArea);
-
-    DefaultListModel<String> listModel;
+    private final JTextArea pageArea = new JTextArea("page", 2, 25);
 
     private final EntryEditor parent;
 
@@ -55,6 +48,7 @@ public class PdfCommentsTab extends JPanel {
     private final JabRefFrame frame;
 
     private final BasePanel basePanel;
+    private JTextField txtCommentContent;
 
     public PdfCommentsTab(EntryEditor parent, JabRefFrame frame, BasePanel basePanel) {
         this.parent = parent;
@@ -62,52 +56,66 @@ public class PdfCommentsTab extends JPanel {
         this.basePanel = basePanel;
         this.tabTitle = "PDF comments";
         this.setUpInformationPanel();
-        listModel  = new DefaultListModel<>();
-        try {
-            this.setUpPdfCommentsTab();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.setUpPdfCommentsTab();
     }
 
-    private void setUpPdfCommentsTab() throws IOException {
+    private void setUpPdfCommentsTab() {
         Optional<String> field = parent.getEntry().getField(FieldName.FILE);
         if (field.isPresent()) {
-
+            DefaultListModel<String> listModel = new DefaultListModel<>();
             commentList.setModel(listModel);
             PdfCommentImporter commentImporter = new PdfCommentImporter();
-            ArrayList<BibEntry> entries = new ArrayList<>();
-            entries.add(parent.getEntry());
-            commentImporter.importPdfFile(entries, basePanel.getBibDatabaseContext());
+            String pdfPath = field.get().replaceFirst(".*?:", System.getProperty("file.separator")).replaceAll(":PDF",
+                    "");
+            HashMap<String, String> importedNotes = commentImporter.importNotes(pdfPath);
+            importedNotes.values().stream().forEach((note) -> listModel.addElement(note));
 
-            HashMap<String, String> importedNotes = commentImporter.importNotes(commentImporter.importPdfFile(entries,
-                    basePanel.getBibDatabaseContext()).get(0));
-            updateShownComments(importedNotes);
+            //TODO add context information for importedNotes
         }
+        setLayout(null);
+        commentScrollPane.setBounds(26, 16, 450, 389);
 
         commentScrollPane.setPreferredSize(new Dimension(450,200));
 
-        this.add(commentScrollPane, BorderLayout.EAST);
-        this.add(informationPanel,BorderLayout.WEST);
-    }
-
-    private void updateShownComments(HashMap<String, String> importedNotes){
-        importedNotes.values().stream().forEach((note) -> listModel.addElement(note));
+        this.add(commentScrollPane);
+        commentScrollPane.setViewportView(commentList);
+        informationPanel.setBounds(583, 16, 650, 389);
+        this.add(informationPanel);
     }
 
 
     private void setUpInformationPanel(){
+        informationPanel.setLayout(null);
+        authorLabel.setBounds(15, 25, 46, 20);
+
+        informationPanel.add(authorLabel);
+        authorScrollPane.setBounds(76, 25, 547, 42);
+        informationPanel.add(authorScrollPane);
+        authorScrollPane.setViewportView(authorArea);
         authorArea.setEditable(false);
+        dateLabel.setBounds(15, 80, 30, 20);
+        informationPanel.add(dateLabel);
+        dateScrollPane.setBounds(76, 83, 547, 42);
+        informationPanel.add(dateScrollPane);
+        dateScrollPane.setViewportView(dateArea);
         dateArea.setEditable(false);
-        siteArea.setEditable(false);
+        pageLabel.setBounds(15, 128, 34, 20);
+        informationPanel.add(pageLabel);
 
-        informationPanel.add(authorLabel, BorderLayout.NORTH);
-        informationPanel.add(authorScrollPane,BorderLayout.NORTH);
-        informationPanel.add(dateLabel, BorderLayout.CENTER);
-        informationPanel.add(dateScrollPane, BorderLayout.CENTER);
-        informationPanel.add(siteLabel, BorderLayout.SOUTH);
-        informationPanel.add(siteScrollPane, BorderLayout.SOUTH);
+        JScrollPane pageScrollPane = new JScrollPane();
+        pageScrollPane.setBounds(76, 141, 547, 42);
+        informationPanel.add(pageScrollPane);
+        pageScrollPane.setViewportView(pageArea);
+        pageArea.setEditable(false);
+
+        txtCommentContent = new JTextField();
+        txtCommentContent.setText("comment content");
+        txtCommentContent.setBounds(76, 200, 547, 173);
+        informationPanel.add(txtCommentContent);
+        txtCommentContent.setColumns(10);
+
+        JLabel lblContent = new JLabel("content");
+        lblContent.setBounds(15, 200, 69, 20);
+        informationPanel.add(lblContent);
     }
-
-
 }
