585cfe6786e9cba7e370c4ccea0d306889e7d874
Tobias Diez <tobiasdiez@gmx.de>
2016-09-11 10:01:59 +0200

    Add a few more tests

--- src/test/java/net/sf/jabref/model/entry/KeywordListTest.java
+++ src/test/java/net/sf/jabref/model/entry/KeywordListTest.java
@@ -1,5 +1,6 @@
 package net.sf.jabref.model.entry;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -45,6 +46,18 @@ public class KeywordListTest {
     public void parseTwoWordReturnsTwoKeywordsWithDifferentDelimiter() throws Exception {
         assertEquals(Arrays.asList(new Keyword("keywordOne"), new Keyword("keywordTwo")),
                 KeywordList.parse("keywordOne| keywordTwo", '|'));
+    }
+
+    @Test
+    public void parseWordsWithWhitespaceReturnsOneKeyword() throws Exception {
+        assertEquals(Collections.singletonList(new Keyword("keyword and one")),
+                KeywordList.parse("keyword and one", ','));
+    }
+
+    @Test
+    public void parseWordsWithWhitespaceAndCommaReturnsTwoKeyword() throws Exception {
+        assertEquals(Arrays.asList(new Keyword("keyword and one"), new Keyword("and two")),
+                KeywordList.parse("keyword and one, and two", ','));
     }
 
     @Test
