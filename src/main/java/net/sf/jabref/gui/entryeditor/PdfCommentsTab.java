d31c64ac950ce9f53a935908a050d3cfa162b0da
braunch <braunch.dev@gmail.com>
2016-09-12 14:45:46 +0200

    Store the selected of the comments list

--- src/main/java/net/sf/jabref/gui/entryeditor/PdfCommentsTab.java
+++ src/main/java/net/sf/jabref/gui/entryeditor/PdfCommentsTab.java
@@ -1,6 +1,7 @@
 package net.sf.jabref.gui.entryeditor;
 
 import java.awt.Dimension;
+import java.awt.SystemColor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -26,8 +28,6 @@ import net.sf.jabref.model.entry.FieldName;
 import net.sf.jabref.model.pdf.PdfComment;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import javax.swing.JButton;
-import java.awt.SystemColor;
 
 public class PdfCommentsTab extends JPanel {
 
@@ -52,6 +52,7 @@ public class PdfCommentsTab extends JPanel {
     private final String tabTitle;
     private final JabRefFrame frame;
     private final BasePanel basePanel;
+    private int commentListSelectedIndex;
 
     HashMap<String, PdfComment> importedNotes = new HashMap<>();
 
@@ -81,8 +82,10 @@ public class PdfCommentsTab extends JPanel {
     public void addComments() throws IOException {
         Optional<String> field = parent.getEntry().getField(FieldName.FILE);
         if (field.isPresent()) {
-            commentList.setModel(listModel);
-            commentList.addListSelectionListener(new CommentListSelectionListener());
+            if(!commentList.getModel().equals(listModel)){
+                commentList.setModel(listModel);
+                commentList.addListSelectionListener(new CommentListSelectionListener());
+            }
             PdfCommentImporter commentImporter = new PdfCommentImporter();
             ArrayList<BibEntry> entries = new ArrayList<>();
             entries.add(parent.getEntry());
@@ -95,6 +98,7 @@ public class PdfCommentsTab extends JPanel {
             } else {
                 importedNotes = commentImporter.importNotes(documents.get(0));
                 updateShownComments(importedNotes);
+                commentList.setSelectedIndex(commentListSelectedIndex);
             }
         }
     }
@@ -172,7 +176,13 @@ public class PdfCommentsTab extends JPanel {
     private class CommentListSelectionListener implements ListSelectionListener {
         @Override
         public void valueChanged(ListSelectionEvent e) {
-            updateTextFields(listModel.get(commentList.getSelectedIndex()));
+            int index;
+            if(commentList.getSelectedIndex() >= 0){
+                index = commentList.getSelectedIndex();
+                updateTextFields(listModel.get(index));
+                commentListSelectedIndex = index;
+            }
+            commentList.setSelectedIndex(commentListSelectedIndex);
         }
     }
 }
