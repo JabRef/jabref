91dc05fdf3e57c4c0ba7c225edb0d11c4dd0f386
Siedlerchr <siedlerkiller@gmail.com>
2016-09-17 13:58:50 +0200

    Make constant static

--- src/main/java/net/sf/jabref/gui/help/HelpAction.java
+++ src/main/java/net/sf/jabref/gui/help/HelpAction.java
@@ -34,7 +34,7 @@ public class HelpAction extends MnemonicAwareAction {
     /**
      * New languages of the help have to be added here
      */
-    private final Set<String> avaiableLangFiles = Stream.of("en", "de", "fr", "in", "ja")
+    private static final Set<String> avaiableLangFiles = Stream.of("en", "de", "fr", "in", "ja")
             .collect(Collectors.toCollection(HashSet::new));
 
     private HelpFile helpPage;
