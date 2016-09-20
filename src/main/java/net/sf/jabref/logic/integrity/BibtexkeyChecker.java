69c47175b012da7fb8fb7eb6e377998e16294862
grimes2 <juergen.lange@unitybox.de>
2016-09-21 00:44:35 +0200

    BibtexkeyChecker

---
+++ src/main/java/net/sf/jabref/logic/integrity/BibtexkeyChecker.java
@@ -0,0 +1,33 @@
+package net.sf.jabref.logic.integrity;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import net.sf.jabref.logic.integrity.IntegrityCheck.Checker;
+import net.sf.jabref.logic.l10n.Localization;
+import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.FieldName;
+
+public class BibtexkeyChecker implements Checker {
+
+    @Override
+    public List<IntegrityMessage> check(BibEntry entry) {
+        Optional<String> valuekey = entry.getField(BibEntry.KEY_FIELD);
+        Optional<String> valueauthor = entry.getField(FieldName.AUTHOR);
+        Optional<String> valuetitle = entry.getField(FieldName.TITLE);
+        Optional<String> valueyear = entry.getField(FieldName.YEAR);
+        String authortitleyear = entry.getAuthorTitleYear(100);
+
+        if (!valueauthor.isPresent() || !valuetitle.isPresent() || !valueyear.isPresent()) {
+            return Collections.emptyList();
+        }
+
+        if (!valuekey.isPresent()) {
+            return Collections.singletonList(new IntegrityMessage(
+                    Localization.lang("empty bibtexkey") + ": " + authortitleyear, entry, BibEntry.KEY_FIELD));
+        }
+
+        return Collections.emptyList();
+    }
+}
