1a9412c5ab532b93329f8e3e220d15fbe1e7395a
Sascha Zeller <zeller.dev@gmail.com>
2016-09-21 14:07:39 +0200

    add more test cases

--- src/integrationTest/java/net/sf/jabref/gui/IdFetcherDialogTest.java
+++ src/integrationTest/java/net/sf/jabref/gui/IdFetcherDialogTest.java
@@ -6,30 +6,32 @@ import java.util.Collection;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
-import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
 
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.dependency.jsr305.Nonnull;
 import org.assertj.swing.fixture.JTableFixture;
+import org.assertj.swing.timing.Condition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import static org.assertj.swing.finder.WindowFinder.findDialog;
+import static org.assertj.swing.timing.Pause.pause;
 
 @RunWith(Parameterized.class)
-public class IdFetcherDialogTest extends AbstractUITest{
+public class IdFetcherDialogTest extends AbstractUITest {
 
-    private final String databaseMode;
+    private final String databaseMode, fetcherType, fetchID;
 
-
-    public IdFetcherDialogTest(String databaseMode) {
+    public IdFetcherDialogTest(String databaseMode, String fetcherType, String fetchID) {
         this.databaseMode = databaseMode;
+        this.fetcherType = fetcherType;
+        this.fetchID = fetchID;
     }
 
     @Test
-    public void addEntryOfGivenType() {
+    public void insertEmptySearchID() {
         mainFrame.menuItemWithPath("File", "New " + databaseMode + " database").click();
         JTableFixture entryTable = mainFrame.table();
 
@@ -47,19 +49,83 @@ public class IdFetcherDialogTest extends AbstractUITest{
 
             @Override
             protected boolean isMatching(@Nonnull JButton jButton) {
-                return "Generate".equals(jButton.getText());
+                return "Generate".equals(jButton.getText()) || "Searching...".equals(jButton.getText());
+            }
+        }).click();
+
+        GenericTypeMatcher<JDialog> matcherEmptyDialog = new GenericTypeMatcher<JDialog>(JDialog.class) {
+            @Override
+            protected boolean isMatching(JDialog dialog) {
+                return "Empty search ID".equals(dialog.getTitle());
+            }
+        };
+
+        findDialog(matcherEmptyDialog).withTimeout(10_000).using(robot()).button(new GenericTypeMatcher<JButton>(JButton.class) {
+
+            @Override
+            protected boolean isMatching(@Nonnull JButton jButton) {
+                return "OK".equals(jButton.getText());
             }
         }).click();
 
         entryTable.requireRowCount(0);
     }
 
+    @Test
+    public void testFetcherDialog() {
+        mainFrame.menuItemWithPath("File", "New " + databaseMode + " database").click();
+        JTableFixture entryTable = mainFrame.table();
 
-    @Parameterized.Parameters(name = "{index}: {0}")
+        entryTable.requireRowCount(0);
+        mainFrame.menuItemWithPath("BibTeX", "New entry...").click();
+
+        GenericTypeMatcher<JDialog> matcher = new GenericTypeMatcher<JDialog>(JDialog.class) {
+            @Override
+            protected boolean isMatching(JDialog dialog) {
+                return "Select entry type".equals(dialog.getTitle());
+            }
+        };
+
+        findDialog(matcher).withTimeout(10_000).using(robot()).comboBox(new GenericTypeMatcher<JComboBox>(JComboBox.class) {
+            @Override
+            protected boolean isMatching(@Nonnull JComboBox component) {
+                return true;
+            }
+        }).selectItem(fetcherType);
+
+        findDialog(matcher).withTimeout(10_000).using(robot()).textBox(new GenericTypeMatcher<JTextComponent>(JTextComponent.class) {
+            @Override
+            protected boolean isMatching(@Nonnull JTextComponent component) {
+                return true;
+            }
+        }).enterText(fetchID);
+
+        findDialog(matcher).withTimeout(10_000).using(robot()).button(new GenericTypeMatcher<JButton>(JButton.class) {
+
+            @Override
+            protected boolean isMatching(@Nonnull JButton jButton) {
+                return "Generate".equals(jButton.getText());
+            }
+        }).click();
+
+        pause(new Condition("entrySize") {
+            @Override
+            public boolean test() {
+                return entryTable.rowCount() == 1;
+            }
+        }, 10_000);
+
+        entryTable.requireRowCount(1);
+    }
+
+
+    @Parameterized.Parameters(name = "{index}: {0} : {1} : {2}")
     public static Collection<Object[]> instancesToTest() {
         return Arrays.asList(
-                new Object[]{"BibTeX"},
-                new Object[]{"BibLaTeX"}
+                new Object[]{"BibTeX", "DOI", "10.1002/9781118257517"},
+                new Object[]{"BibLaTeX", "DOI", "10.1002/9781118257517"},
+                new Object[]{"BibTeX", "ISBN", "9780321356680"},
+                new Object[]{"BibLaTeX", "ISBN", "9780321356680"}
         );
     }
 
