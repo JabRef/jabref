622c5dfe123fdb20cd53e4d5f4b218f7e4af1b50
braunch <braunch.dev@gmail.com>
2016-09-22 14:30:31 +0200

    Fix codacy issue

--- src/main/java/net/sf/jabref/logic/pdf/PdfCommentImporter.java
+++ src/main/java/net/sf/jabref/logic/pdf/PdfCommentImporter.java
@@ -37,9 +37,9 @@ public class PdfCommentImporter {
      * @param path a path to a pdf
      * @return a list with the all the annotations found in the file of the path
      */
-    public ArrayList<PdfComment> importNotes(final String path, final BibDatabaseContext context) {
+    public List<PdfComment> importNotes(final String path, final BibDatabaseContext context) {
 
-        ArrayList<PdfComment> annotationsMap = new ArrayList<>();
+        List<PdfComment> annotationsMap = new ArrayList<>();
 
         PDDocument document = null;
         try {
