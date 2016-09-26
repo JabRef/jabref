3a1f110b3311991fe490b5e528bb5dffbbfbf7d1
Tobias Diez <tobiasdiez@gmx.de>
2016-09-26 15:13:31 +0200

    Remove content selectors (#2068)
    * Remove content selectors
    
    * Fix parsing test
    
    * Remove localization
    
    * Fix roundtrip test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -81,7 +81,7 @@ We refer to [GitHub issues](https://github.com/JabRef/jabref/issues) by using `#
 - Removed 2nd preview style
 - The non-supported feature of being able to define file directories for any extension is removed. Still, it should work for older databases using the legacy `ps` and `pdf` fields, although we strongly encourage using the `file` field.
 - Automatic migration for the `evastar_pdf` field is removed.
-
+- We removed the customizable "content selectors" since they are replaced by the auto-completion feature
 
 
 
--- src/main/java/net/sf/jabref/logic/autocompleter/ContentAutoCompleters.java
+++ src/main/java/net/sf/jabref/logic/autocompleter/ContentAutoCompleters.java
@@ -1,19 +1,17 @@
 package net.sf.jabref.logic.autocompleter;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import net.sf.jabref.logic.journals.JournalAbbreviationLoader;
 import net.sf.jabref.model.database.BibDatabase;
-import net.sf.jabref.model.metadata.MetaData;
 
 public class ContentAutoCompleters extends AutoCompleters {
 
     public ContentAutoCompleters() {
     }
 
-    public ContentAutoCompleters(BibDatabase database, MetaData metaData, AutoCompletePreferences preferences,
+    public ContentAutoCompleters(BibDatabase database, AutoCompletePreferences preferences,
             JournalAbbreviationLoader abbreviationLoader) {
         Objects.requireNonNull(preferences);
 
@@ -25,18 +23,5 @@ public class ContentAutoCompleters extends AutoCompleters {
         }
 
         addDatabase(database);
-
-        addContentSelectorValuesToAutoCompleters(metaData);
-    }
-
-    /**
-     * For all fields with both autocompletion and content selector, add content selector
-     * values to the autocompleter list:
-     */
-    public void addContentSelectorValuesToAutoCompleters(MetaData metaData) {
-        for (Map.Entry<String, AutoCompleter<String>> entry : this.autoCompleters.entrySet()) {
-            AutoCompleter<String> ac = entry.getValue();
-            metaData.getContentSelectors(entry.getKey()).forEach(ac::addItemToIndex);
-        }
     }
 }
