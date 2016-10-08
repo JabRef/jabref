dd4b2b5d7f0145f615ef9188672fb82b9c603f60
Christian Bartsch <bartsch.dev@gmail.com>
2016-10-08 12:13:31 +0200

    reformat

--- src/main/java/net/sf/jabref/gui/maintable/ListSynchronizer.java
+++ src/main/java/net/sf/jabref/gui/maintable/ListSynchronizer.java
@@ -40,8 +40,8 @@ public class ListSynchronizer {
     public void listen(EntryChangedEvent entryChangedEvent) {
         lock();
         try {
-            // can't use list#indexOf b/c it won't distinguish between duplicates
-            for (int i=0; i<list.size(); i++) {
+            // cannot use list#indexOf b/c it won't distinguish between duplicates
+            for (int i = 0; i < list.size(); i++) {
                 if (list.get(i) == entryChangedEvent.getBibEntry()) {
                     list.set(i, entryChangedEvent.getBibEntry());
                     break;
