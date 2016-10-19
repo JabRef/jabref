e1e54997555893e2e3cc29042e71658267d2c86a
Oliver Kopp <kopp.dev@gmail.com>
2016-10-19 11:41:15 +0200

    Improve TitleCaseFormatterTest

--- src/test/java/net/sf/jabref/logic/formatter/casechanger/TitleCaseFormatterTest.java
+++ src/test/java/net/sf/jabref/logic/formatter/casechanger/TitleCaseFormatterTest.java
@@ -17,17 +17,44 @@ public class TitleCaseFormatterTest {
     }
 
     @Test
-    public void test() {
+    public void upperEachFirst() {
         Assert.assertEquals("Upper Each First", formatter.format("upper each first"));
+    }
+
+    @Test
+    public void anUpperEachFirstAnd() {
         Assert.assertEquals("An Upper Each First And", formatter.format("an upper each first and"));
+    }
+
+    @Test
+    public void anUpperEachOfTheAndFirstAnd1(){
         Assert.assertEquals("An Upper Each of the and First And",
                 formatter.format("an upper each of the and first and"));
+    }
+
+    @Test
+    public void anUpperEachOfTheAndFirstAnd2() {
         Assert.assertEquals("An Upper Each of: The and First And",
                 formatter.format("an upper each of: the and first and"));
+    }
+
+    @Test
+    public void anUpperEachFirstWithAndWithoutCurlyBrackets1() {
         Assert.assertEquals("An Upper First with and without {CURLY} {brackets}",
                 formatter.format("AN UPPER FIRST WITH AND WITHOUT {CURLY} {brackets}"));
+    }
+
+    @Test
+    public void anUpperEachFirstWithAndWithoutCurlyBrackets2() {
         Assert.assertEquals("An Upper First with {A}nd without {C}urly {b}rackets",
                 formatter.format("AN UPPER FIRST WITH {A}ND WITHOUT {C}URLY {b}rackets"));
+    }
+
+    @Test
+    public void testTwoExperiencesTitle() {
+        Assert.assertEquals(
+        "Two Experiences Designing for Effective Security",
+        formatter.format("Two experiences designing for effective security"));
     }
 
     @Test
