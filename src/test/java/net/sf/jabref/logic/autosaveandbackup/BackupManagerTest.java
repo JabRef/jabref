d3fe407e0033cce300fb24403654a6ce5994cc38
Oliver Kopp <kopp.dev@gmail.com>
2016-10-24 09:40:11 +0200

    Fix method name

--- src/test/java/net/sf/jabref/logic/autosaveandbackup/BackupManagerTest.java
+++ src/test/java/net/sf/jabref/logic/autosaveandbackup/BackupManagerTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class BackupManagerTest {
 
     @Test
-    public void testBackupFileNameGenerationWithLinuxPath() {
+    public void backupFileNameIsCorrectlyGeneratedWithinTmpDirectory() {
         Path bibPath = Paths.get("tmp", "test.bib");
         Path savPath = BackupManager.getBackupPath(bibPath);
         Assert.assertEquals(Paths.get("tmp", "test.bib.sav"), savPath);
