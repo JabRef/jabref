68abc556a9420daa2652f8b3d66f04ba99c86329
Admir Obralija <admir.obralija@mathematik.uni-stuttgart.de>
2016-10-25 20:13:49 +0200

    Remove unused import.

--- src/main/java/net/sf/jabref/JabRefGUI.java
+++ src/main/java/net/sf/jabref/JabRefGUI.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 import javax.swing.JOptionPane;
 import javax.swing.UIDefaults;
