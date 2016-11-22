ed85047aeade910549845cbaf7c92f7660e34b33
Oliver Kopp <kopp.dev@gmail.com>
2016-11-22 08:43:42 +0100

    Fix typos

--- src/test/java/net/sf/jabref/logic/citationstyle/CitationStyleGeneratorTest.java
+++ src/test/java/net/sf/jabref/logic/citationstyle/CitationStyleGeneratorTest.java
@@ -24,9 +24,9 @@ public class CitationStyleGeneratorTest {
     }
 
     @Test
-    public void testIgnoreeCarriageReturnNewLine() {
+    public void testIgnoreCarriageReturnNewLine() {
         BibEntry entry = new BibEntry();
-        entry.setField(FieldName.AUTHOR, "Last, First and\nDoe, Jane");
+        entry.setField(FieldName.AUTHOR, "Last, First and\r\nDoe, Jane");
 
         // if the default citation style changes this has to be modified
         String expected = "  <div class=\"csl-entry\">\n" +
