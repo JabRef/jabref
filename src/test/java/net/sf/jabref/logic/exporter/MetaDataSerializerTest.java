08718b35d98c32e6d1fc31b4846e84756b8e0419
Joerg Lenhard <joerg.lenhard@kau.se>
2016-11-30 17:32:12 +0100

    Extract MetaDataSerializerTest from MetaDataTest

---
+++ src/test/java/net/sf/jabref/logic/exporter/MetaDataSerializerTest.java
@@ -0,0 +1,66 @@
+package net.sf.jabref.logic.exporter;
+
+
+import net.sf.jabref.logic.formatter.casechanger.LowerCaseFormatter;
+import net.sf.jabref.logic.util.OS;
+import net.sf.jabref.model.bibtexkeypattern.AbstractBibtexKeyPattern;
+import net.sf.jabref.model.bibtexkeypattern.GlobalBibtexKeyPattern;
+import net.sf.jabref.model.cleanup.FieldFormatterCleanup;
+import net.sf.jabref.model.cleanup.FieldFormatterCleanups;
+import net.sf.jabref.model.metadata.ContentSelector;
+import net.sf.jabref.model.metadata.MetaData;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class MetaDataSerializerTest {
+
+    private MetaData metaData;
+    private GlobalBibtexKeyPattern pattern;
+
+    @Before
+    public void setUp() {
+        metaData = new MetaData();
+        pattern = new GlobalBibtexKeyPattern(AbstractBibtexKeyPattern.split("[auth][year]"));
+    }
+
+    @Test
+    public void serializeNewMetadataReturnsEmptyMap() throws Exception {
+        assertEquals(Collections.emptyMap(), MetaDataSerializer.getSerializedStringMap(metaData, pattern));
+    }
+
+    @Test
+    public void serializeSingleSaveAction() {
+        FieldFormatterCleanups saveActions = new FieldFormatterCleanups(true,
+                Collections.singletonList(new FieldFormatterCleanup("title", new LowerCaseFormatter())));
+        metaData.setSaveActions(saveActions);
+
+        Map<String, String> expectedSerialization = new TreeMap<>();
+        expectedSerialization.put("saveActions",
+                "enabled;" + OS.NEWLINE + "title[lower_case]" + OS.NEWLINE + ";");
+        assertEquals(expectedSerialization, MetaDataSerializer.getSerializedStringMap(metaData, pattern));
+    }
+
+    @Test
+    public void serializeSingleContentSelectors() {
+        List<String> values = new ArrayList(4);
+        values.add("approved");
+        values.add("captured");
+        values.add("received");
+        values.add("status");
+
+        metaData.addContentSelector(new ContentSelector("status", values));
+
+        Map<String, String> expectedSerialization = new TreeMap<>();
+        expectedSerialization.put(MetaData.SELECTOR_META_PREFIX + "status",
+                "approved;captured;received;status;");
+        assertEquals(expectedSerialization, MetaDataSerializer.getSerializedStringMap(metaData, pattern));
+    }
+}
