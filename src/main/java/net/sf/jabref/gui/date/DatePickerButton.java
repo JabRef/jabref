0dfa4969cd41290c0339d114f9703df509feb1cc
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2016-12-04 21:34:38 +0100

    Switch to LGoodDatePicker

--- src/main/java/net/sf/jabref/gui/date/DatePickerButton.java
+++ src/main/java/net/sf/jabref/gui/date/DatePickerButton.java
@@ -1,26 +1,30 @@
 package net.sf.jabref.gui.date;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import net.sf.jabref.Globals;
+import net.sf.jabref.gui.IconTheme;
 import net.sf.jabref.gui.fieldeditors.FieldEditor;
 import net.sf.jabref.logic.util.date.EasyDateFormat;
 import net.sf.jabref.preferences.JabRefPreferences;
 
-import com.michaelbaranov.microba.calendar.DatePicker;
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.DatePickerSettings;
+import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
+import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
 
 /**
  * wrapper and service class for the DatePicker handling at the EntryEditor
  */
-public class DatePickerButton implements ActionListener {
+public class DatePickerButton implements DateChangeListener {
 
-    private final DatePicker datePicker = new DatePicker();
+    private final DatePicker datePicker;
     private final JPanel panel = new JPanel();
     private final FieldEditor editor;
     private final boolean isoFormat;
@@ -28,24 +32,29 @@ public class DatePickerButton implements ActionListener {
 
     public DatePickerButton(FieldEditor pEditor, Boolean isoFormat) {
         this.isoFormat = isoFormat;
-        datePicker.showButtonOnly(true);
-        datePicker.addActionListener(this);
-        datePicker.setShowTodayButton(true);
+        // Create a date picker: With Hidden text field (Showing button only).
+        DatePickerSettings dateSettings = new DatePickerSettings();
+        dateSettings.setVisibleDateTextField(false);
+        dateSettings.setGapBeforeButtonPixels(0);
+
+        datePicker = new DatePicker(dateSettings);
+        datePicker.addDateChangeListener(this);
+        datePicker.getComponentToggleCalendarButton().setIcon(IconTheme.JabRefIcon.DATE_PICKER.getIcon());
+        datePicker.getComponentToggleCalendarButton().setText("");
+
         panel.setLayout(new BorderLayout());
         panel.add(datePicker, BorderLayout.WEST);
         editor = pEditor;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        Date date = datePicker.getDate();
+    public void dateChanged(DateChangeEvent dateChangeEvent) {
+        LocalDate date = datePicker.getDate();
         if (date != null) {
             if (isoFormat) {
-                editor.setText(EasyDateFormat.isoDateFormat().getDateAt(date));
+                editor.setText(date.format(DateTimeFormatter.ISO_DATE));
             } else {
-                editor.setText(EasyDateFormat
-                        .fromTimeStampFormat(Globals.prefs.get(JabRefPreferences.TIME_STAMP_FORMAT))
-                        .getDateAt(date));
+                EasyDateFormat.fromTimeStampFormat(Globals.prefs.get(JabRefPreferences.TIME_STAMP_FORMAT)).getDateAt(ZonedDateTime.from(date));
             }
         } else {
             // in this case the user selected "none" in the date picker, so we just clear the field
