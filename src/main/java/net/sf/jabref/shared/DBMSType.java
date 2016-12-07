d7933edcbb6e042c12732342d0a8b14d5cf5ea9e
Oliver Kopp <kopp.dev@gmail.com>
2016-12-07 20:05:34 +0100

    Remove dependency on http://impossibl.github.io/pgjdbc-ng

--- src/main/java/net/sf/jabref/shared/DBMSType.java
+++ src/main/java/net/sf/jabref/shared/DBMSType.java
@@ -18,8 +18,8 @@ public enum DBMSType {
             "jdbc:oracle:thin:@%s:%d:%s", 1521),
     POSTGRESQL(
             "PostgreSQL",
-            "com.impossibl.postgres.jdbc.PGDriver",
-            "jdbc:pgsql://%s:%d/%s", 5432);
+            "org.postgresql.Driver",
+            "jdbc:postgresql://%s:%d/%s", 5432);
 
     private final String type;
     private final String driverPath;
