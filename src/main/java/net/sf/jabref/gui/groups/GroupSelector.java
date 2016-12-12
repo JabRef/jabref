9aaea7611ffe471d1b3ba16b13673ec0a0cfd846
Joerg Lenhard <joerg.lenhard@kau.se>
2016-12-12 11:28:42 +0100

    Try to fix issues in GroupSelector

--- src/main/java/net/sf/jabref/gui/groups/GroupSelector.java
+++ src/main/java/net/sf/jabref/gui/groups/GroupSelector.java
@@ -253,7 +253,7 @@ public class GroupSelector extends SidePaneComponent implements TreeSelectionLis
         helpButton.setMargin(butIns);
         openSettings.setMargin(butIns);
         newButton.addActionListener(e -> {
-            GroupDialog gd = new GroupDialog(frame, panel, null);
+            GroupDialog gd = new GroupDialog(frame, null);
             gd.setVisible(true);
             if (gd.okPressed()) {
                 AbstractGroup newGroup = gd.getResultingGroup();
@@ -787,7 +787,7 @@ public class GroupSelector extends SidePaneComponent implements TreeSelectionLis
         public void actionPerformed(ActionEvent e) {
             final GroupTreeNodeViewModel node = getNodeToUse();
             final AbstractGroup oldGroup = node.getNode().getGroup();
-            final GroupDialog gd = new GroupDialog(frame, panel, oldGroup);
+            final GroupDialog gd = new GroupDialog(frame, oldGroup);
             gd.setVisible(true);
             if (gd.okPressed()) {
                 AbstractGroup newGroup = gd.getResultingGroup();
@@ -835,7 +835,7 @@ public class GroupSelector extends SidePaneComponent implements TreeSelectionLis
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            final GroupDialog gd = new GroupDialog(frame, panel, null);
+            final GroupDialog gd = new GroupDialog(frame, null);
             gd.setVisible(true);
             if (!gd.okPressed()) {
                 return; // ignore
@@ -866,7 +866,7 @@ public class GroupSelector extends SidePaneComponent implements TreeSelectionLis
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            final GroupDialog gd = new GroupDialog(frame, panel, null);
+            final GroupDialog gd = new GroupDialog(frame, null);
             gd.setVisible(true);
             if (!gd.okPressed()) {
                 return; // ignore
