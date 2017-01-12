b21005498ce75146107556f7da6e3dd4ab8208c5
Joerg Lenhard <joerg.lenhard@kau.se>
2017-01-12 17:18:38 +0100

    Apply LatexToUnicode for formatting author names in the main table

--- src/main/java/net/sf/jabref/gui/maintable/MainTableNameFormatter.java
+++ src/main/java/net/sf/jabref/gui/maintable/MainTableNameFormatter.java
@@ -2,9 +2,12 @@ package net.sf.jabref.gui.maintable;
 
 import net.sf.jabref.Globals;
 import net.sf.jabref.model.entry.AuthorList;
+import net.sf.jabref.model.strings.LatexToUnicode;
 import net.sf.jabref.preferences.JabRefPreferences;
 
 public class MainTableNameFormatter {
+
+    private static final LatexToUnicode latexToUnicode = new LatexToUnicode();
 
     /**
      * Format a name field for the table, according to user preferences.
@@ -12,7 +15,7 @@ public class MainTableNameFormatter {
      * @param nameToFormat The contents of the name field.
      * @return The formatted name field.
      */
-    public static String formatName(final String nameToFormat) {
+    public static String formatName(String nameToFormat) {
         if (nameToFormat == null) {
             return null;
         }
@@ -24,6 +27,8 @@ public class MainTableNameFormatter {
         final boolean namesFf = Globals.prefs.getBoolean(JabRefPreferences.NAMES_FIRST_LAST);
 
         final boolean abbrAuthorNames = Globals.prefs.getBoolean(JabRefPreferences.ABBR_AUTHOR_NAMES); //MK:
+
+        nameToFormat = latexToUnicode.format(nameToFormat);
 
         if (namesAsIs) {
             return nameToFormat;
