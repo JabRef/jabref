d975b5490b92b910c7bcb9de1b3e70b577ad3553
Joerg Lenhard <joerg.lenhard@kau.se>
2017-02-15 21:35:17 +0100

    Remove unused import

--- src/main/java/net/sf/jabref/model/entry/KeywordList.java
+++ src/main/java/net/sf/jabref/model/entry/KeywordList.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
