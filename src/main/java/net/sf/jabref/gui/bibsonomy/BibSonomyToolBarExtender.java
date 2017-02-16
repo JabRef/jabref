8d6dc44537d95d275d8d9dc29dd2c7074d8da3d5
Oliver Kopp <kopp.dev@gmail.com>
2017-02-16 20:52:30 +0100

    Rewrite BibSonomyToolbarExtender using streams

--- src/main/java/net/sf/jabref/gui/bibsonomy/BibSonomyToolBarExtender.java
+++ src/main/java/net/sf/jabref/gui/bibsonomy/BibSonomyToolBarExtender.java
@@ -1,10 +1,9 @@
 package net.sf.jabref.gui.bibsonomy;
 
-import java.awt.Component;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.JToolBar;
 
@@ -22,44 +21,26 @@ import net.sf.jabref.gui.actions.bibsonomy.ToggleSidePaneComponentAction;
 public class BibSonomyToolBarExtender {
 
     public static void extend(JabRefFrame jabRefFrame, BibSonomySidePaneComponent sidePaneComponent) {
+        Arrays.stream(jabRefFrame.getComponents())
+                .filter(rp -> rp instanceof JRootPane)
+                .flatMap(rp -> Arrays.stream(((JRootPane) rp).getComponents()))
+                .filter(c -> c instanceof JLayeredPane)
+                .flatMap(lp -> Arrays.stream(((JLayeredPane) lp).getComponents()))
+                .filter(tb -> tb instanceof JToolBar)
+                .map(tb -> (JToolBar) tb)
+                .forEach(toolBar -> {
+                    JButton searchEntries = new JButton(new ToggleSidePaneComponentAction(sidePaneComponent));
+                    searchEntries.setText(BibSonomyGlobals.BIBSONOMY_NAME);
+                    toolBar.add(searchEntries, 5);
 
-        for (Component rp : jabRefFrame.getComponents()) {
+                    JButton exportEntries = new JButton(new ExportSelectedEntriesAction(jabRefFrame));
+                    exportEntries.setText(null);
+                    toolBar.add(exportEntries, 6);
 
-            if (rp instanceof JRootPane) {
-
-                for (Component lp : ((JRootPane) rp).getComponents()) {
-
-                    if (lp instanceof JLayeredPane) {
-
-                        for (Component p : ((JLayeredPane) lp).getComponents()) {
-
-                            if (p instanceof JPanel) {
-
-                                for (Component tb : ((JPanel) p).getComponents()) {
-
-                                    if (tb instanceof JToolBar) {
-
-                                        JToolBar toolBar = (JToolBar) tb;
-
-                                        JButton searchEntries = new JButton(new ToggleSidePaneComponentAction(sidePaneComponent));
-                                        searchEntries.setText(BibSonomyGlobals.BIBSONOMY_NAME);
-                                        toolBar.add(searchEntries, 5);
-
-                                        JButton exportEntries = new JButton(new ExportSelectedEntriesAction(jabRefFrame));
-                                        exportEntries.setText(null);
-                                        toolBar.add(exportEntries, 6);
-
-                                        JButton deleteEntries = new JButton(new DeleteSelectedEntriesAction(jabRefFrame));
-                                        deleteEntries.setText(null);
-                                        toolBar.add(deleteEntries, 7);
-
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+                    JButton deleteEntries = new JButton(new DeleteSelectedEntriesAction(jabRefFrame));
+                    deleteEntries.setText(null);
+                    toolBar.add(deleteEntries, 7);
+                });
     }
+
 }
