147c4eedb99ab03cc1f27b7ecf9af98a36b6f904
Tobias Diez <tobiasdiez@gmx.de>
2017-02-17 20:08:12 +0100

    Groups: click on arrow should toggle expansion status

--- src/main/java/org/jabref/gui/util/RecursiveTreeItem.java
+++ src/main/java/org/jabref/gui/util/RecursiveTreeItem.java
@@ -3,6 +3,7 @@ package org.jabref.gui.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javafx.beans.property.BooleanProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
@@ -14,42 +15,52 @@ import javafx.util.Callback;
  */
 public class RecursiveTreeItem<T> extends TreeItem<T> {
 
+    private final Callback<T, BooleanProperty> expandedProperty;
     private Callback<T, ObservableList<T>> childrenFactory;
 
-    public RecursiveTreeItem(Callback<T, ObservableList<T>> func){
-        this(null, func);
+    public RecursiveTreeItem(final T value, Callback<T, ObservableList<T>> func) {
+        this(value, func, null);
     }
 
-    public RecursiveTreeItem(final T value, Callback<T, ObservableList<T>> func){
-        this(value, (Node) null, func);
+    public RecursiveTreeItem(final T value, Callback<T, ObservableList<T>> func, Callback<T, BooleanProperty> expandedProperty) {
+        this(value, (Node) null, func, expandedProperty);
     }
 
-    public RecursiveTreeItem(final T value, Node graphic, Callback<T, ObservableList<T>> func){
+    public RecursiveTreeItem(final T value, Node graphic, Callback<T, ObservableList<T>> func, Callback<T, BooleanProperty> expandedProperty) {
         super(value, graphic);
 
         this.childrenFactory = func;
+        this.expandedProperty = expandedProperty;
 
         if(value != null) {
             addChildrenListener(value);
+            bindExpandedProperty(value, expandedProperty);
         }
 
         valueProperty().addListener((obs, oldValue, newValue)->{
             if(newValue != null){
                 addChildrenListener(newValue);
+                bindExpandedProperty(value, expandedProperty);
             }
         });
+    }
+
+    private void bindExpandedProperty(T value, Callback<T, BooleanProperty> expandedProperty) {
+        if (expandedProperty != null) {
+            expandedProperty().bindBidirectional(expandedProperty.call(value));
+        }
     }
 
     private void addChildrenListener(T value){
         final ObservableList<T> children = childrenFactory.call(value);
 
-        children.forEach(child ->  RecursiveTreeItem.this.getChildren().add(new RecursiveTreeItem<>(child, getGraphic(), childrenFactory)));
+        children.forEach(child -> RecursiveTreeItem.this.getChildren().add(new RecursiveTreeItem<>(child, getGraphic(), childrenFactory, expandedProperty)));
 
         children.addListener((ListChangeListener<T>) change -> {
             while(change.next()){
 
                 if(change.wasAdded()){
-                    change.getAddedSubList().forEach(t-> RecursiveTreeItem.this.getChildren().add(new RecursiveTreeItem<>(t, getGraphic(), childrenFactory)));
+                    change.getAddedSubList().forEach(t -> RecursiveTreeItem.this.getChildren().add(new RecursiveTreeItem<>(t, getGraphic(), childrenFactory, expandedProperty)));
                 }
 
                 if(change.wasRemoved()){
