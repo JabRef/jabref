81d7c59c398b3a6d69c94304cdcf52edbc651bc0
Stefan Kolb <stefan-kolb@web.de>
2017-02-20 13:46:09 +0100

    Revert "Resolves #2570 Disable DOI retrieval with ArXiV"
    This reverts commit 644192bfd6eaa3431c2c6aba54ec6a81eebcd863.

--- src/test/java/org/jabref/logic/importer/fetcher/ArXivTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/ArXivTest.java
@@ -51,7 +51,7 @@ public class ArXivTest {
     }
 
     @Test
-    public void noIdentifierPresent() throws IOException {
+    public void doiNotPresent() throws IOException {
         assertEquals(Optional.empty(), finder.findFullText(entry));
     }
 
@@ -59,6 +59,13 @@ public class ArXivTest {
     public void rejectNullParameter() throws IOException {
         finder.findFullText(null);
         Assert.fail();
+    }
+
+    @Test
+    public void findByDOI() throws IOException {
+        entry.setField("doi", "10.1529/biophysj.104.047340");
+
+        assertEquals(Optional.of(new URL("http://arxiv.org/pdf/cond-mat/0406246v1")), finder.findFullText(entry));
     }
 
     @Test
