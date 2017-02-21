41d15510cf3ba893e1a8254dedab995ecc43371d
Linus Dietz <linus@lynyus.de>
2017-02-21 17:31:22 +0100

    [WIP] Refactored around the FileAnnotationCache. (#2557)
    * Refactored quite much around the FileAnnotationCache. The Cache now works as intended, i.e., fully automatically instead of manual maintenance.
    
    * Reverted an incorrect renaming.
    
    * Reverted an incorrect renaming to the original
    
    * Fixed wrong locale message
    
    * fixing @koppor's remarks
    
    * fixed imports, consitent naming
    
    * minor change

--- src/main/java/org/jabref/logic/pdf/FileAnnotationCache.java
+++ src/main/java/org/jabref/logic/pdf/FileAnnotationCache.java
@@ -1,17 +1,17 @@
 package org.jabref.logic.pdf;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
+import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.pdf.FileAnnotation;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+
 
 public class FileAnnotationCache {
 
@@ -20,18 +20,13 @@ public class FileAnnotationCache {
     //the inner list holds the annotations per file, the outer collection maps this to a BibEntry.
     private LoadingCache<BibEntry, Map<String, List<FileAnnotation>>> annotationCache;
 
-    public FileAnnotationCache() {
+    public FileAnnotationCache(BibDatabaseContext context) {
         annotationCache = CacheBuilder.newBuilder().maximumSize(CACHE_SIZE).build(new CacheLoader<BibEntry, Map<String, List<FileAnnotation>>>() {
             @Override
-            public Map<String, List<FileAnnotation>> load(BibEntry notUsed) throws Exception {
-                // Automated reloading of entries is not supported.
-                return new HashMap<>();
+            public Map<String, List<FileAnnotation>> load(BibEntry entry) throws Exception {
+                return new EntryAnnotationImporter(entry).importAnnotationsFromFiles(context);
             }
         });
-    }
-
-    public void addToCache(BibEntry entry, final Map<String, List<FileAnnotation>> annotations) {
-        annotationCache.put(entry, annotations);
     }
 
     /**
@@ -40,15 +35,7 @@ public class FileAnnotationCache {
      * @param entry entry for which to get the annotations
      * @return Map containing a list of annotations in a list for each file
      */
-    public Optional<Map<String, List<FileAnnotation>>> getFromCache(Optional<BibEntry> entry) {
-        Optional<Map<String, List<FileAnnotation>>> emptyAnnotation = Optional.empty();
-        try {
-            if (entry.isPresent() && annotationCache.get(entry.get()).size() > 0) {
-                return Optional.of(annotationCache.get(entry.get()));
-            }
-        } catch (ExecutionException failure) {
-            return emptyAnnotation;
-        }
-        return emptyAnnotation;
+    public Map<String, List<FileAnnotation>> getFromCache(BibEntry entry) throws ExecutionException {
+        return annotationCache.get(entry);
     }
 }
