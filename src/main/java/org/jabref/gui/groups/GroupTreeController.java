79ff33072c54124ea7693a66bc26244e604a6ebc
Tobias Diez <tobiasdiez@gmx.de>
2017-03-03 21:49:45 +0100

    Add "Remove group and subgroups" option (#2587)
    * Add "Remove group and subgroups" option
    Issue #1407 is fixed in the process
    
    * Remove old code
    
    * Implement #1904: filter groups
    
    * Entries are filtered again correctly

--- src/main/java/org/jabref/gui/groups/GroupTreeController.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeController.java
@@ -34,7 +34,6 @@ import org.apache.commons.logging.LogFactory;
 import org.controlsfx.control.textfield.CustomTextField;
 import org.controlsfx.control.textfield.TextFields;
 import org.fxmisc.easybind.EasyBind;
-import org.fxmisc.easybind.monadic.PropertyBinding;
 
 public class GroupTreeController extends AbstractController<GroupTreeViewModel> {
 
@@ -54,7 +53,7 @@ public class GroupTreeController extends AbstractController<GroupTreeViewModel> 
         viewModel = new GroupTreeViewModel(stateManager, dialogService);
 
         // Set-up bindings
-        viewModel.selectedGroupProperty().bindBidirectional(
+        viewModel.selectedGroupProperty().bind(
                 EasyBind.monadic(groupTree.selectionModelProperty())
                         .flatMap(SelectionModel::selectedItemProperty)
                         .selectProperty(TreeItem::valueProperty)
@@ -154,7 +153,11 @@ public class GroupTreeController extends AbstractController<GroupTreeViewModel> 
         MenuItem addSubgroup = new MenuItem(Localization.lang("Add subgroup"));
         addSubgroup.setOnAction(event -> viewModel.addNewSubgroup(group));
 
+        MenuItem removeGroupAndSubgroups = new MenuItem(Localization.lang("Remove group and subgroups"));
+        removeGroupAndSubgroups.setOnAction(event -> viewModel.removeGroupAndSubgroups(group));
+
         menu.getItems().add(addSubgroup);
+        menu.getItems().add(removeGroupAndSubgroups);
         return menu;
     }
 
