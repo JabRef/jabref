dd29a189e033c900769fe4256eaa9967575fcc4e
TRvanRossum <trvanrossum@gmail.com>
2017-03-06 13:54:02 +0100

    Cleanup window has a scrollbar now. (#2614)
    * Cleanup dialog has a scroll bar now.
    
    * Remove getPanel.
    
    * Add change to changelog
    
    * Fix checkstyle issue

--- src/main/java/org/jabref/gui/actions/CleanupAction.java
+++ src/main/java/org/jabref/gui/actions/CleanupAction.java
@@ -132,8 +132,7 @@ public class CleanupAction extends AbstractWorker {
 
     private int showDialog(CleanupPresetPanel presetPanel) {
         String dialogTitle = Localization.lang("Cleanup entries");
-
-        Object[] messages = {Localization.lang("What would you like to clean up?"), presetPanel.getPanel()};
+        Object[] messages = {Localization.lang("What would you like to clean up?"), presetPanel.getScrollPane()};
         return JOptionPane.showConfirmDialog(frame, messages, dialogTitle, JOptionPane.OK_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE);
     }
