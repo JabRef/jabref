0a5ee48cb9228e06ace29e893cd8ae30d28b1231
Tim <trvanrossum@gmail.com>
2017-03-15 19:58:27 +0100

    Add more private constructors.

--- src/main/java/org/jabref/logic/groups/DefaultGroupsFactory.java
+++ src/main/java/org/jabref/logic/groups/DefaultGroupsFactory.java
@@ -5,6 +5,9 @@ import org.jabref.model.groups.AllEntriesGroup;
 
 public class DefaultGroupsFactory {
 
+    private DefaultGroupsFactory() {
+    }
+
     public static AllEntriesGroup getAllEntriesGroup() {
         AllEntriesGroup group = new AllEntriesGroup(Localization.lang("All entries"));
         group.setIconCode(org.jabref.gui.IconTheme.JabRefIcon.ALL_ENTRIES_GROUP_ICON.getCode());
