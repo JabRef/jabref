0fcb9a57c6dca16e278d7791ee10158375deec19
Tobias Diez <tobiasdiez@gmx.de>
2017-03-16 22:59:16 +0100

    Mark as deprecated

--- src/main/java/org/jabref/logic/bibtex/InvalidFieldValueException.java
+++ src/main/java/org/jabref/logic/bibtex/InvalidFieldValueException.java
@@ -1,5 +1,9 @@
 package org.jabref.logic.bibtex;
 
+/**
+ * @deprecated implement as {@link org.jabref.logic.integrity.IntegrityCheck} instead.
+ */
+@Deprecated
 public class InvalidFieldValueException extends Exception {
 
     public InvalidFieldValueException(String message) {
