48ca329887f3350f2b80163e32856033e0281176
Stefan Kolb <stefan-kolb@web.de>
2017-03-17 15:05:22 +0100

    These imports are driving me insane...

--- src/main/java/org/jabref/gui/openoffice/OOBibBase.java
+++ src/main/java/org/jabref/gui/openoffice/OOBibBase.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
--- src/main/java/org/jabref/logic/openoffice/OOUtil.java
+++ src/main/java/org/jabref/logic/openoffice/OOUtil.java
@@ -3,7 +3,6 @@ package org.jabref.logic.openoffice;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
