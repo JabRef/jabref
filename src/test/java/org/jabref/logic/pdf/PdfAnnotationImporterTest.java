8ed6abf1a9a3a486f7d62b84e153b1a14130c18d
Linus Dietz <linus@lynyus.de>
2017-03-17 20:20:39 +0100

    Changed a test using Collections.singletonList(). It fails

--- src/test/java/org/jabref/logic/pdf/PdfAnnotationImporterTest.java
+++ src/test/java/org/jabref/logic/pdf/PdfAnnotationImporterTest.java
@@ -1,8 +1,10 @@
 package org.jabref.logic.pdf;
 
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.jabref.model.pdf.FileAnnotation;
 
@@ -45,14 +47,21 @@ public class PdfAnnotationImporterTest {
     @Test
     public void inlineNoteMinimal() {
         List<FileAnnotation> annotations = importer.importAnnotations(Paths.get("src/test/resources/pdfs/minimal-inlinenote.pdf"));
-        assertEquals(1, annotations.size());
 
+        // Begin old test
+        assertEquals(1, annotations.size());
         FileAnnotation note = annotations.get(0);
         assertEquals("Linus Dietz", note.getAuthor());
-        assertEquals("inline note annotation", note.getContent());
-        assertFalse(note.hasLinkedAnnotation());
-        assertEquals("FreeText", note.getAnnotationType());
+        assertEquals(LocalDateTime.of(2017, 3, 12, 20, 25), note.getTimeModified());
         assertEquals(1, note.getPage());
+        assertEquals("inline note annotation", note.getContent());
+        assertEquals("FreeText", note.getAnnotationType());
+        assertFalse(note.hasLinkedAnnotation());
+        // End old test
+
+        FileAnnotation expected = new FileAnnotation("Linus Dietz", LocalDateTime.of(2017, 3, 12, 20, 25), 1, "inline note annotation", "FreeText", Optional.empty());
+        assertEquals("Comparison fails. WHY?", Collections.singletonList(expected), annotations);
+
     }
 
     @Test
