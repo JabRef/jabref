95506f2275b16dafd33833141980d5e1263c3e9c
Siedlerchr <siedlerkiller@gmail.com>
2017-04-16 20:37:47 +0200

    Fix paths handling in test and mock fileDirPrefs

--- src/test/java/org/jabref/model/database/BibDatabaseContextTest.java
+++ src/test/java/org/jabref/model/database/BibDatabaseContextTest.java
@@ -1,12 +1,9 @@
 package org.jabref.model.database;
 
-import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.jabref.model.metadata.FileDirectoryPreferences;
 
 import org.junit.Before;
@@ -15,70 +12,67 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 public class BibDatabaseContextTest {
 
-    // The 'currentWorkingDir' variable is used to re-create the part
-    // of the JabRef internal state that we get when the 'jabref
-    // biblio.bib' command is invoked from a command line (on
-    // Unix/Linux, but I guess on Windows clones as well). In the
-    // above-mentioned command, the current working directory must be
-    // used as a 'biblio.bib' parent directory. Since the current
-    // working directory is different on various computers that invoke
-    // the test, we can not hard-code it into the test but must
-    // determine it at run-time:
-    private String currentWorkingDir;
+
+    private Path currentWorkingDir;
 
     // Store the minimal preferences for the
     // BibDatabaseContext.getFileDirectories(File,
     // FileDirectoryPreferences) incocation:
-    private FileDirectoryPreferences preferences;
+    private FileDirectoryPreferences fileDirPrefs;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() {
-        Map<String, String> mapFieldDirs = new HashMap<>();
-        preferences = new FileDirectoryPreferences("saulius", mapFieldDirs, true);
-        currentWorkingDir = Paths.get(System.getProperty("user.dir")).toString();
+        fileDirPrefs = mock(FileDirectoryPreferences.class);
+        currentWorkingDir = Paths.get(System.getProperty("user.dir"));
+        when(fileDirPrefs.isBibLocationAsPrimary()).thenReturn(true);
     }
 
     @Test
     public void getFileDirectoriesWithEmptyDbParent() {
         BibDatabaseContext dbContext = new BibDatabaseContext();
-        dbContext.setDatabaseFile(new File("biblio.bib"));
-        List<String> fileDirectories = dbContext.getFileDirectories( "file", preferences );
-        assertEquals(Collections.singletonList(currentWorkingDir),
+        dbContext.setDatabaseFile(Paths.get("biblio.bib").toFile());
+        List<String> fileDirectories = dbContext.getFileDirectories("file", fileDirPrefs);
+        assertEquals(Collections.singletonList(currentWorkingDir.toString()),
                 fileDirectories);
     }
 
     @Test
     public void getFileDirectoriesWithRelativeDbParent() {
-        String dbDirectory = "relative/subdir";
+        Path file = Paths.get("relative/subdir").resolve("biblio.bib");
+
         BibDatabaseContext dbContext = new BibDatabaseContext();
-        dbContext.setDatabaseFile(new File(dbDirectory + "/" + "biblio.bib"));
-        List<String> fileDirectories = dbContext.getFileDirectories("file", preferences);
-        assertEquals(Collections.singletonList(currentWorkingDir + "/" + dbDirectory),
+        dbContext.setDatabaseFile(file.toFile());
+        List<String> fileDirectories = dbContext.getFileDirectories("file", fileDirPrefs);
+        assertEquals(Collections.singletonList(currentWorkingDir.resolve(file.getParent()).toString()),
                 fileDirectories);
     }
 
     @Test
     public void getFileDirectoriesWithRelativeDottedDbParent() {
-        String dbDirectory = "./relative/subdir";
+        Path file = Paths.get("./relative/subdir").resolve("biblio.bib");
+
         BibDatabaseContext dbContext = new BibDatabaseContext();
-        dbContext.setDatabaseFile(new File(dbDirectory + "/" + "biblio.bib"));
-        List<String> fileDirectories = dbContext.getFileDirectories("file", preferences);
-        assertEquals(Collections.singletonList(currentWorkingDir + "/" + dbDirectory),
+        dbContext.setDatabaseFile(file.toFile());
+        List<String> fileDirectories = dbContext.getFileDirectories("file", fileDirPrefs);
+        assertEquals(Collections.singletonList(currentWorkingDir.resolve(file.getParent()).toString()),
                 fileDirectories);
     }
 
     @Test
     public void getFileDirectoriesWithAbsoluteDbParent() {
-        String dbDirectory = "/absolute/subdir";
+        Path file = Paths.get("/absolute/subdir").resolve("biblio.bib");
+
         BibDatabaseContext dbContext = new BibDatabaseContext();
-        dbContext.setDatabaseFile(new File(dbDirectory + "/" + "biblio.bib"));
-        List<String> fileDirectories = dbContext.getFileDirectories("file", preferences);
-        assertEquals(Collections.singletonList(dbDirectory), fileDirectories);
+        dbContext.setDatabaseFile(file.toFile());
+        List<String> fileDirectories = dbContext.getFileDirectories("file", fileDirPrefs);
+        assertEquals(Collections.singletonList(currentWorkingDir.resolve(file.getParent()).toString()),
+                fileDirectories);
     }
 }
