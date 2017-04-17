80202542e9f469d9a150ae13f4e6a45c5bac8d35
Oliver Kopp <kopp.dev@gmail.com>
2017-04-17 15:12:23 +0200

    Fix RisImporter

--- src/main/java/org/jabref/logic/importer/fileformat/RisImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/RisImporter.java
@@ -70,7 +70,7 @@ public class RisImporter extends Importer {
             String startPage = "";
             String endPage = "";
             String comment = "";
-            BibEntry b = new BibEntry(type);
+            Optional<Month> month = Optional.empty();
             Map<String, String> fields = new HashMap<>();
 
             String[] lines = entry1.split("\n");
@@ -200,8 +200,7 @@ public class RisImporter extends Importer {
                         if ((parts.length > 1) && !parts[1].isEmpty()) {
                             try {
                                 int monthNumber = Integer.parseInt(parts[1]);
-                                Optional<Month> month = Month.getMonthByNumber(monthNumber);
-                                month.ifPresent(parsedMonth -> b.setMonth(parsedMonth));
+                                month = Month.getMonthByNumber(monthNumber);
                             } catch (NumberFormatException ex) {
                                 // The month part is unparseable, so we ignore it.
                             }
@@ -246,7 +245,11 @@ public class RisImporter extends Importer {
             fields.entrySet().removeIf(key -> (key.getValue() == null) || key.getValue().trim().isEmpty());
 
             // create one here
+            // type is set in the loop above
+            BibEntry b = new BibEntry(type);
             b.setField(fields);
+            // month has a special treatment as we use the separate method "setMonth" of BibEntry instead of directly setting the value
+            month.ifPresent(parsedMonth -> b.setMonth(parsedMonth));
             bibitems.add(b);
 
         }
