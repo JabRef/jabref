e25e32b35fcd83acb0b32a699ee4bbb343390274
Linus Dietz <linus@lynyus.de>
2017-04-21 15:18:26 +0200

    Fixes handling of unknown PDAnnotation types.

--- src/main/java/org/jabref/model/pdf/FileAnnotationType.java
+++ src/main/java/org/jabref/model/pdf/FileAnnotationType.java
@@ -1,6 +1,16 @@
 package org.jabref.model.pdf;
 
 
+import java.util.Locale;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+
+/**
+ * Our representation of the type of the FileAnnotation. This is needed as some FileAnnotationTypes require special
+ * handling (e.g., Highlight or Underline), because of the linked FileAnnotations.
+ */
 public enum FileAnnotationType {
     TEXT("Text"),
     HIGHLIGHT("Highlight"),
@@ -12,12 +22,31 @@ public enum FileAnnotationType {
     FREETEXT("FreeText"),
     STRIKEOUT("Strikeout"),
     LINK("Link"),
+    UNKNOWN("Unknown"),
     NONE("None");
 
     private final String name;
+    private static final Log LOGGER = LogFactory.getLog(FileAnnotationType.class);
+
 
     FileAnnotationType(String name) {
         this.name = name;
+    }
+
+    /**
+     * Determines the FileAnnotationType of a raw PDAnnotation. Returns 'UNKNOWN' if the type is currently not in our
+     * list of FileAnnotationTypes.
+     *
+     * @param annotation the raw PDAnnotation
+     * @return The determined FileAnnotationType
+     */
+    public static FileAnnotationType parseFileAnnotationType(PDAnnotation annotation) {
+        try {
+            return FileAnnotationType.valueOf(annotation.getSubtype().toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException e) {
+            LOGGER.info(String.format("FileAnnotationType %s is not supported and was converted into 'Unknown'!", annotation.getSubtype()));
+            return UNKNOWN;
+        }
     }
 
     public String toString() {
