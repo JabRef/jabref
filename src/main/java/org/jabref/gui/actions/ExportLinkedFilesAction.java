42b0c627e21aa324c81c03c3724ec7f298058dcf
Siedlerchr <siedlerkiller@gmail.com>
2017-04-29 14:35:01 +0200

    try around with some background task stuf

--- src/main/java/org/jabref/gui/actions/ExportLinkedFilesAction.java
+++ src/main/java/org/jabref/gui/actions/ExportLinkedFilesAction.java
@@ -1,7 +1,6 @@
 package org.jabref.gui.actions;
 
 import java.awt.event.ActionEvent;
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -10,22 +9,29 @@ import java.util.function.BiFunction;
 
 import javax.swing.AbstractAction;
 
+import javafx.scene.control.ProgressBar;
+
 import org.jabref.Globals;
 import org.jabref.JabRefGUI;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.FXDialogService;
+import org.jabref.gui.util.BackgroundTask;
 import org.jabref.gui.util.DefaultTaskExecutor;
 import org.jabref.gui.util.DirectoryDialogConfiguration;
-import org.jabref.logic.TypedBibEntry;
+import org.jabref.gui.util.TaskExecutor;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.ParsedFileField;
+import org.jabref.model.entry.LinkedFile;
+import org.jabref.model.util.FileHelper;
 import org.jabref.model.util.OptionalUtil;
 import org.jabref.preferences.JabRefPreferences;
 
+
 public class ExportLinkedFilesAction extends AbstractAction {
+
+    private final TaskExecutor taskExecutor = Globals.taskExecutor;
 
     public ExportLinkedFilesAction() {
         super(Localization.lang("Export linked files"));
@@ -46,19 +52,22 @@ public class ExportLinkedFilesAction extends AbstractAction {
 
         BibDatabaseContext databaseContext = JabRefGUI.getMainFrame().getCurrentBasePanel().getDatabaseContext();
 
-        System.out.println("Files count " + entries.stream()
-                .flatMap(entry -> new TypedBibEntry(entry, databaseContext).getFiles().stream()).count());
+        System.out.println("Files count " + entries.stream().flatMap(entry -> entry.getFiles().stream()).count());
+
+
+        int counter = 0;
+        BackgroundTask<Void> task = BackgroundTask.wrap(() -> {
+
 
         for (BibEntry entry : entries) {
-            TypedBibEntry typedEntry = new TypedBibEntry(entry, databaseContext);
 
-            List<ParsedFileField> files = typedEntry.getFiles();
-            for (ParsedFileField fileEntry : files) {
+            List<LinkedFile> files = entry.getFiles();
+            for (LinkedFile fileEntry : files) {
+
                 String fileName = fileEntry.getLink();
 
-                Optional<Path> fileToExport = FileUtil.expandFilename(fileName,
-                        databaseContext.getFileDirectories(Globals.prefs.getFileDirectoryPreferences()))
-                        .map(File::toPath);
+                Optional<Path> fileToExport = FileHelper.expandFilename(fileName,
+                        databaseContext.getFileDirectories(Globals.prefs.getFileDirectoryPreferences()));
 
                 Optional<Path> newPath = OptionalUtil.combine(path, fileToExport, resolvePathFilename);
 
@@ -70,6 +79,16 @@ public class ExportLinkedFilesAction extends AbstractAction {
 
             }
         }
+            return null;
+        });
+
+        System.out.println("Final counter " + counter);
+
+
+        ProgressBar bar = new ProgressBar();
+        bar.progressProperty().bind(task.);
+
+
     }
 
     BiFunction<Path, Path, Path> resolvePathFilename = (p, f) -> {
