2b942b63cbd61b1909b3e6414d7e0ee450736e42
Tobias Diez <tobiasdiez@gmx.de>
2017-05-31 23:03:55 +0200

    Remove FXML for SimpleEditor to improve performance

--- src/main/java/org/jabref/gui/fieldeditors/SimpleEditor.java
+++ src/main/java/org/jabref/gui/fieldeditors/SimpleEditor.java
@@ -3,26 +3,26 @@ package org.jabref.gui.fieldeditors;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 import org.jabref.gui.fieldeditors.contextmenu.EditorMenus;
-import org.jabref.gui.util.ControlHelper;
 import org.jabref.model.entry.BibEntry;
 
 public class SimpleEditor extends HBox implements FieldEditorFX {
 
     protected final String fieldName;
     @FXML private final SimpleEditorViewModel viewModel;
-    @FXML private EditorTextArea textArea;
 
     public SimpleEditor(String fieldName) {
         this.fieldName = fieldName;
         this.viewModel = new SimpleEditorViewModel();
 
-        ControlHelper.loadFXMLForControl(this);
-
+        EditorTextArea textArea = new EditorTextArea();
+        textArea.prefHeight(0);
+        HBox.setHgrow(textArea, Priority.ALWAYS);
         textArea.textProperty().bindBidirectional(viewModel.textProperty());
-
         textArea.addToContextMenu(EditorMenus.getDefaultMenu(textArea));
+        this.getChildren().add(textArea);
     }
 
     @Override
