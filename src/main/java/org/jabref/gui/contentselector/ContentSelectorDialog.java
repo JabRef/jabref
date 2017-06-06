243fd9f03b61fb9cd00c8ce2bc9b18acded0a697
Tobias Diez <tobiasdiez@gmx.de>
2017-06-06 22:49:31 +0200

    Fix order

--- src/main/java/org/jabref/gui/contentselector/ContentSelectorDialog.java
+++ src/main/java/org/jabref/gui/contentselector/ContentSelectorDialog.java
@@ -73,10 +73,10 @@ public class ContentSelectorDialog extends JabRefDialog {
     private final JTextField wordEditField = new JTextField("", 20);
     private final JScrollPane fPane = new JScrollPane(fieldList);
     private final Map<String, DefaultListModel<String>> wordListModels = new HashMap<>();
+    private final DefaultListModel<String> wordListModel = new DefaultListModel<>();
+    private final JList<String> wordList = new JList<>(wordListModel);
     private final JScrollPane wPane = new JScrollPane(wordList);
     private final List<String> removedFields = new ArrayList<>();
-    private DefaultListModel<String> wordListModel = new DefaultListModel<>();
-    private final JList<String> wordList = new JList<>(wordListModel);
     private String currentField;
 
 
