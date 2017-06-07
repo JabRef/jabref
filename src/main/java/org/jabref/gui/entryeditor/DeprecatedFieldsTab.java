86c6f133338af786b798e7f6b50fe23badfb240c
Tobias Diez <tobiasdiez@gmx.de>
2017-06-07 22:19:44 +0200

    Display deprecated fields in single-column layout

--- src/main/java/org/jabref/gui/entryeditor/DeprecatedFieldsTab.java
+++ src/main/java/org/jabref/gui/entryeditor/DeprecatedFieldsTab.java
@@ -11,7 +11,7 @@ import org.jabref.model.entry.EntryType;
 
 public class DeprecatedFieldsTab extends FieldsEditorTab {
     public DeprecatedFieldsTab(JabRefFrame frame, BasePanel basePanel, EntryType entryType, EntryEditor parent, BibEntry entry) {
-        super(frame, basePanel, entryType.getDeprecatedFields(), parent, false, true, entry);
+        super(frame, basePanel, entryType.getDeprecatedFields(), parent, false, false, entry);
 
         setText(Localization.lang("Deprecated fields"));
         setTooltip(new Tooltip(Localization.lang("Show deprecated BibTeX fields")));
