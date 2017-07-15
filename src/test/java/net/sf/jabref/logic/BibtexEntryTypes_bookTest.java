7ba529c4b5fe2173ccfb7f35ad3283d6ca36c439
Felipe Myose <felipemyose@hotmail.com>
2017-07-14 22:24:11 -0300

    teste funcional para insercao de book

---
+++ src/test/java/net/sf/jabref/logic/BibtexEntryTypes_bookTest.java
@@ -0,0 +1,121 @@
+package net.sf.jabref.logic;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.Set;
+
+import net.sf.jabref.logic.bibtex.BibEntryWriter;
+import net.sf.jabref.logic.bibtex.LatexFieldFormatter;
+import net.sf.jabref.logic.importer.ImportFormatPreferences;
+import net.sf.jabref.logic.importer.ParserResult;
+import net.sf.jabref.logic.importer.fileformat.BibtexParser;
+import net.sf.jabref.logic.util.OS;
+import net.sf.jabref.model.database.BibDatabase;
+import net.sf.jabref.model.database.BibDatabaseMode;
+import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.preferences.JabRefPreferences;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by felipe on 7/14/17.
+ */
+public class BibtexEntryTypes_bookTest {
+
+    private BibEntryWriter writer;
+    private static ImportFormatPreferences importFormatPreferences;
+
+
+    @Before
+    public void setUpWriter() {
+        importFormatPreferences = JabRefPreferences.getInstance().getImportFormatPreferences();
+        writer = new BibEntryWriter(
+                new LatexFieldFormatter(JabRefPreferences.getInstance().getLatexFieldFormatterPreferences()), true);
+    }
+
+    /**
+     * A book with an explicit publisher.
+     * <p>
+     * Required fields: author or editor, title, publisher, year.
+     * Optional fields: volume or number, series, address, edition, month, note.
+     */
+
+    //apenas com os campos obrigatorios/requeridos
+    @Test
+    public void testBibtexEntryType_book_reqFields() throws Exception{
+
+        StringWriter stringWriter = new StringWriter();
+
+        BibEntry entry = new BibEntry("book");
+        entry.setField("author", "George R.R. Martin");
+        entry.setField("title","A Tormenta De Espadas");
+        entry.setField("publisher","LeYa");
+        entry.setField("year","2011");
+        entry.setCiteKey("testeKey");
+
+        writer.write(entry, stringWriter, BibDatabaseMode.BIBTEX);
+
+        String actual = stringWriter.toString();
+
+        // @formatter:off
+        String expected = OS.NEWLINE + "@Book{testeKey," + OS.NEWLINE +
+                "  title     = {A Tormenta De Espadas}," + OS.NEWLINE +
+                "  publisher = {LeYa}," + OS.NEWLINE +
+                "  year      = {2011}," + OS.NEWLINE +
+                "  author    = {George R.R. Martin}," + OS.NEWLINE +
+                "}" + OS.NEWLINE;
+        // @formatter:on
+
+        assertEquals(expected, actual);
+        System.out.print(actual);
+    }
+
+    //com todos os campos preenchidos
+    @Test
+    public void testBibtexEntryType_book_allFields() throws Exception{
+
+        StringWriter stringWriter = new StringWriter();
+
+        BibEntry entry = new BibEntry("book");
+        entry.setField("author", "George R.R. Martin");
+        entry.setField("title","A Tormenta De Espadas");
+        entry.setField("publisher","LeYa");
+        entry.setField("year","2011");
+        entry.setCiteKey("testeKey");
+        entry.setField("volume","3");
+        entry.setField("series","As Cronicas De Gelo E Fogo");
+        entry.setField("address","Sao Paulo");
+        entry.setField("month","setembro");
+        entry.setField("note","livro grande");
+
+        writer.write(entry, stringWriter, BibDatabaseMode.BIBTEX);
+
+        String actual = stringWriter.toString();
+
+        // @formatter:off
+        String expected = OS.NEWLINE + "@Book{testeKey," + OS.NEWLINE +
+                "  title     = {A Tormenta De Espadas}," + OS.NEWLINE +
+                "  publisher = {LeYa}," + OS.NEWLINE +
+                "  year      = {2011}," + OS.NEWLINE +
+                "  author    = {George R.R. Martin}," + OS.NEWLINE +
+                "  volume    = {3}," + OS.NEWLINE +
+                "  series    = {As Cronicas De Gelo E Fogo}," + OS.NEWLINE +
+                "  address   = {Sao Paulo}," + OS.NEWLINE +
+                "  month     = {setembro}," + OS.NEWLINE +
+                "  note      = {livro grande}," + OS.NEWLINE +
+                "}" + OS.NEWLINE;
+        // @formatter:on
+
+        assertEquals(expected, actual);
+        //System.out.print(actual);
+    }
+
+
+}
