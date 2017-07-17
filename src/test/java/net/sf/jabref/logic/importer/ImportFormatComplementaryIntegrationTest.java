c46e3cae71824afb65a4dadf780404bdf87dc076
Daniel Willian <d.willian_sc@hotmail.com>
2017-07-17 18:58:24 -0300

    Adicionado testes para formatos de importacao nao testados

---
+++ src/test/java/net/sf/jabref/logic/importer/ImportFormatComplementaryIntegrationTest.java
@@ -0,0 +1,85 @@
+package net.sf.jabref.logic.importer;
+
+import net.sf.jabref.preferences.JabRefPreferences;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests to complement the equivalence classes of importing database
+ *
+ * Formats already being tested by {@link ImportFormatReaderIntegrationTest}:
+ * Ris
+ * Isi
+ * SilverPlatter
+ * RepecNep
+ * Ovid
+ * Endnote
+ * MsBib
+ * Bibtex
+ *
+ * Formats to be tested by this class:
+ * Biblioscape
+ * BibTeXML
+ * Copac
+ * Inspec
+ * Medline
+ * MedlinePlain
+ * Mods
+ *
+ * Cannot be tested:
+ * FreeCite
+ *
+ * Did not find any examples:
+ * PdfContent
+ * PdfXmp
+ */
+
+@RunWith(Parameterized.class)
+public class ImportFormatComplementaryIntegrationTest {
+    private ImportFormatReader reader;
+
+    private final Path file;
+    public final String format;
+    private final int count;
+
+    public ImportFormatComplementaryIntegrationTest(String resource, String format, int count) throws URISyntaxException {
+        this.file = Paths.get(ImportFormatComplementaryIntegrationTest.class.getResource(resource).toURI());
+        this.format = format;
+        this.count = count;
+    }
+
+    @Before
+    public void setUp() {
+        reader = new ImportFormatReader();
+        reader.resetImportFormats(JabRefPreferences.getInstance().getImportFormatPreferences(),
+                JabRefPreferences.getInstance().getXMPPreferences());
+    }
+
+    @Test
+    public void testImportFormatFromFile() throws Exception {
+        assertEquals(count, reader.importFromFile(format, file).getDatabase().getEntries().size());
+    }
+
+    @Parameterized.Parameters(name = "{index}: {1}")
+    public static Collection<Object[]> importFormats() {
+        Collection<Object[]> result = new ArrayList<>();
+        result.add(new Object[] {"ezboys/BiblioscapeImportTest.btf", "biblioscape", 1});
+        result.add(new Object[] {"ezboys/BibtexmlImportTest.xml", "bibtexml", 1});
+        result.add(new Object[] {"ezboys/CopacImportTest.txt", "cpc", 1});
+        result.add(new Object[] {"ezboys/InspecImportTest.txt", "inspec", 1});
+        result.add(new Object[] {"ezboys/MedlineImportTest.xml", "medline", 1});
+        result.add(new Object[] {"ezboys/MedlinePlainImportTest.txt", "medlineplain", 1});
+        result.add(new Object[] {"ezboys/ModsImportTest.xml", "mods", 1});
+        return result;
+    }
+}
