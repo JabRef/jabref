d1b447b8d53b5286971b5d0b9ac8cf7ad5217510
Oliver Kopp <kopp.dev@gmail.com>
2017-07-18 08:17:18 +0200

    Fix wrong annotation

--- src/main/java/org/jabref/architecture/ApacheCommonsLang3Allowed.java
+++ src/main/java/org/jabref/architecture/ApacheCommonsLang3Allowed.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
  * Annotation to indicate that usage of ApacheCommonsLang3 is explicitly allowed.
  * The intention is to fully switch to Google Guava and only use Apache Commons Lang3 if there is no other possibility
  */
-@Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface ApacheCommonsLang3Allowed {
 
