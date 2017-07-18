105fe3bbb03d6a1c4573d3fe57d29a5b1fc45eab
Linus Dietz <linus@lynyus.de>
2017-07-18 18:51:46 +0200

    Update to JRE 1.8.0_131

--- scripts/prepare-install4j.sh
+++ scripts/prepare-install4j.sh
@@ -7,17 +7,17 @@ fi
 
 # ensure that tar archive of install4j exists
 cd ~/downloads
-wget --quiet -nc http://download-keycdn.ej-technologies.com/install4j/install4j_unix_6_1_5.tar.gz
+wget --quiet -nc http://download-keycdn.ej-technologies.com/install4j/install4j_unix_6_1_6.tar.gz
 
 # extract tar archive of install4j into the source directory of JabRef
 cd ~/jabref
-tar -xzf ~/downloads/install4j_unix_6_1_5.tar.gz
+tar -xzf ~/downloads/install4j_unix_6_1_6.tar.gz
 
 # fetch JREs
 if [ ! -d ~/.install4j6/jres/ ]; then
   mkdir -p ~/.install4j6/jres/
 fi
 cd ~/.install4j6/jres/
-wget --quiet -nc https://files.jabref.org/jres/windows-x86-1.8.0_121.tar.gz
-wget --quiet -nc https://files.jabref.org/jres/windows-amd64-1.8.0_121.tar.gz
-wget --quiet -nc https://files.jabref.org/jres/macosx-amd64-1.8.0_121_unpacked.tar.gz
+wget --quiet -nc https://files.jabref.org/jres/windows-x86-1.8.0_131.tar.gz
+wget --quiet -nc https://files.jabref.org/jres/windows-amd64-1.8.0_131.tar.gz
+wget --quiet -nc https://files.jabref.org/jres/macosx-amd64-1.8.0_131_unpacked.tar.gz
