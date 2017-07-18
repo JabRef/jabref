adc1bfcbcea39417fd82073bee2e46bbf53ea1f8
Oliver Kopp <kopp.dev@gmail.com>
2017-07-18 10:16:00 +0200

    Fix checkstyle

--- src/main/java/org/jabref/architecture/ApacheCommonsLang3Allowed.java
+++ src/main/java/org/jabref/architecture/ApacheCommonsLang3Allowed.java
@@ -1,8 +1,5 @@
 package org.jabref.architecture;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
 /**
  * Annotation to indicate that usage of ApacheCommonsLang3 is explicitly allowed.
  * The intention is to fully switch to Google Guava and only use Apache Commons Lang3 if there is no other possibility
