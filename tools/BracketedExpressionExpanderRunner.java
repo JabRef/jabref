3de29822cce78e7df25bf3fa5bcecb11c9cf56c2
Saulius Gražulis <grazulis@ibt.lt>
2017-08-02 00:35:23 +0300

    Removing the code to set org.apache.commons.logging.Log system property.

--- tools/BracketedExpressionExpanderRunner.java
+++ tools/BracketedExpressionExpanderRunner.java
@@ -1,11 +1,8 @@
-import java.util.Properties;
 import org.jabref.logic.util.BracketedExpressionExpander;
 import org.jabref.model.entry.BibEntry;
 
 class BracketedExpressionExpanderRunner {
     public static void main( String[] args ) {
-        Properties props = System.getProperties();
-        props.setProperty("org.apache.commons.logging.Log", "NoOpLog");
         BracketedExpressionExpander bex = new BracketedExpressionExpander(new BibEntry());
         if( args.length > 0 ) {
             for( int i = 0; i < args.length; i++ ) {
