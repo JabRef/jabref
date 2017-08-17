a06fa19ecf5235c0cd9d44e8dbdcd402ec7bf0ae
Oliver Kopp <kopp.dev@gmail.com>
2017-08-17 08:26:18 +0200

    Release beta version also on snapcraft

--- scripts/run-snapcraft.sh
+++ scripts/run-snapcraft.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 VERSION=`grep "^version =" build.gradle | sed "s/^version = \"\(.*\)\".*/\1/"`
 sed -i "s/4.0-dev/$VERSION/" snapcraft.yaml
-docker run -v $(pwd):$(pwd) -t koppor/snapcraft-java-de:v1.3.0 sh -c "cd $(pwd) && snapcraft && snapcraft push *.snap --release edge"
+
+# last parameter: edge | beta | stable
+docker run -v $(pwd):$(pwd) -t koppor/snapcraft-java-de:v1.3.0 sh -c "cd $(pwd) && snapcraft && snapcraft push *.snap --release beta"
