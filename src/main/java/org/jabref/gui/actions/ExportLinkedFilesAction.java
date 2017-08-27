e1385449b2e6f88d5c624e522cf5651138ab9fe0
Siedlerchr <siedlerkiller@gmail.com>
2017-08-27 19:21:28 +0200

    extract to inner class Add some more output messages renamed variables

--- src/main/java/org/jabref/gui/actions/ExportLinkedFilesAction.java
+++ src/main/java/org/jabref/gui/actions/ExportLinkedFilesAction.java
@@ -30,14 +30,21 @@ import org.jabref.model.util.FileHelper;
 import org.jabref.model.util.OptionalUtil;
 import org.jabref.preferences.JabRefPreferences;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.controlsfx.dialog.ProgressDialog;
 
 public class ExportLinkedFilesAction extends AbstractAction {
 
-    private final BiFunction<Path, Path, Path> resolvePathFilename = (p, f) -> {
-        return p.resolve(f.getFileName());
+    private static final Log LOGGER = LogFactory.getLog(ExportLinkedFilesAction.class);
+    private final BiFunction<Path, Path, Path> resolvePathFilename = (path, file) -> {
+        return path.resolve(file.getFileName());
     };
     private final DialogService ds = new FXDialogService();
+    private long totalFilesCount;
+    private BibDatabaseContext databaseContext;
+    private Optional<Path> exportPath;
+    private List<BibEntry> entries;
 
     public ExportLinkedFilesAction() {
         super(Localization.lang("Export linked files"));
@@ -47,74 +54,80 @@ public class ExportLinkedFilesAction extends AbstractAction {
     public void actionPerformed(ActionEvent e) {
 
         DirectoryDialogConfiguration dirDialogConfiguration = new DirectoryDialogConfiguration.Builder()
-                .withInitialDirectory(
-                        Paths.get(Globals.prefs.get(JabRefPreferences.EXPORT_WORKING_DIRECTORY)))
+                .withInitialDirectory(Paths.get(Globals.prefs.get(JabRefPreferences.EXPORT_WORKING_DIRECTORY)))
                 .build();
-        List<BibEntry> entries = JabRefGUI.getMainFrame().getCurrentBasePanel().getSelectedEntries();
-
-        Optional<Path> path = DefaultTaskExecutor
+        entries = JabRefGUI.getMainFrame().getCurrentBasePanel().getSelectedEntries();
+        exportPath = DefaultTaskExecutor
                 .runInJavaFXThread(() -> ds.showDirectorySelectionDialog(dirDialogConfiguration));
+        databaseContext = JabRefGUI.getMainFrame().getCurrentBasePanel().getDatabaseContext();
+        totalFilesCount = entries.stream().flatMap(entry -> entry.getFiles().stream()).count();
 
-        BibDatabaseContext databaseContext = JabRefGUI.getMainFrame().getCurrentBasePanel().getDatabaseContext();
+        Service<Void> exportService = new ExportService();
+        startServiceAndshowProgessDialog(exportService);
+    }
 
-        long totalFilesCount = entries.stream().flatMap(entry -> entry.getFiles().stream()).count();
-
-        Service<Void> service = new Service<Void>() {
-
-            @Override
-            protected Task<Void> createTask() {
-                return new Task<Void>() {
-
-                    int totalFilesCounter;
-
-                    @Override
-                    protected Void call()
-                            throws InterruptedException {
-                        updateMessage(Localization.lang("Exporting files..."));
-                        updateProgress(0, totalFilesCount);
-
-                        for (int i = 0; i < entries.size(); i++) {
-
-                            List<LinkedFile> files = entries.get(i).getFiles();
-
-                            for (int j = 0; j < files.size(); j++) {
-                                updateMessage(Localization.lang("Exporting file %0 of BibEntry %1", Integer.toString(j), Integer.toString(i)));
-                                Thread.sleep(500); //TODO: Adjust/leave/any other idea?
-
-                                String fileName = files.get(j).getLink();
-                                Optional<Path> fileToExport = FileHelper.expandFilenameAsPath(fileName,
-                                        databaseContext.getFileDirectoriesAsPaths(Globals.prefs.getFileDirectoryPreferences()));
-
-                                Optional<Path> newPath = OptionalUtil.combine(path, fileToExport, resolvePathFilename);
-
-                                newPath.ifPresent(newFile -> {
-                                    FileUtil.copyFile(fileToExport.get(), newFile, false);
-                                    updateProgress(totalFilesCounter++, totalFilesCount);
-                                    updateMessage(Localization.lang("Exported file successfully"));
-
-                                });
-                            }
-                        }
-                        updateMessage(Localization.lang("Finished exporting"));
-                        return null;
-                    }
-                };
-            }
-        };
-
-        service.start();
-
+    private <V> void startServiceAndshowProgessDialog(Service<V> service) {
         DefaultTaskExecutor.runInJavaFXThread(() -> {
-            ProgressDialog dlg = new ProgressDialog(service);
-            dlg.setOnCloseRequest(evt -> service.cancel());
-            DialogPane dialogPane = dlg.getDialogPane();
+            service.start();
+
+            ProgressDialog progressDialog = new ProgressDialog(service);
+            progressDialog.setOnCloseRequest(evt -> service.cancel());
+            DialogPane dialogPane = progressDialog.getDialogPane();
             dialogPane.getButtonTypes().add(ButtonType.CANCEL);
             Button cancelButton = (Button) dialogPane.lookupButton(ButtonType.CANCEL);
             cancelButton.setOnAction(evt -> {
                 service.cancel();
-                dlg.close();
+                progressDialog.close();
             });
-            dlg.showAndWait();
+            progressDialog.showAndWait();
         });
+    }
+
+    private class ExportService extends Service<Void> {
+
+        @Override
+        protected Task<Void> createTask() {
+            return new Task<Void>() {
+
+                int totalFilesCounter;
+                Optional<Path> newPath;
+
+                @Override
+                protected Void call()
+                        throws InterruptedException {
+                    updateMessage(Localization.lang("Exporting files..."));
+                    updateProgress(0, totalFilesCount);
+
+                    for (int i = 0; i < entries.size(); i++) {
+
+                        List<LinkedFile> files = entries.get(i).getFiles();
+
+                        for (int j = 0; j < files.size(); j++) {
+                            updateMessage(Localization.lang("Exporting file %0 of BibEntry %1", Integer.toString(j), Integer.toString(i)));
+                            Thread.sleep(500); //TODO: Adjust/leave/any other idea?
+
+                            String fileName = files.get(j).getLink();
+                            Optional<Path> fileToExport = FileHelper.expandFilenameAsPath(fileName,
+                                    databaseContext.getFileDirectoriesAsPaths(Globals.prefs.getFileDirectoryPreferences()));
+
+                            newPath = OptionalUtil.combine(exportPath, fileToExport, resolvePathFilename);
+
+                            newPath.ifPresent(newFile -> {
+                                boolean success = FileUtil.copyFile(fileToExport.get(), newFile, false);
+                                updateProgress(totalFilesCounter++, totalFilesCount);
+                                if (success) {
+                                    updateMessage(Localization.lang("Exported file successfully"));
+                                } else {
+                                    updateMessage(Localization.lang("Could not export file") + " " + Localization.lang("File exists"));
+                                }
+                            });
+                        }
+                    }
+                    updateMessage(Localization.lang("Finished exporting"));
+                    updateMessage(Localization.lang("Exported %0 files sucessfully to %1", Integer.toString(totalFilesCounter), newPath.map(Path::toString).orElse("")));
+                    return null;
+                }
+            };
+        }
     }
 }
