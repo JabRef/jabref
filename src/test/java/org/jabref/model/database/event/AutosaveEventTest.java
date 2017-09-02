3b46817b47f68d71623c09c7dcfaec63b879cc95
h.k.hiekmann <h.k.hiekmann@nanooq.org>
2017-09-02 17:33:15 +0200

    refactor

--- src/test/java/org/jabref/model/database/event/AutosaveEventTest.java
+++ src/test/java/org/jabref/model/database/event/AutosaveEventTest.java
@@ -1,14 +1,7 @@
 package org.jabref.model.database.event;
 
-import org.jabref.model.database.EntrySorter;
-import org.jabref.model.entry.BibEntry;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class AutosaveEventTest {
