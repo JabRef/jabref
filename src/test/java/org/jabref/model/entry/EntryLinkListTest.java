5a8ace75fb16bfb64cb37652e60f4633d2e6afb5
h.k.hiekmann <h.k.hiekmann@nanooq.org>
2017-09-03 11:12:08 +0200

    one assert per test, add link comparison without toString, use link.isEmpty()

--- src/test/java/org/jabref/model/entry/EntryLinkListTest.java
+++ src/test/java/org/jabref/model/entry/EntryLinkListTest.java
@@ -2,37 +2,44 @@ package org.jabref.model.entry;
 
 import org.jabref.model.database.BibDatabase;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class EntryLinkListTest {
 
-    String fieldValue = "test";
-    BibDatabase bibDatabase = new BibDatabase();
+    private String key = "test";
+    private BibDatabase database = new BibDatabase();
+    private List<ParsedEntryLink> links = EntryLinkList.parse(key, database);
+    private ParsedEntryLink link = links.get(0);
 
     @Test
-    public void givenFieldValueAndDatabaseWhenParsingThenExpectLink() {
-        List<ParsedEntryLink> links = EntryLinkList.parse(fieldValue, bibDatabase);
-        ParsedEntryLink link = links.get(0);
+    public void givenFieldValueAndDatabaseWhenParsingThenExpectKey() {
+        assertEquals(key, link.getKey());
+    }
 
-        assertEquals(fieldValue, link.getKey());
-        assertEquals(bibDatabase, link.getDataBase());
+    @Test
+    public void givenFieldValueAndDatabaseWhenParsingThenExpectDataBase() {
+        assertEquals(database, link.getDataBase());
+    }
+
+    @Test
+    public void givenFieldValueAndDatabaseWhenParsingThenExpectEmptyLinkedEntry() {
         assertEquals(Optional.empty(), link.getLinkedEntry());
     }
 
     @Test
-    public void givenNullFieldValueAndDatabaseWhenParsingThenExpectLinksSizeZero() {
-        List<ParsedEntryLink> links = EntryLinkList.parse(null, bibDatabase);
+    public void givenFieldValueAndDatabaseWhenParsingThenExpectLink() {
+        ParsedEntryLink expected = new ParsedEntryLink(key, database);
+        assertEquals(expected, link);
+    }
 
-        assertEquals(0, links.size());
+    @Test
+    public void givenNullFieldValueAndDatabaseWhenParsingThenExpectLinksIsEmpty() {
+        links = EntryLinkList.parse(null, database);
+        assertTrue(links.isEmpty());
     }
 }
