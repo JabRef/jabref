df53174fe03fb4fadb84f7472f6354730bb475f8
steph37tours <stephane.curet@orange.fr>
2017-09-05 16:21:43 +0200

    complete of the method localisationTestForInvalidStrings

--- src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
+++ src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
@@ -199,8 +199,7 @@ public class LocalizationConsistencyTest {
                 }
 
                 //parse object "properties" to find any spaces
-                //if there is any space replace it with an underscore
-
+                assertEquals("There are invalid characters, space(s), in the localization properties file", properties.replace(" ", ""), properties);
             }
         }
     }
