7c43a61c083c06030df33fba3d422f20e624e8d0
steph37tours <stephane.curet@orange.fr>
2017-09-06 13:46:43 +0200

    Correcting the method localisationTestForInvalidStrings

--- src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
+++ src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
@@ -184,7 +184,7 @@ public class LocalizationConsistencyTest {
     }
 
     @Test
-    public void localisationTestForInvalidStrings() throws IOException {
+    public void localisationTestForInvalidStrings() {
         for (String bundle : Arrays.asList("JabRef", "Menu")) {
             for (String lang : Languages.LANGUAGES.values()) {
                 String propertyFilePath = String.format("/l10n/%s_%s.properties", bundle, lang);
@@ -194,12 +194,7 @@ public class LocalizationConsistencyTest {
                         .getProperties(propertyFilePath);
                 //parse object "textKeys" to find any spaces
                 for (Map.Entry<Object, Object> entry : textKeys.entrySet()) {
-                    String expectedKeyEqualsKey = String.format("%s=%s", entry.getKey(), entry.getKey());
-                    String actualKeyEqualsValue = String.format("%s=%s", entry.getKey(),
-                            entry.getValue().toString().replace(" ", ""));
-
-
-                    assertEquals("Found an invalid character in the "+ lang + "localization of" + bundle + ": The " + entry.getKey().toString() + ":" + expectedKeyEqualsKey + " contains a space!",expectedKeyEqualsKey, actualKeyEqualsValue);
+                    assertTrue("Found an invalid character in the " + lang + " localization of " + bundle + " : The " + entry.getKey().toString() + " : " + entry.getValue().toString() + " contains a space!", entry.getValue().toString().contains(" "));
                 }
             }
         }
