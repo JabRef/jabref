00b8069fbfcaddb07f937cfd5dbde8df016ae410
Linus Dietz <linus.dietz@tum.de>
2017-09-07 10:26:39 +0200

    Finalize the localizationTestForInvalidStrings, improve messages

--- src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
+++ src/test/java/org/jabref/logic/l10n/LocalizationConsistencyTest.java
@@ -23,7 +23,9 @@ import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LocalizationConsistencyTest {
 
@@ -188,14 +190,12 @@ public class LocalizationConsistencyTest {
             for (String lang : Languages.LANGUAGES.values()) {
                 String propertyFilePath = String.format("/l10n/%s_%s.properties", bundle, lang);
 
-                // read in
-                Properties textKeys = LocalizationParser
-                        .getProperties(propertyFilePath);
-
-                //parse object "textKeys" to find any spaces
-                for (Map.Entry<Object, Object> entry : textKeys.entrySet()) {
-                    assertFalse("Found an invalid character in the " + lang + " localization of " + bundle + " : " + entry.getValue().toString() + " At key : " + entry.getKey().toString() + " contains a space!", entry.getValue().toString().contains(" "));
-                    assertFalse("Found an invalid character in the " + lang + " localization of " + bundle + " : The key : " + entry.getKey().toString() + " contains a space!", entry.getKey().toString().contains(" "));
+                // find any spaces
+                for (Map.Entry<Object, Object> entry : LocalizationParser.getProperties(propertyFilePath).entrySet()) {
+                    assertFalse("Found an invalid character in the '" + lang + "' localization of '" + bundle +
+                            "': The key of " + entry.getKey().toString() + "=" + entry.getValue() + " contains a space!", entry.getKey().toString().contains(" "));
+                    assertFalse("Found an invalid character in the '" + lang + "' localization of '" + bundle +
+                            "': The value of " + entry.getKey().toString() + "=" + entry.getValue() + " contains a space!", entry.getValue().toString().contains(" "));
                 }
             }
         }
