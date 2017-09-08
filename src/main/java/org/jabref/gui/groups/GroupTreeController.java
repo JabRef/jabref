4a6bf01d66ca4fe703e964204b19adeb90cab102
Joerg Lenhard <joerg.lenhard@kau.se>
2017-09-08 10:57:42 +0200

    Remove unused import

--- src/main/java/org/jabref/gui/groups/GroupTreeController.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeController.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
