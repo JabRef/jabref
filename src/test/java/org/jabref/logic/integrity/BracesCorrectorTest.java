a562e1ccd670ebbdbc41fcf42ecae304ecee1390
armbruan <armbruan@gmail.com>
2017-09-12 20:10:49 +0200

    Fix formatting

--- src/test/java/org/jabref/logic/integrity/BracesCorrectorTest.java
+++ src/test/java/org/jabref/logic/integrity/BracesCorrectorTest.java
@@ -7,37 +7,37 @@ import static org.junit.Assert.*;
 public class BracesCorrectorTest {
 
     @Test
-    public void inputIsNull (){
+    public void inputIsNull() {
         assertNull(BracesCorrector.apply(null));
     }
 
     @Test
-    public void inputIsEmpty (){
-    assertEquals("", BracesCorrector.apply(""));
+    public void inputIsEmpty() {
+        assertEquals("", BracesCorrector.apply(""));
     }
 
     @Test
-    public void inputWithoutBraces (){
-    assertEquals("banana", BracesCorrector.apply("banana"));
+    public void inputWithoutBraces() {
+        assertEquals("banana", BracesCorrector.apply("banana"));
     }
 
     @Test
-    public void inputMissingClosing (){
-    assertEquals("{banana}", BracesCorrector.apply("{banana"));
+    public void inputMissingClosing() {
+        assertEquals("{banana}", BracesCorrector.apply("{banana"));
     }
 
     @Test
-    public void inputMissingOpening (){
-    assertEquals("{banana}", BracesCorrector.apply("banana}"));
+    public void inputMissingOpening() {
+        assertEquals("{banana}", BracesCorrector.apply("banana}"));
     }
 
     @Test
-    public void inputWithMaskedBraces (){
+    public void inputWithMaskedBraces() {
         assertEquals("\\\\\\{banana", BracesCorrector.apply("\\\\\\{banana"));
     }
 
     @Test
-    public void inputWithMixedBraces (){
+    public void inputWithMixedBraces() {
         assertEquals("{b{anana\\\\\\}}}", BracesCorrector.apply("{b{anana\\\\\\}"));
     }
 }
