d64439baf9187c4b8f3723c4e97ecf89b17d5272
Saulius Gražulis <grazulis@ibt.lt>
2017-09-23 14:04:21 +0300

    Adding a test for string resolution. The test still does not pass.

--- src/test/java/org/jabref/logic/util/BracketedExpressionExpanderTest.java
+++ src/test/java/org/jabref/logic/util/BracketedExpressionExpanderTest.java
@@ -1,6 +1,9 @@
 package org.jabref.logic.util;
 
+import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.BibtexString;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,5 +25,32 @@ public class BracketedExpressionExpanderTest {
     public void bibentryExpansionTest() {
         BracketedExpressionExpander bex = new BracketedExpressionExpander(bibentry);
         assertEquals(bex.expandBrackets("[year]_[auth]_[firstpage]"), "2017_Kitsune_213");
+    }
+
+    @Test
+    public void nullDatabaseExpansionTest() {
+        BibDatabase database = null;
+        BracketedExpressionExpander bex = new BracketedExpressionExpander(bibentry);
+        assertEquals(bex.expandBrackets("[year]_[auth]_[firstpage]", database), "2017_Kitsune_213");
+    }
+
+    @Test
+    public void emptyDatabaseExpansionTest() {
+        BibDatabase database = new BibDatabase();
+        BracketedExpressionExpander bex = new BracketedExpressionExpander(bibentry);
+        assertEquals(bex.expandBrackets("[year]_[auth]_[firstpage]", database), "2017_Kitsune_213");
+    }
+
+    @Test
+    public void databaseWithStringsExpansionTest() {
+        BibDatabase database = new BibDatabase();
+        BibtexString string = new BibtexString("#sgr", "Saulius Gražulis");
+        database.addString(string);
+        bibentry = new BibEntry();
+        bibentry.setField("author", "#sgr");
+        bibentry.setField("year", "2017");
+        bibentry.setField("pages", "213--216");
+        BracketedExpressionExpander bex = new BracketedExpressionExpander(bibentry);
+        assertEquals(bex.expandBrackets("[year]_[auth]_[firstpage]", database), "2017_Gražulis_213");
     }
 }
