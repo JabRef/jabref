09923c43d906046a388f7a8c642cd9342a679433
Saulius Gražulis <grazulis@ibt.lt>
2017-09-23 11:12:37 +0300

    Fixing comments.

--- src/main/java/org/jabref/logic/util/BracketedExpressionExpander.java
+++ src/main/java/org/jabref/logic/util/BracketedExpressionExpander.java
@@ -24,8 +24,8 @@ import org.jabref.model.strings.StringUtil;
  * @author saulius
  * The BracketedExpressionExpander provides methods to expand bracketed expressions,
  * such as [year]_[author]_[firstpage], using information from a provided BibEntry.
- * The above-mentioned expression would yield 2017_Kizune_123 when expanded using the
- * BibTeX entry "@Article{ authors = {A. Kizune}, year = {2017}, pages={123-6}}".
+ * The above-mentioned expression would yield 2017_Kitsune_123 when expanded using the
+ * BibTeX entry "@Article{ authors = {O. Kitsune}, year = {2017}, pages={123-6}}".
  */
 public class BracketedExpressionExpander {
 
