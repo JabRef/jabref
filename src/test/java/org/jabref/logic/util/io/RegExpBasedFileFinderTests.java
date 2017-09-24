7d44ca2808d75fc1040e7491eee8ab58c8d4727b
Saulius Gražulis <grazulis@ibt.lt>
2017-09-24 16:58:09 +0300

    Adding one more test for RegExpBasedFileFinder, checking file names with diacritics.

--- src/test/java/org/jabref/logic/util/io/RegExpBasedFileFinderTests.java
+++ src/test/java/org/jabref/logic/util/io/RegExpBasedFileFinderTests.java
@@ -80,6 +80,28 @@ public class RegExpBasedFileFinderTests {
     }
 
     @Test
+    public void testAuthorWithDiacritics() {
+        //given
+        BibEntry localEntry = new BibEntry(BibtexEntryTypes.ARTICLE.getName());
+        localEntry.setCiteKey("Grazulis2017");
+        localEntry.setField("year", "2017");
+        localEntry.setField("author", "Gražulis, Saulius and O. Kitsune");
+        localEntry.setField("pages", "726--729");
+
+        List<String> extensions = Collections.singletonList("pdf");
+
+        List<Path> dirs = Collections.singletonList(Paths.get(filesDirectory));
+        RegExpBasedFileFinder fileFinder = new RegExpBasedFileFinder("**/[year]_[auth]_[firstpage]\\\\.[extension]", ',');
+
+        //when
+        List<Path> result = fileFinder.findAssociatedFiles(localEntry, dirs, extensions);
+
+        //then
+        assertEquals(Collections.singletonList(Paths.get("src/test/resources/org/jabref/logic/importer/unlinkedFilesTestFolder/directory/subdirectory/2017_Gražulis_.pdf")),
+                result);
+    }
+
+    @Test
     public void testFindFileInSubdirectory() {
         //given
         BibEntry localEntry = new BibEntry(BibtexEntryTypes.ARTICLE.getName());
