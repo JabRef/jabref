11f7f50c044ef58d38273680614eb03e6cd95c0c
Saulius Gražulis <grazulis@ibt.lt>
2017-09-30 12:42:00 +0300

    Adding the first fileNamePattern migration subroutine.

--- src/main/java/org/jabref/migrations/PreferencesMigrations.java
+++ src/main/java/org/jabref/migrations/PreferencesMigrations.java
@@ -196,31 +196,51 @@ public class PreferencesMigrations {
     /**
      * Migrate Import File Name and Directory name Patterns from versions <=4.0 to new BracketedPatterns
      */
+    private static void migrateFileImportPattern(String oldStylePattern, String newStylePattern,
+            JabRefPreferences prefs, Preferences mainPrefsNode) {
+        String preferenceFileNamePattern = mainPrefsNode.get(JabRefPreferences.IMPORT_FILENAMEPATTERN, null);
+
+        if (preferenceFileNamePattern != null) {
+            if (oldStylePattern.equals(preferenceFileNamePattern)) {
+                // Upgrade the old-style File Name pattern to new one:
+                mainPrefsNode.put(JabRefPreferences.IMPORT_FILENAMEPATTERN, newStylePattern);
+                LOGGER.info("migrated old style " + JabRefPreferences.IMPORT_FILENAMEPATTERN +
+                        " value \"" + oldStylePattern + "\" to new value \"" +
+                        newStylePattern + "\" in the preference file");
+
+                if (prefs.hasKey(JabRefPreferences.IMPORT_FILENAMEPATTERN)) {
+                    // Update also the key in the current application settings, if necessary:
+                    String fileNamePattern = prefs.get(JabRefPreferences.IMPORT_FILENAMEPATTERN);
+                    if (oldStylePattern.equals(fileNamePattern)) {
+                        prefs.put(JabRefPreferences.IMPORT_FILENAMEPATTERN, newStylePattern);
+                        LOGGER.info("migrated old style " + JabRefPreferences.IMPORT_FILENAMEPATTERN +
+                                " value \"" + oldStylePattern + "\" to new value \"" +
+                                newStylePattern + "\" in the running application");
+                    }
+                }
+            }
+        }
+    }
+
     public static void upgradeImportFileAndDirePatterns() {
         JabRefPreferences prefs = Globals.prefs;
-        LOGGER.info(">>>" + prefs.get(JabRefPreferences.IMPORT_FILENAMEPATTERN));
-        try {
-            Preferences mainPrefsNode = Preferences.userNodeForPackage(JabRefMain.class);
 
-            LOGGER.info(">>>" + mainPrefsNode.toString());
+        Preferences mainPrefsNode = Preferences.userNodeForPackage(JabRefMain.class);
 
-            for (String key : mainPrefsNode.keys()) {
-                LOGGER.info(">>>>> Key: " + key);
+        // Migrate Import patterns
+        // Check for prefs node for Version <= 4.0
+        if (mainPrefsNode.get(JabRefPreferences.IMPORT_FILENAMEPATTERN, null) != null) {
+
+            String[] oldStylePatterns = new String[] {"\\bibtexkey",
+                    "\\bibtexkey\\begin{title} - \\format[RemoveBrackets]{\\title}\\end{title}"};
+            String[] newStylePatterns = new String[] {"[bibtexkey]",
+                    "[bibtexkey] - [fulltitle]"};
+            for (int i = 0; i < oldStylePatterns.length; i++) {
+                migrateFileImportPattern(oldStylePatterns[i], newStylePatterns[i], prefs, mainPrefsNode);
             }
-            for( String childName : mainPrefsNode.childrenNames()) {
-                LOGGER.info(">>> Child: " + childName);
-            }
-            // Migrate Import patterns
-            // Check for prefs node for Version <= 4.0
-            if (mainPrefsNode.nodeExists(JabRefPreferences.IMPORT_FILENAMEPATTERN)) {
-                LOGGER.info("Found 'importFileNamePattern' preference setting.");
-            }
-            if (mainPrefsNode.nodeExists("jabref/" + JabRefPreferences.IMPORT_FILENAMEPATTERN)) {
-                LOGGER.info("Found 'jabref/importFileNamePattern' preference setting.");
-            }
-        } catch (BackingStoreException e) {
-            LOGGER.error("Migrating old Import file name and directory name patterns failed.", e);
         }
+        // Directory preferences are not yet migrated, since it is not quote clear how to parse and reinterpret
+        // the user defined old-style patterns, and the default pattern is "".
     }
 
     public static void upgradeKeyBindingsToJavaFX() {
