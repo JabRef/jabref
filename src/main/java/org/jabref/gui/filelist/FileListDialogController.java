982b47574ebdff862984d0911818aef0b0efe517
Siedlerchr <siedlerkiller@gmail.com>
2017-11-01 17:09:20 +0100

    Asssign combobox selected value property

--- src/main/java/org/jabref/gui/filelist/FileListDialogController.java
+++ src/main/java/org/jabref/gui/filelist/FileListDialogController.java
@@ -1,32 +1,19 @@
 package org.jabref.gui.filelist;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Optional;
-import java.util.regex.Pattern;
-
 import javax.inject.Inject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-
-import org.jabref.Globals;
 import org.jabref.gui.AbstractController;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.desktop.JabRefDesktop;
 import org.jabref.gui.externalfiletype.ExternalFileType;
-import org.jabref.gui.externalfiletype.ExternalFileTypes;
-import org.jabref.gui.externalfiletype.UnknownExternalFileType;
-import org.jabref.gui.util.FileDialogConfiguration;
-import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.entry.LinkedFile;
-import org.jabref.model.util.FileHelper;
-import org.jabref.preferences.JabRefPreferences;
 import org.jabref.preferences.PreferencesService;
 
 import org.apache.commons.logging.Log;
@@ -35,7 +22,6 @@ import org.apache.commons.logging.LogFactory;
 public class FileListDialogController extends AbstractController<FileListDialogViewModel> {
 
     private static final Log LOGGER = LogFactory.getLog(FileListDialogController.class);
-    private static final Pattern REMOTE_LINK_PATTERN = Pattern.compile("[a-z]+://.*");
 
     @FXML private TextField tfLink;
     @FXML private Button btnBrowse;
@@ -49,11 +35,9 @@ public class FileListDialogController extends AbstractController<FileListDialogV
     @Inject private DialogService dialogService;
     @Inject private StateManager stateManager;
 
-    private boolean showSaveDialog;
-
     @FXML
     private void initialize() {
-        viewModel = new FileListDialogViewModel(stateManager.getActiveDatabase().get());
+        viewModel = new FileListDialogViewModel(stateManager.getActiveDatabase().get(), dialogService);
         setBindings();
 
     }
@@ -63,40 +47,16 @@ public class FileListDialogController extends AbstractController<FileListDialogV
         cmbFileType.itemsProperty().bindBidirectional(viewModel.externalFileTypeProperty());
         tfDescription.textProperty().bindBidirectional(viewModel.descriptionProperty());
         tfLink.textProperty().bindBidirectional(viewModel.linkProperty());
+
+        cmbFileType.valueProperty().bindBidirectional(viewModel.getSelectedExternalFileType());
     }
 
     @FXML
     void browseFileDialog(ActionEvent event) {
 
-        String fileText = viewModel.linkProperty().get();
+        viewModel.browseFileDialog();
+        tfLink.requestFocus();
 
-        Optional<Path> file = FileHelper.expandFilename(stateManager.getActiveDatabase().get(), fileText,
-                Globals.prefs.getFileDirectoryPreferences());
-
-        Path workingDir = file.orElse(Paths.get(Globals.prefs.get(JabRefPreferences.WORKING_DIRECTORY)));
-        String fileName = Paths.get(fileText).getFileName().toString();
-
-        FileDialogConfiguration fileDialogConfiguration = new FileDialogConfiguration.Builder()
-                .withInitialDirectory(workingDir)
-                .withInitialFileName(fileName).build();
-        Optional<Path> path;
-        if (showSaveDialog) {
-            path = dialogService.showFileSaveDialog(fileDialogConfiguration);
-        } else {
-            path = dialogService.showFileOpenDialog(fileDialogConfiguration);
-        }
-        path.ifPresent(newFile -> {
-            // Store the directory for next time:
-            Globals.prefs.put(JabRefPreferences.WORKING_DIRECTORY, newFile.toString());
-
-            // If the file is below the file directory, make the path relative:
-            List<Path> fileDirectories = this.stateManager.getActiveDatabase().get()
-                    .getFileDirectoriesAsPaths(Globals.prefs.getFileDirectoryPreferences());
-            newFile = FileUtil.shortenFileName(newFile, fileDirectories);
-
-            viewModel.linkProperty().set(newFile.toString());
-            tfLink.requestFocus();
-        });
     }
 
     @FXML
@@ -110,36 +70,7 @@ public class FileListDialogController extends AbstractController<FileListDialogV
     }
 
     private void setValues(LinkedFile entry) {
-        tfDescription.setText(entry.getDescription());
-        tfLink.setText(entry.getLink());
-
-        cmbFileType.getSelectionModel().clearSelection();
-        // See what is a reasonable selection for the type combobox:
-        Optional<ExternalFileType> fileType = ExternalFileTypes.getInstance().fromLinkedFile(entry, false);
-        if (fileType.isPresent() && !(fileType.get() instanceof UnknownExternalFileType)) {
-            cmbFileType.getSelectionModel().select(fileType.get());
-        } else if ((entry.getLink() != null) && (!entry.getLink().isEmpty())) {
-            checkExtension();
-        }
-    }
-
-    private void checkExtension() {
-        if (cmbFileType.getSelectionModel().isEmpty() && (!tfLink.getText().trim().isEmpty())) {
-
-            // Check if this looks like a remote link:
-            if (REMOTE_LINK_PATTERN.matcher(tfLink.getText()).matches()) {
-                Optional<ExternalFileType> type = ExternalFileTypes.getInstance().getExternalFileTypeByExt("html");
-                if (type.isPresent()) {
-                    cmbFileType.getSelectionModel().select(type.get());
-                    return;
-                }
-            }
-
-            // Try to guess the file type:
-            String theLink = tfLink.getText().trim();
-            ExternalFileTypes.getInstance().getExternalFileTypeForName(theLink).ifPresent(type -> cmbFileType.getSelectionModel().select(type));
-        }
-
+        viewModel.setValues(entry);
     }
 
     @FXML
--- src/main/java/org/jabref/gui/filelist/FileListDialogViewModel.java
+++ src/main/java/org/jabref/gui/filelist/FileListDialogViewModel.java
@@ -1,32 +1,114 @@
 package org.jabref.gui.filelist;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 
+import org.jabref.Globals;
 import org.jabref.gui.AbstractViewModel;
+import org.jabref.gui.DialogService;
 import org.jabref.gui.externalfiletype.ExternalFileType;
 import org.jabref.gui.externalfiletype.ExternalFileTypes;
+import org.jabref.gui.externalfiletype.UnknownExternalFileType;
+import org.jabref.gui.util.FileDialogConfiguration;
+import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.LinkedFile;
+import org.jabref.model.util.FileHelper;
+import org.jabref.preferences.JabRefPreferences;
 
 public class FileListDialogViewModel extends AbstractViewModel {
 
+    private static final Pattern REMOTE_LINK_PATTERN = Pattern.compile("[a-z]+://.*");
     private final StringProperty linkProperty = new SimpleStringProperty("");
     private final StringProperty descriptionProperty = new SimpleStringProperty("");
     private final ListProperty<ExternalFileType> externalfilesTypes = new SimpleListProperty<>(FXCollections.emptyObservableList());
+    private final ObjectProperty<ExternalFileType> selectedExternalFileType = new SimpleObjectProperty<>();
     private final BibDatabaseContext bibDatabaseContext;
+    private final DialogService dialogService;
 
-    public FileListDialogViewModel(BibDatabaseContext bibDatabaseContext) {
+    private boolean showSaveDialog;
+
+    public FileListDialogViewModel(BibDatabaseContext bibDatabaseContext, DialogService dialogService) {
 
         this.bibDatabaseContext = bibDatabaseContext;
+        this.dialogService = dialogService;
         externalfilesTypes.set(FXCollections.observableArrayList(ExternalFileTypes.getInstance().getExternalFileTypeSelection()));
-
-
     }
-    //
 
+    private void checkExtension() {
+
+        if (!linkProperty.getValueSafe().isEmpty()) {
+
+            // Check if this looks like a remote link:
+            if (REMOTE_LINK_PATTERN.matcher(linkProperty.get()).matches()) {
+                ExternalFileTypes.getInstance().getExternalFileTypeByExt("html").ifPresent(selectedExternalFileType::setValue);
+            }
+
+            // Try to guess the file type:
+            String theLink = linkProperty.get().trim();
+            ExternalFileTypes.getInstance().getExternalFileTypeForName(theLink).ifPresent(selectedExternalFileType::setValue);
+        }
+    }
+
+    public void browseFileDialog() {
+        String fileText = linkProperty().get();
+
+        Optional<Path> file = FileHelper.expandFilename(bibDatabaseContext, fileText,
+                Globals.prefs.getFileDirectoryPreferences());
+
+        Path workingDir = file.orElse(Paths.get(Globals.prefs.get(JabRefPreferences.WORKING_DIRECTORY)));
+        String fileName = Paths.get(fileText).getFileName().toString();
+
+        FileDialogConfiguration fileDialogConfiguration = new FileDialogConfiguration.Builder()
+                .withInitialDirectory(workingDir)
+                .withInitialFileName(fileName).build();
+        Optional<Path> path;
+
+        if (showSaveDialog) {
+            path = dialogService.showFileSaveDialog(fileDialogConfiguration);
+        } else {
+            path = dialogService.showFileOpenDialog(fileDialogConfiguration);
+        }
+        path.ifPresent(newFile -> {
+            // Store the directory for next time:
+            Globals.prefs.put(JabRefPreferences.WORKING_DIRECTORY, newFile.toString());
+
+            // If the file is below the file directory, make the path relative:
+            List<Path> fileDirectories = bibDatabaseContext
+                    .getFileDirectoriesAsPaths(Globals.prefs.getFileDirectoryPreferences());
+            newFile = FileUtil.shortenFileName(newFile, fileDirectories);
+
+            linkProperty().set(newFile.toString());
+            checkExtension();
+        });
+    }
+
+    //
+    public void setValues(LinkedFile entry) {
+        descriptionProperty.set(entry.getDescription());
+        linkProperty.set(entry.getLink());
+
+        selectedExternalFileType.setValue(null);
+
+        // See what is a reasonable selection for the type combobox:
+        Optional<ExternalFileType> fileType = ExternalFileTypes.getInstance().fromLinkedFile(entry, false);
+        if (fileType.isPresent() && !(fileType.get() instanceof UnknownExternalFileType)) {
+            selectedExternalFileType.setValue(fileType.get());
+        } else if ((entry.getLink() != null) && (!entry.getLink().isEmpty())) {
+            checkExtension();
+        }
+    }
 
     public StringProperty linkProperty() {
         return linkProperty;
@@ -40,4 +122,7 @@ public class FileListDialogViewModel extends AbstractViewModel {
         return externalfilesTypes;
     }
 
+    public ObjectProperty<ExternalFileType> getSelectedExternalFileType() {
+        return selectedExternalFileType;
+    }
 }
