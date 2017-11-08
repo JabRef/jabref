63dd033cda283e12b1aac15e1eaca4534447828d
Stefan Kolb <stefan-kolb@web.de>
2017-11-08 10:43:40 +0100

    Formatter docu

--- src/main/java/org/jabref/logic/formatter/bibtexfields/RemoveHyphenatedNewlinesFormatter.java
+++ src/main/java/org/jabref/logic/formatter/bibtexfields/RemoveHyphenatedNewlinesFormatter.java
@@ -11,16 +11,15 @@ import org.jabref.model.cleanup.Formatter;
  */
 public class RemoveHyphenatedNewlinesFormatter implements Formatter {
     private static final Pattern HYPHENATED_WORDS = Pattern.compile("(-\r\n|-\n|-\r)");
-    private static final Pattern LINEBREAKS = Pattern.compile("(\r?\n|\r)");
 
     @Override
     public String getName() {
-        return Localization.lang("Remove line breaks");
+        return Localization.lang("Remove hyphenated line breaks");
     }
 
     @Override
     public String getKey() {
-        return "remove_newlines";
+        return "remove_hyphenated_newlines";
     }
 
     @Override
@@ -28,17 +27,16 @@ public class RemoveHyphenatedNewlinesFormatter implements Formatter {
         Objects.requireNonNull(value);
 
         value = HYPHENATED_WORDS.matcher(value).replaceAll("");
-        value = LINEBREAKS.matcher(value).replaceAll(" ");
         return value.trim();
     }
 
     @Override
     public String getDescription() {
-        return Localization.lang("Removes all line breaks in the field content.");
+        return Localization.lang("Removes all hyphenated line breaks in the field content.");
     }
 
     @Override
     public String getExampleInput() {
-        return "In \n CDMA";
+        return "Gimme shel-\nter";
     }
 }
