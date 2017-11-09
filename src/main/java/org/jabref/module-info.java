f63439414c4970ce730ae0027f16b26614704fc0
siedlerchr <siedlerkiller@gmail.com>
2017-11-09 14:57:11 +0100

    First attempt at jdk9 compilation

---
+++ src/main/java/org/jabref/module-info.java
@@ -0,0 +1,13 @@
+module org.jabref{
+	requires java.desktop;
+	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.swing;	
+	requires javafx.controls;	
+	requires java.prefs;
+	requires jgoodies.looks;
+	requires jgoodies.forms;
+	requires commons.logging;
+	requires com.google.common;
+
+}
