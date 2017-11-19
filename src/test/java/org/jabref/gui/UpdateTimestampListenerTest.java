aced8a08cc32392f2ef0e25851c4880df5fef0c0
Tobias Bouschen <tobias.bouschen@fu-berlin.de>
2017-11-19 03:21:16 +0100

    Fix assertions for Optional values in UpdateTimestampListenerTest

--- src/test/java/org/jabref/gui/UpdateTimestampListenerTest.java
+++ src/test/java/org/jabref/gui/UpdateTimestampListenerTest.java
@@ -1,5 +1,7 @@
 package org.jabref.gui;
 
+import java.util.Optional;
+
 import org.jabref.logic.preferences.TimestampPreferences;
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibEntry;
@@ -11,7 +13,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -48,17 +49,15 @@ public class UpdateTimestampListenerTest {
 
         database.insertEntries(bibEntry);
 
-        assertTrue("Initial timestamp not present", bibEntry.getField(TIMESTAMP_FIELD).isPresent());
         assertEquals("Initial timestamp not set correctly",
-                BASE_DATE, bibEntry.getField(TIMESTAMP_FIELD).get());
+                Optional.of(BASE_DATE), bibEntry.getField(TIMESTAMP_FIELD));
 
         database.registerListener(new UpdateTimestampListener(preferencesMock));
 
         bibEntry.setField("test", "some value");
 
-        assertTrue("Timestamp not present after entry changed", bibEntry.getField(TIMESTAMP_FIELD).isPresent());
         assertEquals("Timestamp not set correctly after entry changed",
-                NEW_DATE, bibEntry.getField(TIMESTAMP_FIELD).get());
+                Optional.of(NEW_DATE), bibEntry.getField(TIMESTAMP_FIELD));
     }
 
     @Test
@@ -82,17 +81,14 @@ public class UpdateTimestampListenerTest {
         bibEntry.setField(TIMESTAMP_FIELD, BASE_DATE);
 
         database.insertEntries(bibEntry);
-
-        assertTrue("Initial timestamp not present", bibEntry.getField(TIMESTAMP_FIELD).isPresent());
         assertEquals("Initial timestamp not set correctly",
-                BASE_DATE, bibEntry.getField(TIMESTAMP_FIELD).get());
+                Optional.of(BASE_DATE), bibEntry.getField(TIMESTAMP_FIELD));
 
         database.registerListener(new UpdateTimestampListener(preferencesMock));
 
         bibEntry.setField("test", "some value");
 
-        assertTrue("Timestamp not present after entry changed", bibEntry.getField(TIMESTAMP_FIELD).isPresent());
         assertEquals("New timestamp set after entry changed even though updates were disabled",
-                BASE_DATE, bibEntry.getField(TIMESTAMP_FIELD).get());
+                Optional.of(BASE_DATE), bibEntry.getField(TIMESTAMP_FIELD));
     }
 }
