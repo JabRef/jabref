6f8a66d7eca480d5c8cdab4ad57f2f89e557d7c7
J.Pfeifer <julian.pfeifer.88@gmail.com>
2017-11-21 14:20:16 +0100

    modified the SearchDescriber Implementations tests
    now they work properly again and check the created Texts inside
    the description TextFlow

--- src/test/java/org/jabref/gui/search/ContainsAndRegexBasedSearchRuleDescriberTest.java
+++ src/test/java/org/jabref/gui/search/ContainsAndRegexBasedSearchRuleDescriberTest.java
@@ -1,7 +1,12 @@
 package org.jabref.gui.search;
 
-import org.jabref.gui.search.rules.describer.ContainsAndRegexBasedSearchRuleDescriber;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
+import org.jabref.gui.search.rules.describer.ContainsAndRegexBasedSearchRuleDescriber;
+import org.jabref.gui.util.TextUtil;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,27 +15,30 @@ public class ContainsAndRegexBasedSearchRuleDescriberTest {
 
     @Test
     public void testNoAst() {
+        double textSize = 13;
         String query = "a b";
-        evaluateNoAst(query, true, true, "This search contains entries in which any field contains the regular expression " +
-                "<b>&#97;</b><b>&#98;</b> (case sensitive). " +
-                "<p><br>Hint: To search specific fields only, " +
-                "enter for example:<p><tt>author=smith and title=electrical</tt>");
-        evaluateNoAst(query, true, false, "This search contains entries in which any field contains the term " +
-                "<b>&#97;</b><b>&#98;</b> (case sensitive). " +
-                "<p><br>Hint: To search specific fields only, enter for " +
-                "example:<p><tt>author=smith and title=electrical</tt>");
-        evaluateNoAst(query, false, false, "This search contains entries in which any field contains the term " +
-                "<b>&#97;</b><b>&#98;</b> (case insensitive). " +
-                "<p><br>Hint: To search specific fields only, enter for " +
-                "example:<p><tt>author=smith and title=electrical</tt>");
-        evaluateNoAst(query, false, true, "This search contains entries in which any field contains the regular " +
-                "expression <b>&#97;</b><b>&#98;</b> (case insensitive). " +
-                "<p><br>Hint: To search specific fields only, enter for " +
-                "example:<p><tt>author=smith and title=electrical</tt>");
+        evaluateNoAst(query, true, true, TextUtil.createText("This search contains entries in which any field contains the regular expression ", textSize),
+                TextUtil.createTextBold("a", textSize), TextUtil.createText(" and ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" (case sensitive). ", textSize),
+                TextUtil.createText("\n\nHint: To search specific fields only, enter for example:\n", textSize), TextUtil.createTextMonospaced("author=smith and title=electrical", textSize));
+        evaluateNoAst(query, true, false, TextUtil.createText("This search contains entries in which any field contains the term ", textSize),
+                TextUtil.createTextBold("a", textSize), TextUtil.createText(" and ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" (case sensitive). ", textSize),
+                TextUtil.createText("\n\nHint: To search specific fields only, enter for example:\n", textSize), TextUtil.createTextMonospaced("author=smith and title=electrical", textSize));
+        evaluateNoAst(query, false, false, TextUtil.createText("This search contains entries in which any field contains the term ", textSize),
+                TextUtil.createTextBold("a", textSize), TextUtil.createText(" and ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" (case insensitive). ", textSize),
+                TextUtil.createText("\n\nHint: To search specific fields only, enter for example:\n", textSize), TextUtil.createTextMonospaced("author=smith and title=electrical", textSize));
+        evaluateNoAst(query, false, true, TextUtil.createText("This search contains entries in which any field contains the regular expression ", textSize),
+                TextUtil.createTextBold("a", textSize), TextUtil.createText(" and ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" (case insensitive). ", textSize),
+                TextUtil.createText("\n\nHint: To search specific fields only, enter for example:\n", textSize), TextUtil.createTextMonospaced("author=smith and title=electrical", textSize));
     }
 
-    private void evaluateNoAst(String query, boolean caseSensitive, boolean regex, String expected) {
-        assertEquals(expected, new ContainsAndRegexBasedSearchRuleDescriber(caseSensitive, regex, query).getDescription());
+    private void evaluateNoAst(String query, boolean caseSensitive, boolean regex, Text... expected) {
+        TextFlow description = new ContainsAndRegexBasedSearchRuleDescriber(caseSensitive, regex, query).getDescription();
+        assertEquals("Wrong number of Texts inside the description TextFlow", expected.length, description.getChildren().size());
+        Text expectedText;
+        for (int i = 0; i < expected.length; i++) {
+            expectedText = expected[i];
+            Assert.assertEquals(expectedText.toString(), description.getChildren().get(i).toString());
+        }
     }
 
 }
--- src/test/java/org/jabref/gui/search/GrammarBasedSearchRuleDescriberTest.java
+++ src/test/java/org/jabref/gui/search/GrammarBasedSearchRuleDescriberTest.java
@@ -1,8 +1,13 @@
 package org.jabref.gui.search;
 
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
 import org.jabref.gui.search.rules.describer.GrammarBasedSearchRuleDescriber;
+import org.jabref.gui.util.TextUtil;
 import org.jabref.model.search.rules.GrammarBasedSearchRule;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,46 +17,53 @@ public class GrammarBasedSearchRuleDescriberTest {
 
     @Test
     public void testSimpleQuery() {
+        double textSize = 13;
         String query = "a=b";
-        evaluate(query, true, true, "This search contains entries in which the field <b>&#97;</b> " +
-                "contains the regular expression <b>&#98;</b>. " +
-                "The search is case sensitive.");
-        evaluate(query, true, false, "This search contains entries in which the field <b>&#97;</b> " +
-                "contains the term <b>&#98;</b>. " +
-                "The search is case sensitive.");
-        evaluate(query, false, false, "This search contains entries in which the field <b>&#97;</b> " +
-                "contains the term <b>&#98;</b>. " +
-                "The search is case insensitive.");
-        evaluate(query, false, true, "This search contains entries in which the field <b>&#97;</b> " +
-                "contains the regular expression <b>&#98;</b>. " +
-                "The search is case insensitive.");
+        evaluate(query, true, true, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the regular expression ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(". ", textSize),
+                TextUtil.createText("The search is case sensitive.", textSize));
+        evaluate(query, true, false, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(". ", textSize),
+                TextUtil.createText("The search is case sensitive.", textSize));
+        evaluate(query, false, false, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(". ", textSize),
+                TextUtil.createText("The search is case insensitive.", textSize));
+        evaluate(query, false, true, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the regular expression ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(". ", textSize),
+                TextUtil.createText("The search is case insensitive.", textSize));
     }
 
     @Test
     public void testComplexQuery() {
+        double textSize = 13;
         String query = "not a=b and c=e or e=\"x\"";
-        evaluate(query, true, true, "This search contains entries in which not ((the field <b>&#97;</b> "
-                + "contains the regular expression <b>&#98;</b> and the field <b>&#99;</b> contains the "
-                + "regular expression <b>&#101;</b>) or the field <b>&#101;</b> contains the regular expression "
-                + "<b>&#120;</b>). The search is case sensitive.");
-        evaluate(query, true, false, "This search contains entries in which not ((the field <b>&#97;</b> "
-                + "contains the term <b>&#98;</b> and the field <b>&#99;</b> contains the term <b>&#101;</b>) "
-                + "or the field <b>&#101;</b> contains the term <b>&#120;</b>). The search is case sensitive.");
-        evaluate(query, false, false, "This search contains entries in which not ((the field <b>&#97;</b> "
-                + "contains the term <b>&#98;</b> and the field <b>&#99;</b> contains the term <b>&#101;</b>) "
-                + "or the field <b>&#101;</b> contains the term <b>&#120;</b>). The search is case insensitive.");
-        evaluate(query, false, true, "This search contains entries in which not ((the field <b>&#97;</b> "
-                + "contains the regular expression <b>&#98;</b> and the field <b>&#99;</b> contains "
-                + "the regular expression <b>&#101;</b>) or the field <b>&#101;</b> contains the regular "
-                + "expression <b>&#120;</b>). The search is case insensitive.");
+        evaluate(query, true, true, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("not ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the regular expression ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" and ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("c", textSize), TextUtil.createText(" contains the regular expression ", textSize),
+                TextUtil.createTextBold("e", textSize), TextUtil.createText(" or ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("e", textSize), TextUtil.createText(" contains the regular expression ", textSize),
+                TextUtil.createTextBold("x", textSize), TextUtil.createText(". ", textSize), TextUtil.createText("The search is case sensitive.", textSize));
+        evaluate(query, false, true, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("not ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the regular expression ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" and ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("c", textSize), TextUtil.createText(" contains the regular expression ", textSize),
+                TextUtil.createTextBold("e", textSize), TextUtil.createText(" or ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("e", textSize), TextUtil.createText(" contains the regular expression ", textSize),
+                TextUtil.createTextBold("x", textSize), TextUtil.createText(". ", textSize), TextUtil.createText("The search is case insensitive.", textSize));
+        evaluate(query, true, false, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("not ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" and ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("c", textSize), TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("e", textSize),
+                TextUtil.createText(" or ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("e", textSize), TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("x", textSize), TextUtil.createText(". ", textSize), TextUtil.createText("The search is case sensitive.", textSize));
+        evaluate(query, false, false, TextUtil.createText("This search contains entries in which ", textSize), TextUtil.createText("not ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("a", textSize),
+                TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("b", textSize), TextUtil.createText(" and ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("c", textSize), TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("e", textSize),
+                TextUtil.createText(" or ", textSize), TextUtil.createText("the field ", textSize), TextUtil.createTextBold("e", textSize), TextUtil.createText(" contains the term ", textSize), TextUtil.createTextBold("x", textSize), TextUtil.createText(". ", textSize), TextUtil.createText("The search is case insensitive.", textSize));
     }
 
 
-
-    private void evaluate(String query, boolean caseSensitive, boolean regex, String expected) {
+    private void evaluate(String query, boolean caseSensitive, boolean regex, Text... expected) {
         GrammarBasedSearchRule grammarBasedSearchRule = new GrammarBasedSearchRule(caseSensitive, regex);
         assertTrue(grammarBasedSearchRule.validateSearchStrings(query));
         GrammarBasedSearchRuleDescriber describer = new GrammarBasedSearchRuleDescriber(caseSensitive, regex, grammarBasedSearchRule.getTree());
-        assertEquals(expected, describer.getDescription());
+        TextFlow description = describer.getDescription();
+        assertEquals("Wrong number of Texts inside the description TextFlow", expected.length, description.getChildren().size());
+        Text expectedText;
+        for (int i = 0; i < expected.length; i++) {
+            expectedText = expected[i];
+            Assert.assertEquals(expectedText.toString(), description.getChildren().get(i).toString());
+        }
     }
 }
