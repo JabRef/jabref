72a1cbda53e3fc9fb680dbb762cf91d9b13345b0
Joerg Lenhard <joerg.lenhard@kau.se>
2017-11-27 16:58:00 +0100

    Remove unused import

--- src/main/java/org/jabref/gui/preftabs/AppearancePrefsTab.java
+++ src/main/java/org/jabref/gui/preftabs/AppearancePrefsTab.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
