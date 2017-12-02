b438c8530206ab770ff00f7cd4018f9e03988052
Tarix <tarix@fu-berlin.de>
2017-12-02 20:01:53 +0100

    addd table view and change vie for right panel

--- src/main/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationTabController.java
+++ src/main/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationTabController.java
@@ -8,17 +8,19 @@ import javafx.beans.binding.Bindings;
 import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import javafx.scene.text.TextAlignment;
 import org.jabref.gui.AbstractController;
 import org.jabref.gui.util.FileUpdateMonitor;
 import org.jabref.gui.util.ViewModelListCellFactory;
@@ -67,8 +69,61 @@ public class FileAnnotationTabController extends AbstractController<FileAnnotati
         annotationList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         annotationList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> viewModel.notifyNewSelectedAnnotation(newValue));
         ViewModelListCellFactory<FileAnnotationViewModel> cellFactory = new ViewModelListCellFactory<FileAnnotationViewModel>()
-                .withTooltip(FileAnnotationViewModel::getDescription)
+                .withTooltip(FileAnnotationViewModel::getMarking)
                 .withGraphic(annotation -> {
+
+                    GridPane node = new GridPane();
+
+                    ColumnConstraints firstColumn = new ColumnConstraints();
+                    ColumnConstraints secondColumn = new ColumnConstraints();
+                    firstColumn.setPercentWidth(70);
+                    secondColumn.setPercentWidth(30);
+                    firstColumn.setHalignment(HPos.LEFT);
+                    secondColumn.setHalignment(HPos.RIGHT);
+                    node.getColumnConstraints().addAll(firstColumn, secondColumn);
+
+                    RowConstraints firstRow = new RowConstraints();
+                    RowConstraints secondRow = new RowConstraints();
+                    firstRow.setMinHeight(10);
+                    firstRow.setPrefHeight(15);
+                    secondRow.setMinHeight(10);
+                    secondRow.setPrefHeight(35);
+                    node.getRowConstraints().addAll(firstRow, secondRow);
+
+                    Label marking = new Label(annotation.getMarking());
+                    Label author = new Label(annotation.getAuthor());
+                    Label date = new Label(annotation.getDate());
+                    Label page = new Label(Localization.lang("Page") + ": " + annotation.getPage());
+                    //marking.getStyleClass().setAll("marking");
+
+
+                    marking.setFont(new Font("System Bold", 14));
+                    marking.setPrefWidth(100);
+                    author.setFont(new Font("System", 14));
+
+                    marking.setPrefHeight(10);
+                    author.setPrefHeight(30);
+                    date.setPrefHeight(10);
+                    page.setPrefHeight(30);
+
+                    marking.setTextAlignment(TextAlignment.LEFT);
+                    marking.setAlignment(Pos.TOP_LEFT);
+
+                    author.setTextAlignment(TextAlignment.LEFT);
+                    author.setAlignment(Pos.TOP_LEFT);
+
+                    date.setTextAlignment(TextAlignment.RIGHT);
+                    date.setAlignment(Pos.TOP_RIGHT);
+
+                    page.setTextAlignment(TextAlignment.RIGHT);
+                    page.setAlignment(Pos.TOP_RIGHT);
+
+                    node.add(marking, 0, 0);
+                    node.add(author, 0, 1);
+                    node.add(date, 1, 0);
+                    node.add(page, 1, 1);
+
+                    /*
                     VBox node = new VBox();
 
                     Text text = new Text();
@@ -82,7 +137,7 @@ public class FileAnnotationTabController extends AbstractController<FileAnnotati
                     details.getChildren().addAll(page);
 
                     node.getChildren().addAll(text, details);
-                    node.setMaxWidth(Control.USE_PREF_SIZE);
+                    node.setMaxWidth(Control.USE_PREF_SIZE);*/
                     return node;
                 });
         annotationList.setCellFactory(cellFactory);
@@ -93,9 +148,9 @@ public class FileAnnotationTabController extends AbstractController<FileAnnotati
                 (ListChangeListener<? super FileAnnotationViewModel>) c -> annotationList.getSelectionModel().selectFirst());
 
         // Set-up details pane
-        author.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::authorProperty));
-        page.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::pageProperty));
-        date.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::dateProperty));
+        //author.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::authorProperty));
+        //page.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::pageProperty));
+        //date.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::dateProperty));
         content.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::contentProperty));
         marking.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::markingProperty));
         grid.disableProperty().bind(viewModel.isAnnotationsEmpty());
