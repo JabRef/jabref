0313bb269f8dce5bf93cf97f0575901b64918918
Jörg Lenhard <lenhard@users.noreply.github.com>
2017-12-07 17:30:48 +0100

    Fix mapping of textquotesingle (#3496)

--- src/test/java/org/jabref/logic/layout/format/HTMLCharsTest.java
+++ src/test/java/org/jabref/logic/layout/format/HTMLCharsTest.java
@@ -85,6 +85,11 @@ public class HTMLCharsTest {
      */
 
     @Test
+    public void testQuoteSingle() {
+        assertEquals("&apos;", layout.format("{\\textquotesingle}"));
+    }
+
+    @Test
     public void unknownCommandIsKept() {
         assertEquals("aaaa", layout.format("\\aaaa"));
     }
