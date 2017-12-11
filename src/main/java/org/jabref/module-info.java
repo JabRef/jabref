f7c42f945e2353e503dabcc00e6971e0279d17c2
Joerg Lenhard <joerg.lenhard@kau.se>
2017-12-11 11:58:46 +0100

    Add open office dependencies

--- src/main/java/org/jabref/module-info.java
+++ src/main/java/org/jabref/module-info.java
@@ -2,12 +2,14 @@ module org.jabref{
 	requires java.desktop;
 	requires java.sql;
 	requires javafx.graphics;
-	requires javafx.swing;	
-	requires javafx.controls;	
+	requires javafx.swing;
+	requires javafx.controls;
 	requires java.prefs;
 	requires jgoodies.looks;
 	requires jgoodies.forms;
 	requires commons.logging;
 	requires com.google.common;
-
+	requires ridl;
+	requires unoil;
+	requires juh;
 }
