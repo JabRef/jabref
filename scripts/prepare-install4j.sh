18f8bab22d8aef3edb466f50c153da002349155e
Christoph <cschwentker@gmail.com>
2017-12-19 12:49:08 +0100

    Disable codesigning until we have a new one (#3546)

--- scripts/prepare-install4j.sh
+++ scripts/prepare-install4j.sh
@@ -7,14 +7,14 @@ fi
 
 # ensure that tar archive of install4j exists
 cd ~/downloads
-wget --quiet -nc http://download-keycdn.ej-technologies.com/install4j/install4j_unix_7_0_1.tar.gz
+wget --quiet -nc http://download-keycdn.ej-technologies.com/install4j/install4j_unix_7_0_3.tar.gz
 
 # extract tar archive of install4j into the source directory of JabRef
 cd ~/jabref
 # version 7.0.1 is NOT zipped any more - old command line: "-xzf"
-tar -xf ~/downloads/install4j_unix_7_0_1.tar.gz
+tar -xf ~/downloads/install4j_unix_7_0_3.tar.gz
 # fix directory name (until install4j 6.1.5 it was install4j6
-mv install4j7.0.1 install4j7
+mv install4j7.0.3 install4j7
 
 # fetch JREs
 if [ ! -d ~/.install4j7/jres/ ]; then
