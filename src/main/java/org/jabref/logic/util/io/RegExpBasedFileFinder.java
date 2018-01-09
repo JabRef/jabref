00691b60b2e4fb1572758ca328833485c70a7d71
Linus Dietz <linus.dietz@tum.de>
2018-01-09 18:11:51 +0100

    Revert "Addition to Merge"
    This reverts commit e12311f0635690f5d558a3a437cf708d970ee95a.

--- src/main/java/org/jabref/logic/util/io/RegExpBasedFileFinder.java
+++ src/main/java/org/jabref/logic/util/io/RegExpBasedFileFinder.java
@@ -6,11 +6,9 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiPredicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -191,20 +189,15 @@ class RegExpBasedFileFinder implements FileFinder {
         try {
             final Pattern toMatch = Pattern.compile('^' + filenameToLookFor.replaceAll("\\\\\\\\", "\\\\") + '$',
                     Pattern.CASE_INSENSITIVE);
-            BiPredicate<Path, BasicFileAttributes> matcher = (path, attributes) -> toMatch.matcher(path.getFileName().toString()).matches();
-            res.addAll(colelct(actualDirectory, matcher));
+
+            List<Path> matches = Files.find(actualDirectory, 1,
+                    (path, attributes) -> toMatch.matcher(path.getFileName().toString()).matches())
+                    .collect(Collectors.toList());
+            res.addAll(matches);
         } catch (UncheckedIOException | PatternSyntaxException e) {
             throw new IOException("Could not look for " + filenameToLookFor, e);
         }
         return Collections.emptyList();
-    }
-
-    private List<Path> colelct(Path actualDirectory, BiPredicate<Path, BasicFileAttributes> matcher) {
-        try (Stream<Path> pathStream = Files.find(actualDirectory, 1, matcher)) {
-            return pathStream.collect(Collectors.toList());
-        } catch (IOException ioe) {
-            return Collections.emptyList();
-        }
     }
 
     private boolean isSubDirectory(Path rootDirectory, Path path) {
