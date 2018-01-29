59a03277bb3e142601b0d1ad16a115d2cb3196fa
Joerg Lenhard <joerg.lenhard@kau.se>
2018-01-29 16:27:27 +0100

    Fix imports in SilverPlatterImporterTest

--- src/test/java/org/jabref/logic/importer/fileformat/SilverPlatterImporterTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/SilverPlatterImporterTest.java
@@ -1,31 +1,16 @@
 package org.jabref.logic.importer.fileformat;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import org.jabref.logic.bibtex.BibEntryAssert;
 import org.jabref.logic.importer.Importer;
 import org.jabref.logic.util.FileType;
-import org.jabref.model.entry.BibEntry;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
 import static org.junit.Assert.assertEquals;
 
@@ -67,5 +52,5 @@ public class SilverPlatterImporterTest {
     public void testGetDescription() {
         assertEquals("Imports a SilverPlatter exported file.", testImporter.getDescription());
     }
-    
+
 }
