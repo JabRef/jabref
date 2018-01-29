2c7a86fcec5b21c4bc70c4b07c11169605cd89a9
Joerg Lenhard <joerg.lenhard@kau.se>
2018-01-29 16:03:52 +0100

    Migrate PdfContentImporter tests to JUnit5

--- src/test/java/org/jabref/logic/importer/fileformat/PdfContentImporterTestFiles.java
+++ src/test/java/org/jabref/logic/importer/fileformat/PdfContentImporterTestFiles.java
@@ -8,12 +8,16 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.jabref.logic.bibtex.BibEntryAssert;
 import org.jabref.logic.importer.ImportFormatPreferences;
 import org.jabref.model.entry.BibEntry;
 
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
@@ -21,34 +25,26 @@ import org.junit.runners.Parameterized.Parameters;
 
 import static org.mockito.Mockito.mock;
 
-@RunWith(Parameterized.class)
 public class PdfContentImporterTestFiles {
 
-    @Parameter
-    public String fileName;
+    private static final String FILE_ENDING = ".pdf";
 
-
-    @Parameters(name = "{index}: {0}")
-    public static Collection<Object[]> fileNames() {
-        // The test folder contains pairs of PDFs and BibTeX files. We check each pair.
-        // This method returns the basenames of the available pairs
-
-        Object[][] data = new Object[][] {
-                // minimal PDF, not encrypted
-                {"LNCS-minimal"},
-                // minimal PDF, write-protected, thus encrypted
-                {"LNCS-minimal-protected"}};
-        return Arrays.asList(data);
+    private static Stream<String> fileNames() throws IOException {
+        Predicate<String> fileName = name -> name.startsWith("LNCS-minimal")
+                && name.endsWith(FILE_ENDING);
+        return ImporterTestEngine.getTestFiles(fileName).stream();
     }
 
-    @Test
-    public void correctContent() throws IOException, URISyntaxException {
-        String pdfFileName = fileName + ".pdf";
-        String bibFileName = fileName + ".bib";
-        PdfContentImporter importer = new PdfContentImporter(mock(ImportFormatPreferences.class));
-        Path pdfFile = Paths.get(PdfContentImporter.class.getResource(pdfFileName).toURI());
-        List<BibEntry> result = importer.importDatabase(pdfFile, StandardCharsets.UTF_8).getDatabase().getEntries();
-        BibEntryAssert.assertEquals(PdfContentImporterTest.class, bibFileName, result);
+    @ParameterizedTest
+    @MethodSource("fileNames")
+    public void testIsRecognizedFormat(String fileName) throws IOException {
+        ImporterTestEngine.testIsRecognizedFormat(new PdfContentImporter(mock(ImportFormatPreferences.class)), fileName);
+    }
+
+    @ParameterizedTest
+    @MethodSource("fileNames")
+    public void testImportEntries(String fileName) throws IOException {
+        ImporterTestEngine.testImportEntries(new PdfContentImporter(mock(ImportFormatPreferences.class)), fileName, FILE_ENDING);
     }
 
 }
