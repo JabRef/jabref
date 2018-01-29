1ab49ecfb36865c3137b5a1a442b3e59851db078
Joerg Lenhard <joerg.lenhard@kau.se>
2018-01-29 15:27:36 +0100

    Convert BibTeXML importer tests to JUnit5

--- src/test/java/org/jabref/logic/importer/fileformat/BibTeXMLImporterTestTypes.java
+++ src/test/java/org/jabref/logic/importer/fileformat/BibTeXMLImporterTestTypes.java
@@ -10,47 +10,36 @@ import java.util.List;
 
 import org.jabref.model.entry.BibEntry;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
 public class BibTeXMLImporterTestTypes {
 
-    private BibTeXMLImporter bibteXMLImporter;
-
-    @Parameter(value = 0)
-    public String bibteXMLType;
-
-    @Parameter(value = 1)
-    public String expectedBibType;
-
-
-    @Parameters
     public static Collection<String[]> types() {
-        return Arrays.asList(new String[][] {{"journal", "article"}, {"book section", "inbook"}, {"book", "book"},
-                {"conference", "inproceedings"}, {"proceedings", "inproceedings"}, {"report", "techreport"},
-                {"master thesis", "mastersthesis"}, {"thesis", "phdthesis"}, {"master", "misc"}});
+        return Arrays.asList(new String[][] {
+                {"journal", "article"},
+                {"book section", "inbook"},
+                {"book", "book"},
+                {"conference", "inproceedings"},
+                {"proceedings", "inproceedings"},
+                {"report", "techreport"},
+                {"master thesis", "mastersthesis"},
+                {"thesis", "phdthesis"},
+                {"master", "misc"}});
     }
 
-    @Before
-    public void setUp() throws Exception {
-        bibteXMLImporter = new BibTeXMLImporter();
-    }
 
-    @Test
-    public void importConvertsToCorrectBibType() throws IOException {
+    @ParameterizedTest
+    @MethodSource("types")
+    public void importConvertsToCorrectBibType(String actualType, String expectedType) throws IOException {
         String bibteXMLInput = "<?xml version=\"1.0\" ?>\n" + "<bibtex:file xmlns:bibtex=\"http://bibtexml.sf.net/\">\n"
-                + "<bibtex:entry>\n" + "<bibtex:" + expectedBibType + ">\n"
+                + "<bibtex:entry>\n" + "<bibtex:" + actualType + ">\n"
                 + "<bibtex:author>Max Mustermann</bibtex:author>\n" + "<bibtex:keywords>java</bibtex:keywords>\n"
                 + "<bibtex:title>Java tricks</bibtex:title>\n" + "<bibtex:year>2016</bibtex:year>\n" + "</bibtex:"
-                + expectedBibType + ">\n" + "</bibtex:entry>\n" + "</bibtex:file>";
+                + actualType + ">\n" + "</bibtex:entry>\n" + "</bibtex:file>";
 
-        List<BibEntry> bibEntries = bibteXMLImporter.importDatabase(new BufferedReader(new StringReader(bibteXMLInput)))
+        List<BibEntry> bibEntries = new BibTeXMLImporter().importDatabase(new BufferedReader(new StringReader(bibteXMLInput)))
                 .getDatabase().getEntries();
 
         BibEntry entry = new BibEntry();
@@ -58,8 +47,8 @@ public class BibTeXMLImporterTestTypes {
         entry.setField("keywords", "java");
         entry.setField("title", "Java tricks");
         entry.setField("year", "2016");
-        entry.setType(expectedBibType);
+        entry.setType(expectedType);
 
-        Assert.assertEquals(Collections.singletonList(entry), bibEntries);
+        Assertions.assertEquals(Collections.singletonList(entry), bibEntries);
     }
 }
