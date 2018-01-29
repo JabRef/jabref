0c2b9b2e5a2adcfbf3f2d59d680be828651c0235
Joerg Lenhard <joerg.lenhard@kau.se>
2018-01-29 16:08:15 +0100

    Migrate RISImporter tests to JUnit5

--- src/test/java/org/jabref/logic/importer/fileformat/RISImporterTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/RISImporterTest.java
@@ -8,44 +8,46 @@ import java.nio.file.Paths;
 
 import org.jabref.logic.util.FileType;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class RISImporterTest {
 
     private RisImporter importer;
 
 
-    @Before
+    @BeforeEach
     public void setUp() {
         importer = new RisImporter();
     }
 
     @Test
     public void testGetFormatName() {
-        Assert.assertEquals("RIS", importer.getName());
+        assertEquals("RIS", importer.getName());
     }
 
     @Test
     public void testGetCLIId() {
-        Assert.assertEquals("ris", importer.getId());
+        assertEquals("ris", importer.getId());
     }
 
     @Test
     public void testsGetExtensions() {
-        Assert.assertEquals(FileType.RIS, importer.getFileType());
+        assertEquals(FileType.RIS, importer.getFileType());
     }
 
     @Test
     public void testGetDescription() {
-        Assert.assertEquals("Imports a Biblioscape Tag File.", importer.getDescription());
+        assertEquals("Imports a Biblioscape Tag File.", importer.getDescription());
     }
 
     @Test
     public void testIfNotRecognizedFormat() throws IOException, URISyntaxException {
         Path file = Paths.get(RISImporterTest.class.getResource("RisImporterCorrupted.ris").toURI());
-        Assert.assertFalse(importer.isRecognizedFormat(file, StandardCharsets.UTF_8));
+        assertFalse(importer.isRecognizedFormat(file, StandardCharsets.UTF_8));
     }
 
 }
