9090b6bce8b10eb3acec72dd7ef76a317cadf8b2
Joerg Lenhard <joerg.lenhard@kau.se>
2018-02-06 18:39:03 +0100

    Extract creation of the contents of FileAnnotationTabController

--- src/main/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationTabController.java
+++ src/main/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationTabController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -71,55 +72,7 @@ public class FileAnnotationTabController extends AbstractController<FileAnnotati
         ViewModelListCellFactory<FileAnnotationViewModel> cellFactory = new ViewModelListCellFactory<FileAnnotationViewModel>()
                 .withTooltip(FileAnnotationViewModel::getMarking)
                 .withGraphic(annotation -> {
-
-                    GridPane node = new GridPane();
-
-                    ColumnConstraints firstColumn = new ColumnConstraints();
-                    ColumnConstraints secondColumn = new ColumnConstraints();
-                    firstColumn.setPercentWidth(70);
-                    secondColumn.setPercentWidth(30);
-                    firstColumn.setHalignment(HPos.LEFT);
-                    secondColumn.setHalignment(HPos.RIGHT);
-                    node.getColumnConstraints().addAll(firstColumn, secondColumn);
-
-                    RowConstraints firstRow = new RowConstraints();
-                    RowConstraints secondRow = new RowConstraints();
-                    firstRow.setMinHeight(10);
-                    firstRow.setPrefHeight(15);
-                    secondRow.setMinHeight(10);
-                    secondRow.setPrefHeight(35);
-                    node.getRowConstraints().addAll(firstRow, secondRow);
-
-                    Label marking = new Label(annotation.getMarking());
-                    Label author = new Label(annotation.getAuthor());
-                    Label date = new Label(annotation.getDate());
-                    Label page = new Label(Localization.lang("Page") + ": " + annotation.getPage());
-
-                    marking.setFont(new Font("System Bold", 15));
-                    marking.setPrefWidth(250);
-                    author.setFont(new Font("System", 14));
-
-                    marking.setPrefHeight(10);
-                    author.setPrefHeight(30);
-                    date.setPrefHeight(10);
-                    page.setPrefHeight(30);
-
-                    // add alignment for text in the list
-                    marking.setTextAlignment(TextAlignment.LEFT);
-                    marking.setAlignment(Pos.TOP_LEFT);
-                    author.setTextAlignment(TextAlignment.LEFT);
-                    author.setAlignment(Pos.TOP_LEFT);
-                    date.setTextAlignment(TextAlignment.RIGHT);
-                    date.setAlignment(Pos.TOP_RIGHT);
-                    page.setTextAlignment(TextAlignment.RIGHT);
-                    page.setAlignment(Pos.TOP_RIGHT);
-
-                    node.add(marking, 0, 0);
-                    node.add(author, 0, 1);
-                    node.add(date, 1, 0);
-                    node.add(page, 1, 1);
-
-                    return node;
+                    return createFileAnnotationNode(annotation);
                 });
         annotationList.setCellFactory(cellFactory);
         annotationList.setPlaceholder(new Label(Localization.lang("File has no attached annotations")));
@@ -132,6 +85,57 @@ public class FileAnnotationTabController extends AbstractController<FileAnnotati
         content.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::contentProperty));
         marking.textProperty().bind(EasyBind.select(viewModel.currentAnnotationProperty()).selectObject(FileAnnotationViewModel::markingProperty));
         grid.disableProperty().bind(viewModel.isAnnotationsEmpty());
+    }
+
+    private Node createFileAnnotationNode(FileAnnotationViewModel annotation) {
+        GridPane node = new GridPane();
+
+        ColumnConstraints firstColumn = new ColumnConstraints();
+        ColumnConstraints secondColumn = new ColumnConstraints();
+        firstColumn.setPercentWidth(70);
+        secondColumn.setPercentWidth(30);
+        firstColumn.setHalignment(HPos.LEFT);
+        secondColumn.setHalignment(HPos.RIGHT);
+        node.getColumnConstraints().addAll(firstColumn, secondColumn);
+
+        RowConstraints firstRow = new RowConstraints();
+        RowConstraints secondRow = new RowConstraints();
+        firstRow.setMinHeight(10);
+        firstRow.setPrefHeight(15);
+        secondRow.setMinHeight(10);
+        secondRow.setPrefHeight(35);
+        node.getRowConstraints().addAll(firstRow, secondRow);
+
+        Label marking = new Label(annotation.getMarking());
+        Label author = new Label(annotation.getAuthor());
+        Label date = new Label(annotation.getDate());
+        Label page = new Label(Localization.lang("Page") + ": " + annotation.getPage());
+
+        marking.setFont(new Font("System Bold", 15));
+        marking.setPrefWidth(250);
+        author.setFont(new Font("System", 14));
+
+        marking.setPrefHeight(10);
+        author.setPrefHeight(30);
+        date.setPrefHeight(10);
+        page.setPrefHeight(30);
+
+        // add alignment for text in the list
+        marking.setTextAlignment(TextAlignment.LEFT);
+        marking.setAlignment(Pos.TOP_LEFT);
+        author.setTextAlignment(TextAlignment.LEFT);
+        author.setAlignment(Pos.TOP_LEFT);
+        date.setTextAlignment(TextAlignment.RIGHT);
+        date.setAlignment(Pos.TOP_RIGHT);
+        page.setTextAlignment(TextAlignment.RIGHT);
+        page.setAlignment(Pos.TOP_RIGHT);
+
+        node.add(marking, 0, 0);
+        node.add(author, 0, 1);
+        node.add(date, 1, 0);
+        node.add(page, 1, 1);
+
+        return node;
     }
 
     public void copy(ActionEvent event) {
