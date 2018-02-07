0e15a372be438f4287782bf0289a054efd7ae7ae
Stefan Kolb <stefan-kolb@web.de>
2018-02-07 16:36:46 +0100

    Fix UI test

--- src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
+++ src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
@@ -48,7 +48,7 @@ public class LinkedFileViewModelTest {
     }
 
     @Test
-    public void deleteWhenFilePathNotPresentReturnsFalse() {
+    public void deleteWhenFilePathNotPresentReturnsTrue() {
         // Making this a spy, so we can inject an empty optional without digging into the implementation
         linkedFile = spy(new LinkedFile("", "nonexistent file", ""));
         doReturn(Optional.empty()).when(linkedFile).findIn(any(BibDatabaseContext.class), any(FileDirectoryPreferences.class));
@@ -56,7 +56,7 @@ public class LinkedFileViewModelTest {
         LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService);
         boolean removed = viewModel.delete();
 
-        assertFalse(removed);
+        assertTrue(removed);
         verifyZeroInteractions(dialogService); // dialog was never shown
     }
 
