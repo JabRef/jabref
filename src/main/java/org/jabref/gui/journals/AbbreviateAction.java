a207c1d6fad19e169f64e9a11e2ac629eaebd5af
Stefan <st155160@stud.uni-stuttgart.de>
2018-02-13 19:39:00 +0100

    Removed unnecessary code. Increased performance slightly

--- src/main/java/org/jabref/gui/journals/AbbreviateAction.java
+++ src/main/java/org/jabref/gui/journals/AbbreviateAction.java
@@ -1,6 +1,5 @@
 package org.jabref.gui.journals;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -14,70 +13,65 @@ import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.InternalBibtexFields;
 
 /**
- * Converts journal full names to either iso or medline abbreviations for all selected entries.
+ * Converts journal full names to either iso or medline abbreviations for all
+ * selected entries.
  */
 public class AbbreviateAction extends AbstractWorker {
 
-    private final BasePanel panel;
-    private String message = "";
-    private final boolean iso;
+	private final BasePanel panel;
+	private String message = "";
+	private final boolean iso;
 
+	public AbbreviateAction(BasePanel panel, boolean iso) {
+		this.panel = panel;
+		this.iso = iso;
+	}
 
-    public AbbreviateAction(BasePanel panel, boolean iso) {
-        this.panel = panel;
-        this.iso = iso;
-    }
+	@Override
+	public void init() {
+		panel.output(Localization.lang("Abbreviating..."));
+	}
 
-    @Override
-    public void init() {
-        panel.output(Localization.lang("Abbreviating..."));
-    }
+	@Override
+	public void run() {
+		List<BibEntry> entries = panel.getSelectedEntries();
 
-    @Override
-    public void run() {
-        List<BibEntry> entries = panel.getSelectedEntries();
-        if (entries == null) {
-            return;
-        }
+		UndoableAbbreviator undoableAbbreviator = new UndoableAbbreviator(
+				Globals.journalAbbreviationLoader.getRepository(Globals.prefs.getJournalAbbreviationPreferences()),
+				iso);
 
-        UndoableAbbreviator undoableAbbreviator = new UndoableAbbreviator(Globals.journalAbbreviationLoader
-                .getRepository(Globals.prefs.getJournalAbbreviationPreferences()), iso);
+		NamedCompound ce = new NamedCompound(Localization.lang("Abbreviate journal names"));
+		int count = 0;
 
-        NamedCompound ce = new NamedCompound(Localization.lang("Abbreviate journal names"));
-        int count = 0;
+		for (BibEntry entry : entries) {
+			Callable<Boolean> callable = () -> {
+				for (String journalField : InternalBibtexFields.getJournalNameFields()) {
+					if (undoableAbbreviator.abbreviate(panel.getDatabase(), entry, journalField, ce)) {
+						return true;
+					}
+				}
 
-        List<Boolean> futures = new ArrayList<>();
-        for (BibEntry entry : entries) {
-            Callable<Boolean> callable = () -> {
-                for (String journalField : InternalBibtexFields.getJournalNameFields()) {
-                    if (undoableAbbreviator.abbreviate(panel.getDatabase(), entry, journalField, ce)) {
-                        return true;
-                    }
-                }
+				return false;
+			};
 
-                return false;
-            };
-            JabRefExecutorService.INSTANCE.executeAndWait(callable);
-            futures.add(JabRefExecutorService.INSTANCE.executeAndWait(callable));
-        }
+			boolean result = JabRefExecutorService.INSTANCE.executeAndWait(callable);
+			if (result) {
+				count++;
+			}
+		}
 
-        for (Boolean future : futures) {
-            if (future)
-                count++;
-        }
+		if (count > 0) {
+			ce.end();
+			panel.getUndoManager().addEdit(ce);
+			panel.markBaseChanged();
+			message = Localization.lang("Abbreviated %0 journal names.", String.valueOf(count));
+		} else {
+			message = Localization.lang("No journal names could be abbreviated.");
+		}
+	}
 
-        if (count > 0) {
-            ce.end();
-            panel.getUndoManager().addEdit(ce);
-            panel.markBaseChanged();
-            message = Localization.lang("Abbreviated %0 journal names.", String.valueOf(count));
-        } else {
-            message = Localization.lang("No journal names could be abbreviated.");
-        }
-    }
-
-    @Override
-    public void update() {
-        panel.output(message);
-    }
+	@Override
+	public void update() {
+		panel.output(message);
+	}
 }
