dfeda8dd4e408bbfb7f56d31aca8551b147ddf90
Linus Dietz <linus.dietz@tum.de>
2018-02-16 07:45:23 +0100

    fix localization

--- src/main/java/org/jabref/gui/dialogs/MergeReviewIntoCommentUIManager.java
+++ src/main/java/org/jabref/gui/dialogs/MergeReviewIntoCommentUIManager.java
@@ -20,8 +20,8 @@ public class MergeReviewIntoCommentUIManager {
 
         int answer = JOptionPane.showConfirmDialog(
                 null,
-                String.join(",\n", bibKeys) +
-                        Localization.lang(" has/have both a 'Comment' and a 'Review' field.") + "\n" +
+                String.join(",\n", bibKeys) + " " +
+                        Localization.lang("has/have both a 'Comment' and a 'Review' field.") + "\n" +
                         Localization.lang("Since the 'Review' field was deprecated in JabRef 4.2, these two fields are about to be merged into the 'Comment' field.") + "\n" +
                         Localization.lang("By clicking 'Yes' the conflicting fields of these entries will be merged into the 'Comment' field."),
                 Localization.lang("Review Field Migration"), JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
