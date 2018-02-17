5b15f2631b0edbb53dcbc053747068d2a2e48b99
Siedlerchr <siedlerkiller@gmail.com>
2018-02-17 17:52:31 +0100

    add save and cancel buttons

--- src/main/java/org/jabref/gui/strings/StringDialogController.java
+++ src/main/java/org/jabref/gui/strings/StringDialogController.java
@@ -8,29 +8,39 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.control.cell.TextFieldTableCell;
 
 import org.jabref.gui.AbstractController;
+import org.jabref.gui.IconTheme.JabRefIcons;
+import org.jabref.gui.help.HelpAction;
+import org.jabref.logic.help.HelpFile;
+import org.jabref.logic.l10n.Localization;
 import org.jabref.gui.StateManager;
 
 public class StringDialogController extends AbstractController<StringDialogViewModel> {
 
     @FXML private Button btnNewString;
-
     @FXML private Button btnRemove;
-
     @FXML private Button btnHelp;
-
+    @FXML private Button btnCancel;
+    @FXML private Button btnSave;
     @FXML private TableView<StringViewModel> tblStrings;
-
     @FXML private TableColumn<StringViewModel, String> colLabel;
-
     @FXML private TableColumn<StringViewModel, String> colContent;
-
     @Inject private StateManager stateManager;
 
     @FXML
     private void initialize() {
+
+        btnHelp.setGraphic(JabRefIcons.HELP.getGraphicNode());
+        btnHelp.setTooltip(new Tooltip(Localization.lang("Open Help page")));
+
+        btnNewString.setGraphic(JabRefIcons.ADD.getGraphicNode());
+        btnNewString.setTooltip(new Tooltip(Localization.lang("New string")));
+
+        btnRemove.setGraphic(JabRefIcons.REMOVE.getGraphicNode());
+        btnRemove.setTooltip(new Tooltip(Localization.lang("Remove string")));
 
         viewModel = new StringDialogViewModel(stateManager);
         colLabel.setCellValueFactory(cellData -> cellData.getValue().getLabel());
@@ -54,20 +64,28 @@ public class StringDialogController extends AbstractController<StringDialogViewM
     }
 
     @FXML
-    void addString(ActionEvent event) {
-
+    private void addString(ActionEvent event) {
         viewModel.addNewString();
-
     }
 
     @FXML
-    void openHelp(ActionEvent event) {
-
+    private void openHelp(ActionEvent event) {
+        HelpAction.openHelpPage(HelpFile.STRING_EDITOR);
     }
 
     @FXML
-    void removeString(ActionEvent event) {
+    private void removeString(ActionEvent event) {
         StringViewModel selected = tblStrings.getSelectionModel().getSelectedItem();
         viewModel.removeString(selected);
+    }
+
+    @FXML
+    private void save() {
+
+    }
+
+    @FXML
+    private void close() {
+        getStage().close();
     }
 }
