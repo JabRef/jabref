19945dfe53d35897f6742337acb884c404c69f28
Tobias Diez <tobiasdiez@gmx.de>
2018-03-15 22:20:06 +0100

    Fix tests

--- src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
+++ src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
@@ -7,13 +7,14 @@ import java.util.Optional;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
-import org.jabref.Globals;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.util.TaskExecutor;
+import org.jabref.logic.journals.JournalAbbreviationLoader;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.LinkedFile;
 import org.jabref.model.metadata.FileDirectoryPreferences;
+import org.jabref.preferences.JabRefPreferences;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,6 +35,8 @@ import static org.mockito.Mockito.when;
 public class LinkedFileViewModelTest {
 
     @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+    private final JabRefPreferences preferences = mock(JabRefPreferences.class);
+    private final JournalAbbreviationLoader abbreviationLoader = mock(JournalAbbreviationLoader.class);
     private LinkedFile linkedFile;
     private BibEntry entry;
     private BibDatabaseContext databaseContext;
@@ -56,7 +59,7 @@ public class LinkedFileViewModelTest {
         linkedFile = spy(new LinkedFile("", "nonexistent file", ""));
         doReturn(Optional.empty()).when(linkedFile).findIn(any(BibDatabaseContext.class), any(FileDirectoryPreferences.class));
 
-        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, Globals.prefs, Globals.journalAbbreviationLoader);
+        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, preferences, abbreviationLoader);
         boolean removed = viewModel.delete(fileDirectoryPreferences);
 
         assertTrue(removed);
@@ -75,7 +78,7 @@ public class LinkedFileViewModelTest {
                 any(ButtonType.class),
                 any(ButtonType.class))).thenAnswer(invocation -> Optional.of(invocation.getArgument(3))); // first vararg - remove button
 
-        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, Globals.prefs, Globals.journalAbbreviationLoader);
+        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, preferences, abbreviationLoader);
         boolean removed = viewModel.delete(fileDirectoryPreferences);
 
         assertTrue(removed);
@@ -94,7 +97,7 @@ public class LinkedFileViewModelTest {
                 any(ButtonType.class),
                 any(ButtonType.class))).thenAnswer(invocation -> Optional.of(invocation.getArgument(4))); // second vararg - delete button
 
-        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, Globals.prefs, Globals.journalAbbreviationLoader);
+        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, preferences, abbreviationLoader);
         boolean removed = viewModel.delete(fileDirectoryPreferences);
 
         assertTrue(removed);
@@ -112,7 +115,7 @@ public class LinkedFileViewModelTest {
                 any(ButtonType.class),
                 any(ButtonType.class))).thenAnswer(invocation -> Optional.of(invocation.getArgument(4))); // second vararg - delete button
 
-        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, Globals.prefs, Globals.journalAbbreviationLoader);
+        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, preferences, abbreviationLoader);
         boolean removed = viewModel.delete(fileDirectoryPreferences);
 
         verify(dialogService).showErrorDialogAndWait(anyString(), anyString());
@@ -131,7 +134,7 @@ public class LinkedFileViewModelTest {
                 any(ButtonType.class),
                 any(ButtonType.class))).thenAnswer(invocation -> Optional.of(invocation.getArgument(5))); // third vararg - cancel button
 
-        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, Globals.prefs, Globals.journalAbbreviationLoader);
+        LinkedFileViewModel viewModel = new LinkedFileViewModel(linkedFile, entry, databaseContext, taskExecutor, dialogService, preferences, abbreviationLoader);
         boolean removed = viewModel.delete(fileDirectoryPreferences);
 
         assertFalse(removed);
