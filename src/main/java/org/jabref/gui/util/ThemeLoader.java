710275cc36864fd3e819cc8bdbd15d7b182b79e9
Patrick Scheibe <patrick@halirutan.de>
2018-03-18 04:47:41 +0100

    Add java property to make location of Main.css customizable
    The "live reloading" has now to be turned on by defining -Djabref.main.css
    Additionally, the location of the Main.css file can be given as parameter to
    this property. This makes it way more convenient when working on the code, because
    usually the location of the Main.css is in the build folder. Now, you can
    use the Main.css from the source directly.

--- src/main/java/org/jabref/gui/util/ThemeLoader.java
+++ src/main/java/org/jabref/gui/util/ThemeLoader.java
@@ -3,6 +3,7 @@ package org.jabref.gui.util;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
@@ -16,9 +17,21 @@ import org.jabref.model.util.FileUpdateMonitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Installs the Main.css style file and provides live reloading. The live reloading has to be turned on by setting
+ * the <code>-Djabref.main.css</code> property. There two possible modes: (1) When only <code>-Djabref.main.css</code>
+ * is specified, then the standard <code>Main.css</code> that is found will be watched and on changes in that file,
+ * the style-sheet will be reloaded and changes are immediately visible.
+ * <p>
+ * When working from an IDE, this usually means that the <code>Main.css</code> is located in the build folder. To use
+ * the css-file that is located in the sources directly, the full path can be given as value:
+ * <p>
+ * <code>-Djabref.main.css="/path/to/src/Main.css"</code>
+ */
 public class ThemeLoader {
 
     private static final String pathDefaultMainCss = AbstractView.class.getResource("Main.css").toExternalForm();
+    private static final String cssSystemProperty = System.getProperty("jabref.main.css");
     private static final Logger LOGGER = LoggerFactory.getLogger(ThemeLoader.class);
     private final FileUpdateMonitor fileUpdateMonitor;
 
@@ -31,15 +44,24 @@ public class ThemeLoader {
      * Changes in the css file lead to a redraw of the scene using the new css file.
      */
     public void installBaseCss(Scene scene) {
-        installCss(scene, pathDefaultMainCss);
+        String pathToMainCss = pathDefaultMainCss;
+        if (cssSystemProperty != null) {
+            final Path path = Paths.get(cssSystemProperty);
+            if ("Main.css".matches(path.getFileName().toString()) && Files.isReadable(path)) {
+                pathToMainCss = path.toUri().toString();
+            }
+        }
+        installCss(scene, pathToMainCss);
     }
 
     private void installCss(Scene scene, String cssUrl) {
         scene.getStylesheets().add(0, cssUrl);
         try {
-            // If the resources are part of a .jar bundle, then we don't want to watch it for changes
-            if (!cssUrl.startsWith("jar:")) {
+            // If -Djabref.main.css is defined and the resources are not part of a .jar bundle,
+            // we watch the file for changes and turn on live reloading
+            if (!cssUrl.startsWith("jar:") && cssSystemProperty != null) {
                 Path cssFile = Paths.get(new URL(cssUrl).toURI());
+                LOGGER.info("Enabling live reloading of " + cssFile);
                 fileUpdateMonitor.addListenerForFile(cssFile, () -> {
                     LOGGER.info("Reload css file " + cssFile);
 
