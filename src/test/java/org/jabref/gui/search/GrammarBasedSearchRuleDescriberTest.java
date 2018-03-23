4f93e1a8ff624c2165142ddd7a0ab08d16771f8c
Tobias Diez <tobiasdiez@gmx.de>
2018-03-23 23:59:31 +0100

    Improve tooltip tests

--- src/test/java/org/jabref/gui/search/GrammarBasedSearchRuleDescriberTest.java
+++ src/test/java/org/jabref/gui/search/GrammarBasedSearchRuleDescriberTest.java
@@ -5,16 +5,27 @@ import java.util.List;
 
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 import org.jabref.gui.search.rules.describer.GrammarBasedSearchRuleDescriber;
 import org.jabref.gui.util.TooltipTextUtil;
 import org.jabref.model.search.rules.GrammarBasedSearchRule;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class GrammarBasedSearchRuleDescriberTest {
+@ExtendWith(ApplicationExtension.class)
+class GrammarBasedSearchRuleDescriberTest {
+
+    @Start
+    void onStart(Stage stage) {
+        // Needed to init JavaFX thread
+        stage.show();
+    }
 
     private TextFlow createDescription(String query, boolean caseSensitive, boolean regExp) {
         GrammarBasedSearchRule grammarBasedSearchRule = new GrammarBasedSearchRule(caseSensitive, regExp);
@@ -24,51 +35,51 @@ public class GrammarBasedSearchRuleDescriberTest {
     }
 
     @Test
-    public void testSimpleQueryCaseSensitiveRegex() {
+    void testSimpleQueryCaseSensitiveRegex() {
         String query = "a=b";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the regular expression "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "),
                 TooltipTextUtil.createText("The search is case sensitive."));
         TextFlow description = createDescription(query, true, true);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testSimpleQueryCaseSensitive() {
+    void testSimpleQueryCaseSensitive() {
         String query = "a=b";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "),
                 TooltipTextUtil.createText("The search is case sensitive."));
         TextFlow description = createDescription(query, true, false);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testSimpleQuery() {
+    void testSimpleQuery() {
         String query = "a=b";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "),
                 TooltipTextUtil.createText("The search is case insensitive."));
         TextFlow description = createDescription(query, false, false);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testSimpleQueryRegex() {
+    void testSimpleQueryRegex() {
         String query = "a=b";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the regular expression "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "),
                 TooltipTextUtil.createText("The search is case insensitive."));
         TextFlow description = createDescription(query, false, true);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testComplexQueryCaseSensitiveRegex() {
+    void testComplexQueryCaseSensitiveRegex() {
         String query = "not a=b and c=e or e=\"x\"";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("not "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the regular expression "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" and "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("c", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the regular expression "),
@@ -76,11 +87,11 @@ public class GrammarBasedSearchRuleDescriberTest {
                 TooltipTextUtil.createText("x", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "), TooltipTextUtil.createText("The search is case sensitive."));
         TextFlow description = createDescription(query, true, true);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testComplexQueryRegex() {
+    void testComplexQueryRegex() {
         String query = "not a=b and c=e or e=\"x\"";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("not "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the regular expression "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" and "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("c", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the regular expression "),
@@ -88,28 +99,28 @@ public class GrammarBasedSearchRuleDescriberTest {
                 TooltipTextUtil.createText("x", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "), TooltipTextUtil.createText("The search is case insensitive."));
         TextFlow description = createDescription(query, false, true);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testComplexQueryCaseSensitive() {
+    void testComplexQueryCaseSensitive() {
         String query = "not a=b and c=e or e=\"x\"";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("not "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" and "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("c", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("e", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" or "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("e", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("x", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "), TooltipTextUtil.createText("The search is case sensitive."));
         TextFlow description = createDescription(query, true, false);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 
     @Test
-    public void testComplexQuery() {
+    void testComplexQuery() {
         String query = "not a=b and c=e or e=\"x\"";
         List<Text> expectedTexts = Arrays.asList(TooltipTextUtil.createText("This search contains entries in which "), TooltipTextUtil.createText("not "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("a", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("b", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" and "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("c", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("e", TooltipTextUtil.TextType.BOLD),
                 TooltipTextUtil.createText(" or "), TooltipTextUtil.createText("the field "), TooltipTextUtil.createText("e", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(" contains the term "), TooltipTextUtil.createText("x", TooltipTextUtil.TextType.BOLD), TooltipTextUtil.createText(". "), TooltipTextUtil.createText("The search is case insensitive."));
         TextFlow description = createDescription(query, false, false);
 
-        assertTrue(TextFlowEqualityHelper.checkIfDescriptionEqualsExpectedTexts(description, expectedTexts));
+        TextFlowEqualityHelper.assertEquals(expectedTexts, description);
     }
 }
