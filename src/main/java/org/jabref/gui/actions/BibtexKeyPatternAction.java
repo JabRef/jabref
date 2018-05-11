15c0a7828e2917af276c8621985fa23854af3e4d
Tobias Diez <tobiasdiez@gmx.de>
2018-05-11 23:00:35 +0200

    Do not cache bibtex key pattern dialog

--- src/main/java/org/jabref/gui/actions/BibtexKeyPatternAction.java
+++ src/main/java/org/jabref/gui/actions/BibtexKeyPatternAction.java
@@ -2,12 +2,10 @@ package org.jabref.gui.actions;
 
 import org.jabref.gui.JabRefFrame;
 import org.jabref.gui.bibtexkeypattern.BibtexKeyPatternDialog;
-import org.jabref.preferences.JabRefPreferences;
 
 public class BibtexKeyPatternAction extends SimpleCommand {
 
     private JabRefFrame frame;
-    private BibtexKeyPatternDialog bibtexKeyPatternDialog;
 
     public BibtexKeyPatternAction(JabRefFrame frame) {
         this.frame = frame;
@@ -15,14 +13,7 @@ public class BibtexKeyPatternAction extends SimpleCommand {
 
     @Override
     public void execute() {
-        JabRefPreferences.getInstance();
-        if (bibtexKeyPatternDialog == null) {
-            // if no instance of BibtexKeyPatternDialog exists, create new one
-            bibtexKeyPatternDialog = new BibtexKeyPatternDialog(frame, frame.getCurrentBasePanel());
-        } else {
-            // BibtexKeyPatternDialog allows for updating content based on currently selected panel
-            bibtexKeyPatternDialog.setPanel(frame.getCurrentBasePanel());
-        }
+        BibtexKeyPatternDialog bibtexKeyPatternDialog = new BibtexKeyPatternDialog(frame.getCurrentBasePanel());
         bibtexKeyPatternDialog.setLocationRelativeTo(null);
         bibtexKeyPatternDialog.setVisible(true);
     }
