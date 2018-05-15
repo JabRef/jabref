1b6a9ca5d6b40bdefdfc39c72908f8218ab8474a
Siedlerchr <siedlerkiller@gmail.com>
2018-05-15 18:03:48 +0200

    fix checkstyle fix driver setup connection

--- src/main/java/org/jabref/logic/shared/PostgreSQLProcessor.java
+++ src/main/java/org/jabref/logic/shared/PostgreSQLProcessor.java
@@ -18,7 +18,6 @@ public class PostgreSQLProcessor extends DBMSProcessor {
 
     private PostgresSQLNotificationListener listener;
 
-
     public PostgreSQLProcessor(DatabaseConnection connection) {
         super(connection);
     }
@@ -31,36 +30,36 @@ public class PostgreSQLProcessor extends DBMSProcessor {
     @Override
     public void setUp() throws SQLException {
         connection.createStatement().executeUpdate(
-                "CREATE TABLE IF NOT EXISTS \"ENTRY\" (" +
-                "\"SHARED_ID\" SERIAL PRIMARY KEY, " +
-                "\"TYPE\" VARCHAR, " +
-                "\"VERSION\" INTEGER DEFAULT 1)");
+                                                   "CREATE TABLE IF NOT EXISTS \"ENTRY\" (" +
+                                                   "\"SHARED_ID\" SERIAL PRIMARY KEY, " +
+                                                   "\"TYPE\" VARCHAR, " +
+                                                   "\"VERSION\" INTEGER DEFAULT 1)");
 
         connection.createStatement().executeUpdate(
-                "CREATE TABLE IF NOT EXISTS \"FIELD\" (" +
-                "\"ENTRY_SHARED_ID\" INTEGER REFERENCES \"ENTRY\"(\"SHARED_ID\") ON DELETE CASCADE, " +
-                "\"NAME\" VARCHAR, " +
-                "\"VALUE\" TEXT)");
+                                                   "CREATE TABLE IF NOT EXISTS \"FIELD\" (" +
+                                                   "\"ENTRY_SHARED_ID\" INTEGER REFERENCES \"ENTRY\"(\"SHARED_ID\") ON DELETE CASCADE, " +
+                                                   "\"NAME\" VARCHAR, " +
+                                                   "\"VALUE\" TEXT)");
 
         connection.createStatement().executeUpdate(
-                "CREATE TABLE IF NOT EXISTS \"METADATA\" ("
-                + "\"KEY\" VARCHAR,"
-                + "\"VALUE\" TEXT)");
+                                                   "CREATE TABLE IF NOT EXISTS \"METADATA\" ("
+                                                   + "\"KEY\" VARCHAR,"
+                                                   + "\"VALUE\" TEXT)");
     }
 
     @Override
     protected void insertIntoEntryTable(BibEntry bibEntry) {
         // Inserting into ENTRY table
         StringBuilder insertIntoEntryQuery = new StringBuilder()
-                .append("INSERT INTO ")
-                .append(escape("ENTRY"))
-                .append("(")
-                .append(escape("TYPE"))
-                .append(") VALUES(?)");
+                                                                .append("INSERT INTO ")
+                                                                .append(escape("ENTRY"))
+                                                                .append("(")
+                                                                .append(escape("TYPE"))
+                                                                .append(") VALUES(?)");
 
         // This is the only method to get generated keys which is accepted by MySQL, PostgreSQL and Oracle.
         try (PreparedStatement preparedEntryStatement = connection.prepareStatement(insertIntoEntryQuery.toString(),
-                Statement.RETURN_GENERATED_KEYS)) {
+                                                                                    Statement.RETURN_GENERATED_KEYS)) {
 
             preparedEntryStatement.setString(1, bibEntry.getType());
             preparedEntryStatement.executeUpdate();
@@ -84,8 +83,6 @@ public class PostgreSQLProcessor extends DBMSProcessor {
     public void startNotificationListener(DBMSSynchronizer dbmsSynchronizer) {
         // Disable cleanup output of ThreadedHousekeeper
         //Logger.getLogger(ThreadedHousekeeper.class.getName()).setLevel(Level.SEVERE);
-
-
         try {
             connection.createStatement().execute("LISTEN jabrefLiveUpdate");
             // Do not use `new PostgresSQLNotificationListener(...)` as the object has to exist continuously!
