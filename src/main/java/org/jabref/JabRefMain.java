888aade35e69a5fa7e9cca87fdaa755f01183c4e
Stefan Kolb <stefan-kolb@web.de>
2018-05-24 13:38:38 +0200

    Structure startup

--- src/main/java/org/jabref/JabRefMain.java
+++ src/main/java/org/jabref/JabRefMain.java
@@ -98,27 +98,68 @@ public class JabRefMain extends Application {
     }
 
     private static void start(String[] args) {
-        // Init preferences
-        JabRefPreferences preferences = JabRefPreferences.getInstance();
-        Globals.prefs = preferences;
-        // Perform Migrations
-        migratePreferences();
-
+        // Fail on unsupported Java versions
+        ensureCorrectJavaVersion();
         FallbackExceptionHandler.installExceptionHandler();
 
-        ensureCorrectJavaVersion();
+        // Init preferences
+        final JabRefPreferences preferences = JabRefPreferences.getInstance();
+        Globals.prefs = preferences;
+        // Perform migrations
+        migratePreferences();
 
-        ProxyPreferences proxyPreferences = preferences.getProxyPreferences();
-        ProxyRegisterer.register(proxyPreferences);
-        if (proxyPreferences.isUseProxy() && proxyPreferences.isUseAuthentication()) {
-            Authenticator.setDefault(new ProxyAuthenticator());
-        }
+        configureProxy(preferences.getProxyPreferences());
 
         Globals.startBackgroundTasks();
 
+        applyPreferences(preferences);
+
+        // Process arguments
+        ArgumentProcessor argumentProcessor = new ArgumentProcessor(args, ArgumentProcessor.Mode.INITIAL_START);
+
+        // Check for running JabRef
+        if (!allowMultipleAppInstances(args)) {
+            return;
+        }
+
+        // See if we should shut down now
+        if (argumentProcessor.shouldShutDown()) {
+            Globals.shutdownThreadPools();
+            Platform.exit();
+            return;
+        }
+
+        // If not, start GUI
+        SwingUtilities
+                .invokeLater(() -> new JabRefGUI(argumentProcessor.getParserResults(), argumentProcessor.isBlank()));
+    }
+
+    private static boolean allowMultipleAppInstances(String[] args) {
+        RemotePreferences remotePreferences = Globals.prefs.getRemotePreferences();
+        if (remotePreferences.useRemoteServer()) {
+            Globals.REMOTE_LISTENER.open(new JabRefMessageHandler(), remotePreferences.getPort());
+
+            if (!Globals.REMOTE_LISTENER.isOpen()) {
+                // we are not alone, there is already a server out there, try to contact already running JabRef:
+                if (new RemoteClient(remotePreferences.getPort()).sendCommandLineArguments(args)) {
+                    // We have successfully sent our command line options through the socket to another JabRef instance.
+                    // So we assume it's all taken care of, and quit.
+                    LOGGER.info(Localization.lang("Arguments passed on to running JabRef instance. Shutting down."));
+                    Globals.shutdownThreadPools();
+                    // needed to tell JavaFx to stop
+                    Platform.exit();
+                    return false;
+                }
+            }
+            // we are alone, we start the server
+            Globals.REMOTE_LISTENER.start();
+        }
+        return true;
+    }
+
+    private static void applyPreferences(JabRefPreferences preferences) {
         // Update handling of special fields based on preferences
-        InternalBibtexFields
-                .updateSpecialFields(Globals.prefs.getBoolean(JabRefPreferences.SERIALIZESPECIALFIELDS));
+        InternalBibtexFields.updateSpecialFields(Globals.prefs.getBoolean(JabRefPreferences.SERIALIZESPECIALFIELDS));
         // Update name of the time stamp field based on preferences
         InternalBibtexFields.updateTimeStampField(Globals.prefs.getTimestampPreferences().getTimestampField());
         // Update which fields should be treated as numeric, based on preferences:
@@ -137,44 +178,16 @@ public class JabRefMain extends Application {
         // Initialize protected terms loader
         Globals.protectedTermsLoader = new ProtectedTermsLoader(Globals.prefs.getProtectedTermsPreferences());
 
-        // Process arguments
-        ArgumentProcessor argumentProcessor = new ArgumentProcessor(args, ArgumentProcessor.Mode.INITIAL_START);
-
-        // Check for running JabRef
-        RemotePreferences remotePreferences = Globals.prefs.getRemotePreferences();
-        if (remotePreferences.useRemoteServer()) {
-            Globals.REMOTE_LISTENER.open(new JabRefMessageHandler(), remotePreferences.getPort());
-
-            if (!Globals.REMOTE_LISTENER.isOpen()) {
-                // we are not alone, there is already a server out there, try to contact already running JabRef:
-                if (new RemoteClient(remotePreferences.getPort()).sendCommandLineArguments(args)) {
-                    // We have successfully sent our command line options through the socket to another JabRef instance.
-                    // So we assume it's all taken care of, and quit.
-                    LOGGER.info(Localization.lang("Arguments passed on to running JabRef instance. Shutting down."));
-                    Globals.shutdownThreadPools();
-                    // needed to tell JavaFx to stop
-                    Platform.exit();
-                    return;
-                }
-            }
-            // we are alone, we start the server
-            Globals.REMOTE_LISTENER.start();
-        }
-
         // override used newline character with the one stored in the preferences
         // The preferences return the system newline character sequence as default
         OS.NEWLINE = Globals.prefs.get(JabRefPreferences.NEWLINE);
+    }
 
-        // See if we should shut down now
-        if (argumentProcessor.shouldShutDown()) {
-            Globals.shutdownThreadPools();
-            Platform.exit();
-            return;
+    private static void configureProxy(ProxyPreferences proxyPreferences) {
+        ProxyRegisterer.register(proxyPreferences);
+        if (proxyPreferences.isUseProxy() && proxyPreferences.isUseAuthentication()) {
+            Authenticator.setDefault(new ProxyAuthenticator());
         }
-
-        // If not, start GUI
-        SwingUtilities
-                .invokeLater(() -> new JabRefGUI(argumentProcessor.getParserResults(), argumentProcessor.isBlank()));
     }
 
     /**
