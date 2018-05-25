e7fd70086494f7f9732c63649ab563c73955e1ec
Siedlerchr <siedlerkiller@gmail.com>
2018-05-25 16:51:29 +0200

    move gui logic to viewmodel add bindings

--- src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogView.java
+++ src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogView.java
@@ -1,51 +1,29 @@
 package org.jabref.gui.shared;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.GeneralSecurityException;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Optional;
-
 import javax.inject.Inject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 
-import org.jabref.Globals;
 import org.jabref.JabRefException;
-import org.jabref.gui.BasePanel;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.JabRefFrame;
-import org.jabref.gui.exporter.SaveDatabaseAction;
-import org.jabref.gui.help.HelpAction;
 import org.jabref.gui.util.BaseDialog;
 import org.jabref.gui.util.ControlHelper;
-import org.jabref.gui.util.FileDialogConfiguration;
-import org.jabref.logic.help.HelpFile;
+import org.jabref.gui.util.IconValidationDecorator;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.shared.DBMSConnectionProperties;
-import org.jabref.logic.shared.exception.InvalidDBMSConnectionPropertiesException;
 import org.jabref.logic.shared.prefs.SharedDatabasePreferences;
-import org.jabref.logic.shared.security.Password;
-import org.jabref.logic.util.FileType;
-import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.database.shared.DBMSType;
-import org.jabref.model.database.shared.DatabaseLocation;
-import org.jabref.model.database.shared.DatabaseNotSupportedException;
-import org.jabref.preferences.JabRefPreferences;
 
 import com.airhacks.afterburner.views.ViewLoader;
+import de.saxsys.mvvmfx.utils.validation.visualization.ControlsFxVisualizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,19 +46,19 @@ public class SharedDatabaseLoginDialogView extends BaseDialog<Void> {
     @Inject private DialogService dialogService;
 
     private final SharedDatabasePreferences prefs = new SharedDatabasePreferences();
+    private final ControlsFxVisualizer visualizer = new ControlsFxVisualizer();
 
     private final JabRefFrame frame;
 
     private DBMSConnectionProperties connectionProperties;
     private SharedDatabaseLoginDialogViewModel viewModel;
-
     public SharedDatabaseLoginDialogView(JabRefFrame frame) {
         this.frame = frame;
         this.setTitle(Localization.lang("Connect to shared database"));
 
         ViewLoader.view(this)
                   .load()
-                  .setAsContent(this.getDialogPane());
+                  .setAsDialogPane(this);
 
         ControlHelper.setAction(connectButton, this.getDialogPane(), event -> openDatabase());
 
@@ -91,120 +69,44 @@ public class SharedDatabaseLoginDialogView extends BaseDialog<Void> {
 
         try {
             checkFields();
-
-            connectionProperties = new DBMSConnectionProperties();
-            connectionProperties.setType(cmbDbType.getSelectionModel().getSelectedItem());
-            connectionProperties.setHost(tbHost.getText());
-            connectionProperties.setPort(Integer.parseInt(tbPort.getText()));
-            connectionProperties.setDatabase(tbDb.getText());
-            connectionProperties.setUser(tbUser.getText());
-            connectionProperties.setPassword(tbPwd.getText());
-
-            openSharedDatabase();
-        } catch (JabRefException exception) {
-            dialogService.showErrorDialogAndWait(Localization.lang("Warning"), exception);
-
+        } catch (JabRefException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        viewModel.openDatabase();
+
     }
 
     @FXML
     private void initialize() {
 
-        viewModel = new SharedDatabaseLoginDialogViewModel();
+        viewModel = new SharedDatabaseLoginDialogViewModel(frame, dialogService);
+
+        visualizer.setDecoration(new IconValidationDecorator());
 
         cmbDbType.itemsProperty().bind(viewModel.dbmstypeProperty());
         cmbDbType.getSelectionModel().select(0);
-        viewModel.selectedDbmstypeProperty().bind(cmbDbType.getSelectionModel().selectedItemProperty());
+        tbHost.textProperty().bindBidirectional(viewModel.hostProperty());
+        tbDb.textProperty().bindBidirectional(viewModel.dbProperty());
+        tbUser.textProperty().bindBidirectional(viewModel.userProperty());
+        tbPwd.textProperty().bindBidirectional(viewModel.passwordProperty());
+        tbPort.textProperty().bindBidirectional(viewModel.portProperty());
+        cmbDbType.valueProperty().bindBidirectional(viewModel.selectedDbmstypeProperty());
 
-        viewModel.selectedDbmstypeProperty().addListener((observable, oldValue, newValue) -> {
-            tbPort.setText(Integer.toString(newValue.getDefaultPort()));
-        });
-
+        tbFolder.textProperty().bindBidirectional(viewModel.folderProperty());
         btnBrowse.disableProperty().bind(chkAutosave.selectedProperty().not());
         tbFolder.disableProperty().bind(chkAutosave.selectedProperty().not());
+        chkAutosave.selectedProperty().bindBidirectional(viewModel.autosaveProperty());
 
-        applyPreferences();
+        viewModel.applyPreferences();
     }
 
     @FXML
     void openFileDialog(ActionEvent event) {
-        FileDialogConfiguration fileDialogConfiguration = new FileDialogConfiguration.Builder()
-                                                                                               .addExtensionFilter(FileType.BIBTEX_DB)
-                                                                                               .withDefaultExtension(FileType.BIBTEX_DB)
-                                                                                               .withInitialDirectory(Globals.prefs.get(JabRefPreferences.WORKING_DIRECTORY))
-                                                                                               .build();
-        Optional<Path> exportPath = dialogService.showFileSaveDialog(fileDialogConfiguration);
-        exportPath.ifPresent(path -> {
-            tbFolder.setText(path.toString());
-        });
+        viewModel.openFileDialog();
     }
 
-    private void openSharedDatabase() {
 
-        if (isSharedDatabaseAlreadyPresent()) {
-
-            dialogService.showWarningDialogAndWait(Localization.lang("Shared database connection"),
-                                                   Localization.lang("You are already connected to a database using entered connection details."));
-
-            return;
-        }
-
-        if (chkAutosave.isSelected()) {
-
-            Path localFilePath = Paths.get(tbFolder.getText());
-
-            if (Files.exists(localFilePath) && !Files.isDirectory(localFilePath)) {
-
-                boolean overwriteFilePressed = dialogService.showConfirmationDialogAndWait(Localization.lang("Existing file"),
-                                                                                           Localization.lang("'%0' exists. Overwrite file?", localFilePath.getFileName().toString()),
-                                                                                           Localization.lang("Overwrite file"),
-                                                                                           Localization.lang("Cancel"));
-                if (!overwriteFilePressed) {
-                    tbFolder.requestFocus();
-                    return;
-                }
-            }
-        }
-
-        setLoadingConnectButtonText(true);
-
-        try {
-            SharedDatabaseUIManager manager = new SharedDatabaseUIManager(frame);
-            BasePanel panel = manager.openNewSharedDatabaseTab(connectionProperties);
-            setPreferences();
-
-            if (!tbFolder.getText().isEmpty()) {
-                try {
-                    new SaveDatabaseAction(panel, Paths.get(tbFolder.getText())).runCommand();
-                } catch (Throwable e) {
-                    LOGGER.error("Error while saving the database", e);
-                }
-            }
-
-            return; // setLoadingConnectButtonText(false) should not be reached regularly.
-        } catch (SQLException | InvalidDBMSConnectionPropertiesException exception) {
-
-            frame.getDialogService().showErrorDialogAndWait(Localization.lang("Connection error"), exception);
-
-        } catch (DatabaseNotSupportedException exception) {
-            ButtonType openHelp = new ButtonType("Open Help", ButtonData.OTHER);
-
-            Optional<ButtonType> result = dialogService.showCustomButtonDialogAndWait(AlertType.INFORMATION,
-                                                                                      Localization.lang("Migration help information"),
-                                                                                      Localization.lang("Entered database has obsolete structure and is no longer supported.")
-                                                                                                                                       + "\n" +
-                                                                                                                                       Localization.lang("Click help to learn about the migration of pre-3.6 databases.")
-                                                                                                                                       + "\n" +
-                                                                                                                                       Localization.lang("However, a new database was created alongside the pre-3.6 one."),
-                                                                                      ButtonType.OK, openHelp);
-
-            result.filter(btn -> btn.equals(openHelp)).ifPresent(btn -> HelpAction.openHelpPage(HelpFile.SQL_DATABASE_MIGRATION));
-            result.filter(btn -> btn == ButtonType.OK).ifPresent(btn -> openSharedDatabase());
-
-        }
-
-        setLoadingConnectButtonText(false);
-    }
 
     private void setLoadingConnectButtonText(boolean isLoading) {
 
@@ -215,35 +117,6 @@ public class SharedDatabaseLoginDialogView extends BaseDialog<Void> {
         }
     }
 
-    private boolean isSharedDatabaseAlreadyPresent() {
-        List<BasePanel> panels = frame.getBasePanelList();
-        return panels.parallelStream().anyMatch(panel -> {
-            BibDatabaseContext context = panel.getBibDatabaseContext();
-
-            return ((context.getLocation() == DatabaseLocation.SHARED) &&
-                    this.connectionProperties.equals(context.getDBMSSynchronizer().getConnectionProperties()));
-        });
-    }
-
-    private void setPreferences() {
-        prefs.setType(cmbDbType.getSelectionModel().getSelectedItem().toString());
-        prefs.setHost(tbHost.getText());
-        prefs.setPort(tbPort.getText());
-        prefs.setName(tbDb.getText());
-        prefs.setUser(tbUser.getText());
-
-        if (chkRememberPassword.isSelected()) {
-            try {
-                prefs.setPassword(new Password(tbPwd.getText(), tbPwd.getText()).encrypt());
-            } catch (GeneralSecurityException | UnsupportedEncodingException e) {
-                LOGGER.error("Could not store the password due to encryption problems.", e);
-            }
-        } else {
-            prefs.clearPassword(); // for the case that the password is already set
-        }
-
-        prefs.setRememberPassword(chkRememberPassword.isSelected());
-    }
 
     private void checkFields() throws JabRefException {
         if (isEmptyField(tbHost)) {
@@ -273,48 +146,6 @@ public class SharedDatabaseLoginDialogView extends BaseDialog<Void> {
         return field.getText().trim().length() == 0;
     }
 
-    /**
-     * Fetches possibly saved data and configures the control elements respectively.
-     */
-    private void applyPreferences() {
-        Optional<String> sharedDatabaseType = prefs.getType();
-        Optional<String> sharedDatabaseHost = prefs.getHost();
-        Optional<String> sharedDatabasePort = prefs.getPort();
-        Optional<String> sharedDatabaseName = prefs.getName();
-        Optional<String> sharedDatabaseUser = prefs.getUser();
-        Optional<String> sharedDatabasePassword = prefs.getPassword();
-        boolean sharedDatabaseRememberPassword = prefs.getRememberPassword();
 
-        if (sharedDatabaseType.isPresent()) {
-            Optional<DBMSType> dbmsType = DBMSType.fromString(sharedDatabaseType.get());
-            dbmsType.ifPresent(dbtype -> cmbDbType.getSelectionModel().select(dbtype));
-        }
-
-        if (sharedDatabaseHost.isPresent()) {
-            tbHost.setText(sharedDatabaseHost.get());
-        }
-
-        if (sharedDatabasePort.isPresent()) {
-            tbPort.setText(sharedDatabasePort.get());
-        }
-
-        if (sharedDatabaseName.isPresent()) {
-            tbDb.setText(sharedDatabaseName.get());
-        }
-
-        if (sharedDatabaseUser.isPresent()) {
-            tbUser.setText(sharedDatabaseUser.get());
-        }
-
-        if (sharedDatabasePassword.isPresent() && sharedDatabaseUser.isPresent()) {
-            try {
-                tbPwd.setText(new Password(sharedDatabasePassword.get().toCharArray(), sharedDatabaseUser.get()).decrypt());
-            } catch (GeneralSecurityException | UnsupportedEncodingException e) {
-                LOGGER.error("Could not read the password due to decryption problems.", e);
-            }
-        }
-
-        chkRememberPassword.setSelected(sharedDatabaseRememberPassword);
-    }
 
 }
--- src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogViewModel.java
+++ src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogViewModel.java
@@ -1,18 +1,264 @@
 package org.jabref.gui.shared;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 
+import org.jabref.Globals;
 import org.jabref.gui.AbstractViewModel;
+import org.jabref.gui.BasePanel;
+import org.jabref.gui.DialogService;
+import org.jabref.gui.JabRefFrame;
+import org.jabref.gui.exporter.SaveDatabaseAction;
+import org.jabref.gui.help.HelpAction;
+import org.jabref.gui.util.FileDialogConfiguration;
+import org.jabref.logic.help.HelpFile;
+import org.jabref.logic.l10n.Localization;
+import org.jabref.logic.shared.DBMSConnectionProperties;
+import org.jabref.logic.shared.exception.InvalidDBMSConnectionPropertiesException;
+import org.jabref.logic.shared.prefs.SharedDatabasePreferences;
+import org.jabref.logic.shared.security.Password;
+import org.jabref.logic.util.FileType;
+import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.database.shared.DBMSType;
+import org.jabref.model.database.shared.DatabaseLocation;
+import org.jabref.model.database.shared.DatabaseNotSupportedException;
+import org.jabref.preferences.JabRefPreferences;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SharedDatabaseLoginDialogViewModel extends AbstractViewModel {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SharedDatabaseLoginDialogViewModel.class);
+
     private final ListProperty<DBMSType> allDBMSTypes = new SimpleListProperty<>(FXCollections.observableArrayList(DBMSType.values()));
     private final ObjectProperty<DBMSType> selectedDBMSType = new SimpleObjectProperty<>();
+
+    private final StringProperty db = new SimpleStringProperty("");
+    private final StringProperty host = new SimpleStringProperty("");
+    private final StringProperty port = new SimpleStringProperty("");
+    private final StringProperty user = new SimpleStringProperty("");
+    private final StringProperty password = new SimpleStringProperty("");
+    private final StringProperty folder = new SimpleStringProperty("");
+    private final BooleanProperty autosave = new SimpleBooleanProperty();
+    private final BooleanProperty rememberPassword = new SimpleBooleanProperty();
+
+    private final JabRefFrame frame;
+    private final DialogService dialogService;
+    private final SharedDatabasePreferences prefs = new SharedDatabasePreferences();
+
+    private DBMSConnectionProperties connectionProperties;
+
+    public SharedDatabaseLoginDialogViewModel(JabRefFrame frame, DialogService dialogService) {
+        this.frame = frame;
+        this.dialogService = dialogService;
+
+        selectedDbmstypeProperty().addListener((observable, oldValue, newValue) -> {
+            port.setValue(Integer.toString(newValue.getDefaultPort()));
+        });
+    }
+
+    public void openDatabase() {
+
+        connectionProperties = new DBMSConnectionProperties();
+        connectionProperties.setType(selectedDBMSType.getValue());
+        connectionProperties.setHost(host.getValue());
+        connectionProperties.setPort(Integer.parseInt(port.getValue()));
+        connectionProperties.setDatabase(db.getValue());
+        connectionProperties.setUser(user.getValue());
+        connectionProperties.setPassword(password.getValue());
+
+        openSharedDatabase();
+    }
+
+    private void openSharedDatabase() {
+        if (isSharedDatabaseAlreadyPresent()) {
+
+            dialogService.showWarningDialogAndWait(Localization.lang("Shared database connection"),
+                                                   Localization.lang("You are already connected to a database using entered connection details."));
+            return;
+        }
+
+        if (autosave.get()) {
+            Path localFilePath = Paths.get(folder.getValue());
+
+            if (Files.exists(localFilePath) && !Files.isDirectory(localFilePath)) {
+
+                boolean overwriteFilePressed = dialogService.showConfirmationDialogAndWait(Localization.lang("Existing file"),
+                                                                                           Localization.lang("'%0' exists. Overwrite file?", localFilePath.getFileName().toString()),
+                                                                                           Localization.lang("Overwrite file"),
+                                                                                           Localization.lang("Cancel"));
+                if (!overwriteFilePressed) {
+                    // tbFolder.requestFocus();
+                    return;
+                }
+            }
+        }
+
+        //  setLoadingConnectButtonText(true);
+
+        try {
+            SharedDatabaseUIManager manager = new SharedDatabaseUIManager(frame);
+            BasePanel panel = manager.openNewSharedDatabaseTab(connectionProperties);
+            setPreferences();
+
+            if (!folder.getValue().isEmpty()) {
+                try {
+                    new SaveDatabaseAction(panel, Paths.get(folder.getValue())).runCommand();
+                } catch (Throwable e) {
+                    LOGGER.error("Error while saving the database", e);
+                }
+            }
+
+            return; // setLoadingConnectButtonText(false) should not be reached regularly.
+        } catch (SQLException | InvalidDBMSConnectionPropertiesException exception) {
+
+            frame.getDialogService().showErrorDialogAndWait(Localization.lang("Connection error"), exception);
+
+        } catch (DatabaseNotSupportedException exception) {
+            ButtonType openHelp = new ButtonType("Open Help", ButtonData.OTHER);
+
+            Optional<ButtonType> result = dialogService.showCustomButtonDialogAndWait(AlertType.INFORMATION,
+                                                                                      Localization.lang("Migration help information"),
+                                                                                      Localization.lang("Entered database has obsolete structure and is no longer supported.")
+                                                                                                                                       + "\n" +
+                                                                                                                                       Localization.lang("Click help to learn about the migration of pre-3.6 databases.")
+                                                                                                                                       + "\n" +
+                                                                                                                                       Localization.lang("However, a new database was created alongside the pre-3.6 one."),
+                                                                                      ButtonType.OK, openHelp);
+
+            result.filter(btn -> btn.equals(openHelp)).ifPresent(btn -> HelpAction.openHelpPage(HelpFile.SQL_DATABASE_MIGRATION));
+            result.filter(btn -> btn == ButtonType.OK).ifPresent(btn -> openSharedDatabase());
+
+        }
+
+    }
+
+    private void setPreferences() {
+        prefs.setType(selectedDBMSType.getValue().toString());
+        prefs.setHost(host.getValue());
+        prefs.setPort(port.getValue());
+        prefs.setName(db.getValue());
+        prefs.setUser(user.getValue());
+
+        if (rememberPassword.get()) {
+            try {
+                prefs.setPassword(new Password(password.getValue(), password.getValue()).encrypt());
+            } catch (GeneralSecurityException | UnsupportedEncodingException e) {
+                LOGGER.error("Could not store the password due to encryption problems.", e);
+            }
+        } else {
+            prefs.clearPassword(); // for the case that the password is already set
+        }
+
+        prefs.setRememberPassword(rememberPassword.get());
+    }
+
+    /**
+     * Fetches possibly saved data and configures the control elements respectively.
+     */
+    public void applyPreferences() {
+        Optional<String> sharedDatabaseType = prefs.getType();
+        Optional<String> sharedDatabaseHost = prefs.getHost();
+        Optional<String> sharedDatabasePort = prefs.getPort();
+        Optional<String> sharedDatabaseName = prefs.getName();
+        Optional<String> sharedDatabaseUser = prefs.getUser();
+        Optional<String> sharedDatabasePassword = prefs.getPassword();
+        boolean sharedDatabaseRememberPassword = prefs.getRememberPassword();
+
+        if (sharedDatabaseType.isPresent()) {
+            Optional<DBMSType> dbmsType = DBMSType.fromString(sharedDatabaseType.get());
+            dbmsType.ifPresent(selectedDBMSType::set);
+        }
+
+        sharedDatabaseHost.ifPresent(host::set);
+        sharedDatabasePort.ifPresent(port::set);
+        sharedDatabaseName.ifPresent(db::set);
+        sharedDatabaseUser.ifPresent(user::set);
+
+        if (sharedDatabasePassword.isPresent() && sharedDatabaseUser.isPresent()) {
+            try {
+                password.setValue(new Password(sharedDatabasePassword.get().toCharArray(), sharedDatabaseUser.get()).decrypt());
+            } catch (GeneralSecurityException | UnsupportedEncodingException e) {
+                LOGGER.error("Could not read the password due to decryption problems.", e);
+            }
+        }
+
+        rememberPassword.set(sharedDatabaseRememberPassword);
+    }
+
+    private boolean isSharedDatabaseAlreadyPresent() {
+        List<BasePanel> panels = frame.getBasePanelList();
+        return panels.parallelStream().anyMatch(panel -> {
+            BibDatabaseContext context = panel.getBibDatabaseContext();
+
+            return ((context.getLocation() == DatabaseLocation.SHARED) &&
+                    this.connectionProperties.equals(context.getDBMSSynchronizer().getConnectionProperties()));
+        });
+    }
+
+    public void openFileDialog() {
+        FileDialogConfiguration fileDialogConfiguration = new FileDialogConfiguration.Builder()
+                                                                                               .addExtensionFilter(FileType.BIBTEX_DB)
+                                                                                               .withDefaultExtension(FileType.BIBTEX_DB)
+                                                                                               .withInitialDirectory(Globals.prefs.get(JabRefPreferences.WORKING_DIRECTORY))
+                                                                                               .build();
+        Optional<Path> exportPath = dialogService.showFileSaveDialog(fileDialogConfiguration);
+        exportPath.ifPresent(path -> {
+            folder.setValue(path.toString());
+        });
+    }
+
+    public StringProperty dbProperty() {
+        return db;
+    }
+
+    public StringProperty hostProperty() {
+        return host;
+    }
+
+    public StringProperty portProperty() {
+        return port;
+    }
+
+    public StringProperty userProperty() {
+        return user;
+    }
+
+    public StringProperty passwordProperty() {
+        return password;
+    }
+
+    public BooleanProperty autosaveProperty() {
+        return autosave;
+    }
+
+    public BooleanProperty rememberPasswordProperty() {
+        return rememberPassword;
+    }
+
+    public StringProperty folderProperty() {
+        return folder;
+    }
 
     public ListProperty<DBMSType> dbmstypeProperty() {
         return allDBMSTypes;
@@ -21,5 +267,4 @@ public class SharedDatabaseLoginDialogViewModel extends AbstractViewModel {
     public ObjectProperty<DBMSType> selectedDbmstypeProperty() {
         return selectedDBMSType;
     }
-
 }
