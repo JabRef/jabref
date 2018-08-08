05c09096a6bbd778065affa361ba00804f4cde8a
1161140207zhengzhibin <2389513327@qq.com>
2018-08-08 11:40:50 +0800

    Add paste action handling in EditorTextArea

--- src/main/java/org/jabref/gui/fieldeditors/EditorTextArea.java
+++ src/main/java/org/jabref/gui/fieldeditors/EditorTextArea.java
@@ -2,6 +2,7 @@ package org.jabref.gui.fieldeditors;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.function.Supplier;
 
@@ -14,6 +15,12 @@ import javafx.scene.input.KeyEvent;
 import com.sun.javafx.scene.control.skin.TextAreaSkin;
 
 public class EditorTextArea extends javafx.scene.control.TextArea implements Initializable {
+
+    /**
+     *  Variable that contains user-defined behavior for paste action.
+     *  Set empty paste behavior by default
+     */
+    private PasteActionHandler pasteActionHandler = new EmptyPasteHandler();
 
     public EditorTextArea() {
         this("");
@@ -68,5 +75,43 @@ public class EditorTextArea extends javafx.scene.control.TextArea implements Ini
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // not needed
+    }
+
+    /**
+     * Set pasteActionHandler variable to passed handler
+     * @param  handler an instance of PasteActionHandler that describes paste behavior
+     */
+    public void setPasteActionHandler(PasteActionHandler handler) {
+        Objects.requireNonNull(handler);
+        this.pasteActionHandler = handler;
+    }
+  
+    /**
+     *  Override javafx TextArea method applying TextArea.paste() and pasteActionHandler after
+     */
+    @Override
+    public void paste() {
+        super.paste();
+        pasteActionHandler.handle();
+    }
+  
+    /**
+     *  Interface presents user-described paste behaviour applying to paste method
+     */
+    @FunctionalInterface
+    public interface PasteActionHandler {
+
+        void handle();
+    }
+  
+    /**
+     * Empty interface implementation to do nothing external on paste method
+     */
+    private static class EmptyPasteHandler implements PasteActionHandler {
+  
+        @Override
+        public void handle() {
+            //empty implementation
+        }
     }
 }
