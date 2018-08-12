747d0ae942662502cf1f34dbddf8cd10bcd66529
Metatronwings <Metatronwings@foxmail.com>
2018-08-12 08:32:59 +0800

    ***DatabaseTest*** Migrate from JUnit 4 to JUnit 5     *DBMSProcessorTest.java         *Wait for the Travis CI

--- src/test/java/org/jabref/gui/UndoTest.java
+++ src/test/java/org/jabref/gui/UndoTest.java
@@ -2,19 +2,17 @@ package org.jabref.gui;
 
 import javax.swing.JButton;
 
-import org.jabref.testutils.category.GUITest;
-
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.dependency.jsr305.Nonnull;
 import org.assertj.swing.fixture.JTableFixture;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.swing.finder.WindowFinder.findDialog;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@Category(GUITest.class)
+@Tag("GUITest")
 public class UndoTest extends AbstractUITest {
 
     @Test
