2c8ddf534af9f3d1c867617f7e88bf99b050660a
Super-Tang <36810099+1160300311@users.noreply.github.com>
2018-08-13 13:52:29 +0800

    Push again to make test pass

--- src/main/java/org/jabref/JabRefGUI.java
+++ src/main/java/org/jabref/JabRefGUI.java
@@ -44,6 +44,7 @@ public class JabRefGUI {
     private final List<ParserResult> failed = new ArrayList<>();
     private final List<ParserResult> toOpenTab = new ArrayList<>();
     private final DialogService dialogService;
+    
     private final String focusedFile;
 
     public JabRefGUI(Stage mainStage, List<ParserResult> argsDatabases, boolean isBlank) {
