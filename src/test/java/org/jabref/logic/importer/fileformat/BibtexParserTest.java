747fb201c9b589794fb9326bcc083d42f0f0dca4
1160300302 <1295536907@qq.com>
2018-08-13 20:56:29 +0800

    BibtexParserTest.java

--- src/test/java/org/jabref/logic/importer/fileformat/BibtexParserTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/BibtexParserTest.java
@@ -44,10 +44,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Answers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
