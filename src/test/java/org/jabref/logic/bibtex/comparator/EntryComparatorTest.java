9da75f3bd4ec0b6c80a275b6a508d832e9822876
1160300302 <1295536907@qq.com>
2018-08-13 20:58:38 +0800

    EntryComparatorTest.java

--- src/test/java/org/jabref/logic/bibtex/comparator/EntryComparatorTest.java
+++ src/test/java/org/jabref/logic/bibtex/comparator/EntryComparatorTest.java
@@ -4,7 +4,7 @@ import org.jabref.model.entry.BibEntry;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EntryComparatorTest {
     @Test
