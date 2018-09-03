7529535c70f8000186df7866f9afdf9250142099
Siedlerchr <siedlerkiller@gmail.com>
2018-09-03 12:50:39 +0200

    fix checkstyle

--- src/main/java/org/jabref/gui/EntryTypeViewModel.java
+++ src/main/java/org/jabref/gui/EntryTypeViewModel.java
@@ -3,7 +3,6 @@ package org.jabref.gui;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
-
 public class EntryTypeViewModel {
 
     private final BooleanProperty searchingProperty = new SimpleBooleanProperty();
