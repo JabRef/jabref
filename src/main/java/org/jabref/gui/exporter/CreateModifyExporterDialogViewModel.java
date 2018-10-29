3a4baa3993c2a24be00f39d7bb5335bcf13ef297
Abraham Polk <abepolk@gmail.com>
2018-10-29 11:28:57 -0400

    Fix browse method in export customization subdialog

--- src/main/java/org/jabref/gui/exporter/CreateModifyExporterDialogViewModel.java
+++ src/main/java/org/jabref/gui/exporter/CreateModifyExporterDialogViewModel.java
@@ -102,7 +102,7 @@ public class CreateModifyExporterDialogViewModel extends AbstractViewModel {
             .addExtensionFilter(Localization.lang("Custom layout file"), StandardFileType.LAYOUT)
             .withDefaultExtension(Localization.lang("Custom layout file"), StandardFileType.LAYOUT)
             .withInitialDirectory(getExportWorkingDirectory()).build();
-        dialogService.showFileOpenDialog(fileDialogConfiguration).ifPresent(f -> setText(f.toAbsolutePath().toString())); //implement setting the text
+        dialogService.showFileOpenDialog(fileDialogConfiguration).ifPresent(f -> layoutFile.set(f.toAbsolutePath().toString())); //implement setting the text
     }
 
 }
