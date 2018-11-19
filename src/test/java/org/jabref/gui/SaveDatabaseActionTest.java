153e4d5686f35f63e47cd6afde4b03e3b69e2703
Ali_Zhagparov <ali_zhagparov@epam.com>
2018-11-19 17:25:52 +0600

    create testcase

--- src/test/java/org/jabref/gui/SaveDatabaseActionTest.java
+++ src/test/java/org/jabref/gui/SaveDatabaseActionTest.java
@@ -1,8 +1,9 @@
 package org.jabref.gui;
 
-import org.jabref.Globals;
+import org.apache.pdfbox.contentstream.operator.state.Save;
 import org.jabref.gui.exporter.SaveDatabaseAction;
 import org.jabref.gui.util.FileDialogConfiguration;
+import org.jabref.logic.exporter.SavePreferences;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.database.shared.DatabaseLocation;
 import org.jabref.preferences.JabRefPreferences;
@@ -10,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.Optional;
 
@@ -22,50 +24,48 @@ public class SaveDatabaseActionTest {
     private Optional<Path> path = Optional.of(file.toPath());
 
     private DialogService dialogService = mock(DialogService.class);
-    private JabRefPreferences jabRefPreferences = mock(JabRefPreferences.class);
+    private JabRefPreferences preferences = mock(JabRefPreferences.class);
     private BasePanel basePanel = mock(BasePanel.class);
     private JabRefFrame jabRefFrame = mock(JabRefFrame.class);
     private SaveDatabaseAction saveDatabaseAction;
-    private BibDatabaseContext dbContext = mock(BibDatabaseContext.class);
+    private BibDatabaseContext dbContext = spy(BibDatabaseContext.class);
 
     @Before
     public void setUp(){
-        Globals.prefs = jabRefPreferences;
-
         when(basePanel.frame()).thenReturn(jabRefFrame);
         when(basePanel.getBibDatabaseContext()).thenReturn(dbContext);
         when(jabRefFrame.getDialogService()).thenReturn(dialogService);
 
-        saveDatabaseAction = spy(new SaveDatabaseAction(basePanel));
+        saveDatabaseAction = spy(new SaveDatabaseAction(basePanel, preferences));
     }
 
     @Test
-    public void saveAsShouldSetWorkingDirectory(){
-        when(jabRefPreferences.get(JabRefPreferences.WORKING_DIRECTORY)).thenReturn("C:\\Users\\John_Doe\\Jabref");
+    public void saveAs_setWorkingDirectory(){
+        when(preferences.get(JabRefPreferences.WORKING_DIRECTORY)).thenReturn("C:\\Users\\John_Doe\\Jabref");
         when(dialogService.showFileSaveDialog(any(FileDialogConfiguration.class))).thenReturn(path);
         doNothing().when(saveDatabaseAction).saveAs(any());
 
         saveDatabaseAction.saveAs();
 
-        verify(jabRefPreferences, times(1)).setWorkingDir(path.get().getParent());
+        verify(preferences, times(1)).setWorkingDir(path.get().getParent());
     }
 
     @Test
-    public void saveAsShouldNotSetWorkingDirectoryIfNotSelected(){
-        when(jabRefPreferences.get(JabRefPreferences.WORKING_DIRECTORY)).thenReturn("C:\\Users\\John_Doe\\Jabref");
+    public void saveAs_notSetWorkingDirectory_ifNotSelected(){
+        when(preferences.get(JabRefPreferences.WORKING_DIRECTORY)).thenReturn("C:\\Users\\John_Doe\\Jabref");
         when(dialogService.showFileSaveDialog(any(FileDialogConfiguration.class))).thenReturn(Optional.empty());
         doNothing().when(saveDatabaseAction).saveAs(any());
 
         saveDatabaseAction.saveAs();
 
-        verify(jabRefPreferences, times(0)).setWorkingDir(path.get().getParent());
+        verify(preferences, times(0)).setWorkingDir(path.get().getParent());
     }
 
     @Test
-    public void saveAsShouldSetNewDatabasePathIntoContext(){
+    public void saveAs_setNewDatabasePath_intoContext(){
         when(dbContext.getDatabasePath()).thenReturn(Optional.empty());
         when(dbContext.getLocation()).thenReturn(DatabaseLocation.LOCAL);
-        when(jabRefPreferences.getBoolean(JabRefPreferences.LOCAL_AUTO_SAVE)).thenReturn(false);
+        when(preferences.getBoolean(JabRefPreferences.LOCAL_AUTO_SAVE)).thenReturn(false);
 
         saveDatabaseAction.saveAs(file.toPath());
 
@@ -73,7 +73,21 @@ public class SaveDatabaseActionTest {
     }
 
     @Test
-    public void saveShouldNotSaveDatabasePathNotSetted(){
+    public void saveAs_saveDatabaseByNewPath(){
+        SavePreferences savePreferences = mock(SavePreferences.class);
+
+        when(dbContext.getLocation()).thenReturn(DatabaseLocation.LOCAL);
+        when(preferences.getBoolean(JabRefPreferences.LOCAL_AUTO_SAVE)).thenReturn(false);
+        when(preferences.getDefaultEncoding()).thenReturn(Charset.defaultCharset());
+        when(preferences.loadForSaveFromPreferences()).thenReturn(savePreferences);
+        when(savePreferences.withEncoding(any())).thenReturn(savePreferences);
+        when(savePreferences.withSaveType(any())).thenReturn(savePreferences);
+
+        saveDatabaseAction.saveAs(file.toPath());
+    }
+
+    @Test
+    public void save_notSaveDatabase_pathNotSet(){
         when(dbContext.getDatabasePath()).thenReturn(Optional.empty());
 
         boolean result = saveDatabaseAction.save();
