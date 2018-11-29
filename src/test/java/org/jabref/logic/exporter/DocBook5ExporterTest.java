a79d0378a436be064b6610158d7f02c50500fe3e
Abraham Polk <abepolk@gmail.com>
2018-11-29 16:31:51 -0500

    Add import

--- src/test/java/org/jabref/logic/exporter/DocBook5ExporterTest.java
+++ src/test/java/org/jabref/logic/exporter/DocBook5ExporterTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
