c492ad2786d6450e9912b484805c67f8684e5dc6
Leonhard Wittmann <leonhard.wittmann@gmail.com>
2019-01-02 22:46:40 +0100

    Add simple text to fallback error view
    Added a label asking the user to look into the logfiles for more details.

--- src/main/java/org/jabref/gui/fallbackerror/FallbackErrorView.java
+++ src/main/java/org/jabref/gui/fallbackerror/FallbackErrorView.java
@@ -1,8 +1,18 @@
 package org.jabref.gui.fallbackerror;
 
 import org.jabref.gui.util.BaseDialog;
+import org.jabref.logic.l10n.Localization;
+
+import com.airhacks.afterburner.views.ViewLoader;
 
 
 public class FallbackErrorView extends BaseDialog<Void> {
 
+    public FallbackErrorView() {
+        this.setTitle(Localization.lang("Unexpected error"));
+
+        ViewLoader.view(this)
+                  .load()
+                  .setAsDialogPane(this);
+    }
 }
