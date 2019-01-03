53280edc5b86ab19cc92b4811e98d092460fe07b
Stefan Kolb <stefan-kolb@web.de>
2019-01-03 18:05:09 +0100

    checkstyle

--- src/main/java/org/jabref/model/util/FileHelper.java
+++ src/main/java/org/jabref/model/util/FileHelper.java
@@ -1,7 +1,6 @@
 package org.jabref.model.util;
 
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
