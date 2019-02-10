1636f78added8d653c7ab95f3399438fc349a89f
John Besse <john.a.besse@gmail.com>
2019-02-10 16:01:01 -0500

    Tested as much as possible without JavaFX unit tests; need functional TestFX for more testing

--- src/test/java/org/jabref/gui/DuplicateResolverDialogTest.java
+++ src/test/java/org/jabref/gui/DuplicateResolverDialogTest.java
@@ -2,15 +2,17 @@ package org.jabref.gui;
 
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.jabref.gui.mergeentries.MergeEntries;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//trying to find how this is linked to BibTexKey Duplicate Check throws NPE Bug#4614
 class DuplicateResolverDialogTest {
 
     @Test
-    void givenNewDuplicateResolverDialogAndEmptyParameters_whenNothing_thenNoError() {
+    void givenNewDuplicateResolverDialogAndEmptyParameters_whenInitializing_thenNoError() {
         Stage stage = new Stage();
         JabRefFrame frame = new JabRefFrame(stage);
         BibEntry one = new BibEntry();
@@ -21,27 +23,56 @@ class DuplicateResolverDialogTest {
     }
 
     @Test
-    void givenNewStage_whenNothing_thenNoError() {
-        Stage sut = new Stage();
+    void givenUninitializedStage_whenInitializing_thenNoError() {
+        Stage sut;
 
     }
 
     @Test
-    void givenNewJabRefFrame_whenNothing_thenNoError() {
+    void givenInitializedStage_whenInitializing_thenNoError() {
+        Stage sut = new Stage(StageStyle.DECORATED);
+
+    }
+
+    @Test
+    void givenNewJabRefFrame_whenInitializing_thenNoError() {
         Stage stage = new Stage();
         JabRefFrame sut = new JabRefFrame(stage);
 
     }
 
     @Test
-    void givenNewBibEntry_whenNothing_thenNoError() {
+    void givenNewBibEntry_whenInitializing_thenNoError() {
         BibEntry sut = new BibEntry();
 
     }
 
     @Test
-    void givenNewDuplicateResolverType_whenNothing_thenNoError() {
+    void givenNewDuplicateResolverTypeIMPORT_CHECK_whenInitializing_thenEqualsIMPORT_CHECK() {
         DuplicateResolverDialog.DuplicateResolverType sut = DuplicateResolverDialog.DuplicateResolverType.IMPORT_CHECK;
         assertEquals(DuplicateResolverDialog.DuplicateResolverType.IMPORT_CHECK,sut);
+    }
+
+    @Test
+    void givenNewDuplicateResolverResultKEEP_LEFT_whenInitializing_thenEqualsIMPORT_CHECK() {
+        DuplicateResolverDialog.DuplicateResolverResult sut = DuplicateResolverDialog.DuplicateResolverResult.KEEP_LEFT;
+        assertEquals(DuplicateResolverDialog.DuplicateResolverResult.KEEP_LEFT,sut);
+    }
+
+    @Test
+    public void givenDuplicateResolverDialog_whenGetMergedEntry_thenMergedBibEntryMeReturned(){
+        Stage stage = new Stage();
+        JabRefFrame frame = new JabRefFrame(stage);
+        BibEntry one = new BibEntry();
+        BibEntry two = new BibEntry();
+        DuplicateResolverDialog.DuplicateResolverType type = DuplicateResolverDialog.DuplicateResolverType.IMPORT_CHECK;
+
+        DuplicateResolverDialog testDialog = new DuplicateResolverDialog(frame, one, two, type);
+
+        BibEntry sut = testDialog.getMergedEntry();
+        BibDatabaseMode mode = BibDatabaseMode.BIBTEX;
+
+        MergeEntries ans = new MergeEntries(one, two, mode);
+        assertEquals(ans, sut);
     }
 }
