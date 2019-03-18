b73aa2e8d6763762236f0d320c34e20f1b337e27
yurickyh <yurickyussuke@hotmail.com>
2019-03-18 17:42:10 -0300

    Change the method getVersion to return the latest version got by Version.getAllAvailableVersions method call.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- src/main/java/org/jabref/logic/util/BuildInfo.java
+++ src/main/java/org/jabref/logic/util/BuildInfo.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -52,7 +53,14 @@ public class BuildInfo {
     }
 
     public Version getVersion() {
-        return version;
+        List<Version> list = null;
+        try {
+            list = Version.getAllAvailableVersions();
+            return list.get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return version;
+        }
     }
 
     public String getAuthors() {
