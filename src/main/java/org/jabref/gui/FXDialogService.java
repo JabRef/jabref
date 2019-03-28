deff46608b5a78dfbb6acbbce2dcd7d6066a519e
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2019-03-28 22:59:06 +0100

    changed constructor to avoid NPE

--- src/main/java/org/jabref/gui/FXDialogService.java
+++ src/main/java/org/jabref/gui/FXDialogService.java
@@ -69,7 +69,7 @@ public class FXDialogService implements DialogService {
      */
     @Deprecated
     public FXDialogService() {
-        this(null, null);
+        this(null);
     }
 
     public FXDialogService(Window mainWindow) {
