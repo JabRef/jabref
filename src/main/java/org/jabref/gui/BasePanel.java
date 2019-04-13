793fa88455410e65281acdda37676deb928bbbba
Tobias Diez <tobiasdiez@gmx.de>
2019-04-13 18:33:15 +0200

    Post change notifications on JavaFX (#4871)
    * Post change notifications on JavaFX
    
    Fixes #4817. The problem is that changes in other threads produce a "Not on FX application thread" exception. This is fixed by adding a wrapper around the list of entries. The wrapper only posts changes in the correct thread, without making a copy of the underlying list - so performance shouldn't be affected.
    
    * fix compile error
    
    * fix checkstyle

--- src/main/java/org/jabref/gui/BasePanel.java
+++ src/main/java/org/jabref/gui/BasePanel.java
@@ -255,7 +255,7 @@ public class BasePanel extends StackPane {
 
     private void setupActions() {
         SaveDatabaseAction saveAction = new SaveDatabaseAction(this, Globals.prefs);
-        CleanupAction cleanUpAction = new CleanupAction(this, Globals.prefs);
+        CleanupAction cleanUpAction = new CleanupAction(this, Globals.prefs, Globals.TASK_EXECUTOR);
 
         actions.put(Actions.UNDO, undoAction);
         actions.put(Actions.REDO, redoAction);
