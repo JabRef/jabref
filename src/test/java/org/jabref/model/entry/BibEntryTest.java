978b072d053f8b4338f74953ba1f62f509e01bc8
CaptainDaVinci <yashkothari199767@gmail.com>
2019-04-24 16:03:48 +0530

    Removed unused import

--- src/test/java/org/jabref/model/entry/BibEntryTest.java
+++ src/test/java/org/jabref/model/entry/BibEntryTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BibEntryTest {
 
