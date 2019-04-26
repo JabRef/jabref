ba56ddeb3bce1c85878e50666c2b89c8edd115ad
core-master <49909555+core-master@users.noreply.github.com>
2019-04-26 08:21:35 +0200

    UI Preferences->advanced tab optimization : separators and text modification (#4922)
    * UI Preferences->advanced tab optimisation : separators and text modifications
    
    * AdvancedTab.java minor imports modifications
    
    * advancedTab : Localization.lang for text
    
    * Rework PosteOpenActions to javafx (custom entry type import)
    
    Convert dialog for importing custom entry types to CheckListView
    Rework threading when opening a database
    
    * rework threading stuff
    simplify code
    
    * rework dialog, create fxml etc
    fix l10n
    Remove obsolete code
    
    * remove dialog service argument
    
    * remove dialogService parameter
    
    * Fixes throwing an exception when 'id' field is present in bib file (#4918)
    
    * Fixes throwing an exception when 'id' field is present in bib file
    
    Fixes #4905
    
    * Remove test for id field
    
    * Renamed ID_FIELD to INTERNAL_ID_FIELD
    
    * Removed unused import
    
    * Toggle enable status of menu items (#4872)
    
    * Toggle enable status of menu items (prototype)
    
    * Use for pushtoapplication
    
    * Improve code around push to applications
    
    * Set enable status for all actions
    
    * Fix tests and checkstyle
    
    * Refactorings, move functionality of CustomEntryTypesManager to Preferences
    Use ObservableList instead of LIstProperty
    
    * Fix for the issue #4912 (#4916)
    
    * Fix for the issue #4912
    
    Apostrophe character removed from KEY_ILLEGAL_CHARACTERS in BibtexKeyGenerator
    
    * Add special character to KEY_ILLEGAL_CHARACTERS
    
    ʹ added to KEY_ILLEGAL_CHARACTERS and an assertion has been added in testMakeLabelAndCheckLegalKeysAccentGrave
    
    * translation items key modification for advanced Tab UI

--- src/main/java/org/jabref/gui/preferences/AdvancedTab.java
+++ src/main/java/org/jabref/gui/preferences/AdvancedTab.java
@@ -11,7 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 import org.jabref.Globals;
 import org.jabref.gui.DialogService;
@@ -44,7 +44,7 @@ class AdvancedTab extends Pane implements PrefsTab {
         this.dialogService = dialogService;
         preferences = prefs;
         remotePreferences = prefs.getRemotePreferences();
-
+        builder.setVgap(5);
         useRemoteServer = new CheckBox(Localization.lang("Listen for remote operation on port") + ':');
         useIEEEAbrv = new CheckBox(Localization.lang("Use IEEE LaTeX abbreviations"));
         remoteServerPort = new TextField();
@@ -54,26 +54,11 @@ class AdvancedTab extends Pane implements PrefsTab {
         Label remoteOperation = new Label(Localization.lang("Remote operation"));
         remoteOperation.getStyleClass().add("sectionHeader");
         builder.add(remoteOperation, 2, 1);
-        builder.add(new Separator(), 2, 1);
-        builder.add(new Pane(), 1, 2);
-        Label label1 = new Label(Localization.lang("This feature lets new files be opened or imported into an "
-
-                + "already running instance of JabRef<BR>instead of opening a new instance. For instance, this "
-
-                + "is useful when you open a file in JabRef<br>from your web browser."
-
-                + "<BR>Note that this will prevent you from running more than one instance of JabRef at a time."));
-        label1.setVisible(false);
-        builder.add(label1, 2, 22);
-
-        Label textLabel1 = new Label("    This feature lets new files be opened or imported into an already running instance of JabRef instead of opening a new instance. For");
-        builder.add(textLabel1, 2, 3);
-        Label textLabel2 = new Label("instance, this is useful when you open a file in JabRef from your web browser. ");
-        builder.add(textLabel2, 2, 4);
-        Label textLabel3 = new Label("    Note that this will prevent you from running more than one instance of JabRef at a time.");
-        builder.add(textLabel3, 2, 5);
-        builder.add(new Line(), 2, 6);
-        builder.add(new Pane(), 2, 7);
+        Text textRemote = new Text(Localization.lang("This feature lets new files be opened or imported into an already running instance of JabRef " +
+                "instead of opening a new instance. For instance, this is useful when you open a file in JabRef " +
+                "from your web browser. Note that this will prevent you from running more than one instance of JabRef at a time."));
+        textRemote.setWrappingWidth(600);
+        builder.add(textRemote, 2, 4);
 
         HBox p = new HBox();
         p.getChildren().add(useRemoteServer);
@@ -84,26 +69,21 @@ class AdvancedTab extends Pane implements PrefsTab {
         help.setMaxWidth(Double.MAX_VALUE);
         p.getChildren().add(help);
 
-        builder.add(p, 2, 9);
-        builder.add(new Label(""), 1, 10);
+        builder.add(p, 2, 6);
+        builder.add(new Separator(), 2, 11);
 
         Label explore = new Label(Localization.lang("Search %0", "IEEEXplore"));
         explore.getStyleClass().add("sectionHeader");
-        builder.add(explore, 2, 11);
-        builder.add(new Separator(), 2, 11);
-        builder.add(new Pane(), 2, 12);
-        builder.add(useIEEEAbrv, 2, 13);
+        builder.add(explore, 2, 12);
+        builder.add(useIEEEAbrv, 2, 14);
 
-        builder.add(new Line(), 2, 16);
-        builder.add(new Label(""), 1, 17);
-
+        builder.add(new Separator(), 2, 19);
         Label importConversions = new Label(Localization.lang("Import conversions"));
         importConversions.getStyleClass().add("sectionHeader");
-        builder.add(importConversions, 2, 18);
+        builder.add(importConversions, 2, 20);
 
-        builder.add(useCaseKeeperOnSearch, 2, 19);
-        builder.add(new Pane(), 2, 20);
-        builder.add(useUnitFormatterOnSearch, 2, 21);
+        builder.add(useCaseKeeperOnSearch, 2, 21);
+        builder.add(useUnitFormatterOnSearch, 2, 23);
 
     }
 
