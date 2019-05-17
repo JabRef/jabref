09564f5c7fbf248ca9bdfdd2b81d0166b328da4e
dpolishc <dpolishc@cisco.com>
2019-05-17 13:09:58 +0200

    fixup! fixup! Too big error message #4827 (adding a test case)

--- src/test/java/org/jabref/logic/util/strings/StringFormatterTest.java
+++ src/test/java/org/jabref/logic/util/strings/StringFormatterTest.java
@@ -8,11 +8,14 @@ class StringFormatterTest {
 
     @Test
     void formatErrorMessage() {
+        String expectedError = "The server time zone value 'MSK' is unrecognized or represents more than one time zone. You must configure either the server or JDBC driver (via \n" +
+                "the serverTimezone configuration property) to use a more specifc time zone value if you want to utilize time zone support.";
         StringFormatter stringFormatter = new StringFormatter();
         String error = "The server time zone value 'MSK' is unrecognized or represents more than one time zone. You must configure either the server or JDBC driver (via the serverTimezone configuration property) to use a more specifc time zone value if you want to utilize time zone support.";
         Exception exceptionUnderTest = new Exception(error);
         String errorToBeThrown = stringFormatter.formatErrorMessage(exceptionUnderTest);
         String[] lines = errorToBeThrown.split(System.lineSeparator());
+        assertEquals(expectedError, errorToBeThrown);
         for (int i = 0; i < lines.length; i++) {
             String line = lines[i];
             String[] split = line.split("\\s+");
