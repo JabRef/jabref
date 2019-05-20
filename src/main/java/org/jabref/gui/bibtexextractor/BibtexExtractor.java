47b30343ec1169c572d5aa82f44521aee90051c8
Nikita Borovikov <nikita.borovikov@firstlinesoftware.com>
2019-05-20 11:22:35 +0300

    Added last parsing part

--- src/main/java/org/jabref/gui/bibtexextractor/BibtexExtractor.java
+++ src/main/java/org/jabref/gui/bibtexextractor/BibtexExtractor.java
@@ -1,14 +1,25 @@
 package org.jabref.gui.bibtexextractor;
 
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.BiblatexEntryType;
 import org.jabref.model.entry.BiblatexEntryTypes;
 import org.jabref.model.entry.FieldName;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BibtexExtractor {
+
+    private final static String authorTag = "[author_tag]";
+    private final static String urlTag = "[url_tag]";
+    private final static String yearTag = "[year_tag]";
+    private final static String pagesTag = "[pages_tag]";
+    private final static String titleTag = "[title_tag]";
+    private final static String journalTag = "[journal_tag]";
 
     private final static String INITIALS_GROUP = "INITIALS";
     private final static String LASTNAME_GROUP = "LASTNAME";
@@ -16,6 +27,10 @@ public class BibtexExtractor {
     private ArrayList<String> urls = new ArrayList<>();
     private ArrayList<String> authors = new ArrayList<>();
     private String year = new String();
+    private String pages = new String();
+    private String title = new String();
+    private boolean isArticle = true;
+    private String journalOrPublisher = new String();
 
     private static final Pattern urlPattern = Pattern.compile(
             "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)" +
@@ -24,29 +39,48 @@ public class BibtexExtractor {
             Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Pattern yearPattern = Pattern.compile(
-            "\\d{4}");
+            "\\d{4}",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Pattern authorPattern1 = Pattern.compile(
             "(?<" + LASTNAME_GROUP + ">\\p{Lu}\\w+),?\\s(?<" + INITIALS_GROUP + ">(\\p{Lu}\\.\\s){1,2})" +
-                    "\\s*(and|,|\\.)*");
+            "\\s*(and|,|\\.)*",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Pattern authorPattern2 = Pattern.compile(
             "(?<" + INITIALS_GROUP + ">(\\p{Lu}\\.\\s){1,2})(?<" + LASTNAME_GROUP + ">\\p{Lu}\\w+)" +
-            "\\s*(and|,|\\.)*");
+            "\\s*(and|,|\\.)*",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+
+    private static final Pattern pagesPattern = Pattern.compile(
+            "(p.)?\\s?\\d+(-\\d+)?",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+
 
     public BibEntry extract(String input){
         String inputWithoutUrls = findUrls(input);
         String inputWithoutAuthors = findAuthors(inputWithoutUrls);
         String inputWithoutYear = findYear(inputWithoutAuthors);
-        return GenerateEntity(inputWithoutYear);
+        String inputWithoutPages = findPages(inputWithoutYear);
+        String nonparsed = findParts(inputWithoutPages);
+        return GenerateEntity(nonparsed);
     }
 
     private BibEntry GenerateEntity(String input){
-        BibEntry extractedEntity = new BibEntry(BiblatexEntryTypes.ARTICLE);
+        BiblatexEntryType type = isArticle ? BiblatexEntryTypes.ARTICLE : BiblatexEntryTypes.BOOK;
+        BibEntry extractedEntity = new BibEntry(type);
         extractedEntity.setField(FieldName.AUTHOR, String.join(" and ", authors));
-        extractedEntity.setField(FieldName.COMMENT, input);
         extractedEntity.setField(FieldName.URL, String.join(", ", urls));
         extractedEntity.setField(FieldName.YEAR, year);
+        extractedEntity.setField(FieldName.PAGES, pages);
+        extractedEntity.setField(FieldName.TITLE, title);
+        if (isArticle){
+            extractedEntity.setField(FieldName.JOURNAL, journalOrPublisher);
+        }
+        else {
+            extractedEntity.setField(FieldName.PUBLISHER, journalOrPublisher);
+        }
+        extractedEntity.setField(FieldName.COMMENT, input);
         return  extractedEntity;
     }
 
@@ -55,7 +89,7 @@ public class BibtexExtractor {
         while (matcher.find()) {
             urls.add(input.substring(matcher.start(1), matcher.end()));
         }
-        return fixSpaces(matcher.replaceAll("[url_tag]"));
+        return fixSpaces(matcher.replaceAll(urlTag));
     }
 
     private String findYear(String input){
@@ -65,7 +99,7 @@ public class BibtexExtractor {
             Integer intYearCandidate = Integer.parseInt(yearCandidate);
             if (intYearCandidate > 1700 && intYearCandidate <= Calendar.getInstance().get(Calendar.YEAR)){
                 year = yearCandidate;
-                return fixSpaces(input.replace(year, "[year_tag]"));
+                return fixSpaces(input.replace(year, yearTag));
             }
         }
         return input;
@@ -81,16 +115,67 @@ public class BibtexExtractor {
         while (matcher.find()) {
             authors.add(GenerateAuthor(matcher.group(LASTNAME_GROUP), matcher.group(INITIALS_GROUP)));
         }
-        return fixSpaces(matcher.replaceAll("[author_tag]"));
+        return fixSpaces(matcher.replaceAll(authorTag));
     }
 
     private String GenerateAuthor(String lastName, String initials){
         return lastName + ", " + initials;
     }
 
+    private String findPages(String input){
+        Matcher matcher = pagesPattern.matcher(input);
+        if (matcher.find()){
+            pages = input.substring(matcher.start(1), matcher.end());
+        }
+        return fixSpaces(matcher.replaceFirst(pagesTag));
+    }
+
     private String fixSpaces(String input){
         return input.replaceAll("[,.!?;:]", "$0 ")
                     .replaceAll("\\p{Lt}", " $0")
                     .replaceAll("\\s+", " ").trim();
+    }
+
+    private String findParts(String input)
+    {
+        ArrayList<String> lastParts = new ArrayList<>();
+        String line = input;
+        int afterAuthorsIndex = input.lastIndexOf(authorTag);
+        if (afterAuthorsIndex == -1){
+            return input;
+        }
+        else {
+            afterAuthorsIndex += authorTag.length();
+        }
+        int delimiterIndex = input.lastIndexOf("//");
+        if (delimiterIndex != -1){
+            lastParts.add(input.substring(afterAuthorsIndex, delimiterIndex)
+                         .replace(yearTag, "")
+                         .replace(pagesTag, ""));
+            lastParts.addAll(Arrays.asList(input.substring(delimiterIndex + 2).split(",|\\.")));
+        }
+
+        else {
+            lastParts.addAll(Arrays.asList(input.substring(afterAuthorsIndex).split(",|\\.")));
+        }
+        int nonDigitParts = 0;
+        for (String part: lastParts) {
+            if (part.matches(".*\\d.*")){
+                break;
+            }
+            nonDigitParts++;
+        }
+        if (nonDigitParts > 0){
+            title = lastParts.get(0);
+            line.replace(title, titleTag);
+        }
+        if (nonDigitParts > 1){
+            journalOrPublisher = lastParts.get(1);
+            line.replace(journalOrPublisher, journalTag);
+        }
+        if (nonDigitParts > 2){
+            isArticle = false;
+        }
+        return fixSpaces(line);
     }
 }
