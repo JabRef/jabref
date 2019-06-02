bad8856ae22cebe33b7b033f450f5adcf689fcb0
diegos <saerd@oregonstate.edu>
2019-06-02 02:14:08 -0400

    modified logic and error message in ValidBibtexKeyCheck.checkValue

--- src/main/java/org/jabref/logic/integrity/ValidBibtexKeyChecker.java
+++ src/main/java/org/jabref/logic/integrity/ValidBibtexKeyChecker.java
@@ -20,15 +20,22 @@ public class ValidBibtexKeyChecker implements ValueChecker {
     public Optional<String> checkValue(String value) {
 
         // Fix #2: BibtexKeyGenerator.cleanKey() does not accept a null value
-        // for the "value" parameter. The 'if' statement below is added to check for
-        // both a null and zero length string and to add the empty key warning.
-        if ((value == null) || (value.length() == 0)) {
-            return Optional.of(Localization.lang("BibTeX key is empty"));
+        // for the "value" parameter. The if statement below changes a null value
+        // to a blank string
+        String testValue;
+        if (value == null) {
+            testValue = "";
+        } else {
+            testValue = value;
         }
 
-        String cleaned = BibtexKeyGenerator.cleanKey(value, enforceLegalKey);
+        String cleaned = BibtexKeyGenerator.cleanKey(testValue, enforceLegalKey);
 
-        if (cleaned.equals(value)) {
+        // Fix #3: added a test case that would take care of cases when the parameter
+        // "value" was either null or blank
+        if (testValue.length() == 0) {
+            return Optional.of(Localization.lang("empty BibTeX key"));
+        } else if (cleaned.equals(testValue)) {
             return Optional.empty();
         } else {
             return Optional.of(Localization.lang("Invalid BibTeX key"));
