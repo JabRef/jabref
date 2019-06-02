a6fc6f3d90f0b497357c3610f64de73cef4ef81d
dpolishc <dpolishc@cisco.com>
2019-06-02 22:06:08 +0200

    Duplicate check on import should be run in background #4963

--- src/main/java/org/jabref/gui/importer/ImportEntriesDialog.java
+++ src/main/java/org/jabref/gui/importer/ImportEntriesDialog.java
@@ -109,8 +109,8 @@ public class ImportEntriesDialog extends BaseDialog<Void> {
                     container.getStyleClass().add("entry-container");
                     BindingsHelper.includePseudoClassWhen(container, entrySelected, addToggle.selectedProperty());
 
-                    BackgroundTask.wrap(() -> viewModel.hasDuplicate(entry)).onSuccess(e -> {
-                        if (e) {
+                    BackgroundTask.wrap(() -> viewModel.hasDuplicate(entry)).onSuccess(duplicateFound -> {
+                        if (duplicateFound) {
                             Button duplicateButton = IconTheme.JabRefIcons.DUPLICATE.asButton();
                             duplicateButton.setTooltip(new Tooltip(Localization.lang("Possible duplicate of existing entry. Click to resolve.")));
                             duplicateButton.setOnAction(event -> viewModel.resolveDuplicate(entry));
