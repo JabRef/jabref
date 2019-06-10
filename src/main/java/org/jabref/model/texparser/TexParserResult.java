2dfbfdbf8778c2ad91e695c1218a6091037f007c
David Méndez <hello@davidemdot.com>
2019-06-10 07:36:20 +0200

    Update tests for an easier understand and fix general issues

--- src/main/java/org/jabref/model/texparser/TexParserResult.java
+++ src/main/java/org/jabref/model/texparser/TexParserResult.java
@@ -3,15 +3,18 @@ package org.jabref.model.texparser;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.StringJoiner;
 
 public class TexParserResult {
 
     private final List<Path> fileList;
     private final List<Path> nestedFiles;
-    private final Map<String, List<Citation>> citations;
+    private final Map<String, Set<Citation>> citations;
 
     public TexParserResult() {
         this.fileList = new ArrayList<>();
@@ -27,14 +30,37 @@ public class TexParserResult {
         return nestedFiles;
     }
 
-    public Map<String, List<Citation>> getCitations() {
+    public Map<String, Set<Citation>> getCitations() {
         return citations;
+    }
+
+    /**
+     * Add a list of files to fileList or nestedFiles, depending on whether this is the first list.
+     */
+    public void addFiles(List<Path> texFiles) {
+        if (fileList.isEmpty()) {
+            fileList.addAll(texFiles);
+        } else {
+            nestedFiles.addAll(texFiles);
+        }
+    }
+
+    /**
+     * Add a citation to the citations map. It puts a new key into the map if does not exist yet.
+     */
+    public void addKey(String key, Path file, int lineNumber, int start, int end, String line) {
+        Citation citation = new Citation(file, lineNumber, start, end, line);
+        citations.computeIfAbsent(key, value -> new HashSet<>())
+                 .add(citation);
     }
 
     @Override
     public String toString() {
-        return String.format("%nTexParserResult{fileList=%s // nestedFiles=%s // citations=%s}%n",
-                fileList, nestedFiles, citations);
+        return new StringJoiner(", ", getClass().getSimpleName() + "[", "]")
+                .add("fileList = " + fileList)
+                .add("nestedFiles = " + nestedFiles)
+                .add("citations = " + citations)
+                .toString();
     }
 
     @Override
@@ -47,11 +73,11 @@ public class TexParserResult {
             return false;
         }
 
-        TexParserResult result = (TexParserResult) o;
+        TexParserResult that = (TexParserResult) o;
 
-        return fileList.equals(result.fileList)
-                && nestedFiles.equals(result.nestedFiles)
-                && citations.equals(result.citations);
+        return Objects.equals(fileList, that.fileList)
+                && Objects.equals(nestedFiles, that.nestedFiles)
+                && Objects.equals(citations, that.citations);
     }
 
     @Override
