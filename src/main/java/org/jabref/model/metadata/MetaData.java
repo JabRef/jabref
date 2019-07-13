2d0406c5a9bdeab7a357c38213c90cec000bdef8
Rodrigo <rodrigopds73@gmail.com>
2019-07-13 00:29:30 -0300

    edition checker modified to reflect alterations

--- src/main/java/org/jabref/model/metadata/MetaData.java
+++ src/main/java/org/jabref/model/metadata/MetaData.java
@@ -55,12 +55,17 @@ public class MetaData {
     private final ContentSelectors contentSelectors = new ContentSelectors();
     private final Map<String, List<String>> unkownMetaData = new HashMap<>();
     private boolean isEventPropagationEnabled = true;
+    private boolean allowIntegerEdition;
 
     /**
      * Constructs an empty metadata.
      */
     public MetaData() {
         // Do nothing
+    }
+
+    public void setAllowIntegerEdition(boolean allowIntegerEdition) {
+        this.allowIntegerEdition = allowIntegerEdition;
     }
 
     public Optional<SaveOrderConfig> getSaveOrderConfig() {
