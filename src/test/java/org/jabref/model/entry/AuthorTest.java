98e0ec5a2a02c0692f0bc331f3f5c947f3da13f1
Clodoaldo da Fonseca <clodoaldofonseca92@gmail.com>
2019-07-15 17:13:22 -0300

    Import the class that was missing

--- src/test/java/org/jabref/model/entry/AuthorTest.java
+++ src/test/java/org/jabref/model/entry/AuthorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.jabref.logic.bibtexkeypattern.BibtexKeyGenerator;
+
 class AuthorTest {
 
     @Test
