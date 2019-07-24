f2f2ad095eab8abb65902f179e7efdf6423ec284
Siedlerchr <siedlerkiller@gmail.com>
2019-07-24 15:15:17 +0200

    revert exception removal

--- src/main/java/org/jabref/gui/remote/JabRefMessageHandler.java
+++ src/main/java/org/jabref/gui/remote/JabRefMessageHandler.java
@@ -9,6 +9,7 @@ import org.jabref.cli.ArgumentProcessor;
 import org.jabref.logic.importer.ParserResult;
 import org.jabref.logic.remote.server.MessageHandler;
 
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,17 +18,20 @@ public class JabRefMessageHandler implements MessageHandler {
 
     @Override
     public void handleCommandLineArguments(String[] message) {
+        try {
+            ArgumentProcessor argumentProcessor = new ArgumentProcessor(message, ArgumentProcessor.Mode.REMOTE_START);
 
-        ArgumentProcessor argumentProcessor = new ArgumentProcessor(message, ArgumentProcessor.Mode.REMOTE_START);
-
-        List<ParserResult> loaded = argumentProcessor.getParserResults();
-        for (int i = 0; i < loaded.size(); i++) {
-            ParserResult pr = loaded.get(i);
-            boolean focusPanel = i == 0;
-            Platform.runLater(() ->
-                    // Need to run this on the JavaFX thread
-                    JabRefGUI.getMainFrame().addParserResult(pr, focusPanel)
-            );
+            List<ParserResult> loaded = argumentProcessor.getParserResults();
+            for (int i = 0; i < loaded.size(); i++) {
+                ParserResult pr = loaded.get(i);
+                boolean focusPanel = i == 0;
+                Platform.runLater(() ->
+                        // Need to run this on the JavaFX thread
+                        JabRefGUI.getMainFrame().addParserResult(pr, focusPanel)
+                );
+            }
+        } catch (ParseException e) {
+            LOGGER.error("Error when parsing CLI args", e);
         }
     }
 }
