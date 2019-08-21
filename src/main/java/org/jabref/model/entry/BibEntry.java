88245437d5cb97d31a0b084a069c2ac2ea0255da
Prasan Yapa <toprasanyapa@gmail.com>
2019-08-21 22:25:15 +0530

    bugfix/5045: Organizes imports

--- src/main/java/org/jabref/model/entry/BibEntry.java
+++ src/main/java/org/jabref/model/entry/BibEntry.java
@@ -1,18 +1,7 @@
 package org.jabref.model.entry;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.regex.Pattern;
-
+import com.google.common.base.Strings;
+import com.google.common.eventbus.EventBus;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.ObjectBinding;
@@ -20,9 +9,6 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
-
-import org.jabref.logic.importer.fetcher.CrossRef;
-import org.jabref.logic.importer.fileformat.bibtexml.Inproceedings;
 import org.jabref.model.FieldChange;
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.event.EntryEventSource;
@@ -35,11 +21,12 @@ import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.identifier.DOI;
 import org.jabref.model.strings.LatexToUnicodeAdapter;
 import org.jabref.model.strings.StringUtil;
-
-import com.google.common.base.Strings;
-import com.google.common.eventbus.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 public class BibEntry implements Cloneable {
 
