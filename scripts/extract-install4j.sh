8bb437e94c15336b0e0d18131e2270e4b2dbfa01
Oliver Kopp <kopp.dev@gmail.com>
2019-08-23 22:39:07 +0200

    Merge download-install4j and extract-install4j into prepare-install4j

--- scripts/extract-install4j.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tar -xf ~/downloads/install4j_unix_8_0.tar.gz
-https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0.tar.gz
-mv install4j8.0 install4j8
--- scripts/prepare-install4j.sh
+++ scripts/prepare-install4j.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# fetch intall4j binary
-if [ ! -d ~/downloads ]; then
-  mkdir ~/downloads
-fi
-cd ~/downloads
 wget --quiet -nc --show-progress https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0.tar.gz
+tar -xf install4j_unix_8_0.tar.gz
+mv install4j8.0 install4j8
