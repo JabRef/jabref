714edf097a8f52a5f5436bda2f7355611196ca59
David Méndez <hello@davidemdot.com>
2019-08-24 18:27:44 +0200

    Update TexBibEntriesResolver for improving readability

--- src/main/java/org/jabref/logic/texparser/TexBibEntriesResolver.java
+++ src/main/java/org/jabref/logic/texparser/TexBibEntriesResolver.java
@@ -2,13 +2,16 @@ package org.jabref.logic.texparser;
 
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jabref.logic.importer.ImportFormatPreferences;
 import org.jabref.logic.importer.OpenDatabase;
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.texparser.Citation;
 import org.jabref.model.texparser.TexBibEntriesResolverResult;
 import org.jabref.model.texparser.TexParserResult;
 import org.jabref.model.util.FileUpdateMonitor;
@@ -35,24 +38,31 @@ public class TexBibEntriesResolver {
         Map<Path, BibDatabase> bibDatabases = resolverResult.getBibFiles().values().stream().distinct().collect(Collectors.toMap(
                 Function.identity(), path -> OpenDatabase.loadDatabase(path.toString(), importFormatPreferences, fileMonitor).getDatabase()));
 
+        // Get all pairs Entry<String entryKey, Citation>.
+        Stream<Map.Entry<String, Citation>> citationsStream = texParserResult.getCitations().entries().stream().distinct();
+
+        Set<BibEntry> newEntries = citationsStream.flatMap(mapEntry -> apply(mapEntry, texParserResult, bibDatabases)).collect(Collectors.toSet());
+
         // Add all new entries to the newEntries set.
-        resolverResult.getNewEntries().addAll(
-                // Get all pairs Entry<String entryKey, Citation>.
-                texParserResult.getCitations().entries().stream().distinct().flatMap(mapEntry ->
-                        // Load the associated BIB file.
-                        texParserResult.getBibFiles().get(mapEntry.getValue().getPath()).stream().distinct().flatMap(bibFile ->
-                                // Get a specific entry from an entryKey and a BIB file.
-                                bibDatabases.get(bibFile).getEntriesByKey(mapEntry.getKey()).stream().distinct()
-                                            // Check if there is already an entry with the same key in the given database.
-                                            .filter(entry -> !entry.equals(masterDatabase.getEntryByKey(entry.getCiteKeyOptional().orElse("")).orElse(new BibEntry())))
-                                            // Add cross-referencing data to the entry (fill empty fields).
-                                            .map(entry -> {
-                                                bibDatabases.get(bibFile).getReferencedEntry(entry).ifPresent(refEntry ->
-                                                        refEntry.getFields().forEach(field -> entry.getFieldMap().putIfAbsent(field, refEntry.getFieldOrAlias(field).orElse(""))));
-                                                return entry;
-                                            })))
-                               .collect(Collectors.toSet()));
+        resolverResult.getNewEntries().addAll(newEntries);
 
         return resolverResult;
+    }
+
+    private Stream<? extends BibEntry> apply(Map.Entry<String, Citation> mapEntry, TexParserResult texParserResult, Map<Path, BibDatabase> bibDatabases) {
+        return texParserResult.getBibFiles().get(mapEntry.getValue().getPath()).stream().distinct().flatMap(bibFile ->
+                // Get a specific entry from an entryKey and a BIB file.
+                bibDatabases.get(bibFile).getEntriesByKey(mapEntry.getKey()).stream().distinct()
+                            // Check if there is already an entry with the same key in the given database.
+                            .filter(entry -> !entry.equals(masterDatabase.getEntryByKey(entry.getCiteKeyOptional().orElse("")).orElse(new BibEntry())))
+                            // Add cross-referencing data to the entry (fill empty fields).
+                            .map(entry -> addCrossReferencingData(entry, bibFile, bibDatabases)));
+    }
+
+    private BibEntry addCrossReferencingData(BibEntry entry, Path bibFile, Map<Path, BibDatabase> bibDatabases) {
+        bibDatabases.get(bibFile).getReferencedEntry(entry).ifPresent(refEntry ->
+                refEntry.getFields().forEach(field -> entry.getFieldMap().putIfAbsent(field, refEntry.getFieldOrAlias(field).orElse(""))));
+
+        return entry;
     }
 }
