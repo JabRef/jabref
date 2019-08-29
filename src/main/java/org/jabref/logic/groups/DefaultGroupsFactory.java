047cbef3e5c1ec1ee20d1d11d231fd8c824f9179
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2019-08-29 15:51:50 +0200

    fixed dependency on gui and added archUnitTest

--- src/main/java/org/jabref/logic/groups/DefaultGroupsFactory.java
+++ src/main/java/org/jabref/logic/groups/DefaultGroupsFactory.java
@@ -1,17 +1,18 @@
 package org.jabref.logic.groups;
 
-import org.jabref.gui.icon.IconTheme;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.groups.AllEntriesGroup;
 
 public class DefaultGroupsFactory {
+
+    private static String ALL_ENTRIES_GROUP_DEFAULT_ICON = "ALL_ENTRIES_GROUP_ICON";
 
     private DefaultGroupsFactory() {
     }
 
     public static AllEntriesGroup getAllEntriesGroup() {
         AllEntriesGroup group = new AllEntriesGroup(Localization.lang("All entries"));
-        group.setIconName(IconTheme.JabRefIcons.ALL_ENTRIES_GROUP_ICON.name());
+        group.setIconName(ALL_ENTRIES_GROUP_DEFAULT_ICON);
         return group;
     }
 }
