ecf2ff98b6eaf91472053f80f8a4a512a8d57fd3
Siedlerchr <siedlerkiller@gmail.com>
2019-09-13 14:58:09 +0200

    fix empty spaces

--- src/main/java/org/jabref/logic/openoffice/ChildFirstClassLoader.java
+++ src/main/java/org/jabref/logic/openoffice/ChildFirstClassLoader.java
@@ -24,7 +24,6 @@ public class ChildFirstClassLoader extends URLClassLoader {
                 loadedClass = super.loadClass(name, resolve);
             }
         }
-
         if (resolve) { // marked to resolve
             resolveClass(loadedClass);
         }
