2fe34307dcc93bcdc6530c0181721aa4211990b9
Tobias Diez <tobiasdiez@gmx.de>
2019-10-01 23:13:03 +0200

    Create chocolatey package for dev version
    Fixes #5031.

---
+++ buildres/chocolatey/jabref-dev/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://builds.jabref.org/master/JabRef-5.0.0.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url
+
+  softwareName  = 'JabRef*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # Checksums are now required as of 0.10.0.
+  # To determine checksums, you can get that from the original site if provided. 
+  # You can also use checksum.exe (choco install checksum) and use it 
+  # e.g. checksum -t sha256 -f path\to\file
+  checksum      = ''
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = ''
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
