5562322d1982ef59f26dc7c95e05a71281a2c882
Tobias Diez <tobiasdiez@gmx.de>
2019-10-10 00:45:18 +0200

    Update jabrefHost.py

--- buildres/linux/jabrefHost.py
+++ buildres/linux/jabrefHost.py
@@ -9,35 +9,31 @@ import sys
 import struct
 import subprocess
 import shlex
+import os
+import logging
+from pathlib import Path
 
 # We assume that this python script is located in "jabref/lib" while the executable is "jabref/bin/JabRef"
 script_dir = os.path.dirname(os.path.realpath('__file__'))
 JABREF_PATH = os.path.join(script_dir, '../bin/JabRef')
-INIT_LOGGER = False
-LOG_FILE = "jabfox_backend_log.txt"
-def logger(msg):
-    global INIT_LOGGER, LOG_FILE
-    if INIT_LOGGER:
-        with open(LOG_FILE, "a") as f:
-            f.write(msg)
-    else:
-        INIT_LOGGER = True
-        with open(LOG_FILE, "w") as f:
-            f.write(msg)
-        
+
+logging_dir = str(Path.home()) + "/.mozilla/native-messaging-hosts/"
+if not os.path.exists(logging_dir):
+    os.makedirs(logging_dir)
+logging.basicConfig(filename=logging_dir + 'jabref_browser_extension.log')
 
 # Read a message from stdin and decode it.
 def get_message():
     raw_length = sys.stdin.buffer.read(4)
     if not raw_length:
-        logger("[ERROR] Raw_length \n")
+        logging.error("Raw_length \n")
         sys.exit(0)
     message_length = struct.unpack('=I', raw_length)[0]
-    logger("[INFO] Got length: {} bytes to be read\n".format(message_length))
+    logging.info("Got length: {} bytes to be read\n".format(message_length))
     message = sys.stdin.buffer.read(message_length).decode("utf-8")
-    logger("[INFO] Got message of {} chars\n".format(len(message)))
+    logging.info("Got message of {} chars\n".format(len(message)))
     data = json.loads(message)
-    logger("[INFO] Successfully retrieved JSON\n")
+    logging.info("Successfully retrieved JSON\n")
     return data
 
 
@@ -60,30 +56,29 @@ def add_jabref_entry(data):
     try: 
         response = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as exc:
-        logger("[ERROR] Failed to call JabRef", exc.returncode, exc.output)
+        logging.error("Failed to call JabRef: %s %s", exc.returncode, exc.output)
     else:
-        logger(str(response))
-        logger("[INFO] Called JabRef")
+        logging.info(f'Called JabRef and got: {response}')
     return response
 
 
-logger("[INFO] Starting JabRef backend\n")
+logging.info("Starting JabRef backend")
 
 try:
     message = get_message()
 except Exception as e:
     message = str(e)
-logger(str(message) + "\n")
+logging.info(str(message))
 
 if 'status' in message and message["status"] == "validate":
     cmd = JABREF_PATH + " -version"
     try: 
         response = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT, shell=True)
     except subprocess.CalledProcessError as exc:
-        logger("[ERROR] Failed to call JabRef", exc.returncode, exc.output)
+        logging.error("Failed to call JabRef", exc.returncode, exc.output)
         send_message({"message": "jarNotFound", "path": JABREF_PATH})
     else:
-        logger(str(response))
+        logging.info(f'{response}')
         send_message({"message": "jarFound"})
 else:
     entry = message["text"]
