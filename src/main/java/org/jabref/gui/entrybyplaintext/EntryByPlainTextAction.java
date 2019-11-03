ed10ec8dae035a9ba54c801134e10fbed9dffa84
Güni <guenes.aydin@stud.unibas.ch>
2019-11-03 16:19:18 +0100

    Work on Actions and Dialog screen for gui

---
+++ src/main/java/org/jabref/gui/entrybyplaintext/EntryByPlainTextAction.java
@@ -0,0 +1,19 @@
+package org.jabref.gui.entrybyplaintext;
+
+import static org.jabref.gui.actions.ActionHelper.needsDatabase;
+
+import org.jabref.gui.StateManager;
+import org.jabref.gui.actions.SimpleCommand;
+
+public class EntryByPlainTextAction extends SimpleCommand {
+
+  public EntryByPlainTextAction(StateManager stateManager){
+    this.executable.bind(needsDatabase(stateManager));
+
+  }
+
+  @Override
+  public void execute() {
+
+  }
+}
