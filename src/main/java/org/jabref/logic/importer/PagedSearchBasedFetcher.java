3f06a376e3d9562fd9b3a47c12415d5ba2dd3b12
Johannes Hupe <joh7923@thi.de>
2019-11-05 05:57:24 +0100

    added default page size and documentation

--- src/main/java/org/jabref/logic/importer/PagedSearchBasedFetcher.java
+++ src/main/java/org/jabref/logic/importer/PagedSearchBasedFetcher.java
@@ -5,13 +5,30 @@ import org.jabref.model.paging.Page;
 
 public interface PagedSearchBasedFetcher extends SearchBasedFetcher {
 
-    Page<BibEntry> performSearchPaged(String query, int size);
+    /**
+     * Takes a Page with empty content and sets its content;
+     *
+     * @param searchPage page with search parameters
+     * @return Page with search results
+     * @apiNote implementations should call the setter of the given page and not create a new Page
+     */
+    Page<BibEntry> performSearchPaged(Page<BibEntry> searchPage) throws FetcherException;
 
-    Page<BibEntry> performSearchForNextPage(Page<BibEntry> currentPage);
+    /**
+     * @return default pageSize
+     */
+    default int getPageSize() {
+        return 20;
+    }
 
-    Page<BibEntry> performSearchForPreviousPage(Page<BibEntry> currentPage);
-
-    Page<BibEntry> performSearchForJumpToPage(Page<BibEntry> currentPage, int jumptTo);
+    /**
+     * @param query      search query send to endpoint
+     * @param pageNumber requested site number
+     * @return Page with search results
+     */
+    default Page<BibEntry> performSearchPaged(String query, int pageNumber) throws FetcherException {
+        return performSearchPaged(new Page<>(query, pageNumber));
+    }
 
     @Override
     default boolean supportsPaging() {
