d95a065e099cb0f9ff3003885b46169b86c3dca9
Güni <guenes.aydin@stud.unibas.ch>
2019-11-13 18:18:39 +0100

    Open now a new tab when parsing the new plaintext

--- src/main/java/org/jabref/gui/entrybyplaintext/EntryByPlainTextViewModel.java
+++ src/main/java/org/jabref/gui/entrybyplaintext/EntryByPlainTextViewModel.java
@@ -9,7 +9,9 @@ import javafx.beans.property.StringProperty;
 import org.jabref.Globals;
 import org.jabref.JabRefGUI;
 import org.jabref.logic.plaintextparser.ParserPipeline;
+import org.jabref.model.Defaults;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.types.EntryType;
 import org.jabref.model.entry.types.StandardEntryType;
@@ -18,12 +20,14 @@ public class EntryByPlainTextViewModel {
 
   private final StringProperty inputText = new SimpleStringProperty("");
   private final BibDatabaseContext bibDatabaseContext;
+  private final BibDatabaseContext newDatabaseContext;
 
 
 
   public EntryByPlainTextViewModel(BibDatabaseContext bibDatabaseContext){
 
     this.bibDatabaseContext = bibDatabaseContext;
+    newDatabaseContext = new BibDatabaseContext(new Defaults(BibDatabaseMode.BIBTEX));
 
   }
 
@@ -38,9 +42,15 @@ public class EntryByPlainTextViewModel {
 
     if(ParserPipeline.parsePlainRefCit(inputText.getValue()).isPresent()){
       BibEntry bibEntry = ParserPipeline.parsePlainRefCit(inputText.getValue()).get();
+
       this.bibDatabaseContext.getDatabase().insertEntry(bibEntry);
+      newDatabaseContext.getDatabase().insertEntry(bibEntry);
+      newDatabaseContext.setMode(BibDatabaseMode.BIBTEX);
+      JabRefGUI.getMainFrame().addTab(newDatabaseContext,true);
+
       JabRefGUI.getMainFrame().getCurrentBasePanel().showAndEdit(bibEntry);
       trackNewEntry(StandardEntryType.Article);
+      JabRefGUI.getMainFrame().getDialogService().notify("Successfully added a new entry.");
     } else{
       parsingFail(inputTextProperty().getValue());
     }
