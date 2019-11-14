24e781710c7007ea437dcc6b119accd00c60448b
nikch <nikodem.kernbach@stud.unibas.ch>
2019-11-14 14:50:26 +0100

    Rewrite HttpPostService to handle async requests using apache and callback functions

--- src/main/java/org/jabref/logic/net/HttpPostService.java
+++ src/main/java/org/jabref/logic/net/HttpPostService.java
@@ -6,6 +6,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
@@ -13,10 +14,9 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
-import org.jabref.JabRefExecutorService;
+import org.apache.http.nio.client.HttpAsyncClient;
 
 /**
  * Makes it easy to send http post requests
@@ -25,38 +25,39 @@ import org.jabref.JabRefExecutorService;
 public class HttpPostService {
 
     private static final HttpClient HTTP_CLIENT = HttpClients.createDefault();
+    private static final HttpAsyncClient ASYNC_HTTP_CLIENT = HttpAsyncClients.createDefault();
     private String url; //The root URL
 
     public HttpPostService(String url) {
         this.url = url;
     }
 
-    public HttpResponse sendPostAndWait(Map<String, String> params) throws HttpPostServiceException {
+    public HttpResponse sendPostAndWait(String subUrl, Map<String, String> params) throws HttpPostServiceException {
         try {
-            return HTTP_CLIENT.execute(prepareHttpPost(params));
+            return HTTP_CLIENT.execute(prepareHttpPost(params, subUrl));
+        } catch (IOException e) {
+            throw new HttpPostServiceException();
+        }
+    }
+
+    public HttpResponse sendPostAndWait(Map<String, String> params) throws HttpPostServiceException {
+        return sendPostAndWait("", params);
+    }
+
+    public Future<HttpResponse> sendPost(String subUrl,
+                                         Map<String, String> params,
+                                         FutureCallback<HttpResponse> httpResponseHandler) throws HttpPostServiceException {
+        try {
+            return ASYNC_HTTP_CLIENT.execute(prepareHttpPost(params, subUrl), httpResponseHandler);
         } catch (IOException e) {
             throw new HttpPostServiceException();
         }
     }
 
     public Future<HttpResponse> sendPost(String subUrl,
-                                         Map<String, String> params,
-                                         FutureCallback<HttpResponse> httpResponseHandler) throws IOException {
-        return JabRefExecutorService.INSTANCE.execute(() -> HTTP_CLIENT.execute(prepareHttpPost(params, subUrl)));
-        return httpAsyncClient.execute(prepareHttpPost(params, subUrl), httpResponseHandler);
-    }
+                                         Map<String, String> params) throws HttpPostServiceException {
+        return sendPost(subUrl, params, null);
 
-    public Future<HttpResponse> sendPost(Map<String, String> params) throws HttpPostServiceException {
-        return JabRefExecutorService.INSTANCE.execute(new Callable<HttpResponse>() {
-          @Override
-          public HttpResponse call() throws HttpPostServiceException {
-            try {
-              return HTTP_CLIENT.execute(prepareHttpPost(params));
-            } catch (IOException e) {
-              throw new HttpPostServiceException();
-            }
-          }
-        });
     }
 
     private HttpPost prepareHttpPost(Map<String, String> params, String urlSubPath) throws UnsupportedEncodingException {
