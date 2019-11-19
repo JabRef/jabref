acf49bb89919d72f99d38ec7e7d219458a881edc
joeyzgraggen <55385340+joeyzgraggen@users.noreply.github.com>
2019-11-19 20:25:51 +0100

    Update GrobidServiceTest.java
    Replaced System.out.println() with Logger

--- src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
+++ src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
@@ -1,20 +1,21 @@
 package org.jabref.logic.importer.util;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GrobidServiceTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrobidServiceTest.class);
+    
     @Test
     public void testConnectionReturn() {
         //TODO: THE CURRENT VERSION IS FOR TESTING PURPOSES ONLY
         try {
             String response = GrobidService.processCitation("Derwing, T. M., Rossiter, M. J., & Munro, M. J. (2002). Teaching native speakers to listen to foreign-accented speech. Journal of Multilingual and Multicultural Development, 23(4), 245-259.", 1);
-            System.out.println(".");
-            System.out.println(response);
+            LOGGER.debug(response);
         } catch (Exception e) {
-            System.out.println("Does not work");
-            System.out.println(e.getMessage());
-            System.out.println(e.getLocalizedMessage());
+            LOGGER.debug("does not work");
         }
     }
 
