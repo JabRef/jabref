196bd45bcb0f0800617023c674363795d58034bf
Joey Zgraggen <joey.zgraggen@stud.unibas.ch>
2019-11-21 15:20:30 +0100

    Implemented Unit Tests for the Class GrobidCitationFetcher (for every method of the class).

--- src/test/java/org/jabref/logic/importer/fetcher/GrobidCitationFetcherTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/GrobidCitationFetcherTest.java
@@ -1,7 +1,10 @@
 package org.jabref.logic.importer.fetcher;
 
+import java.util.Optional;
+import org.apache.logging.log4j.util.Assert;
 import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.importer.ParseException;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.util.DummyFileUpdateMonitor;
 import org.jabref.model.util.FileUpdateMonitor;
@@ -12,7 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.lang.reflect.*;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Used to test the whole logic of the external parser anystyle.
@@ -26,19 +35,11 @@ public class GrobidCitationFetcherTest {
 
     ImportFormatPreferences importFormatPreferences;
     FileUpdateMonitor fileUpdateMonitor;
-    Class c;
-    Method[] m;
 
     @BeforeEach
     public void setup() {
         importFormatPreferences = mock(ImportFormatPreferences.class, Answers.RETURNS_DEEP_STUBS);
         fileUpdateMonitor = new DummyFileUpdateMonitor();
-        try {
-            c = Class.forName("GrobidCitationFetcher.java/");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        m = c.getDeclaredMethods();
     }
 
     /**
@@ -46,7 +47,7 @@ public class GrobidCitationFetcherTest {
      * strings and parses them with the external parser and checks if the corresponding fields
      * are filled in correctly.
      */
-    /*@Test
+    @Test
     public void singleTextResourceParseTest() {
         try {
             List<BibEntry> s = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor)
@@ -57,42 +58,82 @@ public class GrobidCitationFetcherTest {
         } catch (FetcherException e) {
             LOGGER.error("Does not work");
         }
-    }*/
+    }
 
   /**
    * Checks if the Grobid parser returns a String if the input is a text reference
-   * which should be able to be parsed.
-   */
-  /*@Test
-  public void passGrobidRequestTest() throws FetcherException {
-      GrobidCitationFetcher grobidCitationFetcher = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor);
-      String plainTextExample = "M. C. Potter and R. Mackiewicz, Mechanical Vibration and "
-      + "Shock Analysis, 2nd ed. Upper Saddle River, NJ: Pearson Prentice Hall,"
-      + " 2015, pp. 17–19.";
-  }*/
-
-
-  /**
-   * Tests if multiple text references are parsed correctly if every input
-   * is valid.
+   * which should be able to be parsed. The result should either be empty or not empty but
+   * not null.
    */
   @Test
-  public void grobidValidPerformSearchTest() {
-      for(Method x : m) {
-          System.out.println(x.getName());
-      }
-
+  public void passGrobidRequestTest() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    GrobidCitationFetcher grobidCitationFetcher = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor);
+    String plainTextExample = "M. C. Potter and R. Mackiewicz, Mechanical Vibration and "
+      + "Shock Analysis, 2nd ed. Upper Saddle River, NJ: Pearson Prentice Hall," + " 2015, pp. 17–19.";
+    Method parseUsingGrobid = GrobidCitationFetcher.class.getDeclaredMethod("parseUsingGrobid", String.class);
+    parseUsingGrobid.setAccessible(true);
+    String output = (String)parseUsingGrobid.invoke(grobidCitationFetcher, plainTextExample);
+    assertNotSame(null, output);
   }
 
   /**
-   * TODO: Modify the performSearch Method -> add failed Results List
+   * Giving an invalid string as a request to the grobid parser should faild and return an
+   * empty string.
+   */
+  @Test
+  public void failingGrobidRequestTest() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    GrobidCitationFetcher grobidCitationFetcher = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor);
+    String invalidInput = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx________________________________"; // should not be a valid input for the grobid parser
+    Method parseUsingGrobid = GrobidCitationFetcher.class.getDeclaredMethod("parseUsingGrobid", String.class);
+    parseUsingGrobid.setAccessible(true);
+    String emptyOutput = (String)parseUsingGrobid.invoke(grobidCitationFetcher, invalidInput);
+    assertEquals("", emptyOutput);
+  }
+
+
+  /**
+   * Tests if the Optional<BibEntry> is filled correctly with the extracted fields from bibtex string..
+   */
+  @Test
+  public void grobidParseBibToBibEntryTest() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    GrobidCitationFetcher grobidCitationFetcher = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor);
+    String input = "@article{mrx05, \n"
+        + "auTHor = \"Mr. X\", \n"
+        + "Title = {Something Great}, \n"
+        + "publisher = \"nob\" # \"ody\"}, \n"
+        + "YEAR = 2005, \n"
+        + "} ";
+    Method parseBibToBibEntry = GrobidCitationFetcher.class.getDeclaredMethod("parseBibToBibEntry", String.class);
+    parseBibToBibEntry.setAccessible(true);
+    Optional<BibEntry> abc = (Optional<BibEntry>)parseBibToBibEntry.invoke(grobidCitationFetcher, input);
+    assertTrue(abc.isPresent());
+  }
+
+  /**
+   * Checks if parseBibToBibEntry successfully throws an exception if the parsing fails.
+   */
+  @Test
+  public void grobidParseBibToBibEntryThrowsExceptionWhenFailTest() throws NoSuchMethodException {
+    GrobidCitationFetcher grobidCitationFetcher = new GrobidCitationFetcher(importFormatPreferences, fileUpdateMonitor);
+    String failInput = "¦@#¦@#¦@#¦@#¦@#¦@#¦@°#¦@¦°¦@°";
+    Method parseBibToBibEntry = GrobidCitationFetcher.class.getDeclaredMethod("parseBibToBibEntry", String.class);
+    assertThrows(ParseException.class,
+        () -> parseBibToBibEntry.invoke(grobidCitationFetcher, failInput));
+  }
+
+
+
+
+
+  /**
+   * TODO: Modify the performSearch Method -> add failed Results List(not sure to be implemented yet)
    * Tests if multiple text references are parsed correctly and if some of
    * them are failing that they are putted into the right list.
    */
-  /*@Test
+  @Test
   public void grobidPerformSearchWithFailsTest() {
 
-  }*/
+  }
 
 
 }
