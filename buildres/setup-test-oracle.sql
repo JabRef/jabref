78c01744dc6a7a608bb7312d794b8553a0b2b868
Oliver Kopp <kopp.dev@gmail.com>
2019-12-02 07:12:20 +0100

    Create separate DB

--- buildres/setup-test-oracle.sql
+++ buildres/setup-test-oracle.sql
@@ -1,4 +1,6 @@
-create user jabref identified by jabref;
-grant connect, resource to jabref;
-grant unlimited tablespace to jabref;
-grant create session to jabref;
+create pluggable database jabref admin user jabref identified by jabref
+  file_name_convert=('/opt/oracle/oradata/XE/pdbseed','/opt/oracle/oradata/XE/JABREF');
+alter pluggable database jabref open read write;
+alter pluggable database all save state;
+ALTER SESSION SET CONTAINER = jabref;
+grant all privileges to jabref container=current;
