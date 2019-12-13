8913d4b539112407a75cf675e1b83d5d13a01c00
Abraham Polk <abepolk@gmail.com>
2019-12-13 15:34:19 -0500

    More streaming

--- src/main/java/org/jabref/model/database/DuplicationChecker.java
+++ src/main/java/org/jabref/model/database/DuplicationChecker.java
@@ -95,6 +95,7 @@ public class DuplicationChecker {
     @Subscribe
     public void listen(EntriesRemovedEvent entriesRemovedEvent) {
         List<BibEntry> entries = entriesRemovedEvent.getBibEntries();
+        entries.stream().map(BibEntry::getCiteKeyOptional).flatMap(Optional::stream).forEach(citeKey -> addKeyToSet(citeKey));
         for (BibEntry entry : entries) {
             Optional<String> citeKey = entry.getCiteKeyOptional();
             if (citeKey.isPresent()) {
@@ -106,7 +107,7 @@ public class DuplicationChecker {
     @Subscribe
     public void listen(EntriesAddedEvent entriesAddedEvent) {
         List<BibEntry> entries = entriesAddedEvent.getBibEntries();
-        entries.stream().map(BibEntry::getCiteKeyOptional).flatMap(Optional::stream).forEach(citekey -> addKeyToSet(citekey));
+        entries.stream().map(BibEntry::getCiteKeyOptional).flatMap(Optional::stream).forEach(citeKey -> addKeyToSet(citeKey));
     }
 
 }
