59088831fe47aa4e7b25bf75d83473b8626504c3
Joey Zgraggen <joey.zgraggen@stud.unibas.ch>
2019-12-27 19:21:43 +0100

    processValidCitationTest() of the GrobidServiceTest class now verifies that the entry is correctly parsed.

--- src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
+++ src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
@@ -25,8 +25,16 @@ public class GrobidServiceTest {
         String response = grobidService.processCitation("Derwing, T. M., Rossiter, M. J., & Munro, " +
                 "M. J. (2002). Teaching native speakers to listen to foreign-accented speech. " +
                 "Journal of Multilingual and Multicultural Development, 23(4), 245-259.", GrobidService.ConsolidateCitations.WITH_METADATA);
+        String[] responseRows = response.split("\n");
         assertNotNull(response);
         assertTrue(response.charAt(0) == '@');
+        assertTrue(responseRows[1].contains("author") && responseRows[1].contains( "Derwing and M Rossiter"));
+        assertTrue(responseRows[2].contains("title") && responseRows[2].contains( "Teaching native speakers"));
+        assertTrue(responseRows[3].contains("journal") && responseRows[3].contains( "Journal of Multilingual and Multicultural"));
+        assertTrue(responseRows[4].contains("year") && responseRows[4].contains( "2002"));
+        assertTrue(responseRows[5].contains("pages") && responseRows[5].contains( "245--259"));
+        assertTrue(responseRows[6].contains("volume") && responseRows[6].contains( "23"));
+        assertTrue(responseRows[7].contains("number") && responseRows[7].contains( "4"));
     }
 
     @Test
