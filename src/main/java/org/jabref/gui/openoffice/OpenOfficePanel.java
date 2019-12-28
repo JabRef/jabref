ab54a83e5fe8349faba5451908041429f3d84ff1
ddunig2 <36016544+ddunig2@users.noreply.github.com>
2019-12-27 22:00:05 -0500

    Update open OpenOfficePanel to show buttons in three rows of three.

--- src/main/java/org/jabref/gui/openoffice/OpenOfficePanel.java
+++ src/main/java/org/jabref/gui/openoffice/OpenOfficePanel.java
@@ -270,8 +270,18 @@ public class OpenOfficePanel {
         hbox.getChildren().addAll(connect, manualConnect, selectDocument, update, help);
         hbox.getChildren().forEach(btn -> HBox.setHgrow(btn, Priority.ALWAYS));
 
+        VBox row1 = new VBox();
+        VBox row2 = new VBox();
+        VBox row3 = new VBox();
+        row1.getChildren().addAll(setStyleFile,pushEntries, pushEntriesInt);
+        row2.getChildren().addAll(pushEntriesAdvanced, pushEntriesEmpty, merge);
+        row3.getChildren().addAll(manageCitations, exportCitations, settingsB);
+        HBox hbox1 =  new HBox();
+        hbox1.getChildren().addAll(row1,row2,row3);
+        hbox1.getChildren().forEach(btn -> HBox.setHgrow(btn, Priority.ALWAYS));
+
         vbox.setFillWidth(true);
-        vbox.getChildren().addAll(hbox, setStyleFile, pushEntries, pushEntriesInt, pushEntriesAdvanced, pushEntriesEmpty, merge, manageCitations, exportCitations, settingsB);
+        vbox.getChildren().addAll(hbox, hbox1);
     }
 
     private void exportEntries() {
@@ -435,7 +445,7 @@ public class OpenOfficePanel {
             jarURLs.add((jarPath.get().toUri().toURL()));
         }
         return jarURLs;
-      
+
     }
 
     private OOBibBase createBibBase(List<URL> jarUrls) throws IOException, InvocationTargetException, IllegalAccessException,
