34e3c5037d7910d6f473f7fa511edeaf65d83fdd
Tobias Diez <tobiasdiez@gmx.de>
2020-01-10 11:19:02 +0100

    Add detailed view of metadata changes

--- src/main/java/org/jabref/gui/collab/MetaDataChangeViewModel.java
+++ src/main/java/org/jabref/gui/collab/MetaDataChangeViewModel.java
@@ -2,39 +2,37 @@ package org.jabref.gui.collab;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import org.jabref.gui.undo.NamedCompound;
 import org.jabref.logic.bibtex.comparator.MetaDataDiff;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.BibDatabaseContext;
-import org.jabref.model.metadata.MetaData;
 
 class MetaDataChangeViewModel extends DatabaseChangeViewModel {
 
-    private final MetaData newMetaData;
+    private final MetaDataDiff metaDataDiff;
 
     public MetaDataChangeViewModel(MetaDataDiff metaDataDiff) {
         super(Localization.lang("Metadata change"));
-        this.newMetaData = metaDataDiff.getNewMetaData();
+        this.metaDataDiff = metaDataDiff;
     }
 
     @Override
     public Node description() {
+        VBox container = new VBox(15);
 
-        /*
-        // TODO: Show detailed description of the changes
-        StringBuilder sb = new StringBuilder(
-                "<html>" + Localization.lang("Changes have been made to the following metadata elements")
-                        + ":<p><br>&nbsp;&nbsp;");
-        sb.append(changes.stream().map(unit -> unit.key).collect(Collectors.joining("<br>&nbsp;&nbsp;")));
-        sb.append("</html>");
-        infoPane.setText(sb.toString());
-        */
-        return new Label(Localization.lang("Metadata change"));
+        container.getChildren().add(new Label(Localization.lang("The following metadata changed:")));
+
+        for (String change : metaDataDiff.getDifferences()) {
+            container.getChildren().add(new Label(change));
+        }
+
+        return container;
     }
 
     @Override
     public void makeChange(BibDatabaseContext database, NamedCompound undoEdit) {
-        database.setMetaData(newMetaData);
+        database.setMetaData(metaDataDiff.getNewMetaData());
     }
 }
