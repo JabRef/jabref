ec154f6daa6743f1c5cde14c139a5924c74f7fed
Siedlerchr <siedlerkiller@gmail.com>
2020-02-10 20:33:52 +0100

    use utf8 in getBytes to read correct bytes

--- src/main/java/org/jabref/logic/importer/Parser.java
+++ src/main/java/org/jabref/logic/importer/Parser.java
@@ -2,18 +2,18 @@ package org.jabref.logic.importer;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.jabref.model.entry.BibEntry;
 
-/**
- * A parser converts an {@link InputStream} into a list of {@link BibEntry}.
+/** * A parser converts an {@link InputStream} into a list of {@link BibEntry}.
  */
 public interface Parser {
 
     List<BibEntry> parseEntries(InputStream inputStream) throws ParseException;
 
     default List<BibEntry> parseEntries(String dataString) throws ParseException {
-        return parseEntries(new ByteArrayInputStream(dataString.getBytes()));
+        return parseEntries(new ByteArrayInputStream(dataString.getBytes(StandardCharsets.UTF_8)));
     }
 }
