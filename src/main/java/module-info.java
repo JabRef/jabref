1e708c41daf524d7094c26bc1d0f3959ad11b3bc
Tobias Diez <tobiasdiez@gmx.de>
2020-02-10 22:23:04 +0100

    Add to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -63,6 +63,7 @@ open module org.jabref {
     requires org.apache.xmpbox;
     requires de.saxsys.mvvmfx.validation;
     requires richtextfx;
+    require com.google.gson;
     requires unirest.java;
     requires org.apache.httpcomponents.httpclient;
     requires org.jsoup;
