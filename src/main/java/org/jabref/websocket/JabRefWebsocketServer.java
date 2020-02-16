d57cea14ac70adc7da9980086c4561a67b74f70b
systemoperator <3658393+systemoperator@users.noreply.github.com>
2020-02-16 11:54:47 +0100

    websocket server template added

---
+++ src/main/java/org/jabref/websocket/JabRefWebsocketServer.java
@@ -0,0 +1,89 @@
+package org.jabref.websocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+/**
+ * A simple WebSocketServer implementation. Keeps track of a "chatroom".
+ */
+public class JabRefWebsocketServer extends WebSocketServer {
+
+    public JabRefWebsocketServer(int port) throws UnknownHostException {
+        super(new InetSocketAddress(port));
+    }
+
+    public JabRefWebsocketServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conn.send("Welcome to the server!"); //This method sends a message to the new client
+        broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
+        System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        broadcast(conn + " has left the room!");
+        System.out.println(conn + " has left the room!");
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        broadcast(message);
+        System.out.println(conn + ": " + message);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        broadcast(message.array());
+        System.out.println(conn + ": " + message);
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int port = 8887; // 843 flash policy port
+        try {
+            port = Integer.parseInt(args[0]);
+        }
+        catch (Exception ex) {
+        }
+
+        JabRefWebsocketServer s = new JabRefWebsocketServer(port);
+        s.start();
+        System.out.println("JabRefWebsocketServer started on port: " + s.getPort());
+
+        BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String in = sysin.readLine();
+            s.broadcast(in);
+            if (in.equals("exit")) {
+                s.stop(1000);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+            // some errors like port binding failed may not be assignable to a specific websocket
+        }
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("Server started!");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
+    }
+}
