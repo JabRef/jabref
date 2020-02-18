faa2bd776e70e0e9ef59c5e80f56781e296476cd
christinerosquist <60344814+christinerosquist@users.noreply.github.com>
2020-02-18 13:16:55 +0100

    Added Branch Coverage Tool for format()
    * Added our own branch coverage tool to the function format

--- src/main/java/org/jabref/logic/layout/format/HTMLChars.java
+++ src/main/java/org/jabref/logic/layout/format/HTMLChars.java
@@ -1,5 +1,8 @@
 package org.jabref.logic.layout.format;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Map;
 
 import org.jabref.logic.layout.LayoutFormatter;
@@ -12,6 +15,7 @@ import org.jabref.model.strings.StringUtil;
 public class HTMLChars implements LayoutFormatter {
 
     private static final Map<String, String> HTML_CHARS = HTMLUnicodeConversionMaps.LATEX_HTML_CONVERSION_MAP;
+    private static boolean[] visited = new boolean[41];
 
     @Override
     public String format(String inField) {
@@ -30,40 +34,56 @@ public class HTMLChars implements LayoutFormatter {
         boolean incommand = false;
 
         for (i = 0; i < field.length(); i++) {
+            visited[0] = true;
             c = field.charAt(i);
             if (escaped && (c == '\\')) {
+                visited[1] = true;
                 sb.append('\\');
                 escaped = false;
             } else if (c == '\\') {
+                visited[2] = true;
                 if (incommand) {
+                    visited[3] = true;
                     /* Close Command */
                     String command = currentCommand.toString();
                     String result = HTML_CHARS.get(command);
                     if (result == null) {
+                        visited[4] = true;
                         sb.append(command);
                     } else {
+                        visited[5] = true;
                         sb.append(result);
                     }
+                } else {
+                    visited[6] = true;
                 }
                 escaped = true;
                 incommand = true;
                 currentCommand = new StringBuilder();
             } else if (!incommand && ((c == '{') || (c == '}'))) {
+                visited[7] = true;
                 // Swallow the brace.
             } else if (Character.isLetter(c) || (c == '%')
                     || StringUtil.SPECIAL_COMMAND_CHARS.contains(String.valueOf(c))) {
+                visited[8] = true;
                 escaped = false;
 
                 if (!incommand) {
+                    visited[9] = true;
                     sb.append(c);
                 } else {
+                    visited[10] = true;
                     currentCommand.append(c);
                     testCharCom: if ((currentCommand.length() == 1)
                             && StringUtil.SPECIAL_COMMAND_CHARS.contains(currentCommand.toString())) {
+                        visited[11] = true;
                         // This indicates that we are in a command of the type
                         // \^o or \~{n}
                         if (i >= (field.length() - 1)) {
+                            visited[12] = true;
                             break testCharCom;
+                        } else {
+                            visited[13] = true;
                         }
 
                         String command = currentCommand.toString();
@@ -71,25 +91,30 @@ public class HTMLChars implements LayoutFormatter {
                         c = field.charAt(i);
                         String commandBody;
                         if (c == '{') {
+                            visited[14] = true;
                             String part = StringUtil.getPart(field, i, false);
                             i += part.length();
                             commandBody = part;
                         } else {
+                            visited[15] = true;
                             commandBody = field.substring(i, i + 1);
                         }
                         String result = HTML_CHARS.get(command + commandBody);
 
                         if (result == null) {
+                            visited[16] = true;
                             sb.append(commandBody);
                         } else {
+                            visited[17] = true;
                             sb.append(result);
                         }
-
                         incommand = false;
                         escaped = false;
                     } else {
+                        visited[18] = true;
                         //	Are we already at the end of the string?
                         if ((i + 1) == field.length()) {
+                            visited[19] = true;
                             String command = currentCommand.toString();
                             String result = HTML_CHARS.get(command);
                             /* If found, then use translated version. If not,
@@ -97,18 +122,25 @@ public class HTMLChars implements LayoutFormatter {
                              * the text of the parameter intact.
                              */
                             if (result == null) {
+                                visited[20] = true;
                                 sb.append(command);
                             } else {
+                                visited[21] = true;
                                 sb.append(result);
                             }
 
+                        } else {
+                            visited[22] = true;
                         }
                     }
                 }
             } else {
+                visited[23] = true;
                 if (!incommand) {
+                    visited[24] = true;
                     sb.append(c);
                 } else if (Character.isWhitespace(c) || (c == '{') || (c == '}')) {
+                    visited[25] = true;
                     String command = currentCommand.toString();
 
                     // Test if we are dealing with a formatting
@@ -116,10 +148,12 @@ public class HTMLChars implements LayoutFormatter {
                     // If so, handle.
                     String tag = getHTMLTag(command);
                     if (!tag.isEmpty()) {
+                        visited[26] = true;
                         String part = StringUtil.getPart(field, i, true);
                         i += part.length();
                         sb.append('<').append(tag).append('>').append(part).append("</").append(tag).append('>');
                     } else if (c == '{') {
+                        visited[27] = true;
                         String argument = StringUtil.getPart(field, i, true);
                         i += argument.length();
                         // handle common case of general latex command
@@ -128,37 +162,48 @@ public class HTMLChars implements LayoutFormatter {
                         // the text of the parameter intact.
 
                         if (result == null) {
+                            visited[28] = true;
                             if (argument.isEmpty()) {
+                                visited[29] = true;
                                 // Maybe a separator, such as in \LaTeX{}, so use command
                                 sb.append(command);
                             } else {
+                                visited[30] = true;
                                 // Otherwise, use argument
                                 sb.append(argument);
                             }
                         } else {
+                            visited[31] = true;
                             sb.append(result);
                         }
                     } else if (c == '}') {
+                        visited[32] = true;
                         // This end brace terminates a command. This can be the case in
                         // constructs like {\aa}. The correct behaviour should be to
                         // substitute the evaluated command and swallow the brace:
                         String result = HTML_CHARS.get(command);
                         if (result == null) {
+                            visited[33] = true;
                             // If the command is unknown, just print it:
                             sb.append(command);
                         } else {
+                            visited[34] = true;
                             sb.append(result);
                         }
                     } else {
+                        visited[35] = true;
                         String result = HTML_CHARS.get(command);
                         if (result == null) {
+                            visited[36] = true;
                             sb.append(command);
                         } else {
+                            visited[37] = true;
                             sb.append(result);
                         }
                         sb.append(' ');
                     }
                 } else {
+                    visited[38] = true;
                     /*
                      * TODO: this point is reached, apparently, if a command is
                      * terminated in a strange way, such as with "$\omega$".
@@ -173,6 +218,25 @@ public class HTMLChars implements LayoutFormatter {
                 incommand = false;
                 escaped = false;
             }
+        } if (field.length() == 0) {
+            visited[39] = true;
+        }
+        try {
+            File directory = new File("/Temp");
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+            File f = new File(directory + "/format.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            double frac = 0;
+            for (int j = 0; j < visited.length; ++j) {
+                frac += (visited[j] ? 1 : 0);
+                bw.write("branch " + j + " was" + (visited[j] ? " visited. " : " not visited. ") + "\n");
+        }
+        bw.write("" + frac / visited.length);
+        bw.close();
+        } catch (Exception e) {
+            System.err.println("ye");
         }
 
         return sb.toString().replace("~", "&nbsp;"); // Replace any remaining ~ with &nbsp; (non-breaking spaces)
