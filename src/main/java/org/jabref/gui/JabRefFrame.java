9a8554c3a3748c27f8b57196732c51a1284f6136
Stefan Kolb <stefan-kolb@web.de>
2020-02-21 01:43:16 +0100

    Add divider

--- src/main/java/org/jabref/gui/JabRefFrame.java
+++ src/main/java/org/jabref/gui/JabRefFrame.java
@@ -767,6 +767,9 @@ public class JabRefFrame extends BorderPane {
         tools.getItems().addAll(
                 factory.createMenuItem(StandardActions.PARSE_TEX, new ParseTexAction(stateManager)),
                 factory.createMenuItem(StandardActions.NEW_SUB_LIBRARY_FROM_AUX, new NewSubLibraryAction(this, stateManager)),
+
+                new SeparatorMenuItem(),
+                
                 factory.createMenuItem(StandardActions.FIND_UNLINKED_FILES, new FindUnlinkedFilesAction(this, stateManager)),
                 factory.createMenuItem(StandardActions.WRITE_XMP, new OldDatabaseCommandWrapper(Actions.WRITE_XMP, this, stateManager)),
                 factory.createMenuItem(StandardActions.COPY_LINKED_FILES, new CopyFilesAction(stateManager, this.getDialogService())),
