f949a008725c7d885949ab5fe00b8ecd0a00cc75
David Stevens <dstevens@kth.se>
2020-02-25 16:54:50 +0100

    [fix] Rename menu item for searching ShortScience

--- src/main/java/org/jabref/gui/actions/StandardActions.java
+++ src/main/java/org/jabref/gui/actions/StandardActions.java
@@ -33,7 +33,7 @@ public enum StandardActions implements Action {
     SEND_AS_EMAIL(Localization.lang("Send as email"), IconTheme.JabRefIcons.EMAIL),
     OPEN_EXTERNAL_FILE(Localization.lang("Open file"), IconTheme.JabRefIcons.FILE, KeyBinding.OPEN_FILE),
     OPEN_URL(Localization.lang("Open URL or DOI"), IconTheme.JabRefIcons.WWW, KeyBinding.OPEN_URL_OR_DOI),
-    OPEN_SHORTSCIENCE(Localization.lang("Open ShortScience"), IconTheme.JabRefIcons.WWW),
+    OPEN_SHORTSCIENCE(Localization.lang("Search ShortScience")),
     MERGE_WITH_FETCHED_ENTRY(Localization.lang("Get BibTeX data from %0", "DOI/ISBN/...")),
     ATTACH_FILE(Localization.lang("Attach file"), IconTheme.JabRefIcons.ATTACH_FILE),
     PRIORITY(Localization.lang("Priority"), IconTheme.JabRefIcons.PRIORITY),
