5f11a5c4b27f4a34fba9ece9aef161c3ed3d47c0
August Janse <augustj@kth.se>
2020-02-27 12:40:16 +0100

    Parameterize ApsFetcherTest

--- src/test/java/org/jabref/logic/importer/fetcher/ApsFetcherTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/ApsFetcherTest.java
@@ -3,14 +3,16 @@ package org.jabref.logic.importer.fetcher;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.support.DisabledOnCIServer;
 import org.jabref.testutils.category.FetcherTest;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,49 +20,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ApsFetcherTest {
 
     private ApsFetcher finder;
-    private BibEntry entry;
 
     @BeforeEach
     void setUp() {
         finder = new ApsFetcher();
-        entry = new BibEntry();
     }
 
-    @Test
-    @DisabledOnCIServer("CI server is blocked")
-    void findOpenAccess() throws IOException {
-        entry.setField(StandardField.DOI, "10.1103/PhysRevLett.116.061102");
+    private static Stream<Arguments> provideBibEntriesWithDois() throws IOException {
+        // Standard DOI works
+        BibEntry easy = new BibEntry();
+        easy.setField(StandardField.DOI, "10.1103/PhysRevLett.116.061102");
+        Optional<URL> pdfUrl1 = Optional.of(new URL("https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.116.061102"));
 
-        assertEquals(
-                Optional.of(new URL("https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.116.061102")),
-                finder.findFullText(entry)
+        // DOI in lowercase works
+        BibEntry lowercase = new BibEntry();
+        lowercase.setField(StandardField.DOI, "10.1103/physrevlett.124.029002");
+        Optional<URL> pdfUrl2 = Optional.of(new URL("https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.124.029002"));
+
+        // Article behind paywall returns empty
+        BibEntry unauthorized = new BibEntry();
+        unauthorized.setField(StandardField.DOI, "10.1103/PhysRevLett.89.127401");
+
+        // Unavailable article returns empty
+        BibEntry notFoundByDoi = new BibEntry();
+        notFoundByDoi.setField(StandardField.DOI, "10.1016/j.aasri.2014.0559.002");
+
+        return Stream.of(
+                Arguments.of(pdfUrl1, easy),
+                Arguments.of(pdfUrl2, lowercase),
+                Arguments.of(Optional.empty(), unauthorized),
+                Arguments.of(Optional.empty(), notFoundByDoi)
         );
     }
 
-    @Test
-    @DisabledOnCIServer("CI server is blocked")
-    void caseInsensitive() throws IOException {
-        entry.setField(StandardField.DOI, "10.1103/physrevlett.124.029002");
-
-        assertEquals(
-                Optional.of(new URL("https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.124.029002")),
-                finder.findFullText(entry)
-        );
-    }
-
-    @Test
-    @DisabledOnCIServer("CI server is blocked")
-    void notAuthorized() throws IOException {
-        entry.setField(StandardField.DOI, "10.1103/PhysRevLett.89.127401");
-
-        assertEquals(Optional.empty(), finder.findFullText(entry));
-    }
-
-    @Test
-    @DisabledOnCIServer("CI server is blocked")
-    void notFoundByDOI() throws IOException {
-        entry.setField(StandardField.DOI, "10.1016/j.aasri.2014.0559.002");
-
-        assertEquals(Optional.empty(), finder.findFullText(entry));
+    @ParameterizedTest
+    @MethodSource("provideBibEntriesWithDois")
+    void shouldReturnFullTextUrlOrEmpty(Optional<URL> expected, BibEntry entry) throws IOException {
+        assertEquals(expected, finder.findFullText(entry));
     }
 }
