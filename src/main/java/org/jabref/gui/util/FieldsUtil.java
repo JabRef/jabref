6e252fe82a2d0a7ad74593fa840a8556daba12d0
Carl Christian Snethlage <cc.snethlage@gmail.com>
2020-02-27 11:57:34 +0100

    Added Localization for SpecialField

--- src/main/java/org/jabref/gui/util/FieldsUtil.java
+++ src/main/java/org/jabref/gui/util/FieldsUtil.java
@@ -10,6 +10,9 @@ import org.jabref.model.entry.field.InternalField;
 import org.jabref.model.entry.field.SpecialField;
 import org.jabref.model.entry.field.UnknownField;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FieldsUtil {
 
     public static StringConverter<Field> fieldStringConverter = new StringConverter<>() {
@@ -28,9 +31,33 @@ public class FieldsUtil {
         }
     };
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FieldsUtil.class);
+
     public static String getNameWithType(Field field) {
         if (field instanceof SpecialField) {
-            return field.getDisplayName() + " (" + Localization.lang("Special") + ")";
+
+            String fieldName;
+            SpecialField specialField = (SpecialField) field;
+
+            if (specialField == SpecialField.PRINTED) {
+                fieldName = Localization.lang("Printed");
+            } else if (specialField == SpecialField.QUALITY) {
+                fieldName = Localization.lang("Quality");
+            } else if (specialField == SpecialField.RANKING) {
+                fieldName = Localization.lang("Ranking");
+            } else if (specialField == SpecialField.PRIORITY) {
+                fieldName = Localization.lang("Priority");
+            } else if (specialField == SpecialField.RELEVANCE) {
+                fieldName = Localization.lang("Relevance");
+            } else if (specialField == SpecialField.READ_STATUS) {
+                fieldName = Localization.lang("Read status");
+            } else {
+                fieldName = Localization.lang("Unknown");
+                LOGGER.warn("Unknown special field '" + field.getName() + "'.");
+            }
+
+            return fieldName + " (" + Localization.lang("Special") + ")";
+
         } else if (field instanceof IEEEField) {
             return field.getDisplayName() + " (" + Localization.lang("IEEE") + ")";
         } else if (field instanceof InternalField) {
