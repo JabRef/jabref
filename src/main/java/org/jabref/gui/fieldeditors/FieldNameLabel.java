15d7b995c3f1b1a3ac5eefea1bd3f47d4138239a
ShikunXiong <shikun.xiong.work@gmail.com>
2020-03-06 00:19:58 -0800

    Update setTip() method in FieldNameLabel.java
    Change if statement to switch statement, which makes the code more understandable.

--- src/main/java/org/jabref/gui/fieldeditors/FieldNameLabel.java
+++ src/main/java/org/jabref/gui/fieldeditors/FieldNameLabel.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Tooltip;
 
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.field.Field;
+import org.jabref.model.entry.field.InternalField;
 import org.jabref.model.entry.field.StandardField;
 
 public class FieldNameLabel extends Label {
@@ -22,18 +23,34 @@ public class FieldNameLabel extends Label {
 
     public void setTip(Field field) {
         Tooltip tip = new Tooltip();
-        if (StandardField.AUTHOR.getName().equals(field.getName())) {
-            tip.setText(Localization.lang("The name(s) of the author(s), in the format described in the LATEX book. Remember, all names are separated with the and keyword, and not commas."));
-        } else if (StandardField.BIBTEXKEY.getName().equals(field.getName())) {
-            tip.setText(Localization.lang("[First author'last name][Article year] e.g. Jones2020"));
-        } else if (StandardField.JOURNAL.getName().equals(field.getName())) {
-            tip.setText(Localization.lang("Journal name. Abbrevations could be used."));
-        } else if (StandardField.TITLE.getName().equals(field.getName())) {
-            tip.setText(Localization.lang("The work's title"));
-        } else if (StandardField.YEAR.getName().equals(field.getName())) {
-            tip.setText(Localization.lang("The year of publication or, for an unpublished work, the year it was written. Generally it should consist of four numerals, such as 1984, although the standard styles can handle any year whose last four nonpunctuation characters are numerals, such as '(about 1984)'."));
-        } else {
-            return;
+        if (field.isStandardField()) {
+            StandardField standardField = (StandardField)field;
+            switch (standardField) {
+                case AUTHOR:
+                    tip.setText(Localization.lang("The name(s) of the author(s), in the format described in the LATEX book. Remember, all names are separated with the and keyword, and not commas."));
+                    break;
+                case JOURNAL:
+                    tip.setText(Localization.lang("Journal name. Abbrevations could be used."));
+                    break;
+                case TITLE:
+                    tip.setText(Localization.lang("The work's title"));
+                    break;
+                case YEAR:
+                    tip.setText(Localization.lang("The year of publication or, for an unpublished work, the year it was written. Generally it should consist of four numerals, such as 1984, although the standard styles can handle any year whose last four nonpunctuation characters are numerals, such as '(about 1984)'."));
+                    break;
+                default:
+                    return;
+            }
+        }
+        else if (field instanceof InternalField) {
+            InternalField InternalField = (InternalField) field;
+            switch (InternalField) {
+                case KEY_FIELD:
+                    tip.setText(Localization.lang("[First author'last name][Article year] e.g. Jones2020"));
+                    break;
+                default:
+                    return;
+            }
         }
         this.setTooltip(tip);
     }
