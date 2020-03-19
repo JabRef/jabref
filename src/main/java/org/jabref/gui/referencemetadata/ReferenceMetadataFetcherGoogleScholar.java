6c69fcac934566e769afe1770b5b46ab79bdf206
systemoperator <3658393+systemoperator@users.noreply.github.com>
2020-03-19 12:12:04 +0100

    IncompleteItem converted to private, inner class

--- src/main/java/org/jabref/gui/referencemetadata/ReferenceMetadataFetcherGoogleScholar.java
+++ src/main/java/org/jabref/gui/referencemetadata/ReferenceMetadataFetcherGoogleScholar.java
@@ -54,6 +54,42 @@ public class ReferenceMetadataFetcherGoogleScholar {
 
     private final ObservableList<BibEntry> entriesWithIncompleteMetadata = FXCollections.observableArrayList(); // this list contains all entries with still incomplete metadata
 
+    private class IncompleteItem {
+        String key;
+        String title;
+        JsonArray authors;
+
+        public IncompleteItem(String key, String title, JsonArray authors) {
+            this.key = key;
+            this.title = title;
+            this.authors = authors;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public JsonArray getAuthors() {
+            return authors;
+        }
+
+        public void setAuthors(JsonArray authors) {
+            this.authors = authors;
+        }
+    }
+
     /**
      * fetches reference metadata for the given entries
      *
