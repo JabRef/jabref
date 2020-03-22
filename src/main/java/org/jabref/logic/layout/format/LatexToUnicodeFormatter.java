5be8b513e394cb1e77504182546522baa2415072
Oliver Kopp <kopp.dev@gmail.com>
2020-03-22 17:17:51 +0100

    WIP

--- src/main/java/org/jabref/logic/layout/format/LatexToUnicodeFormatter.java
+++ src/main/java/org/jabref/logic/layout/format/LatexToUnicodeFormatter.java
@@ -1,9 +1,12 @@
 package org.jabref.logic.layout.format;
 
+import java.util.Map;
+import java.util.Objects;
+
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.layout.LayoutFormatter;
+import org.jabref.logic.util.strings.HTMLUnicodeConversionMaps;
 import org.jabref.model.cleanup.Formatter;
-import org.jabref.model.strings.LatexToUnicodeAdapter;
 
 /**
  * This formatter converts LaTeX character sequences their equivalent unicode characters,
@@ -22,8 +25,20 @@ public class LatexToUnicodeFormatter extends Formatter implements LayoutFormatte
     }
 
     @Override
-    public String format(String inField) {
-        return LatexToUnicodeAdapter.format(inField);
+    public String format(String text) {
+        String result = Objects.requireNonNull(text);
+
+        if (result.isEmpty()) {
+            return result;
+        }
+
+        // Standard symbols
+        for (Map.Entry<String, String> unicodeLatexPair : HTMLUnicodeConversionMaps.UNICODE_LATEX_CONVERSION_MAP
+                .entrySet()) {
+            result = result.replace(unicodeLatexPair.getValue(), unicodeLatexPair.getKey());
+        }
+
+        return result;
     }
 
     @Override
