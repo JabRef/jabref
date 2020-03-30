834c65f971b709059b67cf24cab876292c1b8ead
Gena928 <GennadiyStakhovskiy@gmail.com>
2020-03-30 22:20:35 +0300

    Init changes

--- src/main/java/org/jabref/gui/groups/GroupTreeView.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeView.java
@@ -44,6 +44,7 @@ import org.jabref.gui.util.ViewModelTreeTableCellFactory;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.groups.AllEntriesGroup;
+import org.jabref.preferences.JabRefPreferences;
 import org.jabref.preferences.PreferencesService;
 
 import org.controlsfx.control.textfield.CustomTextField;
@@ -71,6 +72,7 @@ public class GroupTreeView {
 
     private GroupTreeViewModel viewModel;
     private CustomLocalDragboard localDragboard;
+    private JabRefPreferences preferences;
 
     private DragExpansionHandler dragExpansionHandler;
 
@@ -78,6 +80,7 @@ public class GroupTreeView {
     public void initialize() {
         this.localDragboard = stateManager.getLocalDragboard();
         viewModel = new GroupTreeViewModel(stateManager, dialogService, preferencesService, taskExecutor, localDragboard);
+        preferences = JabRefPreferences.getInstance();
 
         // Set-up groups tree
         groupTree.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -120,13 +123,13 @@ public class GroupTreeView {
                 .withTooltip(GroupNodeViewModel::getDescription)
                 .install(mainColumn);
 
-        // Number of hits
+        // Number of hits (only if user wants to see them)
         PseudoClass anySelected = PseudoClass.getPseudoClass("any-selected");
         PseudoClass allSelected = PseudoClass.getPseudoClass("all-selected");
         new ViewModelTreeTableCellFactory<GroupNodeViewModel>()
                 .withGraphic(group -> {
                     final StackPane node = new StackPane();
-                    node.getStyleClass().setAll("hits");
+
                     if (!group.isRoot()) {
                         BindingsHelper.includePseudoClassWhen(node, anySelected,
                                 group.anySelectedEntriesMatchedProperty());
@@ -134,7 +137,10 @@ public class GroupTreeView {
                                 group.allSelectedEntriesMatchedProperty());
                     }
                     Text text = new Text();
-                    text.textProperty().bind(group.getHits().asString());
+                    if (preferences.getBoolean(JabRefPreferences.DISPLAY_GROUP_QUANTITY)) {
+                        node.getStyleClass().setAll("hits");
+                        text.textProperty().bind(group.getHits().asString());
+                    }
                     text.getStyleClass().setAll("text");
                     node.getChildren().add(text);
                     node.setMaxWidth(Control.USE_PREF_SIZE);
--- src/main/java/org/jabref/gui/preferences/GroupsTabView.java
+++ src/main/java/org/jabref/gui/preferences/GroupsTabView.java
@@ -16,8 +16,10 @@ public class GroupsTabView extends AbstractPreferenceTabView<GroupsTabViewModel>
     @FXML private RadioButton groupViewModeIntersection;
     @FXML private RadioButton groupViewModeUnion;
     @FXML private CheckBox autoAssignGroup;
+    @FXML private CheckBox displayGroupQuantity;
     @FXML private TextField defaultGroupingField;
     @FXML private TextField keywordSeparator;
+
 
     public GroupsTabView(JabRefPreferences preferences) {
         this.preferences = preferences;
@@ -37,6 +39,7 @@ public class GroupsTabView extends AbstractPreferenceTabView<GroupsTabViewModel>
         groupViewModeIntersection.selectedProperty().bindBidirectional(viewModel.groupViewModeIntersectionProperty());
         groupViewModeUnion.selectedProperty().bindBidirectional(viewModel.groupViewModeUnionProperty());
         autoAssignGroup.selectedProperty().bindBidirectional(viewModel.autoAssignGroupProperty());
+        displayGroupQuantity.selectedProperty().bindBidirectional(viewModel.displayGroupQuantity());
         defaultGroupingField.textProperty().bindBidirectional(viewModel.defaultGroupingFieldProperty());
         keywordSeparator.textProperty().bindBidirectional(viewModel.keywordSeparatorProperty());
     }
