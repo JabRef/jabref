0ad24f675d0354c7379468c2364b9dce2408ee0c
Carl Christian Snethlage <cc.snethlage@gmail.com>
2020-04-13 18:29:50 +0200

    Fixed recalculation of selected entry

--- src/main/java/org/jabref/gui/actions/ActionHelper.java
+++ src/main/java/org/jabref/gui/actions/ActionHelper.java
@@ -1,11 +1,12 @@
 package org.jabref.gui.actions;
 
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanExpression;
-import javafx.collections.ObservableList;
 
 import org.jabref.gui.StateManager;
 import org.jabref.model.entry.BibEntry;
@@ -14,8 +15,6 @@ import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.util.FileHelper;
 import org.jabref.preferences.PreferencesService;
-
-import org.fxmisc.easybind.EasyBind;
 
 public class ActionHelper {
     public static BooleanExpression needsDatabase(StateManager stateManager) {
@@ -45,16 +44,18 @@ public class ActionHelper {
     }
 
     public static BooleanExpression isFilePresentForSelectedEntry(StateManager stateManager, PreferencesService preferencesService) {
-        ObservableList<LinkedFile> files = EasyBind.map(stateManager.getSelectedEntries(), BibEntry::getFiles).get(0);
         return Bindings.createBooleanBinding(() -> {
-            if ((files.size() > 0) && stateManager.getActiveDatabase().isPresent()) {
-                return FileHelper.expandFilename(
-                        stateManager.getActiveDatabase().get(),
-                        files.get(0).getLink(),
-                        preferencesService.getFilePreferences()).isPresent();
-            } else {
-                return false;
-            }
-        }, stateManager.getSelectedEntries().get(0).getFieldBinding(StandardField.FILE));
+                    List<LinkedFile> files = stateManager.getSelectedEntries().get(0).getFiles();
+                    if ((files.size() > 0) && stateManager.getActiveDatabase().isPresent()) {
+                        Optional<Path> filename = FileHelper.expandFilename(
+                                stateManager.getActiveDatabase().get(),
+                                files.get(0).getLink(),
+                                preferencesService.getFilePreferences());
+                        return filename.isPresent();
+                    } else {
+                        return false;
+                    }
+                }, stateManager.getSelectedEntries(),
+                stateManager.getSelectedEntries().get(0).getFieldBinding(StandardField.FILE));
     }
 }
