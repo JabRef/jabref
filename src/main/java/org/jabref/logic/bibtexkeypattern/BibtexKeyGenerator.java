ae8b18394df222ced31150d5c32f4d8ab5220524
Grzegorz <grzegorz.popiel@yahoo.com>
2020-04-16 22:06:29 +0200

    Remove dash from illegal characters. Fix #6295 #6257 #6252

--- src/main/java/org/jabref/logic/bibtexkeypattern/BibtexKeyGenerator.java
+++ src/main/java/org/jabref/logic/bibtexkeypattern/BibtexKeyGenerator.java
@@ -27,7 +27,7 @@ public class BibtexKeyGenerator extends BracketedPattern {
      */
     public static final String APPENDIX_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
     private static final Logger LOGGER = LoggerFactory.getLogger(BibtexKeyGenerator.class);
-    private static final String KEY_ILLEGAL_CHARACTERS = "{}(),\\\"-#~^:'`ʹ";
+    private static final String KEY_ILLEGAL_CHARACTERS = "{}(),\\\"#~^:'`ʹ";
     private static final String KEY_UNWANTED_CHARACTERS = "{}(),\\\"-";
     private final AbstractBibtexKeyPattern citeKeyPattern;
     private final BibDatabase database;
