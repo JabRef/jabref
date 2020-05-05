7eaf7d27d32f5c711f6527ff215fba57d31d12ee
Mootez <mootez.saad@medtech.tn>
2020-05-05 17:12:17 +0100

    Add comparison for numeric values in custom entries

--- src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
+++ src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
@@ -9,8 +9,8 @@ public class NumericFieldComparator implements Comparator<String> {
 
     @Override
     public int compare(String val1, String val2) {
-        if (!val1.isEmpty()) {
-            if (!val2.isEmpty()) {
+        if (val1!=null) {
+            if (val2!=null) {
                 if (val1.matches("^[0-9]+$") && val2.matches("^[0-9]+$")) {
                     return Integer.parseInt(val1) - Integer.parseInt(val2);
                 } else if (!val1.matches("^[0-9]+$") && val2.matches("^[0-9]+$")) {
@@ -24,12 +24,36 @@ public class NumericFieldComparator implements Comparator<String> {
                 return 1;
             }
         } else {
-            if (!val2.isEmpty()) {
+            if (val2!=null) {
                 return -1;
             } else {
                 return 0;
             }
         }
     }
+    /*
+    * The purpose of this class is to add the numeric comparison, because values are sorted
+    * as if they were strings.
+    * // We start by implementing the comparison in the edge cases (if one of the values is null)
+    * if (val1 == null && val2 == null) return 0;
+    * if (val1 == null) return -1; // (we assume that "null" is "less than" any other value)
+    * if (val2 == null) return 1;
+    *
+    * // Now we start the conversion to integers.
+    * Integer valInt1 = null;
+    * Integer valInt2 = null;
+    * try {
+    *   valInt1 = Integer.parseInt(val1.trim()); // In case the user added an unnecessary white space (e.g. 1 1 instead of 11)
+    *   valInt2 = Integer.parseInt(val2.trim());
+    * } catch(NumberFormatException ignore) {} // do nothing
+    *
+    * if (valInt1 == null && valInt2 == null) return val1.compareTo(val2); // None of the values were parsed (i.e both are not numeric)
+    *                                                                     // so we will use the normal string comparison.
+    * if (valInt1 == null) return -1; // We assume that strings "are less" than integers
+    * if (valInt2 == null) return 1;
+    *
+    * // If we arrive at this stage then both values are actually numeric !
+    * return valInt1-valInt2;
+     * */
 
 }
