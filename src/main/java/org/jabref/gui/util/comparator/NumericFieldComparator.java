7d0d9ff840d3045a4671677a4a92cdd0c628878a
dimitra-karadima <t8170038@aueb.gr>
2020-05-05 18:20:45 +0300

    Change Comparator class
    Change in order for the Comparator class to wrap String and not Optional<SpecialFieldValueViewModel>

--- src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
+++ src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
@@ -1,35 +1,30 @@
 package org.jabref.gui.util.comparator;
 
 import java.util.Comparator;
-import java.util.Optional;
-
-import org.jabref.gui.specialfields.SpecialFieldValueViewModel;
 
 /**
  * Comparator for numeric cases.
  */
-public class NumericFieldComparator implements Comparator<Optional<SpecialFieldValueViewModel>> {
+public class NumericFieldComparator implements Comparator<String> {
 
     @Override
-    public int compare(Optional<SpecialFieldValueViewModel> val1, Optional<SpecialFieldValueViewModel> val2) {
-        if (val1.isPresent()) {
-            if (val2.isPresent()) {
-                String str1 = String.valueOf(val1);
-                String str2 = String.valueOf(val2);
-                if (str1.matches("^[0-9]+$") && str2.matches("^[0-9]+$")) {
-                    return Integer.parseInt(str1) - Integer.parseInt(str2);
-                } else if (!str1.matches("^[0-9]+$") && str2.matches("^[0-9]+$")) {
+    public int compare(String val1, String val2) {
+        if (!val1.isEmpty()) {
+            if (!val2.isEmpty()) {
+                if (val1.matches("^[0-9]+$") && val2.matches("^[0-9]+$")) {
+                    return Integer.parseInt(val1) - Integer.parseInt(val2);
+                } else if (!val1.matches("^[0-9]+$") && val2.matches("^[0-9]+$")) {
                     return -1;
-                } else if (str1.matches("^[0-9]+$") && !str2.matches("^[0-9]+$")) {
+                } else if (val1.matches("^[0-9]+$") && !val2.matches("^[0-9]+$")) {
                     return 0;
                 } else {
-                    return val1.get().getValue().compareTo(val2.get().getValue());
+                    return val1.compareTo(val2);
                 }
             } else {
                 return 1;
             }
         } else {
-            if (val2.isPresent()) {
+            if (!val2.isEmpty()) {
                 return -1;
             } else {
                 return 0;
