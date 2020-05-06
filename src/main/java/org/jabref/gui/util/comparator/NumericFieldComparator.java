bd64f8903745a1a0e2ed2076dd2bb82538c0eba9
Mootez <mootez.saad@medtech.tn>
2020-05-06 11:30:34 +0100

    Fix CheckStyle styling

--- src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
+++ src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
@@ -15,26 +15,26 @@ public class NumericFieldComparator implements Comparator<String> {
          * */
 
         // We start by implementing the comparison in the edge cases (if one of the values is null)
-        if (val1 == null && val2 == null) return 0;
-        if (val1 == null) return -1; // (we assume that "null" is "less than" any other value)
-        if (val2 == null) return 1;
+        if (val1 == null && val2 == null) { return 0; }
+        if (val1 == null) { return -1; } // (we assume that "null" is "less than" any other value)
+        if (val2 == null) { return 1; }
 
         // Now we start the conversion to integers.
         Integer valInt1 = null;
         Integer valInt2 = null;
         try {
             valInt1 = Integer.parseInt(val1.trim()); // In case the user added an unnecessary white space (e.g. 1 1 instead of 11)
-        } catch(NumberFormatException ignore) {} // do nothing
+        } catch (NumberFormatException ignore) { /* do nothing */ }
         try {
             valInt2 = Integer.parseInt(val2.trim());
-        } catch(NumberFormatException ignore) {}
-        if (valInt1 == null && valInt2 == null) return val1.compareTo(val2); // None of the values were parsed (i.e both are not numeric)
+        } catch (NumberFormatException ignore) { /* do nothing */ }
+        if (valInt1 == null && valInt2 == null) { return val1.compareTo(val2); } // None of the values were parsed (i.e both are not numeric)
         // so we will use the normal string comparison.
-        if (valInt1 == null) return -1; // We assume that strings "are less" than integers
-        if (valInt2 == null) return 1;
+        if (valInt1 == null) { return -1; } // We assume that strings "are less" than integers
+        if (valInt2 == null) { return 1; }
 
         // If we arrive at this stage then both values are actually numeric !
-        return valInt1-valInt2;
+        return valInt1 - valInt2;
     }
 
 
