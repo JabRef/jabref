a3eff0c49c0c63c626f2a6a324b4459000fc7fca
Siedlerchr <siedlerkiller@gmail.com>
2020-05-08 21:06:54 +0200

    Refactor

--- src/main/java/org/jabref/gui/actions/ActionHelper.java
+++ src/main/java/org/jabref/gui/actions/ActionHelper.java
@@ -7,21 +7,21 @@ import java.util.Optional;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanExpression;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 
 import org.jabref.gui.StateManager;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.LinkedFile;
 import org.jabref.model.entry.field.Field;
-import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.util.FileHelper;
 import org.jabref.preferences.PreferencesService;
 
 import org.fxmisc.easybind.EasyBind;
 import org.fxmisc.easybind.monadic.MonadicBinding;
+import org.fxmisc.easybind.monadic.MonadicObservableValue;
 
 public class ActionHelper {
+
     public static BooleanExpression needsDatabase(StateManager stateManager) {
         return stateManager.activeDatabaseProperty().isPresent();
     }
@@ -31,9 +31,8 @@ public class ActionHelper {
     }
 
     public static BooleanExpression needsEntriesSelected(int numberOfEntries, StateManager stateManager) {
-        return Bindings.createBooleanBinding(
-                () -> stateManager.getSelectedEntries().size() == numberOfEntries,
-                stateManager.getSelectedEntries());
+        return Bindings.createBooleanBinding(() -> stateManager.getSelectedEntries().size() == numberOfEntries,
+                                             stateManager.getSelectedEntries());
     }
 
     public static BooleanExpression isFieldSetForSelectedEntry(Field field, StateManager stateManager) {
@@ -51,22 +50,23 @@ public class ActionHelper {
     }
 
     public static BooleanExpression isFilePresentForSelectedEntry(StateManager stateManager, PreferencesService preferencesService) {
-        return Bindings.createBooleanBinding(() -> {
 
-                    if (stateManager.getSelectedEntries().isEmpty()) {
-                        return false;
-                    }
-                    List<LinkedFile> files = stateManager.getSelectedEntries().get(0).getFiles();
-                    if ((files.size() > 0) && stateManager.getActiveDatabase().isPresent()) {
-                        Optional<Path> filename = FileHelper.find(
-                                stateManager.getActiveDatabase().get(),
-                                files.get(0).getLink(),
-                                preferencesService.getFilePreferences());
-                        return filename.isPresent();
-                    } else {
-                        return false;
-                    }
-                }, stateManager.getSelectedEntries(),
-                stateManager.getSelectedEntries().isEmpty() ? new SimpleStringProperty("") : stateManager.getSelectedEntries().get(0).getFieldBinding(StandardField.FILE));
+        ObservableList<BibEntry> selectedEntries = stateManager.getSelectedEntries();
+        MonadicObservableValue<Boolean> fileIsPresent = EasyBind.monadic(Bindings.valueAt(selectedEntries, 0)).map(entry -> {
+            List<LinkedFile> files = entry.getFiles();
+
+            if ((entry.getFiles().size() > 0) && stateManager.getActiveDatabase().isPresent()) {
+                Optional<Path> filename = FileHelper.find(
+                                                          stateManager.getActiveDatabase().get(),
+                                                          files.get(0).getLink(),
+                                                          preferencesService.getFilePreferences());
+                return filename.isPresent();
+            } else {
+                return false;
+            }
+
+        }).orElse(false);
+
+        return BooleanExpression.booleanExpression(fileIsPresent);
     }
 }
