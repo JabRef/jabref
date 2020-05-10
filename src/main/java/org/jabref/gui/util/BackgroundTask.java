d7442cc2247772b6e54bcb68cea947a5a2a604bc
Benedikt Tutzer <benedikt.tutzer@gmail.com>
2020-05-10 16:10:04 +0200

    Registered the save task as background task
    This makes the dialog that pops up if background tasks are running wait
    for pending saves.

--- src/main/java/org/jabref/gui/util/BackgroundTask.java
+++ src/main/java/org/jabref/gui/util/BackgroundTask.java
@@ -35,7 +35,8 @@ import org.jabref.logic.l10n.Localization;
 public abstract class BackgroundTask<V> {
 
     public static ImmutableMap<String, Node> iconMap = ImmutableMap.of(
-            Localization.lang("Downloading"), IconTheme.JabRefIcons.DOWNLOAD.getGraphicNode()
+            Localization.lang("Downloading"), IconTheme.JabRefIcons.DOWNLOAD.getGraphicNode(),
+            Localization.lang("Saving"), IconTheme.JabRefIcons.SAVE_ALL.getGraphicNode()
     );
 
     public static Callback<Task<?>, Node> iconCallback = task -> {
