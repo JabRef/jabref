eaebdf53011881f70e4b81601f3140cf3f3c84de
Jonas Moosmann <j.moosmann@gmx.de>
2020-05-15 10:22:46 +0200

    remove unused import

--- src/main/java/org/jabref/logic/integrity/IntegrityCheck.java
+++ src/main/java/org/jabref/logic/integrity/IntegrityCheck.java
@@ -1,7 +1,6 @@
 package org.jabref.logic.integrity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.jabref.logic.bibtexkeypattern.BibtexKeyPatternPreferences;
