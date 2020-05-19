3045da14e7eabfe720dbf5e7b7c5cfde7277d4d5
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2020-05-19 20:24:56 +0200

    Add Test for CompositeSearchBasedFetcher.
    Signed-off-by: DominikVoigt <dominik.ingo.voigt@gmail.com>

---
+++ src/test/java/org/jabref/logic/importer/fetcher/CompositeSearchBasedFetcherTest.java
@@ -0,0 +1,49 @@
+package org.jabref.logic.importer.fetcher;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.importer.SearchBasedFetcher;
+import org.jabref.logic.importer.SearchBasedParserFetcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class CompositeSearchBasedFetcherTest {
+
+    private CompositeSearchBasedFetcher finder;
+    private SearchBasedFetcher searchBasedFetcher;
+    private SearchBasedParserFetcher searchBasedParserFetcher;
+
+    @BeforeEach
+    public void setUp() {
+        ImportFormatPreferences importFormatPreferences = mock(ImportFormatPreferences.class);
+
+        Set<SearchBasedFetcher> setOfSearchBasedFetchers = new HashSet<>();
+        // Add one searchBasedFetcher and one searchBasedParserFetcher
+        searchBasedFetcher = new ArXiv(importFormatPreferences);
+        searchBasedParserFetcher = new CiteSeer();
+        setOfSearchBasedFetchers.add(searchBasedFetcher);
+        setOfSearchBasedFetchers.add(searchBasedParserFetcher);
+        finder = new CompositeSearchBasedFetcher(setOfSearchBasedFetchers);
+    }
+
+    @Test
+    public void performSearchOnEmptyQuery() {
+        Assertions.assertTrue(finder.performSearch("").isEmpty());
+    }
+
+    @Test
+    public void performSearchContainsResultsOfSearchBasedFetcher() throws FetcherException {
+        Assertions.assertTrue(finder.performSearch("quantum").containsAll(searchBasedFetcher.performSearch("quantum")));
+    }
+
+    @Test
+    public void performSearchContainsResultsOfSearchBasedParserFetcher() throws FetcherException {
+        Assertions.assertTrue(finder.performSearch("quantum").containsAll(searchBasedFetcher.performSearch("quantum")));
+    }
+}
