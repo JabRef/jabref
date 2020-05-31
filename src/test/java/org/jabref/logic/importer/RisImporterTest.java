953e028ccac86bc38381e02565d9164c2cbedf93
braincident <569120128@qq.com>
2020-05-31 12:02:36 +0800

    Update RisImporterTest.java
    Make changes following checkstyle rules.

--- src/test/java/org/jabref/logic/importer/RisImporterTest.java
+++ src/test/java/org/jabref/logic/importer/RisImporterTest.java
@@ -1,50 +1,20 @@
 package org.jabref.logic.importer;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.jabref.logic.importer.fileformat.RisImporter;
+import org.jabref.model.entry.field.Field;
+import org.jabref.model.entry.field.StandardField;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.time.Year;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
-import java.util.regex.Pattern;
-
-import org.jabref.logic.importer.Importer;
-import org.jabref.logic.importer.ParserResult;
-import org.jabref.logic.importer.fileformat.RisImporter;
-import org.jabref.logic.util.OS;
-import org.jabref.logic.util.StandardFileType;
-import org.jabref.model.entry.AuthorList;
-import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.Month;
-import org.jabref.model.entry.field.Field;
-import org.jabref.model.entry.field.StandardField;
-import org.jabref.model.entry.field.UnknownField;
-import org.jabref.model.entry.types.EntryType;
-import org.jabref.model.entry.types.IEEETranEntryType;
-import org.jabref.model.entry.types.StandardEntryType;
-
-/**
- * RisImporter Tester.
- *
- * @author <Authors name>
- * @version 1.0
- * @since <pre>05/31/2020</pre>
- */
 public class RisImporterTest {
     RisImporter ri = null;
 
@@ -53,9 +23,6 @@ public class RisImporterTest {
         ri = new RisImporter();
     }
 
-    /**
-     * Method: addDoi(Map<Field, String> hm, String val)
-     */
     @Test
     public void testAddDoi1() throws Exception {
         String testDoi = "10.1589/jpts.28.186";
