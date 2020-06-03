0fd46d711eba1028aaad45119945e46cd252fef8
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2020-06-03 15:19:31 -0400

    Refactor name of tests for AuthorListParser

--- src/test/java/org/jabref/model/entry/AuthorListParserTest.java
+++ src/test/java/org/jabref/model/entry/AuthorListParserTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class AuthorListParameterTest {
+class AuthorListParserTest {
 
     private static Stream<Arguments> data() {
         return Stream.of(
