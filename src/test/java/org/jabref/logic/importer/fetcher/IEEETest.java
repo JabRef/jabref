db19e60100f253a9a5654254d9be0d12a98e1c82
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2020-07-15 13:22:15 +0200

    Adapt test of getAdvancedURL
    Signed-off-by: Dominik Voigt <dominik.ingo.voigt@gmail.com>

--- src/test/java/org/jabref/logic/importer/fetcher/IEEETest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/IEEETest.java
@@ -125,13 +125,13 @@ class IEEETest {
     }
 
     @Test
-    void getAdvancedURLDownload() {
+    void getAdvancedURL() {
         String expectedURLEncoding = "https://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=s6kcbenkaxdu55c8k8983pw9&author=%28Igor+Steinmacher+AND+Tayana+Uchoa+Conte%29&article_title=%22Overcoming+Open+Source+Project+Entry+Barriers%22";
         ComplexSearchQuery.AdvancedSearchConfigBuilder builder = ComplexSearchQuery.builder();
         builder.author("(Igor Steinmacher AND Tayana Uchoa Conte)");
         builder.title("\"Overcoming Open Source Project Entry Barriers\"");
 
-        String actualURLEncoding = fetcher.getComplexQueryURL(builder.build()).getSource().toString();
+        String actualURLEncoding = fetcher.getComplexQueryURL(builder.build()).toString();
 
         assertEquals(expectedURLEncoding, actualURLEncoding);
     }
