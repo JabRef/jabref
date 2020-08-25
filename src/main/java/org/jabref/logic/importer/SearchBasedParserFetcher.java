404251ab7a945348e9dcd04fdff6814e6426c094
Oliver Kopp <kopp.dev@gmail.com>
2020-08-25 20:23:14 +0200

    Address commetn ins SearchBasedParserFetcher

--- src/main/java/org/jabref/logic/importer/SearchBasedParserFetcher.java
+++ src/main/java/org/jabref/logic/importer/SearchBasedParserFetcher.java
@@ -17,9 +17,11 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Provides a convenient interface for search-based fetcher, which follow the usual three-step procedure:
- * 1. Open a URL based on the search query
- * 2. Parse the response to get a list of {@link BibEntry}
- * 3. Post-process fetched entries
+ * <ol>
+ *     <li>Open a URL based on the search query</li>
+ *     <li>Parse the response to get a list of {@link BibEntry}</li>
+ *     <li>Post-process fetched entries</li>
+ * </ol>
  */
 public interface SearchBasedParserFetcher extends SearchBasedFetcher {
 
@@ -35,6 +37,23 @@ public interface SearchBasedParserFetcher extends SearchBasedFetcher {
      */
     Parser getParser();
 
+    /**
+     * Fetch the entries from the given URL. This method is used by {@link #performSearch(String)} and {@link #performComplexSearch(ComplexSearchQuery)}
+     */
+    default List<BibEntry> getBibEntries(URL urlForQuery) throws FetcherException {
+        LoggerFactory.getLogger(this.getClass()).debug("Using query URL {}", urlForQuery.toString());
+        try (InputStream stream = getUrlDownload(urlForQuery).asInputStream()) {
+            List<BibEntry> fetchedEntries = getParser().parseEntries(stream);
+            fetchedEntries.forEach(this::doPostCleanup);
+            return fetchedEntries;
+        } catch (IOException e) {
+            // TODO: Catch HTTP Response 401/403 errors and report that user has no rights to access resource
+            throw new FetcherException(String.format("A network error occurred when fetching from %s", urlForQuery.toString()), e);
+        } catch (ParseException e) {
+            throw new FetcherException(String.format("An internal parser error occurred when fetching from %s", urlForQuery.toString()), e);
+        }
+    }
+
     @Override
     default List<BibEntry> performSearch(String query) throws FetcherException {
         if (StringUtil.isBlank(query)) {
@@ -44,24 +63,11 @@ public interface SearchBasedParserFetcher extends SearchBasedFetcher {
         URL urlForQuery;
         try {
             urlForQuery = getURLForQuery(query);
-        } catch (URISyntaxException | MalformedURLException e) {
-            LoggerFactory.getLogger(this.getClass()).info("Search URL {} is malformed", query);
-            throw new FetcherException("Search URI is malformed", e);
+        } catch (URISyntaxException | MalformedURLException | FetcherException e) {
+            throw new FetcherException(String.format("Search URI %s is malformed", query), e);
         }
-        try (InputStream stream = getUrlDownload(urlForQuery).asInputStream()) {
-            List<BibEntry> fetchedEntries = getParser().parseEntries(stream);
 
-            // Post-cleanup
-            fetchedEntries.forEach(this::doPostCleanup);
-
-            return fetchedEntries;
-        } catch (IOException e) {
-            LoggerFactory.getLogger(this.getClass()).info("IOException at URL {}", urlForQuery.toString());
-            // TODO: Catch HTTP Response 401/403 errors and report that user has no rights to access resource
-            throw new FetcherException("A network error occurred", e);
-        } catch (ParseException e) {
-            throw new FetcherException("An internal parser error occurred", e);
-        }
+        return getBibEntries(urlForQuery);
     }
 
     /**
@@ -73,23 +79,13 @@ public interface SearchBasedParserFetcher extends SearchBasedFetcher {
      */
     @Override
     default List<BibEntry> performComplexSearch(ComplexSearchQuery complexSearchQuery) throws FetcherException {
-        URL complexQueryURL = null;
+        URL complexQueryURL;
         try {
             complexQueryURL = getComplexQueryURL(complexSearchQuery);
-        } catch (URISyntaxException | MalformedURLException e) {
+        } catch (URISyntaxException | MalformedURLException | FetcherException e) {
             throw new FetcherException("Search URI is malformed", e);
         }
-        LoggerFactory.getLogger(this.getClass()).debug("Using query URL {}", complexQueryURL.toString());
-        try (InputStream stream = getUrlDownload(complexQueryURL).asInputStream()) {
-            List<BibEntry> fetchedEntries = getParser().parseEntries(stream);
-            fetchedEntries.forEach(this::doPostCleanup);
-            return fetchedEntries;
-        } catch (IOException e) {
-            // TODO: Catch HTTP Response 401/403 errors and report that user has no rights to access resource
-            throw new FetcherException("A network error occurred", e);
-        } catch (ParseException e) {
-            throw new FetcherException("An internal parser error occurred", e);
-        }
+        return getBibEntries(complexQueryURL);
     }
 
     default URL getComplexQueryURL(ComplexSearchQuery complexSearchQuery) throws URISyntaxException, MalformedURLException, FetcherException {
@@ -99,15 +95,16 @@ public interface SearchBasedParserFetcher extends SearchBasedFetcher {
 
     /**
      * Performs a cleanup of the fetched entry.
-     *
+     * <p>
      * Only systematic errors of the fetcher should be corrected here
      * (i.e. if information is consistently contained in the wrong field or the wrong format)
      * but not cosmetic issues which may depend on the user's taste (for example, LateX code vs HTML in the abstract).
-     *
+     * <p>
      * Try to reuse existing {@link Formatter} for the cleanup. For example,
      * {@code new FieldFormatterCleanup(StandardField.TITLE, new RemoveBracesFormatter()).cleanup(entry);}
-     *
+     * <p>
      * By default, no cleanup is done.
+     *
      * @param entry the entry to be cleaned-up
      */
     default void doPostCleanup(BibEntry entry) {
