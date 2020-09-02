b9dac8b4bd0c1edc46ecbd33b7ce3436fe8ad15b
Rupert Moran-Adcock <rupert.moran.adcock@gmail.com>
2020-09-03 11:25:23 +1200

    Add parser class

---
+++ src/main/java/org/jabref/jabmap/BibTeXNodeParser.java
@@ -0,0 +1,9 @@
+package org.jabref.jabmap;
+
+/**
+ * This class handles converting data from BibTeX format, which is stored in the database, to JSON to be sent to JapMap
+ * It does the reverse, converting JSON to BibTeX to be stored.
+ */
+public class BibTeXNodeParser {
+
+}
