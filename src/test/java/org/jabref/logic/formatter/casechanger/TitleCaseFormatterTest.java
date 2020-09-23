856c4a7a68d0be38f9f3b519415e14ccc4fa5160
tmrd993 <tmerd@hotmail.de>
2020-09-23 13:05:50 +0200

    fix checkstyle issues

--- src/test/java/org/jabref/logic/formatter/casechanger/TitleCaseFormatterTest.java
+++ src/test/java/org/jabref/logic/formatter/casechanger/TitleCaseFormatterTest.java
@@ -2,8 +2,8 @@ package org.jabref.logic.formatter.casechanger;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
