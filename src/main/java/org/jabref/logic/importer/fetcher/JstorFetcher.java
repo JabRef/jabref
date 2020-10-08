9051e1ef2a471cc57690e716bc309a5de725809e
Johannes Theiner <j.theiner@live.de>
2020-10-08 16:05:30 +0200

    Working Fetcher for jstor.org

--- src/main/java/org/jabref/logic/importer/fetcher/JstorFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/JstorFetcher.java
@@ -1,26 +1,34 @@
 package org.jabref.logic.importer.fetcher;
 
+import com.google.common.base.Charsets;
+import com.microsoft.applicationinsights.core.dependencies.apachecommons.io.IOUtils;
 import org.apache.http.client.utils.URIBuilder;
-import org.jabref.logic.importer.FetcherException;
-import org.jabref.logic.importer.ImportFormatPreferences;
-import org.jabref.logic.importer.Parser;
-import org.jabref.logic.importer.SearchBasedParserFetcher;
-import org.jabref.logic.importer.fileformat.JstorParser;
+import org.jabref.logic.importer.*;
+import org.jabref.logic.importer.fileformat.BibtexParser;
+import org.jabref.logic.util.OS;
+import org.jabref.model.entry.BibEntry;
 import org.jabref.model.util.DummyFileUpdateMonitor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * Fetcher for Jstor
+ * Fetcher for jstor.org
  **/
 public class JstorFetcher implements SearchBasedParserFetcher {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JstorFetcher.class);
-    private static final String HOST = "https://www.jstor.org/open/search/";
+    private static final String SEARCH_HOST = "https://www.jstor.org/open/search/";
+    private static final String CITE_HOST = "https://www.jstor.org/citation/text";
 
     private final ImportFormatPreferences importFormatPreferences;
 
@@ -29,16 +37,33 @@ public class JstorFetcher implements SearchBasedParserFetcher {
     }
 
     @Override
-    public URL getURLForQuery(String query) throws URISyntaxException, MalformedURLException, FetcherException {
-        LOGGER.warn(query);
-        URIBuilder uriBuilder = new URIBuilder(HOST);
-        uriBuilder.addParameter("Query", query); // Query
+    public URL getURLForQuery(String query) throws URISyntaxException, MalformedURLException {
+        URIBuilder uriBuilder = new URIBuilder(SEARCH_HOST);
+        uriBuilder.addParameter("Query", query);
         return uriBuilder.build().toURL();
     }
 
     @Override
     public Parser getParser() {
-        return new JstorParser(importFormatPreferences, new DummyFileUpdateMonitor());
+        return inputStream -> {
+            String response = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining(OS.NEWLINE));
+            List<BibEntry> entries = new ArrayList<>();
+
+            Document doc = Jsoup.parse(response);
+
+            List<Element> elements = doc.body().getElementsByClass("cite-this-item");
+            for (Element element : elements) {
+                BibtexParser parser = new BibtexParser(importFormatPreferences, new DummyFileUpdateMonitor());
+                String id = element.attr("href").replace("citation/info/", "");
+                try {
+                    String data = IOUtils.toString(new URL(CITE_HOST + id), Charsets.UTF_8);
+                    entries.addAll(parser.parseEntries(data));
+                } catch (IOException e) {
+                    throw new ParseException("could not download data from jstor.org", e);
+                }
+            }
+            return entries;
+        };
     }
 
     @Override
