4213a8d1e0a8832acb4c346fa0d16f5750b21986
Martin W. Kirst <maki@bitkings.de>
2020-10-18 18:47:52 +0200

    Improve error handling on GROBID server connection issues (#7026)
    * add explicit configuration for connectTimeout in URLDownload
    add information on GROBID server connection issue for user
    relates to #6517 and #6891
    
    * add CHANGELOG entry
    
    * fix style issues detected by checkstyle
    
    * use .toMilliseconds() is more convenient
    use assertThrow is more convenient
    
    * incorporate feedback from review
    reduce detail level from user message
    rework to use FetcherException instead of UncheckedIOException
    
    * switch to debug level in order to reduce verboseness in the log

--- src/main/java/org/jabref/gui/bibtexextractor/BibtexExtractorViewModel.java
+++ src/main/java/org/jabref/gui/bibtexextractor/BibtexExtractorViewModel.java
@@ -15,6 +15,7 @@ import org.jabref.gui.externalfiles.ImportHandler;
 import org.jabref.gui.externalfiletype.ExternalFileTypes;
 import org.jabref.gui.util.BackgroundTask;
 import org.jabref.gui.util.TaskExecutor;
+import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.fetcher.GrobidCitationFetcher;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.BibDatabaseContext;
@@ -22,7 +23,12 @@ import org.jabref.model.entry.BibEntry;
 import org.jabref.model.util.FileUpdateMonitor;
 import org.jabref.preferences.JabRefPreferences;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BibtexExtractorViewModel {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BibtexExtractorViewModel.class);
 
     private final StringProperty inputTextProperty = new SimpleStringProperty("");
     private DialogService dialogService;
@@ -58,6 +64,15 @@ public class BibtexExtractorViewModel {
     public void startParsing() {
         BackgroundTask.wrap(() -> currentCitationfetcher.performSearch(inputTextProperty.getValue()))
                       .onRunning(() -> dialogService.notify(Localization.lang("Your text is being parsed...")))
+                      .onFailure((e) -> {
+                          if (e instanceof FetcherException) {
+                              String msg = Localization.lang("There are connection issues with a JabRef server. Detailed information: %0.",
+                                      e.getMessage());
+                              dialogService.notify(msg);
+                          } else {
+                              LOGGER.warn("Missing exception handling.", e);
+                          }
+                      })
                       .onSuccess(parsedEntries -> {
                           dialogService.notify(Localization.lang("%0 entries were parsed from your query.", String.valueOf(parsedEntries.size())));
                           importHandler.importEntries(parsedEntries);
