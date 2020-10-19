12a366a7e5a52bc53b5bb225e098eb7c23886575
Johannes Theiner <kontakt@joethei.xyz>
2020-10-19 17:15:31 +0200

    Update BlankLineBehaviour.java

--- src/main/java/org/jabref/logic/exporter/BlankLineBehaviour.java
+++ src/main/java/org/jabref/logic/exporter/BlankLineBehaviour.java
@@ -1,7 +1,7 @@
 package org.jabref.logic.exporter;
 
 /**
-this enum represents the behaviour for blank lines in {@link TemplateExporter}
+ this enum represents the behaviour for blank lines in {@link TemplateExporter}
  */
 public enum BlankLineBehaviour {
     KEEP_BLANKS,
