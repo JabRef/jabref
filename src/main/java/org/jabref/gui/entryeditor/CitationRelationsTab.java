d739bcd491ba844e3efa50754f264786246d1567
Tim Bachmann <58782027+timbachmann@users.noreply.github.com>
2020-10-30 13:17:27 +0100

    Restructured prototype to search in background and implemented basic title search for all DOI's

--- src/main/java/org/jabref/gui/entryeditor/CitationRelationsTab.java
+++ src/main/java/org/jabref/gui/entryeditor/CitationRelationsTab.java
@@ -2,27 +2,25 @@ package org.jabref.gui.entryeditor;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+
 import org.jabref.Globals;
 import org.jabref.gui.DialogService;
+import org.jabref.gui.icon.IconTheme;
+import org.jabref.gui.icon.JabRefIconView;
 import org.jabref.gui.util.BackgroundTask;
-import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.fetcher.CitationRelationFetcher;
-import org.jabref.logic.importer.fetcher.MrDLibFetcher;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.preferences.JabRefPreferences;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 /**
  * GUI for tab displaying an articles citation relations based on the currently selected BibEntry
@@ -33,6 +31,22 @@ public class CitationRelationsTab extends EntryEditorTab {
     private final EntryEditorPreferences preferences;
     private final EntryEditor entryEditor;
     private final DialogService dialogService;
+    private VBox citingVBox;
+    private VBox citedByVBox;
+    private HBox citingHBox;
+    private HBox citedByHBox;
+    private Label citingLabel;
+    private Label citedByLabel;
+    private ListView<String> citingListView;
+    private ListView<String> citedByListView;
+    private StackPane citingStackPane;
+    private StackPane citedByStackPane;
+    private Button startCitingButton;
+    private Button startCitedByButton;
+    private Button refreshCitingButton;
+    private Button refreshCitedByButton;
+    private ProgressIndicator citingProgress;
+    private ProgressIndicator citedByProgress;
 
     public CitationRelationsTab(EntryEditor entryEditor, EntryEditorPreferences preferences, DialogService dialogService) {
         setText(Localization.lang("Citation relations"));
@@ -43,50 +57,64 @@ public class CitationRelationsTab extends EntryEditorTab {
     }
 
     private SplitPane getPane(BibEntry entry) {
-        VBox citedVBox = new VBox();
-        VBox citedByVBox = new VBox();
-        Label citedLabel = new Label("Cited");
-        citedLabel.setStyle("-fx-padding: 5px");
-        Label citedByLabel = new Label("Cited By");
+        citingVBox = new VBox();
+        citedByVBox = new VBox();
+        citingHBox = new HBox();
+        citedByHBox = new HBox();
+        citingLabel = new Label("Citing");
+        citingLabel.setStyle("-fx-padding: 5px");
+        citedByLabel = new Label("Cited By");
         citedByLabel.setStyle("-fx-padding: 5px");
-        ListView<String> citedListView = new ListView<>();
-        ListView<String> citedByListView = new ListView<>();
-        StackPane citedStackPane = new StackPane();
-        StackPane citedByStackPane = new StackPane();
-        Button startCitedButton = new Button("Search");
-        Button startCitedByButton = new Button("Search");
-        citedByStackPane.getChildren().add(citedByListView);
-        citedStackPane.getChildren().add(citedListView);
-        citedByStackPane.getChildren().add(startCitedByButton);
-        citedStackPane.getChildren().add(startCitedButton);
+        citingListView = new ListView<>();
+        citedByListView = new ListView<>();
+        citingStackPane = new StackPane();
+        citedByStackPane = new StackPane();
+        startCitingButton = new Button("Search");
+        startCitedByButton = new Button("Search");
+        refreshCitingButton = new Button();
+        refreshCitingButton.setGraphic(new JabRefIconView(IconTheme.JabRefIcons.REFRESH));
+        refreshCitingButton.setVisible(false);
+        refreshCitedByButton = new Button();
+        refreshCitedByButton.setGraphic(new JabRefIconView(IconTheme.JabRefIcons.REFRESH));
+        refreshCitedByButton.setVisible(false);
 
-        citedVBox.setFillWidth(true);
+        citedByStackPane.getChildren().add(citedByListView);
+        citingStackPane.getChildren().add(citingListView);
+        citedByStackPane.getChildren().add(startCitedByButton);
+        citingStackPane.getChildren().add(startCitingButton);
+        citingProgress = new ProgressIndicator();
+        citingProgress.setMaxSize(50, 50);
+        citingProgress.setVisible(false);
+        citedByProgress = new ProgressIndicator();
+        citedByProgress.setMaxSize(50, 50);
+        citedByProgress.setVisible(false);
+        citingStackPane.getChildren().add(citingProgress);
+        citedByStackPane.getChildren().add(citedByProgress);
+
+        citingVBox.setFillWidth(true);
         citedByVBox.setFillWidth(true);
-        citedVBox.getChildren().add(citedLabel);
-        citedVBox.getChildren().add(citedStackPane);
-        citedByVBox.getChildren().add(citedByLabel);
+        citingHBox.setAlignment(Pos.CENTER);
+        citedByHBox.setAlignment(Pos.CENTER);
+
+        citingHBox.getChildren().add(citingLabel);
+        citedByHBox.getChildren().add(citedByLabel);
+        citingHBox.getChildren().add(refreshCitingButton);
+        citedByHBox.getChildren().add(refreshCitedByButton);
+
+        citingVBox.getChildren().add(citingHBox);
+        citingVBox.getChildren().add(citingStackPane);
+        citedByVBox.getChildren().add(citedByHBox);
         citedByVBox.getChildren().add(citedByStackPane);
-        citedVBox.setAlignment(Pos.TOP_CENTER);
+        citingVBox.setAlignment(Pos.TOP_CENTER);
         citedByVBox.setAlignment(Pos.TOP_CENTER);
 
-        SplitPane container = new SplitPane(citedVBox, citedByVBox);
+        startCitingButton.setOnMouseClicked(event -> citingClicked(entry));
+        startCitedByButton.setOnMouseClicked(event -> citedByClicked(entry));
+        refreshCitingButton.setOnMouseClicked(event -> citingClicked(entry));
+        refreshCitedByButton.setOnMouseClicked(event -> citedByClicked(entry));
+
+        SplitPane container = new SplitPane(citingVBox, citedByVBox);
         setContent(container);
-
-        ObservableList<String> citedList = FXCollections.observableArrayList();
-
-        startCitedButton.setOnMouseClicked(event -> {
-            startCitedButton.setVisible(false);
-            CitationRelationFetcher fetcher = new CitationRelationFetcher();
-            try {
-                List<BibEntry> list = fetcher.performSearch(entry);
-                for(BibEntry e : list) {
-                    e.getField(StandardField.DOI).ifPresent(citedList::add);
-                }
-                citedListView.setItems(citedList);
-            } catch (FetcherException e) {
-                e.printStackTrace();
-            }
-        });
 
         return container;
     }
@@ -105,5 +133,65 @@ public class CitationRelationsTab extends EntryEditorTab {
         } else {
             setContent(getPrivacyDialog(entry));
         }*/
+    }
+
+    private void citingClicked(BibEntry entry) {
+        ObservableList<String> citingList = FXCollections.observableArrayList();
+        startCitingButton.setVisible(false);
+        CitationRelationFetcher fetcher = new CitationRelationFetcher(CitationRelationFetcher.SearchType.CITING, citingListView);
+        BackgroundTask
+                .wrap(() -> fetcher.performSearch(entry))
+                .onRunning(() -> {
+                    citingProgress.setVisible(true);
+                    refreshCitingButton.setVisible(false);
+                })
+                .onSuccess(fetchedList -> {
+                    citingProgress.setVisible(false);
+                    for(BibEntry e : fetchedList) {
+                        e.getField(StandardField.TITLE).ifPresent(citingList::add);
+                    }
+                    if (citingList.isEmpty()) {
+                        citingList.add("No articles found.");
+                    }
+                    citingListView.setItems(citingList);
+                    refreshCitingButton.setVisible(true);
+                })
+                .onFailure(exception -> {
+                    LOGGER.error("Error while fetching citing Articles", exception);
+                    citingProgress.setVisible(false);
+                    citingStackPane.getChildren().add(new Label("Error"));
+                    refreshCitingButton.setVisible(true);
+                })
+                .executeWith(Globals.TASK_EXECUTOR);
+    }
+
+    private void citedByClicked(BibEntry entry) {
+        ObservableList<String> citedByList = FXCollections.observableArrayList();
+        startCitedByButton.setVisible(false);
+        CitationRelationFetcher fetcher = new CitationRelationFetcher(CitationRelationFetcher.SearchType.CITEDBY, citedByListView);
+        BackgroundTask
+                .wrap(() -> fetcher.performSearch(entry))
+                .onRunning(() -> {
+                    citedByProgress.setVisible(true);
+                    refreshCitedByButton.setVisible(false);
+                })
+                .onSuccess(fetchedList -> {
+                    citedByProgress.setVisible(false);
+                    for(BibEntry e : fetchedList) {
+                        e.getField(StandardField.TITLE).ifPresent(citedByList::add);
+                    }
+                    if (citedByList.isEmpty()) {
+                        citedByList.add("No articles found.");
+                    }
+                    citedByListView.setItems(citedByList);
+                    refreshCitedByButton.setVisible(true);
+                })
+                .onFailure(exception -> {
+                    LOGGER.error("Error while fetching citing Articles", exception);
+                    citedByProgress.setVisible(false);
+                    citedByStackPane.getChildren().add(new Label("Error"));
+                    refreshCitedByButton.setVisible(true);
+                })
+                .executeWith(Globals.TASK_EXECUTOR);
     }
 }
--- src/main/java/org/jabref/logic/importer/fetcher/CitationRelationFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/CitationRelationFetcher.java
@@ -6,35 +6,86 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.scene.control.ListView;
+
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONException;
+
 import org.jabref.logic.importer.EntryBasedFetcher;
 import org.jabref.logic.importer.FetcherException;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.preferences.JabRefPreferences;
 
 public class CitationRelationFetcher implements EntryBasedFetcher {
 
-    private String title;
-    private String description;
-    private static final String BASIC_URL = "https://opencitations.net/index/api/v1/citations/";
+    private final SearchType searchType;
+    private static final String BASIC_URL = "https://opencitations.net/index/api/v1/metadata/";
+
+    public enum SearchType {
+        CITING,
+        CITEDBY
+    }
+
+    public CitationRelationFetcher(SearchType searchType, ListView<String> listView) {
+        this.searchType = searchType;
+    }
 
     @Override
     public List<BibEntry> performSearch(BibEntry entry) throws FetcherException {
-        System.out.println("Searching...");
+        String doi = entry.getField(StandardField.DOI).orElse("");
 
+        if (searchType.equals(SearchType.CITING)) {
+            return getCiting(doi);
+        } else if (searchType.equals(SearchType.CITEDBY)) {
+            return getCitedBy(doi);
+        } else {
+            return null;
+        }
+    }
+
+    private List<BibEntry> getCiting(String doi) {
         List<BibEntry> list = new ArrayList<>();
         try {
-            JSONArray json = readJsonFromUrl(BASIC_URL + entry.getField(StandardField.DOI).orElseThrow());
+            JSONArray json = readJsonFromUrl(BASIC_URL + doi);
             System.out.println(json.toString());
             for (int i = 0; i < json.length(); i++) {
-                String citedDoi = json.getJSONObject(i).getString("citing");
-                //System.out.println(citedDoi);
+                String citingDoi = json.getJSONObject(i).getString("cited");
+                System.out.println(citingDoi.substring(8));
+                if (!doi.equals(citingDoi.substring(8))) {
+                    DoiFetcher doiFetcher = new DoiFetcher(JabRefPreferences.getInstance().getImportFormatPreferences());
+                    try {
+                        doiFetcher.performSearchById(citingDoi.substring(8)).ifPresent(list::add);
+                    } catch (FetcherException fetcherException) {
 
-                BibEntry newEntry = new BibEntry();
-                newEntry.setField(StandardField.DOI, citedDoi.substring(8));
-                list.add(newEntry);
+                    }
+                }
             }
+            System.out.println("Finished.");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private List<BibEntry> getCitedBy(String doi) {
+        List<BibEntry> list = new ArrayList<>();
+        try {
+            JSONArray json = readJsonFromUrl(BASIC_URL + doi);
+            System.out.println(json.toString());
+            for (int i = 0; i < json.length(); i++) {
+                String citedByDoi = json.getJSONObject(i).getString("citing");
+                System.out.println(citedByDoi.substring(8));
+                if (!doi.equals(citedByDoi.substring(8))) {
+                    DoiFetcher doiFetcher = new DoiFetcher(JabRefPreferences.getInstance().getImportFormatPreferences());
+                    try {
+                        doiFetcher.performSearchById(citedByDoi.substring(8)).ifPresent(list::add);
+                    } catch (FetcherException fetcherException) {
+
+                    }
+                }
+            }
+            System.out.println("Finished.");
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
@@ -51,28 +102,16 @@ public class CitationRelationFetcher implements EntryBasedFetcher {
     }
 
     public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
+        try (InputStream is = new URL(url).openStream()) {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonText = readAll(rd);
-            JSONArray json = new JSONArray(jsonText);
-            return json;
-        } finally {
-            is.close();
+            return new JSONArray(jsonText);
         }
     }
 
     @Override
     public String getName() {
         return null;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
 }
