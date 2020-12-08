776b9f5b7c64db9cd43f0c7cbf66bef4a6781225
Tim Bachmann <58782027+timbachmann@users.noreply.github.com>
2020-12-08 14:35:41 +0100

    Changed Fetcher implementation to be similar to other fetchers using UrlDownload and getParser()

--- src/main/java/org/jabref/logic/importer/CitationBasedParserFetcher.java
+++ src/main/java/org/jabref/logic/importer/CitationBasedParserFetcher.java
@@ -1,47 +1,67 @@
 package org.jabref.logic.importer;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.SocketException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import org.jabref.logic.cleanup.Formatter;
 import org.jabref.logic.importer.fetcher.OpenCitationFetcher;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.StandardField;
-
-import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONException;
-import kong.unirest.json.JSONObject;
 
 /**
  * Provides a convenient interface for citation-based fetcher, which follow the usual four-step procedure:
- * 1. Send request with doi of the entry
- * 2. Parse the response to get a list of DOI's
- * 3. Send new request to get bibliographic information for every DOI
- * 4. Parse the response and create a List of {@link BibEntry}
+ * <ol>
+ *   <li>Send request with doi of the entry</li>
+ *   <li>Parse the response to get a list of DOIs</li>
+ *   <li>Send new request to get bibliographic information for every DOI</li>
+ *   <li>Parse the response and create a List of {@link BibEntry}</li>
+ * </ol>
  */
 public interface CitationBasedParserFetcher extends CitationFetcher {
 
     /**
-     * Constructs a URL-String based on the given DOI's.
+     * Constructs a URL-String based on the given BibEntries.
      *
-     * @param dois the dois to look information for
+     * @param entries the entries to look information for
      */
-    String getURLForEntries(String... dois);
+    URL getURLForEntries(List<BibEntry> entries) throws URISyntaxException, MalformedURLException, FetcherException;
 
+    /**
+     * Returns the parser used to convert the response to a list of {@link BibEntry}.
+     */
+    Parser getParser(OpenCitationFetcher.SearchType searchType);
+
+    /**
+     * Performs a cleanup of the fetched entry.
+     *
+     * Only systematic errors of the fetcher should be corrected here
+     * (i.e. if information is consistently contained in the wrong field or the wrong format)
+     * but not cosmetic issues which may depend on the user's taste (for example, LateX code vs HTML in the abstract).
+     *
+     * Try to reuse existing {@link Formatter} for the cleanup. For example,
+     * {@code new FieldFormatterCleanup(StandardField.TITLE, new RemoveBracesFormatter()).cleanup(entry);}
+     *
+     * By default, no cleanup is done.
+     *
+     * @param entry the entry to be cleaned-up
+     */
+    default void doPostCleanup(BibEntry entry) {
+        // Do nothing by default
+    }
+
+    @Override
     default List<BibEntry> searchCitedBy(BibEntry entry) throws FetcherException {
         Objects.requireNonNull(entry);
         return performSearch(entry, OpenCitationFetcher.SearchType.CITEDBY);
     }
 
+    @Override
     default List<BibEntry> searchCiting(BibEntry entry) throws FetcherException {
         Objects.requireNonNull(entry);
         return performSearch(entry, OpenCitationFetcher.SearchType.CITING);
@@ -49,63 +69,40 @@ public interface CitationBasedParserFetcher extends CitationFetcher {
 
     /**
      * Searches specified URL for related articles based on a {@link BibEntry}
-     * and the citation type {@link org.jabref.logic.importer.fetcher.OpenCitationFetcher.SearchType}
+     * and the search type {@link org.jabref.logic.importer.fetcher.OpenCitationFetcher.SearchType}
      *
-     * @param entry      entry to search bibliographic information for
-     * @param searchType type of search to perform (CITING, CITEDBY)
+     * @param entry      entry to search information for
+     * @param searchType type of search to perform (CITING, CITEDBY, BIBINFO)
      * @return a list of {@link BibEntry}, which are matched by the query (may be empty)
      * @throws FetcherException Error message passed to {@link org.jabref.gui.entryeditor.CitationRelationsTab}
      */
     default List<BibEntry> performSearch(BibEntry entry, OpenCitationFetcher.SearchType searchType) throws FetcherException {
         Objects.requireNonNull(entry);
         Objects.requireNonNull(searchType);
-        String doi = entry.getField(StandardField.DOI).orElse("");
 
-        List<BibEntry> list = new ArrayList<>();
-        try {
-            JSONArray json = Objects.requireNonNull(readJsonFromUrl(getURLForEntries(doi)));
-            if (json.isEmpty()) {
-                return list;
-            }
-            String[] items = json.getJSONObject(0).getString(searchType.label).split("; ");
-            if (!Arrays.equals(items, new String[] {""})) {
-                JSONArray metaArray = Objects.requireNonNull(readJsonFromUrl(getURLForEntries(items)));
-                if (metaArray.isEmpty()) {
-                    return list;
-                }
-                for (int i = 0; i < metaArray.length(); i++) {
-                    list.add(createNewEntry(metaArray.getJSONObject(i)));
-                }
-            }
-        } catch (IOException | JSONException | NullPointerException e) {
-            throw new FetcherException("Couldn't connect to API! Please try again.");
-        }
-        return list;
-    }
+        List<BibEntry> entries = new ArrayList<>();
+        entries.add(entry);
+        try (InputStream stream = new BufferedInputStream(getURLForEntries(entries).openStream())) {
+            List<BibEntry> fetchedEntries = getParser(searchType).parseEntries(stream);
 
-    default BibEntry createNewEntry(JSONObject jsonObject) {
-        // Create new BibEntry from API response
-        return null;
-    }
-
-    /**
-     * Method to read JSON files from URL-String
-     *
-     * @param url API URL to search
-     * @return JSONArray containing the response of the API
-     */
-    default JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            StringBuilder sb = new StringBuilder();
-            int cp;
-            while ((cp = rd.read()) != -1) {
-                sb.append((char) cp);
+            if (fetchedEntries.isEmpty()) {
+                return fetchedEntries;
             }
-            String jsonText = sb.toString();
-            return new JSONArray(jsonText);
-        } catch (UnknownHostException | SocketException exception) {
-            return null;
+
+            InputStream inputStream = new BufferedInputStream(getURLForEntries(fetchedEntries).openStream());
+            List<BibEntry> finalEntries = getParser(OpenCitationFetcher.SearchType.BIBINFO).parseEntries(inputStream);
+
+            // Post-cleanup
+            finalEntries.forEach(this::doPostCleanup);
+
+            return finalEntries;
+        } catch (URISyntaxException e) {
+            throw new FetcherException("Search URI is malformed", e);
+        } catch (IOException e) {
+            // TODO: Catch HTTP Response 401 errors and report that user has no rights to access resource
+            throw new FetcherException("A network error occurred", e);
+        } catch (ParseException e) {
+            throw new FetcherException("An internal parser error occurred", e);
         }
     }
 }
--- src/main/java/org/jabref/logic/importer/fetcher/OpenCitationFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/OpenCitationFetcher.java
@@ -1,13 +1,25 @@
 package org.jabref.logic.importer.fetcher;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 import org.jabref.logic.importer.CitationBasedParserFetcher;
 import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.Parser;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 
+import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +36,8 @@ public class OpenCitationFetcher implements CitationBasedParserFetcher {
      */
     public enum SearchType {
         CITING("reference"),
-        CITEDBY("citation");
+        CITEDBY("citation"),
+        BIBINFO("info");
 
         public final String label;
 
@@ -38,8 +51,49 @@ public class OpenCitationFetcher implements CitationBasedParserFetcher {
     }
 
     @Override
-    public String getURLForEntries(String... dois) {
-        return dois != null ? BASIC_URL + String.join("__", dois) : BASIC_URL;
+    public URL getURLForEntries(List<BibEntry> entries) throws URISyntaxException, MalformedURLException {
+        StringJoiner stringJoiner = new StringJoiner("__");
+        for (BibEntry entry : entries) {
+            entry.getField(StandardField.DOI).ifPresent(stringJoiner::add);
+        }
+        URIBuilder builder = new URIBuilder(BASIC_URL + stringJoiner.toString());
+        return builder.build().toURL();
+    }
+
+    @Override
+    public Parser getParser(OpenCitationFetcher.SearchType searchType) {
+        return inputStream -> {
+            List<BibEntry> entries = new ArrayList<>();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            try {
+                int cp;
+                while ((cp = rd.read()) != -1) {
+                    sb.append((char) cp);
+                }
+            } catch (IOException e) {
+                return entries;
+            }
+            String jsonText = sb.toString();
+            JSONArray json = new JSONArray(jsonText);
+            if (json.isEmpty()) {
+                return entries;
+            }
+            if (searchType.equals(SearchType.BIBINFO)) {
+                for (int i = 0; i < json.length(); i++) {
+                    entries.add(createNewEntry(json.getJSONObject(i)));
+                }
+            } else {
+                String[] items = json.getJSONObject(0).getString(searchType.label).split("; ");
+                if (items[0].isEmpty()) {
+                    return entries;
+                }
+                for (String doi : items) {
+                    entries.add(new BibEntry().withField(StandardField.DOI, doi));
+                }
+            }
+            return entries;
+        };
     }
 
     @Override
@@ -54,8 +108,12 @@ public class OpenCitationFetcher implements CitationBasedParserFetcher {
         return performSearch(entry, SearchType.CITING);
     }
 
-    @Override
-    public BibEntry createNewEntry(JSONObject jsonObject) {
+    /**
+     * Creates a new BibEntry from JSONObject
+     * @param jsonObject JSONObject
+     * @return BibEntry created
+     */
+    private BibEntry createNewEntry(JSONObject jsonObject) {
         LOGGER.debug("Paper found: {}", jsonObject.getString("doi"));
         return new BibEntry()
                 .withField(StandardField.TITLE, jsonObject.getString("title"))
