3685516eefda43194a1c654c366bf31cb05d6792
Siedlerchr <siedlerkiller@gmail.com>
2020-12-20 20:24:09 +0100

    inject stuff

--- src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogView.java
+++ src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogView.java
@@ -9,6 +9,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
 import javafx.stage.FileChooser;
 
+import org.jabref.gui.DialogService;
+import org.jabref.gui.StateManager;
+import org.jabref.gui.externalfiletype.ExternalFileTypes;
 import org.jabref.gui.util.BaseDialog;
 import org.jabref.preferences.PreferencesService;
 
@@ -20,8 +23,13 @@ public class ExternalFilesDialogView extends BaseDialog<Void> {
     @FXML private ComboBox<FileChooser.ExtensionFilter> fileTypeSelection;
     @FXML private TreeView<?> tree;
     @Inject private PreferencesService preferencesService;
+    @Inject private DialogService dialogService;
+    @Inject private StateManager stateManager;
+    private final ExternalFilesDialogViewModel viewModel;
 
     public ExternalFilesDialogView() {
+
+        viewModel = new ExternalFilesDialogViewModel(dialogService, null, ExternalFileTypes.getInstance(), null, null, preferencesService, stateManager);
 
         ViewLoader.view(this)
                   .load()
--- src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogViewModel.java
+++ src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogViewModel.java
@@ -1,7 +1,34 @@
 package org.jabref.gui.externalfiles;
 
+import javax.swing.undo.UndoManager;
+
+import org.jabref.gui.DialogService;
+import org.jabref.gui.StateManager;
+import org.jabref.gui.externalfiletype.ExternalFileTypes;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.util.FileUpdateMonitor;
+import org.jabref.preferences.PreferencesService;
 
 public class ExternalFilesDialogViewModel {
 
+    private final ImportHandler importHandler;
+
+    public ExternalFilesDialogViewModel(DialogService dialogService, BibDatabaseContext databaseContext,
+                                        ExternalFileTypes externalFileTypes, UndoManager undoManager,
+                                        FileUpdateMonitor fileUpdateMonitor, PreferencesService preferences, StateManager stateManager) {
+        importHandler = new ImportHandler(
+                                          dialogService,
+                                          databaseContext,
+                                          externalFileTypes,
+                                          preferences,
+                                          fileUpdateMonitor,
+                                          undoManager,
+                                          stateManager);
+    }
+
+    public void startImport()
+    {
+
+    }
 
 }
