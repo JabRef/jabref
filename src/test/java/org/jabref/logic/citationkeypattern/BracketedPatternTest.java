ece416a116132cf835f984fee2bdc519e8c6b945
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2020-12-21 16:07:13 -0500

    Drop out-of-scope test case

--- src/test/java/org/jabref/logic/citationkeypattern/BracketedPatternTest.java
+++ src/test/java/org/jabref/logic/citationkeypattern/BracketedPatternTest.java
@@ -316,12 +316,4 @@ class BracketedPatternTest {
 
         assertEquals("eBay", BracketedPattern.expandBrackets("[auth]", null, bibEntry, null));
     }
-
-    @Test
-    void expandBracketsUniversityAbbreviationWithLatex() {
-        BibEntry bibEntry = new BibEntry()
-                .withField(StandardField.AUTHOR, "{{\\\"O}rebro universitet}");
-
-        assertEquals("UniÖrebro", BracketedPattern.expandBrackets("[auth]", null, bibEntry, null));
-    }
 }
