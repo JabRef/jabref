98770bbe45a666aca639d915c9b2494fba37ab5a
Siedlerchr <siedlerkiller@gmail.com>
2020-12-21 18:07:15 +0100

    finish export implementation TODO: Import Button does not yet work, always null

--- src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogView.java
+++ src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogView.java
@@ -4,13 +4,13 @@ import java.nio.file.Files;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.swing.undo.UndoManager;
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
@@ -18,52 +18,58 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 import org.jabref.gui.DialogService;
-import org.jabref.gui.Globals;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.externalfiles.FindUnlinkedFilesDialog.FileNodeWrapper;
 import org.jabref.gui.externalfiletype.ExternalFileType;
 import org.jabref.gui.externalfiletype.ExternalFileTypes;
-import org.jabref.gui.util.BackgroundTask;
 import org.jabref.gui.util.BaseDialog;
+import org.jabref.gui.util.TaskExecutor;
 import org.jabref.gui.util.ViewModelListCellFactory;
 import org.jabref.gui.util.ViewModelTreeCellFactory;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.model.util.FileUpdateMonitor;
 import org.jabref.preferences.PreferencesService;
 
 import com.airhacks.afterburner.views.ViewLoader;
 
-public class ExternalFilesDialogView extends BaseDialog<Void> {
+public class ExternalFilesDialogView extends BaseDialog<Boolean> {
 
     @FXML private TextField directoryPathField;
     @FXML private ComboBox<FileChooser.ExtensionFilter> fileTypeSelection;
     @FXML private TreeView<FileNodeWrapper> tree;
     @FXML private VBox panelSearchProgress;
     @FXML private Button buttonScan;
+    @FXML private ButtonType importButton;
+    @FXML private Button buttonExport;
     @Inject private PreferencesService preferencesService;
     @Inject private DialogService dialogService;
     @Inject private StateManager stateManager;
+    @Inject private UndoManager undoManager;
+    @Inject private TaskExecutor taskExecutor;
+    @Inject private FileUpdateMonitor fileUpdateMonitor;
 
     private ExternalFilesDialogViewModel viewModel;
-    private BackgroundTask<CheckBoxTreeItem<FileNodeWrapper>> findUnlinkedFilesTask;
 
 
     public ExternalFilesDialogView() {
         this.setTitle(Localization.lang("Search for unlinked local files"));
 
-        ViewLoader.view(this)
-                  .load()
-                  .setAsContent(this.getDialogPane());
-
         setResultConverter(buttonPressed -> {
             if (buttonPressed == ButtonType.OK) {
                 startImport();
             } else {
-                if (findUnlinkedFilesTask != null) {
-                    findUnlinkedFilesTask.cancel();
-                }
+                 cancelImport();
             }
-            return null;
+            return false;
         });
+
+        ViewLoader.view(this)
+                  .load()
+                  .setAsContent(this.getDialogPane());
+
+        // TODO: Does not work yet
+        // Button btnImport = (Button) this.getDialogPane().lookupButton(importButton);
+        // btnImport.disableProperty().bindBidirectional(viewModel.applyButtonDisabled());
     }
 
     private void startImport() {
@@ -73,7 +79,7 @@ public class ExternalFilesDialogView extends BaseDialog<Void> {
     @FXML
     private void initialize() {
 
-        viewModel = new ExternalFilesDialogViewModel(dialogService, ExternalFileTypes.getInstance(), Globals.undoManager, Globals.getFileUpdateMonitor(), preferencesService, stateManager);
+        viewModel = new ExternalFilesDialogViewModel(dialogService, ExternalFileTypes.getInstance(), undoManager, fileUpdateMonitor, preferencesService, stateManager, taskExecutor);
         viewModel.directoryPath().bindBidirectional(directoryPathField.textProperty());
 
         fileTypeSelection.setItems(FXCollections.observableArrayList(viewModel.getFileFilters()));
@@ -95,6 +101,21 @@ public class ExternalFilesDialogView extends BaseDialog<Void> {
                     }
                 })
                 .install(tree);
+
+       viewModel.treeRoot().bindBidirectional(tree.rootProperty());
+       viewModel.searchProgressPaneVisible().bindBidirectional(panelSearchProgress.visibleProperty());
+       viewModel.scanButtonDisabled().bindBidirectional(buttonScan.disableProperty());
+       viewModel.scanButtonDefaultButton().bindBidirectional(buttonScan.defaultButtonProperty());
+       viewModel.exportButtonDisabled().bindBidirectional(buttonExport.disableProperty());
+       viewModel.selectedExtension().bind(fileTypeSelection.valueProperty());
+
+       viewModel.scanButtonDefaultButton().setValue(true);
+       viewModel.scanButtonDisabled().setValue(true);
+       fileTypeSelection.getSelectionModel().selectFirst();
+    }
+
+    void cancelImport() {
+        viewModel.cancelImport();
     }
 
     @FXML
@@ -114,7 +135,7 @@ public class ExternalFilesDialogView extends BaseDialog<Void> {
 
     @FXML
     void scanFiles(ActionEvent event) {
-
+        viewModel.startSearch();
     }
 
     @FXML
@@ -125,6 +146,10 @@ public class ExternalFilesDialogView extends BaseDialog<Void> {
     @FXML
     void unselectAll(ActionEvent event) {
 
+    }
+    @FXML
+    void exportSelected(ActionEvent event) {
+       viewModel.startExport();
     }
 
 }
--- src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogViewModel.java
+++ src/main/java/org/jabref/gui/externalfiles/ExternalFilesDialogViewModel.java
@@ -1,40 +1,81 @@
 package org.jabref.gui.externalfiles;
 
+import java.io.BufferedWriter;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.swing.undo.UndoManager;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.control.CheckBoxTreeItem;
+import javafx.scene.control.TreeItem;
 import javafx.stage.FileChooser;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
+import org.jabref.gui.externalfiles.FindUnlinkedFilesDialog.FileNodeWrapper;
 import org.jabref.gui.externalfiletype.ExternalFileTypes;
+import org.jabref.gui.importer.UnlinkedFilesCrawler;
+import org.jabref.gui.util.BackgroundTask;
 import org.jabref.gui.util.DirectoryDialogConfiguration;
+import org.jabref.gui.util.FileDialogConfiguration;
 import org.jabref.gui.util.FileFilterConverter;
+import org.jabref.gui.util.TaskExecutor;
 import org.jabref.logic.util.StandardFileType;
+import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.util.FileUpdateMonitor;
 import org.jabref.preferences.PreferencesService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ExternalFilesDialogViewModel {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExternalFilesDialogViewModel.class);
 
     private final ImportHandler importHandler;
     private final StringProperty directoryPath = new SimpleStringProperty("");
+    private final ObjectProperty<FileChooser.ExtensionFilter> selectedExtension = new SimpleObjectProperty<>();
+
+    private final BooleanProperty searchProgressPaneVisible = new SimpleBooleanProperty();
+    private final BooleanProperty applyButtonDisabled = new SimpleBooleanProperty();
+    private final BooleanProperty scanButtonDisabled = new SimpleBooleanProperty(true);
+    private final BooleanProperty exportButtonDisabled = new SimpleBooleanProperty();
+    private final ObjectProperty<TreeItem<FileNodeWrapper>> treeRoot =  new SimpleObjectProperty<>();
+    private final BooleanProperty treeExpanded = new SimpleBooleanProperty();
+    private final BooleanProperty scanButtonDefaultButton = new SimpleBooleanProperty();
+
     private final List<FileChooser.ExtensionFilter> fileFilterList = List.of(
                                                                              FileFilterConverter.ANY_FILE,
                                                                              FileFilterConverter.toExtensionFilter(StandardFileType.PDF),
                                                                              FileFilterConverter.toExtensionFilter(StandardFileType.BIBTEX_DB));
     private final DialogService dialogService;
     private final PreferencesService preferences;
+    private BackgroundTask<CheckBoxTreeItem<FileNodeWrapper>> findUnlinkedFilesTask;
+    private final BibDatabaseContext bibDatabasecontext;
+    private final TaskExecutor taskExecutor;
 
     public ExternalFilesDialogViewModel(DialogService dialogService, ExternalFileTypes externalFileTypes, UndoManager undoManager,
-                                        FileUpdateMonitor fileUpdateMonitor, PreferencesService preferences, StateManager stateManager) {
+                                        FileUpdateMonitor fileUpdateMonitor, PreferencesService preferences, StateManager stateManager, TaskExecutor taskExecutor) {
         this.preferences = preferences;
         this.dialogService = dialogService;
+        this.taskExecutor = taskExecutor;
+        this.bibDatabasecontext = stateManager.getActiveDatabase().orElseThrow(() -> new NullPointerException("Database null"));
         importHandler = new ImportHandler(
                                           dialogService,
-                                          stateManager.getActiveDatabase().orElseThrow(() -> new NullPointerException("Database null")),
+                                          bibDatabasecontext,
                                           externalFileTypes,
                                           preferences,
                                           fileUpdateMonitor,
@@ -43,11 +84,75 @@ public class ExternalFilesDialogViewModel {
     }
 
     public void startImport() {
+        this.importHandler.importAsNewEntries(null);
 
     }
 
+    /**
+     * This starts the export of all files of all selected nodes in the file tree view.
+     */
     public void startExport() {
+        CheckBoxTreeItem<FileNodeWrapper> root = (CheckBoxTreeItem<FileNodeWrapper>) treeRoot.getValue();
 
+        final List<Path> fileList = getFileListFromNode(root);
+        if (fileList.isEmpty()) {
+            return;
+        }
+
+        exportButtonDisabled.setValue(true);
+        applyButtonDisabled.setValue(true);
+
+        FileDialogConfiguration fileDialogConfiguration = new FileDialogConfiguration.Builder()
+                .withInitialDirectory(preferences.getWorkingDir()).build();
+        Optional<Path> exportPath = dialogService.showFileSaveDialog(fileDialogConfiguration);
+
+        if (exportPath.isEmpty()) {
+            exportButtonDisabled.setValue(false);
+            applyButtonDisabled.setValue(false);
+            return;
+        }
+
+        try (BufferedWriter writer =
+                     Files.newBufferedWriter(exportPath.get(), StandardCharsets.UTF_8,
+                             StandardOpenOption.CREATE)) {
+            for (Path file : fileList) {
+                writer.write(file.toString() + "\n");
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error exporting", e);
+        }
+
+        exportButtonDisabled.setValue(false);
+        applyButtonDisabled.setValue(false);
+
+    }
+
+    public void startSearch() {
+
+        Path directory = this.getSearchDirectory();
+        FileFilter selectedFileFilter = FileFilterConverter.toFileFilter(selectedExtension.getValue());
+
+        findUnlinkedFilesTask = new UnlinkedFilesCrawler(directory, selectedFileFilter, bibDatabasecontext)
+                .onRunning(() -> {
+                    searchProgressPaneVisible.setValue(true);
+                    scanButtonDisabled.setValue(true);
+                    treeRoot.setValue(null);
+                })
+                .onFinished(() -> {
+                    searchProgressPaneVisible.setValue(false);
+                    scanButtonDisabled.setValue(false);
+                })
+                .onSuccess(root -> {
+                    treeRoot.setValue(root);
+                    root.setSelected(true);
+                    root.setExpanded(true);
+
+                   applyButtonDisabled.setValue(false);
+                   exportButtonDisabled.setValue(false);
+                   scanButtonDefaultButton.setValue(false);
+
+                });
+        findUnlinkedFilesTask.executeWith(taskExecutor);
     }
 
     public StringProperty directoryPath() {
@@ -58,6 +163,26 @@ public class ExternalFilesDialogViewModel {
         return this.fileFilterList;
     }
 
+    public ObjectProperty<FileChooser.ExtensionFilter> selectedExtension() {
+        return this.selectedExtension;
+    }
+
+    public BooleanProperty searchProgressPaneVisible() {
+        return this.searchProgressPaneVisible;
+    }
+
+    public BooleanProperty applyButtonDisabled() {
+        return this.applyButtonDisabled;
+    }
+
+    public BooleanProperty scanButtonDisabled() {
+        return this.scanButtonDisabled;
+    }
+
+    public BooleanProperty exportButtonDisabled() {
+        return this.exportButtonDisabled;
+    }
+
     public void browseFileDirectory() {
         DirectoryDialogConfiguration directoryDialogConfiguration = new DirectoryDialogConfiguration.Builder()
             .withInitialDirectory(preferences.getWorkingDir()).build();
@@ -66,6 +191,53 @@ public class ExternalFilesDialogViewModel {
                  .ifPresent(selectedDirectory -> {
                      directoryPath.setValue(selectedDirectory.toAbsolutePath().toString());
                      preferences.setWorkingDirectory(selectedDirectory.toAbsolutePath());
+                     this.scanButtonDisabled.setValue(false);
                  });
+    }
+
+    public ObjectProperty<TreeItem<FileNodeWrapper>> treeRoot() {
+        return this.treeRoot;
+    }
+
+    public void cancelImport() {
+        if (findUnlinkedFilesTask != null) {
+            findUnlinkedFilesTask.cancel();
+        }
+    }
+
+    public BooleanProperty scanButtonDefaultButton() {
+        return this.scanButtonDefaultButton ;
+
+
+    }
+    private Path getSearchDirectory() {
+        Path directory = Path.of(directoryPath.getValue());
+        if (Files.notExists(directory)) {
+            directory = Path.of(System.getProperty("user.dir"));
+            directoryPath.setValue(directory.toAbsolutePath().toString());
+        }
+        if (!Files.isDirectory(directory)) {
+            directory = directory.getParent();
+            directoryPath.setValue(directory.toAbsolutePath().toString());
+        }
+        return directory;
+    }
+
+    private List<Path> getFileListFromNode(CheckBoxTreeItem<FileNodeWrapper> node) {
+        List<Path> filesList = new ArrayList<>();
+        for (TreeItem<FileNodeWrapper> childNode : node.getChildren()) {
+            CheckBoxTreeItem<FileNodeWrapper> child = (CheckBoxTreeItem<FileNodeWrapper>) childNode;
+            if (child.isLeaf()) {
+                if (child.isSelected()) {
+                    Path nodeFile = child.getValue().path;
+                    if ((nodeFile != null) && Files.isRegularFile(nodeFile)) {
+                        filesList.add(nodeFile);
+                    }
+                }
+            } else {
+                filesList.addAll(getFileListFromNode(child));
+            }
+        }
+        return filesList;
     }
 }
