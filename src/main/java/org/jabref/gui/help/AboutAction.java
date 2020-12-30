edc6d4ba7e20685942e2bb10e9436adb3e28e11d
Fiyinfolu Eludire <17176053+faeludire@users.noreply.github.com>
2020-12-30 20:11:25 +0000

    [WIP] Fix for application dialogs opening in wrong displays

--- src/main/java/org/jabref/gui/help/AboutAction.java
+++ src/main/java/org/jabref/gui/help/AboutAction.java
@@ -6,9 +6,20 @@ import org.jabref.gui.actions.SimpleCommand;
 import com.airhacks.afterburner.injection.Injector;
 
 public class AboutAction extends SimpleCommand {
+
+    private final AboutDialogView aboutDialogView;
+
+    public AboutAction() {
+        this.aboutDialogView = new AboutDialogView();
+    }
+
     @Override
     public void execute() {
         DialogService dialogService = Injector.instantiateModelOrService(DialogService.class);
-        dialogService.show(new AboutDialogView());
+        dialogService.show(aboutDialogView);
+    }
+
+    public AboutDialogView getAboutDialogView() {
+        return aboutDialogView;
     }
 }
