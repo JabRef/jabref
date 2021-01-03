6315c61b840027a0363948fa36fafc18197e1e82
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2021-01-03 09:40:36 -0500

    Fix test cases

--- src/test/java/org/jabref/model/groups/AutomaticPersonsGroupTest.java
+++ src/test/java/org/jabref/model/groups/AutomaticPersonsGroupTest.java
@@ -1,26 +1,25 @@
 package org.jabref.model.groups;
 
 import java.util.Arrays;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.model.strings.LatexToUnicodeAdapter;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 class AutomaticPersonsGroupTest {
-    private static Set<GroupTreeNode> createPersonSubGroupFrom(String... lastNames) {
+    private static GroupTreeNode[] createPersonSubGroupFrom(String... lastNames) {
         return Arrays.stream(lastNames)
-                     .distinct()
-                     .map(LatexToUnicodeAdapter::format)
                      .map(lastName ->
                              new LastNameGroup(lastName, GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR, lastName))
                      .map(GroupTreeNode::new)
-                     .collect(Collectors.toSet());
+                     .collect(Collectors.toList())
+                     .toArray(GroupTreeNode[]::new);
     }
 
     @Test
@@ -28,7 +27,7 @@ class AutomaticPersonsGroupTest {
         BibEntry bibEntry = new BibEntry().withField(StandardField.AUTHOR, "Turing, Alan and Hopper, Grace");
         var subgroups = new AutomaticPersonsGroup("", GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR).createSubgroups(bibEntry);
         var expectedSubgroups = createPersonSubGroupFrom("Turing", "Hopper");
-        assertEquals(expectedSubgroups, subgroups);
+        assertThat(subgroups, containsInAnyOrder(expectedSubgroups));
     }
 
     @Test
@@ -36,22 +35,30 @@ class AutomaticPersonsGroupTest {
         BibEntry bibEntry = new BibEntry().withField(StandardField.AUTHOR, "Turing, Alan and Hopper, Grace");
         var subgroups = new AutomaticPersonsGroup("", GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR).createSubgroups(bibEntry);
         var expectedSubgroups = createPersonSubGroupFrom("Turing", "Hopper");
-        assertEquals(expectedSubgroups, subgroups);
+        assertThat(subgroups, containsInAnyOrder(expectedSubgroups));
     }
 
     @Test
-    void createSubgroupFromLatexAndCheckForUnicodeLastName() {
+    void createSubgroupsContainingSpaceSeparatedNames() {
+        BibEntry bibEntry = new BibEntry().withField(StandardField.AUTHOR, "Alan Turing and Grace Hopper");
+        var subgroups = new AutomaticPersonsGroup("", GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR).createSubgroups(bibEntry);
+        var expectedSubgroups = createPersonSubGroupFrom("Turing", "Hopper");
+        assertThat(subgroups, containsInAnyOrder(expectedSubgroups));
+    }
+
+    @Test
+    void createSubgroupFromLatex() {
         BibEntry bibEntry = new BibEntry().withField(StandardField.AUTHOR, "Kurt G{\\\"{o}}del");
         var subgroup = new AutomaticPersonsGroup("", GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR).createSubgroups(bibEntry);
         var expectedSubgroup = createPersonSubGroupFrom("Gödel");
-        assertEquals(expectedSubgroup, subgroup);
+        assertThat(subgroup, contains(expectedSubgroup));
     }
 
     @Test
-    void createSubgroupFromUnicodeAndCheckForLatexLastName() {
+    void createSubgroupFromUnicode() {
         BibEntry bibEntry = new BibEntry().withField(StandardField.AUTHOR, "Kurt Gödel");
         var subgroup = new AutomaticPersonsGroup("", GroupHierarchyType.INDEPENDENT, StandardField.AUTHOR).createSubgroups(bibEntry);
-        var expectedSubgroup = createPersonSubGroupFrom("G{\\\"{o}}del");
-        assertEquals(expectedSubgroup, subgroup);
+        var expectedSubgroup = createPersonSubGroupFrom("Gödel");
+        assertThat(subgroup, contains(expectedSubgroup));
     }
 }
