5913ac44ec8da7f788408a256521fb2959a0763d
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2021-01-05 14:07:55 +0100

    Update Timestamp preferences

--- src/main/java/org/jabref/logic/preferences/TimestampPreferences.java
+++ src/main/java/org/jabref/logic/preferences/TimestampPreferences.java
@@ -6,45 +6,23 @@ import java.time.format.DateTimeFormatter;
 import org.jabref.model.entry.field.Field;
 
 public class TimestampPreferences {
-    private final boolean useTimestamps;
-    private final boolean updateTimestamp;
-    private final Field timestampField;
-    private final String timestampFormat;
-    private final boolean overwriteTimestamp;
+    private final boolean addTimestamp;
+    private final boolean modifyTimestamp;
 
-    public TimestampPreferences(boolean useTimestamps, boolean updateTimestamp, Field timestampField, String timestampFormat, boolean overwriteTimestamp) {
-        this.useTimestamps = useTimestamps;
-        this.updateTimestamp = updateTimestamp;
-        this.timestampField = timestampField;
-        this.timestampFormat = timestampFormat;
-        this.overwriteTimestamp = overwriteTimestamp;
+    public TimestampPreferences(boolean addTimestamp, boolean modifyTimestamp) {
+        this.addTimestamp = addTimestamp;
+        this.modifyTimestamp = modifyTimestamp;
     }
-
-    public boolean shouldUseTimestamps() {
-        return useTimestamps;
-    }
-
-    public boolean shouldUpdateTimestamp() {
-        return updateTimestamp;
-    }
-
-    public Field getTimestampField() {
-        return timestampField;
-    }
-
-    public String getTimestampFormat() {
-        return timestampFormat;
-    }
-
-    public boolean shouldOverwriteTimestamp() {
-        return overwriteTimestamp;
-    }
-
-    public boolean shouldIncludeTimestamps() {
-        return useTimestamps && updateTimestamp;
-    }
-
+1
     public String now() {
-        return DateTimeFormatter.ofPattern(timestampFormat).format(LocalDateTime.now());
+        return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now());
+    }
+
+    public boolean isAddTimestamp() {
+        return addTimestamp;
+    }
+
+    public boolean isModifyTimestamp() {
+        return modifyTimestamp;
     }
 }
