4bd1afb76042641021b3e3b74504da20f272d4d0
Siedlerchr <siedlerkiller@gmail.com>
2021-01-15 21:10:41 +0100

    fix harvard exporter extension

--- src/test/java/org/jabref/logic/layout/format/RTFCharsTest.java
+++ src/test/java/org/jabref/logic/layout/format/RTFCharsTest.java
@@ -185,4 +185,9 @@ class RTFCharsTest {
         assertEquals("\\'d6", formatter.format("\\\"O"));
         assertEquals("\\'dc", formatter.format("\\\"U"));
     }
+
+    @Test
+    void keepBraces() {
+        assertEquals("{Organization and JabRef}", formatter.format("{Organization and Jabef}"));
+    }
 }
