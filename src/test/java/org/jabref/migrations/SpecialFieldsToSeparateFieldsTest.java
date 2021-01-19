be9aca47a533663b9347f09bc036342364bdb849
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2021-01-19 22:31:44 +0100

    Readd all changed files

--- src/test/java/org/jabref/migrations/SpecialFieldsToSeparateFieldsTest.java
+++ src/test/java/org/jabref/migrations/SpecialFieldsToSeparateFieldsTest.java
@@ -19,66 +19,72 @@ class SpecialFieldsToSeparateFieldsTest {
 
     @ParameterizedTest
     @MethodSource("provideKeywordFieldPairs")
-    public void migrateToCorrectField(SpecialField expectedField, String fieldInKeyword) {
-        BibEntry entry = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
-                .withField(StandardField.KEYWORDS, fieldInKeyword);
+    public void migrateToCorrectField(SpecialField field, String fieldInKeyword, BibEntry expected) {
+        BibEntry entry = new BibEntry().withField(StandardField.KEYWORDS, fieldInKeyword);
 
         new SpecialFieldsToSeparateFields(',').performMigration(new ParserResult(List.of(entry)));
 
-        assertEquals(fieldInKeyword, entry.getField(expectedField).get());
+        assertEquals(expected, entry);
     }
 
     @Test
     public void noKewordToMigrate() {
         BibEntry entry = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
                                        .withField(StandardField.KEYWORDS, "tdd");
+        BibEntry expected = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
+                                          .withField(StandardField.KEYWORDS, "tdd");
+
         new SpecialFieldsToSeparateFields(',').performMigration(new ParserResult(List.of(entry)));
 
-        assertEquals("JabRef", entry.getField(StandardField.AUTHOR).get());
-        assertEquals("tdd", entry.getField(StandardField.KEYWORDS).get());
+        assertEquals(expected, entry);
     }
 
     @Test
     public void migrateMultipleSpecialFields() {
         BibEntry entry = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
                                        .withField(StandardField.KEYWORDS, "printed, prio1");
+        BibEntry expected = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
+                                          .withField(SpecialField.PRINTED, "printed")
+                                          .withField(SpecialField.PRIORITY, "prio1");
+
         new SpecialFieldsToSeparateFields(',').performMigration(new ParserResult(List.of(entry)));
 
-        assertEquals("prio1", entry.getField(SpecialField.PRIORITY).get());
-        assertEquals("printed", entry.getField(SpecialField.PRINTED).get());
-
+        assertEquals(expected, entry);
     }
 
     @Test
     public void migrateSpecialFieldsMixedWithKeyword() {
         BibEntry entry = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
                                        .withField(StandardField.KEYWORDS, "tdd, prio1, SE");
+
+        BibEntry expected = new BibEntry().withField(StandardField.AUTHOR, "JabRef")
+                                          .withField(StandardField.KEYWORDS, "tdd, SE")
+                                          .withField(SpecialField.PRIORITY, "prio1");
+
         new SpecialFieldsToSeparateFields(',').performMigration(new ParserResult(List.of(entry)));
 
-        assertEquals("prio1", entry.getField(SpecialField.PRIORITY).get());
-        assertEquals("tdd, SE", entry.getField(StandardField.KEYWORDS).get());
-
+        assertEquals(expected, entry);
     }
 
     private static Stream<Arguments> provideKeywordFieldPairs() {
         return Stream.of(
                 Arguments.of(
-                        SpecialField.PRINTED, "printed"
+                        SpecialField.PRINTED, "printed", new BibEntry().withField(SpecialField.PRINTED, "printed")
                 ),
                 Arguments.of(
-                    SpecialField.PRIORITY, "prio1"
+                        SpecialField.PRIORITY, "prio1", new BibEntry().withField(SpecialField.PRIORITY, "prio1")
                 ),
                 Arguments.of(
-                    SpecialField.QUALITY, "qualityAssured"
+                        SpecialField.QUALITY, "qualityAssured", new BibEntry().withField(SpecialField.QUALITY, "qualityAssured")
                 ),
                 Arguments.of(
-                        SpecialField.RANKING, "rank2"
+                        SpecialField.RANKING, "rank2", new BibEntry().withField(SpecialField.RANKING, "rank2")
                 ),
                 Arguments.of(
-                        SpecialField.READ_STATUS, "skimmed"
+                        SpecialField.READ_STATUS, "skimmed", new BibEntry().withField(SpecialField.READ_STATUS, "skimmed")
                 ),
                 Arguments.of(
-                        SpecialField.RELEVANCE, "relevant"
+                        SpecialField.RELEVANCE, "relevant", new BibEntry().withField(SpecialField.RELEVANCE, "relevant")
                 )
         );
     }
