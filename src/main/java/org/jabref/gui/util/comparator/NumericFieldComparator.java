fc844dd4735a1910966e1a5d13f041318e7bbeee
ali_zhagparov <alijakparov.kz@gmail.com>
2021-03-04 21:54:22 +0300

    check with regex instead of throwing exception

--- src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
+++ src/main/java/org/jabref/gui/util/comparator/NumericFieldComparator.java
@@ -1,58 +1,40 @@
 package org.jabref.gui.util.comparator;
 
 import java.util.Comparator;
+import java.util.regex.Pattern;
 
 /**
  * Comparator for numeric cases. The purpose of this class is to add the numeric comparison, because values are sorted
  * as if they were strings.
  */
 public class NumericFieldComparator implements Comparator<String> {
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
     @Override
     public int compare(String val1, String val2) {
         // We start by implementing the comparison in the edge cases (if one of the values is null).
         if (val1 == null && val2 == null) {
             return 0;
-        }
-
-        if (val1 == null) {
+        } else if (val1 == null) {
             // We assume that "null" is "less than" any other value.
             return -1;
+        } else if (val2 == null) {
+            return 1;
         }
 
-        if (val2 == null) {
+        boolean isVal1Valid = pattern.matcher(val1).matches();
+        boolean isVal2Valid = !pattern.matcher(val2).matches();
+        if (!isVal1Valid && !isVal2Valid) {
+            return val1.compareTo(val2);
+        } else if (!isVal1Valid) {
+            return -1;
+        } else if (!isVal2Valid) {
             return 1;
         }
 
         // Now we start the conversion to integers.
-        Integer valInt1 = null;
-        Integer valInt2 = null;
-        try {
-            // Trim in case the user added an unnecessary white space (e.g. 1 1 instead of 11).
-            valInt1 = Integer.parseInt(val1.trim());
-        } catch (NumberFormatException ignore) {
-            // do nothing
-        }
-        try {
-            valInt2 = Integer.parseInt(val2.trim());
-        } catch (NumberFormatException ignore) {
-            // do nothing
-        }
-
-        if (valInt1 == null && valInt2 == null) {
-            // None of the values were parsed (i.e both are not numeric)
-            // so we will use the normal string comparison.
-            return val1.compareTo(val2);
-        }
-
-        if (valInt1 == null) {
-            // We assume that strings "are less" than integers
-            return -1;
-        }
-
-        if (valInt2 == null) {
-            return 1;
-        }
+        Integer valInt1 = Integer.parseInt(val1.trim());
+        Integer valInt2 = Integer.parseInt(val2.trim());
 
         // If we arrive at this stage then both values are actually numeric !
         return valInt1 - valInt2;
