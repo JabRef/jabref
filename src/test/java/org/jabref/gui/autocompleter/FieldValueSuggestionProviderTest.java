1fcce5faff3bb384710a794d1bdc9efe87d8c773
Bartosz Jaroszkowski <bjaroszkowski@gmail.com>
2021-03-07 15:08:20 +0100

    deleted unused imports

--- src/test/java/org/jabref/gui/autocompleter/FieldValueSuggestionProviderTest.java
+++ src/test/java/org/jabref/gui/autocompleter/FieldValueSuggestionProviderTest.java
@@ -3,7 +3,6 @@ package org.jabref.gui.autocompleter;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import org.jabref.logic.journals.JournalAbbreviationRepository;
