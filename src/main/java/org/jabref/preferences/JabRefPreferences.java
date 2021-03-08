2c7715cf0476d504475cde7b5e23c4bff677036b
BJaroszkowski <54515221+BJaroszkowski@users.noreply.github.com>
2021-03-08 09:47:01 +0100

    Added preference option to use custom URL for DOI generation (#7480)

--- src/main/java/org/jabref/preferences/JabRefPreferences.java
+++ src/main/java/org/jabref/preferences/JabRefPreferences.java
@@ -87,6 +87,7 @@ import org.jabref.logic.layout.format.NameFormatterPreferences;
 import org.jabref.logic.net.ProxyPreferences;
 import org.jabref.logic.openoffice.OpenOfficePreferences;
 import org.jabref.logic.openoffice.StyleLoader;
+import org.jabref.logic.preferences.DOIPreferences;
 import org.jabref.logic.preferences.OwnerPreferences;
 import org.jabref.logic.preferences.TimestampPreferences;
 import org.jabref.logic.preview.PreviewLayout;
@@ -200,6 +201,9 @@ public class JabRefPreferences implements PreferencesService {
     public static final String MEMORY_STICK_MODE = "memoryStickMode";
     public static final String SHOW_ADVANCED_HINTS = "showAdvancedHints";
     public static final String DEFAULT_ENCODING = "defaultEncoding";
+
+    public static final String BASE_DOI_URI = "baseDOIURI";
+    public static final String USE_CUSTOM_DOI_URI = "useCustomDOIURI";
 
     public static final String USE_OWNER = "useOwner";
     public static final String DEFAULT_OWNER = "defaultOwner";
@@ -444,6 +448,9 @@ public class JabRefPreferences implements PreferencesService {
 
         // Set DOI to be the default ID entry generator
         defaults.put(ID_ENTRY_GENERATOR, DoiFetcher.NAME);
+
+        defaults.put(USE_CUSTOM_DOI_URI, Boolean.FALSE);
+        defaults.put(BASE_DOI_URI, "https://doi.org");
 
         if (OS.OS_X) {
             defaults.put(FONT_FAMILY, "SansSerif");
@@ -1359,6 +1366,19 @@ public class JabRefPreferences implements PreferencesService {
     public void storeTelemetryPreferences(TelemetryPreferences preferences) {
         putBoolean(COLLECT_TELEMETRY, preferences.shouldCollectTelemetry());
         putBoolean(ALREADY_ASKED_TO_COLLECT_TELEMETRY, !preferences.shouldAskToCollectTelemetry()); // mind the !
+    }
+
+    @Override
+    public DOIPreferences getDOIPreferences() {
+        return new DOIPreferences(
+                getBoolean(USE_CUSTOM_DOI_URI),
+                get(BASE_DOI_URI));
+    }
+
+    @Override
+    public void storeDOIPreferences(DOIPreferences preferences) {
+        putBoolean(USE_CUSTOM_DOI_URI, preferences.isUseCustom());
+        put(BASE_DOI_URI, preferences.getDefaultBaseURI());
     }
 
     @Override
