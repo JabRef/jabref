b3d4957514d9409ae738ea588ba759a9d34aacf7
Siedlerchr <siedlerkiller@gmail.com>
2021-03-14 23:22:25 +0100

    fix checkstyle

--- src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
+++ src/test/java/org/jabref/gui/fieldeditors/LinkedFileViewModelTest.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.TreeSet;
+
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
