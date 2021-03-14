3d97c850e762f1eb836a0fad37c17c93a2d5dc3c
Davfon <davide.fontanella@uzh.ch>
2021-03-14 12:39:54 +0100

    DateCheckerTest: consider different date formats (partitions)

--- src/test/java/org/jabref/logic/integrity/DateCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/DateCheckerTest.java
@@ -16,12 +16,36 @@ class DateCheckerTest {
     }
 
     @Test
+    void acceptsValidDates() {
+        assertEquals(Optional.empty(), checker.checkValue("2018-04-21"));
+        assertEquals(Optional.empty(), checker.checkValue("2018-04"));
+        assertEquals(Optional.empty(), checker.checkValue("21-04-2018"));
+        assertEquals(Optional.empty(), checker.checkValue("04-2018"));
+        assertEquals(Optional.empty(), checker.checkValue("04/18"));
+        assertEquals(Optional.empty(), checker.checkValue("04/2018"));
+        assertEquals(Optional.empty(), checker.checkValue("April 21, 2018"));
+        assertEquals(Optional.empty(), checker.checkValue("April, 2018"));
+        assertEquals(Optional.empty(), checker.checkValue("21.04.2018"));
+        assertEquals(Optional.empty(), checker.checkValue("2018.04.21"));
+        assertEquals(Optional.empty(), checker.checkValue("2018"));
+    }
+
+    @Test
     void complainsAboutInvalidIsoLikeDate() {
         assertEquals(Optional.of("incorrect format"), checker.checkValue("2018-04-21TZ"));
     }
 
     @Test
-    void acceptsValidIsoDate() {
-        assertEquals(Optional.empty(), checker.checkValue("2018-04-21"));
+    void complainsAboutLetterInput() {
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018-Apr-21"));
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018-Apr-Twentyone"));
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018-Apr-Twentyfirst"));
+    }
+
+    @Test
+    void complainsAboutSpecialCharacterInput() {
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018_04_21"));
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018 04 21"));
+        assertEquals(Optional.of("incorrect format"), checker.checkValue("2018~04~21"));
     }
 }
