afbe62d2e95125dc8280a53115eba5ad2f5f4c5b
BShaq <bulinshaqiri@hotmail.com>
2021-03-14 15:35:25 +0100

    Update ISBNCheckerTest.java
    new unit tests for ISBN-13 control digit partition

--- src/test/java/org/jabref/logic/integrity/ISBNCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/ISBNCheckerTest.java
@@ -2,6 +2,8 @@ package org.jabref.logic.integrity;
 
 import java.util.Optional;
 
+import org.jabref.logic.l10n.Localization;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,4 +33,18 @@ public class ISBNCheckerTest {
         assertNotEquals(Optional.empty(), checker.checkValue("0-201-53082-2"));
     }
 
+    @Test
+    void isbnAcceptsCorrectControlDigitForIsbn13() {
+        assertEquals(Optional.empty(), checker.checkValue("978-0-306-40615-7"));
+    }
+
+    @Test
+    void isbnDoesNotAcceptIncorrectControlDigitForIsbn13() {
+        assertEquals(Optional.of(Localization.lang("incorrect control digit")), checker.checkValue("978-0-306-40615-2"));
+    }
+
+    @Test
+    void isbnDoesNotAcceptInvalidFormatForIsbn13() {
+        assertEquals(Optional.of(Localization.lang("incorrect format")), checker.checkValue("978_0_306_40615_7"));
+    }
 }
