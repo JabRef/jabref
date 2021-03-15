8dfce40a20fb96aee6f2980a5d091b4ec0ec000c
ningxie1991 <ningxie1991@gmail.com>
2021-03-15 21:04:31 +0100

    Added more test inputs and boundary cases

--- src/test/java/org/jabref/logic/layout/format/ShortMonthFormatterTest.java
+++ src/test/java/org/jabref/logic/layout/format/ShortMonthFormatterTest.java
@@ -5,6 +5,8 @@ import org.jabref.logic.layout.LayoutFormatter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShortMonthFormatterTest {
@@ -18,7 +20,26 @@ public class ShortMonthFormatterTest {
 
     @Test
     public void testFormat() {
-        assertEquals("jan", formatter.format("01"));
+        assertEquals("jan", formatter.format("1"));
+        assertEquals("feb", formatter.format("2"));
+        assertEquals("mar", formatter.format("3"));
+        assertEquals("apr", formatter.format("4"));
+        assertEquals("may", formatter.format("5"));
+        assertEquals("jun", formatter.format("6"));
+        assertEquals("jul", formatter.format("7"));
+        assertEquals("aug", formatter.format("8"));
+        assertEquals("sep", formatter.format("9"));
+        assertEquals("oct", formatter.format("10"));
+        assertEquals("nov", formatter.format("11"));
+        assertEquals("dec", formatter.format("12"));
         assertEquals("jan", formatter.format("Januar"));
+    }
+
+    @Test
+    public void testInvalidFormat() {
+        assertEquals("", formatter.format("-1"));
+        assertEquals("", formatter.format("0"));
+        assertEquals("", formatter.format("13"));
+        assertEquals("", formatter.format("abc"));
     }
 }
