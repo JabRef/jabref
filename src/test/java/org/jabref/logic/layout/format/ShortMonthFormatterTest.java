2206b3b1668883f5c80b629ddc9fa9d6aa930be3
ningxie1991 <ningxie1991@gmail.com>
2021-03-16 08:56:38 +0100

    Revert the changes to ShortMonthFormatterTest.java
    Duplicate changes in another pull request

--- src/test/java/org/jabref/logic/layout/format/ShortMonthFormatterTest.java
+++ src/test/java/org/jabref/logic/layout/format/ShortMonthFormatterTest.java
@@ -3,8 +3,7 @@ package org.jabref.logic.layout.format;
 import org.jabref.logic.layout.LayoutFormatter;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,34 +16,9 @@ public class ShortMonthFormatterTest {
         formatter = new ShortMonthFormatter();
     }
 
-    @ParameterizedTest(name = "formattedStr={0}, input={1}")
-    @CsvSource({
-            "jan, 1", // jan
-            "feb, 2", // feb
-            "mar, 3", // mar
-            "apr, 4", // apr
-            "may, 5", // may
-            "jun, 6", // jun
-            "jul, 7", // jul
-            "aug, 8", // aug
-            "sep, 9", // sep
-            "oct, 10", // oct
-            "nov, 11", // nov
-            "dec, 12", // dec
-            "jan, Januar" // jan
-    })
-    public void testValidFormat(String expectedResult, String input) {
-        assertEquals(expectedResult, formatter.format(input));
-    }
-
-    @ParameterizedTest(name = "formattedStr={0}, input={1}")
-    @CsvSource({
-            "'', -1", // -1
-            "'', 0", // 0
-            "'', 13", // 13
-            "'', abc", // abc
-    })
-    public void testInvalidFormat(String expectedResult, String input) {
-        assertEquals(expectedResult, formatter.format(input));
+    @Test
+    public void testFormat() {
+        assertEquals("jan", formatter.format("01"));
+        assertEquals("jan", formatter.format("Januar"));
     }
 }
