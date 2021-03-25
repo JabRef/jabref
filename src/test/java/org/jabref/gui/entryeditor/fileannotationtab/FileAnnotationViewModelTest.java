8add78520504f3cde46e86f727a60de8b2e9da81
BShaq <bulinshaqiri@hotmail.com>
2021-03-25 21:47:29 +0100

    Refactoring of FileAnnotationViewModelTest.java (resolved Test Smell: Eager Test)
    To not introduce another Test Smell (Sensitive Equality), I used the equals() method to compare the strings.

--- src/test/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationViewModelTest.java
+++ src/test/java/org/jabref/gui/entryeditor/fileannotationtab/FileAnnotationViewModelTest.java
@@ -6,14 +6,17 @@ import java.util.Optional;
 import org.jabref.model.pdf.FileAnnotation;
 import org.jabref.model.pdf.FileAnnotationType;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileAnnotationViewModelTest {
 
-    @Test
-    public void removeOnlyLineBreaksNotPrecededByPeriodOrColon() {
+    private FileAnnotationViewModel annotationViewModel;
+
+    @BeforeEach
+    void setup() {
         String content = "This is content";
         String marking = String.format("This is paragraph 1.%n" +
                 "This is paragr-%naph 2, and it crosses%nseveral lines,%nnow you can see next paragraph:%n"
@@ -22,16 +25,39 @@ public class FileAnnotationViewModelTest {
         FileAnnotation linkedFileAnnotation = new FileAnnotation("John", LocalDateTime.now(), 3, content, FileAnnotationType.FREETEXT, Optional.empty());
         FileAnnotation annotation = new FileAnnotation("Jaroslav Kucha ˇr", LocalDateTime.parse("2017-07-20T10:11:30"), 1, marking, FileAnnotationType.HIGHLIGHT, Optional.of(linkedFileAnnotation));
 
-        FileAnnotationViewModel annotationViewModel = new FileAnnotationViewModel(annotation);
+        annotationViewModel = new FileAnnotationViewModel(annotation);
+    }
 
-        assertEquals("Jaroslav Kucha ˇr", annotationViewModel.getAuthor());
-        assertEquals(1, annotation.getPage());
-        assertEquals("2017-07-20 10:11:30", annotationViewModel.getDate());
-        assertEquals("This is content", annotationViewModel.getContent());
+    @Test
+    public void sameAuthor() {
+        String expectedAuthor = "Jaroslav Kucha ˇr";
+        assertTrue(annotationViewModel.getAuthor().equals(expectedAuthor));
+    }
 
-        assertEquals(String.format("This is paragraph 1.%n" +
-                        "This is paragraph 2, and it crosses several lines, now you can see next paragraph:%n"
-                        + "This is paragraph 3."),
-                annotationViewModel.getMarking());
+    @Test
+    public void retrieveCorrectPageNumberAsString() {
+        String expectedPage = "1";
+        assertTrue(annotationViewModel.getPage().equals(expectedPage));
+    }
+
+    @Test
+    public void retrieveCorrectDateAsString() {
+        String expectedDate = "2017-07-20 10:11:30";
+        assertTrue(annotationViewModel.getDate().equals(expectedDate));
+    }
+
+    @Test
+    public void retrieveCorrectContent() {
+        String expectedContent = "This is content";
+        assertTrue(annotationViewModel.getContent().equals(expectedContent));
+    }
+
+    @Test
+    public void removeOnlyLineBreaksNotPrecededByPeriodOrColon() {
+        String expectedMarking = String.format("This is paragraph 1.%n" +
+                "This is paragraph 2, and it crosses several lines, now you can see next paragraph:%n"
+                + "This is paragraph 3.");
+
+        assertTrue(annotationViewModel.getMarking().equals(expectedMarking));
     }
 }
