6be01104d7e1c01ce8a843017c6f8b3788555c72
BShaq <bulinshaqiri@hotmail.com>
2021-03-27 20:29:24 +0100

    Additional unit tests for EntryComparator.java
    Increase of Line/Statement coverage

--- src/test/java/org/jabref/logic/bibtex/comparator/EntryComparatorTest.java
+++ src/test/java/org/jabref/logic/bibtex/comparator/EntryComparatorTest.java
@@ -14,4 +14,80 @@ class EntryComparatorTest {
         BibEntry e2 = e1;
         assertEquals(0, new EntryComparator(false, false, StandardField.TITLE).compare(e1, e2));
     }
+
+    @Test
+    void compareAuthorFieldBiggerAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.AUTHOR, "Stephen King");
+        entry2.setField(StandardField.AUTHOR, "Henning Mankell");
+
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.AUTHOR);
+
+        assertEquals(-2, entryComparator.compare(entry1, entry2));
+    }
+
+    @Test
+    void bothEntriesHaveNotSetTheFieldToCompareAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.BOOKTITLE, "Stark - The Dark Half (1989)");
+        entry2.setField(StandardField.COMMENTATOR, "Some Commentator");
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.TITLE);
+
+        assertEquals(-1, entryComparator.compare(entry1, entry2));
+    }
+
+    @Test
+    void secondEntryHasNotSetFieldToCompareAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.TITLE, "Stark - The Dark Half (1989)");
+        entry2.setField(StandardField.COMMENTATOR, "Some Commentator");
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.TITLE);
+
+        assertEquals(-1, entryComparator.compare(entry1, entry2));
+    }
+
+    @Test
+    void firstEntryHasNotSetFieldToCompareAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.COMMENTATOR, "Some Commentator");
+        entry2.setField(StandardField.TITLE, "Stark - The Dark Half (1989)");
+
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.TITLE);
+
+        assertEquals(1, entryComparator.compare(entry1, entry2));
+    }
+
+    @Test
+    void bothEntriesNumericAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.EDITION, "1");
+        entry2.setField(StandardField.EDITION, "3");
+
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.EDITION);
+
+        assertEquals(-1, entryComparator.compare(entry1, entry2));
+    }
+/*
+    @Test
+    void firstEntryNotNumericAscending() throws Exception {
+        BibEntry entry1 = new BibEntry();
+        BibEntry entry2 = new BibEntry();
+
+        entry1.setField(StandardField.EDITION, "3");
+        entry2.setField(StandardField.EDITION, "1.5");
+
+        EntryComparator entryComparator = new EntryComparator(false, false, StandardField.EDITION);
+
+        assertEquals(-1, entryComparator.compare(entry1, entry2));
+    }*/
 }
