6585b8ea1216d921ef46138b75389f81c1e80cc2
Dario Safa <dario.akhavansafa@uzh.ch>
2021-03-29 23:09:25 +0200

    Refactor SentenceAnalyzerTest
    fixed Assertion Roulette

--- src/test/java/org/jabref/model/search/rules/SentenceAnalyzerTest.java
+++ src/test/java/org/jabref/model/search/rules/SentenceAnalyzerTest.java
@@ -11,9 +11,9 @@ public class SentenceAnalyzerTest {
 
     @Test
     public void testGetWords() {
-        assertEquals(Arrays.asList("a", "b"), new SentenceAnalyzer("a b").getWords());
-        assertEquals(Arrays.asList("a", "b"), new SentenceAnalyzer(" a b ").getWords());
-        assertEquals(Collections.singletonList("b "), new SentenceAnalyzer("\"b \" ").getWords());
-        assertEquals(Collections.singletonList(" a"), new SentenceAnalyzer(" \\ a").getWords());
+        assertEquals(Arrays.asList("a", "b"), new SentenceAnalyzer("a b").getWords(), "Sentence was not parsed correctly.");
+        assertEquals(Arrays.asList("a", "b"), new SentenceAnalyzer(" a b ").getWords(), "Sentence was not parsed correctly. Check leading and trailing spaces.");
+        assertEquals(Collections.singletonList("b "), new SentenceAnalyzer("\"b \" ").getWords(), "Sentence was not parsed correctly. Check escaped characters and trailing spaces.");
+        assertEquals(Collections.singletonList(" a"), new SentenceAnalyzer(" \\ a").getWords(), "Sentence was not parsed correctly. Check escaped characters and leading spaces.");
     }
 }
