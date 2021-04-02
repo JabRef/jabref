bbf78a71811ee041bb116f60ed3592bdb63519f8
Antal K <antalk2@gmail.com>
2021-04-02 23:12:31 +0200

    comments

--- src/main/java/org/jabref/logic/openoffice/OOBibStyleGetCitationMarker.java
+++ src/main/java/org/jabref/logic/openoffice/OOBibStyleGetCitationMarker.java
@@ -71,6 +71,22 @@ class OOBibStyleGetCitationMarker {
         uniquefiers[from] = sb.toString();
     }
 
+
+    /**
+     * @param entries
+     * @param database
+     * @param inParenthesis Emit "(Au, 2000)" if true, "Au (2000)" if false.
+     * @param uniquefiers null or of length entries.size(), with
+     *                    elements null or letter making a cited
+     *                    source unique in citations.
+     *
+     * @param unlimAuthors Can this be null?
+     *                     If not null, then its elements are ...
+     *
+     * @param pageInfosForCitations
+     *                    May be null.
+     *                    Any or of its elements can be null.
+     */
     public static String getCitationMarker(OOBibStyle style,
                                            List<BibEntry> entries,
                                            Map<BibEntry, BibDatabase> database,
@@ -79,11 +95,33 @@ class OOBibStyleGetCitationMarker {
                                            int[] unlimAuthors,
                                            List<String> pageInfosForCitations
         ) {
+
+        List<String> pageInfos =
+            OOBibStyle.regularizePageInfosForCitations(pageInfosForCitations,
+                                                       entries.size());
+        // Original:
+        //
         // Look for groups of uniquefied entries that should be combined in the output.
         // E.g. (Olsen, 2005a, b) should be output instead of (Olsen, 2005a; Olsen, 2005b).
+        //
+        // Now:
+        // - handle pageInfos
+        // - allow duplicate entries with same or different pageInfos.
+        //
+        // We assume entries are already sorted, all we need is to
+        // group consecutive entries if we can.
+        //
+        // TODO: imposeLocalOrder should consider pageInfos, to make
+        //       sure multiple citations of the same entry with the same pageInfos
+        //
+        //
+        //
         int piv = -1;
+
         String tmpMarker = null;
+
         if (uniquefiers != null) {
+
             for (int i = 0; i < uniquefiers.length; i++) {
 
                 if ((uniquefiers[i] == null) || uniquefiers[i].isEmpty()) {
@@ -143,7 +181,7 @@ class OOBibStyleGetCitationMarker {
                 // Do the grouping:
                 group(style, entries, uniquefiers, piv, uniquefiers.length - 1);
             }
-        }
+        } // if uniquefiers are not null
 
         if (inParenthesis) {
             return getAuthorYearParenthesisMarker(style, entries, database, uniquefiers, unlimAuthors);
