f3e62c3bb018917cd6797078386c28b41b672365
Antal K <antalk2@gmail.com>
2021-04-06 22:28:59 +0200

    format StringUtil.intValueOfOptional

--- src/main/java/org/jabref/model/strings/StringUtil.java
+++ src/main/java/org/jabref/model/strings/StringUtil.java
@@ -486,7 +486,13 @@ public class StringUtil {
         boolean sign = false;
         char ch;
 
-        if ((str == null) || ((end = str.length()) == 0) || ((((ch = str.charAt(0)) < '0') || (ch > '9')) && (!(sign = ch == '-') || (++idx == end) || ((ch = str.charAt(idx)) < '0') || (ch > '9')))) {
+        if ((str == null)
+            || ((end = str.length()) == 0)
+            || ((((ch = str.charAt(0)) < '0') || (ch > '9'))
+                && (!(sign = ch == '-')
+                    || (++idx == end)
+                    || ((ch = str.charAt(idx)) < '0')
+                    || (ch > '9')))) {
             return Optional.empty();
         }
 
