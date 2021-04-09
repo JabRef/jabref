986f35ce76b55d8bfaa16b18d05716468622258d
Antal K <antalk2@gmail.com>
2021-04-09 19:58:06 +0200

    CitationMarkerEntryImpl.java : checkstyle, cleanup, describe

--- src/main/java/org/jabref/logic/openoffice/CitationMarkerEntryImpl.java
+++ src/main/java/org/jabref/logic/openoffice/CitationMarkerEntryImpl.java
@@ -1,54 +1,20 @@
 package org.jabref.logic.openoffice;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.function.ToIntFunction;
-import java.util.regex.Pattern;
-
-import org.jabref.logic.layout.Layout;
-import org.jabref.logic.layout.LayoutFormatter;
-import org.jabref.logic.layout.LayoutFormatterPreferences;
-import org.jabref.logic.layout.LayoutHelper;
 import org.jabref.model.database.BibDatabase;
-import org.jabref.model.entry.Author;
-import org.jabref.model.entry.AuthorList;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.Field;
-import org.jabref.model.entry.field.FieldFactory;
-import org.jabref.model.entry.field.OrFields;
-import org.jabref.model.entry.field.StandardField;
-import org.jabref.model.entry.types.EntryType;
-import org.jabref.model.entry.types.EntryTypeFactory;
-import org.jabref.model.strings.StringUtil;
 
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+/**
+ * Implement CitationMarkerEntry by containing the data needed.
+ *
+ * {@see CitationMarkerEntry} for description.
+ *
+ */
 public class CitationMarkerEntryImpl implements CitationMarkerEntry {
     final String citationKey;
-    final BibEntry bibEntry;    // should allow null
-    final BibDatabase database; // should allow null
-    final String uniqueLetter;  // may be null
-    final String pageInfo;  //  null for empty
+    final BibEntry bibEntry;
+    final BibDatabase database;
+    final String uniqueLetter;
+    final String pageInfo;
     final boolean isFirstAppearanceOfSource;
 
     public CitationMarkerEntryImpl(String citationKey,
