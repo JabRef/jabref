a19cf30335d7ea5d82bc8335dbcaa1677cd2078b
Qiming-Liu <prossliu@gmail.com>
2021-04-15 05:37:32 +0930

    finished updateContextMenu, languages needed

--- src/main/java/org/jabref/gui/maintable/MainTableHeaderRightClickMenu.java
+++ src/main/java/org/jabref/gui/maintable/MainTableHeaderRightClickMenu.java
@@ -1,35 +1,50 @@
 package org.jabref.gui.maintable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.StackPane;
+
+import org.jabref.gui.DialogService;
+import org.jabref.gui.LibraryTab;
+import org.jabref.gui.preferences.PreferencesDialogView;
 
 public class MainTableHeaderRightClickMenu extends ContextMenu {
 
-    public MainTableHeaderRightClickMenu() {
-        super();
-
-        RadioMenuItem groupColor = new RadioMenuItem("Group color");
-        RadioMenuItem linkedFile = new RadioMenuItem("Linked file");
-        RadioMenuItem linkedId = new RadioMenuItem("Linked id");
-        RadioMenuItem entryType = new RadioMenuItem("Entrytype");
-        RadioMenuItem author = new RadioMenuItem("Author/Editor");
-        MenuItem columnDialog = new MenuItem("ColumnDialog");
-
-        columnDialog.setOnAction(event -> {
-            //new columnDialog
-        });
-
-        this.getItems().addAll(groupColor, linkedFile, linkedId, entryType, author, columnDialog);
-    }
-
-    public void show(MainTable mainTable) {
+    public void show(MainTable mainTable, LibraryTab libraryTab, DialogService dialogService) {
         mainTable.setOnContextMenuRequested(event -> {
             if (!(event.getTarget() instanceof StackPane)) {
+                updateContextMenu(mainTable, libraryTab, dialogService);
                 this.show(mainTable, event.getScreenX(), event.getScreenY());
             }
             event.consume();
         });
+    }
+
+    private void updateContextMenu(MainTable mainTable, LibraryTab libraryTab, DialogService dialogService) {
+        List<RadioMenuItem> radioMenuItems = new ArrayList<>();
+        mainTable.getColumns().forEach(tableColumn -> radioMenuItems.add(createRadioMenuItem(tableColumn)));
+
+        SeparatorMenuItem line = new SeparatorMenuItem();
+        MenuItem columnsPreferences = new MenuItem("Columns preferences");
+        columnsPreferences.setOnAction(event -> {
+            PreferencesDialogView preferencesDialogView = new PreferencesDialogView(libraryTab.frame());
+            preferencesDialogView.getPreferenceTabList().getSelectionModel().select(3);
+            dialogService.showCustomDialog(preferencesDialogView);
+        });
+
+        this.getItems().clear();
+        this.getItems().addAll(radioMenuItems);
+        this.getItems().addAll(line, columnsPreferences);
+    }
+
+    private RadioMenuItem createRadioMenuItem(TableColumn<BibEntryTableViewModel, ?> tableColumn) {
+        RadioMenuItem radioMenuItem = new RadioMenuItem(tableColumn.getText());
+        return radioMenuItem;
     }
 }
