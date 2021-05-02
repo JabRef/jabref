40cfca6aec5da40711a05620d5cc14ca7b0b157c
BShaq <bulinshaqiri@hotmail.com>
2021-05-02 21:26:41 +0200

    fixed checkstyle issues

--- src/test/java/org/jabref/logic/layout/format/DOICheckTest.java
+++ src/test/java/org/jabref/logic/layout/format/DOICheckTest.java
@@ -5,12 +5,11 @@ import java.util.stream.Stream;
 import org.jabref.logic.layout.LayoutFormatter;
 
 import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DOICheckTest {
 
--- src/test/java/org/jabref/logic/util/strings/StringLengthComparatorTest.java
+++ src/test/java/org/jabref/logic/util/strings/StringLengthComparatorTest.java
@@ -2,10 +2,7 @@ package org.jabref.logic.util.strings;
 
 import java.util.stream.Stream;
 
-import org.jabref.logic.formatter.bibtexfields.NormalizePagesFormatter;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
