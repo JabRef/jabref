46598dd73fee907d7def9cd6e65bfbfc97202bcf
BShaq <bulinshaqiri@hotmail.com>
2021-05-02 20:38:37 +0200

    fixed checkstyle issue

--- src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/ValidCitationKeyCheckerTest.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 
 import org.jabref.logic.l10n.Localization;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidCitationKeyCheckerTest {
 
