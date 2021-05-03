49a9ad872dfd6c477d57a32300bd924ab053d227
ningxie <ning.xie@uzh.ch>
2021-05-03 22:24:48 +0200

    Remove unused import

--- src/test/java/org/jabref/logic/integrity/NoBibTexFieldCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/NoBibTexFieldCheckerTest.java
@@ -10,7 +10,6 @@ import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.field.UnknownField;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
