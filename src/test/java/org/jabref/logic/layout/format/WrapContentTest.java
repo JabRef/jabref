0a628c07e2e73e0ef1872c55be63f61c89961459
BShaq <bulinshaqiri@hotmail.com>
2021-05-03 11:32:31 +0200

    moved setup up to field variable and converted test class to parameterized test

--- src/test/java/org/jabref/logic/layout/format/WrapContentTest.java
+++ src/test/java/org/jabref/logic/layout/format/WrapContentTest.java
@@ -1,65 +1,39 @@
 package org.jabref.logic.layout.format;
 
+import java.util.stream.Stream;
+
 import org.jabref.logic.layout.ParamLayoutFormatter;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WrapContentTest {
 
-    private ParamLayoutFormatter wrapContentParamLayoutFormatter;
+    private ParamLayoutFormatter wrapContentParamLayoutFormatter = new WrapContent();
 
-    @BeforeEach
-    void setup() {
-        wrapContentParamLayoutFormatter = new WrapContent();
+    @ParameterizedTest
+    @MethodSource("provideContent")
+    void formatContent(String formattedContent, String originalContent, String desiredFormat) {
+        if(!desiredFormat.isEmpty()) {
+            wrapContentParamLayoutFormatter.setArgument(desiredFormat);
+        }
+
+        assertEquals(formattedContent, wrapContentParamLayoutFormatter.format(originalContent));
     }
 
-    @Test
-    public void formatSimpleText() {
-        wrapContentParamLayoutFormatter.setArgument("<,>");
-        assertEquals("<Bob>", wrapContentParamLayoutFormatter.format("Bob"));
-    }
-
-    @Test
-    public void formatEmptyStart() {
-        wrapContentParamLayoutFormatter.setArgument(",:");
-        assertEquals("Bob:", wrapContentParamLayoutFormatter.format("Bob"));
-    }
-
-    @Test
-    public void formatEmptyEnd() {
-        wrapContentParamLayoutFormatter.setArgument("Content: ,");
-        assertEquals("Content: Bob", wrapContentParamLayoutFormatter.format("Bob"));
-    }
-
-    @Test
-    public void formatEscaping() {
-        wrapContentParamLayoutFormatter.setArgument("Name\\,Field\\,,\\,Author");
-        assertEquals("Name,Field,Bob,Author", wrapContentParamLayoutFormatter.format("Bob"));
-    }
-
-    @Test
-    public void formatNull() {
-        wrapContentParamLayoutFormatter.setArgument("Eds.,Ed.");
-        assertEquals(null, wrapContentParamLayoutFormatter.format(null));
-    }
-
-    @Test
-    public void formatEmptyString() {
-        wrapContentParamLayoutFormatter.setArgument("Eds.,Ed.");
-        assertEquals("", wrapContentParamLayoutFormatter.format(""));
-    }
-
-    @Test
-    public void noArgumentSetInFormatter() {
-        assertEquals("Bob Bruce and Jolly Jumper", wrapContentParamLayoutFormatter.format("Bob Bruce and Jolly Jumper"));
-    }
-
-    @Test
-    public void formatNoProperArgumentSet() {
-        wrapContentParamLayoutFormatter.setArgument("Eds.");
-        assertEquals("Bob Bruce and Jolly Jumper", wrapContentParamLayoutFormatter.format("Bob Bruce and Jolly Jumper"));
+    private static Stream<Arguments> provideContent() {
+        return Stream.of(
+                Arguments.of("<Bob>", "Bob", "<,>"),
+                Arguments.of("Bob:", "Bob", ",:"),
+                Arguments.of("Content: Bob", "Bob", "Content: ,"),
+                Arguments.of("Name,Field,Bob,Author", "Bob", "Name\\,Field\\,,\\,Author"),
+                Arguments.of(null, null, "Eds.,Ed."),
+                Arguments.of("", "", "Eds.,Ed."),
+                Arguments.of("Bob Bruce and Jolly Jumper", "Bob Bruce and Jolly Jumper", ""),
+                Arguments.of("Bob Bruce and Jolly Jumper", "Bob Bruce and Jolly Jumper", "Eds.")
+        );
     }
 }
