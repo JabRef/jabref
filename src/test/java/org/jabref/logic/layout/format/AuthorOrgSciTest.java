cb43f3baf42c21e6c590fd9e19134a8d328906d4
Dario Safa <dario.akhavansafa@uzh.ch>
2021-05-03 23:54:59 +0200

    AuthorOrgSciTest -> converted into ParametrizedTest

--- src/test/java/org/jabref/logic/layout/format/AuthorOrgSciTest.java
+++ src/test/java/org/jabref/logic/layout/format/AuthorOrgSciTest.java
@@ -1,27 +1,42 @@
 package org.jabref.logic.layout.format;
 
+import java.util.stream.Stream;
+
 import org.jabref.logic.layout.LayoutFormatter;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AuthorOrgSciTest {
 
-    @Test
-    public void testOrgSci() {
-        LayoutFormatter f = new AuthorOrgSci();
+    LayoutFormatter authorOrgNatFormatter = new AuthorOrgSci();
+    LayoutFormatter authorOrgNatFormatterComposite = new CompositeFormat(new AuthorOrgSci(), new NoSpaceBetweenAbbreviations());
 
-        assertEquals("Flynn, J., S. Gartska", f.format("John Flynn and Sabine Gartska"));
-        assertEquals("Garvin, D. A.", f.format("David A. Garvin"));
-        assertEquals("Makridakis, S., S. C. Wheelwright, V. E. McGee", f.format("Sa Makridakis and Sa Ca Wheelwright and Va Ea McGee"));
+    @ParameterizedTest
+    @MethodSource("formatTests")
+    void paramLayoutFormatTest(String expectedString, String inputString, boolean removeSpacesBetweenAbbreviations){
+        if (!removeSpacesBetweenAbbreviations) {
+            assertEquals(expectedString, authorOrgNatFormatter.format(inputString));
+            }
+        else{
+            assertEquals(expectedString, authorOrgNatFormatterComposite.format(inputString));
+        }
     }
 
-    @Test
-    public void testOrgSciPlusAbbreviation() {
-        LayoutFormatter f = new CompositeFormat(new AuthorOrgSci(), new NoSpaceBetweenAbbreviations());
-        assertEquals("Flynn, J., S. Gartska", f.format("John Flynn and Sabine Gartska"));
-        assertEquals("Garvin, D.A.", f.format("David A. Garvin"));
-        assertEquals("Makridakis, S., S.C. Wheelwright, V.E. McGee", f.format("Sa Makridakis and Sa Ca Wheelwright and Va Ea McGee"));
+    private static Stream<Arguments> formatTests() {
+        return Stream.of(
+                //OrgSci Formatting Tests
+                Arguments.of("Flynn, J., S. Gartska", "John Flynn and Sabine Gartska", false),
+                Arguments.of("Garvin, D. A.", "David A. Garvin", false),
+                Arguments.of("Makridakis, S., S. C. Wheelwright, V. E. McGee", "Sa Makridakis and Sa Ca Wheelwright and Va Ea McGee", false),
+
+                //Composite OrgSci Formatting Tests
+                Arguments.of("Flynn, J., S. Gartska", "John Flynn and Sabine Gartska", true),
+                Arguments.of("Garvin, D.A.", "David A. Garvin", true),
+                Arguments.of("Makridakis, S., S.C. Wheelwright, V.E. McGee", "Sa Makridakis and Sa Ca Wheelwright and Va Ea McGee", true)
+        );
     }
 }
