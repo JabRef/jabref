7d1b2ae0f11c9058e1bde55247a4d1cf1b88450a
Dario Safa <dario.akhavansafa@uzh.ch>
2021-05-03 23:53:22 +0200

    IfPluralTest -> converted into ParametrizedTest

--- src/test/java/org/jabref/logic/layout/format/IfPluralTest.java
+++ src/test/java/org/jabref/logic/layout/format/IfPluralTest.java
@@ -1,51 +1,36 @@
 package org.jabref.logic.layout.format;
 
+import java.util.stream.Stream;
+
 import org.jabref.logic.layout.ParamLayoutFormatter;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IfPluralTest {
 
-    @Test
-    public void testStandardUsageOneEditor() {
-        ParamLayoutFormatter a = new IfPlural();
-        a.setArgument("Eds.,Ed.");
-        assertEquals("Ed.", a.format("Bob Bruce"));
+    ParamLayoutFormatter ifPluralFormatter = new IfPlural();
+
+    @ParameterizedTest
+    @MethodSource("formatTests")
+    void testIfPluralFormat(String expectedString, String inputString, String formatterArgument) {
+        if (!formatterArgument.isEmpty()) {
+            ifPluralFormatter.setArgument(formatterArgument);
+        }
+        assertEquals(expectedString, ifPluralFormatter.format(inputString));
     }
 
-    @Test
-    public void testStandardUsageTwoEditors() {
-        ParamLayoutFormatter a = new IfPlural();
-        a.setArgument("Eds.,Ed.");
-        assertEquals("Eds.", a.format("Bob Bruce and Jolly Jumper"));
-    }
-
-    @Test
-    public void testFormatNull() {
-        ParamLayoutFormatter a = new IfPlural();
-        a.setArgument("Eds.,Ed.");
-        assertEquals("", a.format(null));
-    }
-
-    @Test
-    public void testFormatEmpty() {
-        ParamLayoutFormatter a = new IfPlural();
-        a.setArgument("Eds.,Ed.");
-        assertEquals("", a.format(""));
-    }
-
-    @Test
-    public void testNoArgumentSet() {
-        ParamLayoutFormatter a = new IfPlural();
-        assertEquals("", a.format("Bob Bruce and Jolly Jumper"));
-    }
-
-    @Test
-    public void testNoProperArgument() {
-        ParamLayoutFormatter a = new IfPlural();
-        a.setArgument("Eds.");
-        assertEquals("", a.format("Bob Bruce and Jolly Jumper"));
+    private static Stream<Arguments> formatTests() {
+        return Stream.of(
+                Arguments.of("Ed.", "Bob Bruce", "Eds.,Ed."),
+                Arguments.of("Eds.", "Bob Bruce and Jolly Jumper", "Eds.,Ed."),
+                Arguments.of("", null, "Eds.,Ed."),
+                Arguments.of("", "", "Eds.,Ed."),
+                Arguments.of("", "Bob Bruce and Jolly Jumper", ""),
+                Arguments.of("", "Bob Bruce and Jolly Jumper", "Eds.")
+        );
     }
 }
