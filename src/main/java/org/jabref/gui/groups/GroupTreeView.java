35b9a0605400253a240ee7f84bced66b5f13136a
daolin <s4380597@uq.edu.au>
2021-05-04 16:14:35 +0930

    fixed the header title with the latest code

--- src/main/java/org/jabref/gui/groups/GroupTreeView.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeView.java
@@ -348,7 +348,7 @@ public class GroupTreeView {
         MenuItem addSubgroup = new MenuItem(Localization.lang("Add subgroup"));
         addSubgroup.setOnAction(event -> {
             menu.hide();
-            viewModel.addNewSubgroup(group, true);
+            viewModel.addNewSubgroup(group, false);
         });
 
         MenuItem removeSubgroups = new MenuItem(Localization.lang("Remove subgroups"));
