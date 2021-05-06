0c1954c0b5a9987c2fa81c2906b657ae0b46eb9d
Carl Christian Snethlage <cc.snethlage@gmail.com>
2021-05-07 00:40:56 +0200

    Removed static variable

--- src/main/java/org/jabref/gui/preferences/keybindings/presets/NewEntryBindingPreset.java
+++ src/main/java/org/jabref/gui/preferences/keybindings/presets/NewEntryBindingPreset.java
@@ -7,27 +7,6 @@ import org.jabref.gui.keyboard.KeyBinding;
 
 public class NewEntryBindingPreset implements KeyBindingPreset {
 
-    private static final Map<KeyBinding, String> KEY_BINDINGS = new HashMap<>();
-
-    static {
-        // Clear conflicting default presets
-        KEY_BINDINGS.put(KeyBinding.PULL_CHANGES_FROM_SHARED_DATABASE, "");
-        KEY_BINDINGS.put(KeyBinding.COPY_PREVIEW, "");
-
-        // Add new entry presets
-        KEY_BINDINGS.put(KeyBinding.NEW_ARTICLE, "Ctrl+shift+A");
-        KEY_BINDINGS.put(KeyBinding.NEW_BOOK, "Ctrl+shift+B");
-        KEY_BINDINGS.put(KeyBinding.NEW_ENTRY, "Ctrl+N");
-        KEY_BINDINGS.put(KeyBinding.NEW_ENTRY_FROM_PLAIN_TEXT, "Ctrl+shift+N");
-        KEY_BINDINGS.put(KeyBinding.NEW_INBOOK, "Ctrl+shift+I");
-        KEY_BINDINGS.put(KeyBinding.NEW_INPROCEEDINGS, "Ctrl+shift+C");
-        KEY_BINDINGS.put(KeyBinding.NEW_MASTERSTHESIS, "Ctrl+shift+M");
-        KEY_BINDINGS.put(KeyBinding.NEW_PHDTHESIS, "Ctrl+shift+T");
-        KEY_BINDINGS.put(KeyBinding.NEW_PROCEEDINGS, "Ctrl+shift+P");
-        KEY_BINDINGS.put(KeyBinding.NEW_TECHREPORT, "Ctrl+shift+R");
-        KEY_BINDINGS.put(KeyBinding.NEW_UNPUBLISHED, "Ctrl+shift+U");
-    }
-
     @Override
     public String getName() {
         return "New Entries";
@@ -35,6 +14,25 @@ public class NewEntryBindingPreset implements KeyBindingPreset {
 
     @Override
     public Map<KeyBinding, String> getKeyBindings() {
-        return KEY_BINDINGS;
+        final Map<KeyBinding, String> keyBindings = new HashMap<>();
+
+        // Clear conflicting default presets
+        keyBindings.put(KeyBinding.PULL_CHANGES_FROM_SHARED_DATABASE, "");
+        keyBindings.put(KeyBinding.COPY_PREVIEW, "");
+
+        // Add new entry presets
+        keyBindings.put(KeyBinding.NEW_ARTICLE, "Ctrl+shift+A");
+        keyBindings.put(KeyBinding.NEW_BOOK, "Ctrl+shift+B");
+        keyBindings.put(KeyBinding.NEW_ENTRY, "Ctrl+N");
+        keyBindings.put(KeyBinding.NEW_ENTRY_FROM_PLAIN_TEXT, "Ctrl+shift+N");
+        keyBindings.put(KeyBinding.NEW_INBOOK, "Ctrl+shift+I");
+        keyBindings.put(KeyBinding.NEW_INPROCEEDINGS, "Ctrl+shift+C");
+        keyBindings.put(KeyBinding.NEW_MASTERSTHESIS, "Ctrl+shift+M");
+        keyBindings.put(KeyBinding.NEW_PHDTHESIS, "Ctrl+shift+T");
+        keyBindings.put(KeyBinding.NEW_PROCEEDINGS, "Ctrl+shift+P");
+        keyBindings.put(KeyBinding.NEW_TECHREPORT, "Ctrl+shift+R");
+        keyBindings.put(KeyBinding.NEW_UNPUBLISHED, "Ctrl+shift+U");
+
+        return keyBindings;
     }
 }
