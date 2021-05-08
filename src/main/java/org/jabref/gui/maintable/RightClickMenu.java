69e80f53580c824c6f8a2085f4362ec0d3824957
Tiago Maia <jtdmaia@gmail.com>
2021-05-08 12:20:28 +0100

    Added new option in RightClick Mouse Button

--- src/main/java/org/jabref/gui/maintable/RightClickMenu.java
+++ src/main/java/org/jabref/gui/maintable/RightClickMenu.java
@@ -69,6 +69,7 @@ public class RightClickMenu {
 
         contextMenu.getItems().add(factory.createMenuItem(StandardActions.OPEN_FOLDER, new OpenFolderAction(dialogService, stateManager, preferencesService)));
         contextMenu.getItems().add(factory.createMenuItem(StandardActions.OPEN_EXTERNAL_FILE, new OpenExternalFileAction(dialogService, stateManager, preferencesService)));
+        contextMenu.getItems().add(factory.createMenuItem(StandardActions.OPEN_MULTIPLE_EXTERNAL_FILE, new OpenMultipleExternalFilesAction(dialogService, stateManager, preferencesService)));
         contextMenu.getItems().add(factory.createMenuItem(StandardActions.OPEN_URL, new OpenUrlAction(dialogService, stateManager, preferencesService)));
         contextMenu.getItems().add(factory.createMenuItem(StandardActions.SEARCH_SHORTSCIENCE, new SearchShortScienceAction(dialogService, stateManager)));
 
