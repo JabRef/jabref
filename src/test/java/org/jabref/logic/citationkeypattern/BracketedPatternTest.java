a26d0bdc49ce42116e6ee693cc1c4b435b702949
sxcz <11812714@mail.sustech.edu.cn>
2021-05-19 10:50:35 +0800

    fix checkstyle

--- src/test/java/org/jabref/logic/citationkeypattern/BracketedPatternTest.java
+++ src/test/java/org/jabref/logic/citationkeypattern/BracketedPatternTest.java
@@ -1,5 +1,7 @@
 package org.jabref.logic.citationkeypattern;
 
+import java.util.stream.Stream;
+
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibtexString;
@@ -11,8 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
