3c45a8a162de5e30707e14c89b86520f5d46e43e
Antal K <antalk2@gmail.com>
2021-06-03 10:26:37 +0200

    add Tuple3

---
+++ src/main/java/org/jabref/model/openoffice/util/OOTuple3.java
@@ -0,0 +1,13 @@
+package org.jabref.model.openoffice.util;
+
+public class OOTuple3<A, B, C> {
+    public final A a;
+    public final B b;
+    public final C c;
+    public OOTuple3(A a, B b, C c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
