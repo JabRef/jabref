87ff0ef687fa8c720313b9ef80dd60a2df1a6105
Oliver Kopp <kopp.dev@gmail.com>
2021-06-07 23:00:42 +0200

    Fix imports

--- src/test/java/org/jabref/logic/layout/format/EntryTypeFormatterTest.java
+++ src/test/java/org/jabref/logic/layout/format/EntryTypeFormatterTest.java
@@ -1,10 +1,10 @@
 package org.jabref.logic.layout.format;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
