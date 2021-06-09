1c4dce328a1509872bbab5fe0d11a08f14fbf535
Benedikt Tutzer <benedikt.tutzer@gmail.com>
2021-06-09 08:58:37 +0200

    Parameterized test

--- src/test/java/org/jabref/logic/journals/AbbreviationTest.java
+++ src/test/java/org/jabref/logic/journals/AbbreviationTest.java
@@ -1,8 +1,13 @@
 package org.jabref.logic.journals;
 
+import java.util.stream.Stream;
+
 import org.jabref.gui.preferences.journals.AbbreviationViewModel;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -106,12 +111,21 @@ class AbbreviationTest {
         assertEquals(abbreviation.getAbbreviation(), abbreviation.getShortestUniqueAbbreviation());
     }
 
-    @Test
-    void testStringIsFoundInAllAbbreviationFields() {
-        AbbreviationViewModel abbreviationViewModel = new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique"));
-        assertTrue(abbreviationViewModel.containsCaseIndependent("name"));
-        assertTrue(abbreviationViewModel.containsCaseIndependent("bBr"));
-        assertTrue(abbreviationViewModel.containsCaseIndependent("Uniq"));
-        assertFalse(abbreviationViewModel.containsCaseIndependent("Something else"));
+    @ParameterizedTest
+    @MethodSource("provideTestStringIsFoundInAllAbbreviationFields")
+    void testStringIsFoundInAllAbbreviationFields(String searchTerm, AbbreviationViewModel abbreviation, boolean expected) {
+        assertEquals(expected, abbreviation.containsCaseIndependent(searchTerm));
+    }
+
+    private static Stream<Arguments> provideTestStringIsFoundInAllAbbreviationFields() {
+        return Stream.of(
+                Arguments.of("name", new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique")), true),
+                Arguments.of("bBr", new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique")), true),
+                Arguments.of("Uniq", new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique")), true),
+                Arguments.of("Something else", new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique")), false),
+                Arguments.of("", new AbbreviationViewModel(new Abbreviation("Long Name", "abbr", "unique")), true),
+                Arguments.of("", new AbbreviationViewModel(new Abbreviation("", "", "")), true),
+                Arguments.of("Something", new AbbreviationViewModel(new Abbreviation("", "", "")), false)
+        );
     }
 }
