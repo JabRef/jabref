30c9276339e5d48a7b16c149f91679fad4a6cc99
Oliver Kopp <kopp.dev@gmail.com>
2021-06-20 13:32:38 +0200

    Rewrite FieldChangeTest

--- src/test/java/org/jabref/model/FieldChangeTest.java
+++ src/test/java/org/jabref/model/FieldChangeTest.java
@@ -1,63 +1,55 @@
 package org.jabref.model;
 
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FieldChangeTest {
 
-  private FieldChange Fieldchange;
+    private BibEntry entry = new BibEntry()
+            .withField(StandardField.DOI, "foo");
+    private BibEntry entryOther = new BibEntry();
+    private FieldChange fc = new FieldChange(entry, StandardField.DOI, "foo", "bar");
 
-  @Test
-  void testHashCode() {
-    FieldChange fcNull = new FieldChange(null, null, null, null);
-    assertEquals(923521, fcNull.hashCode());
-  }
+    @Test
+    void fieldChangeOnNullEntryNotAllowed() {
+        assertThrows(NullPointerException.class, () -> new FieldChange(null, StandardField.DOI, "foo", "bar"));
+    }
 
-  @Test
-  void testEquals() {
-    BibEntry entry = new BibEntry();
-    BibEntry entryOther = new BibEntry();
-    final Field field = StandardField.DOI;
-    final Field fieldOther = StandardField.DOI;
-    entry.setField(StandardField.DOI, "foo");
-    final String oldValue = "foo";
-    final String newValue = "bar";
-    final String oldValueOther = "fooX";
-    final String newValueOther = "barX";
+    @Test
+    void fieldChangeOnNullFieldNotAllowed() {
+        assertThrows(NullPointerException.class, () -> new FieldChange(entry, null, "foo", "bar"));
+    }
 
-    FieldChange fc = new FieldChange(entry, field, oldValue, newValue);
-    FieldChange fcOther = new FieldChange(entryOther, fieldOther, oldValueOther, newValueOther);
-    FieldChange fcBlankAll = new FieldChange(null, null, null, null);
-    FieldChange fcBlankField = new FieldChange(entry, null, oldValue, newValue);
-    FieldChange fcBlankOldValue = new FieldChange(entry, field, null, newValue);
-    FieldChange fcBlankNewValue = new FieldChange(entry, field, oldValue, null);
+    @Test
+    void blankFieldChangeNotAllowed() {
+        assertThrows(NullPointerException.class, () -> new FieldChange(null, null, null, null));
+    }
 
-    assertFalse(fc.equals("foo"));
-    assertTrue(fc.equals(fc));
-    assertFalse(fcBlankAll.equals(fc));
-    assertFalse(fc.equals(fcOther));
-    assertFalse(fcBlankField.equals(fc));
-    assertFalse(fcBlankOldValue.equals(fc));
-    assertFalse(fcBlankNewValue.equals(fc));
-    assertTrue(fcBlankAll.equals(fcBlankAll));
-  }
+    @Test
+    void equalFieldChange() {
+        FieldChange fcBlankNewValue = new FieldChange(entry, StandardField.DOI, "foo", null);
+        assertNotEquals(fc, fcBlankNewValue);
+    }
 
-  @Test
-  void testToString() {
-    BibEntry entry = new BibEntry();
-    Field field = StandardField.DOI;
-    entry.setCitationKey("CitationKey");
-    final String oldValue = "Old";
-    final String newValue = "New";
+    @Test
+    void fieldChangeDoesNotEqualString() {
+        assertNotEquals(fc, "foo");
+    }
 
-    FieldChange fc = new FieldChange(entry, field, oldValue, newValue);
-    assertEquals("FieldChange [entry=CitationKey, field=DOI, oldValue=Old, newValue=New]", fc.toString());
-  }
+    @Test
+    void fieldChangeEqualsItSelf() {
+        assertEquals(fc, fc);
+    }
+
+    @Test
+    void differentFieldChangeIsNotEqual() {
+        FieldChange fcOther = new FieldChange(entryOther, StandardField.DOI, "fooX", "barX");
+        assertEquals(fc, fcOther);
+    }
 }
