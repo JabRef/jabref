04962f03ee3784f288f57283da1082fa2d45d502
George Liargkovas <t8180061@aueb.gr>
2021-06-21 18:08:15 +0300

    Add FileFilterUtils.java
    The file contains the implementation of the functionality for filtering the unlinked files by specified filters.

---
+++ src/main/java/org/jabref/gui/externalfiles/FileFilterUtils.java
@@ -0,0 +1,81 @@
+package org.jabref.gui.externalfiles;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class FileFilterUtils {
+
+    public static LocalDateTime getFileTime(Path path) {
+        FileTime lastEditedTime = null;
+        try {
+            lastEditedTime = Files.getLastModifiedTime(path);
+        } catch (IOException e) {
+            System.err.println("Could not retrieve file time");
+        }
+        LocalDateTime localDateTime = lastEditedTime
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        return localDateTime;
+    }
+
+    public boolean isDuringLastDay(LocalDateTime fileEditTime) {
+        LocalDateTime NOW = LocalDateTime.now(ZoneId.systemDefault());
+        if (fileEditTime.isAfter(NOW.minusHours(24))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isDuringLastWeek(LocalDateTime fileEditTime) {
+        LocalDateTime NOW = LocalDateTime.now(ZoneId.systemDefault());
+        if (fileEditTime.isAfter(NOW.minusDays(7))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isDuringLastMonth(LocalDateTime fileEditTime) {
+        LocalDateTime NOW = LocalDateTime.now(ZoneId.systemDefault());
+        if (fileEditTime.isAfter(NOW.minusDays(30))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isDuringLastYear(LocalDateTime fileEditTime) {
+        LocalDateTime NOW = LocalDateTime.now(ZoneId.systemDefault());
+        if (fileEditTime.isAfter(NOW.minusYears(1))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean filterByDate(Path path, String filter) {
+        FileFilterUtils fileFilter = new FileFilterUtils();
+        LocalDateTime fileTime = FileFilterUtils.getFileTime(path);
+        switch(filter) {
+            case "Last day":
+                return fileFilter.isDuringLastDay(fileTime);
+            case "Last week":
+                return fileFilter.isDuringLastWeek(fileTime);
+            case "Last month":
+                return fileFilter.isDuringLastMonth(fileTime);
+            case "Last year":
+                return fileFilter.isDuringLastYear(fileTime);
+            case "All time":
+                return true;
+            default:
+                return true;
+
+        }
+    }
+}
