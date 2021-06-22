f49a0aeca134543fa548cf06bddc3d175ee2bdd8
Giorgos Drosos <t8180024@aueb.gr>
2021-06-22 18:37:52 +0300

    Improve code quality of FileFilterUtilsTest

--- src/test/java/org/jabref/gui/externalfiles/FileFilterUtilsTest.java
+++ src/test/java/org/jabref/gui/externalfiles/FileFilterUtilsTest.java
@@ -7,59 +7,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileFilterUtilsTest {
 
+    private FileFilterUtils fileFilterUtils = new FileFilterUtils();
+    private LocalDateTime time = LocalDateTime.now();
+   
     @Test
     public void isDuringLastDayNegativeTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusHours(24);
-        assertEquals(fileFilterUtils.isDuringLastDay(time), false);
+        assertEquals(fileFilterUtils.isDuringLastDay(time.minusHours(24)), false);
     }
 
     @Test
     public void isDuringLastDayPositiveTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusHours(23);
-        assertEquals(fileFilterUtils.isDuringLastDay(time), true);
+        assertEquals(fileFilterUtils.isDuringLastDay(time.minusHours(23)), true);
     }
 
     @Test
     public void isDuringLastWeekNegativeTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusDays(7);
-        assertEquals(fileFilterUtils.isDuringLastWeek(time), false);
+        assertEquals(fileFilterUtils.isDuringLastWeek(time.minusDays(7)), false);
     }
 
     @Test
     public void isDuringLastWeekPositiveTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusDays(6).minusHours(23);
-        assertEquals(fileFilterUtils.isDuringLastWeek(time), true);
+        assertEquals(fileFilterUtils.isDuringLastWeek(time.minusDays(6).minusHours(23)), true);
     }
 
     @Test
     public void isDuringLastMonthNegativeTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusMonths(1);
-        assertEquals(fileFilterUtils.isDuringLastMonth(time), false);
+        assertEquals(fileFilterUtils.isDuringLastMonth(time.minusMonths(1)), false);
     }
 
     @Test
     public void isDuringLastMonthPositiveTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusDays(29).minusHours(23);
-        assertEquals(fileFilterUtils.isDuringLastMonth(time), true);
+        assertEquals(fileFilterUtils.isDuringLastMonth(time.minusDays(29).minusHours(23)), true);
     }
 
     @Test
     public void isDuringLastYearNegativeTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusYears(1);
-        assertEquals(fileFilterUtils.isDuringLastYear(time), false);
+        assertEquals(fileFilterUtils.isDuringLastYear(time.minusYears(1)), false);
     }
 
     @Test
     public void isDuringLastYearPositiveTest() {
-        FileFilterUtils fileFilterUtils = new FileFilterUtils();
-        LocalDateTime time = LocalDateTime.now().minusMonths(11).minusDays(29).minusHours(23);
-        assertEquals(fileFilterUtils.isDuringLastYear(time), true);
+        assertEquals(fileFilterUtils.isDuringLastYear(time.minusMonths(11).minusDays(29).minusHours(23)), true);
     }
 }
