c099ce07bd0b4e04c255b16a07c72472f7fc2e96
George Liargkovas <t8180061@aueb.gr>
2021-06-22 10:00:18 +0300

    Add FileSortViewModel
    Create the view model representing a selected sort-by method.

---
+++ src/main/java/org/jabref/gui/externalfiles/FileSortViewModel.java
@@ -0,0 +1,23 @@
+package org.jabref.gui.externalfiles;
+
+import org.jabref.gui.externalfiles.ExternalFileSorter;
+
+public class FileSortViewModel {
+
+    private final String description;
+    private final String sorter;
+
+    public FileSortViewModel(String sorter) {
+        this.description = sorter;
+        this.sorter = sorter;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getSorter() {
+        return this.sorter;
+    }
+
+}
