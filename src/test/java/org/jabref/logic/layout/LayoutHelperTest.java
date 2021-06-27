906fe1bb1eeb31b99fdbd80a50a7853e54bcab52
Oliver Kopp <kopp.dev@gmail.com>
2021-06-27 23:28:00 +0200

    Fix checkstyle

--- src/test/java/org/jabref/logic/layout/LayoutHelperTest.java
+++ src/test/java/org/jabref/logic/layout/LayoutHelperTest.java
@@ -5,7 +5,8 @@ import java.io.StringReader;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 
 class LayoutHelperTest {
