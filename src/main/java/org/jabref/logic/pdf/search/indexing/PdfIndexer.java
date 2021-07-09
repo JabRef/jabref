2e5818b27e09f34ee5964a9b895f47b8db9981f1
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-07-09 23:41:46 +0200

    Remove unecessary return

--- src/main/java/org/jabref/logic/pdf/search/indexing/PdfIndexer.java
+++ src/main/java/org/jabref/logic/pdf/search/indexing/PdfIndexer.java
@@ -174,7 +174,6 @@ public class PdfIndexer {
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         try (IndexWriter deleter = new IndexWriter(directoryToIndex, config)) {
             // Do nothing. Index is deleted.
-            return;
         } catch (IOException e) {
             LOGGER.warn("The IndexWriter could not be initialized", e);
         }
