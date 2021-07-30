c3729874d5f5600fa64038367610888308185be4
Siedlerchr <siedlerkiller@gmail.com>
2021-07-31 00:01:33 +0200

    Fix file field parser not recognizing online urls
    Fixes #7882

--- src/main/java/org/jabref/logic/importer/util/FileFieldParser.java
+++ src/main/java/org/jabref/logic/importer/util/FileFieldParser.java
@@ -9,7 +9,12 @@ import java.util.List;
 
 import org.jabref.model.entry.LinkedFile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FileFieldParser {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileFieldParser.class);
+
 
     public static List<LinkedFile> parse(String value) {
         List<LinkedFile> files = new ArrayList<>();
@@ -17,6 +22,17 @@ public class FileFieldParser {
         if ((value == null) || value.trim().isEmpty()) {
             return files;
         }
+
+        if (LinkedFile.isOnlineLink(value.trim())) {
+            // needs to be modifiable
+            try {
+                return List.of(new LinkedFile(new URL(value), ""));
+            } catch (MalformedURLException e) {
+                LOGGER.error("invalid url", e);
+                return files;
+            }
+        }
+
 
         List<String> linkedFileData = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
