82866351a5138f26fba86fba090b5883883d113b
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-08-18 15:38:44 +0200

    Checkstyle

--- src/main/java/org/jabref/model/pdf/search/SearchResult.java
+++ src/main/java/org/jabref/model/pdf/search/SearchResult.java
@@ -1,7 +1,6 @@
 package org.jabref.model.pdf.search;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
