44fee74862022b757641aed5b11c17634f80b710
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-08-18 08:50:44 +0200

    Improved display

--- src/main/java/org/jabref/gui/mergeentries/MultiMergeEntriesView.java
+++ src/main/java/org/jabref/gui/mergeentries/MultiMergeEntriesView.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
@@ -282,6 +283,8 @@ public class MultiMergeEntriesView extends BaseDialog<BibEntry> {
                 buttonText.maxWidthProperty().bind(widthProperty().add(-10));
                 buttonText.maxHeightProperty().bind(heightProperty());
                 cellButton.setGraphic(buttonText);
+                cellButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                cellButton.setContentDisplay(ContentDisplay.CENTER);
 
                 // Tooltip
                 Tooltip buttonTooltip = new Tooltip(content);
