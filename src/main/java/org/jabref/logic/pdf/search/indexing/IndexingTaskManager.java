b31bc26c36992850dae8cf4658359c1b545c58c5
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-08-19 19:14:00 +0200

    Localization

--- src/main/java/org/jabref/logic/pdf/search/indexing/IndexingTaskManager.java
+++ src/main/java/org/jabref/logic/pdf/search/indexing/IndexingTaskManager.java
@@ -52,7 +52,7 @@ public class IndexingTaskManager extends BackgroundTask<Void> {
 
     private void updateProgress() {
         DefaultTaskExecutor.runInJavaFXThread(() -> {
-            updateMessage(Localization.lang("%0 of %1 files added to the index", numOfIndexedFiles, numOfIndexedFiles + taskQueue.size()));
+            updateMessage(Localization.lang("%0 of %1 linked files added to the index", numOfIndexedFiles, numOfIndexedFiles + taskQueue.size()));
             updateProgress(numOfIndexedFiles, numOfIndexedFiles + taskQueue.size());
         });
     }
