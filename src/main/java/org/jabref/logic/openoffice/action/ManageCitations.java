21a6351477d92de6150ad196b0039449899935c6
Antal K <antalk2@gmail.com>
2021-08-20 15:02:52 +0200

    sync to improve-reversibility-rebased-03 cb132566f32cba17af24b41d037446a96a90dc16
    commit cb132566f32cba17af24b41d037446a96a90dc16 (HEAD -> improve-reversibility-rebased-03, origin/improve-reversibility-rebased-03)
    Author: Antal K <antalk2@gmail.com>
    Date:   Fri Aug 20 11:39:39 2021 +0200
    
        align dots

--- src/main/java/org/jabref/logic/openoffice/action/ManageCitations.java
+++ src/main/java/org/jabref/logic/openoffice/action/ManageCitations.java
@@ -7,19 +7,19 @@ import org.jabref.model.openoffice.CitationEntry;
 import org.jabref.model.openoffice.uno.NoDocumentException;
 
 import com.sun.star.beans.IllegalTypeException;
-import com.sun.star.beans.NotRemoveableException;
-import com.sun.star.beans.PropertyExistException;
 import com.sun.star.beans.PropertyVetoException;
-import com.sun.star.beans.UnknownPropertyException;
 import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.text.XTextDocument;
 
 public class ManageCitations {
 
+    private ManageCitations() {
+        /**/
+    }
+
     public static List<CitationEntry> getCitationEntries(XTextDocument doc)
         throws
         NoDocumentException,
-        UnknownPropertyException,
         WrappedTargetException {
         OOFrontend fr = new OOFrontend(doc);
         return fr.getCitationEntries(doc);
@@ -28,9 +28,6 @@ public class ManageCitations {
     public static void applyCitationEntries(XTextDocument doc, List<CitationEntry> citationEntries)
         throws
         NoDocumentException,
-        UnknownPropertyException,
-        NotRemoveableException,
-        PropertyExistException,
         PropertyVetoException,
         IllegalTypeException,
         WrappedTargetException,
