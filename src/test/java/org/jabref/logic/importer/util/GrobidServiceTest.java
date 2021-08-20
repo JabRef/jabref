78fe5fa7bda26a4a993070487d9e2aaf47ab5f90
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-08-20 09:28:05 +0200

    Checkstyle

--- src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
+++ src/test/java/org/jabref/logic/importer/util/GrobidServiceTest.java
@@ -21,7 +21,6 @@ import org.mockito.Answers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
