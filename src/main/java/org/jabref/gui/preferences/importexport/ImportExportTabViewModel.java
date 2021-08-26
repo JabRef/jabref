3e7a26c92506b8ae96c76a81489fd14c85e44b89
Carl Christian Snethlage <cc.snethlage@gmail.com>
2021-08-26 13:03:53 +0200

    Fixed checkstyle

--- src/main/java/org/jabref/gui/preferences/importexport/ImportExportTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/importexport/ImportExportTabViewModel.java
@@ -3,7 +3,6 @@ package org.jabref.gui.preferences.importexport;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import javafx.beans.property.BooleanProperty;
