c563ff072eb79cfe39ed5a9588fa44d647fe4f31
colinhex <colin.fingerlin@gmail.com>
2021-10-25 21:43:09 +0200

    convert to parametrized test

--- src/test/java/org/jabref/model/entry/identifier/IacrEprintTest.java
+++ src/test/java/org/jabref/model/entry/identifier/IacrEprintTest.java
@@ -1,20 +1,44 @@
 package org.jabref.model.entry.identifier;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IacrEprintTest {
 
-    @Test
-    public void acceptPlainIacrEprint() {
-        assertEquals("2019/001", new IacrEprint("2019/001").getNormalized());
-    }
-
-    @Test
-    public void ignoreLeadingAndTrailingWhitespaces() {
-        assertEquals("2019/001", new IacrEprint(" 2019/001   ").getNormalized());
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.arguments(
+                        "acceptPlainIacrEprint",
+                        new IacrEprint("2019/001").getNormalized()
+                ),
+                Arguments.arguments(
+                        "ignoreLeadingAndTrailingWhitespaces",
+                        new IacrEprint(" 2019/001   ").getNormalized()
+                ),
+                Arguments.arguments(
+                        "acceptFullUrlIacrEprint",
+                        new IacrEprint("https://eprint.iacr.org/2019/001").getNormalized()
+                ),
+                Arguments.arguments(
+                        "acceptShortenedUrlIacrEprint",
+                        new IacrEprint("https://ia.cr/2019/001").getNormalized()
+                ),
+                Arguments.arguments(
+                        "acceptDomainUrlIacrEprint",
+                        new IacrEprint("eprint.iacr.org/2019/001").getNormalized()
+                ),
+                Arguments.arguments(
+                        "acceptShortenedDomainUrlIacrEprint",
+                        new IacrEprint("ia.cr/2019/001").getNormalized()
+                )
+        );
     }
 
     @Test
@@ -22,24 +46,10 @@ public class IacrEprintTest {
         assertThrows(IllegalArgumentException.class, () -> new IacrEprint("2021/12"));
     }
 
-    @Test
-    public void acceptFullUrlIacrEprint() {
-        assertEquals("2019/001", new IacrEprint("https://eprint.iacr.org/2019/001").getNormalized());
-    }
-
-    @Test
-    public void acceptShortenedUrlIacrEprint() {
-        assertEquals("2019/001", new IacrEprint("https://ia.cr/2019/001").getNormalized());
-    }
-
-    @Test
-    public void acceptDomainUrlIacrEprint() {
-        assertEquals("2019/001", new IacrEprint("eprint.iacr.org/2019/001").getNormalized());
-    }
-
-    @Test
-    public void acceptShortenedDomainUrlIacrEprint() {
-        assertEquals("2019/001", new IacrEprint("ia.cr/2019/001").getNormalized());
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("provideTestData")
+    public void acceptCorrectIacrEprintIdentifier(String name, String arg) {
+        assertEquals("2019/001", arg);
     }
 
     @Test
