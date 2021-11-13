929a26811bb5adf18b232ea7e34978a14941fe08
Oliver Kopp <kopp.dev@gmail.com>
2021-11-13 12:00:31 +0100

    Fix checkstyle

--- src/main/java/org/jabref/logic/util/OS.java
+++ src/main/java/org/jabref/logic/util/OS.java
@@ -6,10 +6,10 @@ import java.util.Locale;
  * Operating system (OS) detection
  */
 public class OS {
+    public static final String NEWLINE = System.lineSeparator();
+
     // File separator obtained from system
     private static final String FILE_SEPARATOR = System.getProperty("file.separator");
-
-    public static final String NEWLINE = System.lineSeparator();
 
     // https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/org/apache/commons/lang/SystemUtils.html
     private static final String OS_NAME = System.getProperty("os.name", "unknown").toLowerCase(Locale.ROOT);
