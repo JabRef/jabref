bf482a93c4af9390da35ba425ae6bb0cbc30514d
Benedikt Tutzer <benedikt_tutzer@yahoo.de>
2021-12-06 21:52:39 +0100

    Rename context-menu because we do not only write xmp

--- src/main/java/org/jabref/gui/keyboard/KeyBinding.java
+++ src/main/java/org/jabref/gui/keyboard/KeyBinding.java
@@ -106,7 +106,7 @@ public enum KeyBinding {
     UNABBREVIATE("Unabbreviate", Localization.lang("Unabbreviate"), "ctrl+alt+shift+A", KeyBindingCategory.TOOLS),
     UNDO("Undo", Localization.lang("Undo"), "ctrl+Z", KeyBindingCategory.EDIT),
     WEB_SEARCH("Web search", Localization.lang("Web search"), "alt+4", KeyBindingCategory.SEARCH),
-    WRITE_XMP("Write XMP", Localization.lang("Write XMP"), "F6", KeyBindingCategory.TOOLS),
+    WRITE_METADATA_TO_PDF("Write XMP", Localization.lang("Write XMP"), "F6", KeyBindingCategory.TOOLS),
     CLEAR_SEARCH("Clear search", Localization.lang("Clear search"), "ESCAPE", KeyBindingCategory.SEARCH),
     CLEAR_READ_STATUS("Clear read status", Localization.lang("Clear read status"), "", KeyBindingCategory.EDIT),
     READ("Set read status to read", Localization.lang("Set read status to read"), "", KeyBindingCategory.EDIT),
