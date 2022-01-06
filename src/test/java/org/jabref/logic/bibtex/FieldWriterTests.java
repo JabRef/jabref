8b2f76f91edd6eb78358c89bff4e1aa340734605
Siedlerchr <siedlerkiller@gmail.com>
2022-01-06 21:52:10 +0100

    fix checkstyle

--- src/test/java/org/jabref/logic/bibtex/FieldWriterTests.java
+++ src/test/java/org/jabref/logic/bibtex/FieldWriterTests.java
@@ -8,6 +8,7 @@ import org.jabref.model.entry.field.UnknownField;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
