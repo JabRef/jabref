1303bdd3781309b13a18a8e4f86e49bd151ff0b9
Siedlerchr <siedlerkiller@gmail.com>
2022-01-09 15:18:31 +0100

    fix unit test

--- src/test/java/org/jabref/logic/importer/fileformat/BibtexImporterTest.java
+++ src/test/java/org/jabref/logic/importer/fileformat/BibtexImporterTest.java
@@ -115,9 +115,7 @@ public class BibtexImporterTest {
     @Test
     public void testGetDescription() {
         assertEquals(
-                "This importer exists only to enable `--importToOpen someEntry.bib`\n"
-                        + "It is NOT intended to import a BIB file. This is done via the option action, which treats the metadata fields.\n"
-                        + "The metadata is not required to be read here, as this class is NOT called at --import.",
+                "This importer enables `--importToOpen someEntry.bib`",
                 importer.getDescription());
     }
 
