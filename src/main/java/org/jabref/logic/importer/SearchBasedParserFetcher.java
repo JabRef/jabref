f451c3eeabec3b975be692d5fe0635d1a6c2471d
Siedlerchr <siedlerkiller@gmail.com>
2022-03-07 22:58:06 +0100

    fix checkstyle javadoc

--- src/main/java/org/jabref/logic/importer/SearchBasedParserFetcher.java
+++ src/main/java/org/jabref/logic/importer/SearchBasedParserFetcher.java
@@ -30,7 +30,7 @@ import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
  * <p>
  *     Note that this interface "should" be an abstract class.
  *     However, Java does not support multi inheritance with classes (but with interfaces).
- *     We need multi inheritance, because a fetcher might implement multiple query types (such as id fetching {@link IdBasedFetcher)}, complete entry {@link EntryBasedFetcher}, and search-based fetcher (this class).
+ *     We need multi inheritance, because a fetcher might implement multiple query types (such as id fetching {@link IdBasedFetcher}), complete entry {@link EntryBasedFetcher}, and search-based fetcher (this class).
  * </p>
  */
 public interface SearchBasedParserFetcher extends SearchBasedFetcher {
