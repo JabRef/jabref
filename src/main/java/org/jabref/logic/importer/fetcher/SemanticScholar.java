707a1159b3433561e8514c181a257a1920363c05
etienne428 <etienne.428@hotmail.com>
2022-03-12 22:45:04 +0100

    Web search works for SemanticScholar

--- src/main/java/org/jabref/logic/importer/fetcher/SemanticScholar.java
+++ src/main/java/org/jabref/logic/importer/fetcher/SemanticScholar.java
@@ -1,31 +1,49 @@
 package org.jabref.logic.importer.fetcher;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.FulltextFetcher;
+import org.jabref.logic.importer.PagedSearchBasedParserFetcher;
+import org.jabref.logic.importer.ParseException;
+import org.jabref.logic.importer.Parser;
+import org.jabref.logic.importer.fetcher.transformers.DefaultQueryTransformer;
+import org.jabref.logic.importer.util.JsonReader;
 import org.jabref.logic.net.URLDownload;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.identifier.ArXivIdentifier;
 import org.jabref.model.entry.identifier.DOI;
+import org.jabref.model.entry.types.StandardEntryType;
 
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONException;
 import kong.unirest.json.JSONObject;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.jabref.model.entry.field.StandardField.EPRINT;
 
-public class SemanticScholar implements FulltextFetcher {
+public class SemanticScholar implements FulltextFetcher, PagedSearchBasedParserFetcher {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SemanticScholar.class);
 
-    private static final String SOURCE = "https://api.semanticscholar.org/v1/paper/%s";
+    private static final String SOURCE_ID_SEARCH = "https://api.semanticscholar.org/v1/paper/";
+    private static final String SOURCE_WEB_SEARCH = "https://api.semanticscholar.org/graph/v1/paper/search?";
+    private int PAGE_SIZE = PagedSearchBasedParserFetcher.super.getPageSize();
 
     /**
      * Tries to find a fulltext URL for a given BibTex entry.
@@ -35,7 +53,7 @@ public class SemanticScholar implements FulltextFetcher {
      * @param entry The Bibtex entry
      * @return The fulltext PDF URL Optional, if found, or an empty Optional if not found.
      * @throws NullPointerException if no BibTex entry is given
-     * @throws IOException if one of the URL is not correct
+     * @throws IOException          if one of the URL is not correct
      */
     @Override
     public Optional<URL> findFullText(BibEntry entry) throws IOException {
@@ -48,7 +66,7 @@ public class SemanticScholar implements FulltextFetcher {
         if (doi.isPresent()) {
             try {
                 // Retrieve PDF link
-                String source = String.format(SOURCE, doi.get().getDOI());
+                String source = SOURCE_ID_SEARCH + doi.get().getDOI();
                 html = Jsoup.connect(getURLBySource(source))
                         .userAgent(URLDownload.USER_AGENT)
                         .referrer("https://www.google.com")
@@ -64,7 +82,7 @@ public class SemanticScholar implements FulltextFetcher {
             if (!arXivString.startsWith("arXiv:")) {
                 arXivString = "arXiv:" + arXivString;
             }
-            String source = String.format(SOURCE, arXivString);
+            String source = SOURCE_ID_SEARCH + arXivString;
             html = Jsoup.connect(getURLBySource(source))
                     .userAgent(URLDownload.USER_AGENT)
                     .referrer("https://www.google.com")
@@ -78,7 +96,9 @@ public class SemanticScholar implements FulltextFetcher {
         // Retrieve PDF link from button on the webpage
         // First checked is a drop-down menu, as it has the correct URL if present
         Elements metaLinks = html.getElementsByClass("flex-item alternate-sources__dropdown");
-        String link = metaLinks.select("a").attr("href");
+        // TODO verbessern
+        String link = metaLinks.select("a[href^=https]").select("a[href$=.pdf]").attr("href");
+        link = metaLinks.select("a").attr("href");
         if (link.length() < 10) {
             metaLinks = html.getElementsByClass("flex-paper-actions__button--primary");
             link = metaLinks.select("a").attr("href");
@@ -89,7 +109,7 @@ public class SemanticScholar implements FulltextFetcher {
 
     @Override
     public TrustLevel getTrustLevel() {
-        return TrustLevel.PUBLISHER;
+        return TrustLevel.META_SEARCH;
     }
 
     /**
@@ -102,6 +122,130 @@ public class SemanticScholar implements FulltextFetcher {
     public String getURLBySource(String source) throws IOException {
         URLDownload download = new URLDownload(source);
         JSONObject json = new JSONObject(download.asString());
+        LOGGER.debug(json.get("url").toString());
         return json.get("url").toString();
     }
+
+    /**
+     * Constructs a URL based on the query, size and page number.
+     *
+     * @param luceneQuery the search query
+     * @param pageNumber  the number of the page indexed from 0
+     */
+    @Override
+    public URL getURLForQuery(QueryNode luceneQuery, int pageNumber) throws URISyntaxException, MalformedURLException, FetcherException {
+        URIBuilder uriBuilder = new URIBuilder(SOURCE_WEB_SEARCH);
+        uriBuilder.addParameter("query", new DefaultQueryTransformer().transformLuceneQuery(luceneQuery).orElse(""));
+        uriBuilder.addParameter("offset", String.valueOf(pageNumber * getPageSize()));
+        uriBuilder.addParameter("limit", String.valueOf(Math.min(getPageSize(), 10000 - pageNumber * getPageSize())));
+        // All fields need to be specified
+        uriBuilder.addParameter("fields", "paperId,externalIds,url,title,abstract,venue,year,authors");
+        LOGGER.debug(uriBuilder.build().toURL().toString());
+        return uriBuilder.build().toURL();
+    }
+
+    /**
+     * Returns the parser used to convert the response to a list of {@link BibEntry}.
+     */
+    @Override
+    public Parser getParser() {
+        return inputStream -> {
+
+            JSONObject response = JsonReader.toJsonObject(inputStream);
+            LOGGER.debug(response.toString());
+            if (response.isEmpty()) {
+                return Collections.emptyList();
+            }
+
+            int total = response.getInt("total");
+            if (total == 0) {
+                return Collections.emptyList();
+            } else if (response.has("next")) {
+                total = Math.min(total, response.getInt("next") - response.getInt("offset"));
+            }
+
+//            if (!response.has("data")) {
+//                // Singleton response
+//                BibEntry entry = jsonItemToBibEntry(response);
+//                return Collections.singletonList(entry);
+//            }
+
+            // Response contains a list
+            JSONArray items = response.getJSONArray("data");
+            List<BibEntry> entries = new ArrayList<>(items.length());
+            for (int i = 0; i < total; i++) {
+                JSONObject item = items.getJSONObject(i);
+                BibEntry entry = jsonItemToBibEntry(item);
+                entries.add(entry);
+            }
+
+            return entries;
+        };
+    }
+
+    /**
+     * This is copy-paste from CrossRef, need to be checked.
+     *
+     * @param item an entry received, needs to be parsed into a BibEntry
+     * @return The BibEntry that corresponds to the received object
+     */
+    private BibEntry jsonItemToBibEntry(JSONObject item) throws ParseException {
+        try {
+            BibEntry entry = new BibEntry(StandardEntryType.Article);
+            entry.setField(StandardField.URL, item.optString("url"));
+            entry.setField(StandardField.TITLE, item.optString("title"));
+            entry.setField(StandardField.ABSTRACT, item.optString("abstract"));
+            entry.setField(StandardField.VENUE, item.optString("venue"));
+            entry.setField(StandardField.YEAR, item.optString("year"));
+
+            entry.setField(StandardField.AUTHOR, toAuthors(item.optJSONArray("authors")));
+
+            // TODO: check. I'm not sure about the EPRINT and arXiv fields
+            JSONObject externalIds = item.optJSONObject("externalIds");
+            entry.setField(StandardField.DOI, externalIds.optString("DOI"));
+            if (externalIds.has("ArXiv")) {
+                entry.setField(EPRINT, externalIds.getString("ArXiv"));
+                entry.setField(StandardField.ARCHIVEPREFIX, "arXiv");
+            }
+            entry.setField(StandardField.PMID, externalIds.optString("PubMed"));
+//            exIds.addAll(externalIds.keySet());
+            return entry;
+        } catch (JSONException exception) {
+            throw new ParseException("SemanticScholar API JSON format has changed", exception);
+        }
+    }
+
+    private String toAuthors(JSONArray author) {
+        if (author == null || author.isEmpty()) {
+            return "";
+        }
+        StringBuilder authors = new StringBuilder();
+        for (int i = 0; i < author.length(); i++) {
+            JSONObject item = author.getJSONObject(i);
+            authors.append(item.getString("name")).append(" and ");
+        }
+        authors.delete(authors.length() - 5, authors.length());
+        return authors.toString();
+    }
+
+    /**
+     * Returns the localized name of this fetcher.
+     * The title can be used to display the fetcher in the menu and in the side pane.
+     *
+     * @return the localized name
+     */
+    @Override
+    public String getName() {
+        return "SemanticScholar";
+    }
+
+    public void setPageSize(int size) {
+        PAGE_SIZE = size;
+    }
+
+    @Override
+    public int getPageSize() {
+        return PAGE_SIZE;
+    }
 }
+
--- src/test/java/org/jabref/logic/importer/fetcher/SemanticScholarTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/SemanticScholarTest.java
@@ -1,21 +1,31 @@
 package org.jabref.logic.importer.fetcher;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.PagedSearchBasedFetcher;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.jabref.support.DisabledOnCIServer;
 import org.jabref.testutils.category.FetcherTest;
 
+import org.apache.lucene.queryparser.flexible.core.QueryNodeParseException;
+import org.apache.lucene.queryparser.flexible.core.parser.SyntaxParser;
+import org.apache.lucene.queryparser.flexible.standard.parser.StandardSyntaxParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @FetcherTest
-public class SemanticScholarTest {
+public class SemanticScholarTest implements PagedSearchFetcherTest {
 
 
     private static final String URL_PDF = "http://dl.ifip.org/db/conf/networking/networking2021/1570714032.pdf";
@@ -30,41 +40,45 @@ public class SemanticScholarTest {
     private static final String URL_PDF4 = "https://arxiv.org/pdf/1407.3561.pdf";
     private static final String ARXIV = "1407.3561";
 
-    private SemanticScholar finder;
+    private final BibEntry IGOR_NEWCOMERS = new BibEntry(StandardEntryType.Article)
+            .withField(StandardField.AUTHOR, "Igor Steinmacher and Tayana Conte and Christoph Treude and M. Gerosa")
+            .withField(StandardField.YEAR, "2016")
+            .withField(StandardField.DOI, "10.1145/2884781.2884806")
+            .withField(StandardField.TITLE, "Overcoming Open Source Project Entry Barriers with a Portal for Newcomers")
+            .withField(StandardField.URL, "https://www.semanticscholar.org/paper/4bea2b4029a895bf898701329409e5a784fc2090")
+            .withField(StandardField.VENUE, "2016 IEEE/ACM 38th International Conference on Software Engineering (ICSE)");
+
+    private SemanticScholar fetcher;
+    private BibEntry entryfinal;
     private BibEntry entry;
-    private BibEntry entry2;
     private BibEntry entry3;
     private BibEntry entry4;
 
     @BeforeEach
     void setUp() {
-        finder = new SemanticScholar();
+        fetcher = new SemanticScholar();
+        entryfinal = new BibEntry();
         entry = new BibEntry().withField(StandardField.DOI, DOI);
-        entry2 = new BibEntry().withField(StandardField.DOI, DOI2);
         entry3 = new BibEntry().withField(StandardField.DOI, DOI3);
         entry4 = new BibEntry().withField(StandardField.EPRINT, ARXIV)
-                .withField(StandardField.EPRINTTYPE, "arXiv")
                 .withField(StandardField.ARCHIVEPREFIX, "arXiv");
     }
 
     @Test
-    void getDocument() {
-        String source = null;
-        try {
-            source = finder.getURLBySource(
-                    String.format("https://api.semanticscholar.org/v1/paper/%s", entry.getDOI().get().getDOI()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    void getDocument() throws IOException {
+        String source = fetcher.getURLBySource(
+                String.format("https://api.semanticscholar.org/v1/paper/%s", DOI));
+
         assertEquals("https://www.semanticscholar.org/paper/7f7b38604a2c167f6d5fb1c5dffcbb127d0525c0", source);
     }
 
     @Test
     @DisabledOnCIServer("CI server is unreliable")
     void fullTextFindByDOI() throws IOException {
+        entryfinal.withField(StandardField.DOI, DOI2);
         assertEquals(
                 Optional.of(new URL(URL_PDF2)),
-                finder.findFullText(entry2)
+                fetcher.findFullText(entryfinal)
         );
     }
 
@@ -73,7 +87,7 @@ public class SemanticScholarTest {
     void fullTextFindByDOIAlternate() throws IOException {
         assertEquals(
                 Optional.of(new URL(URL_PDF3)),
-                finder.findFullText(entry3)
+                fetcher.findFullText(entry3)
         );
     }
 
@@ -81,7 +95,7 @@ public class SemanticScholarTest {
     @DisabledOnCIServer("CI server is unreliable")
     void fullTextSearchOnEmptyEntry() throws IOException {
 
-        assertEquals(Optional.empty(), finder.findFullText(new BibEntry()));
+        assertEquals(Optional.empty(), fetcher.findFullText(new BibEntry()));
     }
 
     @Test
@@ -89,7 +103,7 @@ public class SemanticScholarTest {
     void fullTextNotFoundByDOI() throws IOException {
         entry.setField(StandardField.DOI, "10.1021/bk-2006-WWW.ch014");
 
-        assertEquals(Optional.empty(), finder.findFullText(entry));
+        assertEquals(Optional.empty(), fetcher.findFullText(entry));
     }
 
     @Test
@@ -97,17 +111,61 @@ public class SemanticScholarTest {
     void fullTextFindByArXiv() throws IOException {
         assertEquals(
                 Optional.of(new URL(URL_PDF4)),
-                finder.findFullText(entry4)
+                fetcher.findFullText(entry4)
         );
     }
 
     @Test
     void fullTextEntityWithoutDoi() throws IOException {
-        assertEquals(Optional.empty(), finder.findFullText(new BibEntry()));
+        assertEquals(Optional.empty(), fetcher.findFullText(new BibEntry()));
     }
 
     @Test
     void trustLevel() {
-        assertEquals(TrustLevel.PUBLISHER, finder.getTrustLevel());
+        assertEquals(TrustLevel.META_SEARCH, fetcher.getTrustLevel());
+    }
+
+    @Override
+    public PagedSearchBasedFetcher getPagedFetcher() {
+        return fetcher;
+    }
+
+    @Test
+    void getURLForQueryWithLucene() throws QueryNodeParseException, MalformedURLException, FetcherException, URISyntaxException {
+        String query = "Software engineering";
+        SyntaxParser parser = new StandardSyntaxParser();
+        URL url = fetcher.getURLForQuery(parser.parse(query, "default"), 0);
+        assertEquals("https://api.semanticscholar.org/graph/v1/paper/search?query=Software+engineering&offset=0&limit=20&fields=paperId%2CexternalIds%2Curl%2Ctitle%2Cabstract%2Cvenue%2Cyear%2Cauthors", url.toString());
+    }
+
+    @Test
+    void searchByQueryFindsEntry() throws Exception {
+        BibEntry master = new BibEntry(StandardEntryType.Article)
+                .withField(StandardField.AUTHOR, "Tobias Diez")
+                .withField(StandardField.TITLE, "Slice theorem for Fréchet group actions and covariant symplectic field theory")
+                .withField(StandardField.YEAR, "2014")
+                .withField(StandardField.EPRINT, "1405.2249")
+                .withField(StandardField.ARCHIVEPREFIX, "arXiv")
+                .withField(StandardField.ABSTRACT, "A general slice theorem for the action of a Fr\\'echet Lie group on a Fr\\'echet manifolds is established. The Nash-Moser theorem provides the fundamental tool to generalize the result of Palais to this infinite-dimensional setting. The presented slice theorem is illustrated by its application to gauge theories: the action of the gauge transformation group admits smooth slices at every point and thus the gauge orbit space is stratified by Fr\\'echet manifolds. \n" +
+                        "Furthermore, a covariant and symplectic formulation of classical field theory is proposed and extensively discussed. At the root of this novel framework is the incorporation of field degrees of freedom F and spacetime M into the product manifold F * M. The induced bigrading of differential forms is used in order to carry over the usual symplectic theory to this new setting. The examples of the Klein-Gordon field and general Yang-Mills theory illustrate that the presented approach conveniently handles the occurring symmetries.")
+                .withField(StandardField.URL, "https://www.semanticscholar.org/paper/4986c1060236e7190b63f934df7806fbf2056cec");
+        List<BibEntry> fetchedEntries = fetcher.performSearch("Slice theorem for Fréchet group actions and covariant symplectic");
+        assertEquals(Collections.singletonList(master), fetchedEntries);
+    }
+
+    @Test
+    void searchByPlainQueryFindsEntry() throws Exception {
+        List<BibEntry> fetchedEntries = fetcher.performSearch("Overcoming Open Source Project Entry Barriers with a Portal for Newcomers");
+        // Abstract should not be included in JabRef tests
+        fetchedEntries.forEach(entry -> entry.clearField(StandardField.ABSTRACT));
+        assertEquals(Collections.singletonList(IGOR_NEWCOMERS), fetchedEntries);
+    }
+
+    @Test
+    void searchByQuotedQueryFindsEntry() throws Exception {
+        List<BibEntry> fetchedEntries = fetcher.performSearch("\"Overcoming Open Source Project Entry Barriers with a Portal for Newcomers\"");
+        // Abstract should not be included in JabRef tests
+        fetchedEntries.forEach(entry -> entry.clearField(StandardField.ABSTRACT));
+        assertEquals(Collections.singletonList(IGOR_NEWCOMERS), fetchedEntries);
     }
 }
