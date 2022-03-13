44f005d0b66e586a637c052c5d5dc32e8d775d02
Siedlerchr <siedlerkiller@gmail.com>
2022-03-13 16:30:27 +0100

    Add more logging output

--- src/main/java/org/jabref/logic/citationstyle/CitationStyle.java
+++ src/main/java/org/jabref/logic/citationstyle/CitationStyle.java
@@ -22,6 +22,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.jabref.logic.util.StandardFileType;
 
+import de.undercouch.citeproc.CSL;
 import de.undercouch.citeproc.helper.CSLUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,10 +61,12 @@ public class CitationStyle {
     private static Optional<CitationStyle> createCitationStyleFromSource(final String source, final String filename) {
         if ((filename != null) && !filename.isEmpty() && (source != null) && !source.isEmpty()) {
             try {
+                LOGGER.debug("creating file from source");
                 InputSource is = new InputSource();
                 is.setCharacterStream(new StringReader(stripInvalidProlog(source)));
 
                 Document doc = FACTORY.newDocumentBuilder().parse(is);
+
                 NodeList nodes = doc.getElementsByTagName("info");
 
                 NodeList titleNode = ((Element) nodes.item(0)).getElementsByTagName("title");
@@ -96,6 +99,12 @@ public class CitationStyle {
         }
 
         try {
+            CSL.canFormatBibliographies(styleFile);
+        } catch (IOException e1) {
+            LOGGER.debug("cannt load", e1);
+        }
+
+        try {
             String text;
             String internalFile = STYLES_ROOT + (styleFile.startsWith("/") ? "" : "/") + styleFile;
             URL url = CitationStyle.class.getResource(internalFile);
@@ -104,6 +113,7 @@ public class CitationStyle {
             if (url != null) {
                 text = CSLUtils.readURLToString(url, StandardCharsets.UTF_8.toString());
             } else {
+                LOGGER.debug("outside of classpath {}", Path.of(styleFile));
                 // if the url is null then the style is located outside the classpath
                 text = new String(Files.readAllBytes(Path.of(styleFile)), StandardCharsets.UTF_8);
             }
