bc002fa3840958f7a566f2121831465fd6c8df2d
Carlos Zoft <carlosk2gunn@hotmail.com>
2022-03-27 20:41:23 -0300

    Fix order of imports

--- src/test/java/org/jabref/model/FieldChangeTest.java
+++ src/test/java/org/jabref/model/FieldChangeTest.java
@@ -6,10 +6,10 @@ import org.jabref.model.entry.field.StandardField;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class FieldChangeTest {
 
