d9da5761473bcfa59c31d12a4a46a932a03765e9
luiz-herique <luizhemrique.fnandes@gmail.com>
2022-03-26 21:56:22 -0300

    include tests

--- src/test/java/org/jabref/logic/bibtex/comparator/PreambleDiffTest.java
+++ src/test/java/org/jabref/logic/bibtex/comparator/PreambleDiffTest.java
@@ -1,14 +1,15 @@
 package org.jabref.logic.bibtex.comparator;
 
-import java.util.Optional;
-
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.database.BibDatabaseContext;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,5 +42,38 @@ public class PreambleDiffTest {
         Optional<PreambleDiff> expected = Optional.of(new PreambleDiff("preamble", "otherPreamble"));
         Optional<PreambleDiff> result = PreambleDiff.compare(originalDataBaseContext, newDataBaseContext);
         assertEquals(expected, result);
+    }
+
+    @Test // Case test 3
+    void equalNullPreambleTest() {
+        PreambleDiff preambletest = new PreambleDiff("original", "new");
+        assertTrue(!preambletest.equals(null));
+    }
+
+    @Test // Case test 1
+    void equalOtherPreambleTest() {
+        PreambleDiff preambletest = new PreambleDiff("original", "new");
+        assertTrue(preambletest.equals(preambletest));
+    }
+
+    @Test // Case test 4
+    void equalSameClassTest() {
+        PreambleDiff preambletest = new PreambleDiff("original", "new");
+        String otherclasstest = new String("string");
+        assertTrue(!preambletest.equals(otherclasstest));
+    }
+
+    @Test // Case test 2
+    void equalSameAttributesTest() {
+        PreambleDiff preambletest = new PreambleDiff("original", "new");
+        PreambleDiff preambleattributestest = new PreambleDiff("original", "new");
+        assertTrue(preambletest.equals(preambleattributestest));
+    }
+
+    @Test // Case test 5
+    void equalDiferentAttributesTest() {
+        PreambleDiff preambletest = new PreambleDiff("original", "new");
+        PreambleDiff preambleattributestest = new PreambleDiff("original false", "new false");
+        assertTrue(!preambletest.equals(preambleattributestest));
     }
 }
