26bf1150d06b9e6cbbfed7a7b11efcce2b6eb24c
Siedlerchr <siedlerkiller@gmail.com>
2022-04-09 19:22:36 +0200

    checkstyle

--- src/main/java/org/jabref/logic/exporter/EmbeddedBibFilePdfExporter.java
+++ src/main/java/org/jabref/logic/exporter/EmbeddedBibFilePdfExporter.java
@@ -119,7 +119,7 @@ public class EmbeddedBibFilePdfExporter extends Exporter {
                 nameDictionary.setEmbeddedFiles(efTree);
                 document.getDocumentCatalog().setNames(nameDictionary);
             }
-            Path newFile = Path.of(file.toAbsolutePath().toString()+ ".copy");
+            Path newFile = Path.of(file.toAbsolutePath().toString() + ".copy");
             document.save(newFile.toFile());
             FileUtil.copyFile(newFile, file, true);
             Files.delete(newFile);
