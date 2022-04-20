ce04ff0bc920fc26f8c93ebf7a40ee9f4af02cbb
Oliver Kopp <kopp.dev@gmail.com>
2022-04-20 23:47:01 +0200

    Add more test cases (and convert to parameterized tests)
    Co-authored-by: Christoph <siedlerkiller@gmail.com>
    Co-authored-by: Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
    Co-authored-by: ThiloteE <73715071+ThiloteE@users.noreply.github.com>

--- src/test/java/org/jabref/logic/formatter/bibtexfields/EscapeDollarSignFormatterTest.java
+++ src/test/java/org/jabref/logic/formatter/bibtexfields/EscapeDollarSignFormatterTest.java
@@ -1,27 +1,37 @@
 package org.jabref.logic.formatter.bibtexfields;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EscapeDollarSignFormatterTest {
 
-    private EscapeDollarSignFormatter formatter;
+    private EscapeDollarSignFormatter formatter = new EscapeDollarSignFormatter();
 
-    @BeforeEach
-    void setUp() {
-        formatter = new EscapeDollarSignFormatter();
+    private static Stream<Arguments> correctlyFormats() {
+        return Stream.of(
+                // formatReturnsSameTextIfNoDollarSignPresent
+                Arguments.of("Lorem ipsum", "Lorem ipsum"),
+                // formatEscapesDollarSignIfPresent
+                Arguments.of("Lorem\\$ipsum", "Lorem$ipsum"),
+                // keeps escapings
+                Arguments.of("Lorem\\$ipsum", "Lorem\\$ipsum"),
+                Arguments.of("Dollar sign: 1x: \\$ 2x: \\$\\$ 3x: \\$\\$\\$", "Dollar sign: 1x: \\$ 2x: \\$\\$ 3x: \\$\\$\\$")
+                // mixed field
+                Arguments.of("Dollar sign: 1x: \\$ 2x: \\$\\$ 3x: \\$\\$\\$", "Dollar sign: 1x: $ 2x: $$ 3x: $$$"),
+
+        );
     }
 
-    @Test
-    void formatReturnsSameTextIfNoDollarSignPresent() throws Exception {
-        assertEquals("Lorem ipsum", formatter.format("Lorem ipsum"));
-    }
-
-    @Test
-    void formatEscapesDollarSignIfPresent() throws Exception {
-        assertEquals("Lorem\\$ipsum", formatter.format("Lorem$ipsum"));
+    @ParameterizedTest
+    @MethodSource
+    void correctlyFormats(String expected, String input) throws Exception {
+        assertEquals(expected, input);
     }
 
     @Test
