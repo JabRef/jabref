e599525d9665379ea6fd364f0b03a99d45710eda
Siedlerchr <siedlerkiller@gmail.com>
2022-04-22 22:54:24 +0200

    try again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -116,9 +116,6 @@ open module org.jabref {
     requires com.fasterxml.jackson.dataformat.yaml;
     requires com.fasterxml.jackson.datatype.jsr310;
     requires net.harawata.appdirs;
-
     requires org.eclipse.jgit;
-    uses org.eclipse.jgit.transport.SshSessionFactory;
-    uses org.eclipse.jgit.lib.GpgSigner;
 
 }
