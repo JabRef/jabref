eac088c9f7b6da963b34611d2c3fb31ce0424193
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2022-04-25 05:25:33 +0700

    create some test cases for writing and export XMP metadata with and without braces

--- src/test/java/org/jabref/logic/xmp/XmpUtilWriterTest.java
+++ src/test/java/org/jabref/logic/xmp/XmpUtilWriterTest.java
@@ -1,25 +1,45 @@
 package org.jabref.logic.xmp;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.transform.TransformerException;
 
+import org.jabref.logic.bibtex.FieldContentFormatterPreferences;
+import org.jabref.logic.bibtex.FieldWriterPreferences;
+import org.jabref.logic.exporter.EmbeddedBibFilePdfExporter;
+import org.jabref.model.database.BibDatabase;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.BibEntryTypesManager;
 import org.jabref.model.entry.Date;
+import org.jabref.model.entry.LinkedFile;
 import org.jabref.model.entry.Month;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.field.UnknownField;
 import org.jabref.model.entry.types.StandardEntryType;
+import org.jabref.preferences.FilePreferences;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
+import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecification;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDEmbeddedFile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import static org.jabref.logic.exporter.EmbeddedBibFilePdfExporter.EMBEDDED_FILE_NAME;
 import static org.jabref.logic.xmp.DublinCoreExtractor.DC_COVERAGE;
 import static org.jabref.logic.xmp.DublinCoreExtractor.DC_RIGHTS;
 import static org.jabref.logic.xmp.DublinCoreExtractor.DC_SOURCE;
@@ -89,6 +109,7 @@ class XmpUtilWriterTest {
         xmpPreferences = mock(XmpPreferences.class);
         // The code assumes privacy filters to be off
         when(xmpPreferences.shouldUseXmpPrivacyFilter()).thenReturn(false);
+        when(xmpPreferences.shouldEnableEnclosingBracketsFilter()).thenReturn(false);
 
         when(xmpPreferences.getKeywordSeparator()).thenReturn(',');
 
@@ -99,20 +120,38 @@ class XmpUtilWriterTest {
      * Test for writing a PDF file with a single DublinCore metadata entry.
      */
     @Test
-    void testWriteXmp(@TempDir Path tempDir) throws IOException, TransformerException {
-        Path pdfFile = this.createDefaultFile("JabRef_writeSingle.pdf", tempDir);
+    void testWriteXmp(@TempDir Path tempDir) throws Exception {
+        BibDatabaseMode bibDatabaseMode = BibDatabaseMode.BIBTEX;
+        BibEntryTypesManager bibEntryTypesManager = new BibEntryTypesManager();
+        FieldWriterPreferences fieldWriterPreferences = new FieldWriterPreferences(true, List.of(StandardField.MONTH), new FieldContentFormatterPreferences());
+        BibDatabaseContext databaseContext = new BibDatabaseContext();
+        BibDatabase database = databaseContext.getDatabase();
 
+        FilePreferences filePreferences = mock(FilePreferences.class);
+        when(filePreferences.getUser()).thenReturn(tempDir.toAbsolutePath().toString());
+        when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(false);
+
+//        Path pdfFile = this.createDefaultFile("JabRef_writeSingle.pdf", tempDir);
+        Path pdfFile = tempDir.resolve("existing.pdf").toAbsolutePath();
         // read a bib entry from the tests before
         BibEntry entry = vapnik2000;
-        entry.setCitationKey("WriteXMPTest");
-        entry.setId("ID4711");
+        LinkedFile linkedFile = createDefaultLinkedFile("existing.pdf", tempDir);
+        entry.setFiles(Arrays.asList(linkedFile));
+        database.insertEntry(entry);
 
         // write the changed bib entry to the PDF
         XmpUtilWriter.writeXmp(pdfFile.toAbsolutePath().toString(), entry, null, xmpPreferences);
 
+        // export
+        EmbeddedBibFilePdfExporter embeddedBibExporter = new EmbeddedBibFilePdfExporter(bibDatabaseMode, bibEntryTypesManager, fieldWriterPreferences, xmpPreferences);
+        embeddedBibExporter.exportToFileByPath(databaseContext, database, filePreferences, pdfFile);
+
         // read entry again
         List<BibEntry> entriesWritten = XmpUtilReader.readXmp(pdfFile.toAbsolutePath().toString(), xmpPreferences);
         BibEntry entryWritten = entriesWritten.get(0);
+
+        System.out.println(entryWritten);
+
         entryWritten.clearField(StandardField.FILE);
 
         // compare the two entries
@@ -134,6 +173,136 @@ class XmpUtilWriterTest {
         assertEquals(3, entryList.size());
     }
 
+    /**
+     * Test for writing XMP metadata to PDF file and export without enclosing braces.
+     */
+    @Test
+    void testWriteAndExportXmpWithoutEnclosingBraces(@TempDir Path tempDir) throws Exception {
+        // turn filter on
+        when(xmpPreferences.shouldEnableEnclosingBracketsFilter()).thenReturn(true);
+        // some initialization
+        BibDatabaseMode bibDatabaseMode = BibDatabaseMode.BIBTEX;
+        BibEntryTypesManager bibEntryTypesManager = new BibEntryTypesManager();
+        FieldWriterPreferences fieldWriterPreferences = new FieldWriterPreferences(true, List.of(StandardField.MONTH), new FieldContentFormatterPreferences());
+        BibDatabaseContext databaseContext = new BibDatabaseContext();
+        BibDatabase database = databaseContext.getDatabase();
+
+        FilePreferences filePreferences = mock(FilePreferences.class);
+        when(filePreferences.getUser()).thenReturn(tempDir.toAbsolutePath().toString());
+        when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(false);
+
+        Path pdfFile = tempDir.resolve("existing.pdf").toAbsolutePath();
+        // read a bib entry from the tests before
+        BibEntry entry = vapnik2000;
+        // link file to entry
+        LinkedFile linkedFile = createDefaultLinkedFile("existing.pdf", tempDir);
+        entry.setFiles(Arrays.asList(linkedFile));
+        // insert entry to database
+        database.insertEntry(entry);
+
+        String expectBibTeX = "@Book{vapnik2000,\n" +
+                "  author    = Vladimir N. Vapnik,\n" +
+                "  publisher = Springer Science + Business Media,\n" +
+                "  title     = The Nature of Statistical Learning Theory,\n" +
+                "  year      = 2000,\n" +
+                "  month     = may,\n" +
+                "  coverage  = coverageField,\n" +
+                "  doi       = 10.1007/978-1-4757-3264-1,\n" +
+                "  language  = English, Japanese,\n" +
+                "  owner     = Ich,\n" +
+                "  rights    = Right To X,\n" +
+                "  source    = JabRef,\n" +
+                "}";
+
+        // write the bib entry to the PDF and export
+        XmpUtilWriter.writeXmp(pdfFile.toAbsolutePath().toString(), entry, null, xmpPreferences);
+        EmbeddedBibFilePdfExporter embeddedBibExporter = new EmbeddedBibFilePdfExporter(bibDatabaseMode, bibEntryTypesManager, fieldWriterPreferences, xmpPreferences);
+        embeddedBibExporter.exportToFileByPath(databaseContext, database, filePreferences, pdfFile);
+
+        // extract bibTeX embedded in PDF
+        String bibTeX = extractBibTeX(pdfFile);
+
+        // remove FILE for now due to different separator
+        // not sure why
+        bibTeX = bibTeX.replaceAll("(?m)^  file.*(?:\\r?\\n)?", "").strip().replaceAll("\\r\\n?", "\n");
+
+        assertEquals(expectBibTeX, bibTeX);
+    }
+
+    /**
+     * Test for writing XMP metadata to PDF file and export with enclosing braces.
+     */
+    @Test
+    void testWriteAndExportXmpWithEnclosingBraces(@TempDir Path tempDir) throws Exception {
+        // turn filter on
+        when(xmpPreferences.shouldEnableEnclosingBracketsFilter()).thenReturn(false);
+        // some initialization
+        BibDatabaseMode bibDatabaseMode = BibDatabaseMode.BIBTEX;
+        BibEntryTypesManager bibEntryTypesManager = new BibEntryTypesManager();
+        FieldWriterPreferences fieldWriterPreferences = new FieldWriterPreferences(true, List.of(StandardField.MONTH), new FieldContentFormatterPreferences());
+        BibDatabaseContext databaseContext = new BibDatabaseContext();
+        BibDatabase database = databaseContext.getDatabase();
+
+        FilePreferences filePreferences = mock(FilePreferences.class);
+        when(filePreferences.getUser()).thenReturn(tempDir.toAbsolutePath().toString());
+        when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(false);
+
+        Path pdfFile = tempDir.resolve("existing.pdf").toAbsolutePath();
+        // read a bib entry from the tests before
+        BibEntry entry = vapnik2000;
+        // link file to entry
+        LinkedFile linkedFile = createDefaultLinkedFile("existing.pdf", tempDir);
+        entry.setFiles(Arrays.asList(linkedFile));
+        // insert entry to database
+        database.insertEntry(entry);
+
+        String expectBibTeX = "@Book{vapnik2000,\n" +
+                "  author    = {Vladimir N. Vapnik},\n" +
+                "  publisher = {Springer Science + Business Media},\n" +
+                "  title     = {The Nature of Statistical Learning Theory},\n" +
+                "  year      = {2000},\n" +
+                "  month     = may,\n" +
+                "  coverage  = {coverageField},\n" +
+                "  doi       = {10.1007/978-1-4757-3264-1},\n" +
+                "  language  = {English, Japanese},\n" +
+                "  owner     = {Ich},\n" +
+                "  rights    = {Right To X},\n" +
+                "  source    = {JabRef},\n" +
+                "}";
+
+        // write the bib entry to the PDF and export
+        XmpUtilWriter.writeXmp(pdfFile.toAbsolutePath().toString(), entry, null, xmpPreferences);
+        EmbeddedBibFilePdfExporter embeddedBibExporter = new EmbeddedBibFilePdfExporter(bibDatabaseMode, bibEntryTypesManager, fieldWriterPreferences, xmpPreferences);
+        embeddedBibExporter.exportToFileByPath(databaseContext, database, filePreferences, pdfFile);
+
+        // extract bibTeX embedded in PDF
+        String bibTeX = extractBibTeX(pdfFile);
+
+        // remove FILE for now due to different separator
+        // not sure why
+        bibTeX = bibTeX.replaceAll("(?m)^  file.*(?:\\r?\\n)?", "").strip().replaceAll("\\r\\n?", "\n");
+
+        assertEquals(expectBibTeX, bibTeX);
+    }
+
+    /**
+     * Extract BibTeX that is embedded in the PDF file during export.
+     */
+    private String extractBibTeX(Path path) throws IOException {
+        PDDocument document = Loader.loadPDF(path.toFile());
+        PDDocumentCatalog documentCatalog = document.getDocumentCatalog();
+        PDDocumentNameDictionary nameDictionary = documentCatalog.getNames();
+        PDEmbeddedFilesNameTreeNode efTree = nameDictionary.getEmbeddedFiles();
+        Map<String, PDComplexFileSpecification> names = efTree.getNames();
+        PDComplexFileSpecification fileSpecification = names.get(EMBEDDED_FILE_NAME);
+        PDEmbeddedFile embeddedFile = fileSpecification.getEmbeddedFile();
+        InputStream inputStream = embeddedFile.createInputStream();
+
+        String bibTeX = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+
+        return bibTeX;
+    }
+
     private Path createDefaultFile(String fileName, Path tempDir) throws IOException {
         // create a default PDF
         Path pdfFile = tempDir.resolve(fileName);
@@ -144,5 +313,17 @@ class XmpUtilWriterTest {
         }
 
         return pdfFile;
+    }
+
+    private static LinkedFile createDefaultLinkedFile(String fileName, Path tempDir) throws IOException {
+        Path pdfFile = tempDir.resolve(fileName);
+        try (PDDocument pdf = new PDDocument()) {
+            pdf.addPage(new PDPage());
+            pdf.save(pdfFile.toAbsolutePath().toString());
+        }
+
+        LinkedFile linkedFile = new LinkedFile("A linked pdf", pdfFile, "PDF");
+
+        return linkedFile;
     }
 }
