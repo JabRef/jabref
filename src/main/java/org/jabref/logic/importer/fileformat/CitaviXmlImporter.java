f339dab8d4b8728f7b3f301ab431dea761b798dc
Shangyu Li <lishangyu9@gmail.com>
2022-04-27 17:15:17 +0930

    Modify the citavi importer, draft of importDatabase()

--- src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
@@ -1,32 +1,27 @@
 package org.jabref.logic.importer.fileformat;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.jabref.logic.importer.ImportFormatPreferences;
 import org.jabref.logic.importer.Importer;
 import org.jabref.logic.importer.Parser;
 import org.jabref.logic.importer.ParserResult;
-import org.jabref.logic.importer.fileformat.endnote.Record;
-import org.jabref.logic.importer.fileformat.endnote.Xml;
 import org.jabref.logic.util.StandardFileType;
 import org.jabref.model.entry.BibEntry;
 
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,7 +33,7 @@ public class CitaviXmlImporter extends Importer implements Parser {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CitaviXmlImporter.class);
     private final ImportFormatPreferences preferences;
-    private Unmarshaller unmarshaller;
+    // private Unmarshaller unmarshaller;
 
     public CitaviXmlImporter(ImportFormatPreferences preferences) {
         this.preferences = preferences;
@@ -66,43 +61,61 @@ public class CitaviXmlImporter extends Importer implements Parser {
 
     @Override
     public boolean isRecognizedFormat(BufferedReader reader) throws IOException {
-        String str;
-        int i = 0;
-        while (((str = reader.readLine()) != null) && (i < 50)) {
-            if (str.toLowerCase(Locale.ENGLISH).contains("<records>")) {
-                return true;
-            }
-
-            i++;
-        }
-        return false;
+//        String str;
+//        int i = 0;
+//        while (((str = reader.readLine()) != null) && (i < 50)) {
+//            if (str.toLowerCase(Locale.ENGLISH).contains("<records>")) {
+//                return true;
+//            }
+//
+//            i++;
+//        }
+//        return false;
+        // todo recognized logic
+        return true;
     }
 
     @Override
+    public ParserResult importDatabase(Path filePath) throws IOException {
+        BufferedReader reader = getReaderFromZip(filePath);
+
+        List<BibEntry> bibEntries = new ArrayList<>();
+
+        // todo add parse logic
+
+        return new ParserResult(bibEntries);
+    }
+
     public ParserResult importDatabase(BufferedReader reader) throws IOException {
         Objects.requireNonNull(reader);
-
-        try {
-            Object unmarshalledObject = unmarshallRoot(reader);
-
-            if (unmarshalledObject instanceof Xml) {
-                // Check whether we have an article set, an article, a book article or a book article set
-                Xml root = (Xml) unmarshalledObject;
-                List<BibEntry> bibEntries = root
-                        .getRecords().getRecord()
-                        .stream()
-                        .map(this::parseRecord)
-                        .collect(Collectors.toList());
-
-                return new ParserResult(bibEntries);
-            } else {
-                return ParserResult.fromErrorMessage("File does not start with xml tag.");
-            }
-        } catch (JAXBException | XMLStreamException e) {
-            LOGGER.debug("could not parse document", e);
-            return ParserResult.fromError(e);
-        }
+        throw new UnsupportedOperationException("CitaviXmlImporter does not support importDatabase(BufferedReader reader)."
+                + "Instead use importDatabase(Path filePath, Charset defaultEncoding).");
     }
+//    @Override
+//    public ParserResult importDatabase(BufferedReader reader) throws IOException {
+//        Objects.requireNonNull(reader);
+//
+//        try {
+//            Object unmarshalledObject = unmarshallRoot(reader);
+//
+//            if (unmarshalledObject instanceof Xml) {
+//                // Check whether we have an article set, an article, a book article or a book article set
+//                Xml root = (Xml) unmarshalledObject;
+//                List<BibEntry> bibEntries = root
+//                        .getRecords().getRecord()
+//                        .stream()
+//                        .map(this::parseRecord)
+//                        .collect(Collectors.toList());
+//
+//                return new ParserResult(bibEntries);
+//            } else {
+//                return ParserResult.fromErrorMessage("File does not start with xml tag.");
+//            }
+//        } catch (JAXBException | XMLStreamException e) {
+//            LOGGER.debug("could not parse document", e);
+//            return ParserResult.fromError(e);
+//        }
+//    }
 
     @Override
     public List<BibEntry> parseEntries(InputStream inputStream) {
@@ -115,31 +128,55 @@ public class CitaviXmlImporter extends Importer implements Parser {
         return Collections.emptyList();
     }
 
-    private BibEntry parseRecord(Record record) {
-        BibEntry entry = new BibEntry();
-        // todo add parse logic
-        return entry;
-    }
+//    private BibEntry parseRecord(Record record) {
+//        BibEntry entry = new BibEntry();
+//        // add parse logic
+//        return entry;
+//    }
 
-    private Object unmarshallRoot(BufferedReader reader) throws XMLStreamException, JAXBException {
-        initUnmarshaller();
+//    private Object unmarshallRoot(BufferedReader reader) throws XMLStreamException, JAXBException {
+//        initUnmarshaller();
+//
+//        XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
+//        XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(reader);
+//
+//        // Go to the root element
+//        while (!xmlStreamReader.isStartElement()) {
+//            xmlStreamReader.next();
+//        }
+//
+//        return unmarshaller.unmarshal(xmlStreamReader);
+//    }
 
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
-        XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(reader);
+//    private void initUnmarshaller() throws JAXBException {
+//        if (unmarshaller == null) {
+//            // Lazy init because this is expensive
+//            JAXBContext context = JAXBContext.newInstance("org.jabref.logic.importer.fileformat.citavi");
+//            unmarshaller = context.createUnmarshaller();
+//        }
+//    }
 
-        // Go to the root element
-        while (!xmlStreamReader.isStartElement()) {
-            xmlStreamReader.next();
+    private BufferedReader getReaderFromZip(Path filePath) throws IOException {
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(filePath.toFile()));
+        ZipEntry zipEntry = zis.getNextEntry();
+
+        while (zipEntry != null) {
+            Path newFile = Files.createTempFile("citavicontent", "xml");
+
         }
 
-        return unmarshaller.unmarshal(xmlStreamReader);
-    }
+        // todo add unzip and transfer buffered reader logic
 
-    private void initUnmarshaller() throws JAXBException {
-        if (unmarshaller == null) {
-            // Lazy init because this is expensive
-            JAXBContext context = JAXBContext.newInstance("org.jabref.logic.importer.fileformat.citavi");
-            unmarshaller = context.createUnmarshaller();
-        }
+        zis.closeEntry();
+
+//        InputStream stream = Files.newInputStream(filePath, StandardOpenOption.READ);
+//
+//        if (FileUtil.isBibFile(filePath)) {
+//            return getReader(stream);
+//        }
+//
+//        return new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+
+        return null;
     }
 }
