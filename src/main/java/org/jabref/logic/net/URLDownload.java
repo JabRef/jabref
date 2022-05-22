d32eae2617b00861b01d8418255a7e3ca32635b0
zkl-ai <907668776@qq.com>
2022-05-23 00:26:34 +0800

    modify Localization.lang(message)

--- src/main/java/org/jabref/logic/net/URLDownload.java
+++ src/main/java/org/jabref/logic/net/URLDownload.java
@@ -363,11 +363,13 @@ public class URLDownload {
             }
             // status code 4xx
             if (status > HttpURLConnection.HTTP_BAD_REQUEST && status < HttpURLConnection.HTTP_INTERNAL_ERROR) {
-                throw new IOException(Localization.lang(("Client Error:" + status)));
+                String message = String.format("Client Error %d", status);
+                throw new IOException(Localization.lang(message));
 
                 // status code 5xx
             } else if (status >= HttpURLConnection.HTTP_INTERNAL_ERROR) {
-                throw new IOException(Localization.lang(("Server Error:" + status)));
+                String message = String.format("Server Error %d", status);
+                throw new IOException(Localization.lang(message));
             }
         }
 
