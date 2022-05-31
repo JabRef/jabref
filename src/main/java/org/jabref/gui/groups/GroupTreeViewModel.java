66cacb479cb0b5695dc999da0d9218bdbe7e31a0
LIM0000 <hihi19982015@hotmail.com>
2022-05-31 18:05:17 +0930

    Fix #8390 by allowing multiple group deletion for Remove groups > Keep subgroups

--- src/main/java/org/jabref/gui/groups/GroupTreeViewModel.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeViewModel.java
@@ -260,20 +260,38 @@ public class GroupTreeViewModel extends AbstractViewModel {
     }
 
     public void removeGroupKeepSubgroups(GroupNodeViewModel group) {
-        boolean confirmation = dialogService.showConfirmationDialogAndWait(
-                Localization.lang("Remove group"),
-                Localization.lang("Remove group \"%0\"?", group.getDisplayName()));
+        boolean confirmed;
+        if (selectedGroups.size() <= 1) {
+            confirmed = dialogService.showConfirmationDialogAndWait(
+                    Localization.lang("Remove group"),
+                    Localization.lang("Remove group \"%0\" and keep its subgroups?", group.getDisplayName()),
+                    Localization.lang("Remove"));
+        } else {
+            confirmed = dialogService.showConfirmationDialogAndWait(
+                    Localization.lang("Remove groups"),
+                    Localization.lang("Remove all selected groups and keep their subgroups?"),
+                    Localization.lang("Remove all"));
+        }
 
-        if (confirmation) {
+        if (confirmed) {
             // TODO: Add undo
             // final UndoableAddOrRemoveGroup undo = new UndoableAddOrRemoveGroup(groupsRoot, node, UndoableAddOrRemoveGroup.REMOVE_NODE_KEEP_CHILDREN);
             // panel.getUndoManager().addEdit(undo);
-            GroupTreeNode groupNode = group.getGroupNode();
-            groupNode.getParent()
-                     .ifPresent(parent -> groupNode.moveAllChildrenTo(parent, parent.getIndexOfChild(groupNode).get()));
-            groupNode.removeFromParent();
 
-            dialogService.notify(Localization.lang("Removed group \"%0\".", group.getDisplayName()));
+            ArrayList<GroupNodeViewModel> selectedGroupNodes = new ArrayList<>(selectedGroups);
+            selectedGroupNodes.forEach(eachNode -> {
+                GroupTreeNode groupNode = eachNode.getGroupNode();
+
+                groupNode.getParent()
+                         .ifPresent(parent -> groupNode.moveAllChildrenTo(parent, parent.getIndexOfChild(groupNode).get()));
+                groupNode.removeFromParent();
+            });
+
+            if (selectedGroupNodes.size() > 1) {
+                dialogService.notify(Localization.lang("Removed all selected groups."));
+            } else {
+                dialogService.notify(Localization.lang("Removed group \"%0\".", group.getDisplayName()));
+            }
             writeGroupChangesToMetaData();
         }
     }
