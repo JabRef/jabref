60bc190a9e5b9912d8ebd60a477b2650eb0f3cbf
Houssem Nasri <housi.housi2015@gmail.com>
2022-06-11 00:44:02 +0100

    Fix language tests & Checkstyle

--- src/main/java/org/jabref/gui/edit/automaticfiededitor/AutomaticFieldEditorTab.java
+++ src/main/java/org/jabref/gui/edit/automaticfiededitor/AutomaticFieldEditorTab.java
@@ -1,7 +1,6 @@
 package org.jabref.gui.edit.automaticfiededitor;
 
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
 
 public interface AutomaticFieldEditorTab {
     Pane getContent();
