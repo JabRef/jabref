70522eb1b62ea0e87603f6f83a41b261e27c40d3
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 03:55:13 +0100

    Display entry type and bind merged entry

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/FieldRowController.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/FieldRowController.java
@@ -3,7 +3,6 @@ package org.jabref.gui.mergeentries.newmergedialog;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.scene.control.ToggleGroup;
 
-import org.jabref.gui.mergeentries.newmergedialog.cell.AbstractCell;
 import org.jabref.gui.mergeentries.newmergedialog.cell.FieldNameCell;
 import org.jabref.gui.mergeentries.newmergedialog.cell.FieldValueCell;
 import org.jabref.gui.mergeentries.newmergedialog.cell.MergedFieldCell;
@@ -17,18 +16,37 @@ public class FieldRowController {
 
     private final ToggleGroup toggleGroup = new ToggleGroup();
 
-    public FieldRowController(String fieldName, String leftValue, String rightValue, AbstractCell.BackgroundTone backgroundTone) {
-        fieldNameCell = new FieldNameCell(fieldName, backgroundTone);
-        leftValueCell = new FieldValueCell(leftValue, backgroundTone);
-        rightValueCell = new FieldValueCell(rightValue, backgroundTone);
-        mergedValueCell = new MergedFieldCell(StringUtil.isNullOrEmpty(leftValue) ? rightValue : leftValue, backgroundTone);
+    public FieldRowController(String fieldName, String leftValue, String rightValue, int rowIndex) {
+        fieldNameCell = new FieldNameCell(fieldName, rowIndex);
+        leftValueCell = new FieldValueCell(leftValue, rowIndex);
+        rightValueCell = new FieldValueCell(rightValue, rowIndex);
+        mergedValueCell = new MergedFieldCell(StringUtil.isNullOrEmpty(leftValue) ? rightValue : leftValue, rowIndex);
 
         toggleGroup.getToggles().addAll(leftValueCell, rightValueCell);
         toggleGroup.selectToggle(StringUtil.isNullOrEmpty(leftValue) ? rightValueCell : leftValueCell);
         toggleGroup.selectedToggleProperty().addListener(invalidated -> {
-            mergedValueCell.setText((String) toggleGroup.getSelectedToggle().getUserData());
+            if (toggleGroup.getSelectedToggle() != null) {
+                mergedValueCell.setText((String) toggleGroup.getSelectedToggle().getUserData());
+            }
         });
 
+        mergedValueCell.textProperty().addListener((observable, old, mergedValue) -> {
+            if (!StringUtil.isNullOrEmpty(mergedValue)) {
+                if (mergedValue.equals(leftValue)) {
+                    toggleGroup.selectToggle(leftValueCell);
+                } else if (mergedValue.equals(rightValue)) {
+                    toggleGroup.selectToggle(rightValueCell);
+                } else {
+                    // deselect all toggles because left and right values don't equal the merged value
+                    toggleGroup.selectToggle(null);
+                }
+            } else {
+                // deselect all toggles because empty toggles cannot be selected
+                toggleGroup.selectToggle(null);
+            }
+        });
+
+        // empty toggles are disabled and cannot be selected
         if (StringUtil.isNullOrEmpty(leftValue)) {
             leftValueCell.setDisable(true);
         } else if (StringUtil.isNullOrEmpty(rightValue)) {
@@ -64,9 +82,5 @@ public class FieldRowController {
         return !StringUtil.isNullOrEmpty(leftValueCell.getText()) &&
                 !StringUtil.isNullOrEmpty(rightValueCell.getText()) &&
                 leftValueCell.getText().equals(rightValueCell.getText());
-    }
-
-    public void deselectLeft() {
-        toggleGroup.getSelectedToggle().setSelected(false);
     }
 }
