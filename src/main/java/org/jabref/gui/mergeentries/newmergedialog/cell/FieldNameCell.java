93a17c6448546ba9fcdcc3764afbe3e265d02517
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 02:02:59 +0100

    Implement FieldNameCell and define style class

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/FieldNameCell.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/FieldNameCell.java
@@ -1,7 +1,29 @@
 package org.jabref.gui.mergeentries.newmergedialog.cell;
 
+import javafx.scene.control.Label;
+
 /**
  * A non-editable cell that contains the name of some field
  */
-public class FieldNameCell {
+public class FieldNameCell extends AbstractCell {
+    public static final String DEFAULT_STYLE_CLASS = "field-name";
+    private final Label label = new Label();
+
+    public FieldNameCell(String text, BackgroundTone backgroundTone) {
+        super(text, backgroundTone);
+    }
+
+    public FieldNameCell(String text) {
+        super(text);
+    }
+
+    private void initialize() {
+        getStyleClass().add(DEFAULT_STYLE_CLASS);
+        initializeLabel();
+        getChildren().add(label);
+    }
+
+    private void initializeLabel() {
+        label.textProperty().bind(textProperty());
+    }
 }
