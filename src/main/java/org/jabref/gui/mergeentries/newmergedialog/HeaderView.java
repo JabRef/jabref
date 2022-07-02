1c7e0127b86bc9e45de2ef212b658a0969b809b6
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 02:54:00 +0100

    Create HeadView for rendering left, right and merged entry headers

---
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/HeaderView.java
@@ -0,0 +1,10 @@
+package org.jabref.gui.mergeentries.newmergedialog;
+
+import javafx.scene.layout.HBox;
+
+public class HeaderView extends HBox {
+    public HeaderView() {
+        setPrefHeight(50d);
+        setStyle("-fx-background-color: #878787");
+    }
+}
