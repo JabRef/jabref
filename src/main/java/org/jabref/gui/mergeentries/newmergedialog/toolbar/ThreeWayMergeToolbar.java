0f3e1545c5983824bc00a737b8e037baebcd3c80
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 04:04:13 +0100

    Add buttons to accept all left or right entry changes to ThreeWayMergeToolbar

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/toolbar/ThreeWayMergeToolbar.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/toolbar/ThreeWayMergeToolbar.java
@@ -1,14 +1,18 @@
-package org.jabref.gui.mergeentries.newmergedialog.toolbox;
+package org.jabref.gui.mergeentries.newmergedialog.toolbar;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
+import javafx.beans.binding.BooleanExpression;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.StringConverter;
 
@@ -16,15 +20,15 @@ import com.airhacks.afterburner.views.ViewLoader;
 import com.tobiasdiez.easybind.EasyBind;
 import com.tobiasdiez.easybind.EasyBinding;
 
-public class ThreeWayMergeToolbox extends AnchorPane {
+public class ThreeWayMergeToolbar extends AnchorPane {
     @FXML
-    private RadioButton compareCharactersRadioButtons;
+    private RadioButton highlightCharactersRadioButtons;
 
     @FXML
-    private RadioButton compareWordsRadioButton;
+    private RadioButton highlightWordsRadioButton;
 
     @FXML
-    private ToggleGroup diffCompareMethodToggleGroup;
+    private ToggleGroup diffHighlightModeToggleGroup;
 
     @FXML
     private ComboBox<DiffView> diffViewComboBox;
@@ -32,10 +36,16 @@ public class ThreeWayMergeToolbox extends AnchorPane {
     @FXML
     private ComboBox<PlainTextOrDiff> plainTextOrDiffComboBox;
 
-    private final ObjectProperty<DiffCompareMethod> diffCompareMethod = new SimpleObjectProperty<>();
+    @FXML
+    private Button selectLeftEntryValuesButton;
+
+    @FXML
+    private Button selectRightEntryValuesButton;
+
+    private final ObjectProperty<DiffHighlightMode> diffHighlightMode = new SimpleObjectProperty<>();
     private EasyBinding<Boolean> showDiff;
 
-    public ThreeWayMergeToolbox() {
+    public ThreeWayMergeToolbar() {
         ViewLoader.view(this)
                 .root(this)
                 .load();
@@ -74,33 +84,18 @@ public class ThreeWayMergeToolbox extends AnchorPane {
             }
         });
 
-        compareWordsRadioButton.disableProperty().bind(notShowDiffProperty());
-        compareCharactersRadioButtons.disableProperty().bind(notShowDiffProperty());
+        highlightWordsRadioButton.disableProperty().bind(notShowDiffProperty());
+        highlightCharactersRadioButtons.disableProperty().bind(notShowDiffProperty());
 
-        diffCompareMethodToggleGroup.selectedToggleProperty().addListener((observable -> {
-            if (diffCompareMethodToggleGroup.getSelectedToggle().equals(compareCharactersRadioButtons)) {
-                diffCompareMethod.set(DiffCompareMethod.CHARS);
+        diffHighlightModeToggleGroup.selectedToggleProperty().addListener((observable -> {
+            if (diffHighlightModeToggleGroup.getSelectedToggle().equals(highlightCharactersRadioButtons)) {
+                diffHighlightMode.set(DiffHighlightMode.CHARS);
             } else {
-                diffCompareMethod.set(DiffCompareMethod.WORDS);
+                diffHighlightMode.set(DiffHighlightMode.WORDS);
             }
         }));
 
-        diffCompareMethodToggleGroup.selectToggle(compareWordsRadioButton);
-    }
-
-    private void initializeDiffViewComboBox() {
-        diffViewComboBox.getSelectionModel().select(DiffView.UNIFIED);
-        diffViewComboBox.setConverter(new StringConverter<>() {
-            @Override
-            public String toString(DiffView diffView) {
-                return diffView.getValue();
-            }
-
-            @Override
-            public DiffView fromString(String string) {
-                return DiffView.fromString(string);
-            }
-        });
+        diffHighlightModeToggleGroup.selectToggle(highlightWordsRadioButton);
     }
 
     public ReadOnlyObjectProperty<DiffView> diffViewProperty() {
@@ -115,6 +110,14 @@ public class ThreeWayMergeToolbox extends AnchorPane {
         return showDiff;
     }
 
+    /**
+     * Convince method used to disable diff related views when diff is not selected.
+     *
+     * <p>
+     * This method is required because {@link EasyBinding} class doesn't have a method to invert a boolean property,
+     * like {@link BooleanExpression#not()}
+     * </p>
+     * */
     public EasyBinding<Boolean> notShowDiffProperty() {
         return showDiffProperty().map(showDiff -> !showDiff);
     }
@@ -123,12 +126,20 @@ public class ThreeWayMergeToolbox extends AnchorPane {
         return showDiffProperty().get();
     }
 
-    public ObjectProperty<DiffCompareMethod> diffCompareMethodProperty() {
-        return diffCompareMethod;
+    public ObjectProperty<DiffHighlightMode> diffHighlightModeProperty() {
+        return diffHighlightMode;
     }
 
-    public DiffCompareMethod getDiffCompareMethod() {
-        return diffCompareMethodProperty().get();
+    public DiffHighlightMode getDiffHighlightMode() {
+        return diffHighlightModeProperty().get();
+    }
+
+    public void setOnSelectLeftEntryValuesButtonClicked(Consumer<MouseEvent> onClick) {
+        selectLeftEntryValuesButton.setOnMouseClicked(onClick::accept);
+    }
+
+    public void setOnSelectRightEntryValuesButtonClicked(Consumer<MouseEvent> onClick) {
+        selectRightEntryValuesButton.setOnMouseClicked(onClick::accept);
     }
 
     public enum PlainTextOrDiff {
@@ -173,7 +184,7 @@ public class ThreeWayMergeToolbox extends AnchorPane {
         }
     }
 
-    public enum DiffCompareMethod {
+    public enum DiffHighlightMode {
         WORDS, CHARS
     }
 }
