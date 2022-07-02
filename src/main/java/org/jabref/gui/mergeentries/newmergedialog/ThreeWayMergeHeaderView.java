d14cb0f00162ee4d81ab633d14315df886483996
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 04:02:50 +0100

    Rename HeaderView to ThreeWayMergeHeaderView

---
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/ThreeWayMergeHeaderView.java
@@ -0,0 +1,58 @@
+package org.jabref.gui.mergeentries.newmergedialog;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Control;
+import javafx.scene.layout.GridPane;
+
+import org.jabref.gui.mergeentries.newmergedialog.cell.EmptyCell;
+import org.jabref.gui.mergeentries.newmergedialog.cell.HeaderCell;
+
+/**
+ * I used a GridPane instead of a Hbox because Hbox allocates more space for cells
+ * with longer text, but I wanted all cells to have the same width
+ */
+public class ThreeWayMergeHeaderView extends GridPane {
+    public static final String DEFAULT_STYLE_CLASS = "merge-header";
+    private final HeaderCell leftHeaderCell;
+    private final HeaderCell rightHeaderCell;
+
+    private final HeaderCell mergedHeaderCell;
+
+    public ThreeWayMergeHeaderView(String leftHeader, String rightHeader) {
+        getStyleClass().add(DEFAULT_STYLE_CLASS);
+
+        this.leftHeaderCell = new HeaderCell(leftHeader);
+        this.rightHeaderCell = new HeaderCell(rightHeader);
+        this.mergedHeaderCell = new HeaderCell("Merged Entry");
+
+        addRow(0,
+               new EmptyCell(HeaderCell.DEFAULT_STYLE_CLASS),
+               leftHeaderCell,
+               rightHeaderCell,
+               mergedHeaderCell
+        );
+
+        setPrefHeight(Control.USE_COMPUTED_SIZE);
+        setMaxHeight(Control.USE_PREF_SIZE);
+        setMinHeight(Control.USE_PREF_SIZE);
+
+        bindHeaderWidthToFieldGridWidth();
+    }
+
+    public void setLeftHeader(String leftHeader) {
+        leftHeaderCell.setText(leftHeader);
+    }
+
+    public void setRightHeader(String rightHeader) {
+        rightHeaderCell.setText(rightHeader);
+    }
+
+    /**
+     * The fields grid pane is contained within a scroll pane, thus it doesn't allocate the full available width. In
+     * fact, it uses the available width minus the size of the scrollbar which is 8. This leads to header columns being
+     * always larger than fields columns. This hack should fix it.
+     */
+    private void bindHeaderWidthToFieldGridWidth() {
+        setPadding(new Insets(0, 8, 0, 0));
+    }
+}
