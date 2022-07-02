0b9a345a9f3faf4ad91568639e7491a1924dc7b4
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-02 01:58:15 +0100

    Create grid cells for rendering field name and field value

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -116,6 +116,7 @@ open module org.jabref {
     requires net.harawata.appdirs;
 
     requires org.eclipse.jgit;
+    requires wellbehavedfx;
     uses org.eclipse.jgit.transport.SshSessionFactory;
     uses org.eclipse.jgit.lib.GpgSigner;
 }
