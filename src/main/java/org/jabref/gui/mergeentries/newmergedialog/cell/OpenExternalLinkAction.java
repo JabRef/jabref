b8a357574d6f1f2eb1a00b67109cba98689c354e
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-03 19:55:19 +0100

    Add the ability to open DOI identifier

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/OpenExternalLinkAction.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/OpenExternalLinkAction.java
@@ -1,9 +1,11 @@
 package org.jabref.gui.mergeentries.newmergedialog.cell;
 
 import java.io.IOException;
+import java.net.URI;
 
 import org.jabref.gui.actions.SimpleCommand;
 import org.jabref.gui.desktop.JabRefDesktop;
+import org.jabref.model.entry.identifier.DOI;
 import org.jabref.model.strings.StringUtil;
 
 /**
@@ -23,7 +25,14 @@ public class OpenExternalLinkAction extends SimpleCommand {
         }
 
         try {
-            JabRefDesktop.openBrowser(urlOrDoi);
+            String url;
+            if (DOI.isValid(urlOrDoi)) {
+                url = DOI.parse(urlOrDoi).flatMap(DOI::getExternalURI).map(URI::toString).orElse("");
+            } else {
+                url = urlOrDoi;
+            }
+
+            JabRefDesktop.openBrowser(url);
         } catch (
                 IOException ex) {
             // TODO: Do something
