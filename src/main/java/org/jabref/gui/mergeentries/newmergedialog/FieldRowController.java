a4049ba84fd94fbe9e8d051fc294d8202269bb0b
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-06 02:12:15 +0100

    Fix typo

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/FieldRowController.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/FieldRowController.java
@@ -66,7 +66,7 @@ public class FieldRowController {
     }
 
     public void selectRightValue() {
-        if (rightValueCell == null) {
+        if (isRightValueCellHidden()) {
             selectLeftValue();
         } else {
             toggleGroup.selectToggle(rightValueCell);
