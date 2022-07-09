421705779179ff546a09067a2f8de6743ea54fb5
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-09 04:31:28 +0100

    Almost f**** everything up. I deleted GroupsFieldNameCell content by mistake, it's all ok now
    - I did a rebase and then when resolving conflicts, I chose "Accept Theirs" all the way but that didn't go well, now I have no history of GroupsFieldNameCell

--- src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/GroupsFieldNameCell.java
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/GroupsFieldNameCell.java
@@ -1,32 +1,48 @@
 package org.jabref.gui.mergeentries.newmergedialog.cell;
 
-public class GroupsFieldNameCell extends FieldNameCell {
-    private Runnable onMergeGroups;
-    private Runnable onUnmergeGroups;
-    private boolean isMerged = false;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
-    public GroupsFieldNameCell(String text, int rowIndex) {
-        super(text, rowIndex);
-        setOnMouseClicked(e -> {
-            if (isMerged) {
-                if (onUnmergeGroups != null) {
-                    onUnmergeGroups.run();
-                    isMerged = false;
-                }
+import org.jabref.gui.icon.IconTheme;
+import org.jabref.logic.l10n.Localization;
+import org.jabref.model.entry.field.StandardField;
+
+import de.saxsys.mvvmfx.utils.commands.Command;
+
+public class GroupsFieldNameCell extends FieldNameCell {
+    private Command mergeGroupsCommand;
+    private Command unmergeGroupsCommand;
+
+    private final ObjectProperty<MergeAction> mergeAction = new SimpleObjectProperty<>();
+
+    public GroupsFieldNameCell(int rowIndex) {
+        super(StandardField.GROUPS.getDisplayName(), rowIndex);
+        mergeActionProperty().addListener((obs, old, newValue) -> {
+            if (newValue == MergeAction.MERGE) {
+                setAction(Localization.lang("Merge Groups"), IconTheme.JabRefIcons.MERGE_GROUPS, mergeGroupsCommand);
             } else {
-                if (onMergeGroups != null) {
-                    onMergeGroups.run();
-                    isMerged = true;
-                }
+                setAction(Localization.lang("Unmerge Groups"), IconTheme.JabRefIcons.UNDO, unmergeGroupsCommand);
             }
         });
     }
 
-    public void setOnMergeGroups(Runnable mergeGroups) {
-        this.onMergeGroups = mergeGroups;
+    public void setMergeGroupsCommand(Command mergeGroupsCommand) {
+        this.mergeGroupsCommand = mergeGroupsCommand;
     }
 
-    public void setOnUnmergeGroups(Runnable unmergeGroups) {
-        this.onUnmergeGroups = unmergeGroups;
+    public void setUnmergeGroupsCommand(Command unmergeGroupsCommand) {
+        this.unmergeGroupsCommand = unmergeGroupsCommand;
+    }
+
+    public ObjectProperty<MergeAction> mergeActionProperty() {
+        return mergeAction;
+    }
+
+    public void setMergeAction(MergeAction mergeAction) {
+        mergeActionProperty().set(mergeAction);
+    }
+
+    public enum MergeAction {
+        MERGE, UNMERGE
     }
 }
