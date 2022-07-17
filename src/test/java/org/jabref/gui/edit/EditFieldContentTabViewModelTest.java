5bb34ff794756d168829865961c42967204942b8
Houssem Nasri <housi.housi2015@gmail.com>
2022-07-17 11:32:12 +0100

    Test EditFieldContentTabViewModel

---
+++ src/test/java/org/jabref/gui/edit/EditFieldContentTabViewModelTest.java
@@ -0,0 +1,115 @@
+package org.jabref.gui.edit;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.jabref.gui.edit.automaticfiededitor.editfieldcontent.EditFieldContentViewModel;
+import org.jabref.gui.undo.NamedCompound;
+import org.jabref.model.database.BibDatabase;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class EditFieldContentTabViewModelTest {
+    EditFieldContentViewModel editFieldContentViewModel;
+    BibEntry entryA;
+    BibEntry entryB;
+
+    BibDatabase bibDatabase;
+
+    @BeforeEach
+    void setup() {
+        entryA = new BibEntry(BibEntry.DEFAULT_TYPE)
+                .withField(StandardField.YEAR, "2015")
+                .withField(StandardField.DATE, "2014");
+
+        entryB = new BibEntry(BibEntry.DEFAULT_TYPE)
+                .withField(StandardField.DATE, "1998")
+                .withField(StandardField.YEAR, "");
+
+        bibDatabase = new BibDatabase();
+        editFieldContentViewModel = new EditFieldContentViewModel(bibDatabase, List.of(entryA, entryB), new NamedCompound(""));
+    }
+
+    @Test
+    void clearSelectedFieldShouldClearFieldContentEvenWhenOverwriteFieldContentIsNotEnabled() {
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.overwriteFieldContentProperty().set(false);
+        editFieldContentViewModel.clearSelectedField();
+
+        assertEquals(Optional.empty(), entryA.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void clearSelectedFieldShouldDoNothingWhenFieldDoesntExistOrIsEmpty() {
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.FILE);
+        editFieldContentViewModel.clearSelectedField();
+
+        assertEquals(Optional.empty(), entryA.getField(StandardField.FILE));
+    }
+
+    @Test
+    void setFieldValueShouldNotDoAnythingIfOverwriteFieldContentIsNotEnabled() {
+        editFieldContentViewModel.overwriteFieldContentProperty().set(false);
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.fieldValueProperty().set("2001");
+        editFieldContentViewModel.setFieldValue();
+
+        assertEquals(Optional.of("2015"), entryA.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void setFieldValueShouldSetFieldValueIfOverwriteFieldContentIsEnabled() {
+        editFieldContentViewModel.overwriteFieldContentProperty().set(true);
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.fieldValueProperty().set("2001");
+        editFieldContentViewModel.setFieldValue();
+
+        assertEquals(Optional.of("2001"), entryA.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void setFieldValueShouldSetFieldValueIfFieldContentIsEmpty() {
+        editFieldContentViewModel.overwriteFieldContentProperty().set(false);
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.fieldValueProperty().set("2001");
+        editFieldContentViewModel.setFieldValue();
+
+        assertEquals(Optional.of("2001"), entryB.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void appendToFieldValueShouldDoNothingIfOverwriteFieldContentIsNotEnabled() {
+        editFieldContentViewModel.overwriteFieldContentProperty().set(false);
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.fieldValueProperty().set("0");
+        editFieldContentViewModel.appendToFieldValue();
+
+        assertEquals(Optional.of("2015"), entryA.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void appendToFieldValueShouldAppendFieldValueIfOverwriteFieldContentIsEnabled() {
+        editFieldContentViewModel.overwriteFieldContentProperty().set(true);
+        editFieldContentViewModel.selectedFieldProperty().set(StandardField.YEAR);
+        editFieldContentViewModel.fieldValueProperty().set("0");
+        editFieldContentViewModel.appendToFieldValue();
+
+        assertEquals(Optional.of("20150"), entryA.getField(StandardField.YEAR));
+    }
+
+    @Test
+    void getAllFieldsShouldNeverBeEmpty() {
+        assertNotEquals(0, editFieldContentViewModel.getAllFields().size());
+    }
+
+    @Test
+    void getSelectedFieldShouldHaveADefaultValue() {
+        assertNotEquals(null, editFieldContentViewModel.getSelectedField());
+    }
+}
