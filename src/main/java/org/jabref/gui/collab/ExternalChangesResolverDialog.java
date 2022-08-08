38f7137e280d501321913c6fe2b055d523fa7229
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-08 16:56:32 +0100

    Convert ExternalChangesResolverDialog to MVVM

--- src/main/java/org/jabref/gui/collab/ExternalChangesResolverDialog.java
+++ src/main/java/org/jabref/gui/collab/ExternalChangesResolverDialog.java
@@ -3,10 +3,8 @@ package org.jabref.gui.collab;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
@@ -14,7 +12,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
-import org.jabref.gui.undo.NamedCompound;
 import org.jabref.gui.util.BaseDialog;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.BibDatabaseContext;
@@ -40,13 +37,10 @@ public class ExternalChangesResolverDialog extends BaseDialog<Boolean> {
     private TableColumn<DatabaseChangeViewModel, String> changeName;
 
     private final BibDatabaseContext database;
-    private final NamedCompound ce = new NamedCompound(Localization.lang("Merged external changes"));
-
-    private final BooleanBinding areChangesResolved;
-
-    private final BooleanBinding canOpenAdvancedMergeDialog;
 
     private final List<DatabaseChangeViewModel> changes;
+
+    private ExternalChangesResolverViewModel viewModel;
 
     public ExternalChangesResolverDialog(BibDatabaseContext database, List<DatabaseChangeViewModel> changes) {
         this.database = database;
@@ -57,72 +51,61 @@ public class ExternalChangesResolverDialog extends BaseDialog<Boolean> {
 
         ViewLoader.view(this)
                 .load().setAsDialogPane(this);
-        changesTableView.itemsProperty().set(FXCollections.observableList(changes));
-        changesTableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        changesTableView.getSelectionModel().selectFirst();
-
-        areChangesResolved = Bindings.createBooleanBinding(() -> changesTableView.getItems().isEmpty(), changesTableView.itemsProperty().getValue());
-        EasyBind.subscribe(areChangesResolved, isResolved -> {
-            if (isResolved) {
-                LOGGER.info("Closing ExternalChangesResolverDialog");
-                close();
-            }
-        });
-
-        canOpenAdvancedMergeDialog = Bindings.createBooleanBinding(() -> changesTableView.getSelectionModel().getSelectedItems().size() == 1 &&
-                changesTableView.getSelectionModel().getSelectedItems().get(0).hasAdvancedMergeDialog(), changesTableView.getSelectionModel().getSelectedItems());
-
-        openAdvancedMergeDialogButton.disableProperty().bind(canOpenAdvancedMergeDialog.not());
 
         setResultConverter(button -> {
-            if (areChangesResolved.get()) {
+            if (viewModel.areAllChangesResolved()) {
                 LOGGER.info("External changes are resolved successfully");
-                Platform.runLater(this::applyChanges);
-                // TODO: panel.getUndoManager().addEdit(ce);
                 return true;
             } else {
                 LOGGER.info("External changes ARE NOT resolved");
                 return false;
             }
         });
-        EasyBind.subscribe(changesTableView.getSelectionModel().selectedItemProperty(), selectedChange -> {
-            if (selectedChange != null) {
-                changeInfoPane.setCenter(selectedChange.description());
+    }
+
+    @FXML
+    private void initialize() {
+        viewModel = new ExternalChangesResolverViewModel(changes, database);
+
+        changeName.setCellValueFactory(data -> data.getValue().nameProperty());
+
+        changesTableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        changesTableView.getSelectionModel().selectFirst();
+
+        EasyBind.bindContent(changesTableView.getItems(), viewModel.getChanges());
+        EasyBind.bindContent(viewModel.getSelectedChanges(), changesTableView.getSelectionModel().getSelectedItems());
+
+        viewModel.lastSelectedChangeProperty().bind(changesTableView.getSelectionModel().selectedItemProperty());
+
+        EasyBind.subscribe(viewModel.lastSelectedChangeProperty(), lastSelectedChange -> {
+            if (lastSelectedChange != null) {
+                changeInfoPane.setCenter(lastSelectedChange.description());
+            }
+        });
+
+        openAdvancedMergeDialogButton.disableProperty().bind(viewModel.canOpenAdvancedMergeDialogProperty().not());
+
+        EasyBind.subscribe(viewModel.areAllChangesResolvedProperty(), isResolved -> {
+            if (isResolved) {
+                LOGGER.info("Closing ExternalChangesResolverDialog");
+                close();
             }
         });
     }
 
     @FXML
-    private void initialize() {
-        changeName.setCellValueFactory(data -> data.getValue().nameProperty());
-    }
-
-    @FXML
     public void denyChanges() {
-        changesTableView.getItems().removeAll(changesTableView.getSelectionModel().getSelectedItems());
+        viewModel.denySelectedChanges();
     }
 
     @FXML
     public void acceptChanges() {
-        // Changes will be applied when closing the dialog as a transaction
-        changesTableView.getSelectionModel().getSelectedItems().forEach(DatabaseChangeViewModel::accept);
-        changesTableView.getItems().removeAll(changesTableView.getSelectionModel().getSelectedItems());
+        viewModel.acceptSelectedChanges();
     }
 
     @FXML
     public void openAdvancedMergeDialog() {
-        assert changesTableView.getSelectionModel().getSelectedItems().size() == 1;
-        assert changesTableView.getSelectionModel().getSelectedItems().get(0).hasAdvancedMergeDialog();
-
-        DatabaseChangeViewModel changeViewModel = changesTableView.getSelectionModel().getSelectedItems().get(0);
-        changeViewModel.openAdvancedMergeDialog().ifPresent(change -> {
-            change.accept();
-            changesTableView.getItems().removeAll(changeViewModel);
-        });
-    }
-
-    private void applyChanges() {
-        changes.stream().filter(DatabaseChangeViewModel::isAccepted).forEach(change -> change.makeChange(database, ce));
-        ce.end();
+        viewModel.getLastSelectedChange().flatMap(DatabaseChangeViewModel::openAdvancedMergeDialog)
+                 .ifPresent(change -> viewModel.acceptSelectedChanges());
     }
 }
--- src/main/java/org/jabref/gui/collab/ExternalChangesResolverViewModel.java
+++ src/main/java/org/jabref/gui/collab/ExternalChangesResolverViewModel.java
@@ -1,6 +1,102 @@
 package org.jabref.gui.collab;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import org.jabref.gui.AbstractViewModel;
+import org.jabref.gui.undo.NamedCompound;
+import org.jabref.logic.l10n.Localization;
+import org.jabref.model.database.BibDatabaseContext;
+
+import com.tobiasdiez.easybind.EasyBind;
 
 public class ExternalChangesResolverViewModel extends AbstractViewModel {
+    private final NamedCompound ce = new NamedCompound(Localization.lang("Merged external changes"));
+
+    private final BibDatabaseContext databaseContext;
+    private final ObservableList<DatabaseChangeViewModel> changes = FXCollections.observableArrayList();
+
+    /**
+     * Because the other changes list will be bound to the UI, certain changes can be removed. This list is used to keep
+     * track of changes even when they're removed from the UI.
+     */
+    private final ObservableList<DatabaseChangeViewModel> immutableChanges = FXCollections.observableArrayList();
+
+    private final ObservableList<DatabaseChangeViewModel> selectedChanges = FXCollections.observableArrayList();
+
+    private final ObjectProperty<DatabaseChangeViewModel> lastSelectedChange = new SimpleObjectProperty<>();
+
+    private final BooleanBinding areAllChangesResolved;
+
+    private final BooleanBinding canOpenAdvancedMergeDialog;
+
+    public ExternalChangesResolverViewModel(List<DatabaseChangeViewModel> externalChanges, BibDatabaseContext databaseContext) {
+        Objects.requireNonNull(databaseContext);
+        Objects.requireNonNull(externalChanges);
+        assert !externalChanges.isEmpty();
+
+        this.changes.addAll(externalChanges);
+        this.immutableChanges.addAll(externalChanges);
+        this.databaseContext = databaseContext;
+
+        areAllChangesResolved = Bindings.createBooleanBinding(changes::isEmpty, changes);
+        canOpenAdvancedMergeDialog = Bindings.createBooleanBinding(() -> selectedChanges.size() == 1 && lastSelectedChange.get() != null && lastSelectedChange.get().hasAdvancedMergeDialog(), selectedChanges);
+
+        EasyBind.subscribe(areAllChangesResolved, isResolved -> {
+            if (isResolved) {
+                Platform.runLater(this::makeChanges);
+            }
+        });
+    }
+
+    public ObservableList<DatabaseChangeViewModel> getChanges() {
+        return changes;
+    }
+
+    public ObservableList<DatabaseChangeViewModel> getSelectedChanges() {
+        return selectedChanges;
+    }
+
+    public ObjectProperty<DatabaseChangeViewModel> lastSelectedChangeProperty() {
+        return lastSelectedChange;
+    }
+
+    public Optional<DatabaseChangeViewModel> getLastSelectedChange() {
+        return Optional.ofNullable(lastSelectedChangeProperty().get());
+    }
+
+    public BooleanBinding areAllChangesResolvedProperty() {
+        return areAllChangesResolved;
+    }
+
+    public boolean areAllChangesResolved() {
+        return areAllChangesResolvedProperty().get();
+    }
+
+    public BooleanBinding canOpenAdvancedMergeDialogProperty() {
+        return canOpenAdvancedMergeDialog;
+    }
+
+    public void acceptSelectedChanges() {
+        getSelectedChanges().forEach(DatabaseChangeViewModel::accept);
+        getChanges().removeAll(getSelectedChanges());
+    }
+
+    public void denySelectedChanges() {
+        getChanges().removeAll(getSelectedChanges());
+    }
+
+    private void makeChanges() {
+        immutableChanges.stream().filter(DatabaseChangeViewModel::isAccepted).forEach(change -> change.makeChange(databaseContext, ce));
+        ce.end();
+    }
 }
