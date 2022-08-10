85c5bc6a8e9d6a8b637234d0b68b8be4bb6e203b
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-10 20:51:13 +0100

    Commit

--- src/main/java/org/jabref/gui/collab/experimental/ExternalChangeDetailsViewFactory.java
+++ src/main/java/org/jabref/gui/collab/experimental/ExternalChangeDetailsViewFactory.java
@@ -1,13 +1,8 @@
 package org.jabref.gui.collab.experimental;
 
-import org.jabref.gui.collab.experimental.entrychange.EntryChangeDetailsView;
-import org.jabref.gui.collab.experimental.entrychange.EntryChangeResolver;
-
 public class ExternalChangeDetailsViewFactory {
 
     public ExternalChangeDetailsView create(ExternalChangeResolver resolver) {
-        return switch (resolver) {
-            case EntryChangeResolver entryChange -> new EntryChangeDetailsView(entryChange.getName(), null, null);
-        };
+        return null;
     }
 }
