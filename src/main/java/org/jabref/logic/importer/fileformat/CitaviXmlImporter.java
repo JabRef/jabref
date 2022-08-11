5d978f37a465a28c45c77283512e1df0195ff9e7
Siedlerchr <siedlerkiller@gmail.com>
2022-08-11 16:50:52 +0200

    checkstyle import

--- src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
@@ -22,7 +22,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
