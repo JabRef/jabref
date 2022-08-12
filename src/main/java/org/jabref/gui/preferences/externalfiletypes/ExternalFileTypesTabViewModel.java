3a51931cfd008932b4e9abdbd8eda94e9f3827ec
Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
2022-08-12 14:48:25 +0200

    Fixed table update in eft preferences

--- src/main/java/org/jabref/gui/preferences/externalfiletypes/ExternalFileTypesTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/externalfiletypes/ExternalFileTypesTabViewModel.java
@@ -1,77 +1,77 @@
 package org.jabref.gui.preferences.externalfiletypes;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import org.jabref.gui.DialogService;
-import org.jabref.gui.externalfiletype.CustomExternalFileType;
-import org.jabref.gui.externalfiletype.EditExternalFileTypeEntryDialog;
 import org.jabref.gui.externalfiletype.ExternalFileType;
 import org.jabref.gui.externalfiletype.ExternalFileTypes;
-import org.jabref.gui.icon.IconTheme;
 import org.jabref.gui.preferences.PreferenceTabViewModel;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.preferences.FilePreferences;
 
-import com.airhacks.afterburner.injection.Injector;
-
 public class ExternalFileTypesTabViewModel implements PreferenceTabViewModel {
 
-    private final FilePreferences filePreferences;
-    private final ObservableList<ExternalFileType> fileTypes = FXCollections.observableArrayList();
+    private final ObservableList<ExternalFileTypeItemViewModel> fileTypes = FXCollections.observableArrayList();
 
-    public ExternalFileTypesTabViewModel(FilePreferences filePreferences) {
+    private final FilePreferences filePreferences;
+    private final DialogService dialogService;
+
+    public ExternalFileTypesTabViewModel(FilePreferences filePreferences, DialogService dialogService) {
         this.filePreferences = filePreferences;
+        this.dialogService = dialogService;
     }
 
     @Override
     public void setValues() {
-        fileTypes.setAll(filePreferences.getExternalFileTypes());
-        fileTypes.sort(Comparator.comparing(ExternalFileType::getName));
+        fileTypes.addAll(filePreferences.getExternalFileTypes().stream()
+                       .map(ExternalFileTypeItemViewModel::new)
+                       .toList());
+        fileTypes.sort(Comparator.comparing(ExternalFileTypeItemViewModel::getName));
     }
 
     public void storeSettings() {
+        Set<ExternalFileType> saveList = new HashSet<>();
+
+        fileTypes.stream().map(ExternalFileTypeItemViewModel::toExternalFileType)
+                 .forEach(type -> ExternalFileTypes.getDefaultExternalFileTypes().stream()
+                                                   .filter(type::equals).findAny()
+                                                   .ifPresentOrElse(saveList::add, () -> saveList.add(type)));
+
         filePreferences.getExternalFileTypes().clear();
-        filePreferences.getExternalFileTypes().addAll(fileTypes);
+        filePreferences.getExternalFileTypes().addAll(saveList);
     }
 
     public void resetToDefaults() {
-        List<ExternalFileType> list = ExternalFileTypes.getDefaultExternalFileTypes();
-        fileTypes.setAll(list);
-        fileTypes.sort(Comparator.comparing(ExternalFileType::getName));
+        fileTypes.setAll(ExternalFileTypes.getDefaultExternalFileTypes().stream()
+                                          .map(ExternalFileTypeItemViewModel::new)
+                                          .toList());
+        fileTypes.sort(Comparator.comparing(ExternalFileTypeItemViewModel::getName));
     }
 
     public void addNewType() {
-        CustomExternalFileType type = new CustomExternalFileType("", "", "", "", "new", IconTheme.JabRefIcons.FILE);
-        fileTypes.add(type);
-        showEditDialog(type, Localization.lang("Add new file type"));
+        ExternalFileTypeItemViewModel item = new ExternalFileTypeItemViewModel();
+        fileTypes.add(item);
+        showEditDialog(item, Localization.lang("Add new file type"));
     }
 
-    public ObservableList<ExternalFileType> getFileTypes() {
+    public ObservableList<ExternalFileTypeItemViewModel> getFileTypes() {
         return fileTypes;
     }
 
-    private void showEditDialog(ExternalFileType type, String dialogTitle) {
-        CustomExternalFileType typeForEdit;
-        if (type instanceof CustomExternalFileType) {
-            typeForEdit = (CustomExternalFileType) type;
-        } else {
-            typeForEdit = new CustomExternalFileType(type);
-            fileTypes.add(fileTypes.indexOf(type), typeForEdit);
-            fileTypes.remove(type);
-        }
-        DialogService dialogService = Injector.instantiateModelOrService(DialogService.class);
-        dialogService.showCustomDialogAndWait(new EditExternalFileTypeEntryDialog(typeForEdit, dialogTitle));
+    private void showEditDialog(ExternalFileTypeItemViewModel item, String dialogTitle) {
+        dialogService.showCustomDialogAndWait(new EditExternalFileTypeEntryDialog(item, dialogTitle));
     }
 
-    public void edit(ExternalFileType type) {
+    public void edit(ExternalFileTypeItemViewModel type) {
         showEditDialog(type, Localization.lang("Edit file type"));
     }
 
-    public void remove(ExternalFileType type) {
+    public void remove(ExternalFileTypeItemViewModel type) {
         fileTypes.remove(type);
     }
 }
