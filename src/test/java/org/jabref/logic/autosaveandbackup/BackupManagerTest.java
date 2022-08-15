1e2c2fa19d4bafa6ec295c7e276a005083290ae7
Oliver Kopp <kopp.dev@gmail.com>
2022-08-15 22:28:55 +0200

    Fix checkstyle

--- src/test/java/org/jabref/logic/autosaveandbackup/BackupManagerTest.java
+++ src/test/java/org/jabref/logic/autosaveandbackup/BackupManagerTest.java
@@ -9,7 +9,6 @@ import org.jabref.logic.util.io.BackupFileUtil;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
