f308f87fa9a15a4f2dc2ce2a190fa582c7ad7823
Benedikt Tutzer <btut@users.noreply.github.com>
2022-08-16 17:46:12 +0200

    Only show two decimal places of the search score

--- src/main/java/org/jabref/gui/maintable/MainTableColumnFactory.java
+++ src/main/java/org/jabref/gui/maintable/MainTableColumnFactory.java
@@ -141,7 +141,7 @@ public class MainTableColumnFactory {
         Tooltip.install(header, new Tooltip(MainTableColumnModel.Type.SCORE.getDisplayName()));
         column.setGraphic(header);
         column.setStyle("-fx-alignment: CENTER-RIGHT;");
-        column.setCellValueFactory(cellData -> cellData.getValue().searchScoreProperty().asString());
+        column.setCellValueFactory(cellData -> cellData.getValue().searchScoreProperty().asString("%.2f"));
         new ValueTableCellFactory<BibEntryTableViewModel, String>()
                 .withText(text -> text)
                 .install(column);
