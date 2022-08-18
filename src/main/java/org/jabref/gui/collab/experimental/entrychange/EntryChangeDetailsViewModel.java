82846d71a3feeef64514d966f47eae5683df9db1
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-18 20:55:17 +0100

    Remove EntryChangeDetailsViewModel.java because it's useless

--- src/main/java/org/jabref/gui/collab/experimental/entrychange/EntryChangeDetailsViewModel.java
+++
@@ -1,7 +0,0 @@
-package org.jabref.gui.collab.experimental.entrychange;
-
-import org.jabref.gui.collab.experimental.ExternalChangeDetailsViewModel;
-
-// TODO: This view model seems to be useless, as the view as very little state to manage
-public class EntryChangeDetailsViewModel extends ExternalChangeDetailsViewModel {
-}
