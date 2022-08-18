d2167f6a8faca4b868a1d9e6c764ce5d359539b2
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-18 20:47:09 +0100

    Initialize the preview of an Entry change

--- src/main/java/org/jabref/gui/collab/experimental/entrychange/EntryChangeDetailsView.java
+++ src/main/java/org/jabref/gui/collab/experimental/entrychange/EntryChangeDetailsView.java
@@ -1,23 +1,48 @@
 package org.jabref.gui.collab.experimental.entrychange;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.collab.experimental.ExternalChangeDetailsView;
+import org.jabref.gui.preview.PreviewViewer;
 import org.jabref.gui.theme.ThemeManager;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.preferences.PreferencesService;
 
 public final class EntryChangeDetailsView extends ExternalChangeDetailsView {
 
     private EntryChangeDetailsViewModel viewModel;
-    private EntryChange entryChange;
+    private final EntryChange entryChange;
+    private final BibDatabaseContext databaseContext;
+    private final DialogService dialogService;
+    private final StateManager stateManager;
+    private final ThemeManager themeManager;
 
-    public EntryChangeDetailsView(EntryChange entryChange, BibDatabaseContext bibDatabaseContext, DialogService dialogService, StateManager stateManager, ThemeManager themeManager) {
+    private PreferencesService preferencesService;
+
+    public EntryChangeDetailsView(EntryChange entryChange, BibDatabaseContext bibDatabaseContext, DialogService dialogService, StateManager stateManager, ThemeManager themeManager, PreferencesService preferencesService) {
+        this.entryChange = entryChange;
+        this.databaseContext = bibDatabaseContext;
+        this.dialogService = dialogService;
+        this.stateManager = stateManager;
+        this.themeManager = themeManager;
     }
 
     @FXML
     private void initialize() {
         viewModel = new EntryChangeDetailsViewModel();
+
+        PreviewViewer previewViewer = new PreviewViewer(databaseContext, dialogService, stateManager, themeManager);
+        previewViewer.setLayout(preferencesService.getPreviewPreferences().getSelectedPreviewLayout());
+        previewViewer.setEntry(entryChange.getNewEntry());
+
+        setLeftAnchor(previewViewer, 8d);
+        setTopAnchor(previewViewer, 8d);
+        setRightAnchor(previewViewer, 8d);
+        setBottomAnchor(previewViewer, 8d);
+
+        getChildren().setAll(previewViewer);
     }
 }
