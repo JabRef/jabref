19a36e581ac9ee507cbd172e1013440e222ca067
Houssem Nasri <housi.housi2015@gmail.com>
2022-08-27 22:47:00 +0100

    Extract logic from ThreeWayMergeCell to a view model

---
+++ src/main/java/org/jabref/gui/mergeentries/newmergedialog/cell/ThreeWayMergeCellViewModel.java
@@ -0,0 +1,79 @@
+package org.jabref.gui.mergeentries.newmergedialog.cell;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import com.tobiasdiez.easybind.EasyBind;
+
+import static org.jabref.gui.mergeentries.newmergedialog.cell.ThreeWayMergeCell.HEADER_ROW;
+
+public class ThreeWayMergeCellViewModel {
+    private final StringProperty text = new SimpleStringProperty();
+    private final BooleanProperty odd = new SimpleBooleanProperty(ThreeWayMergeCell.class, "odd");
+    private final BooleanProperty even = new SimpleBooleanProperty(ThreeWayMergeCell.class, "even");
+
+    public ThreeWayMergeCellViewModel(String text, int rowIndex) {
+        setText(text);
+        if (rowIndex != HEADER_ROW) {
+            if (rowIndex % 2 == 1) {
+                odd.setValue(true);
+            } else {
+                even.setValue(true);
+            }
+        }
+
+        EasyBind.subscribe(odd, isOdd -> {
+           if (isOdd) {
+               setEven(false);
+           } else {
+               setOdd(false);
+           }
+        });
+
+        EasyBind.subscribe(even, isEven -> {
+            if (isEven) {
+                setOdd(false);
+            } else {
+                setEven(false);
+            }
+        });
+    }
+
+    public String getText() {
+        return text.get();
+    }
+
+    public StringProperty textProperty() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text.set(text);
+    }
+
+    public boolean isOdd() {
+        return odd.get();
+    }
+
+    public BooleanProperty oddProperty() {
+        return odd;
+    }
+
+    public void setOdd(boolean odd) {
+        this.odd.set(odd);
+    }
+
+    public boolean isEven() {
+        return even.get();
+    }
+
+    public BooleanProperty evenProperty() {
+        return even;
+    }
+
+    public void setEven(boolean even) {
+        this.even.set(even);
+    }
+}
