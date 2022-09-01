4143b1d3bd7b6891926039f4b05f8a3db9e659d8
Oliver Kopp <kopp.dev@gmail.com>
2022-09-01 23:18:11 +0200

    Add comment

--- src/main/java/org/jabref/logic/layout/format/FileLinkPreferences.java
+++ src/main/java/org/jabref/logic/layout/format/FileLinkPreferences.java
@@ -17,6 +17,15 @@ public class FileLinkPreferences {
         return mainFileDirectory;
     }
 
+    /**
+     * The following field is used as a global variable during the export of a database.
+     * By setting this field to the path of the database's default file directory, formatters
+     * that should resolve external file paths can access this field. This is an ugly hack
+     * to solve the problem of formatters not having access to any context except for the
+     * string to be formatted and possible formatter arguments.
+     *
+     * See also {@link org.jabref.preferences.JabRefPreferences#fileDirForDatabase}
+     */
     public List<Path> getFileDirForDatabase() {
         return fileDirForDatabase;
     }
