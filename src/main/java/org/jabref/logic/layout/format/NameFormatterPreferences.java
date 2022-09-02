25441a088d6b1241c1383640d70dd298d5952159
Kevin Klein <kevin@0x002A.com>
2022-09-02 22:34:53 +0200

    Fix code style.

--- src/main/java/org/jabref/logic/layout/format/NameFormatterPreferences.java
+++ src/main/java/org/jabref/logic/layout/format/NameFormatterPreferences.java
@@ -1,9 +1,9 @@
 package org.jabref.logic.layout.format;
 
+import java.util.List;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
-import java.util.List;
 
 public class NameFormatterPreferences {
 
