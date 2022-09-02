db875b0e50f1c8e8e8ff72aa09d44ec486656fdb
Siedlerchr <siedlerkiller@gmail.com>
2022-09-02 16:38:31 +0200

    Cleanup

--- src/test/java/org/jabref/cli/ArgumentProcessorTest.java
+++ src/test/java/org/jabref/cli/ArgumentProcessorTest.java
@@ -8,6 +8,7 @@ import org.jabref.cli.ArgumentProcessor.Mode;
 import org.jabref.logic.exporter.SavePreferences;
 import org.jabref.model.metadata.SaveOrderConfig;
 import org.jabref.preferences.PreferencesService;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -17,14 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 class ArgumentProcessorTest {
 
     private ArgumentProcessor processor;
-
     private final PreferencesService preferencesService = mock(PreferencesService.class, Answers.RETURNS_DEEP_STUBS);
     private final SavePreferences savePreferences = mock(SavePreferences.class, Answers.RETURNS_DEEP_STUBS);
-
 
     @BeforeEach()
     void setup() {
@@ -32,14 +30,13 @@ class ArgumentProcessorTest {
         when(preferencesService.getSavePreferences()).thenReturn(savePreferences);
     }
 
-
     @Test
-    void testAux(@TempDir Path tempDir) throws Exception {
+    void testAuxImport(@TempDir Path tempDir) throws Exception {
 
         String auxFile = Path.of(AuxCommandLineTest.class.getResource("paper.aux").toURI()).toAbsolutePath().toString();
         String originBib = Path.of(AuxCommandLineTest.class.getResource("origin.bib").toURI()).toAbsolutePath().toString();
 
-        Path outputBib = tempDir.resolve("output.bib").toAbsolutePath();
+        Path outputBib = tempDir.resolve("output.bisb").toAbsolutePath();
         String outputBibFile = outputBib.toAbsolutePath().toString();
 
         List<String> args = List.of("--nogui", "--debug", "--aux", auxFile + "," + outputBibFile, originBib);
@@ -47,7 +44,5 @@ class ArgumentProcessorTest {
         processor = new ArgumentProcessor(args.toArray(String[]::new), Mode.INITIAL_START, preferencesService);
 
         assertTrue(Files.exists(outputBib));
-
     }
-
 }
