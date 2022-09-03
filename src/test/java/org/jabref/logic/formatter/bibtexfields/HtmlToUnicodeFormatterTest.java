b98354446b2cdb677e39bb09ba564f1e8914773c
Siedlerchr <siedlerkiller@gmail.com>
2022-09-03 12:02:13 +0200

    fix checkstyle and tests

--- src/test/java/org/jabref/logic/formatter/bibtexfields/HtmlToUnicodeFormatterTest.java
+++ src/test/java/org/jabref/logic/formatter/bibtexfields/HtmlToUnicodeFormatterTest.java
@@ -17,16 +17,15 @@ public class HtmlToUnicodeFormatterTest {
         return Stream.of(
                          Arguments.of("abc", "abc"),
                          Arguments.of("åäö", "&aring;&auml;&ouml;"),
-                         Arguments.of("i", "i&#x301;"),
+                         Arguments.of("í", "i&#x301;"),
                          Arguments.of("Ε", "&Epsilon;"),
                          Arguments.of("ä", "&auml;"),
-                         Arguments.of("ä", "&#228"),
+                         Arguments.of("ä", "&#228;"),
                          Arguments.of("ä", "&#xe4;"),
                          Arguments.of("ñ", "&#241;"),
                          Arguments.of("aaa", "<p>aaa</p>"),
                          Arguments.of("bread & butter", "<b>bread</b> &amp; butter"));
     }
-
 
     @BeforeEach
     public void setUp() {
