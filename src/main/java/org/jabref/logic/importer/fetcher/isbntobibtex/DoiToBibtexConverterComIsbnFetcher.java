67054277455f2e3ff7d9c2a8a89c886f710fdaef
ezienecker <manuzidtv@gmail.com>
2022-10-03 14:06:13 +0200

    ISSUE-9145: Add new ISBN fetcher doi-to-bibtex-converter.herokuapp.com

---
+++ src/main/java/org/jabref/logic/importer/fetcher/isbntobibtex/DoiToBibtexConverterComIsbnFetcher.java
@@ -0,0 +1,107 @@
+package org.jabref.logic.importer.fetcher.isbntobibtex;
+
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONException;
+import kong.unirest.json.JSONObject;
+import org.apache.http.client.utils.URIBuilder;
+import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.importer.ParseException;
+import org.jabref.logic.importer.Parser;
+import org.jabref.logic.importer.fetcher.AbstractIsbnFetcher;
+import org.jabref.logic.importer.util.JsonReader;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
+import org.jabref.model.strings.StringUtil;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Fetcher for ISBN using <a href="https://doi-to-bibtex-converter.herokuapp.com">https://doi-to-bibtex-converter.herokuapp.com</a>.
+ */
+public class DoiToBibtexConverterComIsbnFetcher extends AbstractIsbnFetcher {
+    private static final String BASE_URL = "https://doi-to-bibtex-converter.herokuapp.com";
+
+    public DoiToBibtexConverterComIsbnFetcher(ImportFormatPreferences importFormatPreferences) {
+        super(importFormatPreferences);
+    }
+
+    @Override
+    public String getName() {
+        return "ISBN (doi-to-bibtex-converter.herokuapp.com)";
+    }
+
+    @Override
+    public URL getUrlForIdentifier(String identifier) throws URISyntaxException, MalformedURLException, FetcherException {
+        this.ensureThatIsbnIsValid(identifier);
+        return new URIBuilder(BASE_URL)
+                .setPathSegments("getInfo.php")
+                .setParameter("query", identifier)
+                .setParameter("format", "json")
+                .build()
+                .toURL();
+    }
+
+    @Override
+    public Parser getParser() {
+        return inputStream -> {
+            JSONObject response = JsonReader.toJsonObject(inputStream);
+            if (response.isEmpty()) {
+                return Collections.emptyList();
+            }
+
+            String error = response.optString("error");
+            if (StringUtil.isNotBlank(error)) {
+                throw new ParseException(error);
+            }
+
+            BibEntry entry = jsonItemToBibEntry(response);
+            return List.of(entry);
+        };
+    }
+
+    @Override
+    public void doPostCleanup(BibEntry entry) {
+    }
+
+    private BibEntry jsonItemToBibEntry(JSONObject item) throws ParseException {
+        try {
+            JSONArray data = item.optJSONArray("data");
+            var type = getElementFromJSONArrayByKey(data, "type");
+
+            BibEntry entry = new BibEntry(evaluateBibEntryTypeFromString(type));
+            entry.setField(StandardField.AUTHOR, getElementFromJSONArrayByKey(data, "author"));
+            entry.setField(StandardField.PAGES, getElementFromJSONArrayByKey(data, "pagecount"));
+            entry.setField(StandardField.ISBN, getElementFromJSONArrayByKey(data, "isbn"));
+            entry.setField(StandardField.TITLE, getElementFromJSONArrayByKey(data, "title"));
+            entry.setField(StandardField.YEAR, getElementFromJSONArrayByKey(data, "year"));
+            entry.setField(StandardField.MONTH, getElementFromJSONArrayByKey(data, "month"));
+            entry.setField(StandardField.DAY, getElementFromJSONArrayByKey(data, "day"));
+            return entry;
+        } catch (JSONException exception) {
+            throw new ParseException("CrossRef API JSON format has changed", exception);
+        }
+    }
+
+    private String getElementFromJSONArrayByKey(JSONArray jsonArray, String key) {
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(jsonArray::getJSONObject)
+                .map(obj -> obj.getString(key))
+                .findFirst()
+                .orElse("");
+    }
+
+    private StandardEntryType evaluateBibEntryTypeFromString(String type) {
+        return Stream.of(StandardEntryType.values())
+                .filter(entryType -> entryType.name().equalsIgnoreCase(type))
+                .findAny()
+                .orElse(StandardEntryType.Book);
+    }
+}
---
+++ src/test/java/org/jabref/logic/importer/fetcher/isbntobibtex/DoiToBibtexConverterComIsbnFetcherTest.java
@@ -0,0 +1,86 @@
+package org.jabref.logic.importer.fetcher.isbntobibtex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.jabref.logic.importer.FetcherClientException;
+import org.jabref.logic.importer.FetcherException;
+import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.importer.fetcher.AbstractIsbnFetcherTest;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.entry.types.StandardEntryType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+
+import java.util.Optional;
+
+public class DoiToBibtexConverterComIsbnFetcherTest extends AbstractIsbnFetcherTest {
+
+    @BeforeEach
+    public void setUp() {
+        bibEntryEffectiveJava = new BibEntry(StandardEntryType.Book)
+                .withField(StandardField.TITLE, "Effective Java(TM) Programming Language Guide (2nd Edition) (The Java Series)")
+                .withField(StandardField.PUBLISHER, "Prentice Hall PTR")
+                .withField(StandardField.YEAR, "2007")
+                .withField(StandardField.AUTHOR, "Bloch, Joshua")
+                .withField(StandardField.ISBN, "9780321356680")
+                .withField(StandardField.PAGES, "256");
+
+        fetcher = new DoiToBibtexConverterComIsbnFetcher(mock(ImportFormatPreferences.class, Answers.RETURNS_DEEP_STUBS));
+    }
+
+    @Test
+    @Override
+    public void testName() {
+        assertEquals("ISBN (doi-to-bibtex-converter.herokuapp.com)", fetcher.getName());
+    }
+
+    @Test
+    @Disabled
+    @Override
+    public void searchByIdSuccessfulWithShortISBN() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    @Disabled
+    @Override
+    public void searchByIdSuccessfulWithLongISBN() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    @Override
+    public void authorsAreCorrectlyFormatted() throws Exception {
+        BibEntry bibEntry = new BibEntry(StandardEntryType.Book)
+                .withField(StandardField.TITLE, "Repository")
+                .withField(StandardField.ISBN, "9783110702125")
+                .withField(StandardField.AUTHOR, "Hans-Joachim Habermann and Frank Leymann")
+                .withField(StandardField.PAGES, "294")
+                .withField(StandardField.YEAR, "2020")
+                .withField(StandardField.DAY, "12")
+                .withField(StandardField.MONTH, "10");
+        Optional<BibEntry> fetchedEntry = fetcher.performSearchById("9783110702125");
+        assertEquals(Optional.of(bibEntry), fetchedEntry);
+    }
+
+    @Test
+    public void testIsbnNeitherAvailable() {
+        assertThrows(FetcherClientException.class, () -> fetcher.performSearchById("9785646216541"));
+    }
+
+    @Test
+    public void searchByIdFailedWithLongISBN() {
+        assertThrows(FetcherClientException.class, () -> fetcher.performSearchById("9780321356680"));
+    }
+
+    @Test
+    public void searchByIdFailedWithShortISBN() throws FetcherException {
+        assertThrows(FetcherClientException.class, () -> fetcher.performSearchById("0321356683"));
+    }
+}
