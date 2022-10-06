50e4afa83ac63fd70497ec0400868302d937d372
Lord <github@gor1lla.de>
2022-10-07 00:46:18 +0200

    disable menu for normalize BibTex name format when empty

--- src/main/java/org/jabref/gui/fieldeditors/contextmenu/EditorMenus.java
+++ src/main/java/org/jabref/gui/fieldeditors/contextmenu/EditorMenus.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
+import com.tobiasdiez.easybind.EasyBind;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
@@ -19,6 +20,7 @@ import org.jabref.gui.edit.CopyDoiUrlAction;
 import org.jabref.logic.formatter.bibtexfields.CleanupUrlFormatter;
 import org.jabref.logic.formatter.bibtexfields.NormalizeNamesFormatter;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.model.strings.StringUtil;
 
 /**
  * Provides context menus for the text fields of the entry editor. Note that we use {@link Supplier} to prevent an early
@@ -37,6 +39,7 @@ public class EditorMenus {
     public static Supplier<List<MenuItem>> getNameMenu(final TextInputControl textInput) {
         return () -> {
             CustomMenuItem normalizeNames = new CustomMenuItem(new Label(Localization.lang("Normalize to BibTeX name format")));
+            EasyBind.subscribe(textInput.textProperty(), value -> normalizeNames.setDisable(StringUtil.isNullOrEmpty(value)));
             normalizeNames.setOnAction(event -> textInput.setText(new NormalizeNamesFormatter().format(textInput.getText())));
             Tooltip toolTip = new Tooltip(Localization.lang("If possible, normalize this list of names to conform to standard BibTeX name formatting"));
             Tooltip.install(normalizeNames.getContent(), toolTip);
