ab5a4e6e7c80e096ec4b40e8463af89a1ab57715
Kevin Klein <kevin@0x002A.com>
2022-10-09 14:32:57 +0200

    Place subgroups w.r.t. alphabetical ordering

--- src/main/java/org/jabref/gui/groups/GroupTreeViewModel.java
+++ src/main/java/org/jabref/gui/groups/GroupTreeViewModel.java
@@ -150,7 +150,8 @@ public class GroupTreeViewModel extends AbstractViewModel {
     }
 
     /**
-     * Opens "New Group Dialog" and add the resulting group to the specified group
+     * Opens "New Group Dialog" and adds the resulting group as subgroup to the specified group while maintaining the
+     * alphabetical order
      */
 
     public void addNewSubgroup(GroupNodeViewModel parent, GroupDialogHeader groupDialogHeader) {
@@ -171,6 +172,8 @@ public class GroupTreeViewModel extends AbstractViewModel {
 
                 // TODO: Expand parent to make new group visible
                 // parent.expand();
+
+                sortAlphabeticallyRecursive(parent.getGroupNode());
 
                 dialogService.notify(Localization.lang("Added group \"%0\".", group.getName()));
                 writeGroupChangesToMetaData();
@@ -259,6 +262,11 @@ public class GroupTreeViewModel extends AbstractViewModel {
                 // if (!addChange.isEmpty()) {
                 //    undoAddPreviousEntries = UndoableChangeEntriesOfGroup.getUndoableEdit(null, addChange);
                 // }
+
+                oldGroup.getParent().ifPresent(parent -> {
+                    sortAlphabeticallyRecursive(parent);
+                });
+
 
                 dialogService.notify(Localization.lang("Modified group \"%0\".", group.getName()));
                 writeGroupChangesToMetaData();
@@ -420,7 +428,7 @@ public class GroupTreeViewModel extends AbstractViewModel {
         //    mPanel.getUndoManager().addEdit(UndoableChangeEntriesOfGroup.getUndoableEdit(mNode, undo));
     }
 
-    public void sortAlphabeticallyRecursive(GroupNodeViewModel group) {
-        group.getGroupNode().sortChildren(compAlphabetIgnoreCase, true);
+    public void sortAlphabeticallyRecursive(GroupTreeNode group) {
+        group.sortChildren(compAlphabetIgnoreCase, true);
     }
 }
