bf5577eeb60ec5161a744a47eae6077589c4e4f4
Tobias Diez <tobiasdiez@gmx.de>
2022-10-19 21:10:10 +0000

    fix style

--- src/test/java/org/jabref/logic/tele/TelePreferencesTest.java
+++ src/test/java/org/jabref/logic/tele/TelePreferencesTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.jabref.logic.tele.TelePreferences;
-
 public class TelePreferencesTest {
 
     private TelePreferences preferences;
--- src/test/java/org/jabref/logic/tele/TeleUtilTest.java
+++ src/test/java/org/jabref/logic/tele/TeleUtilTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.jabref.logic.tele.TeleUtil;
-
 public class TeleUtilTest {
 
     @Test
