8435d961f9a33d76a0f703fb825bc0f7ccb34e30
Tobias Diez <tobiasdiez@gmx.de>
2022-10-21 22:22:39 +0000

    Extend transformer

--- src/main/java/org/jabref/logic/jabrefonline/JabRefOnlineTransformer.java
+++ src/main/java/org/jabref/logic/jabrefonline/JabRefOnlineTransformer.java
@@ -1,7 +1,13 @@
 package org.jabref.logic.jabrefonline;
 
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jabref.jabrefonline.UserChangesQuery.Author;
 import org.jabref.jabrefonline.UserChangesQuery.Node;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
 
 public class JabRefOnlineTransformer {
 
@@ -10,7 +16,35 @@ public class JabRefOnlineTransformer {
      */
     public BibEntry toBibEntry(Node document) {
         BibEntry entry = new BibEntry();
-        // TODO: Implement
+
+        if (!document.citationKeys.isEmpty()) {
+            entry.setCitationKey(document.citationKeys.get(0));
+            if (document.citationKeys.size() > 1) {
+                entry.setField(StandardField.IDS, String.join(", ", document.citationKeys.subList(1, document.citationKeys.size())));
+            }
+        }
+        entry.setField(StandardField.MODIFICATIONDATE, document.lastModified.format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+        entry.setField(StandardField.CREATIONDATE, document.added.format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+        entry.setField(StandardField.TITLE, document.title);
+        entry.setField(StandardField.SUBTITLE, document.subtitle);
+        entry.setField(StandardField.TITLEADDON, document.titleAddon);
+        entry.setField(StandardField.ABSTRACT, document.abstract_);
+        entry.setField(StandardField.AUTHOR, toBibAuthor(document.authors));
+        entry.setField(StandardField.NOTE, document.note);
+        entry.setField(StandardField.LANGUAGE, String.join(", ", document.languages));
+        entry.setField(StandardField.PUBSTATE, document.publicationState);
+        entry.setField(StandardField.DOI, document.doi);
+        entry.setField(StandardField.KEYWORDS, String.join(", ", document.keywords));
+
+        // TODO: Finish
         return entry;
-    }    
+    }
+
+    private String toBibAuthor(List<Author> authors) {
+        return authors.stream()
+        .map(author -> author.allDetails.onPerson)
+        .map(author -> author.name)
+        .collect(Collectors.joining(" and "));
+        // TODO: Handle onOrganization
+    }
 }
