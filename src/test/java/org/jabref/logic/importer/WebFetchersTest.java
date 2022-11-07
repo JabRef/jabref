e5fa5f9795ffd725b42a50d97786db7069e40f2f
Thiago Ferreira <thiagocf042@gmail.com>
2022-11-07 15:55:20 -0300

    Update WebFetchersTest.java to include error on Logger
    Co-authored-by: Christoph <siedlerkiller@gmail.com>

--- src/test/java/org/jabref/logic/importer/WebFetchersTest.java
+++ src/test/java/org/jabref/logic/importer/WebFetchersTest.java
@@ -56,7 +56,7 @@ class WebFetchersTest {
                              return Class.forName(classPath);
                          } catch (ClassNotFoundException e) {
                              LOGGER.error(e.getLocalizedMessage());
-                             LOGGER.error("Some of the ignored classes were not found");
+                             LOGGER.error("Some of the ignored classes were not found {}" e);
                              return null;
                          }
                      }).filter(Objects::nonNull).collect(Collectors.toSet());
