1e3bc02475ccef7183c6d866808f3e6be5e31cc5
Manu Vaillant <60919966+manu-vaillant-afk@users.noreply.github.com>
2022-11-30 20:02:07 +0100

    Exemplified menu entries for change case menu (#9382)
    * We modified the Change case sub-menus and their corresponding tips (displayed when you stay long over the menu) to properly reflect exemplified cases. [#9339](https://github.com/Jabref/jabref/issues/9339)
    
    * Use l10n for menu entries string
    
    * fix changelog codestyle issue
    
    * fix test issue
    
    Co-authored-by: manu-vaillant-afk <manu.vaillant@student.hers.be>

--- src/main/java/org/jabref/logic/formatter/casechanger/LowerCaseFormatter.java
+++ src/main/java/org/jabref/logic/formatter/casechanger/LowerCaseFormatter.java
@@ -7,7 +7,7 @@ public class LowerCaseFormatter extends Formatter {
 
     @Override
     public String getName() {
-        return Localization.lang("Lower case");
+        return Localization.lang("lower case");
     }
 
     @Override
@@ -29,7 +29,7 @@ public class LowerCaseFormatter extends Formatter {
 
     @Override
     public String getDescription() {
-        return Localization.lang("Changes all letters to lower case.");
+        return Localization.lang("changes all letters to lower case.");
     }
 
     @Override
