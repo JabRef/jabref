b4c083a4a61990d67faaaf8617e447feb1c856d6
Oliver Kopp <kopp.dev@gmail.com>
2022-12-13 00:20:19 +0100

    Fix checkstyle

--- src/main/java/org/jabref/gui/importer/actions/OpenDatabaseAction.java
+++ src/main/java/org/jabref/gui/importer/actions/OpenDatabaseAction.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
