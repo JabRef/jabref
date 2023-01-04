8a29d6e3c2b902b3ff9b9fbeee228c04ed308d24
Siedlerchr <siedlerkiller@gmail.com>
2023-01-04 15:34:58 +0100

    cleanup after merge

--- src/test/java/org/jabref/gui/fieldeditors/LinkedFilesEditorViewModelTest.java
+++ src/test/java/org/jabref/gui/fieldeditors/LinkedFilesEditorViewModelTest.java
@@ -3,8 +3,6 @@ package org.jabref.gui.fieldeditors;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.autocompleter.EmptySuggestionProvider;
 import org.jabref.gui.util.CurrentThreadTaskExecutor;
-import org.jabref.logic.importer.ImportFormatPreferences;
-import org.jabref.logic.importer.ImporterPreferences;
 import org.jabref.logic.integrity.FieldCheckers;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
@@ -17,19 +15,15 @@ import org.mockito.Answers;
 import static org.mockito.Mockito.mock;
 
 class LinkedFilesEditorViewModelTest {
+
     private LinkedFilesEditorViewModel ViewModel;
 
     private DialogService dialogService;
 
-    private ImportFormatPreferences importFormatPreferences;
-
-    private ImporterPreferences importerPreferences;
-
-
     @Test
-    void emptyUrlNotext(){
+    void emptyUrlNotext() {
         ViewModel = new LinkedFilesEditorViewModel(StandardField.FILE, new EmptySuggestionProvider(), mock(DialogService.class), mock(BibDatabaseContext.class),
-                new CurrentThreadTaskExecutor(), mock(FieldCheckers.class), mock(PreferencesService.class, Answers.RETURNS_DEEP_STUBS));
+                           new CurrentThreadTaskExecutor(), mock(FieldCheckers.class), mock(PreferencesService.class, Answers.RETURNS_DEEP_STUBS));
         ViewModel.entry = new BibEntry();
         ViewModel.entry.setField(StandardField.URL, "http://example.com");
         dialogService = mock(DialogService.class);
