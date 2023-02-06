669ba6024eef6f5471d8db0fab4f12a6d056e3fe
Houssem Nasri <housi.housi2015@gmail.com>
2023-02-06 23:54:40 +0100

    Re-fix memory leak

--- src/main/java/org/jabref/gui/fieldeditors/identifier/EprintIdentifierEditorViewModel.java
+++ src/main/java/org/jabref/gui/fieldeditors/identifier/EprintIdentifierEditorViewModel.java
@@ -1,5 +1,6 @@
 package org.jabref.gui.fieldeditors.identifier;
 
+import javafx.collections.MapChangeListener;
 import javafx.collections.WeakMapChangeListener;
 
 import org.jabref.gui.DialogService;
@@ -18,9 +19,15 @@ import com.tobiasdiez.easybind.EasyBind;
 
 public class EprintIdentifierEditorViewModel extends BaseIdentifierEditorViewModel<EprintIdentifier> {
 
-    // Since it's not obvious when to unregister the field listener, let's delegate this task to the garbage collector.
+    // The following listener will be wrapped in a weak reference change listener, thus it will be garbage collected
+    // automatically once this object is disposed.
     // https://en.wikipedia.org/wiki/Lapsed_listener_problem
-    private WeakMapChangeListener<Field, String> weakEprintTypeFieldListener;
+    private MapChangeListener<Field, String> eprintTypeFieldListener = change -> {
+        Field changedField = change.getKey();
+        if (StandardField.EPRINTTYPE.equals(changedField)) {
+            updateIdentifier();
+        }
+    };
 
     public EprintIdentifierEditorViewModel(SuggestionProvider<?> suggestionProvider, FieldCheckers fieldCheckers, DialogService dialogService, TaskExecutor taskExecutor, PreferencesService preferences) {
         super(StandardField.EPRINT, suggestionProvider, fieldCheckers, dialogService, taskExecutor, preferences);
@@ -40,16 +47,9 @@ public class EprintIdentifierEditorViewModel extends BaseIdentifierEditorViewMod
     @Override
     public void bindToEntry(BibEntry entry) {
         super.bindToEntry(entry);
-        weakEprintTypeFieldListener = new WeakMapChangeListener<>(change -> {
-                    Field changedField = change.getKey();
-                    if (StandardField.EPRINTTYPE.equals(changedField)) {
-                        updateIdentifier();
-                    }
-                }
-        );
         // Unlike other identifiers (they only depend on their own field value), eprint  depends on eprinttype thus
         // its identity changes whenever the eprinttype field changes .e.g. If eprinttype equals 'arxiv' then the eprint identity
         // will be of type ArXivIdentifier and if it equals 'ark' then it switches to type ARK.
-        entry.getFieldsObservable().addListener(weakEprintTypeFieldListener);
+        entry.getFieldsObservable().addListener(new WeakMapChangeListener<>(eprintTypeFieldListener));
     }
 }
