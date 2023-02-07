34605d71a4d0e55b86a7e68301c5a1c653236adb
lak7 <114904959+lak7@users.noreply.github.com>
2023-02-07 17:58:50 +0530

    Updated INSPIREFetcher.java
    Updated INSPIRE HEP Fetcher to implement the EntryBasedFetcher Interface.

--- src/main/java/org/jabref/logic/importer/fetcher/INSPIREFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/INSPIREFetcher.java
@@ -3,35 +3,61 @@ package org.jabref.logic.importer.fetcher;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.jabref.logic.cleanup.FieldFormatterCleanup;
 import org.jabref.logic.formatter.bibtexfields.ClearFormatter;
 import org.jabref.logic.formatter.bibtexfields.RemoveBracesFormatter;
 import org.jabref.logic.help.HelpFile;
-import org.jabref.logic.importer.FetcherException;
-import org.jabref.logic.importer.ImportFormatPreferences;
-import org.jabref.logic.importer.Parser;
-import org.jabref.logic.importer.SearchBasedParserFetcher;
+import org.jabref.logic.importer.*;
 import org.jabref.logic.importer.fetcher.transformers.DefaultLuceneQueryTransformer;
 import org.jabref.logic.importer.fileformat.BibtexParser;
 import org.jabref.logic.importer.util.MediaTypes;
 import org.jabref.logic.layout.format.LatexToUnicodeFormatter;
 import org.jabref.logic.net.URLDownload;
+import org.jabref.model.database.BibDatabase;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.field.UnknownField;
+import org.jabref.model.entry.identifier.DOI;
 import org.jabref.model.util.DummyFileUpdateMonitor;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
+import org.jabref.model.util.OptionalUtil;
+
+
+
+
+
+
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jbibtex.BibTeXDatabase;
+import org.jbibtex.BibTeXEntry;
+import org.jbibtex.BibTeXParser;
+import org.jbibtex.Key;
 
 /**
  * Fetches data from the INSPIRE database.
  */
-public class INSPIREFetcher implements SearchBasedParserFetcher {
+public class INSPIREFetcher implements SearchBasedParserFetcher, EntryBasedFetcher {
 
     private static final String INSPIRE_HOST = "https://inspirehep.net/api/literature/";
+    private static final String INSPIRE_EXTERNAL_HOST = "https://inspirehep.net/api/doi/";
 
     private final ImportFormatPreferences importFormatPreferences;
 
@@ -77,5 +103,43 @@ public class INSPIREFetcher implements SearchBasedParserFetcher {
     @Override
     public Parser getParser() {
         return new BibtexParser(importFormatPreferences, new DummyFileUpdateMonitor());
+    }
+
+
+    public List<BibEntry> performSearch(BibEntry entry) {
+        List<BibEntry> results = new ArrayList<>();
+
+        String doi = entry.getField(StandardField.DOI).orElse(null);
+        if (doi == null) {
+            return results;
+        }
+
+        String url = "https://inspirehep.net/api/doi" + doi;
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode != 200) {
+                return results;
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            Parser bibtexParser = new BibtexParser(importFormatPreferences, new DummyFileUpdateMonitor());
+            return bibtexParser.parseEntries(doi);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return results;
     }
 }
