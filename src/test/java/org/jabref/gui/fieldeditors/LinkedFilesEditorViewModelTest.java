8eca875a3b1aa544b7ed197956cc07818ef3fbef
Siedlerchr <siedlerkiller@gmail.com>
2023-02-27 19:47:57 +0100

    add test, mark as fetcher test

--- src/test/java/org/jabref/gui/fieldeditors/LinkedFilesEditorViewModelTest.java
+++ src/test/java/org/jabref/gui/fieldeditors/LinkedFilesEditorViewModelTest.java
@@ -1,32 +1,57 @@
 package org.jabref.gui.fieldeditors;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import javafx.collections.FXCollections;
+
 import org.jabref.gui.DialogService;
 import org.jabref.gui.autocompleter.EmptySuggestionProvider;
+import org.jabref.gui.externalfiletype.StandardExternalFileType;
 import org.jabref.gui.util.CurrentThreadTaskExecutor;
 import org.jabref.logic.integrity.FieldCheckers;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.preferences.FilePreferences;
 import org.jabref.preferences.PreferencesService;
+import org.jabref.testutils.category.FetcherTest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Answers;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@FetcherTest("in case our CI gets blocked")
 class LinkedFilesEditorViewModelTest {
 
     private LinkedFilesEditorViewModel viewModel;
-
-    private DialogService dialogService;
+    private final PreferencesService preferencesService = mock(PreferencesService.class, Answers.RETURNS_DEEP_STUBS);
+    private final FilePreferences filePreferences = mock(FilePreferences.class, Answers.RETURNS_DEEP_STUBS);
+    private final BibDatabaseContext bibDatabaseContext = mock(BibDatabaseContext.class);
 
     @Test
-    void emptyUrlNotext() {
-        viewModel = new LinkedFilesEditorViewModel(StandardField.FILE, new EmptySuggestionProvider(), mock(DialogService.class), mock(BibDatabaseContext.class),
-                           new CurrentThreadTaskExecutor(), mock(FieldCheckers.class), mock(PreferencesService.class, Answers.RETURNS_DEEP_STUBS));
-        viewModel.entry = new BibEntry();
-        viewModel.entry.setField(StandardField.URL, "http://example.com");
-        dialogService = mock(DialogService.class);
+    void urlFieldShouldDownloadFile(@TempDir Path tempDir) {
+
+        when(preferencesService.getFilePreferences()).thenReturn(filePreferences);
+        when(filePreferences.getExternalFileTypes()).thenReturn(FXCollections.observableSet(StandardExternalFileType.values()));
+        when(filePreferences.getFileNamePattern()).thenReturn("[bibtexkey]");
+        when(filePreferences.getFileDirectoryPattern()).thenReturn("");
+        when(bibDatabaseContext.getFirstExistingFileDir(any())).thenReturn(Optional.of(tempDir));
+
+        viewModel = new LinkedFilesEditorViewModel(StandardField.FILE, new EmptySuggestionProvider(), mock(DialogService.class), bibDatabaseContext,
+                           new CurrentThreadTaskExecutor(), mock(FieldCheckers.class), preferencesService);
+
+        BibEntry entry = new BibEntry().withCitationKey("test")
+            .withField(StandardField.URL, "https://ceur-ws.org/Vol-847/paper6.pdf");
+                viewModel.entry = entry;
         viewModel.fetchFulltext();
+
+        assertTrue(Files.exists(tempDir.resolve("test.pdf")));
     }
 }
