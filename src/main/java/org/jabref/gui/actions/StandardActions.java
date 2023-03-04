b62b0a0299f730c76c573a040088f9d715ff89b4
Benjamin Flesch <bfitservice@gmail.com>
2023-03-04 20:33:44 +0100

    implement "Attach feile from URL" in RightCLickMenu

--- src/main/java/org/jabref/gui/actions/StandardActions.java
+++ src/main/java/org/jabref/gui/actions/StandardActions.java
@@ -32,6 +32,7 @@ public enum StandardActions implements Action {
     SEARCH_SHORTSCIENCE(Localization.lang("Search ShortScience")),
     MERGE_WITH_FETCHED_ENTRY(Localization.lang("Get bibliographic data from %0", "DOI/ISBN/...")),
     ATTACH_FILE(Localization.lang("Attach file"), IconTheme.JabRefIcons.ATTACH_FILE),
+    ATTACH_FILE_FROM_URL(Localization.lang("Attach file from URL"), IconTheme.JabRefIcons.DOWNLOAD_FILE),
     PRIORITY(Localization.lang("Priority"), IconTheme.JabRefIcons.PRIORITY),
     CLEAR_PRIORITY(Localization.lang("Clear priority")),
     PRIORITY_HIGH(Localization.lang("Set priority to high"), IconTheme.JabRefIcons.PRIORITY_HIGH),
