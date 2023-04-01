25180f885590beb51a375a12e2d1bf8739cd7a51
NiD133 <k19052355@kcl.ac.uk>
2023-04-01 19:51:49 +0100

    Create user specific comment field

---
+++ src/main/java/org/jabref/model/entry/field/UserSpecificCommentField.java
@@ -0,0 +1,55 @@
+package org.jabref.model.entry.field;
+
+import java.util.EnumSet;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Set;
+
+public class UserSpecificCommentField implements Field {
+    private final String name;
+    private final Set<FieldProperty> properties;
+
+    public UserSpecificCommentField(String username) {
+        this.name = "comment" + username;
+        this.properties = EnumSet.of(FieldProperty.COMMENT);
+    }
+
+    @Override
+    public Set<FieldProperty> getProperties() {
+        return properties;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isStandardField() {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Field)) {
+            return false;
+        }
+        Field other = (Field) o;
+        return name.equalsIgnoreCase(other.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name.toLowerCase(Locale.ENGLISH));
+    }
+
+    @Override
+    public String toString() {
+        return "UnknownCommentField{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
