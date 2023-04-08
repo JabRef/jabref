37dd6b9220d1543cc351f5584b87de53dd3b1624
Oliver Kopp <kopp.dev@gmail.com>
2023-04-09 01:15:27 +0200

    Remove temporary code

--- src/test/java/org/jabref/http/server/mwessl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Temporary code.
- *
- * Code based on https://github.com/eclipse-ee4j/grizzly/tree/master/samples/http-server-samples/src/main/java/org/glassfish/grizzly/samples/httpserver/secure
- *
- * BSD-3-Clause license
- */
-package org.jabref.http.server.mwessl;
