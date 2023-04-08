5789411445a020e9fa992942d3b74d9b062f75b2
Zylence <kirsch.julian@yahoo.com>
2023-04-08 18:31:57 +0200

    Made AmpersandChecker able to handle an arbitrary number of backslashes

--- src/main/java/org/jabref/logic/integrity/AmpersandChecker.java
+++ src/main/java/org/jabref/logic/integrity/AmpersandChecker.java
@@ -1,5 +1,6 @@
 package org.jabref.logic.integrity;
 
+import com.google.common.base.CharMatcher;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.Field;
@@ -7,23 +8,30 @@ import org.jabref.model.entry.field.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 /**
  * Checks if the BibEntry contains unescaped ampersands.
  */
 public class AmpersandChecker implements EntryChecker{
-    private static final Pattern UNESCAPED_AMPERSAND = Pattern.compile("(?<!\\\\)&");
+    // matches for an & preceded by any number of \
+    private static final Pattern BACKSLASH_PRECEDED_AMPERSAND = Pattern.compile("\\\\*&");
 
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
         List<IntegrityMessage> results = new ArrayList<>();
 
         for (Map.Entry<Field, String> field : entry.getFieldMap().entrySet()) {
-            Matcher matcher = UNESCAPED_AMPERSAND.matcher(field.getValue());
-            if (matcher.find()) {
-                results.add(new IntegrityMessage(Localization.lang("Found unescaped '&'"), entry, field.getKey()));
+            // counts the number of even \ occurrences preceding an &
+            long unescapedAmpersands = BACKSLASH_PRECEDED_AMPERSAND.matcher(field.getValue())
+                    .results()
+                    .map(MatchResult::group)
+                    .filter(m -> CharMatcher.is('\\').countIn(m) % 2 == 0)
+                    .count();
+
+            if (unescapedAmpersands > 0) {
+                results.add(new IntegrityMessage(Localization.lang("Found " + unescapedAmpersands + " unescaped '&'"), entry, field.getKey()));
             }
         }
         return results;
