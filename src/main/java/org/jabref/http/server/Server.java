be1266495086b9057b2b155a10c9bf1b4b185a10
Oliver Kopp <kopp.dev@gmail.com>
2023-04-20 11:41:31 +0200

    Enables passing files to a test Server

--- src/main/java/org/jabref/http/server/Server.java
+++ src/main/java/org/jabref/http/server/Server.java
@@ -1,26 +1,81 @@
 package org.jabref.http.server;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.CountDownLatch;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.net.ssl.SSLContext;
 
+import javafx.collections.ObservableList;
+
 import org.jabref.logic.util.OS;
+import org.jabref.preferences.JabRefPreferences;
 
 import jakarta.ws.rs.SeBootstrap;
 import net.harawata.appdirs.AppDirsFactory;
 import org.glassfish.grizzly.ssl.SSLContextConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Server {
     private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     private static SeBootstrap.Instance serverInstance;
 
-    static void startServer(CountDownLatch latch) {
+    /**
+     * Starts an http server serving the last files opened in JabRef<br>
+     * More files can be provided as args.
+     */
+    public static void main(final String[] args) throws InterruptedException, URISyntaxException {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
+        final ObservableList<String> lastFilesOpened = JabRefPreferences.getInstance().getGuiPreferences().getLastFilesOpened();
+
+        // The server serves the last opened files (see org.jabref.http.server.LibraryResource.getLibraryPath)
+        // In a testing environment, this might be difficult to handle
+        // This is a quick solution. The architectural fine solution would use some http context or other @Inject_ed variables in org.jabref.http.server.LibraryResource
+        if (args.length > 0) {
+            LOGGER.debug("Command line parameters passed");
+            List<String> filesToAdd = Arrays.stream(args)
+                                            .map(Path::of)
+                                            .filter(Files::exists)
+                                            .map(Path::toString)
+                                            .filter(path -> !lastFilesOpened.contains(path))
+                                            .toList();
+
+            LOGGER.debug("Adding following files to the list of opened libraries: {}", filesToAdd);
+
+            // add the files in the front of the last opened libraries
+            Collections.reverse(filesToAdd);
+            for (String path : filesToAdd) {
+                lastFilesOpened.add(0, path);
+            }
+        }
+
+        if (lastFilesOpened.isEmpty()) {
+            LOGGER.debug("still no library available to serve, serve the demo library");
+            // Server.class.getResource("...") is always null here, thus trying relative path
+            // Path bibPath = Path.of(Server.class.getResource("http-server-demo.bib").toURI());
+            Path bibPath = Path.of("src/main/resources/org/jabref/http/server/http-server-demo.bib").toAbsolutePath();
+            LOGGER.debug("Location of demo library: {}", bibPath);
+            lastFilesOpened.add(bibPath.toString());
+        }
+
+        LOGGER.debug("Libraries served: {}", lastFilesOpened);
+
+        Server.startServer();
+
+        // Keep the http server running until user kills the process (e.g., presses Ctrl+C)
+        Thread.currentThread().join();
+    }
+
+    private static void startServer() {
         SSLContext sslContext = getSslContext();
         SeBootstrap.Configuration configuration = SeBootstrap.Configuration
                 .builder()
@@ -28,7 +83,9 @@ public class Server {
                 .protocol("HTTPS")
                 .port(6051)
                 .build();
+        LOGGER.debug("Starting server...");
         SeBootstrap.start(Application.class, configuration).thenAccept(instance -> {
+            LOGGER.debug("Server started.");
             instance.stopOnShutdown(stopResult ->
                     System.out.printf("Stop result: %s [Native stop result: %s].%n", stopResult,
                             stopResult.unwrap(Object.class)));
@@ -37,7 +94,6 @@ public class Server {
                     instance.unwrap(Object.class));
             System.out.println("Send SIGKILL to shutdown.");
             serverInstance = instance;
-            latch.countDown();
         });
     }
 
