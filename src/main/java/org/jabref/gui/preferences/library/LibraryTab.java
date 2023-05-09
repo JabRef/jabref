949bff2e3f0f9cf6f3a69f2f05880722492bdd64
Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
2023-05-09 23:57:18 +0200

    Moved default library to EntryTab

--- src/main/java/org/jabref/gui/preferences/library/LibraryTab.java
+++ src/main/java/org/jabref/gui/preferences/library/LibraryTab.java
@@ -2,19 +2,15 @@ package org.jabref.gui.preferences.library;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
 
 import org.jabref.gui.preferences.AbstractPreferenceTabView;
 import org.jabref.gui.preferences.PreferencesTab;
-import org.jabref.gui.util.ViewModelListCellFactory;
 import org.jabref.logic.l10n.Localization;
-import org.jabref.model.database.BibDatabaseMode;
 
 import com.airhacks.afterburner.views.ViewLoader;
 
 public class LibraryTab extends AbstractPreferenceTabView<LibraryTabViewModel> implements PreferencesTab {
 
-    @FXML private ComboBox<BibDatabaseMode> biblatexMode;
     @FXML private CheckBox collectTelemetry;
 
     public LibraryTab() {
@@ -30,12 +26,6 @@ public class LibraryTab extends AbstractPreferenceTabView<LibraryTabViewModel> i
 
     public void initialize() {
         this.viewModel = new LibraryTabViewModel(dialogService, preferencesService.getGeneralPreferences(), preferencesService.getTelemetryPreferences());
-
-        new ViewModelListCellFactory<BibDatabaseMode>()
-                .withText(BibDatabaseMode::getFormattedName)
-                .install(biblatexMode);
-        biblatexMode.itemsProperty().bind(viewModel.biblatexModeListProperty());
-        biblatexMode.valueProperty().bindBidirectional(viewModel.selectedBiblatexModeProperty());
 
         collectTelemetry.selectedProperty().bindBidirectional(viewModel.collectTelemetryProperty());
     }
--- src/main/java/org/jabref/gui/preferences/library/LibraryTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/library/LibraryTabViewModel.java
@@ -1,22 +1,14 @@
 package org.jabref.gui.preferences.library;
 
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.preferences.PreferenceTabViewModel;
-import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.preferences.GeneralPreferences;
 import org.jabref.preferences.TelemetryPreferences;
 
 public class LibraryTabViewModel implements PreferenceTabViewModel {
-    private final ListProperty<BibDatabaseMode> bibliographyModeListProperty = new SimpleListProperty<>();
-    private final ObjectProperty<BibDatabaseMode> selectedBiblatexModeProperty = new SimpleObjectProperty<>();
 
     private final BooleanProperty collectTelemetryProperty = new SimpleBooleanProperty();
 
@@ -32,23 +24,11 @@ public class LibraryTabViewModel implements PreferenceTabViewModel {
     }
 
     public void setValues() {
-        bibliographyModeListProperty.setValue(FXCollections.observableArrayList(BibDatabaseMode.values()));
-        selectedBiblatexModeProperty.setValue(generalPreferences.getDefaultBibDatabaseMode());
-
         collectTelemetryProperty.setValue(telemetryPreferences.shouldCollectTelemetry());
     }
 
     public void storeSettings() {
-        generalPreferences.setDefaultBibDatabaseMode(selectedBiblatexModeProperty.getValue());
         telemetryPreferences.setCollectTelemetry(collectTelemetryProperty.getValue());
-    }
-
-    public ListProperty<BibDatabaseMode> biblatexModeListProperty() {
-        return this.bibliographyModeListProperty;
-    }
-
-    public ObjectProperty<BibDatabaseMode> selectedBiblatexModeProperty() {
-        return this.selectedBiblatexModeProperty;
     }
 
     public BooleanProperty collectTelemetryProperty() {
