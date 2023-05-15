e9537a6baa0d108272d36465a79be12cd4e671e3
Oliver Kopp <kopp.dev@gmail.com>
2023-05-15 21:05:20 +0200

    Fix checkstyle

--- src/test/java/org/jabref/logic/importer/util/MetaDataParserTest.java
+++ src/test/java/org/jabref/logic/importer/util/MetaDataParserTest.java
@@ -3,7 +3,7 @@ package org.jabref.logic.importer.util;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MetaDataParserTest {
 
