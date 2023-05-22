5563b517dd3f72e6a549eed8e507d538fbc41121
desperadomzc <miaozechuan@yeah.net>
2023-05-22 18:45:34 +0930

    fix issue 429 make the export format matches import format in JabRef CLI -h

--- src/test/java/org/jabref/cli/JabRefCLITest.java
+++ src/test/java/org/jabref/cli/JabRefCLITest.java
@@ -3,6 +3,7 @@ package org.jabref.cli;
 import java.util.Collections;
 import java.util.List;
 
+import org.jabref.preferences.JabRefPreferences;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -142,5 +143,11 @@ class JabRefCLITest {
                 oocalc, ods, MSBib, mods, xmp, pdf, bib""";
 
         assertEquals(expected, "Available export formats: " + JabRefCLI.wrapStringList(given, 26));
+    }
+
+    @Test
+    void uniformImportExportFormat(){
+        JabRefPreferences preferences = JabRefPreferences.getInstance();
+        JabRefCLI.printUsage(preferences);
     }
 }
