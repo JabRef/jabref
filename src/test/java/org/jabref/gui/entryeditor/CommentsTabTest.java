f023fecf9b6be6aa41b9e7c94e48647f62dc7168
NiD133 <k19052355@kcl.ac.uk>
2023-05-24 16:51:50 +0200

    Refactor tests about the CommentsTab

--- src/test/java/org/jabref/gui/entryeditor/CommentsTabTest.java
+++ src/test/java/org/jabref/gui/entryeditor/CommentsTabTest.java
@@ -1,50 +1,80 @@
 package org.jabref.gui.entryeditor;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.autocompleter.SuggestionProviders;
+import org.jabref.gui.fieldeditors.FieldEditorFX;
 import org.jabref.gui.theme.ThemeManager;
+import org.jabref.gui.util.TaskExecutor;
 import org.jabref.logic.journals.JournalAbbreviationRepository;
+import org.jabref.logic.pdf.search.indexing.IndexingTaskManager;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.BibEntryType;
 import org.jabref.model.entry.BibEntryTypesManager;
 import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.model.entry.field.UnknownField;
+import org.jabref.model.entry.field.UserSpecificCommentField;
+import org.jabref.model.entry.types.EntryType;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.jabref.preferences.PreferencesService;
-import org.jabref.logic.preferences.OwnerPreferences;
+import org.jabref.testutils.category.GUITest;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testfx.framework.junit5.ApplicationExtension;
 
 import javax.swing.undo.UndoManager;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@GUITest
+@ExtendWith(ApplicationExtension.class)
 class CommentsTabTest {
 
     private CommentsTab commentsTab;
 
-    @Test
-    void testDetermineFieldsToShow() {
-        BibDatabaseContext databaseContext = new BibDatabaseContext();
-        SuggestionProviders suggestionProviders = Mockito.mock(SuggestionProviders.class);
-        UndoManager undoManager = Mockito.mock(UndoManager.class);
-        DialogService dialogService = Mockito.mock(DialogService.class);
-        PreferencesService preferences = Mockito.mock(PreferencesService.class);
-        OwnerPreferences ownerPreferences = Mockito.mock(OwnerPreferences.class);
-        Mockito.when(preferences.getOwnerPreferences()).thenReturn(ownerPreferences);
-        Mockito.when(ownerPreferences.getDefaultOwner()).thenReturn("defaultOwner");
-        StateManager stateManager = Mockito.mock(StateManager.class);
-        ThemeManager themeManager = Mockito.mock(ThemeManager.class);
-        BibEntryTypesManager entryTypesManager = Mockito.mock(BibEntryTypesManager.class);
-        JournalAbbreviationRepository journalAbbreviationRepository = Mockito.mock(JournalAbbreviationRepository.class);
+    @Mock
+    private BibEntryTypesManager entryTypesManager;
+    @Mock
+    private BibEntry entry;
+    @Mock
+    private FieldEditorFX fieldEditorFX;
+    @Mock
+    private BibDatabaseContext databaseContext;
+    @Mock
+    private SuggestionProviders suggestionProviders;
+    @Mock
+    private UndoManager undoManager;
+    @Mock
+    private DialogService dialogService;
+    @Mock
+    private PreferencesService preferences;
+    @Mock
+    private StateManager stateManager;
+    @Mock
+    private ThemeManager themeManager;
+    @Mock
+    private TaskExecutor taskExecutor;
+    @Mock
+    private JournalAbbreviationRepository journalAbbreviationRepository;
+    @Mock
+    private IndexingTaskManager indexingTaskManager;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
 
         commentsTab = new CommentsTab(
-                preferences.getOwnerPreferences().getDefaultOwner(),
+                "TestName",
                 databaseContext,
                 suggestionProviders,
                 undoManager,
@@ -52,20 +82,76 @@ class CommentsTabTest {
                 preferences,
                 stateManager,
                 themeManager,
-                null,
+                indexingTaskManager,
                 entryTypesManager,
-                null,
+                taskExecutor,
                 journalAbbreviationRepository
         );
+    }
 
+    @Test
+    void testDetermineFieldsToShowWithoutEntryType() {
+        // Setup
         BibEntry entry = new BibEntry();
-        entry.setField(StandardField.COMMENT, "This is a comment");
-        entry.setField(new UnknownField("customcomment1"), "Custom comment 1");
-        entry.setField(new UnknownField("customcomment2"), "Custom comment 2");
 
-        Set<Field> expectedFields = new HashSet<>(Arrays.asList(StandardField.COMMENT, new UnknownField("customcomment1"), new UnknownField("customcomment2")));
-        Set<Field> actualFields = commentsTab.determineFieldsToShow(entry);
+        // Add a standard comment and a user-specific comment to the entry
+        Field standardComment = StandardField.COMMENT;
+        UserSpecificCommentField userComment = new UserSpecificCommentField("TestName");
 
-        assertTrue(expectedFields.containsAll(actualFields) && actualFields.containsAll(expectedFields));
+        entry.setField(standardComment, "Standard comment text");
+        entry.setField(userComment, "User-specific comment text");
+
+        // Run the method under test
+        Set<Field> fields = commentsTab.determineFieldsToShow(entry);
+
+        // Verify that the result is an empty set
+        assertTrue(fields.isEmpty());
+    }
+
+    @Test
+    void testDetermineFieldsToShow() {
+        // Setup
+        BibEntry entry = new BibEntry();
+        EntryType entryType = StandardEntryType.Book;
+        entry.setType(entryType);
+        String username = "user1";
+
+        // Mock dependencies
+        when(databaseContext.getMode()).thenReturn(BibDatabaseMode.BIBLATEX);
+
+        // Mocking BibEntryType
+        BibEntryType entryTypeMock = mock(BibEntryType.class);
+        when(entryTypesManager.enrich(any(), any())).thenReturn(Optional.of(entryTypeMock));
+
+        // Add a standard comment and a user-specific comment to the entry
+        Field standardComment = StandardField.COMMENT;
+        UserSpecificCommentField userComment = new UserSpecificCommentField(username); // Use username
+
+        entry.setField(standardComment, "Standard comment text");
+        entry.setField(userComment, "User-specific comment text");
+
+        // Run the method under test
+        Set<Field> fields = commentsTab.determineFieldsToShow(entry);
+
+        // Verify that both comments are in the resulting set
+        assertTrue(fields.contains(standardComment));
+        assertTrue(fields.contains(userComment));
+
+        // Verify that defaultCommentField is in the resulting set
+        UserSpecificCommentField defaultCommentField = new UserSpecificCommentField(username);
+        assertTrue(fields.contains(defaultCommentField));
+    }
+
+    @Test
+    public void testDifferentiateCaseInUserName() {
+
+        UserSpecificCommentField field1 = new UserSpecificCommentField("USER");
+        UserSpecificCommentField field2 = new UserSpecificCommentField("user");
+
+        assertFalse(field1.equals(field2),
+                "Two UserSpecificCommentField instances with usernames that differ only by case should be considered different");
+
+        assertNotEquals(field1.hashCode(), field2.hashCode(),
+                "Hash codes of two UserSpecificCommentField instances with usernames that differ only by case should be different");
     }
 }
