4c6ba164b6cb86fb9e62bb1900c20dd1866fa6b4
wy8881 <wy7382@gmail.com>
2023-06-02 17:28:38 +0930

    Remove listener to constructor

--- src/main/java/org/jabref/gui/fieldeditors/GroupEditor.java
+++ src/main/java/org/jabref/gui/fieldeditors/GroupEditor.java
@@ -2,6 +2,7 @@ package org.jabref.gui.fieldeditors;
 
 import java.util.List;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.input.TransferMode;
 
 import org.jabref.gui.DragAndDropDataFormats;
@@ -12,9 +13,12 @@ import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.preferences.PreferencesService;
 
+import com.tobiasdiez.easybind.EasyBind;
+import com.tobiasdiez.easybind.optional.ObservableOptionalValue;
+
 public class GroupEditor extends SimpleEditor {
 
-    private final PreferencesService preferences;
+    private ObservableOptionalValue<BibEntry> bibEntry = EasyBind.wrapNullable(new SimpleObjectProperty<>());
 
     public GroupEditor(final Field field,
                        final SuggestionProvider<?> suggestionProvider,
@@ -22,12 +26,7 @@ public class GroupEditor extends SimpleEditor {
                        final PreferencesService preferences,
                        final boolean isMultiLine) {
         super(field, suggestionProvider, fieldCheckers, preferences, isMultiLine);
-        this.preferences = preferences;
-    }
 
-    @Override
-    public void bindToEntry(BibEntry entry) {
-        super.bindToEntry(entry);
         this.setOnDragOver(event -> {
             if (event.getDragboard().hasContent(DragAndDropDataFormats.GROUP)) {
                 event.acceptTransferModes(TransferMode.MOVE);
@@ -37,18 +36,26 @@ public class GroupEditor extends SimpleEditor {
 
         this.setOnDragDropped(event -> {
             boolean success = false;
-
             if (event.getDragboard().hasContent(DragAndDropDataFormats.GROUP)) {
                 List<String> group = (List<String>) event.getDragboard().getContent(DragAndDropDataFormats.GROUP);
-
-                String changedGroup = entry.getField(StandardField.GROUPS)
-                                           .map(setGroup -> setGroup + (preferences.getBibEntryPreferences().getKeywordSeparator()) + (group.get(0)))
-                                           .orElse(group.get(0));
-                entry.setField(StandardField.GROUPS, changedGroup);
-                success = true;
+                if (bibEntry.isValuePresent()) {
+                    String changedGroup = bibEntry.getValue()
+                                                  .map(entry -> entry.getField(StandardField.GROUPS)
+                                                                     .map(setGroup -> setGroup + (preferences.getBibEntryPreferences().getKeywordSeparator()) + (group.get(0)))
+                                                                     .orElse(group.get(0)))
+                                                  .orElse(null);
+                    bibEntry.getValue().map(entry -> entry.setField(StandardField.GROUPS, changedGroup));
+                    success = true;
+                }
             }
             event.setDropCompleted(success);
             event.consume();
         });
+    }
+
+    @Override
+    public void bindToEntry(BibEntry entry) {
+        bibEntry = EasyBind.wrapNullable(new SimpleObjectProperty<>(entry));
+        super.bindToEntry(entry);
     }
 }
