846dda5d868de5e68ec5d5f423803dfd394ff26d
Alexandra Stathopoulou <alexandra.kast2001@gmail.com>
2023-06-06 20:30:44 +0300

    Update tests structure and add more cases

--- src/test/java/org/jabref/logic/cleanup/URLCleanupTest.java
+++ src/test/java/org/jabref/logic/cleanup/URLCleanupTest.java
@@ -23,68 +23,96 @@ public class URLCleanupTest {
     }
 
     private static Stream<Arguments> provideURL() {
-        /*
-         * Expected entries with various types of URLs(e.g, not secure protocol, password included for
-         * authentication, IP address, port etc.)
-         */
-        BibEntry[] expectedwithURL = {
-                new BibEntry().withField(StandardField.URL, "https://hdl.handle.net/10442/hedi/6089"),
-                new BibEntry().withField(StandardField.URL, "http://hdl.handle.net/10442/hedi/6089"),
-                new BibEntry().withField(StandardField.URL, "http://userid:password@example.com:8080"),
-                new BibEntry().withField(StandardField.URL, "http://142.42.1.1:8080"),
-                new BibEntry().withField(StandardField.URL, "http://☺.damowmow.com"),
-                new BibEntry().withField(StandardField.URL, "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com"),
-                new BibEntry().withField(StandardField.URL, "https://www.example.com/foo/?bar=baz&inga=42&quux"),
-        };
-
-        // Expected entry in case note field holds more than two values
-        BibEntry expectedWithTwoNoteValues = new BibEntry()
-                .withField(StandardField.URL, "https://hdl.handle.net/10442/hedi/6089")
-                .withField(StandardField.NOTE, "this is a note");
-
-        // Expected entry in case note field holds more than one URLs
-        BibEntry expectedWithMultipleURLs = new BibEntry()
-                .withField(StandardField.URL, "https://hdl.handle.net/10442/hedi/6089")
-                .withField(StandardField.NOTE, "\\url{http://142.42.1.1:8080}");
-
         return Stream.of(
 
-                // Input Note field has two arguments stored , with the latter being a url
-                Arguments.of(expectedWithTwoNoteValues, new BibEntry().withField(
-                        StandardField.NOTE, "this is a note, \\url{https://hdl.handle.net/10442/hedi/6089}")),
+            // Input Note field has two arguments stored , with the latter being a url.
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://hdl.handle.net/10442/hedi/6089")
+                              .withField(StandardField.NOTE,
+                                    "this is a note"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "this is a note, \\url{https://hdl.handle.net/10442/hedi/6089}")),
 
-                // Input Note field has two arguments stored, with the former being a url
-                Arguments.of(expectedWithTwoNoteValues, new BibEntry().withField(
-                        StandardField.NOTE, "\\url{https://hdl.handle.net/10442/hedi/6089}, this is a note")),
+            // Input Note field has two arguments stored, with the former being a url.
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://hdl.handle.net/10442/hedi/6089")
+                              .withField(StandardField.NOTE,
+                                    "this is a note"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{https://hdl.handle.net/10442/hedi/6089}, this is a note")),
 
-                // Input Note field has more than one URLs stored
-                Arguments.of(expectedWithMultipleURLs, new BibEntry().withField(
-                        StandardField.NOTE, "\\url{https://hdl.handle.net/10442/hedi/6089}, \\url{http://142.42.1.1:8080}")),
+            // Input Note field has more than one URLs stored.
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://hdl.handle.net/10442/hedi/6089")
+                              .withField(StandardField.NOTE,
+                                    "\\url{http://142.42.1.1:8080}"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{https://hdl.handle.net/10442/hedi/6089}, "
+                                        + "\\url{http://142.42.1.1:8080}")),
 
-                // Several input URL types to be correctly identified
-                Arguments.of(expectedwithURL[0], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{https://hdl.handle.net/10442/hedi/6089}")),
+            // Input Note field has several values stored.
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://example.org")
+                              .withField(StandardField.NOTE,
+                                    "cited by Kramer, 2002."),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{https://example.org}, cited by Kramer, 2002.")),
 
-                Arguments.of(expectedwithURL[1], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{http://hdl.handle.net/10442/hedi/6089}")),
+            /*
+             * Several input URL types (e.g, not secure protocol, password included for
+             * authentication, IP address, port etc.) to be correctly identified.
+             */
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://hdl.handle.net/10442/hedi/6089"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{https://hdl.handle.net/10442/hedi/6089}")),
 
-                Arguments.of(expectedwithURL[2], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{http://userid:password@example.com:8080}")),
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "http://hdl.handle.net/10442/hedi/6089"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{http://hdl.handle.net/10442/hedi/6089}")),
 
-                Arguments.of(expectedwithURL[3], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{http://142.42.1.1:8080}")),
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "http://userid:password@example.com:8080"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{http://userid:password@example.com:8080}")),
 
-                Arguments.of(expectedwithURL[4], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{http://☺.damowmow.com}")),
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "http://142.42.1.1:8080"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{http://142.42.1.1:8080}")),
 
-                Arguments.of(expectedwithURL[5], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com}")),
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "http://☺.damowmow.com"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{http://☺.damowmow.com}")),
 
-                Arguments.of(expectedwithURL[6], new BibEntry().withField(
-                        StandardField.NOTE, "\\url{https://www.example.com/foo/?bar=baz&inga=42&quux}")),
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com}")),
 
-                Arguments.of(expectedwithURL[6], new BibEntry().withField(
-                        StandardField.NOTE, "https://www.example.com/foo/?bar=baz&inga=42&quux"))
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://www.example.com/foo/?bar=baz&inga=42&quux"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "\\url{https://www.example.com/foo/?bar=baz&inga=42&quux}")),
+
+            Arguments.of(
+                new BibEntry().withField(StandardField.URL,
+                                    "https://www.example.com/foo/?bar=baz&inga=42&quux"),
+                new BibEntry().withField(StandardField.NOTE,
+                                    "https://www.example.com/foo/?bar=baz&inga=42&quux"))
         );
     }
 }
