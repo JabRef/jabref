554261956d14e1c210bcf0ab0cff482154cd862d
desperadomzc <miaozechuan@yeah.net>
2023-06-09 13:38:18 +0930

    undo changes in p tags

--- src/main/java/org/jabref/gui/autocompleter/AutoCompletionTextInputBinding.java
+++ src/main/java/org/jabref/gui/autocompleter/AutoCompletionTextInputBinding.java
@@ -1,7 +1,6 @@
 /**
  * Copyright (c) 2014, 2015, ControlsFX
  * All rights reserved.
- * <p>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
@@ -12,7 +11,6 @@
  * * Neither the name of ControlsFX, any associated website, nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
- * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
