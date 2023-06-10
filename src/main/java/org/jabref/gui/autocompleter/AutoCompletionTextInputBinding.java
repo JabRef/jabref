874d281b881fe46845b531f4116327fd431d1ced
desperadomzc <miaozechuan@yeah.net>
2023-06-10 17:32:14 +0930

    revert autocompleter/AutoCompletionTextInputBinding.java

--- src/main/java/org/jabref/gui/autocompleter/AutoCompletionTextInputBinding.java
+++ src/main/java/org/jabref/gui/autocompleter/AutoCompletionTextInputBinding.java
@@ -1,6 +1,7 @@
 /**
  * Copyright (c) 2014, 2015, ControlsFX
  * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
@@ -11,6 +12,7 @@
  * * Neither the name of ControlsFX, any associated website, nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
