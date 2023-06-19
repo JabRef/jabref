047ad88053c9b2ae5c22d7ed8767488f21215b3a
Nitin Suresh <aqurilla@gmail.com>
2023-06-18 22:12:40 -0700

    add placeholder charts

--- src/main/java/org/jabref/gui/fieldeditors/journalinfo/JournalInfoView.java
+++ src/main/java/org/jabref/gui/fieldeditors/journalinfo/JournalInfoView.java
@@ -2,12 +2,18 @@ package org.jabref.gui.fieldeditors.journalinfo;
 
 import java.util.Objects;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import com.airhacks.afterburner.views.ViewLoader;
 
 public class JournalInfoView extends VBox {
+    @FXML private Label journalLabel;
+    @FXML private LineChart<String, Double> sjrChart;
+    @FXML private LineChart<String, Double> snipChart;
     private final JournalInfoViewModel viewModel;
 
     public JournalInfoView() {
@@ -18,9 +24,17 @@ public class JournalInfoView extends VBox {
                   .load();
 
         this.getStylesheets().add(Objects.requireNonNull(JournalInfoView.class.getResource("JournalInfo.css")).toExternalForm());
+
+        journalLabel.textProperty().bind(viewModel.headingProperty());
+        bindChartProperties();
     }
 
     public Parent getNode() {
         return this;
+    }
+
+    private void bindChartProperties() {
+        sjrChart.setData(viewModel.getSjrData());
+        snipChart.setData(viewModel.getSnipData());
     }
 }
--- src/main/java/org/jabref/gui/fieldeditors/journalinfo/JournalInfoViewModel.java
+++ src/main/java/org/jabref/gui/fieldeditors/journalinfo/JournalInfoViewModel.java
@@ -1,15 +1,25 @@
 package org.jabref.gui.fieldeditors.journalinfo;
 
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.XYChart;
 
 import org.jabref.gui.AbstractViewModel;
-import org.jabref.logic.l10n.Localization;
 
 public class JournalInfoViewModel extends AbstractViewModel {
     private final ReadOnlyStringWrapper heading = new ReadOnlyStringWrapper();
+    private final ObservableList<XYChart.Series<String, Double>> sjrData = FXCollections.observableArrayList();
+    private final ObservableList<XYChart.Series<String, Double>> snipData = FXCollections.observableArrayList();
 
     public JournalInfoViewModel() {
-        heading.set(Localization.lang("Journal Information"));
+        init();
+    }
+
+    private void init() {
+        setHeading("Oncotarget");
+        setSjrData();
+        setSnipData();
     }
 
     public String getHeading() {
@@ -18,5 +28,41 @@ public class JournalInfoViewModel extends AbstractViewModel {
 
     public ReadOnlyStringWrapper headingProperty() {
         return heading;
+    }
+
+    private void setHeading(String heading) {
+        this.heading.set(heading);
+    }
+
+    public ObservableList<XYChart.Series<String, Double>> getSjrData() {
+        return sjrData;
+    }
+
+    public ObservableList<XYChart.Series<String, Double>> getSnipData() {
+        return snipData;
+    }
+
+    public void setSjrData() {
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("2013", 0.85));
+        series.getData().add(new XYChart.Data<>("2014", 0.75));
+        series.getData().add(new XYChart.Data<>("2015", 0.695));
+        series.getData().add(new XYChart.Data<>("2016", 0.25));
+        series.getData().add(new XYChart.Data<>("2017", 0.15));
+        series.getData().add(new XYChart.Data<>("2018", 0.95));
+
+        sjrData.add(series);
+    }
+
+    public void setSnipData() {
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("2013", 0.85));
+        series.getData().add(new XYChart.Data<>("2014", 0.75));
+        series.getData().add(new XYChart.Data<>("2015", 0.695));
+        series.getData().add(new XYChart.Data<>("2016", 0.25));
+        series.getData().add(new XYChart.Data<>("2017", 0.15));
+        series.getData().add(new XYChart.Data<>("2018", 0.95));
+
+        snipData.add(series);
     }
 }
