98a2cd8dc21049c844b85334c755eb433f37b555
Nitin Suresh <aqurilla@gmail.com>
2023-07-03 07:56:35 -0700

    add check for id in websearch query

--- src/test/java/org/jabref/gui/importer/fetcher/WebSearchPaneViewModelTest.java
+++ src/test/java/org/jabref/gui/importer/fetcher/WebSearchPaneViewModelTest.java
@@ -75,4 +75,28 @@ class WebSearchPaneViewModelTest {
         viewModel.queryProperty().setValue("101.1007/JHEP02(2023)082");
         assertFalse(viewModel.queryValidationStatus().validProperty().getValue());
     }
+
+    @Test
+    void queryConsistingOfISBNIsValid() {
+        viewModel.queryProperty().setValue("9780134685991");
+        assertTrue(viewModel.queryValidationStatus().validProperty().getValue());
+    }
+
+    @Test
+    void canExtractISBNFromQueryText() {
+        viewModel.queryProperty().setValue(";:isbn (9780134685991), text2");
+        assertTrue(viewModel.queryValidationStatus().validProperty().getValue());
+    }
+
+    @Test
+    void queryConsistingOfArXivIdIsValid() {
+        viewModel.queryProperty().setValue("arXiv:2110.02957");
+        assertTrue(viewModel.queryValidationStatus().validProperty().getValue());
+    }
+
+    @Test
+    void canExtractArXivIdFromQueryText() {
+        viewModel.queryProperty().setValue("this query contains an ArXiv identifier 2110.02957");
+        assertTrue(viewModel.queryValidationStatus().validProperty().getValue());
+    }
 }
