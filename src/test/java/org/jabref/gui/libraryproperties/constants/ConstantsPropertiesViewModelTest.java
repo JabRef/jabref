6450bd9a0d3bd077731bdc5a3348d0d8425e3a66
Stephan Lukasczyk <stephan@lukasczyk.me>
2023-07-14 17:26:31 +0200

    Apply suggestions from code review

--- src/test/java/org/jabref/gui/libraryproperties/constants/ConstantsPropertiesViewModelTest.java
+++ src/test/java/org/jabref/gui/libraryproperties/constants/ConstantsPropertiesViewModelTest.java
@@ -1,9 +1,8 @@
 package org.jabref.gui.libraryproperties.constants;
 
-import java.util.Collection;
 import java.util.List;
 
-import javafx.beans.property.ListProperty;
+import javafx.beans.property.StringProperty;
 
 import org.jabref.gui.DialogService;
 import org.jabref.model.database.BibDatabase;
@@ -13,31 +12,30 @@ import org.jabref.model.entry.BibtexString;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class ConstantsPropertiesViewModelTest {
 
     @DisplayName("Check that the list of strings is sorted according to their keys")
     @Test
     void testStringsListPropertySorting() {
-        DialogService service = mock(DialogService.class);
-        BibDatabaseContext context = mock(BibDatabaseContext.class);
-        BibDatabase db = mock(BibDatabase.class);
         BibtexString string1 = new BibtexString("TSE", "Transactions on Software Engineering");
         BibtexString string2 = new BibtexString("ICSE", "International Conference on Software Engineering");
-        Collection<BibtexString> stringValues = List.of(string1, string2);
-        when(db.getStringValues()).thenReturn(stringValues);
-        when(context.getDatabase()).thenReturn(db);
+        BibDatabase db = new BibDatabase();
+        db.setStrings(List.of(string1, string2));
+        BibDatabaseContext context = new BibDatabaseContext(db);
+        DialogService service = mock(DialogService.class);
+        List<String> expected = List.of(string2.getName(), string1.getName()); // ICSE before TSE
 
         ConstantsPropertiesViewModel model = new ConstantsPropertiesViewModel(context, service);
         model.setValues();
 
-        ListProperty<ConstantsItemModel> strings = model.stringsListProperty();
-        assertAll(
-                () -> assertEquals("ICSE", strings.get(0).labelProperty().getValue()),
-                () -> assertEquals("TSE", strings.get(1).labelProperty().getValue()));
+        List<String> actual = model.stringsListProperty().stream()
+                .map(ConstantsItemModel::labelProperty)
+                .map(StringProperty::getValue)
+                .toList();
+
+        assertEquals(expected, actual);
     }
 }
