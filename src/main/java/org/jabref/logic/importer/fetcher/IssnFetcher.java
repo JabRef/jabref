557f25b054d95bad3acd6b19a1ff33cc246b9151
guipmenezes <guimenezesunb@gmail.com>
2023-08-15 11:02:54 -0300

    Started to implement the ISSN search logic

--- src/main/java/org/jabref/logic/importer/fetcher/IssnFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/IssnFetcher.java
@@ -7,9 +7,9 @@ import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.IdBasedFetcher;
 import org.jabref.logic.importer.IdFetcher;
 import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.integrity.ISSNChecker;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.identifier.ISSN;
-import org.jabref.model.strings.StringUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,26 +23,36 @@ public class IssnFetcher implements IdBasedFetcher, IdFetcher<ISSN> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IssnFetcher.class);
     private final DOAJFetcher doajFetcher;
+    private final String SEARCH_URL = "https://doaj.org/api/search/journals/";
+    private final ISSNChecker issnChecker;
     private final ImportFormatPreferences importFormatPreferences;
 
     public IssnFetcher(ImportFormatPreferences importFormatPreferences) {
-        this.doajFetcher = new DOAJFetcher(importFormatPreferences);
         this.importFormatPreferences = importFormatPreferences;
+        this.doajFetcher = new DOAJFetcher(importFormatPreferences);
+        this.issnChecker = new ISSNChecker();
     }
 
     @Override
     public Optional<BibEntry> performSearchById(String identifier) throws FetcherException {
 
-        if (StringUtil.isBlank(identifier)) {
+        Optional<String> checkedId = issnChecker.checkValue(identifier);
+        if (checkedId.isEmpty()) {
+            LOGGER.warn("Not a valid ISSN");
             return Optional.empty();
         }
 
         Optional<BibEntry> bibEntry = Optional.empty();
 
-        // Aparently, to do a queryString in DOAJ, you only need to add issn:xxxx-xxxx after the base url
-        List<BibEntry> bibEntries = doajFetcher.performSearch(identifier);
-        for (BibEntry entry: bibEntries) {
-            bibEntry = Optional.ofNullable(bibEntries.get(0));
+        String queryString = concatenateIssnWithId(identifier);
+        List<BibEntry> bibEntries = doajFetcher.performSearch(queryString);
+
+        if (!bibEntries.isEmpty()) {
+            for (int i = 0; i < bibEntries.size(); i++) {
+                bibEntry = Optional.ofNullable(bibEntries.get(0));
+            }
+        } else {
+            LOGGER.warn("ISSN search failed, no results found");
         }
 
         return bibEntry;
@@ -50,6 +60,7 @@ public class IssnFetcher implements IdBasedFetcher, IdFetcher<ISSN> {
 
     @Override
     public Optional<ISSN> findIdentifier(BibEntry entry) throws FetcherException {
+        // Need to create a getIssn() method in BibEntry that returns a Optional
         return Optional.empty();
     }
 
@@ -61,5 +72,9 @@ public class IssnFetcher implements IdBasedFetcher, IdFetcher<ISSN> {
     @Override
     public String getName() {
         return "ISSN";
+    }
+
+    public String concatenateIssnWithId(String identifier) {
+        return "issn:" + identifier;
     }
 }
