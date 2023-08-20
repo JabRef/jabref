ab36944b83eb641652362e09b313011ec533b707
Lucas Freund <127773292+Luggas4you@users.noreply.github.com>
2023-08-20 15:31:22 +0200

    Add TestLibraryA & testUpperAndLowerWordSearch

--- src/test/java/org/jabref/logic/search/SearchFunctionalityTest.java
+++ src/test/java/org/jabref/logic/search/SearchFunctionalityTest.java
@@ -14,12 +14,11 @@ import org.jabref.model.database.BibDatabase;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
-import org.jabref.model.groups.AllEntriesGroup;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.jabref.model.search.rules.SearchRules;
 import org.jabref.model.util.DummyFileUpdateMonitor;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 
@@ -29,10 +28,35 @@ import static org.mockito.Mockito.mock;
 public class SearchFunctionalityTest {
 
     private BibDatabase database;
+    List<BibEntry> testLibraryA;
+    List<BibEntry> testLibraryB;
+    List<BibEntry> testLibraryC;
+    List<BibEntry> testLibraryD;
 
     @BeforeEach
     public void setUp() {
         database = new BibDatabase();
+        testLibraryA = List.of(
+                new BibEntry(StandardEntryType.Misc)
+                        .withCitationKey("entry1")
+                        .withField(StandardField.AUTHOR, "Test")
+                        .withField(StandardField.TITLE, "cASe"),
+                new BibEntry(StandardEntryType.Misc)
+                        .withCitationKey("entry2")
+                        .withField(StandardField.AUTHOR, "test")
+                        .withField(StandardField.TITLE, "casE"),
+                new BibEntry(StandardEntryType.Misc)
+                        .withCitationKey("entry3")
+                        .withField(StandardField.AUTHOR, "tESt")
+                        .withField(StandardField.TITLE, "Case"),
+                new BibEntry(StandardEntryType.Misc)
+                        .withCitationKey("entry4")
+                        .withField(StandardField.AUTHOR, "tesT")
+                        .withField(StandardField.TITLE, "CASE"),
+                new BibEntry(StandardEntryType.Misc)
+                        .withCitationKey("entry5")
+                        .withField(StandardField.AUTHOR, "TEST")
+                        .withField(StandardField.TITLE, "case"));
     }
 
     private void initializeDatabaseFromPath(Path testFile) throws IOException {
@@ -47,6 +71,19 @@ public class SearchFunctionalityTest {
         initializeDatabaseFromPath(Path.of(SearchFunctionalityTest.class.getResource("empty.bib").toURI()));
 
         List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("Test", EnumSet.noneOf(SearchRules.SearchFlags.class)), database).getMatches();
+        assertEquals(Collections.emptyList(), matches);
+    }
+
+    @Test
+    public void testUpperAndLowerWordSearch() throws IOException, URISyntaxException {
+        initializeDatabaseFromPath(Path.of(SearchFunctionalityTest.class.getResource("test-library-A.bib").toURI()));
+
+        //Positive search test
+        List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("Test", EnumSet.noneOf(SearchRules.SearchFlags.class)), database).getMatches();
+        assertEquals(testLibraryA, matches);
+
+        //Negative search test
+        matches = new DatabaseSearcher(new SearchQuery("Best", EnumSet.noneOf(SearchRules.SearchFlags.class)), database).getMatches();
         assertEquals(Collections.emptyList(), matches);
     }
 }
