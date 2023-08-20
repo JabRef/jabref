d789dd35c5cdc3cd898c56dc69589c02dad46353
Lucas Freund <127773292+Luggas4you@users.noreply.github.com>
2023-08-20 14:27:43 +0200

    Add testEmptyLibrarySearch

---
+++ src/test/java/org/jabref/logic/search/SearchFunctionalityTest.java
@@ -0,0 +1,51 @@
+package org.jabref.logic.search;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.logic.importer.ParserResult;
+import org.jabref.logic.importer.fileformat.BibtexImporter;
+import org.jabref.model.database.BibDatabase;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
+import org.jabref.model.groups.AllEntriesGroup;
+import org.jabref.model.search.rules.SearchRules;
+import org.jabref.model.util.DummyFileUpdateMonitor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class SearchFunctionalityTest {
+
+    private BibDatabase database;
+
+    @BeforeEach
+    public void setUp() {
+        database = new BibDatabase();
+    }
+
+    @Test
+    public void testEmptyLibrarySearch() throws IOException, URISyntaxException {
+
+        Path testFile = Path.of(SearchFunctionalityTest.class.getResource("empty.bib").toURI());
+        ParserResult result = new BibtexImporter(mock(ImportFormatPreferences.class, Answers.RETURNS_DEEP_STUBS), new DummyFileUpdateMonitor()).importDatabase(testFile);
+        BibDatabaseContext context = new BibDatabaseContext(result.getDatabase(), result.getMetaData());
+
+        database = context.getDatabase();
+
+        List<BibEntry> matches = new DatabaseSearcher(new SearchQuery("Test", EnumSet.noneOf(SearchRules.SearchFlags.class)), database).getMatches();
+        assertEquals(Collections.emptyList(), matches);
+    }
+}
+
+
