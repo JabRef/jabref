e448d65616a191d0c28d3405a8b49503cfa0f8be
Lucas Freund <127773292+Luggas4you@users.noreply.github.com>
2023-08-25 16:40:58 +0200

    SimplePDFFulltextSearch WIP

--- src/test/java/org/jabref/logic/search/SearchFunctionalityTest.java
+++ src/test/java/org/jabref/logic/search/SearchFunctionalityTest.java
@@ -10,24 +10,36 @@ import java.util.List;
 import org.jabref.logic.importer.ImportFormatPreferences;
 import org.jabref.logic.importer.ParserResult;
 import org.jabref.logic.importer.fileformat.BibtexImporter;
+import org.jabref.logic.pdf.search.indexing.PdfIndexer;
+import org.jabref.logic.pdf.search.retrieval.PdfSearcher;
+import org.jabref.logic.util.StandardFileType;
 import org.jabref.model.database.BibDatabase;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.LinkedFile;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.types.StandardEntryType;
+import org.jabref.model.pdf.search.PdfSearchResults;
 import org.jabref.model.search.rules.SearchRules;
 import org.jabref.model.util.DummyFileUpdateMonitor;
+import org.jabref.preferences.FilePreferences;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Answers;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SearchFunctionalityTest {
 
     private BibDatabase database;
+    private PdfSearcher search;
+    private BibDatabaseContext context;
+    private FilePreferences filePreferences;
     BibEntry entry1A = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("entry1")
             .withField(StandardField.AUTHOR, "Test")
@@ -64,16 +76,51 @@ public class SearchFunctionalityTest {
             .withCitationKey("entry4")
             .withField(StandardField.AUTHOR, "Special")
             .withField(StandardField.TITLE, "192? title.");
+    BibEntry entry1C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal1");
+    BibEntry entry2C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal2");
+
+    BibEntry entry3C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal3");
+    BibEntry entry4C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal-note1");
+    BibEntry entry5C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal-note2");
+    BibEntry entry6C = new BibEntry(StandardEntryType.Misc)
+            .withCitationKey("minimal-note3");
+
     @BeforeEach
-    public void setUp() {
+    public void setUp(@TempDir Path indexDir) throws IOException {
+        filePreferences = mock(FilePreferences.class);
         database = new BibDatabase();
+        context = mock(BibDatabaseContext.class);
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimal", "minimal.pdf", StandardFileType.PDF.getName())));
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimal 1", "minimal1.pdf", StandardFileType.PDF.getName())));
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimal 2", "minimal2.pdf", StandardFileType.PDF.getName())));
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimalnote", "minimal-note.pdf", StandardFileType.PDF.getName())));
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimalnote 1", "minimal-note1.pdf", StandardFileType.PDF.getName())));
+        entry1C.setFiles(Collections.singletonList(new LinkedFile("Minimalnote 2", "minimal-note2.pdf", StandardFileType.PDF.getName())));
+        when(context.getFileDirectories(Mockito.any())).thenReturn(Collections.singletonList(Path.of("src/test/resources/org/jabref/logic/search")));
+        when(context.getFulltextIndexPath()).thenReturn(indexDir);
+        when(context.getDatabase()).thenReturn(database);
+        when(context.getEntries()).thenReturn(database.getEntries());
+
+        database.insertEntry(entry1C);
+
+        PdfIndexer indexer = PdfIndexer.of(context, filePreferences);
+        search = PdfSearcher.of(context);
+
+        indexer.createIndex();
+        indexer.addToIndex(context);
     }
 
     private void initializeDatabaseFromPath(Path testFile) throws IOException {
         ParserResult result = new BibtexImporter(mock(ImportFormatPreferences.class, Answers.RETURNS_DEEP_STUBS), new DummyFileUpdateMonitor()).importDatabase(testFile);
-        BibDatabaseContext context = new BibDatabaseContext(result.getDatabase(), result.getMetaData());
+        context = new BibDatabaseContext(result.getDatabase(), result.getMetaData());
 
         database = context.getDatabase();
+        search = PdfSearcher.of(context);
     }
 
     @Test
@@ -166,6 +213,14 @@ public class SearchFunctionalityTest {
         //Negative search test
         matches = new DatabaseSearcher(new SearchQuery("\\bCas\\b", EnumSet.of(SearchRules.SearchFlags.REGULAR_EXPRESSION, SearchRules.SearchFlags.CASE_SENSITIVE)), database).getMatches();
         assertEquals(Collections.emptyList(), matches);
+    }
+
+    @Test
+    public void testSimplePDFFulltextSearch() throws IOException, URISyntaxException {
+        initializeDatabaseFromPath(Path.of(SearchFunctionalityTest.class.getResource("test-library-C.bib").toURI()));
+
+        PdfSearchResults result = search.search("This is a short sentence, comma included.", 10);
+        assertEquals(6, result.numSearchResults());
     }
 }
 
