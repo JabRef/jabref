fc4ea9d3c5f2eb2f53fa9f3e35dbcaf42dd400d7
Oliver Kopp <kopp.dev@gmail.com>
2023-09-07 23:24:51 +0200

    Remove oboslete class StaxParser

--- src/main/java/org/jabref/logic/importer/util/MathMLParser.java
+++ src/main/java/org/jabref/logic/importer/util/MathMLParser.java
@@ -1,12 +1,13 @@
 package org.jabref.logic.importer.util;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Objects;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.Result;
@@ -14,6 +15,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stax.StAXSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -32,24 +34,22 @@ public class MathMLParser {
      * @return Returns the LaTeX representation
      */
     public static String parse(XMLStreamReader reader) {
-        String xmlContent = "";
         String latexResult = "<Unsupported MathML expression>";
 
         try {
-            // extract XML content
-            xmlContent = StaxParser.getXMLContent(reader);
-
-            // convert to LaTeX using XSLT file
-            Source xmlSource = new StreamSource(new StringReader(xmlContent));
-
             URL xsltResource = MathMLParser.class.getResource(XSLT_FILE_PATH);
             Source xsltSource = new StreamSource(Objects.requireNonNull(xsltResource).openStream(), xsltResource.toURI().toASCIIString());
-
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer(xsltSource);
 
+            // Code inspired by https://stackoverflow.com/a/11269092/873282
+            XMLInputFactory xmlif = XMLInputFactory.newInstance();
+            XMLEventReader xmlEventReader = xmlif.createXMLEventReader(reader);
+            StAXSource xmlSource = new StAXSource(xmlEventReader);
+
             StringWriter writer = new StringWriter();
             Result result = new StreamResult(writer);
+
             transformer.transform(xmlSource, result);
 
             latexResult = writer.getBuffer().toString();
