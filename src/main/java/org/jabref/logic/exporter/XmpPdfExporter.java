6f49c3034ba80629281f2f399b50bc7b7159fbc0
Lucas Freund <127773292+Luggas4you@users.noreply.github.com>
2023-09-09 17:10:02 +0200

    Create PDF WIP

--- src/main/java/org/jabref/logic/exporter/XmpPdfExporter.java
+++ src/main/java/org/jabref/logic/exporter/XmpPdfExporter.java
@@ -1,15 +1,18 @@
 package org.jabref.logic.exporter;
 
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Objects;
+        import java.io.IOException;
+        import java.nio.file.Path;
+        import java.util.List;
+        import java.util.Objects;
 
-import org.jabref.logic.l10n.Localization;
-import org.jabref.logic.util.StandardFileType;
-import org.jabref.logic.xmp.XmpPreferences;
-import org.jabref.logic.xmp.XmpUtilWriter;
-import org.jabref.model.database.BibDatabaseContext;
-import org.jabref.model.entry.BibEntry;
+        import org.jabref.logic.l10n.Localization;
+        import org.jabref.logic.util.StandardFileType;
+        import org.jabref.logic.xmp.XmpPreferences;
+        import org.jabref.logic.xmp.XmpUtilWriter;
+        import org.jabref.model.database.BibDatabaseContext;
+        import org.jabref.model.entry.BibEntry;
+
+        import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class XmpPdfExporter extends Exporter {
 
@@ -26,8 +29,22 @@ public class XmpPdfExporter extends Exporter {
         Objects.requireNonNull(pdfFile);
         Objects.requireNonNull(entries);
 
-        if (pdfFile.toString().endsWith(".pdf")) {
+        if (!pdfFile.toString().endsWith(".pdf")) {
+            throw new IllegalArgumentException("Invalid PDF file extension");
+        }
+
+        try (PDDocument document = new PDDocument()) {
+
+            document.save("C:/Users/wicht/Desktop/metadata.pdf");
+            document.close();
+
+            // Embed metadata using XmpUtilWriter
             new XmpUtilWriter(xmpPreferences).writeXmp(pdfFile, entries, databaseContext.getDatabase());
+
+            // Save the PDF to the specified file
+            document.save(pdfFile.toFile());
+        } catch (IOException e) {
+            throw new Exception("Error creating PDF", e);
         }
     }
 }
