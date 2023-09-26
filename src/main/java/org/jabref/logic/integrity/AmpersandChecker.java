dc82d445c8bbc1ac5126947cb895e062bf6bf6b5
Oliver Kopp <kopp.dev@gmail.com>
2023-09-26 16:44:13 +0200

    Switch from classic for to streaming

--- src/main/java/org/jabref/logic/integrity/AmpersandChecker.java
+++ src/main/java/org/jabref/logic/integrity/AmpersandChecker.java
@@ -1,17 +1,16 @@
 package org.jabref.logic.integrity;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.FieldProperty;
 
 import com.google.common.base.CharMatcher;
+import org.jooq.lambda.tuple.Tuple2;
 
 /**
  * Checks if the BibEntry contains unescaped ampersands.
@@ -23,24 +22,22 @@ public class AmpersandChecker implements EntryChecker {
 
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
-        List<IntegrityMessage> results = new ArrayList<>();
-
-        for (Map.Entry<Field, String> field : entry.getFieldMap().entrySet()) {
-            if (field.getKey().getProperties().contains(FieldProperty.VERBATIM)) {
-                continue;
-            }
-            // counts the number of even \ occurrences preceding an &
-            long unescapedAmpersands = BACKSLASH_PRECEDED_AMPERSAND.matcher(field.getValue())
-                    .results()
-                    .map(MatchResult::group)
-                    .filter(m -> CharMatcher.is('\\').countIn(m) % 2 == 0)
-                    .count();
-
-            if (unescapedAmpersands > 0) {
-                results.add(new IntegrityMessage(Localization.lang("Found %0 unescaped '&'", unescapedAmpersands), entry, field.getKey()));
-                // note: when changing the message - also do so in tests
-            }
-        }
-        return results;
+        return entry.getFieldMap().entrySet().stream()
+                    .filter(field -> !field.getKey().getProperties().contains(FieldProperty.VERBATIM))
+                    // We use "flatMap" instead of filtering later, because we assume there won't be that much error messages - and construction of Stream.empty() is faster than construction of a new Tuple2 (including lifting long to Long)
+                    .flatMap(field -> {
+                        // counts the number of even \ occurrences preceding an &
+                        long unescapedAmpersands = BACKSLASH_PRECEDED_AMPERSAND.matcher(field.getValue())
+                                                                               .results()
+                                                                               .map(MatchResult::group)
+                                                                               .filter(m -> CharMatcher.is('\\').countIn(m) % 2 == 0)
+                                                                               .count();
+                        if (unescapedAmpersands == 0) {
+                            return Stream.empty();
+                        }
+                        return Stream.of(new Tuple2<>(field.getKey(), unescapedAmpersands));
+                    })
+                    .map(tuple -> new IntegrityMessage(Localization.lang("Found %0 unescaped '&'", tuple.v2()), entry, tuple.v1()))
+                    .toList();
     }
 }
