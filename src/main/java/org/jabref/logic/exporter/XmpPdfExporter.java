a73bb070197d59f4b20fe861ae1a2df7463c342e
Luggas <127773292+Luggas4you@users.noreply.github.com>
2023-10-03 19:09:13 +0200

    Fix PDF export (#10361)
    * Create PDF WIP
    
    * Create PDF WIP
    
    * Create PDF WIP
    
    * Add @Test to XmpPdfExporterTest WIP
    
    * Add Importer to XmpPdfExporterTest
    
    * WIP
    
    * Fix testRoundtripExportImport
    
    * Finish testRoundtripExportImport
    
    * Add @AfterEach to XmpPdfExporterTest.java
    
    * Change @Test to @ParameterizedTest
    
    * Delete IllegalArgumentException in XmpPdfExporter.java
    
    * add changelog and change message
    
    ---------
    
    Co-authored-by: Siedlerchr <siedlerkiller@gmail.com>

--- src/main/java/org/jabref/logic/exporter/XmpPdfExporter.java
+++ src/main/java/org/jabref/logic/exporter/XmpPdfExporter.java
@@ -1,5 +1,7 @@
 package org.jabref.logic.exporter;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
@@ -10,6 +12,12 @@ import org.jabref.logic.xmp.XmpPreferences;
 import org.jabref.logic.xmp.XmpUtilWriter;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 
 public class XmpPdfExporter extends Exporter {
 
@@ -26,7 +34,24 @@ public class XmpPdfExporter extends Exporter {
         Objects.requireNonNull(pdfFile);
         Objects.requireNonNull(entries);
 
-        if (pdfFile.toString().endsWith(".pdf")) {
+        Path filePath = pdfFile.toAbsolutePath();
+
+        if (!Files.exists(filePath)) {
+            try (PDDocument document = new PDDocument()) {
+                PDPage page = new PDPage();
+                document.addPage(page);
+
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                    contentStream.beginText();
+                    contentStream.newLineAtOffset(25, 500);
+                    contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 12);
+                    contentStream.showText("This PDF was created by JabRef. It demonstrates the embedding of XMP data in PDF files. Please open the file metadata view of your PDF viewer to see the attached files. Note that the normal usage is to embed the BibTeX data in an existing PDF.");
+                    contentStream.endText();
+                }
+                document.save(filePath.toString());
+            } catch (IOException e) {
+                throw new Exception("Error creating PDF", e);
+            }
             new XmpUtilWriter(xmpPreferences).writeXmp(pdfFile, entries, databaseContext.getDatabase());
         }
     }
