a4953e7dcf9dc666488e8050c7eb5f1df133d7d6
Jawad <j.mouza@student.tue.nl>
2023-10-06 08:28:43 +0200

    Intitial hayagriva yaml support

---
+++ src/main/java/org/jabref/logic/layout/format/HayagrivaType.java
@@ -0,0 +1,20 @@
+package org.jabref.logic.layout.format;
+
+import org.jabref.logic.layout.LayoutFormatter;
+import org.jabref.model.entry.types.StandardEntryType;
+
+public class HayagrivaType implements LayoutFormatter {
+
+    @Override
+    public String format(String value) {
+        return switch (StandardEntryType.valueOf(value)) {
+            case Article, Conference -> "article";
+            case Book -> "book";
+            case Report, TechReport -> "report";
+            case Thesis, MastersThesis, PhdThesis -> "thesis";
+            case WWW, Online -> "web";
+
+            default -> "Misc";
+        };
+    }
+}
