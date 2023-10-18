3e088967b62c12726177d23df209496e3524c99b
Campbell <u7122878@anu.edu.au>
2023-10-18 23:21:42 +1100

    Added Date, Author and Page Number checks for BooktitleChecker.java
    * Added check to see if booktitle includes Date
    * Added check to see if booktitle includes Author
    * Added check to see if booktitle includes Page Numbers

--- src/main/java/org/jabref/logic/integrity/BooktitleChecker.java
+++ src/main/java/org/jabref/logic/integrity/BooktitleChecker.java
@@ -2,11 +2,18 @@ package org.jabref.logic.integrity;
 
 import java.util.Locale;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.strings.StringUtil;
 
 public class BooktitleChecker implements ValueChecker {
+
+    private static final Predicate<String> DATE_IN_BOOKTITLE = Pattern.compile("(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s\\d{1,2}(?:-\\d{1,2})?,\\s\\d{4}").asPredicate();
+    private static final Predicate<String> AUTHOR_IN_BOOKTITLE = Pattern.compile("\\{([^}]+)\\}").asPredicate();
+    private static final Predicate<String> PAGE_NUMBERS_IN_BOOKTITLE = Pattern.compile("\\b(P(?:age)?\\.?\\s?\\d+(?:-\\d+)?)\\b").asPredicate();
 
     @Override
     public Optional<String> checkValue(String value) {
@@ -16,7 +23,24 @@ public class BooktitleChecker implements ValueChecker {
 
         if (value.toLowerCase(Locale.ENGLISH).endsWith("conference on")) {
             return Optional.of(Localization.lang("booktitle ends with 'conference on'"));
+        } else
+            // DATE CHECK
+            if (DATE_IN_BOOKTITLE.test(value.trim())) {
+                return Optional.of(Localization.lang("Date is contained in the booktitle"));
+        } else
+            // AUTHOR CHECK
+            if (AUTHOR_IN_BOOKTITLE.test(value.trim())) {
+                return Optional.of(Localization.lang("Author is contained in the booktitle"));
+        } else
+            // PAGE NUMBER CHECK
+            if (PAGE_NUMBERS_IN_BOOKTITLE.test(value.trim())) {
+                return Optional.of(Localization.lang("Page numbers are contained in the booktitle"));
+
         }
+
+        // YEAR CHECK
+
+        // LOCATION CHECK
 
         return Optional.empty();
     }
