be45124d01a9f1a7160aa97463daed1a61d4d3e4
Oliver Kopp <kopp.dev@gmail.com>
2023-10-19 23:27:58 +0200

    Switch from JDK XML parser to aalto-xml (#10489)
    * Switch from JDK XML parser to aalto-xml
    
    * fix test, move up
    
    * cleanup in importers
    
    * fix compile and checkstyle
    
    * fix tests
    
    * fix tests and possible npe
    
    ---------
    
    Co-authored-by: Siedlerchr <siedlerkiller@gmail.com>

--- src/main/java/org/jabref/logic/importer/fileformat/EndnoteXmlImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/EndnoteXmlImporter.java
@@ -15,7 +15,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
-import javax.xml.XMLConstants;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
@@ -51,9 +50,16 @@ public class EndnoteXmlImporter extends Importer implements Parser {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EndnoteXmlImporter.class);
     private final ImportFormatPreferences preferences;
+    private final XMLInputFactory xmlInputFactory;
 
     public EndnoteXmlImporter(ImportFormatPreferences preferences) {
         this.preferences = preferences;
+        xmlInputFactory = XMLInputFactory.newInstance();
+        // prevent xxe (https://rules.sonarsource.com/java/RSPEC-2755)
+        // not suported by aalto-xml
+        // xmlInputFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+        // required for reading Unicode characters such as &#xf6;
+        xmlInputFactory.setProperty(XMLInputFactory.IS_COALESCING, true);
     }
 
     private static String join(List<String> list, String string) {
@@ -101,13 +107,6 @@ public class EndnoteXmlImporter extends Importer implements Parser {
         List<BibEntry> bibItems = new ArrayList<>();
 
         try {
-            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-
-            // prevent xxe (https://rules.sonarsource.com/java/RSPEC-2755)
-            xmlInputFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
-            // required for reading Unicode characters such as &#xf6;
-            xmlInputFactory.setProperty(XMLInputFactory.IS_COALESCING, true);
-
             XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(input);
 
             while (reader.hasNext()) {
