be9c01ba2e920042441a286f47c213730d5ee9f1
Campbell <u7122878@anu.edu.au>
2023-10-21 23:03:59 +1100

    Adjusted Page Numbers Check
    Adjusted page numbers check to ensure only one regex check is being used

--- src/main/java/org/jabref/logic/integrity/BooktitleChecker.java
+++ src/main/java/org/jabref/logic/integrity/BooktitleChecker.java
@@ -3,7 +3,6 @@ package org.jabref.logic.integrity;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.jabref.logic.l10n.Localization;
@@ -11,9 +10,11 @@ import org.jabref.model.strings.StringUtil;
 
 public class BooktitleChecker implements ValueChecker {
 
-    private static final Predicate<String> DATE_IN_BOOKTITLE = Pattern.compile("(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s\\d{1,2}(?:-\\d{1,2})?,\\s\\d{4}").asPredicate();
-    private static final Predicate<String> AUTHOR_IN_BOOKTITLE = Pattern.compile("\\{([^}]+)\\}").asPredicate();
-    private static final Predicate<String> PAGE_NUMBERS_IN_BOOKTITLE = Pattern.compile("\\b(P(?:age)?\\.?\\s?\\d+(?:-\\d+)?)\\b").asPredicate();
+    private static final Predicate<String> DATE_CHECK = Pattern.compile("(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s\\d{1,2}(?:-\\d{1,2})?,\\s\\d{4}").asPredicate();
+    private static final Predicate<String> AUTHOR_CHECK = Pattern.compile("\\{([^}]+)\\}").asPredicate();
+    private static final Predicate<String> PAGE_NUMBERS_CHECK = Pattern.compile("\\b(Page|Pg|Pp)\\.? \\d+\\b", Pattern.CASE_INSENSITIVE).asPredicate();
+    private static final Predicate<String> YEAR_CHECK = Pattern.compile("").asPredicate();
+    private static final Predicate<String> LOCATION_CHECK = Pattern.compile("").asPredicate();
 
     @Override
     public Optional<String> checkValue(String value) {
@@ -25,15 +26,15 @@ public class BooktitleChecker implements ValueChecker {
             return Optional.of(Localization.lang("booktitle ends with 'conference on'"));
         } else
             // DATE CHECK
-            if (DATE_IN_BOOKTITLE.test(value.trim())) {
+            if (DATE_CHECK.test(value.trim())) {
                 return Optional.of(Localization.lang("Date is contained in the booktitle"));
         } else
             // AUTHOR CHECK
-            if (AUTHOR_IN_BOOKTITLE.test(value.trim())) {
+            if (AUTHOR_CHECK.test(value.trim())) {
                 return Optional.of(Localization.lang("Author is contained in the booktitle"));
         } else
             // PAGE NUMBER CHECK
-            if (PAGE_NUMBERS_IN_BOOKTITLE.test(value.trim())) {
+            if (PAGE_NUMBERS_CHECK.test(value.trim())){
                 return Optional.of(Localization.lang("Page numbers are contained in the booktitle"));
 
         }
