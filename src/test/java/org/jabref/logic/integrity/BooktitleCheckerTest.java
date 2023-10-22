98ba89c20f0e95f67dd964f6c4b2af8cb0a6c425
Campbell <u7122878@anu.edu.au>
2023-10-22 15:02:01 +1100

    Added Basic Tests for integrity checks
    * Added tests for author, data, year and page number checks
    * Changed JabRef_en.properties to ensure correct Localization is used

--- src/test/java/org/jabref/logic/integrity/BooktitleCheckerTest.java
+++ src/test/java/org/jabref/logic/integrity/BooktitleCheckerTest.java
@@ -2,6 +2,8 @@ package org.jabref.logic.integrity;
 
 import java.util.Optional;
 
+import org.jabref.logic.l10n.Localization;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,6 +21,26 @@ public class BooktitleCheckerTest {
     @Test
     void booktitleDoesNotAcceptsIfItEndsWithConferenceOn() {
         assertNotEquals(Optional.empty(), checker.checkValue("Digital Information and Communication Technology and it's Applications (DICTAP), 2014 Fourth International Conference on"));
+    }
+
+    @Test
+    void booktitleContainsDate() {
+        assertEquals(Optional.of(Localization.lang("Date is contained in the booktitle")), checker.checkValue("Service-Oriented Computing - {ICSOC} 2007, Fifth International Conference, Vienna, Austria, September 17-20, 2007, Proceedings"));
+    }
+
+    @Test
+    void booktitleContainsYear() {
+        assertEquals(Optional.of(Localization.lang("Year is contained in the booktitle")), checker.checkValue("Service-Oriented Computing - {ICSOC} 2007, Fifth International Conference, Vienna, Austria, Proceedings"));
+    }
+
+    @Test
+    void booktitleContainsAuthor() {
+        assertEquals(Optional.of(Localization.lang("Author is contained in the booktitle")), checker.checkValue("Service-Oriented Computing - {ICSOC}, Fifth International Conference, Vienna, Austria, Proceedings"));
+    }
+
+    @Test
+    void booktitleContainsPageNumbers() {
+        assertEquals(Optional.of(Localization.lang("Page numbers are contained in the booktitle")), checker.checkValue("Service-Oriented Computing, Fifth International Conference, Vienna, Austria, Proceedings, Page 3-5"));
     }
 
     @Test
