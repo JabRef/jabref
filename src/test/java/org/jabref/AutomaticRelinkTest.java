ae3a1c145c3aa473b7a548dc8cea20db35cda1d8
Lucas Vial <u7491018@anu.edu.au>
2023-10-22 22:55:37 +1100

    First Cycle completed, Redy for feedback/next steps

--- src/test/java/org/jabref/AutomaticRelinkTest.java
+++ src/test/java/org/jabref/AutomaticRelinkTest.java
@@ -1,73 +1,174 @@
 package org.jabref;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.jabref.model.database.BibDatabase;
+import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.LinkedFile;
+import org.jabref.model.metadata.MetaData;
+import org.jabref.preferences.FilePreferences;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AutomaticRelinkTest {
 
-    /** Set up Directories 
-     *
-     */
-    public void setUpD() {
-        String filepath = "src/test/TestDirectory";
-        File main = new File(filepath);
-        Boolean makeM = main.mkdir();
+    private Path main;
+    private Path B;
+    private Path A;
+    private Path beforeFilePath;
+    private BibEntry entry;
+    private FilePreferences filePreferences;
+    private BibDatabaseContext databaseContext;
+    private String defaultDirectory;
 
-        String filepath2 = "src/test/TestDirectory/A";
-        File a = new File(filepath2);
-        Boolean makeA = a.mkdir();
-
-        String filepath3 = "src/test/TestDirectory/B";
-        File b = new File(filepath3);
-        Boolean makeB = b.mkdir();
-
-        assertTrue(makeM, "Error Creating Main");
-        assertTrue(makeA, "Error Creating A");
-        assertTrue(makeB, "Error Creating B");
-    }
-
-    @Test
-    void setUp() {
-     //   setUpD();
-    }
     /**
-     * Copy test pdf into my folder
+     * `MoveFilesCleanupTest` Used as an example of how to do this
+     *
+     * @param bibFolder
+     * @throws IOException
      */
+    @BeforeEach
+    void setUp(@TempDir Path bibFolder) throws IOException {
+        // Create Main
+        main = bibFolder.resolve("main");
+        Files.createDirectory(main);
 
-    public void copy(String source, String destination) throws IOException {
-        Path src = Path.of(source);
-        Path dst = Path.of(destination);
-        Files.copy(src, dst.resolve(src.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+        // Create B Directory
+        B = main.resolve("B");
+        Files.createDirectory(B);
+
+        // Create A Directory & test file
+        A = main.resolve("A");
+        Files.createDirectory(A);
+        beforeFilePath = A.resolve("test.pdf");
+        Files.createFile(beforeFilePath);
+
+        MetaData metaData = new MetaData();
+        defaultDirectory = main.toAbsolutePath().toString();
+        metaData.setDefaultFileDirectory(defaultDirectory);
+        databaseContext = new BibDatabaseContext(new BibDatabase(), metaData);
+        Files.createFile(bibFolder.resolve("test.bib"));
+        databaseContext.setDatabasePath(bibFolder.resolve("test.bib"));
+
+        entry = new BibEntry();
+        LinkedFile linkedFile = new LinkedFile("desc", beforeFilePath, "");
+        List<LinkedFile> listLinked = new ArrayList<>();
+        listLinked.add(linkedFile);
+        entry.setFiles(listLinked);
+
+        filePreferences = mock(FilePreferences.class);
+        when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(false); // Biblocation as Primary overwrites all other dirs, therefore we set it to false here
     }
 
-    //Make sure the copy method works
-   @Test
-   void copyTest() throws IOException {
-        copy("src/test/resources/pdfs/minimal.pdf", "src/test/TestDirectory/A");
-       File file = new File("src/test/TestDirectory/A/minimal.pdf");
-       assertTrue(file.exists(), "Copy did not work!");
-    }
-
-
-    //Still working this out!
+    /**
+     * Run
+     * on
+     * empty
+     * Bib
+     * Entry
+     */
     @Test
-    void checkEntry() {
+    void checkEmpty() {
+        System.out.println(beforeFilePath);
         BibEntry bib = new BibEntry();
-        bib.addFile(new LinkedFile("description", "src/test/TestDirectory/A/minimal.pdf",".pdf"));
-        List<LinkedFile> list = new List<LinkedFile>() ;
-        list.add(new LinkedFile("description", "src/test/TestDirectory/A/minimal.pdf",".pdf"));
-        assertEquals(list, bib.getFiles());
+        AutomaticRelink.relink(bib, defaultDirectory);
+        BibEntry empty = new BibEntry();
+        assertEquals(empty, bib);
+    }
+
+    /**
+     * Check that relink returns the same entries
+     */
+    @Test
+    void checkSame() {
+        BibEntry bib = new BibEntry();
+        LinkedFile linkedFile = new LinkedFile("desc", beforeFilePath, "");
+        List<LinkedFile> listLinked = new ArrayList<>();
+        listLinked.add(linkedFile);
+        bib.setFiles(listLinked);
+        AutomaticRelink.relink(bib, defaultDirectory);
+        assertEquals(entry, bib);
+    }
+
+    /**
+     * Check that it stays the same once copied
+     */
+    @Test
+    void checkCopy() {
+        // Run "Automatically set file links" - check that the bib file was not modified
+        BibEntry bib = new BibEntry();
+        LinkedFile linkedFile = new LinkedFile("desc", beforeFilePath, "");
+        List<LinkedFile> listLinked = new ArrayList<>();
+        listLinked.add(linkedFile);
+        bib.setFiles(listLinked);
+        AutomaticRelink.relink(bib, defaultDirectory);
+        assertEquals(entry, bib);
+
+        // Copy Bib file from A to B
+        Path destination = B.resolve("test.pdf");
+        try {
+            Files.copy(A.resolve("test.pdf"), destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+            fail("File copy failed!");
+        }
+
+        AutomaticRelink.relink(bib, defaultDirectory);
+        assertEquals(entry, bib);
+    }
+
+    /**
+     * Check that the file was relinked once moved
+     */
+    @Test
+    void relinkTest() {
+        // Run "Automatically set file links" - check that the bib file was not modified
+        BibEntry bib = new BibEntry();
+        LinkedFile linkedFile = new LinkedFile("desc", beforeFilePath, "");
+        List<LinkedFile> listLinked = new ArrayList<>();
+        listLinked.add(linkedFile);
+        bib.setFiles(listLinked);
+        AutomaticRelink.relink(bib, defaultDirectory);
+        assertEquals(entry, bib, "1");
+
+        // Copy Bib file from A to B
+        Path destination = B.resolve("test.pdf");
+        try {
+            Files.copy(A.resolve("test.pdf"), destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+            fail("File copy failed!");
+        }
+        // Delete old file
+        try {
+            Files.deleteIfExists(A.resolve("test.pdf"));
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+            fail("Delete File Failed");
+        }
+
+        // Change Entry to match required result and run method on bib
+        LinkedFile linkedDestFile = new LinkedFile("desc", B.resolve("test.pdf"), "");
+        List<LinkedFile> listLinked2 = new ArrayList<>();
+        listLinked2.add(linkedDestFile);
+        entry.setFiles(listLinked2);
+        AutomaticRelink.relink(bib, defaultDirectory);
+        assertEquals(entry, bib, "Something went wrong");
     }
 }
