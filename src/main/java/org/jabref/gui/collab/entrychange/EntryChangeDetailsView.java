96da096e819af29f8d4cd53a82737ae92175a21f
Ren <u7074786@anu.edu.au>
2023-10-28 17:04:04 +1100

    Modified EntryChangeDetailsView scrolling sync to sync when the scroll bar is dragged.

--- src/main/java/org/jabref/gui/collab/entrychange/EntryChangeDetailsView.java
+++ src/main/java/org/jabref/gui/collab/entrychange/EntryChangeDetailsView.java
@@ -1,9 +1,11 @@
 package org.jabref.gui.collab.entrychange;
 
+import javafx.event.Event;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 
@@ -24,6 +26,7 @@ import com.tobiasdiez.easybind.EasyBind;
 public final class EntryChangeDetailsView extends DatabaseChangeDetailsView {
     private final PreviewWithSourceTab oldPreviewWithSourcesTab = new PreviewWithSourceTab();
     private final PreviewWithSourceTab newPreviewWithSourcesTab = new PreviewWithSourceTab();
+    private boolean scrolling = false;
 
     public EntryChangeDetailsView(BibEntry oldEntry,
                                   BibEntry newEntry,
@@ -44,18 +47,11 @@ public final class EntryChangeDetailsView extends DatabaseChangeDetailsView {
         PreviewViewer previewClone = new PreviewViewer(databaseContext, dialogService, preferencesService, stateManager, themeManager, taskExecutor);
 
         // The scroll bar used is not part of ScrollPane, but the attached WebView.
-        // https://stackoverflow.com/questions/31264847/how-to-set-remember-scrollbar-thumb-position-in-javafx-8-webview
         WebView previewCloneView = (WebView) previewClone.getContent();
         WebView previewViewerView = (WebView) previewViewer.getContent();
-        previewCloneView.setOnScroll(event -> {
-            int value = (Integer) previewCloneView.getEngine().executeScript("window.scrollY");
-            previewViewerView.getEngine().executeScript("window.scrollTo(0, " + value + ")");
-        });
-        previewViewerView.setOnScroll(event -> {
-            int value = (Integer) previewViewerView.getEngine().executeScript("window.scrollY");
-            previewCloneView.getEngine().executeScript("window.scrollTo(0, " + value + ")");
-        });
-        // TODO: Currently does not update position when scroll bar itself is dragged, and the updated position appears to lag behind for the other panel.
+        synchronizeScrolling(previewCloneView, previewViewerView);
+        synchronizeScrolling(previewViewerView, previewCloneView);
+        // TODO: Also sync scrolling for BibTeX view.
 
         TabPane oldEntryTabPane = oldPreviewWithSourcesTab.getPreviewWithSourceTab(oldEntry, databaseContext, preferencesService, entryTypesManager, previewClone, Localization.lang("Entry Preview"));
         TabPane newEntryTabPane = newPreviewWithSourcesTab.getPreviewWithSourceTab(newEntry, databaseContext, preferencesService, entryTypesManager, previewViewer, Localization.lang("Entry Preview"));
@@ -82,5 +78,23 @@ public final class EntryChangeDetailsView extends DatabaseChangeDetailsView {
         setBottomAnchor(split, 8d);
 
         this.getChildren().add(split);
+    }
+
+    // Method adapted from:
+    // https://stackoverflow.com/questions/49509395/synchronize-scrollbars-of-two-javafx-webviews
+    // https://stackoverflow.com/questions/31264847/how-to-set-remember-scrollbar-thumb-position-in-javafx-8-webview
+    private void synchronizeScrolling(WebView webView, WebView otherWebView) {
+        webView.addEventHandler(Event.ANY, event -> {
+            if (!scrolling) {
+                scrolling = true;
+                if (event instanceof MouseEvent) {
+                    int value = (Integer) webView.getEngine().executeScript("window.scrollY");
+                    otherWebView.getEngine().executeScript("window.scrollTo(0, " + value + ")");
+                } else {
+                    otherWebView.fireEvent(event.copyFor(otherWebView, otherWebView));
+                }
+                scrolling = false;
+            }
+        });
     }
 }
