a17c0b415902a6d27ceaae78646ae06a3380e13e
Siedlerchr <siedlerkiller@gmail.com>
2023-11-11 19:41:58 +0100

    checkstyle

--- src/main/java/org/jabref/logic/journals/PredatoryJournalInformation.java
+++ src/main/java/org/jabref/logic/journals/PredatoryJournalInformation.java
@@ -4,9 +4,10 @@ import java.io.Serializable;
 
 /**
  * Represents predatory journal information
+ *
  * @param name The full journal name
  * @param abbr Abbreviation, if any
- * @param url Url of the journal
+ * @param url  Url of the journal
  */
 public record PredatoryJournalInformation(
         String name,
