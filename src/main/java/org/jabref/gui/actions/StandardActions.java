5e24256323047a6bf4ef70ea1ec37124a41febdf
lenkabuebnkova <bubenkovalenka@gmail.com>
2023-11-18 17:11:53 +0100

    Change to only hovering through files

--- src/main/java/org/jabref/gui/actions/StandardActions.java
+++ src/main/java/org/jabref/gui/actions/StandardActions.java
@@ -55,6 +55,7 @@ public enum StandardActions implements Action {
     CLEAR_READ_STATUS(Localization.lang("Clear read status"), KeyBinding.CLEAR_READ_STATUS),
     READ(Localization.lang("Set read status to read"), IconTheme.JabRefIcons.READ_STATUS_READ, KeyBinding.READ),
     SKIMMED(Localization.lang("Set read status to skimmed"), IconTheme.JabRefIcons.READ_STATUS_SKIMMED, KeyBinding.SKIMMED),
+    RELEVANCE(Localization.lang("Relevance"), IconTheme.JabRefIcons.RELEVANCE),
     RELEVANT(Localization.lang("Toggle relevance"), IconTheme.JabRefIcons.RELEVANCE),
     NEW_LIBRARY(Localization.lang("New library"), IconTheme.JabRefIcons.NEW),
     OPEN_LIBRARY(Localization.lang("Open library"), IconTheme.JabRefIcons.OPEN, KeyBinding.OPEN_DATABASE),
