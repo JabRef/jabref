1ea5b2e13fc9cc341f393cf723143d2c852df9de
Siedlerchr <siedlerkiller@gmail.com>
2023-11-21 17:49:40 +0100

    revert deletion

--- src/main/java/org/jabref/gui/entryeditor/citationrelationtab/semanticscholar/SemanticScholarFetcher.java
+++ src/main/java/org/jabref/gui/entryeditor/citationrelationtab/semanticscholar/SemanticScholarFetcher.java
@@ -33,7 +33,8 @@ public class SemanticScholarFetcher implements CitationFetcher, CustomizableKeyF
                     .append("paper/")
                     .append("DOI:").append(entry.getDOI().get().getDOI())
                     .append("/citations")
-                    .append("?fields=");
+                    .append("?fields=").append("title,authors,year,citationCount,referenceCount")
+                    .append("&limit=1000");
             try {
                 URL citationsUrl = URI.create(urlBuilder.toString()).toURL();
                 URLDownload urlDownload = new URLDownload(citationsUrl);
@@ -63,7 +64,8 @@ public class SemanticScholarFetcher implements CitationFetcher, CustomizableKeyF
                     .append("paper/")
                     .append("DOI:").append(entry.getDOI().get().getDOI())
                     .append("/references")
-                    .append("?fields=").append("title,authors,year,citationCount,referenceCount")
+                    .append("?fields=")
+                    .append("title,authors,year,citationCount,referenceCount")
                     .append("&limit=1000");
             try {
                 URL referencesUrl = URI.create(urlBuilder.toString()).toURL();
