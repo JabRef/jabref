2a4bd6cac3593fafa88e4909953c03b96339cb72
Théo <127297775+theoo33@users.noreply.github.com>
2023-11-27 21:30:18 +0100

    Add 'More' option to right click menu in the main table #9432 (#10659)
    * add show more option in the right click main table header menu,
    openingthe preferences
    
    * comments
    
    * Achieving contribution 2
    
    * adjustments for right click menu on main table
    
    * Update CHANGELOG.md for 9432
    
    * PR test requirements
    
    * Other PR requirements
    
    * Other PR requirements
    
    * remove optional from parameter
    
    * openrewrite test
    
    * openrewrite test
    
    * change string to class for selection
    
    * change string to class for selection
    
    * remove comment + pass keybindings as parameter
    
    * Update JabRef_en.properties
    
    * Introduced StandardActions.withText
    
    * Refactored for code maintenance
    
    * Moved calls to frame out of PreferencesDialogView
    
    ---------
    
    Co-authored-by: Théo Granier <theo.granier@imt-atlantique.net>
    Co-authored-by: newma2n <hamidou.kane@imt-atlantique.net>
    Co-authored-by: Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>

--- src/main/java/org/jabref/gui/preferences/ShowPreferencesAction.java
+++ src/main/java/org/jabref/gui/preferences/ShowPreferencesAction.java
@@ -3,23 +3,31 @@ package org.jabref.gui.preferences;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.JabRefFrame;
 import org.jabref.gui.actions.SimpleCommand;
-import org.jabref.gui.util.TaskExecutor;
 
 import com.airhacks.afterburner.injection.Injector;
 
 public class ShowPreferencesAction extends SimpleCommand {
 
     private final JabRefFrame jabRefFrame;
-    private final TaskExecutor taskExecutor;
+    private final Class<? extends PreferencesTab> preferencesTabToSelectClass;
 
-    public ShowPreferencesAction(JabRefFrame jabRefFrame, TaskExecutor taskExecutor) {
+    public ShowPreferencesAction(JabRefFrame jabRefFrame) {
+        this(jabRefFrame, null);
+    }
+
+    public ShowPreferencesAction(JabRefFrame jabRefFrame, Class<? extends PreferencesTab> preferencesTabToSelectClass) {
         this.jabRefFrame = jabRefFrame;
-        this.taskExecutor = taskExecutor;
+        this.preferencesTabToSelectClass = preferencesTabToSelectClass;
     }
 
     @Override
     public void execute() {
         DialogService dialogService = Injector.instantiateModelOrService(DialogService.class);
-        dialogService.showCustomDialog(new PreferencesDialogView(jabRefFrame));
+        dialogService.showCustomDialog(new PreferencesDialogView(preferencesTabToSelectClass));
+
+        // Refresh frame and tables
+        jabRefFrame.getGlobalSearchBar().updateHintVisibility();
+        jabRefFrame.setupAllTables();
+        jabRefFrame.getLibraryTabs().forEach(panel -> panel.getMainTable().getTableModel().refresh());
     }
 }
