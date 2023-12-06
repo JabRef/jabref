454f38f8583e877d222772d9952306b8dcac3ae1
Siedlerchr <siedlerkiller@gmail.com>
2023-12-06 19:38:12 +0100

    Fix resource does not exist

--- src/main/java/org/jabref/logic/journals/predatory/PredatoryJournalListLoader.java
+++ src/main/java/org/jabref/logic/journals/predatory/PredatoryJournalListLoader.java
@@ -2,6 +2,7 @@ package org.jabref.logic.journals.predatory;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.slf4j.Logger;
@@ -12,8 +13,6 @@ public class PredatoryJournalListLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(PredatoryJournalListLoader.class);
 
     public static PredatoryJournalRepository loadRepository() {
-        PredatoryJournalRepository repository = new PredatoryJournalRepository();
-
         Path path;
         try {
             URL resource = PredatoryJournalRepository.class.getResource("/journals/predatory-journals.mv");
@@ -22,11 +21,14 @@ public class PredatoryJournalListLoader {
                 return new PredatoryJournalRepository();
             }
             path = Path.of(resource.toURI());
+            if (!Files.exists(path)) {
+                LOGGER.error("predatoryJournal-list.mv not found. Using demo list.");
+                return new PredatoryJournalRepository();
+            }
         } catch (URISyntaxException e) {
             LOGGER.error("Could not determine path to predatoryJournal-list.mv. Using demo list.");
             return new PredatoryJournalRepository();
         }
-
         return new PredatoryJournalRepository(path);
     }
 }
