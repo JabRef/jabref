1074e5e7ed3ca08b250d9d626b4c481129a8940b
Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
2023-12-25 17:14:36 +0100

    Extracted calls to JabRefFrame from OpenEntryEditorAction

--- src/main/java/org/jabref/gui/entryeditor/OpenEntryEditorAction.java
+++ src/main/java/org/jabref/gui/entryeditor/OpenEntryEditorAction.java
@@ -1,17 +1,17 @@
 package org.jabref.gui.entryeditor;
 
-import org.jabref.gui.JabRefFrame;
+import org.jabref.gui.LibraryTabContainer;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.actions.ActionHelper;
 import org.jabref.gui.actions.SimpleCommand;
 
 public class OpenEntryEditorAction extends SimpleCommand {
 
-    private final JabRefFrame frame;
+    private final LibraryTabContainer tabContainer;
     private final StateManager stateManager;
 
-    public OpenEntryEditorAction(JabRefFrame frame, StateManager stateManager) {
-        this.frame = frame;
+    public OpenEntryEditorAction(LibraryTabContainer tabContainer, StateManager stateManager) {
+        this.tabContainer = tabContainer;
         this.stateManager = stateManager;
 
         this.executable.bind(ActionHelper.needsEntriesSelected(stateManager));
@@ -19,7 +19,7 @@ public class OpenEntryEditorAction extends SimpleCommand {
 
     public void execute() {
         if (!stateManager.getSelectedEntries().isEmpty()) {
-            frame.getCurrentLibraryTab().showAndEdit(stateManager.getSelectedEntries().get(0));
+            tabContainer.getCurrentLibraryTab().showAndEdit(stateManager.getSelectedEntries().getFirst());
         }
     }
 }
