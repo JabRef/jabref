3ff4457e03c44f18b0451343bbee6d6ef0b898f6
MohdAzghar <mohammedazhar1210@gmail.com>
2024-01-23 16:16:17 +0530

    issue1#10557

--- src/main/java/org/jabref/logic/integrity/JournalInAbbreviationListChecker.java
+++ src/main/java/org/jabref/logic/integrity/JournalInAbbreviationListChecker.java
@@ -22,6 +22,7 @@ public class JournalInAbbreviationListChecker implements EntryChecker {
 
     @Override
     public List<IntegrityMessage> check(BibEntry entry) {
+        
         Optional<String> value = entry.getFieldOrAliasLatexFree(field);
         if (value.isEmpty()) {
             return Collections.emptyList();
