2777ebcd238f5f123e834d15b576a62b6a0edbb7
Roc <1844478+ror3d@users.noreply.github.com>
2024-02-18 21:24:08 +0000

    Keep source urls on file download (#10855)
    * Add source URL to the linked files and update it when a file is initially downloaded
    
    * Add context menu action to redownload a file
    
    * Add main menu tools action to redownload missing files
    Refactor redownload code into an action
    
    * Don't change the local link name when redownloading, the link should already have an appropriate file name, so use that for the downloaded file.
    
    * Update CHANGELOG.md
    
    * revert method change
    
    * Revert changes in property files
    
    * Improve extension check
    
    * Fix issue where action would only be executed once per jabref execution.
    
    * Make string empty check more readable
    
    * Fix test for FileFieldParser
    
    * Small refactoring and language fix
    
    * Refactor LinkedFileViewModel to use the DownloadLinkedFileAction for downloading
    Change test to use that too.
    
    * Fix rewrite check
    
    * Change file field format to be backwards compatible with old versions with mediaType.
    Recover/add tests for the file field.
    
    * Small language fix and reduced public fingerprint
    
    * Extract downloadProgress
    
    * DownloadLinkedFileActionTest
    
    Co-authored-by: Christoph <siedlerkiller@gmail.com>
    
    * Fixed checkstyle
    
    * Revert "Change file field format to be backwards compatible with old versions with mediaType."
    
    This reverts commit 2230310fce66d9c705a3857878f8f33b1bf1030b.
    
    * Reapply and modify good tests
    
    ---------
    
    Co-authored-by: Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
    Co-authored-by: Christoph <siedlerkiller@gmail.com>

--- src/main/java/org/jabref/gui/actions/StandardActions.java
+++ src/main/java/org/jabref/gui/actions/StandardActions.java
@@ -30,6 +30,7 @@ public enum StandardActions implements Action {
     SEND_AS_EMAIL(Localization.lang("As Email")),
     SEND_TO_KINDLE(Localization.lang("To Kindle")),
     REBUILD_FULLTEXT_SEARCH_INDEX(Localization.lang("Rebuild fulltext search index"), IconTheme.JabRefIcons.FILE),
+    REDOWNLOAD_MISSING_FILES(Localization.lang("Redownload missing files"), IconTheme.JabRefIcons.DOWNLOAD),
     OPEN_EXTERNAL_FILE(Localization.lang("Open file"), IconTheme.JabRefIcons.FILE, KeyBinding.OPEN_FILE),
     OPEN_URL(Localization.lang("Open URL or DOI"), IconTheme.JabRefIcons.WWW, KeyBinding.OPEN_URL_OR_DOI),
     SEARCH_SHORTSCIENCE(Localization.lang("Search ShortScience")),
@@ -149,6 +150,7 @@ public enum StandardActions implements Action {
 
     EDIT_FILE_LINK(Localization.lang("Edit"), IconTheme.JabRefIcons.EDIT, KeyBinding.EDIT_ENTRY),
     DOWNLOAD_FILE(Localization.lang("Download file"), IconTheme.JabRefIcons.DOWNLOAD_FILE),
+    REDOWNLOAD_FILE(Localization.lang("Redownload file"), IconTheme.JabRefIcons.DOWNLOAD_FILE),
     RENAME_FILE_TO_PATTERN(Localization.lang("Rename file to defined pattern"), IconTheme.JabRefIcons.AUTO_RENAME),
     RENAME_FILE_TO_NAME(Localization.lang("Rename file to a given name"), IconTheme.JabRefIcons.RENAME, KeyBinding.REPLACE_STRING),
     MOVE_FILE_TO_FOLDER(Localization.lang("Move file to file directory"), IconTheme.JabRefIcons.MOVE_TO_FOLDER),
