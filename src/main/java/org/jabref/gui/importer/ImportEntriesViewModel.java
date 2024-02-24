84509b39268f5e729ae20e94173837c8b9aed118
Siedlerchr <siedlerkiller@gmail.com>
2024-02-24 19:32:25 +0100

    add icon, downloading is also handled in import entries

--- src/main/java/org/jabref/gui/importer/ImportEntriesViewModel.java
+++ src/main/java/org/jabref/gui/importer/ImportEntriesViewModel.java
@@ -17,9 +17,7 @@ import javafx.collections.ObservableList;
 import org.jabref.gui.AbstractViewModel;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
-import org.jabref.gui.duplicationFinder.DuplicateResolverDialog;
 import org.jabref.gui.externalfiles.ImportHandler;
-import org.jabref.gui.fieldeditors.LinkedFileViewModel;
 import org.jabref.gui.util.BackgroundTask;
 import org.jabref.gui.util.TaskExecutor;
 import org.jabref.logic.bibtex.BibEntryWriter;
@@ -33,7 +31,6 @@ import org.jabref.logic.util.OS;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibEntryTypesManager;
-import org.jabref.model.entry.LinkedFile;
 import org.jabref.model.util.FileUpdateMonitor;
 import org.jabref.preferences.PreferencesService;
 
@@ -142,22 +139,8 @@ public class ImportEntriesViewModel extends AbstractViewModel {
      * @param entriesToImport subset of the entries contained in parserResult
      */
     public void importEntries(List<BibEntry> entriesToImport, boolean shouldDownloadFiles) {
-
         // Remember the selection in the dialog
         preferences.getFilePreferences().setDownloadLinkedFiles(shouldDownloadFiles);
-
-        if (shouldDownloadFiles) {
-            for (BibEntry bibEntry : entriesToImport) {
-                bibEntry.getFiles().stream().filter(LinkedFile::isOnlineLink).forEach(linkedFile ->
-                        new LinkedFileViewModel(
-                                linkedFile,
-                                bibEntry,
-                                databaseContext,
-                                taskExecutor,
-                                dialogService,
-                                preferences).download());
-            }
-        }
 
         new DatabaseMerger(preferences.getBibEntryPreferences().getKeywordSeparator()).mergeStrings(
                 databaseContext.getDatabase(),
@@ -181,7 +164,6 @@ public class ImportEntriesViewModel extends AbstractViewModel {
                 dialogService,
                 taskExecutor);
         importHandler.importEntriesWithDuplicateCheck(selectedDb.getValue(), entriesToImport);
-        dialogService.notify(Localization.lang("Number of entries successfully imported") + ": " + entriesToImport.size());
     }
 
     /**
@@ -200,62 +182,5 @@ public class ImportEntriesViewModel extends AbstractViewModel {
             }
         }
         return Optional.empty();
-    }
-
-    public void resolveDuplicate(BibEntry entry) {
-        // First, try to find duplicate in the existing library
-        Optional<BibEntry> other = new DuplicateCheck(entryTypesManager).containsDuplicate(databaseContext.getDatabase(), entry, databaseContext.getMode());
-        if (other.isPresent()) {
-            DuplicateResolverDialog dialog = new DuplicateResolverDialog(other.get(),
-                    entry, DuplicateResolverDialog.DuplicateResolverType.IMPORT_CHECK, databaseContext, stateManager, dialogService, preferences);
-
-            DuplicateResolverDialog.DuplicateResolverResult result = dialogService.showCustomDialogAndWait(dialog)
-                                                                                  .orElse(DuplicateResolverDialog.DuplicateResolverResult.BREAK);
-
-            if (result == DuplicateResolverDialog.DuplicateResolverResult.KEEP_LEFT) {
-                // TODO: Remove old entry. Or... add it to a list of entries
-                // to be deleted. We only delete
-                // it after Ok is clicked.
-                // entriesToDelete.add(other.get());
-            } else if (result == DuplicateResolverDialog.DuplicateResolverResult.KEEP_RIGHT) {
-                // Remove the entry from the import inspection dialog.
-                entries.remove(entry);
-            } else if (result == DuplicateResolverDialog.DuplicateResolverResult.KEEP_BOTH) {
-                // Do nothing.
-            } else if (result == DuplicateResolverDialog.DuplicateResolverResult.KEEP_MERGE) {
-                // TODO: Remove old entry. Or... add it to a list of entries
-                // to be deleted. We only delete
-                // it after Ok is clicked.
-                // entriesToDelete.add(other.get());
-
-                // Replace entry by merged entry
-                entries.add(dialog.getMergedEntry());
-                entries.remove(entry);
-            }
-            return;
-        }
-        // Second, check if the duplicate is of another entry in the import:
-        other = findInternalDuplicate(entry);
-        if (other.isPresent()) {
-            DuplicateResolverDialog diag = new DuplicateResolverDialog(entry,
-                    other.get(), DuplicateResolverDialog.DuplicateResolverType.DUPLICATE_SEARCH, databaseContext, stateManager, dialogService, preferences);
-
-            DuplicateResolverDialog.DuplicateResolverResult answer = dialogService.showCustomDialogAndWait(diag)
-                                                                                  .orElse(DuplicateResolverDialog.DuplicateResolverResult.BREAK);
-            if (answer == DuplicateResolverDialog.DuplicateResolverResult.KEEP_LEFT) {
-                // Remove other entry
-                entries.remove(other.get());
-            } else if (answer == DuplicateResolverDialog.DuplicateResolverResult.KEEP_RIGHT) {
-                // Remove entry
-                entries.remove(entry);
-            } else if (answer == DuplicateResolverDialog.DuplicateResolverResult.KEEP_BOTH) {
-                // Do nothing
-            } else if (answer == DuplicateResolverDialog.DuplicateResolverResult.KEEP_MERGE) {
-                // Replace both entries by merged entry
-                entries.add(diag.getMergedEntry());
-                entries.remove(entry);
-                entries.remove(other.get());
-            }
-        }
     }
 }
