b8d707960f131aeda942d4457df2901bee71b14c
maxisr <maxism29.mi@gmail.com>
2024-02-29 10:39:13 +0100

    doc: javadoc for PushToTeXworks (#3197)
    Co-Authored-By: JohannBiorck <85625348+johannbiorck@users.noreply.github.com>

--- src/main/java/org/jabref/gui/push/PushToTeXworks.java
+++ src/main/java/org/jabref/gui/push/PushToTeXworks.java
@@ -9,20 +9,49 @@ public class PushToTeXworks extends AbstractPushToApplication {
 
     public static final String NAME = PushToApplications.TEXWORKS;
 
+    /**
+     * Constructs a new {@code PushToTeXworks} instance.
+     *
+     * @param dialogService The dialog service for displaying messages to the user.
+     * @param preferencesService The service for accessing user preferences.
+     */
     public PushToTeXworks(DialogService dialogService, PreferencesService preferencesService) {
         super(dialogService, preferencesService);
     }
+
+    /**
+     * Gets the display name for the TeXworks push operation. This name is used 
+     * in the GUI to represent the push action to the user.
+     *
+     * @return The display name for the push operation.
+     */
 
     @Override
     public String getDisplayName() {
         return NAME;
     }
 
+    /**
+     * Gets the icon associated with the TeXworks application.
+     * TODO: replace the placerholer icon with the real one.
+     *
+     * @return The icon for the TeXworks application.
+     */
+
     @Override
     public JabRefIcon getApplicationIcon() {
         return IconTheme.JabRefIcons.DEFAULT_GROUP_ICON; // Temporary Icon that needs to be changed
     }
 
+
+     /**
+     * Constructs the command line arguments for pushing citations to TeXworks.
+     * The method formats the citation key and prefixes/suffixes as per user preferences
+     * before invoking TeXworks with the command to insert text.
+     *
+     * @param keyString The citation key to be pushed.
+     * @return An array of {@code String} containing the command line to execute.
+     */
     @Override
     protected String[] getCommandLine(String keyString) {
         return new String[] {commandPath, "--insert-text", "%s%s%s".formatted(getCitePrefix(), keyString, getCiteSuffix())};
