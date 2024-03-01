4173beb383dea2860d3ad325fd0e047d72df4056
Loay Ghreeb <loayahmed655@gmail.com>
2024-03-01 02:29:54 +0200

    Rewrite keywords field: used tags field

--- src/main/java/org/jabref/gui/fieldeditors/KeywordsEditor.java
+++ src/main/java/org/jabref/gui/fieldeditors/KeywordsEditor.java
@@ -1,41 +1,45 @@
 package org.jabref.gui.fieldeditors;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 import javax.swing.undo.UndoManager;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import org.jabref.gui.DialogService;
-import org.jabref.gui.autocompleter.AutoCompletionTextInputBinding;
 import org.jabref.gui.autocompleter.SuggestionProvider;
-import org.jabref.gui.fieldeditors.contextmenu.DefaultMenu;
+import org.jabref.gui.icon.IconTheme;
 import org.jabref.gui.util.ViewModelListCellFactory;
 import org.jabref.logic.integrity.FieldCheckers;
-import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.Keyword;
 import org.jabref.model.entry.field.Field;
 import org.jabref.preferences.PreferencesService;
 
 import com.airhacks.afterburner.views.ViewLoader;
+import com.dlsc.gemsfx.TagsField;
 import jakarta.inject.Inject;
 
 public class KeywordsEditor extends HBox implements FieldEditorFX {
 
-    @FXML private final KeywordsEditorViewModel viewModel;
-    @FXML private EditorTextField textField;
-    @FXML private ComboBox<Keyword> keywordsComboBox;
+    @FXML private TagsField<Keyword> keywordTagsField;
 
     @Inject private PreferencesService preferencesService;
     @Inject private DialogService dialogService;
     @Inject private UndoManager undoManager;
 
+    private final KeywordsEditorViewModel viewModel;
+
     public KeywordsEditor(Field field,
                           SuggestionProvider<?> suggestionProvider,
-                          FieldCheckers fieldCheckers,
-                          BibDatabaseContext databaseContext) {
+                          FieldCheckers fieldCheckers) {
 
         ViewLoader.view(this)
                   .root(this)
@@ -46,21 +50,41 @@ public class KeywordsEditor extends HBox implements FieldEditorFX {
                 suggestionProvider,
                 fieldCheckers,
                 preferencesService,
-                databaseContext,
-                undoManager,
-                dialogService);
+                undoManager);
 
-        textField.textProperty().bindBidirectional(viewModel.textProperty());
-        textField.initContextMenu(new DefaultMenu(textField));
+        keywordTagsField.setCellFactory(new ViewModelListCellFactory<Keyword>().withText(Keyword::toString));
+        keywordTagsField.setTagViewFactory(this::createTag);
 
-        new ViewModelListCellFactory<Keyword>().withText(Keyword::toString).install(keywordsComboBox);
+        keywordTagsField.setSuggestionProvider(request ->
+                suggestionProvider.getPossibleSuggestions().stream()
+                                  .filter(suggestion -> suggestion.toString()
+                                                                  .toLowerCase()
+                                                                  .contains(request.getUserText().toLowerCase()))
+                                  .map(suggestion -> new Keyword(suggestion.toString()))
+                                  .distinct()
+                                  .collect(Collectors.toList()));
 
-        AutoCompletionTextInputBinding<?> autoCompleter = AutoCompletionTextInputBinding.autoComplete(textField, viewModel::complete, viewModel.getAutoCompletionStrategy());
-        autoCompleter.setShowOnFocus(true);
+        keywordTagsField.setConverter(viewModel.getStringConverter());
+        keywordTagsField.setMatcher((keyword, searchText) -> keyword.get().toLowerCase().startsWith(searchText.toLowerCase()));
+        keywordTagsField.setComparator(Comparator.comparing(Keyword::get));
 
-        new EditorValidator(preferencesService).configureValidation(viewModel.getFieldValidator().getValidationStatus(), textField);
+        keywordTagsField.setNewItemProducer(searchText -> viewModel.getStringConverter().fromString(searchText));
 
-        keywordsComboBox.setItems(viewModel.getFilteredKeywords());
+        keywordTagsField.setPlaceholder(null);
+        keywordTagsField.setShowSearchIcon(false);
+        keywordTagsField.getEditor().getStyleClass().clear();
+        keywordTagsField.getEditor().getStyleClass().add("tags-field-editor");
+
+        Bindings.bindContentBidirectional(keywordTagsField.getTags(), viewModel.keywordListProperty());
+    }
+
+    private Node createTag(Keyword keyword) {
+        Label tagLabel = new Label();
+        tagLabel.setText(keyword.get());
+        tagLabel.setGraphic(IconTheme.JabRefIcons.REMOVE_TAGS.getGraphicNode());
+        tagLabel.getGraphic().setOnMouseClicked(event -> keywordTagsField.removeTags(keyword));
+        tagLabel.setContentDisplay(ContentDisplay.RIGHT);
+        return tagLabel;
     }
 
     public KeywordsEditorViewModel getViewModel() {
@@ -79,16 +103,11 @@ public class KeywordsEditor extends HBox implements FieldEditorFX {
 
     @Override
     public void requestFocus() {
-        textField.requestFocus();
+        keywordTagsField.requestFocus();
     }
 
     @Override
     public double getWeight() {
         return 2;
-    }
-
-    @FXML
-    public void addKeyword() {
-        viewModel.addKeyword(keywordsComboBox.getSelectionModel().getSelectedItem());
     }
 }
