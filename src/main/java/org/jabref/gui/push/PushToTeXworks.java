25eb6e0a10936b0a04365c8dec02d2c4e67e3dcd
Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
2024-03-04 16:47:23 +0100

    Modified tests, moved comments to interface

--- src/main/java/org/jabref/gui/push/PushToTeXworks.java
+++ src/main/java/org/jabref/gui/push/PushToTeXworks.java
@@ -19,47 +19,19 @@ public class PushToTeXworks extends AbstractPushToApplication {
         super(dialogService, preferencesService);
     }
 
-    /**
-     * Gets the display name for the TeXworks push operation. This name is used 
-     * in the GUI to represent the push action to the user.
-     *
-     * @return The display name for the push operation.
-     */
     @Override
     public String getDisplayName() {
         return NAME;
     }
 
-    /**
-     * Gets the icon associated with the TeXworks application.
-     * TODO: replace the placerholer icon with the real one.
-     *
-     * @return The icon for the TeXworks application.
-     */
     @Override
     public JabRefIcon getApplicationIcon() {
+        // TODO: replace the placeholder icon with the real one.
         return IconTheme.JabRefIcons.APPLICATION_GENERIC;
     }
 
-    /**
-    * Constructs the command line arguments for pushing citations to TeXworks.
-    * The method formats the citation key and prefixes/suffixes as per user preferences
-    * before invoking TeXworks with the command to insert text.
-    *
-    * @param keyString The citation key to be pushed.
-    * @return An array of {@code String} containing the command line to execute.
-    */
     @Override
     protected String[] getCommandLine(String keyString) {
         return new String[] {commandPath, "--insert-text", "%s%s%s".formatted(getCitePrefix(), keyString, getCiteSuffix())};
-    }
-
-    /**
-    * Gets the tooltip for the TeXworks push operation. The tooltip is used to display a short description of the push operation in the GUI.
-    *
-    * @param newCommandPath The new command path to be set.
-    */
-    public void setCommandPath(String newCommandPath) {
-        commandPath = newCommandPath;
     }
 }
--- src/test/java/org/jabref/gui/push/PushToTeXworksTest.java
+++ src/test/java/org/jabref/gui/push/PushToTeXworksTest.java
@@ -2,7 +2,6 @@ package org.jabref.gui.push;
 
 import java.util.Map;
 
-import javafx.beans.property.MapProperty;
 import javafx.beans.property.SimpleMapProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
@@ -14,80 +13,63 @@ import org.jabref.preferences.PreferencesService;
 import org.jabref.preferences.PushToApplicationPreferences;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class PushToTeXworksTest {
+
+    private static final String TEXWORKS_CLIENT_PATH = "/usr/bin/texworks";
+    private static final String DISPLAY_NAME = "TeXworks";
 
     private PushToTeXworks pushToTeXworks;
 
     @BeforeEach
     public void setup() {
-        // Mock the DialogService and PreferencesService
         DialogService dialogService = mock(DialogService.class, Answers.RETURNS_DEEP_STUBS);
         PreferencesService preferencesService = mock(PreferencesService.class);
         PushToApplicationPreferences pushToApplicationPreferences = mock(PushToApplicationPreferences.class);
-        ExternalApplicationsPreferences externalApplicationsPreferences = mock(ExternalApplicationsPreferences.class);
 
-        // Mock the return value for getCommandPaths()
-        String teXworksClientPath = "/usr/bin/texworks";
-        String displayName = "TeXworks";
-
-        Map<String, String> commandPaths = Map.of(displayName, teXworksClientPath);
+        // Mock the command path
+        Map<String, String> commandPaths = Map.of(DISPLAY_NAME, TEXWORKS_CLIENT_PATH);
         ObservableMap<String, String> observableCommandPaths = FXCollections.observableMap(commandPaths);
         when(pushToApplicationPreferences.getCommandPaths()).thenReturn(new SimpleMapProperty<>(observableCommandPaths));
         when(preferencesService.getPushToApplicationPreferences()).thenReturn(pushToApplicationPreferences);
 
         // Mock the return value for getCiteCommand()
+        ExternalApplicationsPreferences externalApplicationsPreferences = mock(ExternalApplicationsPreferences.class);
         CitationCommandString mockCiteCommand = mock(CitationCommandString.class);
         when(mockCiteCommand.prefix()).thenReturn("");
         when(mockCiteCommand.suffix()).thenReturn("");
         when(externalApplicationsPreferences.getCiteCommand()).thenReturn(mockCiteCommand);
-
-        // Mock the return value for getExternalApplicationsPreferences()
         when(preferencesService.getExternalApplicationsPreferences()).thenReturn(externalApplicationsPreferences);
 
         // Create a new instance of PushToTeXworks
         pushToTeXworks = new PushToTeXworks(dialogService, preferencesService);
-
-        // Verify that the command path is correctly set in the preferences
-        MapProperty<String, String> actualCommandPaths = pushToApplicationPreferences.getCommandPaths();
-        Map<String, String> actualMap = actualCommandPaths.get();
-        String actualPath = actualMap.get(displayName);
-        assertEquals(teXworksClientPath, actualPath);
     }
 
     /**
      * To verify that the PushToTeXworks class correctly returns its designated display name.
      * The display name is used to identify the application in the GUI.
-     *
-     * Method: getDisplayName()
-     *
      */
     @Test
     void displayName() {
-        assertEquals("TeXworks", pushToTeXworks.getDisplayName());
+        assertEquals(DISPLAY_NAME, pushToTeXworks.getDisplayName());
     }
 
     /**
      * To verify that the PushToTeXworks class correctly returns the command line for TeXworks.
      * The command line is used to execute the application from the command line.
-     *
-     * Method: getCommandLine()
-     *
      */
     @Test
     void getCommandLine() {
         String keyString = "TestKey";
-        String[] expectedCommand = new String[] {"/usr/bin/texworks", "--insert-text", keyString};
-
-        pushToTeXworks.setCommandPath("/usr/bin/texworks");
+        String[] expectedCommand = new String[] {null, "--insert-text", keyString}; // commandPath is only set in pushEntries
 
         String[] actualCommand = pushToTeXworks.getCommandLine(keyString);
 
@@ -95,24 +77,23 @@ class PushToTeXworksTest {
     }
 
     /**
-     * This test run the external application to push the keys to be cited
-     *
-     * Method: pushEntries()
-     *
-     * [Precondition]: TeXworks must run in the background
+     * Check for the actual command and path with path is run.
      */
-    @Disabled("Disabled as it needs running TeXworks in the background. Start TeXworks with &")
     @Test
     void pushEntries() {
-        pushToTeXworks.pushEntries(null, null, "key1,key2");
+        ProcessBuilder processBuilder = mock(ProcessBuilder.class);
+
+        String testKey = "TestKey";
+        String[] expectedCommand = new String[] {TEXWORKS_CLIENT_PATH, "--insert-text", testKey};
+
+        pushToTeXworks.pushEntries(null, null, testKey, processBuilder);
+
+        verify(processBuilder).command(expectedCommand);
     }
 
     /**
      * To verify that the PushToTeXworks class correctly returns the tooltip for TeXworks.
      * The tooltip is used to display a short description of the application in the GUI.
-     *
-     * Method: getTooltip()
-     *
      */
     @Test
     void getTooltip() {
