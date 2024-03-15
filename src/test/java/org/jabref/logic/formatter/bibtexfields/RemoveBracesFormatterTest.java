11457882bcd3b8ae576bff5b48b6d28866d5a46b
Carl Christian Snethlage <50491877+calixtus@users.noreply.github.com>
2024-03-15 15:36:24 +0100

    Convert to csvsource

--- src/test/java/org/jabref/logic/formatter/bibtexfields/RemoveBracesFormatterTest.java
+++ src/test/java/org/jabref/logic/formatter/bibtexfields/RemoveBracesFormatterTest.java
@@ -1,11 +1,8 @@
 package org.jabref.logic.formatter.bibtexfields;
 
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,52 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class RemoveBracesFormatterTest {
 
-    private RemoveBracesFormatter formatter = new RemoveBracesFormatter();
+    private final RemoveBracesFormatter formatter = new RemoveBracesFormatter();
 
     @ParameterizedTest
-    @MethodSource
+    @CsvSource({
+            "test, {test}", // formatRemovesSingleEnclosingBraces
+            "{test, {test", // formatKeepsUnmatchedBracesAtBeginning
+            "test}, test}", // formatKeepsUnmatchedBracesAtEnd
+            "t, t", // formatKeepsShortString
+            "'', {}", // formatRemovesBracesOnly
+            "test, {{test}}", // formatKeepsEmptyString
+            "test, {{{test}}}", // formatRemovesDoubleEnclosingBraces
+            "{A} and {B}, {A} and {B}", // formatRemovesTripleEnclosingBraces
+            "{A} and {B}, {{A} and {B}}", // formatKeepsNonMatchingBraces
+            "{A} and {B}}, {A} and {B}}", // formatRemovesOnlyMatchingBraces
+            "Vall{\\'e}e Poussin, {Vall{\\'e}e Poussin}", // formatDoesNotRemoveBracesInBrokenString
+            "Vall{\\'e}e Poussin, Vall{\\'e}e Poussin"
+    })
     public void format(String expected, String input) {
         assertEquals(expected, formatter.format(input));
-    }
-
-    private static Stream<Arguments> format() {
-        return Stream.of(
-                // formatRemovesSingleEnclosingBraces
-                Arguments.of("test", "{test}"),
-
-                // formatKeepsUnmatchedBracesAtBeginning
-                Arguments.of("{test", "{test"),
-
-                // formatKeepsUnmatchedBracesAtEnd
-                Arguments.of("test}", "test}"),
-
-                // formatKeepsShortString
-                Arguments.of("t", "t"),
-
-                // formatRemovesBracesOnly
-                Arguments.of("", "{}"),
-
-                // formatKeepsEmptyString
-                Arguments.of("", ""),
-
-                // formatRemovesDoubleEnclosingBraces
-                Arguments.of("test", "{{test}}"),
-
-                // formatRemovesTripleEnclosingBraces
-                Arguments.of("test", "{{{test}}}"),
-
-                // formatKeepsNonMatchingBraces
-                Arguments.of("{A} and {B}", "{A} and {B}"),
-
-                // formatRemovesOnlyMatchingBraces
-                Arguments.of("{A} and {B}", "{{A} and {B}}"),
-
-                // formatDoesNotRemoveBracesInBrokenString
-                Arguments.of("{A} and {B}}", "{A} and {B}}"),
-
-                Arguments.of("Vall{\\'e}e Poussin", "{Vall{\\'e}e Poussin}"),
-                Arguments.of("Vall{\\'e}e Poussin", "Vall{\\'e}e Poussin")
-        );
     }
 
     @Test
