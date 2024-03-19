3dc1f55755259d57d5f0d351dfce848d5448a3b6
subhramit <subhramit.bb@live.in>
2024-03-20 03:18:49 +0530

    Fix Objects import missing

--- src/main/java/org/jabref/logic/importer/fetcher/MathSciNet.java
+++ src/main/java/org/jabref/logic/importer/fetcher/MathSciNet.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
