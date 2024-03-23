201c054f8e0d7a6c019c3724fc293d0fa637ed7a
subhramit <subhramit.bb@live.in>
2024-03-24 01:11:41 +0530

    Add test

--- src/test/java/org/jabref/model/entry/identifier/DOITest.java
+++ src/test/java/org/jabref/model/entry/identifier/DOITest.java
@@ -316,4 +316,10 @@ public class DOITest {
     public void rejectNullDoiParameter() {
         assertThrows(NullPointerException.class, () -> new DOI(null));
     }
+
+    @Test
+    public void findDoiWithSpecialCharactersInText() {
+        assertEquals(Optional.of(new DOI("10.1175/1520-0493(2002)130<1913:EDAWPO>2.0.CO;2")),
+                DOI.findInText("https://doi.org/10.1175/1520-0493(2002)130%3C1913:EDAWPO%3E2.0.CO;2"));
+    }
 }
