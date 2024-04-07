1b211e84bdf1bf42522bfc3133900cadc062c445
subhramit <subhramit.bb@live.in>
2024-04-07 19:01:37 +0530

    Review changes-1

--- src/main/java/org/jabref/logic/exporter/EndnoteXmlExporter.java
+++ src/main/java/org/jabref/logic/exporter/EndnoteXmlExporter.java
@@ -8,15 +8,16 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
+import org.jabref.logic.importer.AuthorListParser;
 import org.jabref.logic.util.StandardFileType;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.Author;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.types.EntryType;
@@ -26,7 +27,6 @@ import org.jabref.model.entry.types.StandardEntryType;
 public class EndnoteXmlExporter extends Exporter {
 
     private static final String ENDNOTE_XML_VERSION = "20.1";
-    private static final String ENDNOTE_NAMESPACE_URI = "http://www.endnote.com/ns/ENdnx";
     private static final Map<EntryType, String> EXPORT_ITEM_TYPE = Map.ofEntries(
             Map.entry(StandardEntryType.Article, "Journal Article"),
             Map.entry(StandardEntryType.Book, "Book"),
@@ -51,8 +51,13 @@ public class EndnoteXmlExporter extends Exporter {
                                                                                                                                                                            .collect(Collectors.toList())
                                                                                                                                                                            .indexOf(entry.getValue()) + 1)));
 
+    private final XMLOutputFactory xmlOutputFactory;
+    private final AuthorListParser authorListParser;
+
     public EndnoteXmlExporter() {
         super("endnote", "EndNote XML", StandardFileType.XML);
+        this.xmlOutputFactory = XMLOutputFactory.newFactory();
+        this.authorListParser = new AuthorListParser();
     }
 
     @Override
@@ -66,8 +71,7 @@ public class EndnoteXmlExporter extends Exporter {
         }
 
         try (BufferedWriter writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8)) {
-            XMLOutputFactory xof = XMLOutputFactory.newFactory();
-            XMLStreamWriter xml = xof.createXMLStreamWriter(writer);
+            XMLStreamWriter xml = xmlOutputFactory.createXMLStreamWriter(writer);
             xml.writeStartDocument(StandardCharsets.UTF_8.name(), "1.0");
             xml.writeStartElement("xml");
             xml.writeStartElement("records");
@@ -91,12 +95,12 @@ public class EndnoteXmlExporter extends Exporter {
         // Write the necessary fields and elements
         writeField(xml, "database", "endnote.enl", Map.of("name", "My EndNote Library.enl", "path", "/path/to/My EndNote Library.enl"));
         writeField(xml, "source-app", "JabRef", Map.of("name", "JabRef", "version", ENDNOTE_XML_VERSION));
-        writeField(xml, "rec-number", String.valueOf(entry.getId()), null);
+        writeField(xml, "rec-number", entry.getId(), null);
 
         xml.writeStartElement("foreign-keys");
         xml.writeStartElement("key");
         xml.writeAttribute("app", "EN");
-        xml.writeCharacters(String.valueOf(entry.getId()));
+        xml.writeCharacters(entry.getId());
         xml.writeEndElement(); // key
         xml.writeEndElement(); // foreign-keys
 
@@ -110,7 +114,7 @@ public class EndnoteXmlExporter extends Exporter {
         writeField(xml, "volume", entry.getField(StandardField.VOLUME).orElse(""), null);
         writeField(xml, "number", entry.getField(StandardField.NUMBER).orElse(""), null);
         writeField(xml, "dates", null, Map.of(), entry.getField(StandardField.YEAR).orElse(""), "year");
-        writeField(xml, "publisher", entry.getField(StandardField.PUBLISHER).orElse(""), null);
+        writePublisher(entry, xml);
         writeField(xml, "isbn", entry.getField(StandardField.ISBN).orElse(""), null);
         writeField(xml, "abstract", entry.getField(StandardField.ABSTRACT).orElse(""), null);
         writeField(xml, "notes", entry.getField(StandardField.NOTE).orElse(""), null);
@@ -159,60 +163,47 @@ public class EndnoteXmlExporter extends Exporter {
     }
 
     private void writeContributors(BibEntry entry, XMLStreamWriter xml) throws XMLStreamException {
-        Optional<String> authors = entry.getField(StandardField.AUTHOR);
-        if (authors.isPresent()) {
-            xml.writeStartElement("contributors");
-            xml.writeStartElement("authors");
-            for (String author : authors.get().split("and")) {
-                xml.writeStartElement("author");
+        entry.getField(StandardField.AUTHOR).ifPresent(authors -> {
+            try {
+                xml.writeStartElement("contributors");
+                xml.writeStartElement("authors");
+                for (Author author : authorListParser.parse(authors).getAuthors()) {
+                    xml.writeStartElement("author");
+                    xml.writeStartElement("style");
+                    xml.writeAttribute("face", "normal");
+                    xml.writeAttribute("font", "default");
+                    xml.writeAttribute("size", "100%");
+                    xml.writeCharacters(author.getFamilyGiven(false));
+                    xml.writeEndElement(); // style
+                    xml.writeEndElement(); // author
+                }
+                xml.writeEndElement(); // authors
+                xml.writeEndElement(); // contributors
+            } catch (XMLStreamException e) {
+                throw new RuntimeException("Error writing contributors", e);
+            }
+        });
+    }
+
+    private void writePublisher(BibEntry entry, XMLStreamWriter xml) throws XMLStreamException {
+        String publisher = entry.getField(StandardField.PUBLISHER).orElse("");
+        String address = entry.getField(StandardField.ADDRESS).orElse("");
+        if (!publisher.isEmpty() || !address.isEmpty()) {
+            xml.writeStartElement("publisher");
+            if (!publisher.isEmpty()) {
+                writeField(xml, "publisher", publisher, null);
+            }
+            if (!address.isEmpty()) {
+                xml.writeStartElement("place");
                 xml.writeStartElement("style");
                 xml.writeAttribute("face", "normal");
                 xml.writeAttribute("font", "default");
                 xml.writeAttribute("size", "100%");
-                xml.writeCharacters(author.trim());
+                xml.writeCharacters(address);
                 xml.writeEndElement(); // style
-                xml.writeEndElement(); // author
+                xml.writeEndElement(); // place
             }
-            xml.writeEndElement(); // authors
-            xml.writeEndElement(); // contributors
+            xml.writeEndElement(); // publisher
         }
-    }
-
-    private void writeDates(BibEntry entry, XMLStreamWriter xml) throws XMLStreamException {
-        Optional<String> year = entry.getField(StandardField.YEAR);
-        if (year.isPresent()) {
-            xml.writeStartElement("dates");
-            xml.writeStartElement("year");
-            xml.writeCharacters(year.get());
-            xml.writeEndElement(); // year
-            xml.writeEndElement(); // dates
-        }
-    }
-
-    private void writeUrls(BibEntry entry, XMLStreamWriter xml) throws XMLStreamException {
-        xml.writeStartElement("urls");
-        entry.getField(StandardField.URL).ifPresent(url -> {
-            try {
-                xml.writeStartElement("web-urls");
-                xml.writeStartElement("url");
-                xml.writeCharacters(url);
-                xml.writeEndElement(); // url
-                xml.writeEndElement(); // web-urls
-            } catch (XMLStreamException e) {
-                // Ignore exception and continue
-            }
-        });
-        entry.getField(StandardField.FILE).ifPresent(file -> {
-            try {
-                xml.writeStartElement("file-urls");
-                xml.writeStartElement("url");
-                xml.writeCharacters(file);
-                xml.writeEndElement(); // url
-                xml.writeEndElement(); // file-urls
-            } catch (XMLStreamException e) {
-                // Ignore exception and continue
-            }
-        });
-        xml.writeEndElement(); // urls
     }
 }
--- src/main/java/org/jabref/logic/exporter/ExporterFactory.java
+++ src/main/java/org/jabref/logic/exporter/ExporterFactory.java
@@ -55,7 +55,6 @@ public class ExporterFactory {
         exporters.add(new TemplateExporter("MIS Quarterly", "misq", "misq", "misq", StandardFileType.RTF, layoutPreferences, saveOrder));
         exporters.add(new TemplateExporter("CSL YAML", "yaml", "yaml", null, StandardFileType.YAML, layoutPreferences, saveOrder, BlankLineBehaviour.DELETE_BLANKS));
         exporters.add(new TemplateExporter("Hayagriva YAML", "hayagrivayaml", "hayagrivayaml", null, StandardFileType.YAML, layoutPreferences, saveOrder, BlankLineBehaviour.DELETE_BLANKS));
-        exporters.add(new TemplateExporter("Endnote XML", "endnotexml", "Endnote XML", "endnotexml", StandardFileType.XML, layoutPreferences, saveOrder));
         exporters.add(new OpenOfficeDocumentCreator());
         exporters.add(new OpenDocumentSpreadsheetCreator());
         exporters.add(new MSBibExporter());
