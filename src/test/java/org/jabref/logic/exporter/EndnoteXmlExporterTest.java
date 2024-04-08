720d0acabcac795fbb43f9b5e67d741a6899d0bb
subhramit <subhramit.bb@live.in>
2024-04-09 05:16:29 +0530

    Fix ref-type in test

--- src/test/java/org/jabref/logic/exporter/EndnoteXmlExporterTest.java
+++ src/test/java/org/jabref/logic/exporter/EndnoteXmlExporterTest.java
@@ -63,7 +63,7 @@ public class EndnoteXmlExporterTest {
                 "        <key app=\"EN\">" + entry.getId() + "</key>",
                 "      </foreign-keys>",
                 "      <ref-type name=\"Journal Article\">",
-                "        <style face=\"normal\" font=\"default\" size=\"100%\">9</style>",
+                "        <style face=\"normal\" font=\"default\" size=\"100%\">15</style>",
                 "      </ref-type>",
                 "      <contributors>",
                 "        <authors>",
