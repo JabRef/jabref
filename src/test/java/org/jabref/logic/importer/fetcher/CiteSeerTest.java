b3e5c950f3c65a2fdb203ff06b294718d0e515fe
Oliver Kopp <kopp.dev@gmail.com>
2024-04-29 14:49:20 +0200

    Fix import

--- src/test/java/org/jabref/logic/importer/fetcher/CiteSeerTest.java
+++ src/test/java/org/jabref/logic/importer/fetcher/CiteSeerTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 @FetcherTest
 class CiteSeerTest {
