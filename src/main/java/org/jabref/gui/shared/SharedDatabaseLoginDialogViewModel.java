4cf199b445e0c8967e6fff61abcd31340286d273
Siedlerchr <siedlerkiller@gmail.com>
2024-05-19 11:51:48 +0200

    checkstyle

--- src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogViewModel.java
+++ src/main/java/org/jabref/gui/shared/SharedDatabaseLoginDialogViewModel.java
@@ -13,7 +13,6 @@ import javax.swing.undo.UndoManager;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
