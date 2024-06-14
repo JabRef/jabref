e7cb925b60b19a32104d229096a8064e55093fa2
Oliver Kopp <kopp.dev@gmail.com>
2024-06-15 00:45:22 +0200

    Fix imports

--- src/main/java/org/jabref/gui/mergeentries/DiffHighlighting.java
+++ src/main/java/org/jabref/gui/mergeentries/DiffHighlighting.java
@@ -1,9 +1,7 @@
 package org.jabref.gui.mergeentries;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javafx.scene.text.Text;
 
