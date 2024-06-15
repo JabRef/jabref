804e851f646689c5f257babd7b72da481dfe1d16
Siedlerchr <siedlerkiller@gmail.com>
2024-06-15 15:35:53 +0200

    fix autosave thread

--- src/main/java/org/jabref/gui/fieldeditors/FieldEditorFX.java
+++ src/main/java/org/jabref/gui/fieldeditors/FieldEditorFX.java
@@ -3,11 +3,13 @@ package org.jabref.gui.fieldeditors;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.application.Platform;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Parent;
 import javafx.scene.control.TextInputControl;
 
 import org.jabref.gui.util.ControlHelper;
+import org.jabref.gui.util.DefaultTaskExecutor;
 import org.jabref.model.entry.BibEntry;
 
 import com.github.difflib.DiffUtils;
@@ -27,71 +29,78 @@ public interface FieldEditorFX {
         Logger logger = LoggerFactory.getLogger(FieldEditorFX.class);
 
         EasyBind.subscribe(viewModelTextProperty, newText -> {
-            int lastCaretPosition = textInputControl.getCaretPosition();
-            logger.trace("Caret at position {}", lastCaretPosition);
-            String oldText = textInputControl.getText();
-            textInputControl.setText(newText);
-            logger.trace("listener triggered: '{}' -> '{}'", oldText, newText);
-            if (oldText == null) {
-                logger.trace("Empty field");
-                return;
-            }
-            if (newText == null) {
-                logger.trace("Field cleared");
-                return;
-            }
-            if (oldText.equals(newText)) {
-                logger.trace("No change, returned.");
-                return;
-            }
-            logger.trace("Trying to adapt...");
-            // This is a special case when the text is set to a new value
-            // In this case, we want to adjust the caret position
-            List<String> oldValueCharacters = Arrays.asList(oldText.split(""));
-            List<String> newValueCharacters = Arrays.asList(newText.split(""));
-            List<AbstractDelta<String>> deltaList = DiffUtils.diff(oldValueCharacters, newValueCharacters).getDeltas();
-            logger.trace("Deltas: {}", deltaList);
-            AbstractDelta<String> lastDelta = null;
-            for (AbstractDelta<String> delta : deltaList) {
-                if (delta.getSource().getPosition() > lastCaretPosition) {
-                    break;
-                }
-                lastDelta = delta;
-            }
-            if (lastDelta == null) {
-                // Change happened after current caret position
-                // Thus, simply restore the old position
-                textInputControl.positionCaret(lastCaretPosition);
+            // This might be triggered by save actions from a background thread, so we need to check if we are in the FX thread
+            if (Platform.isFxApplicationThread()) {
+                setTextAndUpdateCaretPosition(textInputControl, newText, logger);
             } else {
-                logger.trace("Last Delta: {}", lastDelta);
-                logger.trace("Last Delta source: {}", lastDelta.getSource());
-                logger.trace("Last Delta target: {}", lastDelta.getTarget());
-                int offset = lastDelta.getTarget().getPosition() - lastDelta.getSource().getPosition();
-                logger.trace("Offset before patching: {}", offset);
-
-                switch (lastDelta.getType()) {
-                    case DELETE:
-                        offset -= lastDelta.getSource().size();
-                        break;
-                    case INSERT:
-                        offset += lastDelta.getTarget().size();
-                        break;
-                    case CHANGE:
-                        offset += lastDelta.getTarget().size() - lastDelta.getSource().size();
-                        break;
-                    default:
-                        break;
-                }
-                logger.trace("Offset after patching: {}", offset);
-
-                int newCaretPosition = lastCaretPosition + offset;
-                textInputControl.positionCaret(newCaretPosition);
-                logger.trace("newCaretPosition: {}", newCaretPosition);
+                DefaultTaskExecutor.runInJavaFXThread(() -> setTextAndUpdateCaretPosition(textInputControl, newText, logger));
             }
         });
-        EasyBind.subscribe(textInputControl.textProperty(), newText -> {
-            viewModelTextProperty.set(newText);
-        });
+        EasyBind.subscribe(textInputControl.textProperty(), viewModelTextProperty::set);
+    }
+
+    private void setTextAndUpdateCaretPosition(TextInputControl textInputControl, String newText, Logger logger) {
+        int lastCaretPosition = textInputControl.getCaretPosition();
+        logger.trace("Caret at position {}", lastCaretPosition);
+        String oldText = textInputControl.getText();
+        textInputControl.setText(newText);
+        logger.trace("listener triggered: '{}' -> '{}'", oldText, newText);
+        if (oldText == null) {
+            logger.trace("Empty field");
+            return;
+        }
+        if (newText == null) {
+            logger.trace("Field cleared");
+            return;
+        }
+        if (oldText.equals(newText)) {
+            logger.trace("No change, returned.");
+            return;
+        }
+        logger.trace("Trying to adapt...");
+        // This is a special case when the text is set to a new value
+        // In this case, we want to adjust the caret position
+        List<String> oldValueCharacters = Arrays.asList(oldText.split(""));
+        List<String> newValueCharacters = Arrays.asList(newText.split(""));
+        List<AbstractDelta<String>> deltaList = DiffUtils.diff(oldValueCharacters, newValueCharacters).getDeltas();
+        logger.trace("Deltas: {}", deltaList);
+        AbstractDelta<String> lastDelta = null;
+        for (AbstractDelta<String> delta : deltaList) {
+            if (delta.getSource().getPosition() > lastCaretPosition) {
+                break;
+            }
+            lastDelta = delta;
+        }
+        if (lastDelta == null) {
+            // Change happened after current caret position
+            // Thus, simply restore the old position
+            textInputControl.positionCaret(lastCaretPosition);
+        } else {
+            logger.trace("Last Delta: {}", lastDelta);
+            logger.trace("Last Delta source: {}", lastDelta.getSource());
+            logger.trace("Last Delta target: {}", lastDelta.getTarget());
+            int offset = lastDelta.getTarget().getPosition() - lastDelta.getSource().getPosition();
+            logger.trace("Offset before patching: {}", offset);
+
+            switch (lastDelta.getType()) {
+                case DELETE:
+                    offset -= lastDelta.getSource().size();
+                    break;
+                case INSERT:
+                    offset += lastDelta.getTarget().size();
+                    break;
+                case CHANGE:
+                    offset += lastDelta.getTarget().size() - lastDelta.getSource().size();
+                    break;
+                default:
+                    break;
+            }
+            logger.trace("Offset after patching: {}", offset);
+
+            int newCaretPosition = lastCaretPosition + offset;
+            textInputControl.positionCaret(newCaretPosition);
+            logger.trace("newCaretPosition: {}", newCaretPosition);
+        }
     }
 
     Parent getNode();
