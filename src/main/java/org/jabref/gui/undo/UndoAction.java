765e6435bfbc43b1dfefc0d5110b12e304baa982
Oliver Kopp <kopp.dev@gmail.com>
2024-07-08 09:12:42 +0200

    Inline UndoRedoAction

--- src/main/java/org/jabref/gui/undo/UndoAction.java
+++ src/main/java/org/jabref/gui/undo/UndoAction.java
@@ -7,11 +7,24 @@ import javax.swing.undo.CannotUndoException;
 import org.jabref.gui.DialogService;
 import org.jabref.gui.LibraryTab;
 import org.jabref.gui.StateManager;
+import org.jabref.gui.actions.ActionHelper;
+import org.jabref.gui.actions.SimpleCommand;
 import org.jabref.logic.l10n.Localization;
 
-public class UndoAction extends UndoRedoAction {
+/**
+ * @implNote See also {@link RedoAction}
+ */
+public class UndoAction extends SimpleCommand {
+    private final Supplier<LibraryTab> tabSupplier;
+    private final DialogService dialogService;
+
     public UndoAction(Supplier<LibraryTab> tabSupplier, DialogService dialogService, StateManager stateManager) {
-        super(tabSupplier, dialogService, stateManager);
+        this.tabSupplier = tabSupplier;
+        this.dialogService = dialogService;
+
+        // TODO: Rework the UndoManager to something like the following, if it had a property.
+        //  this.executable.bind(frame.getCurrentBasePanel().getUndoManager().canUndo())
+        this.executable.bind(ActionHelper.needsDatabase(stateManager));
     }
 
     @Override
