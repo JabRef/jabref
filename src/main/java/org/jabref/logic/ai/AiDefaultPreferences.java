861da126c77f4f66cc397d2c8bb8980479748cd1
Oliver Kopp <kopp.dev@gmail.com>
2024-07-10 17:41:26 +0200

    Fix issue list index issue of langchain4j

--- src/main/java/org/jabref/logic/ai/AiDefaultPreferences.java
+++ src/main/java/org/jabref/logic/ai/AiDefaultPreferences.java
@@ -9,7 +9,7 @@ public class AiDefaultPreferences {
     public static final AiPreferences.EmbeddingModel EMBEDDING_MODEL = AiPreferences.EmbeddingModel.ALL_MINLM_l6_V2;
     public static final String SYSTEM_MESSAGE = "You are an AI assistant that analyses research papers.";
     public static final double TEMPERATURE = 0.7;
-    public static final int MESSAGE_WINDOW_SIZE = 10;
+    public static final int MESSAGE_WINDOW_SIZE = 1_000; // "2 + 2 = 4, 3 + 3 = 6" has size 20. Source: {@link dev.langchain4j.memory.chat.TokenWindowChatMemoryTest#should_evict_multiple_orphan_ToolExecutionResultMessages_when_evicting_AiMessage_with_ToolExecutionRequests_when_SystemMessage_is_present}
     public static final int DOCUMENT_SPLITTER_CHUNK_SIZE = 300;
     public static final int DOCUMENT_SPLITTER_OVERLAP = 100;
     public static final int RAG_MAX_RESULTS_COUNT = 10;
