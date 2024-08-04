db3cddca2df25a7913d316b8a5b7b590d4c1ed66
Oliver Kopp <kopp.dev@gmail.com>
2024-08-04 09:18:05 +0200

    Remove not required comments

--- src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
@@ -454,11 +454,8 @@ public class CitaviXmlImporter extends Importer implements Parser {
         }
 
         InputStream stream = Files.newInputStream(newFile, StandardOpenOption.READ);
-
-        // check and delete the utf-8 BOM bytes
         InputStream newStream = checkForUtf8BOMAndDiscardIfAny(stream);
 
-        // clean up the temp file
         Files.delete(newFile);
 
         return new BufferedReader(new InputStreamReader(newStream, StandardCharsets.UTF_8));
