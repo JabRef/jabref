83dcfab6771655f0256ff88b19ea44deb77baa07
Siedlerchr <siedlerkiller@gmail.com>
2024-08-07 22:29:35 +0200

    fix checkstyle

--- src/main/java/org/jabref/gui/preferences/general/GeneralTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/general/GeneralTabViewModel.java
@@ -335,7 +335,6 @@ public class GeneralTabViewModel implements PreferenceTabViewModel {
     }
 
     public void importCSSFile() {
-
         String fileDir = customPathToThemeProperty.getValue().isEmpty() ? preferences.getInternalPreferences().getLastPreferencesExportPath().toString()
                 : customPathToThemeProperty.getValue();
 
