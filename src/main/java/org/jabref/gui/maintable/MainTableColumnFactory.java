bd13cb9d7af6ec88c03b55a6511f646936d17a65
Loay Ghreeb <loayahmed655@gmail.com>
2024-08-16 15:48:42 +0300

    JavaDoc

--- src/main/java/org/jabref/gui/maintable/MainTableColumnFactory.java
+++ src/main/java/org/jabref/gui/maintable/MainTableColumnFactory.java
@@ -145,14 +145,15 @@ public class MainTableColumnFactory {
         column.setMaxWidth(width);
     }
 
+    /**
+     * Creates a column for the match category.
+     * <p>This column is always hidden but is used for sorting the table
+     * in the floating mode. The order of the {@link MatchCategory} enum constants
+     * determines the sorting order.</p>
+     */
     private TableColumn<BibEntryTableViewModel, MatchCategory> createMatchCategoryColumn(MainTableColumnModel columnModel) {
         TableColumn<BibEntryTableViewModel, MatchCategory> column = new MainTableColumn<>(columnModel);
-        Node header = new Text();
-        header.getStyleClass().add("mainTable-header");
-        Tooltip.install(header, new Tooltip(MainTableColumnModel.Type.MATCH_CATEGORY.getDisplayName()));
-        column.setGraphic(header);
         column.setCellValueFactory(cellData -> cellData.getValue().matchCategory());
-        new ValueTableCellFactory<BibEntryTableViewModel, MatchCategory>().withText(String::valueOf).install(column);
         column.setSortable(true);
         column.setSortType(TableColumn.SortType.ASCENDING);
         column.setVisible(false);
