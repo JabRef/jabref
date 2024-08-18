ccb53889a6e1d6a7eced2046a5d73cc82671cfb3
psyren99 <jetpsyren99@gmail.com>
2024-08-18 19:24:01 -0400

    imports

--- src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/CitaviXmlImporter.java
@@ -17,11 +17,7 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Objects;
-import java.util.StringJoiner;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
