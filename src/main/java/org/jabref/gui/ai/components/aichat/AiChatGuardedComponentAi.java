120c8c2da021b1eef354f65148dc32a62967085e
InAnYan <ruslanpopov1512@gmail.com>
2024-08-23 18:23:15 +0300

    Introduce EmbeddingModelGuardedComponent

--- src/main/java/org/jabref/gui/ai/components/aichat/AiChatGuardedComponentAi.java
+++ src/main/java/org/jabref/gui/ai/components/aichat/AiChatGuardedComponentAi.java
@@ -4,21 +4,16 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 
 import org.jabref.gui.DialogService;
-import org.jabref.gui.ai.components.privacynotice.AiPrivacyNoticeGuardedComponent;
-import org.jabref.gui.ai.components.util.errorstate.ErrorStateComponent;
+import org.jabref.gui.ai.components.util.EmbeddingModelGuardedComponent;
 import org.jabref.gui.util.TaskExecutor;
-import org.jabref.gui.util.UiTaskExecutor;
 import org.jabref.logic.ai.AiService;
-import org.jabref.logic.ai.models.JabRefEmbeddingModel;
-import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.preferences.FilePreferences;
 
-import com.google.common.eventbus.Subscribe;
 import dev.langchain4j.data.message.ChatMessage;
 
-public class AiChatGuardedComponentAi extends AiPrivacyNoticeGuardedComponent {
+public class AiChatGuardedComponentAi extends EmbeddingModelGuardedComponent {
     private final String name;
     private final ObservableList<ChatMessage> chatHistory;
     private final ObservableList<BibEntry> entries;
@@ -36,7 +31,7 @@ public class AiChatGuardedComponentAi extends AiPrivacyNoticeGuardedComponent {
                                     BibDatabaseContext bibDatabaseContext,
                                     TaskExecutor taskExecutor
     ) {
-        super(aiService.getPreferences(), filePreferences, dialogService);
+        super(aiService, filePreferences, dialogService);
 
         this.name = name;
         this.chatHistory = chatHistory;
@@ -50,36 +45,7 @@ public class AiChatGuardedComponentAi extends AiPrivacyNoticeGuardedComponent {
     }
 
     @Override
-    protected Node showGuardedContent() {
-        if (!aiService.getEmbeddingModel().isPresent()) {
-            if (aiService.getEmbeddingModel().hadErrorWhileBuildingModel()) {
-                return showErrorWhileBuildingEmbeddingModel();
-            } else {
-                return showBuildingEmbeddingModel();
-            }
-        } else {
-            return showAiChat();
-        }
-    }
-
-    private Node showErrorWhileBuildingEmbeddingModel() {
-        return ErrorStateComponent.withTextAreaAndButton(
-                Localization.lang("Unable to chat"),
-                Localization.lang("An error occurred while building the embedding model"),
-                aiService.getEmbeddingModel().getErrorWhileBuildingModel(),
-                Localization.lang("Rebuild"),
-                () -> aiService.getEmbeddingModel().startRebuildingTask()
-        );
-    }
-
-    public Node showBuildingEmbeddingModel() {
-        return ErrorStateComponent.withSpinner(
-                Localization.lang("Downloading..."),
-                Localization.lang("Downloading embedding model... Afterward, you will be able to chat with your files.")
-        );
-    }
-
-    private Node showAiChat() {
+    protected Node showEmbeddingModelGuardedContent() {
         return new AiChatComponent(
                 aiService,
                 name,
@@ -89,15 +55,5 @@ public class AiChatGuardedComponentAi extends AiPrivacyNoticeGuardedComponent {
                 dialogService,
                 taskExecutor
         );
-    }
-
-    @Subscribe
-    public void listen(JabRefEmbeddingModel.EmbeddingModelBuiltEvent event) {
-        UiTaskExecutor.runInJavaFXThread(AiChatGuardedComponentAi.this::rebuildUi);
-    }
-
-    @Subscribe
-    public void listen(JabRefEmbeddingModel.EmbeddingModelBuildingErrorEvent event) {
-        UiTaskExecutor.runInJavaFXThread(AiChatGuardedComponentAi.this::rebuildUi);
     }
 }
