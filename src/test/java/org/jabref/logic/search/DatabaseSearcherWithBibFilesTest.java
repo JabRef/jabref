12ff246dc47d07ac5ffca167726f18e3e6b5a9f9
Loay Ghreeb <loayahmed655@gmail.com>
2024-08-29 18:28:50 +0300

    DatabaseSearcherWithBibFilesTest

--- src/test/java/org/jabref/logic/search/DatabaseSearcherWithBibFilesTest.java
+++ src/test/java/org/jabref/logic/search/DatabaseSearcherWithBibFilesTest.java
@@ -7,12 +7,10 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import org.jabref.gui.StateManager;
 import org.jabref.logic.importer.ImportFormatPreferences;
 import org.jabref.logic.importer.ParserResult;
 import org.jabref.logic.importer.fileformat.BibtexImporter;
 import org.jabref.logic.util.StandardFileType;
-import org.jabref.model.database.BibDatabase;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.LinkedFile;
@@ -22,9 +20,7 @@ import org.jabref.model.search.SearchFlags;
 import org.jabref.model.search.SearchQuery;
 import org.jabref.model.util.DummyFileUpdateMonitor;
 import org.jabref.preferences.FilePreferences;
-import org.jabref.preferences.PreferencesService;
 
-import com.airhacks.afterburner.injection.Injector;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -38,66 +34,53 @@ import static org.mockito.Mockito.when;
 
 class DatabaseSearcherWithBibFilesTest {
 
-    private static BibEntry titleSentenceCased = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry TITLE_SENTENCE_CASED = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("title-sentence-cased")
             .withField(StandardField.TITLE, "Title Sentence Cased");
-    private static BibEntry titleMixedCased = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry TITLE_MIXED_CASED = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("title-mixed-cased")
             .withField(StandardField.TITLE, "TiTle MiXed CaSed");
-    private static BibEntry titleUpperCased = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry TITLE_UPPER_CASED = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("title-upper-cased")
             .withField(StandardField.TITLE, "TITLE UPPER CASED");
 
-    private static BibEntry mininimalSentenceCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_SENTENCE_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-sentence-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-sentence-case.pdf", StandardFileType.PDF.getName())));
-    private static BibEntry minimalAllUpperCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_ALL_UPPER_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-all-upper-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-all-upper-case.pdf", StandardFileType.PDF.getName())));
-    private static BibEntry minimalMixedCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_MIXED_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-mixed-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-mixed-case.pdf", StandardFileType.PDF.getName())));
-    private static BibEntry minimalNoteSentenceCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_NOTE_SENTENCE_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-note-sentence-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-note-sentence-case.pdf", StandardFileType.PDF.getName())));
-    private static BibEntry minimalNoteAllUpperCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_NOTE_ALL_UPPER_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-note-all-upper-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-note-all-upper-case.pdf", StandardFileType.PDF.getName())));
-    private static BibEntry minimalNoteMixedCase = new BibEntry(StandardEntryType.Misc)
+    private static final BibEntry MINIMAL_NOTE_MIXED_CASE = new BibEntry(StandardEntryType.Misc)
             .withCitationKey("minimal-note-mixed-case")
             .withFiles(Collections.singletonList(new LinkedFile("", "minimal-note-mixed-case.pdf", StandardFileType.PDF.getName())));
 
-
+    private static final FilePreferences FILE_PREFERENCES = mock(FilePreferences.class);
     @TempDir
     private Path indexDir;
-    private StateManager stateManager;
-    private PreferencesService preferencesService;
 
-    private BibDatabase initializeDatabaseFromPath(String testFile) throws Exception {
+    private BibDatabaseContext initializeDatabaseFromPath(String testFile) throws Exception {
         return initializeDatabaseFromPath(Path.of(Objects.requireNonNull(DatabaseSearcherWithBibFilesTest.class.getResource(testFile)).toURI()));
     }
 
-    private BibDatabase initializeDatabaseFromPath(Path testFile) throws Exception {
+    private BibDatabaseContext initializeDatabaseFromPath(Path testFile) throws Exception {
         ParserResult result = new BibtexImporter(mock(ImportFormatPreferences.class, Answers.RETURNS_DEEP_STUBS), new DummyFileUpdateMonitor()).importDatabase(testFile);
-        BibDatabase database = result.getDatabase();
+        BibDatabaseContext databaseContext = result.getDatabaseContext();
 
         BibDatabaseContext context = mock(BibDatabaseContext.class);
         when(context.getFileDirectories(Mockito.any())).thenReturn(List.of(testFile.getParent()));
         when(context.getFulltextIndexPath()).thenReturn(indexDir);
-        when(context.getDatabase()).thenReturn(database);
-        when(context.getEntries()).thenReturn(database.getEntries());
+        when(FILE_PREFERENCES.shouldFulltextIndexLinkedFiles()).thenReturn(true);
 
-        // Required because of {@Link org.jabref.model.search.rules.FullTextSearchRule.FullTextSearchRule}
-        stateManager = new StateManager();
-        stateManager.setActiveDatabase(context);
-        Injector.setModelOrService(StateManager.class, stateManager);
-
-        preferencesService = mock(PreferencesService.class);
-        FilePreferences filePreferences = mock(FilePreferences.class);
-        when(preferencesService.getFilePreferences()).thenReturn(filePreferences);
-        Injector.setModelOrService(PreferencesService.class, preferencesService);
-
-        return database;
+        return databaseContext;
     }
 
     private static Stream<Arguments> searchLibrary() {
@@ -108,10 +91,10 @@ class DatabaseSearcherWithBibFilesTest {
                 // test-library-title-casing
 
                 Arguments.of(List.of(), "test-library-title-casing.bib", "NotExisting", EnumSet.noneOf(SearchFlags.class)),
-                Arguments.of(List.of(titleSentenceCased, titleMixedCased, titleUpperCased), "test-library-title-casing.bib", "Title", EnumSet.noneOf(SearchFlags.class)),
+                Arguments.of(List.of(TITLE_SENTENCE_CASED, TITLE_MIXED_CASED, TITLE_UPPER_CASED), "test-library-title-casing.bib", "Title", EnumSet.noneOf(SearchFlags.class)),
 
                 Arguments.of(List.of(), "test-library-title-casing.bib", "title=NotExisting", EnumSet.noneOf(SearchFlags.class)),
-                Arguments.of(List.of(titleSentenceCased, titleMixedCased, titleUpperCased), "test-library-title-casing.bib", "title=Title", EnumSet.noneOf(SearchFlags.class)),
+                Arguments.of(List.of(TITLE_SENTENCE_CASED, TITLE_MIXED_CASED, TITLE_UPPER_CASED), "test-library-title-casing.bib", "title=Title", EnumSet.noneOf(SearchFlags.class)),
 
                 // Arguments.of(List.of(), "test-library-title-casing.bib", "title=TiTLE", EnumSet.of(SearchFlags.CASE_SENSITIVE)),
                 // Arguments.of(List.of(titleSentenceCased), "test-library-title-casing.bib", "title=Title", EnumSet.of(SearchFlags.CASE_SENSITIVE)),
@@ -123,7 +106,7 @@ class DatabaseSearcherWithBibFilesTest {
                 // Arguments.of(List.of(titleMixedCased), "test-library-title-casing.bib", "title=TiTle", EnumSet.of(SearchFlags.CASE_SENSITIVE)),
 
                 Arguments.of(List.of(), "test-library-title-casing.bib", "[Y]", EnumSet.of(SearchFlags.REGULAR_EXPRESSION)),
-                Arguments.of(List.of(titleUpperCased), "test-library-title-casing.bib", "[U]", EnumSet.of(SearchFlags.REGULAR_EXPRESSION)),
+                Arguments.of(List.of(TITLE_UPPER_CASED), "test-library-title-casing.bib", "[U]", EnumSet.of(SearchFlags.REGULAR_EXPRESSION)),
 
                 // Word boundaries
                 // Arguments.of(List.of(), "test-library-title-casing.bib", "\\bTit\\b", EnumSet.of(SearchFlags.REGULAR_EXPRESSION, SearchFlags.CASE_SENSITIVE)),
@@ -133,8 +116,8 @@ class DatabaseSearcherWithBibFilesTest {
 
                 // Arguments.of(List.of(), "test-library-with-attached-files.bib", "This is a test.", EnumSet.of(SearchFlags.FULLTEXT, SearchFlags.CASE_SENSITIVE)),
 
-                Arguments.of(List.of(mininimalSentenceCase, minimalAllUpperCase, minimalMixedCase), "test-library-with-attached-files.bib", "This is a short sentence, comma included.", EnumSet.of(SearchFlags.FULLTEXT)),
-                Arguments.of(List.of(mininimalSentenceCase, minimalAllUpperCase, minimalMixedCase), "test-library-with-attached-files.bib", "comma", EnumSet.of(SearchFlags.FULLTEXT)),
+                Arguments.of(List.of(MINIMAL_SENTENCE_CASE, MINIMAL_ALL_UPPER_CASE, MINIMAL_MIXED_CASE), "test-library-with-attached-files.bib", "This is a short sentence, comma included.", EnumSet.of(SearchFlags.FULLTEXT)),
+                Arguments.of(List.of(MINIMAL_SENTENCE_CASE, MINIMAL_ALL_UPPER_CASE, MINIMAL_MIXED_CASE), "test-library-with-attached-files.bib", "comma", EnumSet.of(SearchFlags.FULLTEXT)),
                 // TODO: PDF search does not support case sensitive search (yet)
                 // Arguments.of(List.of(minimalAllUpperCase, minimalMixedCase), "test-library-with-attached-files.bib", "THIS", EnumSet.of(SearchFlags.FULLTEXT, SearchFlags.CASE_SENSITIVE)),
                 // Arguments.of(List.of(minimalAllUpperCase), "test-library-with-attached-files.bib", "THIS is a short sentence, comma included.", EnumSet.of(SearchFlags.FULLTEXT, SearchFlags.CASE_SENSITIVE)),
@@ -143,8 +126,8 @@ class DatabaseSearcherWithBibFilesTest {
 
                 Arguments.of(List.of(), "test-library-with-attached-files.bib", "NotExisting", EnumSet.of(SearchFlags.FULLTEXT)),
 
-                Arguments.of(List.of(minimalNoteSentenceCase, minimalNoteAllUpperCase, minimalNoteMixedCase), "test-library-with-attached-files.bib", "world", EnumSet.of(SearchFlags.FULLTEXT)),
-                Arguments.of(List.of(minimalNoteSentenceCase, minimalNoteAllUpperCase, minimalNoteMixedCase), "test-library-with-attached-files.bib", "Hello World", EnumSet.of(SearchFlags.FULLTEXT))
+                Arguments.of(List.of(MINIMAL_NOTE_SENTENCE_CASE, MINIMAL_NOTE_ALL_UPPER_CASE, MINIMAL_NOTE_MIXED_CASE), "test-library-with-attached-files.bib", "world", EnumSet.of(SearchFlags.FULLTEXT)),
+                Arguments.of(List.of(MINIMAL_NOTE_SENTENCE_CASE, MINIMAL_NOTE_ALL_UPPER_CASE, MINIMAL_NOTE_MIXED_CASE), "test-library-with-attached-files.bib", "Hello World", EnumSet.of(SearchFlags.FULLTEXT))
                 // TODO: PDF search does not support case sensitive search (yet)
                 // Arguments.of(List.of(minimalNoteAllUpperCase), "test-library-with-attached-files.bib", "HELLO WORLD", EnumSet.of(SearchFlags.FULLTEXT, SearchFlags.CASE_SENSITIVE)),
                 // Arguments.of(List.of(), "test-library-with-attached-files.bib", "NotExisting", EnumSet.of(SearchFlags.FULLTEXT, SearchFlags.CASE_SENSITIVE))
@@ -154,8 +137,8 @@ class DatabaseSearcherWithBibFilesTest {
     @ParameterizedTest(name = "{index} => query={2}, searchFlags={3}, testFile={1}, expected={0}")
     @MethodSource
     void searchLibrary(List<BibEntry> expected, String testFile, String query, EnumSet<SearchFlags> searchFlags) throws Exception {
-        BibDatabase database = initializeDatabaseFromPath(testFile);
-        List<BibEntry> matches = new DatabaseSearcher(new SearchQuery(query, searchFlags), database).getMatches();
+        BibDatabaseContext databaseContext = initializeDatabaseFromPath(testFile);
+        List<BibEntry> matches = new DatabaseSearcher(new SearchQuery(query, searchFlags), databaseContext, FILE_PREFERENCES).getMatches();
         assertEquals(expected, matches);
     }
 }
