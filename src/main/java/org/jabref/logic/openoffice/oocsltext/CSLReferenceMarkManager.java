bf406d9945eaa5f90ef5c7a818f00c918f623d6a
subhramit <subhramit.bb@live.in>
2024-09-07 04:12:26 +0530

    Change log level (non-breaking)

--- src/main/java/org/jabref/logic/openoffice/oocsltext/CSLReferenceMarkManager.java
+++ src/main/java/org/jabref/logic/openoffice/oocsltext/CSLReferenceMarkManager.java
@@ -176,7 +176,7 @@ public class CSLReferenceMarkManager {
                     marksInOrder.add(mark);
                     highestCitationNumber = Math.max(highestCitationNumber, citationNumber);
                 } else {
-                    LOGGER.error("Cannot parse reference mark - invalid format: {}", name);
+                    LOGGER.warn("Cannot parse reference mark - invalid format: {}", name);
                 }
             }
         }
