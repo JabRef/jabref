66ddcf9379c09a5ca297ee32d82c822affc4c062
Oliver Kopp <kopp.dev@gmail.com>
2024-09-07 14:30:13 +0200

    Fix checkstyle

--- src/main/java/org/jabref/logic/importer/FulltextFetchers.java
+++ src/main/java/org/jabref/logic/importer/FulltextFetchers.java
@@ -14,7 +14,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import org.jabref.logic.net.URLDownload;
 import org.jabref.logic.util.HeadlessExecutorService;
