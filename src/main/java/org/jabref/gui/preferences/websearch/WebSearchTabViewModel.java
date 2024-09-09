ad2982baa7a4e78119f9f6d51aacca84045e44e3
Siedlerchr <siedlerkiller@gmail.com>
2024-09-09 11:29:16 +0200

    fix ui

--- src/main/java/org/jabref/gui/preferences/websearch/WebSearchTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/websearch/WebSearchTabViewModel.java
@@ -173,6 +173,10 @@ public class WebSearchTabViewModel implements PreferenceTabViewModel {
         return shouldDownloadLinkedOnlineFiles;
     }
 
+    public BooleanProperty shouldKeepDownloadUrl() {
+        return shouldkeepDownloadUrl;
+    }
+
     public ReadOnlyBooleanProperty apiKeyPersistAvailable() {
         return apikeyPersistAvailableProperty;
     }
