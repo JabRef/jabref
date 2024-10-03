f3921c25066504e68890bf1fc489a474cc03bfca
Oliver Kopp <kopp.dev@gmail.com>
2024-10-03 22:29:36 +0200

    Rename maps
    Co-authored-by: Christoph <siedlerkiller@gmail.com>

--- src/main/java/org/jabref/model/database/BibDatabase.java
+++ src/main/java/org/jabref/model/database/BibDatabase.java
@@ -49,14 +49,11 @@ public class BibDatabase {
     private static final Logger LOGGER = LoggerFactory.getLogger(BibDatabase.class);
     private static final Pattern RESOLVE_CONTENT_PATTERN = Pattern.compile(".*#[^#]+#.*");
 
-    /**
-     * State attributes
-     */
     private final ObservableList<BibEntry> entries = FXCollections.synchronizedObservableList(FXCollections.observableArrayList(BibEntry::getObservables));
 
-    // BibEntryId to BibEntry
-    private final Map<String, BibEntry> entriesId = new HashMap<>();
-    private Map<String, BibtexString> bibtexStrings = new ConcurrentHashMap<>();
+    private final Map<String, BibEntry> entryIdToBibEntry = new HashMap<>();
+
+    private Map<String, BibtexString> stringToBibtexString = new ConcurrentHashMap<>();
 
     // Not included in equals, because it is not relevant for the content of the database
     private final EventBus eventBus = new EventBus();
@@ -203,12 +200,12 @@ public class BibDatabase {
             entry.registerListener(this);
         }
         if (newEntries.isEmpty()) {
-            eventBus.post(new EntriesAddedEvent(newEntries, eventSource));
+            LOGGER.warn("No entries to insert");
         } else {
-            eventBus.post(new EntriesAddedEvent(newEntries, newEntries.getFirst(), eventSource));
+            eventBus.post(new EntriesAddedEvent(newEntries, eventSource));
         }
         entries.addAll(newEntries);
-        newEntries.forEach(entry -> entriesId.put(entry.getId(), entry));
+        newEntries.forEach(entry -> entryIdToBibEntry.put(entry.getId(), entry));
     }
 
     public synchronized void removeEntry(BibEntry bibEntry) {
@@ -245,7 +242,7 @@ public class BibDatabase {
         }
         boolean anyRemoved = entries.removeIf(entry -> ids.contains(entry.getId()));
         if (anyRemoved) {
-            toBeDeleted.forEach(entry -> entriesId.remove(entry.getId()));
+            toBeDeleted.forEach(entry -> entryIdToBibEntry.remove(entry.getId()));
             eventBus.post(new EntriesRemovedEvent(toBeDeleted, eventSource));
         }
     }
@@ -279,11 +276,11 @@ public class BibDatabase {
             throw new KeyCollisionException("A string with that label already exists", id);
         }
 
-        if (bibtexStrings.containsKey(id)) {
+        if (stringToBibtexString.containsKey(id)) {
             throw new KeyCollisionException("Duplicate BibTeX string id.", id);
         }
 
-        bibtexStrings.put(id, string);
+        stringToBibtexString.put(id, string);
     }
 
     /**
@@ -293,7 +290,7 @@ public class BibDatabase {
      * @param stringsToAdd The collection of strings to set
      */
     public void setStrings(List<BibtexString> stringsToAdd) {
-        bibtexStrings = new ConcurrentHashMap<>();
+        stringToBibtexString = new ConcurrentHashMap<>();
         stringsToAdd.forEach(this::addString);
     }
 
@@ -301,7 +298,7 @@ public class BibDatabase {
      * Removes the string with the given id.
      */
     public void removeString(String id) {
-        bibtexStrings.remove(id);
+        stringToBibtexString.remove(id);
     }
 
     /**
@@ -309,7 +306,7 @@ public class BibDatabase {
      * These are in no sorted order.
      */
     public Set<String> getStringKeySet() {
-        return bibtexStrings.keySet();
+        return stringToBibtexString.keySet();
     }
 
     /**
@@ -317,14 +314,14 @@ public class BibDatabase {
      * These are in no particular order.
      */
     public Collection<BibtexString> getStringValues() {
-        return bibtexStrings.values();
+        return stringToBibtexString.values();
     }
 
     /**
      * Returns the string with the given id.
      */
     public BibtexString getString(String id) {
-        return bibtexStrings.get(id);
+        return stringToBibtexString.get(id);
     }
 
     /**
@@ -338,14 +335,14 @@ public class BibDatabase {
      * Returns the number of strings.
      */
     public int getStringCount() {
-        return bibtexStrings.size();
+        return stringToBibtexString.size();
     }
 
     /**
      * Check if there are strings.
      */
     public boolean hasNoStrings() {
-        return bibtexStrings.isEmpty();
+        return stringToBibtexString.isEmpty();
     }
 
     /**
@@ -361,7 +358,7 @@ public class BibDatabase {
      * Returns true if a string with the given label already exists.
      */
     public synchronized boolean hasStringByName(String label) {
-        return bibtexStrings.values().stream().anyMatch(value -> value.getName().equals(label));
+        return stringToBibtexString.values().stream().anyMatch(value -> value.getName().equals(label));
     }
 
     /**
@@ -391,7 +388,7 @@ public class BibDatabase {
             }
         }
 
-        return allUsedIds.stream().map(bibtexStrings::get).toList();
+        return allUsedIds.stream().map(stringToBibtexString::get).toList();
     }
 
     /**
@@ -453,7 +450,7 @@ public class BibDatabase {
         Objects.requireNonNull(usedIds);
         Objects.requireNonNull(allUsedIds);
 
-        for (BibtexString string : bibtexStrings.values()) {
+        for (BibtexString string : stringToBibtexString.values()) {
             if (string.getName().equalsIgnoreCase(label)) {
                 // First check if this string label has been resolved
                 // earlier in this recursion. If so, we have a
@@ -657,7 +654,7 @@ public class BibDatabase {
     }
 
     public BibEntry getEntryById(String id) {
-        return entriesId.get(id);
+        return entryIdToBibEntry.get(id);
     }
 
     @Override
@@ -669,7 +666,7 @@ public class BibDatabase {
             return false;
         }
         return Objects.equals(entries, that.entries)
-                && Objects.equals(bibtexStrings, that.bibtexStrings)
+                && Objects.equals(stringToBibtexString, that.stringToBibtexString)
                 && Objects.equals(preamble, that.preamble)
                 && Objects.equals(epilog, that.epilog)
                 && Objects.equals(sharedDatabaseID, that.sharedDatabaseID)
@@ -678,6 +675,6 @@ public class BibDatabase {
 
     @Override
     public int hashCode() {
-        return Objects.hash(entries, bibtexStrings, preamble, epilog, sharedDatabaseID, newLineSeparator);
+        return Objects.hash(entries, stringToBibtexString, preamble, epilog, sharedDatabaseID, newLineSeparator);
     }
 }
