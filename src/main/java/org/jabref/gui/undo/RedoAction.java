4c5e4d53689dc8c8249b46db89b03ce631bd8909
HoussemNasri <houssemnasri2001@gmail.com>
2024-10-04 16:49:52 +0100

    Use the fact that there is only one UndoManager instance shared between libraries

--- src/main/java/org/jabref/gui/undo/RedoAction.java
+++ src/main/java/org/jabref/gui/undo/RedoAction.java
@@ -1,12 +1,10 @@
 package org.jabref.gui.undo;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 import javax.swing.undo.CannotRedoException;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.WeakChangeListener;
+import javafx.beans.binding.Bindings;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.LibraryTab;
@@ -14,34 +12,34 @@ import org.jabref.gui.StateManager;
 import org.jabref.gui.actions.SimpleCommand;
 import org.jabref.logic.l10n.Localization;
 
+import static org.jabref.gui.actions.ActionHelper.needsDatabase;
+
 /**
  * @implNote See also {@link UndoAction}
  */
 public class RedoAction extends SimpleCommand {
     private final Supplier<LibraryTab> tabSupplier;
     private final DialogService dialogService;
+    private final CountingUndoManager undoManager;
 
-    public RedoAction(Supplier<LibraryTab> tabSupplier, DialogService dialogService, StateManager stateManager) {
+    public RedoAction(Supplier<LibraryTab> tabSupplier, CountingUndoManager undoManager, DialogService dialogService, StateManager stateManager) {
         this.tabSupplier = tabSupplier;
         this.dialogService = dialogService;
+        this.undoManager = undoManager;
 
-        ChangeListener<Optional<LibraryTab>> listener = (observable, oldValue, activeLibraryTab) -> {
-            activeLibraryTab.ifPresent(libraryTab ->
-                    this.executable.bind(libraryTab.getUndoManager().getRedoableProperty()));
-
-            oldValue.ifPresent(libraryTab -> this.executable.unbind());
-        };
-
-        WeakChangeListener<Optional<LibraryTab>> weakListener = new WeakChangeListener<>(listener);
-        stateManager.activeTabProperty().addListener(weakListener);
+        this.executable.bind(Bindings.and(needsDatabase(stateManager), undoManager.getRedoableProperty()));
     }
 
     @Override
     public void execute() {
         LibraryTab libraryTab = this.tabSupplier.get();
         try {
-            libraryTab.getUndoManager().redo();
-            dialogService.notify(Localization.lang("Redo"));
+            if (undoManager.canRedo()) {
+                undoManager.redo();
+                dialogService.notify(Localization.lang("Redo"));
+            } else {
+                throw new CannotRedoException();
+            }
         } catch (CannotRedoException ex) {
             dialogService.notify(Localization.lang("Nothing to redo") + '.');
         }
--- src/main/java/org/jabref/gui/undo/UndoAction.java
+++ src/main/java/org/jabref/gui/undo/UndoAction.java
@@ -1,12 +1,10 @@
 package org.jabref.gui.undo;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 import javax.swing.undo.CannotUndoException;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.WeakChangeListener;
+import javafx.beans.binding.Bindings;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.LibraryTab;
@@ -14,34 +12,34 @@ import org.jabref.gui.StateManager;
 import org.jabref.gui.actions.SimpleCommand;
 import org.jabref.logic.l10n.Localization;
 
+import static org.jabref.gui.actions.ActionHelper.needsDatabase;
+
 /**
  * @implNote See also {@link RedoAction}
  */
 public class UndoAction extends SimpleCommand {
     private final Supplier<LibraryTab> tabSupplier;
     private final DialogService dialogService;
+    private final CountingUndoManager undoManager;
 
-    public UndoAction(Supplier<LibraryTab> tabSupplier, DialogService dialogService, StateManager stateManager) {
+    public UndoAction(Supplier<LibraryTab> tabSupplier, CountingUndoManager undoManager, DialogService dialogService, StateManager stateManager) {
         this.tabSupplier = tabSupplier;
         this.dialogService = dialogService;
+        this.undoManager = undoManager;
 
-        ChangeListener<Optional<LibraryTab>> listener = (observable, oldValue, activeLibraryTab) -> {
-            activeLibraryTab.ifPresent(libraryTab ->
-                    this.executable.bind(libraryTab.getUndoManager().getRedoableProperty()));
-
-            oldValue.ifPresent(libraryTab -> this.executable.unbind());
-        };
-
-        WeakChangeListener<Optional<LibraryTab>> weakListener = new WeakChangeListener<>(listener);
-        stateManager.activeTabProperty().addListener(weakListener);
+        this.executable.bind(Bindings.and(needsDatabase(stateManager), undoManager.getUndoableProperty()));
     }
 
     @Override
     public void execute() {
         LibraryTab libraryTab = this.tabSupplier.get();
         try {
-            libraryTab.getUndoManager().undo();
-            dialogService.notify(Localization.lang("Undo"));
+            if (undoManager.canUndo()) {
+                undoManager.undo();
+                dialogService.notify(Localization.lang("Undo"));
+            } else {
+                throw new CannotUndoException();
+            }
         } catch (CannotUndoException ex) {
             dialogService.notify(Localization.lang("Nothing to undo") + '.');
         }
