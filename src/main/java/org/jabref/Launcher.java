8815445f703e39d5fc2b17b5126a728041183dc6
jiaixn xiao <u7769692@anu.edu.au>
2024-10-16 23:09:17 +1100

    add some change

--- src/main/java/org/jabref/Launcher.java
+++ src/main/java/org/jabref/Launcher.java
@@ -20,6 +20,8 @@ import com.airhacks.afterburner.injection.Injector;
 ///
 /// - Handle the command line arguments
 /// - Start the JavaFX application (if not in CLI mode)
+
+// jabref
 public class Launcher {
 
     public static void main(String[] args) {
