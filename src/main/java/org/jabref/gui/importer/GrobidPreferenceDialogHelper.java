0e0ec95f31ddfc5ac37efdef65cb54bd1cbe0143
Arsh Chawla <chawla.arsh@gmail.com>
2024-10-20 10:45:19 +1100

    Removed checkbox entirely, dialog logic updated such that users will only be asked about Grobid permissions once, with the preference being stored for the future. Updated CHANGELOG.md

--- src/main/java/org/jabref/gui/importer/GrobidPreferenceDialogHelper.java
+++ src/main/java/org/jabref/gui/importer/GrobidPreferenceDialogHelper.java
@@ -7,27 +7,27 @@ import org.jabref.logic.l10n.Localization;
 /**
  * Metadata extraction from PDFs and plaintext works very well using Grobid, but we do not want to enable it by default
  * due to data privacy concerns.
- * To make users aware of the feature, we ask each time before querying Grobid, giving the option to opt-out.
+ * To make users aware of the feature, we ask before querying Grobid, saving the users' preference for the future.
  */
 public class GrobidPreferenceDialogHelper {
 
     /**
-     * If Grobid is not enabled but the user has not explicitly opted-out of Grobid, we ask for permission to send data
-     * to Grobid by using a dialog and giving an explicit 'save preference' option.
+     * If the user has not explicitly opted-in/out of Grobid, we ask for permission to send data to Grobid by using
+     * a dialog. The users' preference is saved.
      *
      * @param dialogService the DialogService to use
-     * @return if the user enabled Grobid, either in the past or after being asked by the dialog, save preference
-     * if specified.
+     * @return if the user enabled Grobid, either in the past or after being asked by the dialog.
      */
     public static boolean showAndWaitIfUserIsUndecided(DialogService dialogService, GrobidPreferences preferences) {
         if (preferences.isGrobidPreference()) {
             return preferences.isGrobidEnabled();
         }
-        boolean grobidEnabled = dialogService.showConfirmationDialogWithOptOutAndWait(
+        boolean grobidEnabled = dialogService.showConfirmationDialogAndWait(
                 Localization.lang("Remote services"),
                 Localization.lang("Allow sending PDF files and raw citation strings to a JabRef online service (Grobid) to determine Metadata. This produces better results."),
-                Localization.lang("Save Preference"),
-                preferences::setGrobidPreference);
+                Localization.lang("Yes"),
+                Localization.lang("No"));
+        preferences.setGrobidPreference(true);
         preferences.setGrobidEnabled(grobidEnabled);
         return grobidEnabled;
     }
