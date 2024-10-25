49b7973d2c7be95f6159e978adc7d705c3c433af
Rifang Zhou <u7780442@anu.edu.au>
2024-10-25 23:19:36 +1100

    Fix for issue #551

--- src/main/java/org/jabref/gui/externalfiles/UnlinkedFilesDialogViewModel.java
+++ src/main/java/org/jabref/gui/externalfiles/UnlinkedFilesDialogViewModel.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
