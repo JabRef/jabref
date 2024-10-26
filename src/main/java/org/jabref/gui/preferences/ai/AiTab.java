6c9c7e02bf2d375a2e2dc6df83117513dbc67046
Ruslan <ruslanpopov1512@gmail.com>
2024-10-26 18:49:57 +0300

    Update src/main/java/org/jabref/gui/preferences/ai/AiTab.java

--- src/main/java/org/jabref/gui/preferences/ai/AiTab.java
+++ src/main/java/org/jabref/gui/preferences/ai/AiTab.java
@@ -29,7 +29,7 @@ import org.controlsfx.control.textfield.CustomPasswordField;
 
 public class AiTab extends AbstractPreferenceTabView<AiTabViewModel> implements PreferencesTab {
     private static final String HUGGING_FACE_CHAT_MODEL_PROMPT = "TinyLlama/TinyLlama_v1.1 (or any other model name)";
-    private static final String GPT_4_ALL_CHAT_MODEL_PROMPT = "Phi-3.1-mini (or any other local model name from Gpt4All)";
+    private static final String GPT_4_ALL_CHAT_MODEL_PROMPT = "Phi-3.1-mini (or any other local model name from GPT4All)";
 
     @FXML private CheckBox enableAi;
     @FXML private CheckBox autoGenerateEmbeddings;
