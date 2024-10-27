d412e9b7ce77e8abad1f7cfbae4a88e9d404ce98
Thomas Bridgart <u7291634@anu.edu.au>
2024-10-27 15:26:15 +1100

    fix lwta abbreviations to only allow suffixes/prefixes unless specified (e.g. turn international into int. instead of int.al)

--- src/main/java/org/jabref/logic/journals/LwtaAbbreviation.java
+++ src/main/java/org/jabref/logic/journals/LwtaAbbreviation.java
@@ -4,15 +4,19 @@ public class LwtaAbbreviation {
     private String unAbbreviated;
     private String abbreviation;
     private Position position;
+    private boolean allowsSuffix;
+    private boolean allowsPrefix;
 
     enum Position {
         ENDS_WORD, STARTS_WORD, IN_WORD, FULL_WORD
     }
 
-    LwtaAbbreviation(String unAbbreviated, String abbreviation, Position position) {
+    LwtaAbbreviation(String unAbbreviated, String abbreviation, Position position, boolean allowsPrefix, boolean allowsSuffix) {
         this.unAbbreviated = unAbbreviated;
         this.abbreviation = abbreviation;
         this.position = position;
+        this.allowsPrefix = allowsPrefix;
+        this.allowsSuffix = allowsSuffix;
     }
 
     public String getAbbreviation() {
@@ -25,5 +29,13 @@ public class LwtaAbbreviation {
 
     public Position getPosition() {
         return position;
+    }
+
+    public boolean getAllowsPrefix() {
+        return allowsPrefix;
+    }
+
+    public boolean getAllowsSuffix() {
+        return allowsSuffix;
     }
 }
