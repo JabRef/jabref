82d69b7b6cd599c7cf201ede9360c5a2d5898c59
Rhonda Luu <u7127350@anu.edu.au>
2024-10-27 19:17:55 +1100

    Implemented file icons to be displayed in menu item for move and rename file

--- src/main/java/org/jabref/gui/fieldeditors/LinkedFileViewModel.java
+++ src/main/java/org/jabref/gui/fieldeditors/LinkedFileViewModel.java
@@ -364,6 +364,14 @@ public class LinkedFileViewModel extends AbstractViewModel {
         if (targetDirectory.isPresent()) {
             FileDirectoryHandler.DirectoryInfo dirInfo = targetDirectory.get();
             moveAndRenameFileItem.setText(Localization.lang("Move file to %0 and Rename", dirInfo.label()));
+
+            if (dirInfo.label().equals("main file directory")) {
+                moveAndRenameFileItem.setGraphic(IconTheme.JabRefIcons.WORLD_PDF.getGraphicNode());
+            } else if (dirInfo.label().equals("user-specific file directory")) {
+                moveAndRenameFileItem.setGraphic(IconTheme.JabRefIcons.PERSON_PDF.getGraphicNode());
+            } else {
+                moveAndRenameFileItem.setGraphic(IconTheme.JabRefIcons.MOVE_TO_FOLDER.getGraphicNode());
+            }
         } else {
             //moveAndRenameFileItem.setDisable(true);
             moveAndRenameFileItem.setText(Localization.lang("Move file to directory and Rename"));
