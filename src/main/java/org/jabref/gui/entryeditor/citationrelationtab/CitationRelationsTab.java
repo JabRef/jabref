a2a64f1fe56401768ba54ceec65ef0613033d518
Oliver Kopp <kopp.dev@gmail.com>
2024-10-30 18:40:46 +0100

    Fix BibTeX "twin" at citation relations (#12124)
    * Add cloning of BibEntries while adding
    
    - Add getter+setter for "cites" to BibEntry
    - Refactor CitationsRelationsTabViewModel
    - Group citation keys together in JabRef_en.properties
    - We fixed an issue where the import of an entry cited by an entry caused a change of the citation key of the citing entry.
    
    * Reorder JabRef_en.properties
    
    * Merge same language strings into another string
    
    * Exit early
    
    * Convert everything to a parameter
    
    * use "entry(s)" instead of "entry(ies)"
    
    * The tests are BibTeX (not biblatex)
    
    * Line breaks...
    
    * Fix test "importedEntriesWithExistingCitationKeysCiteExistingEntry"
    
    * Fix localization
    
    * Always select entry in main table when showing entry editor
    
    * Revert "Always select entry in main table when showing entry editor"
    
    This reverts commit d61872854616b65dbbcef51305d471a5f4b9696f.

--- src/main/java/org/jabref/gui/entryeditor/citationrelationtab/CitationRelationsTab.java
+++ src/main/java/org/jabref/gui/entryeditor/citationrelationtab/CitationRelationsTab.java
@@ -526,7 +526,7 @@ public class CitationRelationsTab extends EntryEditorTab {
 
         citationsRelationsTabViewModel.importEntries(entriesToImport, searchType, existingEntry);
 
-        dialogService.notify(Localization.lang("Number of entries successfully imported") + ": " + entriesToImport.size());
+        dialogService.notify(Localization.lang("%0 entry(s) imported", entriesToImport.size()));
     }
 
     /**
