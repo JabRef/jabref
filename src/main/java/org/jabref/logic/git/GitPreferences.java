25d4423c26dab5250dcc3c2c35122034cc99d18e
felicitasmaia <felicitas.haag@unibas.ch>
2024-11-19 17:02:27 +0100

    adapt Git Preferences to accomodate Git authentication and saving SSH path

--- src/main/java/org/jabref/logic/git/GitPreferences.java
+++ src/main/java/org/jabref/logic/git/GitPreferences.java
@@ -4,16 +4,26 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 public class GitPreferences {
-    private BooleanProperty gitSupportEnabledProperty = new SimpleBooleanProperty();
-    private BooleanProperty frequencyLabelEnabledProperty = new SimpleBooleanProperty();
-    private ObjectProperty<AuthenticationViewMode> authenticationMethod = new SimpleObjectProperty<>();
+    private final BooleanProperty gitSupportEnabledProperty;
+    private final BooleanProperty frequencyLabelEnabledProperty;
+    private final ObjectProperty<AuthenticationViewMode> authenticationMethod;
+    private SimpleStringProperty sshPath;
 
-    public GitPreferences(boolean gitSupportEnabledProperty, AuthenticationViewMode authenticationMethod, boolean frequencyLabelEnabledProperty) {
+    // TODO: encrypt
+    private final SimpleStringProperty password;
+
+    private final SimpleStringProperty username;
+
+    public GitPreferences(boolean gitSupportEnabledProperty, AuthenticationViewMode authenticationMethod, boolean frequencyLabelEnabledProperty, String username, String sshPath, String password) {
         this.gitSupportEnabledProperty = new SimpleBooleanProperty(gitSupportEnabledProperty);
         this.authenticationMethod = new SimpleObjectProperty<>(authenticationMethod);
         this.frequencyLabelEnabledProperty = new SimpleBooleanProperty(frequencyLabelEnabledProperty);
+        this.username = new SimpleStringProperty(username != null ? username : ""); // Default to empty if null
+        this.sshPath = new SimpleStringProperty(sshPath);
+        this.password = new SimpleStringProperty(password);
     }
 
     public AuthenticationViewMode getAuthenticationMethod() {
@@ -26,6 +36,44 @@ public class GitPreferences {
 
     public void setAuthenticationMethod(AuthenticationViewMode authenticationMethod) {
         this.authenticationMethod.set(authenticationMethod);
+    }
+
+    public void setSshPath(String sshPath) {
+        this.sshPath.set(sshPath);
+    }
+
+    public SimpleStringProperty getSshPathProperty() {
+        return this.sshPath;
+    }
+
+    public String getSshPath() {
+        System.out.println("Get Method SSH PATH is " + sshPath.get());
+        return sshPath.get();
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    public SimpleStringProperty getPasswordProperty() {
+        return this.password;
+    }
+
+    public String getPassword() {
+        return this.password.get();
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public SimpleStringProperty getUsernameProperty() {
+        System.out.println("Get Method Username is " + username);
+        return username;
+    }
+
+    public String getUsername() {
+        return username.get();
     }
 
     public boolean isGitEnabled() {
