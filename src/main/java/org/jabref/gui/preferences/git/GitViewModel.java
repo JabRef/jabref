aa9f575c59dd4d6ed6794680236bb127d74e8db5
felicitasmaia <felicitas.haag@unibas.ch>
2024-11-19 17:02:58 +0100

    changed gitTab in Preferences with changes discussed

--- src/main/java/org/jabref/gui/preferences/git/GitViewModel.java
+++ src/main/java/org/jabref/gui/preferences/git/GitViewModel.java
@@ -1,35 +1,32 @@
 package org.jabref.gui.preferences.git;
 
+import java.nio.file.Path;
 import java.util.List;
 
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 
+import org.jabref.gui.DialogService;
 import org.jabref.gui.preferences.PreferenceTabViewModel;
-import org.jabref.logic.git.AuthenticationViewMode;
+import org.jabref.gui.util.DirectoryDialogConfiguration;
 import org.jabref.logic.git.GitPreferences;
 
 public class GitViewModel implements PreferenceTabViewModel {
 
     private final BooleanProperty gitSupportEnabledProperty = new SimpleBooleanProperty();
     private final BooleanProperty frequencyLabelEnabledProperty = new SimpleBooleanProperty();
-    private final ObjectProperty<AuthenticationViewMode> authenticationMethod = new SimpleObjectProperty<>();
+    private final SimpleStringProperty sshPath = new SimpleStringProperty("");
+    private final DialogService dialogService;
     private final GitPreferences gitPreferences;
 
-    public GitViewModel(GitPreferences gitPreferences) {
+    public GitViewModel(GitPreferences gitPreferences, DialogService dialogservice) {
+        this.dialogService = dialogservice;
         this.gitPreferences = gitPreferences;
     }
 
     @Override
     public void setValues() {
-        switch (gitPreferences.getAuthenticationMethod()) {
-            case SSH ->
-                    authenticationMethod.setValue(AuthenticationViewMode.SSH);
-            case CREDENTIALS ->
-                    authenticationMethod.setValue(AuthenticationViewMode.CREDENTIALS);
-        }
         gitSupportEnabledProperty.setValue(gitPreferences.isGitEnabled());
         frequencyLabelEnabledProperty.setValue(gitPreferences.isFrequencyLabelEnabled());
     }
@@ -41,12 +38,6 @@ public class GitViewModel implements PreferenceTabViewModel {
     public void storeSettings() {
         gitPreferences.setGitSupportEnabledProperty(gitSupportEnabledProperty.getValue());
         gitPreferences.setFrequencyLabelEnabledProperty(frequencyLabelEnabledProperty.getValue());
-
-        if (AuthenticationViewMode.SSH == authenticationMethod.getValue()) {
-            gitPreferences.setAuthenticationMethod(AuthenticationViewMode.SSH);
-        } else if (AuthenticationViewMode.CREDENTIALS == authenticationMethod.getValue()) {
-            gitPreferences.setAuthenticationMethod(AuthenticationViewMode.CREDENTIALS);
-        }
     }
 
     public BooleanProperty gitSupportEnabledProperty() {
@@ -57,14 +48,6 @@ public class GitViewModel implements PreferenceTabViewModel {
         return frequencyLabelEnabledProperty;
     }
 
-    public AuthenticationViewMode authenticationViewMode() {
-        return authenticationMethod.getValue();
-    }
-
-    public ObjectProperty<AuthenticationViewMode> authenticationViewModeObjectProperty() {
-        return authenticationMethod;
-    }
-
     @Override
     public boolean validateSettings() {
         return PreferenceTabViewModel.super.validateSettings();
@@ -73,5 +56,23 @@ public class GitViewModel implements PreferenceTabViewModel {
     @Override
     public List<String> getRestartWarnings() {
         return PreferenceTabViewModel.super.getRestartWarnings();
+    }
+
+    public void authentify() {
+        AuthentifyDialogView dialog = new AuthentifyDialogView(this.gitPreferences, this.dialogService);
+        dialog.showAndWait();
+        // System.out.println("we made it to GitViewModel.authentify()");
+    }
+
+    public void sshBrowse() {
+        DirectoryDialogConfiguration dirDialogConfiguration =
+                new DirectoryDialogConfiguration.Builder().withInitialDirectory(Path.of(sshPath.getValue())).build();
+        dialogService.showDirectorySelectionDialog(dirDialogConfiguration).ifPresent(f -> sshPath.setValue(f.toString()));
+        gitPreferences.setSshPath(sshPath.get());
+    }
+
+    public SimpleStringProperty sshPathProperty() {
+        // System.out.println("sshPathProperty() in GitViewModel: " + gitPreferences.getSshPathProperty());
+        return gitPreferences.getSshPathProperty();
     }
 }
