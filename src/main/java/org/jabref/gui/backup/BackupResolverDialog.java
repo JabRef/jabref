b1c006e69ba258748b2787d5d6b832179fdc36a3
Gillan0 <antonino.gillard@orange.fr>
2024-12-04 23:03:04 +0100

    Remove usage of BackupFileUtil in UI

--- src/main/java/org/jabref/gui/backup/BackupResolverDialog.java
+++ src/main/java/org/jabref/gui/backup/BackupResolverDialog.java
@@ -1,23 +1,12 @@
 package org.jabref.gui.backup;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Optional;
 
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Hyperlink;
 
 import org.jabref.gui.FXDialog;
-import org.jabref.gui.desktop.os.NativeDesktop;
-import org.jabref.gui.frame.ExternalApplicationsPreferences;
 import org.jabref.logic.l10n.Localization;
-import org.jabref.logic.util.BackupFileType;
-import org.jabref.logic.util.io.BackupFileUtil;
-
-import org.controlsfx.control.HyperlinkLabel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class BackupResolverDialog extends FXDialog {
     public static final ButtonType RESTORE_FROM_BACKUP = new ButtonType(Localization.lang("Restore from latest backup"), ButtonBar.ButtonData.OK_DONE);
@@ -25,38 +14,15 @@ public class BackupResolverDialog extends FXDialog {
     public static final ButtonType IGNORE_BACKUP = new ButtonType(Localization.lang("Ignore backup"), ButtonBar.ButtonData.CANCEL_CLOSE);
     public static final ButtonType COMPARE_OLDER_BACKUP = new ButtonType("Compare older backup", ButtonBar.ButtonData.LEFT);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BackupResolverDialog.class);
-
-    public BackupResolverDialog(Path originalPath, Path backupDir, ExternalApplicationsPreferences externalApplicationsPreferences) {
+    public BackupResolverDialog(Path originalPath) {
         super(AlertType.CONFIRMATION, Localization.lang("Backup found"), true);
         setHeaderText(null);
         getDialogPane().setMinHeight(180);
         getDialogPane().getButtonTypes().setAll(RESTORE_FROM_BACKUP, REVIEW_BACKUP, IGNORE_BACKUP, COMPARE_OLDER_BACKUP);
 
-        Optional<Path> backupPathOpt = BackupFileUtil.getPathOfLatestExistingBackupFile(originalPath, BackupFileType.BACKUP, backupDir);
-        String backupFilename = backupPathOpt.map(Path::getFileName).map(Path::toString).orElse(Localization.lang("File not found"));
-        String content = Localization.lang("A backup file for '%0' was found at [%1]", originalPath.getFileName().toString(), backupFilename) + "\n" +
+        String content = Localization.lang("A backup file for '%0' was found.", originalPath.getFileName().toString()) + "\n" +
                 Localization.lang("This could indicate that JabRef did not shut down cleanly last time the file was used.") + "\n\n" +
                 Localization.lang("Do you want to recover the library from the backup file?");
         setContentText(content);
-
-        HyperlinkLabel contentLabel = new HyperlinkLabel(content);
-        contentLabel.setPrefWidth(360);
-        contentLabel.setOnAction(e -> {
-            if (backupPathOpt.isPresent()) {
-                if (!(e.getSource() instanceof Hyperlink)) {
-                    return;
-                }
-                String clickedLinkText = ((Hyperlink) (e.getSource())).getText();
-                if (backupFilename.equals(clickedLinkText)) {
-                    try {
-                        NativeDesktop.openFolderAndSelectFile(backupPathOpt.get(), externalApplicationsPreferences, null);
-                    } catch (IOException ex) {
-                        LOGGER.error("Could not open backup folder", ex);
-                    }
-                }
-            }
-        });
-        getDialogPane().setContent(contentLabel);
     }
 }
--- src/main/java/org/jabref/gui/dialogs/BackupUIManager.java
+++ src/main/java/org/jabref/gui/dialogs/BackupUIManager.java
@@ -21,7 +21,6 @@ import org.jabref.gui.collab.DatabaseChange;
 import org.jabref.gui.collab.DatabaseChangeList;
 import org.jabref.gui.collab.DatabaseChangeResolverFactory;
 import org.jabref.gui.collab.DatabaseChangesResolverDialog;
-import org.jabref.gui.frame.ExternalApplicationsPreferences;
 import org.jabref.gui.preferences.GuiPreferences;
 import org.jabref.gui.undo.NamedCompound;
 import org.jabref.gui.util.UiTaskExecutor;
@@ -57,26 +56,31 @@ public class BackupUIManager {
         LOGGER.info("Show restore backup dialog");
         var actionOpt = showBackupResolverDialog(
                 dialogService,
-                preferences.getExternalApplicationsPreferences(),
-                originalPath,
-                preferences.getFilePreferences().getBackupDirectory());
+                originalPath);
 
         return actionOpt.flatMap(action -> {
             try {
+
                 List<RevCommit> commits = BackupManagerGit.retrieveCommits(originalPath, preferences.getFilePreferences().getBackupDirectory(), -1);
                 List<BackupEntry> backups = BackupManagerGit.retrieveCommitDetails(commits, originalPath, preferences.getFilePreferences().getBackupDirectory()).reversed();
+
                 if (action == BackupResolverDialog.RESTORE_FROM_BACKUP) {
                     ObjectId commitId = backups.getFirst().getId();
+
                     BackupManagerGit.restoreBackup(originalPath, preferences.getFilePreferences().getBackupDirectory(), commitId);
+
                     return Optional.empty();
                 } else if (action == BackupResolverDialog.REVIEW_BACKUP) {
                     ObjectId commitId = backups.getFirst().getId();
+
                     return showReviewBackupDialog(dialogService, originalPath, preferences, fileUpdateMonitor, undoManager, stateManager, commitId, commitId);
                 } else if (action == BackupResolverDialog.COMPARE_OLDER_BACKUP) {
                     var recordBackupChoice = showBackupChoiceDialog(dialogService, originalPath, preferences, backups);
+
                     if (recordBackupChoice.isEmpty()) {
                         return Optional.empty();
                     }
+
                     if (recordBackupChoice.get().action() == BackupChoiceDialog.RESTORE_BACKUP) {
                         LOGGER.warn(recordBackupChoice.get().entry().getSize());
                         ObjectId commitId = recordBackupChoice.get().entry().getId();
@@ -99,11 +103,9 @@ public class BackupUIManager {
     }
 
     private static Optional<ButtonType> showBackupResolverDialog(DialogService dialogService,
-                                                                 ExternalApplicationsPreferences externalApplicationsPreferences,
-                                                                 Path originalPath,
-                                                                 Path backupDir) {
+                                                                 Path originalPath) {
         return UiTaskExecutor.runInJavaFXThread(
-                () -> dialogService.showCustomDialogAndWait(new BackupResolverDialog(originalPath, backupDir, externalApplicationsPreferences)));
+                () -> dialogService.showCustomDialogAndWait(new BackupResolverDialog(originalPath)));
     }
 
     private static Optional<BackupChoiceDialogRecord> showBackupChoiceDialog(DialogService dialogService,
