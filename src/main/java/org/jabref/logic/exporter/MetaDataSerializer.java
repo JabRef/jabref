d0420df51a6cc63785fa76a2f5d10f283e9c37fb
Nitin Suresh <aqurilla@gmail.com>
2024-12-17 21:35:33 -0800

    fix import issue

--- src/main/java/org/jabref/logic/exporter/MetaDataSerializer.java
+++ src/main/java/org/jabref/logic/exporter/MetaDataSerializer.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.jabref.logic.citationkeypattern.AbstractCitationKeyPatterns;
 import org.jabref.logic.citationkeypattern.CitationKeyPattern;
