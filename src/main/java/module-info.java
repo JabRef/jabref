429936a386ac7fd2fcffaf7bfeb19e710c21552b
Siedlerchr <siedlerkiller@gmail.com>
2024-12-23 15:57:33 +0100

    update index version and module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -167,7 +167,7 @@ open module org.jabref {
     /*
      * In case the version is updated, please also increment {@link org.jabref.model.search.LinkedFilesConstants.VERSION} to trigger reindexing.
      */
-    uses org.apache.lucene.codecs.lucene100.Lucene100Codec;
+    uses org.apache.lucene.codecs.lucene101.Lucene101Codec;
     requires org.apache.lucene.analysis.common;
     requires org.apache.lucene.core;
     requires org.apache.lucene.highlighter;
