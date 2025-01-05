97cb3e4582054294d95ee87c8f4b3938b0d5d5b1
laky241 <lakshay.kapoor.3333@gmail.com>
2025-01-06 00:48:04 +0530

    Revert "fixed Area of preview on hover to the size of the text displayed"
    This reverts commit 708d297ba1938e20dabd4f65822ebf74c270c578.

--- src/main/java/org/jabref/gui/maintable/MainTableTooltip.java
+++ src/main/java/org/jabref/gui/maintable/MainTableTooltip.java
@@ -2,7 +2,6 @@ package org.jabref.gui.maintable;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
@@ -18,52 +17,26 @@ public class MainTableTooltip extends Tooltip {
 
     private final PreviewViewer preview;
     private final GuiPreferences preferences;
-    private final VBox tooltipContent = new VBox(); // A vertical box to hold the tooltip content
-    private final Label fieldValueLabel = new Label(); // Label to show field value
+    private final VBox tooltipContent = new VBox();
+    private final Label fieldValueLabel = new Label();
 
     public MainTableTooltip(DialogService dialogService, GuiPreferences preferences, ThemeManager themeManager, TaskExecutor taskExecutor) {
         this.preferences = preferences;
         this.preview = new PreviewViewer(dialogService, preferences, themeManager, taskExecutor);
-
-        // Reduce delay before showing the tooltip
-        this.setShowDelay(Duration.millis(100)); // 100ms delay
-        this.setHideDelay(Duration.millis(500)); // Slight delay before hiding
-
-        // Add the field value label and preview area to the tooltip content
+        this.setShowDelay(Duration.seconds(1));
         this.tooltipContent.getChildren().addAll(fieldValueLabel, preview);
-
-        // Add some basic styling to the tooltip
-        tooltipContent.setStyle("-fx-padding: 10; -fx-background-color: white; -fx-border-color: gray; -fx-border-radius: 5;");
     }
 
     public Tooltip createTooltip(BibDatabaseContext databaseContext, BibEntry entry, String fieldValue) {
-        // Set the text for the field value label
         fieldValueLabel.setText(fieldValue + "\n");
-
-        // Make sure the text wraps if it's too long
-        fieldValueLabel.setWrapText(true);
-        fieldValueLabel.setMaxWidth(400); // Set a limit for how wide the text can be
-
-        // Automatically size the tooltip content based on what's inside
-        tooltipContent.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
-        tooltipContent.setMaxWidth(500); // Prevent the tooltip from being too wide
-        tooltipContent.setMaxHeight(400); // Prevent the tooltip from being too tall
-
-        // If the user has enabled preview in the preferences
         if (preferences.getPreviewPreferences().shouldShowPreviewEntryTableTooltip()) {
-            // Set up the preview
             preview.setLayout(preferences.getPreviewPreferences().getSelectedPreviewLayout());
             preview.setDatabaseContext(databaseContext);
             preview.setEntry(entry);
-
-            // Show both the field value and the preview
             this.setGraphic(tooltipContent);
         } else {
-            // If preview is disabled, only show the field value
             this.setGraphic(fieldValueLabel);
         }
-
-        return this; // Return the tooltip object
+        return this;
     }
 }
-
