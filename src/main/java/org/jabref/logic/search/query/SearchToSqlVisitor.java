f7bc3f8a90c6638184869135faf5df3f41bc19ce
Loay Ghreeb <loayahmed655@gmail.com>
2025-01-10 15:12:10 +0200

    Simplify condition

--- src/main/java/org/jabref/logic/search/query/SearchToSqlVisitor.java
+++ src/main/java/org/jabref/logic/search/query/SearchToSqlVisitor.java
@@ -209,10 +209,12 @@ public class SearchToSqlVisitor extends SearchBaseVisitor<SqlQueryNode> {
 
         // field = "" -> should find entries where the field is empty
         // field != "" -> should find entries where the field is not empty
-        if (term.isEmpty() && searchFlags.contains(NEGATION)) {
-            searchFlags.remove(NEGATION);
-        } else if (term.isEmpty()) {
-            searchFlags.add(NEGATION);
+        if (term.isEmpty()) {
+            if (searchFlags.contains(NEGATION)) {
+                searchFlags.remove(NEGATION);
+            } else {
+                searchFlags.add(NEGATION);
+            }
         }
 
         return getFieldQueryNode(field.toLowerCase(Locale.ROOT), term, searchFlags);
