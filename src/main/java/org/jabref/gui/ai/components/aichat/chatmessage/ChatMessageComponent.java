2721ff854e075b4d0fa2487c8ade5d1ca312964a
Aryan Rana <aryan.rana2005@gmail.com>
2025-01-12 21:28:54 +0530

    added copy button and text

--- src/main/java/org/jabref/gui/ai/components/aichat/chatmessage/ChatMessageComponent.java
+++ src/main/java/org/jabref/gui/ai/components/aichat/chatmessage/ChatMessageComponent.java
@@ -1,5 +1,8 @@
 package org.jabref.gui.ai.components.aichat.chatmessage;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.function.Consumer;
 
 import javafx.beans.property.ObjectProperty;
@@ -9,12 +12,13 @@ import javafx.geometry.NodeOrientation;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import org.jabref.logic.ai.util.ErrorMessage;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.logic.layout.format.MarkdownFormatter;
 
 import com.airhacks.afterburner.views.ViewLoader;
-import com.dlsc.gemsfx.ExpandingTextArea;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
@@ -24,13 +28,15 @@ import org.slf4j.LoggerFactory;
 public class ChatMessageComponent extends HBox {
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatMessageComponent.class);
 
+    MarkdownFormatter markdownFormatter = new MarkdownFormatter();
+
     private final ObjectProperty<ChatMessage> chatMessage = new SimpleObjectProperty<>();
     private final ObjectProperty<Consumer<ChatMessageComponent>> onDelete = new SimpleObjectProperty<>();
 
     @FXML private HBox wrapperHBox;
     @FXML private VBox vBox;
     @FXML private Label sourceLabel;
-    @FXML private ExpandingTextArea contentTextArea;
+    @FXML private Text contentText;
     @FXML private VBox buttonsVBox;
 
     public ChatMessageComponent() {
@@ -67,23 +73,26 @@ public class ChatMessageComponent extends HBox {
         switch (chatMessage.get()) {
             case UserMessage userMessage -> {
                 setColor("-jr-ai-message-user", "-jr-ai-message-user-border");
+                setTextWrapping(400.0);
                 setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
                 sourceLabel.setText(Localization.lang("User"));
-                contentTextArea.setText(userMessage.singleText());
+                contentText.setText(userMessage.singleText());
             }
 
             case AiMessage aiMessage -> {
                 setColor("-jr-ai-message-ai", "-jr-ai-message-ai-border");
+                setTextWrapping(400.0);
                 setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
                 sourceLabel.setText(Localization.lang("AI"));
-                contentTextArea.setText(aiMessage.text());
+                contentText.setText(aiMessage.text());
             }
 
             case ErrorMessage errorMessage -> {
                 setColor("-jr-ai-message-error", "-jr-ai-message-error-border");
+                setTextWrapping(400.0);
                 setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
                 sourceLabel.setText(Localization.lang("Error"));
-                contentTextArea.setText(errorMessage.getText());
+                contentText.setText(errorMessage.getText());
             }
 
             default ->
@@ -103,7 +112,17 @@ public class ChatMessageComponent extends HBox {
         }
     }
 
+    @FXML
+    private void copyToClipboard() {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(new StringSelection(contentText.getText()), null);
+    }
+
     private void setColor(String fillColor, String borderColor) {
         vBox.setStyle("-fx-background-color: " + fillColor + "; -fx-border-radius: 10; -fx-background-radius: 10; -fx-border-color: " + borderColor + "; -fx-border-width: 3;");
+    }
+
+    private void setTextWrapping(Double wrappingWidth) {
+        contentText.setWrappingWidth(wrappingWidth);
     }
 }
