fbf4a7c546a2ff4d928358a53ea2fd99faea5b48
InAnYan <ruslanpopov1512@gmail.com>
2025-01-21 12:00:33 +0200

    Fix checkers

--- src/test/java/org/jabref/logic/importer/fileformat/ImporterTestEngine.java
+++ src/test/java/org/jabref/logic/importer/fileformat/ImporterTestEngine.java
@@ -35,7 +35,7 @@ public class ImporterTestEngine {
             return stream
                     .map(path -> path.getFileName().toString())
                     .filter(fileNamePredicate)
-                    .filter(name -> !name.equals("pdf")) // There is a `pdf` in {@link TEST_RESOURCES}.
+                    .filter(name -> !"pdf".equals(name)) // There is a `pdf` in {@link TEST_RESOURCES}.
                     .collect(Collectors.toList());
         }
     }
