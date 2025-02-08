e11b319db161140600442670da50cd8b9d75eb89
subhramit <subhramit.bb@live.in>
2025-02-08 15:11:40 +0530

    Revert `getBaseList` visibility change

--- src/main/java/org/jabref/gui/openoffice/OpenOfficePanel.java
+++ src/main/java/org/jabref/gui/openoffice/OpenOfficePanel.java
@@ -338,7 +338,7 @@ public class OpenOfficePanel {
         }
     }
 
-    public List<BibDatabase> getBaseList() {
+    private List<BibDatabase> getBaseList() {
         List<BibDatabase> databases = new ArrayList<>();
         if (openOfficePreferences.getUseAllDatabases()) {
             for (BibDatabaseContext database : stateManager.getOpenDatabases()) {
