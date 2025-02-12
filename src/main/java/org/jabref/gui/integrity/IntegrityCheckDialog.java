9b11edf566ea8b82a8f5043b685cb6a3c911e206
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-02-13 03:42:05 +0530

    Add fix options for integrity issues

--- src/main/java/org/jabref/gui/integrity/IntegrityCheckDialog.java
+++ src/main/java/org/jabref/gui/integrity/IntegrityCheckDialog.java
@@ -2,23 +2,38 @@ package org.jabref.gui.integrity;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
+
+import javax.swing.undo.UndoManager;
 
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Modality;
 
+import org.jabref.gui.DialogService;
 import org.jabref.gui.LibraryTab;
+import org.jabref.gui.StateManager;
+import org.jabref.gui.citationkeypattern.GenerateCitationKeyAction;
 import org.jabref.gui.theme.ThemeManager;
 import org.jabref.gui.util.BaseDialog;
 import org.jabref.gui.util.ValueTableCellFactory;
 import org.jabref.logic.integrity.IntegrityMessage;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.logic.preferences.CliPreferences;
+import org.jabref.logic.util.TaskExecutor;
+import org.jabref.model.entry.field.InternalField;
+import org.jabref.model.entry.field.StandardField;
 
 import com.airhacks.afterburner.views.ViewLoader;
 import jakarta.inject.Inject;
@@ -30,20 +45,40 @@ public class IntegrityCheckDialog extends BaseDialog<Void> {
     @FXML private TableColumn<IntegrityMessage, String> keyColumn;
     @FXML private TableColumn<IntegrityMessage, String> fieldColumn;
     @FXML private TableColumn<IntegrityMessage, String> messageColumn;
+    @FXML private TableColumn<IntegrityMessage, String> fixesColumn;
     @FXML private MenuButton keyFilterButton;
     @FXML private MenuButton fieldFilterButton;
     @FXML private MenuButton messageFilterButton;
+    @FXML private ComboBox<String> entryTypeCombo;
 
     @Inject private ThemeManager themeManager;
 
     private final List<IntegrityMessage> messages;
-    private final LibraryTab libraryTab;
+    private final Supplier<LibraryTab> tabSupplier;
+    private final DialogService dialogService;
+    private final StateManager stateManager;
+    private final TaskExecutor taskExecutor;
+    private final CliPreferences preferences;
+    private final UndoManager undoManager;
+
     private IntegrityCheckDialogViewModel viewModel;
     private TableFilter<IntegrityMessage> tableFilter;
 
-    public IntegrityCheckDialog(List<IntegrityMessage> messages, LibraryTab libraryTab) {
+    public IntegrityCheckDialog(List<IntegrityMessage> messages,
+                                Supplier<LibraryTab> tabSupplier,
+                                DialogService dialogService,
+                                StateManager stateManager,
+                                TaskExecutor taskExecutor,
+                                CliPreferences preferences,
+                                UndoManager undoManager) {
         this.messages = messages;
-        this.libraryTab = libraryTab;
+        this.tabSupplier = tabSupplier;
+        this.dialogService = dialogService;
+        this.stateManager = stateManager;
+        this.taskExecutor = taskExecutor;
+        this.preferences = preferences;
+        this.undoManager = undoManager;
+
         this.setTitle(Localization.lang("Check integrity"));
         this.initModality(Modality.NONE);
 
@@ -57,7 +92,7 @@ public class IntegrityCheckDialog extends BaseDialog<Void> {
     private void onSelectionChanged(ListChangeListener.Change<? extends IntegrityMessage> change) {
         if (change.next()) {
             change.getAddedSubList().stream().findFirst().ifPresent(message ->
-                    libraryTab.editEntryAndFocusField(message.entry(), message.field()));
+                    tabSupplier.get().editEntryAndFocusField(message.entry(), message.field()));
         }
     }
 
@@ -67,13 +102,49 @@ public class IntegrityCheckDialog extends BaseDialog<Void> {
 
     @FXML
     private void initialize() {
-        viewModel = new IntegrityCheckDialogViewModel(messages);
+        viewModel = new IntegrityCheckDialogViewModel(messages, dialogService);
 
         messagesTable.getSelectionModel().getSelectedItems().addListener(this::onSelectionChanged);
         messagesTable.setItems(viewModel.getMessages());
+        entryTypeCombo.getItems().addAll(viewModel.getEntryTypes());
+        entryTypeCombo.getSelectionModel().selectFirst();
         keyColumn.setCellValueFactory(row -> new ReadOnlyStringWrapper(row.getValue().entry().getCitationKey().orElse("")));
         fieldColumn.setCellValueFactory(row -> new ReadOnlyStringWrapper(row.getValue().field().getDisplayName()));
         messageColumn.setCellValueFactory(row -> new ReadOnlyStringWrapper(row.getValue().message()));
+
+        fixesColumn.setCellFactory(row -> new TableCell<>() {
+            private final Button button = new Button();
+
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || getTableRow() == null || getTableRow().getItem() == null) {
+                    setGraphic(null);
+                } else {
+                    IntegrityMessage rowData = getTableRow().getItem();
+                    switch (rowData.field()) {
+                        case StandardField.TITLE:
+                            button.setText("Fix");
+                            button.setOnAction(event -> {
+                                viewModel.fixTitle(rowData);
+                                dialogService.notify(Localization.lang("Masked capital letters using curly brackets {}"));
+                                removeRowFromTable(getTableRow().getItem());
+                            });
+                            setGraphic(button);
+                            break;
+                        case InternalField.KEY_FIELD:
+                            button.setText(Localization.lang("Generate Key"));
+                            button.setOnAction(event -> new GenerateCitationKeyAction(tabSupplier, dialogService, stateManager, taskExecutor, preferences, undoManager).execute());
+                            removeRowFromTable(getTableRow().getItem());
+                            setGraphic(button);
+                            break;
+                        default:
+                            setGraphic(null);
+                            break;
+                    }
+                }
+            }
+        });
 
         new ValueTableCellFactory<IntegrityMessage, String>()
                 .withText(Function.identity())
@@ -114,6 +185,31 @@ public class IntegrityCheckDialog extends BaseDialog<Void> {
                 tableFilter.selectAllValues(column);
                 column.setGraphic(null);
             });
+        }
+    }
+
+    private void removeRowFromTable(IntegrityMessage message) {
+        ObservableList<IntegrityMessage> mutableMessages = FXCollections.observableArrayList(messagesTable.getItems());
+        mutableMessages.remove(message);
+        messagesTable.setItems(mutableMessages);
+    }
+
+    @FXML
+    private void fixByType() {
+        String selectedType = entryTypeCombo.getSelectionModel().getSelectedItem();
+        if (selectedType.equals(StandardField.TITLE.getDisplayName())) {
+            for (IntegrityMessage message : messagesTable.getItems()) {
+                if (message.field().getDisplayName().equals(selectedType)) {
+                    viewModel.fixTitle(message);
+                    removeRowFromTable(message);
+                    viewModel.removeFromEntryTypes(message.field().getDisplayName());
+                    entryTypeCombo.getItems().setAll(viewModel.getEntryTypes());
+                    entryTypeCombo.getSelectionModel().selectFirst();
+                    dialogService.notify(Localization.lang("Fixed successfully!"));
+                }
+            }
+        } else {
+            dialogService.notify(Localization.lang("No fixes available!"));
         }
     }
 }
--- src/main/java/org/jabref/gui/integrity/IntegrityCheckDialogViewModel.java
+++ src/main/java/org/jabref/gui/integrity/IntegrityCheckDialogViewModel.java
@@ -1,22 +1,62 @@
 package org.jabref.gui.integrity;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 
 import org.jabref.gui.AbstractViewModel;
+import org.jabref.gui.DialogService;
 import org.jabref.logic.integrity.IntegrityMessage;
+import org.jabref.model.entry.field.Field;
+import org.jabref.model.entry.field.StandardField;
 
 public class IntegrityCheckDialogViewModel extends AbstractViewModel {
 
     private final ObservableList<IntegrityMessage> messages;
+    private final ObservableSet<String> entryTypes;
 
-    public IntegrityCheckDialogViewModel(List<IntegrityMessage> messages) {
+    private final DialogService dialogService;
+
+    public IntegrityCheckDialogViewModel(List<IntegrityMessage> messages, DialogService dialogService) {
         this.messages = FXCollections.observableArrayList(messages);
+        this.dialogService = dialogService;
+
+        Set<String> types = messages.stream()
+                                    .map(item -> item.field().getDisplayName())
+                                    .collect(Collectors.toSet());
+        this.entryTypes = FXCollections.observableSet(types);
     }
 
     public ObservableList<IntegrityMessage> getMessages() {
         return messages;
+    }
+
+    public Set<String> getEntryTypes() {
+        return entryTypes;
+    }
+
+    public void removeFromEntryTypes(String entry) {
+        entryTypes.remove(entry);
+    }
+
+    public void fixTitle(IntegrityMessage message) {
+        String title = message.entry().getTitle().get();
+        StringBuilder result = new StringBuilder();
+        for (char ch : title.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                result.append("{").append(ch).append("}");
+            } else {
+                result.append(ch);
+            }
+        }
+        Map<Field, String> fields = new HashMap<>();
+        fields.put(StandardField.TITLE, result.toString());
+        message.entry().setField(fields);
     }
 }
