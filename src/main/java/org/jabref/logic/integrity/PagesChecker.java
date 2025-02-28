19a78811287c5f6ba258c0cf72318b3b5d3458b0
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-02-28 07:46:19 +0530

    Revert "Fix the checkstyle check"
    This reverts commit 102e7894f0ccd3a8ffb4a2a86ad8de5610d65b66.

--- src/main/java/org/jabref/logic/integrity/PagesChecker.java
+++ src/main/java/org/jabref/logic/integrity/PagesChecker.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
+import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.strings.StringUtil;
 
