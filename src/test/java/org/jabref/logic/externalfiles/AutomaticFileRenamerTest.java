d02f1af6a5d6e0f175d445c55a355e10dbd49988
Jiewu (jack deng) Deng <jiewu.deng@kcl.ac.uk>
2025-03-02 20:44:24 +0000

    fix: remove unused import in AutomaticFileRenamerTest

--- src/test/java/org/jabref/logic/externalfiles/AutomaticFileRenamerTest.java
+++ src/test/java/org/jabref/logic/externalfiles/AutomaticFileRenamerTest.java
@@ -29,8 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.stream.Stream;
-
 class AutomaticFileRenamerTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AutomaticFileRenamerTest.class);
