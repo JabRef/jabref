af0c3c8ebf4a51a0301520882b2919d6489f85b2
Raphael Ahiable <raphahi@hotmail.com>
2025-03-03 16:58:36 +0000

    Add git preferences

---
+++ src/main/java/org/jabref/logic/git/GitPreferences.java
@@ -0,0 +1,25 @@
+package org.jabref.logic.git;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GitPreferences {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GitPreferences.class);
+
+    private final BooleanProperty autoPushEnabled;
+
+    public GitPreferences(boolean autoPushEnabled) {
+        this.autoPushEnabled = new SimpleBooleanProperty(autoPushEnabled);
+    }
+
+    public boolean getAutoPushEnabled() {
+        return autoPushEnabled.get();
+    }
+
+    public BooleanProperty getAutoPushEnabledProperty() {
+        return autoPushEnabled;
+    }
+}
