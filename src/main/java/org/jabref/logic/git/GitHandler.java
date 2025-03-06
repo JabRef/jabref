796f1c7326e39825aa76521697241ffc6c1afdb8
Jiewu (jack deng) Deng <jiewu.deng@kcl.ac.uk>
2025-03-06 23:29:50 +0000

    change method isGitRepository access specifier to public

--- src/main/java/org/jabref/logic/git/GitHandler.java
+++ src/main/java/org/jabref/logic/git/GitHandler.java
@@ -91,7 +91,7 @@ public class GitHandler {
     /**
      * Returns true if the given path points to a directory that is a git repository (contains a .git folder)
      */
-    boolean isGitRepository() {
+    public boolean isGitRepository() {
         // For some reason the solution from https://www.eclipse.org/lists/jgit-dev/msg01892.html does not work
         // This solution is quite simple but might not work in special cases, for us it should suffice.
         return Files.exists(Path.of(repositoryPath.toString(), ".git"));
