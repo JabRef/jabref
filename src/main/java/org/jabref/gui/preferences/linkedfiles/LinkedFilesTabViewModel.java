5036cf573f1b753418452dad1fb97850ff80ac3a
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-03-07 22:55:54 +0530

    Remove commented method

--- src/main/java/org/jabref/gui/preferences/linkedfiles/LinkedFilesTabViewModel.java
+++ src/main/java/org/jabref/gui/preferences/linkedfiles/LinkedFilesTabViewModel.java
@@ -187,10 +187,6 @@ public class LinkedFilesTabViewModel implements PreferenceTabViewModel {
         return defaultFileNamePatternsProperty;
     }
 
-    /* public StringProperty fileNamePatternProperty() {
-        return fileNamePatternProperty;
-    }*/
-
     public StringProperty fileDirectoryPatternProperty() {
         return fileDirectoryPatternProperty;
     }
