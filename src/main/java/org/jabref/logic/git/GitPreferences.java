3ff8bc3c232b4bc09ae22b1eabf12eac7ccefa86
Raphael Ahiable <raphahi@hotmail.com>
2025-03-10 00:00:00 +0000

    Add autoPushMode to GitPreferences

--- src/main/java/org/jabref/logic/git/GitPreferences.java
+++ src/main/java/org/jabref/logic/git/GitPreferences.java
@@ -1,7 +1,11 @@
 package org.jabref.logic.git;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import org.jabref.logic.preferences.AutoPushMode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +14,11 @@ public class GitPreferences {
     private static final Logger LOGGER = LoggerFactory.getLogger(GitPreferences.class);
 
     private final BooleanProperty autoPushEnabled;
+    private final ObjectProperty<AutoPushMode> autoPushMode;
 
-    public GitPreferences(boolean autoPushEnabled) {
+    public GitPreferences(boolean autoPushEnabled, AutoPushMode autoPushMode) {
         this.autoPushEnabled = new SimpleBooleanProperty(autoPushEnabled);
+        this.autoPushMode = new SimpleObjectProperty<>(autoPushMode);
     }
 
     public boolean getAutoPushEnabled() {
@@ -29,5 +35,13 @@ public class GitPreferences {
 
     public GitPreferences withAutoPushEnabled(boolean enabled) {
         return new GitPreferences(enabled);
+    }
+
+    public AutoPushMode getAutoPushMode() {
+        return autoPushMode.get();
+    }
+
+    public ObjectProperty<AutoPushMode> getAutoPushModeProperty() {
+        return autoPushMode;
     }
 }
