f889c3803e31adcccd01b9660c2beff15fe3dcef
Jiewu (jack deng) Deng <jiewu.deng@kcl.ac.uk>
2025-03-12 18:45:27 +0000

    Revert "Revert "using localisation in the getGitStatusText instead of hardcoing English string,restore removed updatetaptitle method and integrate showing git status on taptitle feature to the method""
    This reverts commit 700d6f22ca1485634be040fc67d2a0ecaf920283.

--- src/main/java/org/jabref/gui/importer/actions/CheckForVersionControlAction.java
+++ src/main/java/org/jabref/gui/importer/actions/CheckForVersionControlAction.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import org.jabref.gui.DialogService;
 import org.jabref.logic.git.GitHandler;
 import org.jabref.logic.importer.ParserResult;
+import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.preferences.CliPreferences;
 
 import org.slf4j.Logger;
