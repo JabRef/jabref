17d6cdb7e833b130473f304a893bbec5ad01b73f
Rishivardhan <mmrishivardhan@gmail.com>
2025-03-17 23:20:24 +0530

    fixing dropdown arrow miss alignment in "Search for unlinked local files" dialog

--- src/main/java/org/jabref/gui/externalfiles/UnlinkedFilesDialogView.java
+++ src/main/java/org/jabref/gui/externalfiles/UnlinkedFilesDialogView.java
@@ -269,6 +269,8 @@ public class UnlinkedFilesDialogView extends BaseDialog<Void> {
     @FXML
     void startImport() {
         viewModel.startImport();
+
+        // Already imported files should not be re-added at a second click on "Import". Therefore, all imported files are unchecked.
         unlinkedFilesList.getCheckModel().clearChecks();
 
         Platform.runLater(() -> {
