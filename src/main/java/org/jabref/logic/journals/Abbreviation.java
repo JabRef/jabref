b1fecacf1c928a0797402bd37b1e5641cf77974b
Marwan Emad <marwanemad910@gmail.com>
2025-03-17 22:42:58 +0200

    Remove transient types

--- src/main/java/org/jabref/logic/journals/Abbreviation.java
+++ src/main/java/org/jabref/logic/journals/Abbreviation.java
@@ -11,9 +11,9 @@ public class Abbreviation implements Comparable<Abbreviation>, Serializable {
 
     private static final long serialVersionUID = 1;
 
-    private transient final String name;
+    private final String name;
     private final String abbreviation;
-    private transient final String dotlessAbbreviation;
+    private final String dotlessAbbreviation;
 
     // Is the empty string if not available
     private String shortestUniqueAbbreviation;
