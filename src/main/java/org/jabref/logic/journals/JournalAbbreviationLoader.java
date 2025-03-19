57f614e6e4b5b053e1942f06a287602054f4c42a
Oliver Kopp <kopp.dev@gmail.com>
2025-03-19 21:04:35 +0100

    Fix checkstyle

--- src/main/java/org/jabref/logic/journals/JournalAbbreviationLoader.java
+++ src/main/java/org/jabref/logic/journals/JournalAbbreviationLoader.java
@@ -1,8 +1,5 @@
 package org.jabref.logic.journals;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -11,6 +8,9 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>
@@ -62,8 +62,8 @@ public class JournalAbbreviationLoader {
             for (String filename : lists) {
                 try {
                     repository.addCustomAbbreviations(readAbbreviationsFromCsvFile(Path.of(filename)));
-                    // invalid path might come from unix/windows mixup of prefs
                 } catch (IOException | InvalidPathException e) {
+                    // invalid path might come from unix/windows mixup of prefs
                     LOGGER.error("Cannot read external journal list file {}", filename, e);
                 }
             }
