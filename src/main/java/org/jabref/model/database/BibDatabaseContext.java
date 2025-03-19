1bdecbbb8ef9863325a03a7584e69f2f57ccd9b0
Jiewu (jack deng) Deng <jiewu.deng@kcl.ac.uk>
2025-03-19 13:02:06 +0000

    fix unit test problem use Paths.of() instead of Paths.get() in getGitStatus() method

--- src/main/java/org/jabref/model/database/BibDatabaseContext.java
+++ src/main/java/org/jabref/model/database/BibDatabaseContext.java
@@ -2,7 +2,6 @@ package org.jabref.model.database;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -291,7 +290,7 @@ public class BibDatabaseContext {
             GitHandler gitHandler = new GitHandler(databasePath);
             return gitHandler.getFileStatus(databasePath);
         } catch (SecurityException e) {
-            LOGGER.warn("No permission to check Git status at {}", getDatabasePath().orElse(Paths.get("unknown")), e);
+            LOGGER.warn("No permission to check Git status at {}", getDatabasePath().orElse(Path.of("unknown")), e);
             return Optional.empty();
         } catch (IllegalStateException e) {
             LOGGER.warn("Failed to retrieve database path: {}", e);
