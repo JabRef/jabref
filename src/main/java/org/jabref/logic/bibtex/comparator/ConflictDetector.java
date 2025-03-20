3c9ef55f2de814f291c8cd01ddb885b653e1c5ee
RapidShotzz <arjan.bedi@kcl.ac.uk>
2025-03-20 14:37:39 +0000

    amended conflict detector class to only handle git conflict detection

--- src/main/java/org/jabref/logic/bibtex/comparator/ConflictDetector.java
+++ src/main/java/org/jabref/logic/bibtex/comparator/ConflictDetector.java
@@ -7,15 +7,6 @@ import org.jabref.model.database.BibDatabaseContext;
 
 public class ConflictDetector {
 
-    public static Optional<BibDatabaseDiff> detectConflicts(BibDatabaseContext local, BibDatabaseContext remote) {
-        BibDatabaseDiff diff = BibDatabaseDiff.compare(local, remote);
-
-        if (!diff.getEntryDifferences().isEmpty() || diff.getMetaDataDifferences().isPresent()) {
-            return Optional.of(diff);
-        }
-        return Optional.empty();
-    }
-
     public static Optional<GitBibDatabaseDiff> detectGitConflicts(BibDatabaseContext base, BibDatabaseContext local, BibDatabaseContext remote) {
         GitBibDatabaseDiff diff = GitBibDatabaseDiff.compare(base, local, remote);
 
@@ -27,16 +18,8 @@ public class ConflictDetector {
         return Optional.empty();
     }
 
-    public static boolean hasConflicts(BibDatabaseContext local, BibDatabaseContext remote) {
-        return detectConflicts(local, remote).isPresent();
-    }
-
     public static boolean hasGitConflicts(BibDatabaseContext base, BibDatabaseContext local, BibDatabaseContext remote) {
         return detectGitConflicts(base, local, remote).isPresent();
-    }
-
-    public static List<BibEntryDiff> getEntryConflicts(BibDatabaseContext local, BibDatabaseContext remote) {
-        return BibDatabaseDiff.compare(local, remote).getEntryDifferences();
     }
 
     public static List<GitBibEntryDiff> getGitEntryConflicts(BibDatabaseContext base, BibDatabaseContext local, BibDatabaseContext remote) {
