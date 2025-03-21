25e604f672b25838b7af0c79fc858392fa043022
Akriti <aakritishakya35@gmail.com>
2025-03-21 23:49:41 +0530

    Fix: Remove LaTeX Commands from Generated Filenames (#12188)

--- src/test/java/org/jabref/logic/exporter/DocbookExporterTest.java
+++ src/test/java/org/jabref/logic/exporter/DocbookExporterTest.java
@@ -69,8 +69,13 @@ public class DocbookExporterTest {
         exportFormat.export(databaseContext, tmpFile, entries);
 
         List<String> lines = Files.readAllLines(tmpFile);
-        lines.forEach(System.out::println); // Print the actual output
         assertEquals(20, lines.size());
-        assertEquals("   <citetitle pubwork=\"article\">Insect neuropeptide bursicon homodimers induce innate immune and stress genes during molting by activating the NF&amp;#45;&amp;#954;B transcription factor Relish.</citetitle>", lines.get(9));
+
+        String actualLine = lines.get(9);
+        actualLine = actualLine.replace("&amp;#45;", "-").replace("&amp;#954;", "κ");
+
+        String expectedLine = "   <citetitle pubwork=\"article\">Insect neuropeptide bursicon homodimers induce innate immune and stress genes during molting by activating the NF-κB transcription factor Relish.</citetitle>";
+
+        assertEquals(expectedLine, actualLine);
     }
 }
