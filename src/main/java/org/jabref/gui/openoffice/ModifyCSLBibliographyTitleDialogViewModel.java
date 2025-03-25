3fd34a46ad4603fa527bcdc7c6fbc6d4c50469b5
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-03-25 20:06:09 +0530

    Update ModifyCSLBibliographyTitleDialogViewModel.java
    Co-authored-by: Subhramit Basu <subhramit.bb@live.in>

--- src/main/java/org/jabref/gui/openoffice/ModifyCSLBibliographyTitleDialogViewModel.java
+++ src/main/java/org/jabref/gui/openoffice/ModifyCSLBibliographyTitleDialogViewModel.java
@@ -33,8 +33,8 @@ public class ModifyCSLBibliographyTitleDialogViewModel {
         return cslBibliographyTitle;
     }
 
-    public StringProperty cslBibliographySelectedHeaderFormat() {
-        return this.cslBibliographySelectedHeaderFormat;
+    public StringProperty getCslBibliographySelectedHeaderFormat() {
+        return this.cslBibliographySelectedHeaderFormat.get();
     }
 
     public ReadOnlyListProperty<String> formatListProperty() {
