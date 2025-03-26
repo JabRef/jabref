2501beaf9ed0fa3c2d8594486468c92a5d21457d
Guilherme Ribeiro Pereira <guilherme.ribeiro.pereira@tecnico.ulisboa.pt>
2025-03-26 16:29:31 +0000

    Fix crash when scrolling through cited by in Citation relations
    Uses Webview instead of Label and TextFlowLimited
    when trying to display right to left text
    (like persian or arabic) in BibEntryView. This resolves the
    issue caused by a javafx bug that causes an exception when
    rendering wrapped RTL text.
    Fixes #12410

--- src/main/java/org/jabref/gui/entryeditor/citationrelationtab/BibEntryView.java
+++ src/main/java/org/jabref/gui/entryeditor/citationrelationtab/BibEntryView.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
 
 import org.jabref.gui.icon.IconTheme;
 import org.jabref.gui.util.TextFlowLimited;
@@ -32,15 +33,16 @@ public class BibEntryView {
     public static Node getEntryNode(BibEntry entry) {
         Node entryType = getIcon(entry.getType()).getGraphicNode();
         entryType.getStyleClass().add("type");
-        Label authors = new Label(entry.getFieldOrAliasLatexFree(StandardField.AUTHOR).orElse(""));
+        String authorsText = entry.getFieldOrAliasLatexFree(StandardField.AUTHOR).orElse("");
+        Node authors = createTextNode(authorsText, "authors");
         authors.getStyleClass().add("authors");
-        authors.setWrapText(true);
-        Label title = new Label(entry.getFieldOrAliasLatexFree(StandardField.TITLE).orElse(""));
+        String titleText = entry.getFieldOrAliasLatexFree(StandardField.TITLE).orElse("");
+        Node title = createTextNode(titleText, "title");
         title.getStyleClass().add("title");
-        title.setWrapText(true);
         Label year = new Label(entry.getFieldOrAliasLatexFree(StandardField.YEAR).orElse(""));
         year.getStyleClass().add("year");
-        Label journal = new Label(entry.getFieldOrAliasLatexFree(StandardField.JOURNAL).orElse(""));
+        String journalText = entry.getFieldOrAliasLatexFree(StandardField.JOURNAL).orElse("");
+        Node journal = createTextNode(journalText, "journal");
         journal.getStyleClass().add("journal");
 
         VBox entryContainer = new VBox(
@@ -48,8 +50,9 @@ public class BibEntryView {
                 new HBox(5, year, journal),
                 authors
         );
+
         entry.getFieldOrAliasLatexFree(StandardField.ABSTRACT).ifPresent(summaryText -> {
-            TextFlowLimited summary = new TextFlowLimited(new Text(summaryText));
+            Node summary = createTextNode(summaryText, "summary");
             summary.getStyleClass().add("summary");
             entryContainer.getChildren().add(summary);
         });
@@ -73,5 +76,34 @@ public class BibEntryView {
             }
         }
         return IconTheme.JabRefIcons.ARTICLE;
+    }
+
+    private static boolean isRTL(String text) {
+        for (char c : text.toCharArray()) {
+            if (Character.getDirectionality(c) == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
+                    Character.getDirectionality(c) == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC ||
+                    Character.getDirectionality(c) == Character.DIRECTIONALITY_RIGHT_TO_LEFT_EMBEDDING ||
+                    Character.getDirectionality(c) == Character.DIRECTIONALITY_RIGHT_TO_LEFT_OVERRIDE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Node createTextNode(String text, String fieldName) {
+        if (isRTL(text)) {
+            WebView webView = new WebView();
+            webView.getEngine().loadContent(
+                    "<html><body dir='rtl'>" + text + "</body></html>"
+            );
+            webView.setPrefSize(200, 38);
+            return webView;
+        } else if (fieldName.equals("summary")) {
+            return new TextFlowLimited(new Text(text));
+        } else {
+            Label label = new Label(text);
+            label.setWrapText(true);
+            return label;
+        }
     }
 }
