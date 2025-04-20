5a63b71495045c316fd39f637f7541d41b5675c8
Rajas <rajasyardi18@gmail.com>
2025-04-20 18:36:35 -0400

    Fix unused import to pass Checkstyle

--- src/main/java/org/jabref/model/entry/KeywordList.java
+++ src/main/java/org/jabref/model/entry/KeywordList.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
