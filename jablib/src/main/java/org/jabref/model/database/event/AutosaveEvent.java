05a86fbb59baff12cdecbee0a7f8470b3ab30256
Oliver Kopp <kopp.dev@gmail.com>
2025-04-21 01:40:13 +0200

    Fix import

--- jablib/src/main/java/org/jabref/model/database/event/AutosaveEvent.java
+++ jablib/src/main/java/org/jabref/model/database/event/AutosaveEvent.java
@@ -1,9 +1,7 @@
 package org.jabref.model.database.event;
 
-import org.jabref.gui.autosaveandbackup.AutosaveManager;
-
 /**
- * This Event is fired from {@link AutosaveManager} in case that a save task is pending.
+ * This Event is fired from {@link org.jabref.gui.autosaveandbackup.AutosaveManager} in case that a save task is pending.
  */
 public class AutosaveEvent {
     // no data
