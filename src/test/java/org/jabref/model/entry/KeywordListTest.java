35d6a345e3ed664099889c031a842b0e9165a7e7
Rajas <rajasyardi18@gmail.com>
2025-04-23 03:37:48 -0400

    chore: remove unused import to fix checkstyle error

--- src/test/java/org/jabref/model/entry/KeywordListTest.java
+++ src/test/java/org/jabref/model/entry/KeywordListTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class KeywordListTest {
 
