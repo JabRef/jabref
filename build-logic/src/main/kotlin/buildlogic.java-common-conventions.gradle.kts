f0660068ecc8bb727796ac48700990d5a44831b5
Oliver Kopp <kopp.dev@gmail.com>
2025-04-26 01:25:26 +0200

    Load Liberica JDK also in Gradle

--- build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -49,6 +49,8 @@ java {
         // - mise.toml
         languageVersion = JavaLanguageVersion.of(24)
         // See https://docs.gradle.org/current/javadoc/org/gradle/jvm/toolchain/JvmVendorSpec.html for a full list
-        // vendor = JvmVendorSpec.AMAZON
+        // See https://docs.gradle.org/current/javadoc/org/gradle/jvm/toolchain/JvmVendorSpec.html for a full list
+        // Temurin does not ship jmods, thus we need to use another JDK -- see https://github.com/actions/setup-java/issues/804
+        vendor = JvmVendorSpec.AZUL
     }
 }
