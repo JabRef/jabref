08454e2b75481d1d708236cad2257baf6a59c591
Oliver Kopp <kopp.dev@gmail.com>
2025-04-27 00:52:08 +0200

    Fix checkstyle

--- jablib/src/main/java/org/jabref/logic/importer/fileformat/ModsImporter.java
+++ jablib/src/main/java/org/jabref/logic/importer/fileformat/ModsImporter.java
@@ -14,7 +14,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
