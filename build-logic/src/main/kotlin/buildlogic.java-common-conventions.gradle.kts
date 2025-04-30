aac904ff09c5bc837f16f57358c89a36ef13be4f
Oliver Kopp <kopp.dev@gmail.com>
2025-04-30 12:46:31 +0200

    Use mise and JDK25-ea

--- build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -35,6 +35,8 @@ testing {
 }
 
 java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
     toolchain {
         // If this is updated, also update
         // - build.gradle -> jacoco -> toolVersion (because JaCoCo does not support newest JDK out of the box. Check versions at https://www.jacoco.org/jacoco/trunk/doc/changes.html)
@@ -45,10 +47,10 @@ java {
         // - .github/workflows/update-gradle-wrapper.yml
         // - docs/getting-into-the-code/guidelines-for-setting-up-a-local-workspace/intellij-12-build.md
         // - mise.toml
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
         // See https://docs.gradle.org/current/javadoc/org/gradle/jvm/toolchain/JvmVendorSpec.html for a full list
         // See https://docs.gradle.org/current/javadoc/org/gradle/jvm/toolchain/JvmVendorSpec.html for a full list
         // Temurin does not ship jmods, thus we need to use another JDK -- see https://github.com/actions/setup-java/issues/804
-        vendor = JvmVendorSpec.AZUL
+        // vendor = JvmVendorSpec.AZUL
     }
 }
