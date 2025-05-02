1cee088a28c43713ea0fe2a16162574afbfcf58b
Oliver Kopp <kopp.dev@gmail.com>
2025-05-02 09:51:09 +0200

    Fix path

--- jabsrv/src/test/java/org/jabref/http/server/LibrariesResourceTest.java
+++ jabsrv/src/test/java/org/jabref/http/server/LibrariesResourceTest.java
@@ -1,7 +1,6 @@
 package org.jabref.http.server;
 
 import java.util.EnumSet;
-import java.util.stream.Collectors;
 
 import jakarta.ws.rs.core.Application;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -22,15 +21,23 @@ class LibrariesResourceTest extends ServerTest {
 
     @Test
     void defaultOneTestLibrary() {
-        assertEquals("[\"" + TestBibFile.GENERAL_SERVER_TEST.id + "\"]", target("/libraries").request().get(String.class));
+        String expected = String.format("""
+                [
+                    "%s"
+                ]""", TestBibFile.GENERAL_SERVER_TEST.id);
+        assertEquals(expected, target("/libraries").request().get(String.class));
     }
 
     @Test
     void twoTestLibraries() {
         EnumSet<TestBibFile> availableLibraries = EnumSet.of(TestBibFile.GENERAL_SERVER_TEST, TestBibFile.CHOCOLATE_BIB);
         setAvailableLibraries(availableLibraries);
-        // We cannot use a string constant as the path changes from OS to OS. Therefore, we need to dynamically create the expected result.
-        String expected = availableLibraries.stream().map(file -> file.id).collect(Collectors.joining("\",\"", "[\"", "\"]"));
+
+        String expected = String.format("""
+                [
+                    "%s",
+                    "%s
+                ]""", TestBibFile.GENERAL_SERVER_TEST.id, TestBibFile.CHOCOLATE_BIB.id);
         assertEquals(expected, target("/libraries").request().get(String.class));
     }
 }
--- jabsrv/src/test/java/org/jabref/http/server/TestBibFile.java
+++ jabsrv/src/test/java/org/jabref/http/server/TestBibFile.java
@@ -4,6 +4,9 @@ import java.nio.file.Path;
 
 import org.jabref.logic.util.io.BackupFileUtil;
 
+/// Holds information about test .bib files
+///
+/// We cannot use a string constant as the path changes from OS to OS. Therefore, we need to dynamically create the expected result.
 public enum TestBibFile {
     GENERAL_SERVER_TEST("src/test/resources/org/jabref/http/server/general-server-test.bib"),
     CHOCOLATE_BIB("src/main/resources/chocolate.bib");
