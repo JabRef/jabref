de1f4abc023301c149b553fcba9452c5f2b3aaa0
subhramit <subhramit.bb@live.in>
2025-05-07 03:55:02 +0530

    wip
    Signed-off-by: subhramit <subhramit.bb@live.in>

---
+++ jabgui/src/main/java/org/jabref/gui/openoffice/ModifyCSLBibliographyPropertiesDialogViewModel.java
@@ -0,0 +1,57 @@
+package org.jabref.gui.openoffice;
+
+import java.util.Arrays;
+
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.property.ReadOnlyListWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+
+import org.jabref.logic.openoffice.OpenOfficePreferences;
+import org.jabref.logic.openoffice.oocsltext.CSLFormatUtils;
+
+public class ModifyCSLBibliographyPropertiesDialogViewModel {
+
+    private final StringProperty cslBibliographyTitle = new SimpleStringProperty();
+    private final StringProperty cslBibliographySelectedHeaderFormat = new SimpleStringProperty();
+    private final StringProperty cslBibliographySelectedBodyFormat = new SimpleStringProperty();
+    private final ReadOnlyListProperty<String> headerFormatListProperty =
+             new ReadOnlyListWrapper<>(FXCollections.observableArrayList(
+                    Arrays.stream(CSLFormatUtils.BibliographyTitleFormat.values()).map(CSLFormatUtils.BibliographyTitleFormat::getFormat).toList()
+            ));
+    private final ReadOnlyListProperty<String> bodyFormatListProperty =
+            new ReadOnlyListWrapper<>(FXCollections.observableArrayList(
+                    Arrays.stream(CSLFormatUtils.BibliographyBodyFormat.values()).map(CSLFormatUtils.BibliographyBodyFormat::getFormat).toList()
+            ));
+
+    public ModifyCSLBibliographyPropertiesDialogViewModel(OpenOfficePreferences openOfficePreferences) {
+        this.cslBibliographyTitle.set(openOfficePreferences.getCslBibliographyTitle());
+        this.cslBibliographySelectedHeaderFormat.set(openOfficePreferences.getCslBibliographyHeaderFormat());
+        this.cslBibliographySelectedBodyFormat.set(openOfficePreferences.getCslBibliographyBodyFormat());
+
+        cslBibliographyTitle.bindBidirectional(openOfficePreferences.cslBibliographyTitleProperty());
+        cslBibliographySelectedHeaderFormat.bindBidirectional(openOfficePreferences.cslBibliographyHeaderFormatProperty());
+        cslBibliographySelectedBodyFormat.bindBidirectional(openOfficePreferences.cslBibliographyBodyFormatProperty());
+    }
+
+    public StringProperty cslBibliographyTitleProperty() {
+        return cslBibliographyTitle;
+    }
+
+    public StringProperty cslBibliographySelectedHeaderFormatProperty() {
+        return cslBibliographySelectedHeaderFormat;
+    }
+
+    public ReadOnlyListProperty<String> headerFormatListProperty() {
+        return headerFormatListProperty;
+    }
+
+    public StringProperty cslBibliographySelectedBodyFormatProperty() {
+        return cslBibliographySelectedBodyFormat;
+    }
+
+    public ReadOnlyListProperty<String> bodyFormatListProperty() {
+        return bodyFormatListProperty;
+    }
+}
