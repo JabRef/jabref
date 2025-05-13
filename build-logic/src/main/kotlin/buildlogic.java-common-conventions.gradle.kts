c367d1055947fde97e607b3cb708b0842c00ee16
Oliver Kopp <kopp.dev@gmail.com>
2025-05-13 23:09:47 +0200

    Remove version number at Juptier dependency

--- build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -85,7 +85,7 @@ extraJavaModuleInfo {
 testing {
     suites {
         val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter("5.12.2")
+            useJUnitJupiter()
         }
     }
 }
