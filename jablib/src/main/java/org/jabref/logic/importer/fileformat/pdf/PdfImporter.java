bd31582ee9366acc7b708e8e37ce66d60c3bb927
Oliver Kopp <kopp.dev@gmail.com>
2025-05-16 22:17:13 +0200

    Fix checkstyle

--- jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfImporter.java
+++ jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfImporter.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.jabref.logic.importer.Importer;
 import org.jabref.logic.importer.ParseException;
 import org.jabref.logic.importer.ParserResult;
-import org.jabref.logic.importer.fileformat.PdfMergeMetadataImporter;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.util.StandardFileType;
 import org.jabref.logic.xmp.EncryptedPdfsNotSupportedException;
@@ -21,13 +20,13 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 /// which allows user for more fine-grained control of how [BibEntry] is extracted from a PDF file.
 ///
 /// [PdfImporter]s are used in two places in JabRef:
-/// 1. [PdfMergeMetadataImporter]: uses several [PdfImporter] and automatically
+/// 1. [org.jabref.logic.importer.fileformat.PdfMergeMetadataImporter]: uses several [PdfImporter] and automatically
 ///    merges them into 1 [BibEntry].
 /// 2. `org.jabref.gui.externalfiles.PdfMergeDialog` also uses several [PdfImporter], but
 ///    it shows a merge dialog (instead of automatic merging).
 ///
 /// Note, that this step should not add PDF file to [BibEntry], it will be finally added either in
-/// [#importDatabase(Path)] or [PdfMergeMetadataImporter].
+/// [#importDatabase(Path)] or [org.jabref.logic.importer.fileformat.PdfMergeMetadataImporter].
 public abstract class PdfImporter extends Importer {
     public abstract List<BibEntry> importDatabase(Path filePath, PDDocument document) throws IOException, ParseException;
 
