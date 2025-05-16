1142edb7825bde97cb7c169380136b49199c7eb9
Oliver Kopp <kopp.dev@gmail.com>
2025-05-16 22:54:09 +0200

    Add tinylog.properties

--- .jbang/JabKitLauncher.java
+++ .jbang/JabKitLauncher.java
@@ -16,6 +16,7 @@
 //SOURCES ../jabkit/src/main/java/org/jabref/cli/Preferences.java
 //SOURCES ../jabkit/src/main/java/org/jabref/cli/Search.java
 //SOURCES ../jabkit/src/main/java/org/jabref/JabKit.java
+//FILES ../jabkit/src/main/resources/tinylog.properties
 
 //REPOS mavencentral,mavencentralsnapshots=https://central.sonatype.com/repository/maven-snapshots/,s01oss=https://s01.oss.sonatype.org/content/repositories/snapshots/,oss=https://oss.sonatype.org/content/repositories,jitpack=https://jitpack.io,oss2=https://oss.sonatype.org/content/groups/public,ossrh=https://oss.sonatype.org/content/repositories/snapshots
 
