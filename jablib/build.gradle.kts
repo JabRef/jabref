ab747bdd40fb2c83a58a3814a1c2836c67a405f4
Siedlerchr <siedlerkiller@gmail.com>
2025-05-19 21:27:36 +0200

    use bom for langhchain

--- jablib/build.gradle.kts
+++ jablib/build.gradle.kts
@@ -1,8 +1,9 @@
+
 import com.vanniktech.maven.publish.JavaLibrary
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import java.util.Calendar;
+import java.util.Calendar
 
 plugins {
     id("buildlogic.java-common-conventions")
@@ -148,23 +149,23 @@ dependencies {
     implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
 
     // region AI
-    implementation("dev.langchain4j:langchain4j:1.0.0")
+    implementation("dev.langchain4j:langchain4j-bom:1.0.0")
     // Even though we use jvm-openai for LLM connection, we still need this package for tokenization.
-    implementation("dev.langchain4j:langchain4j-open-ai:1.0.0") {
+    implementation("dev.langchain4j:langchain4j-open-ai") {
         exclude(group = "com.squareup.okhttp3")
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
         exclude(group = "org.jetbrains.kotlin")
     }
-    implementation("dev.langchain4j:langchain4j-mistral-ai:1.0.0") {
+    implementation("dev.langchain4j:langchain4j-mistral-ai") {
         exclude(group = "com.squareup.okhttp3")
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
         exclude(group = "org.jetbrains.kotlin")
     }
-    implementation("dev.langchain4j:langchain4j-google-ai-gemini:1.0.0") {
+    implementation("dev.langchain4j:langchain4j-google-ai-gemini") {
         exclude(group = "com.squareup.okhttp3")
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
     }
-    implementation("dev.langchain4j:langchain4j-hugging-face:1.0.0") {
+    implementation("dev.langchain4j:langchain4j-hugging-face") {
         exclude(group = "com.squareup.okhttp3")
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
         exclude(group = "org.jetbrains.kotlin")
