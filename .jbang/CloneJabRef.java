f86f0d3504722fc2f367bfa4eebdc2e17896c634
Oliver Kopp <kopp.dev@gmail.com>
2025-05-23 14:46:58 +0200

    Add support for path as first argument

--- .jbang/CloneJabRef.java
+++ .jbang/CloneJabRef.java
@@ -9,15 +9,21 @@ import java.nio.file.Path;
 //DEPS org.eclipse.jgit:org.eclipse.jgit.pgm:7.2.1.202505142326-r
 
 public class CloneJabRef {
-    public static void main(String[] noArgs) throws Exception {
-        Path targetDir = Path.of(System.getProperty("java.io.tmpdir"), "JabRef");
+    public static void main(String[] args) throws Exception {
+        Path targetDir;
+        if (args.length == 1) {
+            targetDir = Path.of(args[1]).toAbsolutePath();
+        } else {
+            targetDir = Path.of(System.getProperty("java.io.tmpdir"), "JabRef");
+        }
+
         if (Files.exists(targetDir)) {
             System.out.println("Directory already exists: " + targetDir);
             return;
         }
 
-        String[] args = {"clone", "https://github.com/JabRef/jabref.git", "--recurse-submodules", targetDir.toString()};
-        org.eclipse.jgit.pgm.Main.main(args);
+        String[] jGitArgs = {"clone", "https://github.com/JabRef/jabref.git", "--recurse-submodules", targetDir.toString()};
+        org.eclipse.jgit.pgm.Main.main(jGitArgs);
 
         System.out.println("JabRef code available at: " + targetDir);
     }
