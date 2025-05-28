bf2ef67b34c889a2f553b463fed105ae7c4548ce
Aspasia Tsagkaropoulou <t8220228@aueb.gr>
2025-05-28 15:10:19 +0300

    Initialize JournalAbbreviationValidatorTest class

---
+++ src/test/java/org/jabref/logic/journals/JournalAbbreviationValidatorTest.java
@@ -0,0 +1,13 @@
+package org.jabref.logic.journals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class JournalAbbreviationValidatorTest {
+
+    private JournalAbbreviationValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new JournalAbbreviationValidator();
+    }
+}
