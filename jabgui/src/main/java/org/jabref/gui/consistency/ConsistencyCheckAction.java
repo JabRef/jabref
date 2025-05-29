50c605c6d421110c871b41c7f47c38df52448f08
Christoph <siedlerkiller@gmail.com>
2025-05-29 14:12:31 +0200

    Add progess output for consisteny check (#13181)
    * Add progess output for consisteny check
    
    Fixes https://github.com/JabRef/jabref/issues/12487
    
    * better handling of not present
    
    * checkstyle
    
    * fix test
    
    * Revert some intellij formatting changes
    
    * Update ArgumentProcessorTest.java
    
    ---------
    
    Co-authored-by: Oliver Kopp <kopp.dev@gmail.com>
    Co-authored-by: Subhramit Basu <subhramit.bb@live.in>

--- jabgui/src/main/java/org/jabref/gui/consistency/ConsistencyCheckAction.java
+++ jabgui/src/main/java/org/jabref/gui/consistency/ConsistencyCheckAction.java
@@ -1,7 +1,10 @@
 package org.jabref.gui.consistency;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
+
+import javafx.concurrent.Task;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.LibraryTab;
@@ -11,7 +14,6 @@ import org.jabref.gui.preferences.GuiPreferences;
 import org.jabref.gui.util.UiTaskExecutor;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.quality.consistency.BibliographyConsistencyCheck;
-import org.jabref.logic.util.BackgroundTask;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibEntryTypesManager;
@@ -45,22 +47,39 @@ public class ConsistencyCheckAction extends SimpleCommand {
 
     @Override
     public void execute() {
-        BackgroundTask.wrap(() -> {
-            BibDatabaseContext databaseContext = stateManager.getActiveDatabase()
-                                                             .orElseThrow(() -> new NullPointerException("Database null"));
-            List<BibEntry> entries = databaseContext.getDatabase().getEntries();
+        Task<BibliographyConsistencyCheck.Result> task = new Task<>() {
+            @Override
+            public BibliographyConsistencyCheck.Result call() throws Exception {
 
-            BibliographyConsistencyCheck consistencyCheck = new BibliographyConsistencyCheck();
-            return consistencyCheck.check(entries);
-        }).onSuccess(result -> {
-            if (result.entryTypeToResultMap().isEmpty()) {
+                Optional<BibDatabaseContext> databaseContext = stateManager.getActiveDatabase();
+                if (databaseContext.isEmpty()) {
+                    throw new IllegalStateException((Localization.lang("No library present")));
+                }
+                List<BibEntry> entries = databaseContext.get().getEntries();
+
+                BibliographyConsistencyCheck consistencyCheck = new BibliographyConsistencyCheck();
+                return consistencyCheck.check(entries, (count, total) ->
+                        UiTaskExecutor.runInJavaFXThread(() -> {
+                            updateProgress(count, total);
+                            updateMessage(Localization.lang("%0/%1 entry types", count + 1, total));
+                        }));
+            }
+        };
+
+        task.setOnFailed(_ -> dialogService.showErrorDialogAndWait(Localization.lang("Consistency check failed."), task.getException()));
+        task.setOnSucceeded(_ -> {
+            if (task.getValue().entryTypeToResultMap().isEmpty()) {
                 dialogService.notify(Localization.lang("No problems found."));
             } else {
                 dialogService.showCustomDialogAndWait(
-                        new ConsistencyCheckDialog(tabSupplier.get(), dialogService, preferences, entryTypesManager, result));
+                        new ConsistencyCheckDialog(tabSupplier.get(), dialogService, preferences, entryTypesManager, task.getValue()));
             }
-        }).onFailure(exception ->
-                dialogService.showErrorDialogAndWait(Localization.lang("Consistency check failed."), exception)
-        ).executeWith(taskExecutor);
+        });
+        taskExecutor.execute(task);
+
+        dialogService.showProgressDialogAndWait(
+                Localization.lang("Check consistency"),
+                Localization.lang("Checking consistency..."),
+                task);
     }
 }
