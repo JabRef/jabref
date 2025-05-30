d5b7033594b3d907edb3f68cca3e721a4353e4d1
Brandon Lau <1brandonlau@gmail.com>
2025-05-30 16:40:19 -0700

    test to use List.of()

--- jablib/src/test/java/org/jabref/logic/util/ExternalLinkCreatorTest.java
+++ jablib/src/test/java/org/jabref/logic/util/ExternalLinkCreatorTest.java
@@ -2,6 +2,7 @@ package org.jabref.logic.util;
 
 import java.net.MalformedURLException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -32,7 +33,7 @@ class ExternalLinkCreatorTest {
                     Collections.emptySet(), // apiKeys
                     Collections.emptyMap(), // defaultApiKeys
                     true, // persistCustomKeys
-                    Collections.emptyList(), // catalogs
+                    List.of(), // catalogs
                     null, // defaultPlainCitationParser
                     Collections.emptyMap() // searchEngineUrlTemplates
             );
