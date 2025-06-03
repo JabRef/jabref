5ff26895603f8cabb12e59895ec9bdcf1b8a19ef
Christoph <siedlerkiller@gmail.com>
2025-06-03 12:20:15 +0200

    Update jablib/build.gradle.kts

--- jablib/build.gradle.kts
+++ jablib/build.gradle.kts
@@ -164,7 +164,7 @@ dependencies {
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
         exclude(group = "org.jetbrains.kotlin")
     }
-    implementation("dev.langchain4j:langchain4j-mistral-ai:1.0.1-beta5") {
+    implementation("dev.langchain4j:langchain4j-mistral-ai:1.0.1-beta6") {
         exclude(group = "com.squareup.okhttp3")
         exclude(group = "com.squareup.retrofit2", module = "retrofit")
         exclude(group = "org.jetbrains.kotlin")
