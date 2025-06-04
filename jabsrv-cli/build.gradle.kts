0d0712689273ebe19d1575bf8e3d0475b0287f97
Oliver Kopp <kopp.dev@gmail.com>
2025-06-04 14:06:06 +0200

    Remove spaces

--- jabsrv-cli/build.gradle.kts
+++ jabsrv-cli/build.gradle.kts
@@ -159,9 +159,9 @@ jlink {
         uses("org.glassfish.jersey.internal.inject.InjectionManager")
         uses("dev.langchain4j.spi.prompt.PromptTemplateFactory")
 
-        excludeRequires("org.glassfish.hk2.locator")
-        excludeRequires("org.apache.logging.log4j")
-        excludeRequires("kotlin.stdlib")
+        excluderequires("org.glassfish.hk2.locator")
+        excluderequires("org.apache.logging.log4j")
+        excluderequires("kotlin.stdlib")
 
     }
     jpackage {
