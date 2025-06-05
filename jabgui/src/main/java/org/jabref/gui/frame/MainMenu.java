da44233a2ab5141a0c3d0191770bfff60cdea95d
Oliver Kopp <kopp.dev@gmail.com>
2025-06-05 18:11:24 +0200

    Compilefix

--- jabgui/src/main/java/org/jabref/gui/frame/MainMenu.java
+++ jabgui/src/main/java/org/jabref/gui/frame/MainMenu.java
@@ -302,9 +302,8 @@ public class MainMenu extends MenuBar {
 
                 // region file-related
                 factory.createMenuItem(StandardActions.DOWNLOAD_FULL_TEXT, new DownloadFullTextAction(dialogService, stateManager, preferences, (UiTaskExecutor) taskExecutor)),
-                factory.createMenuItem(StandardActions.FIND_UNLINKED_FILES, new FindUnlinkedFilesAction(dialogService, stateManager)),
+                factory.createMenuItem(StandardActions.FIND_UNLINKED_FILES, new FindUnlinkedFilesAction(dialogService, stateManager))
                 // endregion
-
         );
 
         final MenuItem pushToApplicationMenuItem = factory.createMenuItem(pushToApplicationCommand.getAction(), pushToApplicationCommand);
