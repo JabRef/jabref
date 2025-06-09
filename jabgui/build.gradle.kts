772bfcd5ed6288f07484c1d422a5cd40c8fa52c8
Oliver Kopp <kopp.dev@gmail.com>
2025-06-10 00:33:45 +0200

    Refine handlebars dependencies

--- jabgui/build.gradle.kts
+++ jabgui/build.gradle.kts
@@ -139,8 +139,13 @@ dependencies {
 
     testImplementation("org.wiremock:wiremock:3.13.0")
     // Required by Wiremock - and our patching of Wiremock
-    implementation("com.github.jknack:handlebars:4.3.1")
-    implementation("com.github.jknack:handlebars-helpers:4.3.1")
+    implementation("com.github.jknack:handlebars:4.3.1") {
+        exclude(group = "org.mozilla", module = "rhino")
+    }
+    implementation("com.github.jknack:handlebars-helpers:4.3.1") {
+        exclude(group = "org.mozilla", module = "rhino")
+        exclude(group = "org.apache.commons", module = "commons-lang3")
+    }
     implementation("com.github.koppor:wiremock-slf4j-shim:main-SNAPSHOT")
     testImplementation("com.github.koppor:wiremock-slf4j-spi-shim:main-SNAPSHOT")
 
