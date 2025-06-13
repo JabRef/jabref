60c6f2b4d562f66deb1cfe7aab8bb5bf1352d350
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-13 11:19:24 +0200

    Add 'ubuntu-22.04-arm' target

--- build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -37,9 +37,12 @@ val os = OperatingSystem.current()
 
 val osTarget = when {
     os.isMacOsX -> {
-        val osVersion = System.getProperty("os.version")
         val arch = System.getProperty("os.arch")
         if (arch.contains("aarch")) "macos-14" else "macos-13"
+    }
+    os.isLinux -> {
+        val arch = System.getProperty("os.arch")
+        if (arch.contains("aarch")) "ubuntu-22.04-arm" else "ubuntu-22.04"
     }
     os.isLinux -> "ubuntu-22.04"
     os.isWindows -> "windows-2022"
@@ -52,6 +55,11 @@ javaModulePackaging {
     target("ubuntu-22.04") {
         operatingSystem = OperatingSystemFamily.LINUX
         architecture = MachineArchitecture.X86_64
+        packageTypes = listOf("deb")
+    }
+    target("ubuntu-22.04-arm") {
+        operatingSystem = OperatingSystemFamily.LINUX
+        architecture = MachineArchitecture.ARM64
         packageTypes = listOf("deb")
     }
     target("macos-13") {
