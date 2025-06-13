e9ab3474466443b3b2ec967c501111e2888829eb
RUSHILPATEL33 <rushilp274@gmail.com>
2025-06-14 00:22:18 +0530

    fix: allow Unicode characters in citation keys for LibreOffice integration

--- jablib/src/main/java/org/jabref/logic/openoffice/ReferenceMark.java
+++ jablib/src/main/java/org/jabref/logic/openoffice/ReferenceMark.java
@@ -15,8 +15,9 @@ public class ReferenceMark {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReferenceMark.class);
 
-    private static final Pattern REFERENCE_MARK_FORMAT = Pattern.compile("^(JABREF_[\\w-:./]+ CID_\\d+(?:, JABREF_[\\w-:./]+ CID_\\d+)*) (\\w+)$");
-    private static final Pattern ENTRY_PATTERN = Pattern.compile("JABREF_([\\w-:./]+) CID_(\\d+)");
+   private static final Pattern REFERENCE_MARK_FORMAT = Pattern.compile("^(JABREF_[\\p{L}\\p{N}_\\-.]+ CID_\\d+(?:, JABREF_[\\p{L}\\p{N}_\\-.]+ CID_\\d+)*) (\\p{L}.*)$");
+   private static final Pattern ENTRY_PATTERN = Pattern.compile("JABREF_([\\p{L}\\p{N}_\\-.]+) CID_(\\d+)");
+
 
     private final String name;
     private List<String> citationKeys;
