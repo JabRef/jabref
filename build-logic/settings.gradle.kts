eceeb4b9d7cb5edf4ea951e02c87e51747301aa5
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-14 16:02:12 +0200

    Clone java-module-packaging repo to use preview

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "build-logic"
 
-// If this is not found:
-// - clone https://github.com/gradlex-org/java-module-packaging next to this repository
-// - switch to branch 'preview' and pull
-includeBuild("../../java-module-packaging")
+if (File("java-module-packaging").exists()) {
+  include("java-module-packaging")
+} else {
+  // If this is not found:
+  // - clone https://github.com/gradlex-org/java-module-packaging next to this repository
+  // - switch to branch 'preview' and pull
+  includeBuild("../../java-module-packaging")
+}
