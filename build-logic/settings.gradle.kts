e53e34e686ec08668b40c6dfb180abe6a18feb02
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-14 16:14:11 +0200

    Simplify include logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,10 +1,6 @@
 rootProject.name = "build-logic"
 
-if (File("java-module-packaging").exists()) {
-  include("java-module-packaging")
-} else {
-  // If this is not found:
-  // - clone https://github.com/gradlex-org/java-module-packaging next to this repository
-  // - switch to branch 'preview' and pull
-  includeBuild("../../java-module-packaging")
-}
+// If this is not found:
+// - clone https://github.com/gradlex-org/java-module-packaging next to this file
+// - switch to branch 'preview' and pull
+include("java-module-packaging")
