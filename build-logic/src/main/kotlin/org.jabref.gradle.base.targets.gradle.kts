98ca45242865a80a117a0ba2fa97d8b2626942d6
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-17 16:02:59 +0200

    Move host identification to plugin

--- build-logic/src/main/kotlin/org.jabref.gradle.base.targets.gradle.kts
+++ build-logic/src/main/kotlin/org.jabref.gradle.base.targets.gradle.kts
@@ -1,25 +1,5 @@
-import org.gradle.internal.os.OperatingSystem
-
 plugins {
     id("org.gradlex.java-module-packaging")
-}
-
-// TODO jjohannes: OS detection should be part of packaging plugin
-//  https://github.com/gradlex-org/java-module-packaging/issues/51
-// the result name must equal the name of one of the targets below in javaModulePackaging
-val os = OperatingSystem.current()
-val osTarget = when {
-    os.isMacOsX -> {
-        val osVersion = System.getProperty("os.version")
-        val arch = System.getProperty("os.arch")
-        if (arch.contains("aarch")) "macos-14" else "macos-13"
-    }
-    os.isLinux ->  {
-        val arch = System.getProperty("os.arch")
-        if (arch.contains("aarch")) "ubuntu-22.04-arm" else "ubuntu-22.04"
-    }
-    os.isWindows -> "windows-2022"
-    else -> error("Unsupported OS")
 }
 
 // Source: https://github.com/jjohannes/java-module-system/blob/main/gradle/plugins/src/main/kotlin/targets.gradle.kts
@@ -60,5 +40,4 @@ javaModulePackaging {
         architecture = MachineArchitecture.X86_64
         packageTypes = listOf("msi")
     }
-    primaryTarget(target(osTarget))
 }
