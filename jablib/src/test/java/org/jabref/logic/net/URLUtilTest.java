cee17653b88f3edaba1cffd3a5ac7ec121b368ea
Subhramit Basu <subhramit.bb@live.in>
2025-06-18 00:11:16 +0530

    Add missing import for `assertNotEquals`

--- jablib/src/test/java/org/jabref/logic/net/URLUtilTest.java
+++ jablib/src/test/java/org/jabref/logic/net/URLUtilTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
