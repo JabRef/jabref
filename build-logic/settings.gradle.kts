b4bf3fadbae5ad72402b8b503b99826aef7326d0
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-17 14:26:10 +0200

    Fix includeBuild logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -3,4 +3,4 @@ rootProject.name = "build-logic"
 // If this is not found:
 // - clone https://github.com/gradlex-org/java-module-packaging next to this file
 // - switch to branch 'preview' and pull
-include("java-module-packaging")
+includeBuild("java-module-packaging")
