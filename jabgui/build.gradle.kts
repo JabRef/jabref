d75b5f3808ad3d9663fa022e423de20c6675da91
Oliver Kopp <kopp.dev@gmail.com>
2025-06-21 21:51:34 +0200

    "Reduce" dependencies

--- jabgui/build.gradle.kts
+++ jabgui/build.gradle.kts
@@ -194,9 +194,7 @@ javaModuleTesting.whitebox(testing.suites["test"]) {
     requires.add("org.junit.jupiter.params")
     requires.add("org.mockito")
 
-    requires.add("org.testfx")
     requires.add("org.testfx.junit5")
-    requires.add("org.hamcrest")
 
     requires.add("wiremock")
     requires.add("wiremock.slf4j.spi.shim")
