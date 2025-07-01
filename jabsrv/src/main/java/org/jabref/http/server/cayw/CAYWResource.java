4f516fc402068b12a5e35a1c1f39ca993dd63bc8
Oliver Kopp <kopp.dev@gmail.com>
2025-07-01 22:20:48 +0200

    Fix import

--- jabsrv/src/main/java/org/jabref/http/server/cayw/CAYWResource.java
+++ jabsrv/src/main/java/org/jabref/http/server/cayw/CAYWResource.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
