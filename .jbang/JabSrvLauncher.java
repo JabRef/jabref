ebce0e6edffffba25d1288a19cfc412c77408bd0
Oliver Kopp <kopp.dev@gmail.com>
2025-07-03 00:06:23 +0200

    Add more snapshot repositories (because of e-adr)

--- .jbang/JabSrvLauncher.java
+++ .jbang/JabSrvLauncher.java
@@ -28,7 +28,8 @@
 //SOURCES ../jabsrv/src/main/java/org/jabref/http/server/services/FilesToServe.java
 
 // REPOS mavencentral,snapshots=https://central.sonatype.com/repository/maven-snapshots/
-//REPOS mavencentral,jitpack=https://jitpack.io
+//REPOS mavencentral,mavencentralsnapshots=https://central.sonatype.com/repository/maven-snapshots/,s01oss=https://s01.oss.sonatype.org/content/repositories/snapshots/,oss=https://oss.sonatype.org/content/repositories,jitpack=https://jitpack.io,oss2=https://oss.sonatype.org/content/groups/public,ossrh=https://oss.sonatype.org/content/repositories/snapshots
+// REPOS mavencentral,jitpack=https://jitpack.io
 
 // TODO: ASCII things won't work, but we accept for now to keep things going
 //DEPS com.ibm.icu:icu4j:72.1
