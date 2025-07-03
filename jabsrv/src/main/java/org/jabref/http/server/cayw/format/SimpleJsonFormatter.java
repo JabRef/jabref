f16c6b4cd18a141e98d7c4dc961ce18475a5723f
Philip <37398281+palukku@users.noreply.github.com>
2025-07-03 23:00:19 +0200

    add gson in constructor

--- jabsrv/src/main/java/org/jabref/http/server/cayw/format/SimpleJsonFormatter.java
+++ jabsrv/src/main/java/org/jabref/http/server/cayw/format/SimpleJsonFormatter.java
@@ -7,10 +7,17 @@ import org.jabref.http.dto.cayw.SimpleJson;
 import org.jabref.http.server.cayw.CAYWQueryParams;
 import org.jabref.http.server.cayw.gui.CAYWEntry;
 
+import com.google.gson.Gson;
 import org.jvnet.hk2.annotations.Service;
 
 @Service
 public class SimpleJsonFormatter implements CAYWFormatter {
+
+    private final Gson gson;
+
+    public SimpleJsonFormatter() {
+        this.gson = new GsonFactory().provide();
+    }
 
     @Override
     public String getFormatName() {
@@ -22,6 +29,6 @@ public class SimpleJsonFormatter implements CAYWFormatter {
         List<SimpleJson> simpleJsons = caywEntries.stream()
                                               .map(caywEntry -> SimpleJson.fromBibEntry(caywEntry.getBibEntry()))
                                               .toList();
-        return new GsonFactory().provide().toJson(simpleJsons);
+        return gson.toJson(simpleJsons);
     }
 }
