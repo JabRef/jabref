c32d5a2bbf01670a737362beaf4669213bcc755c
Oliver Kopp <kopp.dev@gmail.com>
2025-07-07 11:27:45 +0200

    Fix checkstyle

--- jabgui/src/main/java/org/jabref/gui/JabRefGUI.java
+++ jabgui/src/main/java/org/jabref/gui/JabRefGUI.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.undo.UndoManager;
 
