42704ea62c0cb68592e0111d942c7d8c566aa076
Siedlerchr <siedlerkiller@gmail.com>
2025-07-10 17:16:24 +0200

    fix module access

--- build-logic/src/main/kotlin/org.jabref.gradle.base.dependency-rules.gradle.kts
+++ build-logic/src/main/kotlin/org.jabref.gradle.base.dependency-rules.gradle.kts
@@ -443,6 +443,7 @@ extraJavaModuleInfo {
     module("net.sourceforge.lept4j:lept4j", "net.sourceforge.lept4j") {
         exportAllPackages()
         requireAllDefinedDependencies()
+        requires("java.desktop")
     }
 
     module("org.apache.pdfbox:pdfbox", "org.apache.pdfbox") {
