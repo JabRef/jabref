86e648c79dec011b22b7ead525f6065e9496ce21
Umut Akbayin <umutakbayin@posteo.de>
2025-07-13 23:03:17 +0200

    test(LinkedFileTransferHelperTest): add tests for all three scenarios

--- jablib/src/test/java/org/jabref/logic/externalfiles/LinkedFileTransferHelperTest.java
+++ jablib/src/test/java/org/jabref/logic/externalfiles/LinkedFileTransferHelperTest.java
@@ -1,53 +1,175 @@
 package org.jabref.logic.externalfiles;
 
 import org.jabref.logic.FilePreferences;
+import org.jabref.model.database.BibDatabase;
 import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.LinkedFile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class LinkedFileTransferHelperTest {
+  private BibDatabaseContext sourceContext;
+  private BibDatabaseContext targetContext;
+  private Path sourceDir;
+  private Path targetDir;
+  private Path testFile;
+  private BibEntry sourceEntry;
+  private BibEntry targetEntry;
+  private FilePreferences filePreferences = mock(FilePreferences.class);
 
   @Nested
-  class IsReachableFromPrimaryDirectoryTest {
+  @DisplayName("When the linked file is reachable in the new context")
+  class WhenFileIsReachable {
 
-    FilePreferences prefs = mock(FilePreferences.class);
-    BibDatabaseContext targetContext = mock(BibDatabaseContext.class);
+    @BeforeEach
+    void setup(@TempDir Path tempDir) throws IOException {
+      sourceDir = tempDir.resolve("source/target");
+      targetDir = tempDir.resolve("source");
 
-    @Test
-    void case1_reachableWithinPrimaryDirectory() {
-      Path targetPrimaryDir = Path.of("/project");
-      Path file = Path.of("/project/bibs/personalfiles/file.pdf");
+      when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(true);
 
-      when(targetContext.getFileDirectories(prefs)).thenReturn(List.of(targetPrimaryDir));
+      Files.createDirectories(sourceDir);
+      Files.createDirectories(targetDir);
 
-      assertTrue(LinkedFileTransferHelper.isReachableFromPrimaryDirectory(file, targetContext, prefs));
+      testFile = sourceDir.resolve("sourcefiles/test.pdf");
+      Files.createDirectories(testFile.getParent());
+      Files.createFile(testFile);
+
+      sourceContext = new BibDatabaseContext(new BibDatabase());
+      sourceContext.setDatabasePath(sourceDir.resolve("personal.bib"));
+      targetContext = new BibDatabaseContext(new BibDatabase());
+      targetContext.setDatabasePath(targetDir.resolve("papers.bib"));
+
+      sourceEntry = new BibEntry();
+      LinkedFile linkedFile = new LinkedFile("Test", "sourcefiles/test.pdf", "PDF");
+
+      sourceEntry.setFiles(List.of(linkedFile));
+      targetEntry = (BibEntry) sourceEntry.clone();
+      targetEntry.setFiles(List.of(linkedFile));
+
+      sourceContext.getDatabase().insertEntry(sourceEntry);
+      targetContext.getDatabase().insertEntry(targetEntry);
     }
 
     @Test
-    void case2_sameRelativePathButDifferentBaseDirectory_shouldNotBeReachable() {
-      Path targetPrimaryDir = Path.of("/project/parentfiles");
-      Path file = Path.of("/project/bibs/personalfiles/file.pdf");
+    void pathDiffers_ShouldAdjustPath(@TempDir Path tempDir) throws IOException {
+      var returnedEntries = LinkedFileTransferHelper.adjustLinkedFilesForTarget(sourceContext, targetContext,
+        filePreferences);
 
-      when(targetContext.getFileDirectories(prefs)).thenReturn(List.of(targetPrimaryDir));
+      assertEquals(1, returnedEntries.size());
+      assertEquals("sourcefiles/test.pdf", sourceContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      assertEquals("target/sourcefiles/test.pdf",
+        targetContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      Path expectedFile = targetDir.resolve("test.pdf");
+      assertFalse(Files.exists(expectedFile), "File should not have been copied to target directory");
+    }
+  }
 
-      assertFalse(LinkedFileTransferHelper.isReachableFromPrimaryDirectory(file, targetContext, prefs));
+  @Nested
+  @DisplayName("When the linked file is NOT reachable, but the paths do NOT differ")
+  class WhenFileIsNotReachable {
+
+    @BeforeEach
+    void setup(@TempDir Path tempDir) throws IOException {
+      sourceDir = tempDir.resolve("target/targetfiles");
+      targetDir = tempDir.resolve("source/sourcefiles");
+
+      when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(true);
+
+      Files.createDirectories(sourceDir);
+      Files.createDirectories(targetDir);
+
+      testFile = sourceDir.resolve("test.pdf");
+      Files.createDirectories(testFile.getParent());
+      Files.createFile(testFile);
+
+      sourceContext = new BibDatabaseContext(new BibDatabase());
+      sourceContext.setDatabasePath(sourceDir.resolve("personal.bib"));
+      targetContext = new BibDatabaseContext(new BibDatabase());
+      targetContext.setDatabasePath(targetDir.resolve("papers.bib"));
+
+      sourceEntry = new BibEntry();
+      LinkedFile linkedFile = new LinkedFile("Test", "test.pdf", "PDF");
+
+      sourceEntry.setFiles(List.of(linkedFile));
+      targetEntry = (BibEntry) sourceEntry.clone();
+      targetEntry.setFiles(List.of(linkedFile));
+
+      sourceContext.getDatabase().insertEntry(sourceEntry);
+      targetContext.getDatabase().insertEntry(targetEntry);
     }
 
     @Test
-    void case3_differentRelativePathAndDifferentBaseDirectory_shouldNotBeReachable() {
-      Path targetPrimaryDir = Path.of("/project/parentfiles");
-      Path file = Path.of("/project/bibs/personalfiles/file.pdf");
+    void fileNotReachable_ShouldCopyFile(@TempDir Path tempDir) throws IOException {
+      var returnedEntries = LinkedFileTransferHelper.adjustLinkedFilesForTarget(sourceContext, targetContext,
+        filePreferences);
 
-      when(targetContext.getFileDirectories(prefs)).thenReturn(List.of(targetPrimaryDir));
+      assertEquals(1, returnedEntries.size());
+      assertEquals("test.pdf", sourceContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      assertEquals("test.pdf",
+        targetContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      Path expectedFile = targetDir.resolve("test.pdf");
+      assertTrue(Files.exists(expectedFile), "File should have been copied to target directory");
+    }
+  }
 
-      assertFalse(LinkedFileTransferHelper.isReachableFromPrimaryDirectory(file, targetContext, prefs));
+  @Nested
+  @DisplayName("When the linked file is NOT reachable, and a nested structure needs to be created")
+  class WhenFileIsNotReachableAndPathsDiffer {
+
+    @BeforeEach
+    void setup(@TempDir Path tempDir) throws IOException {
+      sourceDir = tempDir.resolve("source");
+      targetDir = tempDir.resolve("target/targetfiles");
+
+      when(filePreferences.shouldStoreFilesRelativeToBibFile()).thenReturn(true);
+
+      Files.createDirectories(sourceDir);
+      Files.createDirectories(targetDir);
+
+      testFile = sourceDir.resolve("sourcefiles/test.pdf");
+      Files.createDirectories(testFile.getParent());
+      Files.createFile(testFile);
+
+      sourceContext = new BibDatabaseContext(new BibDatabase());
+      sourceContext.setDatabasePath(sourceDir.resolve("personal.bib"));
+      targetContext = new BibDatabaseContext(new BibDatabase());
+      targetContext.setDatabasePath(targetDir.resolve("papers.bib"));
+
+      sourceEntry = new BibEntry();
+      LinkedFile linkedFile = new LinkedFile("Test", "sourcefiles/test.pdf", "PDF");
+
+      sourceEntry.setFiles(List.of(linkedFile));
+      targetEntry = (BibEntry) sourceEntry.clone();
+      targetEntry.setFiles(List.of(linkedFile));
+
+      sourceContext.getDatabase().insertEntry(sourceEntry);
+      targetContext.getDatabase().insertEntry(targetEntry);
+    }
+
+    @Test
+    void fileNotReachableAndPathsDiffer_ShouldCopyFileAndCreateDirectory() throws IOException {
+      var returnedEntries = LinkedFileTransferHelper.adjustLinkedFilesForTarget(sourceContext, targetContext,
+        filePreferences);
+
+      assertEquals(1, returnedEntries.size());
+      assertEquals("sourcefiles/test.pdf", sourceContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      assertEquals("sourcefiles/test.pdf",
+        targetContext.getEntries().getFirst().getFiles().getFirst().getLink());
+      Path expectedFile = targetDir.resolve("sourcefiles/test.pdf");
+      assertTrue(Files.exists(expectedFile), "File should have been copied to target directory");
     }
   }
 }
