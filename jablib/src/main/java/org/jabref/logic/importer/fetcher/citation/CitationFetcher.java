5bd1d5a27bc201f99bd0375a8f9a3a0f035a2e46
Salvador Romo <salvadorromo@MacBook-Air-de-Salvador.local>
2025-07-17 10:39:17 -0600

    fix tests

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/citation/CitationFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/citation/CitationFetcher.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.jabref.logic.importer.FetcherException;
-import org.jabref.logic.importer.fetcher.citation.semanticscholar.PaperDetails;
 import org.jabref.model.entry.BibEntry;
 
 /**
