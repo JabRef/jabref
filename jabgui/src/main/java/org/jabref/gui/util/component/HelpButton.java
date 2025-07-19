ab260b4e5774a4fa196487ee71a2da2275b6044a
Yubo-Cao <cao2006721@gmail.com>
2025-07-19 11:34:01 -0400

    Apply fix per Trag

--- jabgui/src/main/java/org/jabref/gui/util/component/HelpButton.java
+++ jabgui/src/main/java/org/jabref/gui/util/component/HelpButton.java
@@ -8,10 +8,11 @@ import org.jabref.gui.icon.IconTheme;
 import org.jabref.gui.preferences.GuiPreferences;
 
 import com.airhacks.afterburner.injection.Injector;
+import org.jspecify.annotations.NonNull;
 
 public class HelpButton extends Button {
 
-    public HelpButton(String url) {
+    public HelpButton(@NonNull String url) {
         setGraphic(IconTheme.JabRefIcons.HELP.getGraphicNode());
         getStyleClass().add("icon-button");
         setPrefSize(28, 28);
