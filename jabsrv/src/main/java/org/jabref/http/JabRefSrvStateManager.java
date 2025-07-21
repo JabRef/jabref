b11217b889b1b629914d593483d7012335f82d58
Philip <37398281+palukku@users.noreply.github.com>
2025-07-21 20:28:01 +0200

    add empty returns to JabRefSrvStateManager

--- jabsrv/src/main/java/org/jabref/http/JabRefSrvStateManager.java
+++ jabsrv/src/main/java/org/jabref/http/JabRefSrvStateManager.java
@@ -1,62 +1,45 @@
 package org.jabref.http;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableMap;
 
 import org.jabref.logic.search.IndexManager;
 import org.jabref.logic.util.OptionalObjectProperty;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class JabRefSrvStateManager implements SrvStateManager {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(JabRefSrvStateManager.class);
-    protected final ObservableList<BibDatabaseContext> openDatabases = FXCollections.observableArrayList();
-    protected final OptionalObjectProperty<BibDatabaseContext> activeDatabase = OptionalObjectProperty.empty();
-    protected final ObservableList<BibEntry> selectedEntries = FXCollections.observableArrayList();
-    protected final ObservableMap<String, IndexManager> indexManagers = FXCollections.observableHashMap();
 
     @Override
     public ObservableList<BibDatabaseContext> getOpenDatabases() {
-        return openDatabases;
+        return FXCollections.emptyObservableList();
     }
 
     @Override
     public OptionalObjectProperty<BibDatabaseContext> activeDatabaseProperty() {
-        return activeDatabase;
+        return OptionalObjectProperty.empty();
     }
 
     @Override
     public ObservableList<BibEntry> getSelectedEntries() {
-        return selectedEntries;
+        return FXCollections.emptyObservableList();
     }
 
     @Override
     public Optional<IndexManager> getIndexManager(BibDatabaseContext database) {
-        return Optional.ofNullable(indexManagers.get(database.getUid()));
+        return Optional.empty();
     }
 
     @Override
     public Optional<BibDatabaseContext> getActiveDatabase() {
-        return activeDatabase.get();
+        return Optional.empty();
     }
 
     @Override
     public List<String> collectAllDatabasePaths() {
-        List<String> list = new ArrayList<>();
-        getOpenDatabases().stream()
-                          .map(BibDatabaseContext::getDatabasePath)
-                          .forEachOrdered(pathOptional -> pathOptional.ifPresentOrElse(
-                                  path -> list.add(path.toAbsolutePath().toString()),
-                                  () -> list.add("")));
-        return list;
+        return List.of();
     }
 }
