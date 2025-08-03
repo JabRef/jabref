11d7dbee695ee795540c3aad01ab0197855befe7
Umut Akbayin <umutakbayin@posteo.de>
2025-08-03 17:08:52 +0200

    chore: fix indentation in module-info.java after rebase

--- jablib/src/main/java/module-info.java
+++ jablib/src/main/java/module-info.java
@@ -113,14 +113,17 @@ open module org.jabref.jablib {
     exports org.jabref.logic.git.status;
     exports org.jabref.logic.command;
 
-  requires javafx.base;
+    requires java.base;
+
+    requires javafx.base;
     requires javafx.graphics; // because of javafx.scene.paint.Color
     requires afterburner.fx;
     requires com.tobiasdiez.easybind;
 
     // for java.awt.geom.Rectangle2D required by org.jabref.logic.pdf.TextExtractor
+    requires java.desktop;
 
-  // SQL
+    // SQL
     requires java.sql;
     requires java.sql.rowset;
 
@@ -252,6 +255,6 @@ open module org.jabref.jablib {
     requires mslinks;
     requires org.antlr.antlr4.runtime;
     requires org.libreoffice.uno;
-  requires org.jetbrains.annotations;
+    requires org.jetbrains.annotations;
   // endregion
 }
