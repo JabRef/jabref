28d6c3b216854c1e547093302c9a1e1252194c20
Yubo-Cao <cao2006721@gmail.com>
2025-08-16 12:14:41 -0400

    test(donation): Use appropriate mock parameters for testing

--- jabgui/src/test/java/org/jabref/gui/welcome/components/DonationProviderTest.java
+++ jabgui/src/test/java/org/jabref/gui/welcome/components/DonationProviderTest.java
@@ -2,14 +2,20 @@ package org.jabref.gui.welcome.components;
 
 import java.time.LocalDate;
 
+import javafx.scene.layout.StackPane;
+
+import org.jabref.gui.DialogService;
+import org.jabref.gui.preferences.GuiPreferences;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 class DonationProviderTest {
     @Test
     public void calculateDaysUntilNextPopup() {
-        DonationProvider donationProvider = new DonationProvider(null, null, null);
+        DonationProvider donationProvider = new DonationProvider(new StackPane(), mock(GuiPreferences.class), mock(DialogService.class));
 
         int lastShownEpochDay = (int) LocalDate.now().minusDays(400).toEpochDay();
         int daysUntilNextPopup = donationProvider.calculateDaysUntilNextPopup(lastShownEpochDay);
