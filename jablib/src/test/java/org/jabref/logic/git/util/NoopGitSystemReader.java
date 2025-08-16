de7f5d71bc69765b13dbfa18f9fb1d896e108c07
Siedlerchr <siedlerkiller@gmail.com>
2025-08-16 11:37:42 +0200

    Simplify code
    Co-authored-by: Chris Denman <chrisdenman@ceilingcat.co.uk>

--- jablib/src/test/java/org/jabref/logic/git/util/NoopGitSystemReader.java
+++ jablib/src/test/java/org/jabref/logic/git/util/NoopGitSystemReader.java
@@ -1,82 +1,32 @@
 package org.jabref.logic.git.util;
 
-import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.storage.file.FileBasedConfig;
-import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.util.SystemReader;
 
 /**
- * Nooop implementation of {@link SystemReader}
+ * Noop implementation of {@link SystemReader}
  * to prevent leaking of system and user git config into tests.
  */
-public class NoopGitSystemReader extends SystemReader {
+public class NoopGitSystemReader extends SystemReader.Delegate {
+    private static final StoredConfig NOP = new StoredConfig() {
+        @Override
+        public void load() { }
 
-    private static final SystemReader PROXY = SystemReader.getInstance();
+        @Override
+        public void save() { }
+    };
 
-    @Override
-    public String getHostname() {
-        return PROXY.getHostname();
+    public NoopGitSystemReader() {
+        super(SystemReader.getInstance());
     }
 
     @Override
-    public String getenv(String variable) {
-        return PROXY.getenv(variable);
+    public StoredConfig getUserConfig() {
+        return NOP;
     }
 
     @Override
-    public String getProperty(String key) {
-        return PROXY.getProperty(key);
-    }
-
-    @Override
-    public FileBasedConfig openUserConfig(Config parent, FS fs) {
-        return new FileBasedConfig(parent, null, fs) {
-            @Override
-            public void load() {
-            }
-
-            @Override
-            public boolean isOutdated() {
-                return false;
-            }
-        };
-    }
-
-    @Override
-    public FileBasedConfig openSystemConfig(Config parent, FS fs) {
-        return new FileBasedConfig(parent, null, fs) {
-            @Override
-            public void load() {
-            }
-
-            @Override
-            public boolean isOutdated() {
-                return false;
-            }
-        };
-    }
-
-    @Override
-    public FileBasedConfig openJGitConfig(Config parent, FS fs) {
-        return new FileBasedConfig(parent, null, fs) {
-            @Override
-            public void load() {
-            }
-
-            @Override
-            public boolean isOutdated() {
-                return false;
-            }
-        };
-    }
-
-    @Override
-    public long getCurrentTime() {
-        return PROXY.getCurrentTime();
-    }
-
-    @Override
-    public int getTimezone(long when) {
-        return PROXY.getTimezone(when);
+    public StoredConfig getSystemConfig() {
+        return NOP;
     }
 }
