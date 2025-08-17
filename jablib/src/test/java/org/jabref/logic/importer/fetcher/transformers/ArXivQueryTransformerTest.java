3977901e44c8e59be6c465e820c0c4c7befcf350
RunningEscaping <turhantolga.unal@bahcesehir.edu.tr>
2025-08-17 23:46:23 +0300

    Updated ArxivPortalFetcher to use the new parser and node logic

--- jablib/src/test/java/org/jabref/logic/importer/fetcher/transformers/ArXivQueryTransformerTest.java
+++ jablib/src/test/java/org/jabref/logic/importer/fetcher/transformers/ArXivQueryTransformerTest.java
@@ -2,9 +2,11 @@ package org.jabref.logic.importer.fetcher.transformers;
 
 import java.util.Optional;
 
+import org.jabref.logic.search.query.SearchQueryVisitor;
+import org.jabref.model.search.query.BaseQueryNode;
+import org.jabref.model.search.query.SearchQuery;
+
 import org.apache.lucene.queryparser.flexible.core.QueryNodeParseException;
-import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
-import org.apache.lucene.queryparser.flexible.standard.parser.StandardSyntaxParser;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -41,8 +43,9 @@ class ArXivQueryTransformerTest extends YearRangeByFilteringQueryTransformerTest
     public void convertYearField() throws QueryNodeParseException {
         ArXivQueryTransformer transformer = getTransformer();
         String queryString = "year:2018";
-        QueryNode luceneQuery = new StandardSyntaxParser().parse(queryString, AbstractQueryTransformer.NO_EXPLICIT_FIELD);
-        Optional<String> query = transformer.transformLuceneQuery(luceneQuery);
+        SearchQuery searchQuery = new SearchQuery(queryString);
+        BaseQueryNode searchQueryList = new SearchQueryVisitor(searchQuery.getSearchFlags()).visitStart(searchQuery.getContext());
+        Optional<String> query = getTransformer().transformSearchQuery(searchQueryList);
         assertEquals(Optional.of("2018"), query);
         assertEquals(Optional.of(2018), transformer.getStartYear());
         assertEquals(Optional.of(2018), transformer.getEndYear());
