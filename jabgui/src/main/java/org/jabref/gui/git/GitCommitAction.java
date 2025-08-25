f937b5ada27e8927ce6c1a19cd28bc2e748328e4
Wanling Fu <wanlingfu3@gmail.com>
2025-08-25 13:08:07 +0100

    Add early Git status check in GitCommitAction

--- jabgui/src/main/java/org/jabref/gui/git/GitCommitAction.java
+++ jabgui/src/main/java/org/jabref/gui/git/GitCommitAction.java
@@ -4,6 +4,9 @@ import org.jabref.gui.DialogService;
 import org.jabref.gui.StateManager;
 import org.jabref.gui.actions.ActionHelper;
 import org.jabref.gui.actions.SimpleCommand;
+import org.jabref.logic.git.GitHandler;
+import org.jabref.logic.git.status.GitStatusChecker;
+import org.jabref.logic.l10n.Localization;
 
 public class GitCommitAction extends SimpleCommand {
 
@@ -19,8 +22,22 @@ public class GitCommitAction extends SimpleCommand {
 
     @Override
     public void execute() {
+        if (hasNothingToCommit()) {
+            dialogService.notify(Localization.lang("Nothing to commit."));
+            return;
+        }
+
         dialogService.showCustomDialogAndWait(
                 new GitCommitDialogView()
         );
+    }
+
+    private boolean hasNothingToCommit() {
+        return stateManager.getActiveDatabase()
+                           .flatMap(context -> context.getDatabasePath())
+                           .flatMap(GitHandler::fromAnyPath)
+                           .map(GitStatusChecker::checkStatus)
+                           .map(status -> !status.uncommittedChanges())
+                           .orElse(true);
     }
 }
