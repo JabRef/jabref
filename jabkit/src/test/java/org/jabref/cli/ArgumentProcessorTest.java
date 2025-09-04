bd58b141d86c959b56d7d3612520b8c050c8e840
Jenish-1235 <jenishtogadiya549@gmail.com>
2025-09-05 01:04:29 +0530

    fix import checkstyles

--- jabkit/src/test/java/org/jabref/cli/ArgumentProcessorTest.java
+++ jabkit/src/test/java/org/jabref/cli/ArgumentProcessorTest.java
@@ -36,12 +36,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import org.mockito.Answers;
 import picocli.CommandLine;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
