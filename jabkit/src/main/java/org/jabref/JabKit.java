9c24a2f248d503ea4733e535382809ec64a803c9
dcarpentiero <carpentierodaniele@gmail.com>
2025-09-08 14:18:12 +0200

    fix: removal unnecessary parameter

--- jabkit/src/main/java/org/jabref/JabKit.java
+++ jabkit/src/main/java/org/jabref/JabKit.java
@@ -9,58 +9,27 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.prefs.BackingStoreException;
 import java.util.stream.Collectors;
 
 import javafx.util.Pair;
 
 import org.jabref.cli.ArgumentProcessor;
-import org.jabref.logic.FilePreferences;
-import org.jabref.logic.InternalPreferences;
-import org.jabref.logic.JabRefException;
-import org.jabref.logic.LibraryPreferences;
-import org.jabref.logic.ai.AiPreferences;
-import org.jabref.logic.bibtex.FieldPreferences;
-import org.jabref.logic.citationkeypattern.CitationKeyPatternPreferences;
-import org.jabref.logic.cleanup.CleanupPreferences;
-import org.jabref.logic.exporter.ExportPreferences;
-import org.jabref.logic.exporter.SelfContainedSaveConfiguration;
-import org.jabref.logic.git.preferences.GitPreferences;
-import org.jabref.logic.importer.ImportFormatPreferences;
-import org.jabref.logic.importer.ImporterPreferences;
 import org.jabref.logic.importer.SearchBasedFetcher;
 import org.jabref.logic.importer.WebFetcher;
 import org.jabref.logic.importer.WebFetchers;
-import org.jabref.logic.importer.fetcher.MrDlibPreferences;
-import org.jabref.logic.importer.util.GrobidPreferences;
 import org.jabref.logic.journals.JournalAbbreviationLoader;
-import org.jabref.logic.journals.JournalAbbreviationPreferences;
 import org.jabref.logic.journals.JournalAbbreviationRepository;
 import org.jabref.logic.l10n.Localization;
-import org.jabref.logic.layout.LayoutFormatterPreferences;
-import org.jabref.logic.layout.format.NameFormatterPreferences;
 import org.jabref.logic.net.ProxyAuthenticator;
 import org.jabref.logic.net.ProxyPreferences;
 import org.jabref.logic.net.ProxyRegisterer;
 import org.jabref.logic.net.ssl.SSLPreferences;
 import org.jabref.logic.net.ssl.TrustStoreManager;
-import org.jabref.logic.openoffice.OpenOfficePreferences;
 import org.jabref.logic.preferences.CliPreferences;
-import org.jabref.logic.preferences.DOIPreferences;
 import org.jabref.logic.preferences.JabRefCliPreferences;
-import org.jabref.logic.preferences.LastFilesOpenedPreferences;
-import org.jabref.logic.preferences.OwnerPreferences;
-import org.jabref.logic.preferences.TimestampPreferences;
 import org.jabref.logic.protectedterms.ProtectedTermsLoader;
-import org.jabref.logic.protectedterms.ProtectedTermsPreferences;
-import org.jabref.logic.push.PushToApplicationPreferences;
-import org.jabref.logic.remote.RemotePreferences;
-import org.jabref.logic.search.SearchPreferences;
 import org.jabref.logic.util.BuildInfo;
 import org.jabref.logic.util.Directories;
-import org.jabref.logic.util.io.AutoLinkPreferences;
-import org.jabref.logic.xmp.XmpPreferences;
-import org.jabref.model.entry.BibEntryPreferences;
 import org.jabref.model.entry.BibEntryTypesManager;
 import org.jabref.model.strings.StringUtil;
 import org.jabref.model.util.DummyFileUpdateMonitor;
@@ -89,212 +58,6 @@ public class JabKit {
 
     private static final String JABKIT_BRAND = "JabKit - command line toolkit for JabRef";
     private static final BibEntryTypesManager bibEntryTypesManager = new BibEntryTypesManager();
-    private static final CliPreferences cliPreferences = new CliPreferences() {
-        @Override
-        public void clear() throws BackingStoreException {
-
-        }
-
-        @Override
-        public void deleteKey(String key) throws IllegalArgumentException {
-
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public void exportPreferences(Path file) throws JabRefException {
-
-        }
-
-        @Override
-        public void importPreferences(Path file) throws JabRefException {
-
-        }
-
-        @Override
-        public InternalPreferences getInternalPreferences() {
-            return null;
-        }
-
-        @Override
-        public BibEntryPreferences getBibEntryPreferences() {
-            return null;
-        }
-
-        @Override
-        public JournalAbbreviationPreferences getJournalAbbreviationPreferences() {
-            return null;
-        }
-
-        @Override
-        public FilePreferences getFilePreferences() {
-            return null;
-        }
-
-        @Override
-        public FieldPreferences getFieldPreferences() {
-            return null;
-        }
-
-        @Override
-        public Map<String, Object> getPreferences() {
-            return Map.of();
-        }
-
-        @Override
-        public Map<String, Object> getDefaults() {
-            return Map.of();
-        }
-
-        @Override
-        public LayoutFormatterPreferences getLayoutFormatterPreferences() {
-            return null;
-        }
-
-        @Override
-        public ImportFormatPreferences getImportFormatPreferences() {
-            return null;
-        }
-
-        @Override
-        public SelfContainedSaveConfiguration getSelfContainedExportConfiguration() {
-            return null;
-        }
-
-        @Override
-        public BibEntryTypesManager getCustomEntryTypesRepository(BibEntryTypesManager bibEntryTypesManager) {
-            return bibEntryTypesManager;
-        }
-
-        @Override
-        public void storeCustomEntryTypesRepository(BibEntryTypesManager entryTypesManager) {
-
-        }
-
-        @Override
-        public CleanupPreferences getCleanupPreferences() {
-            return null;
-        }
-
-        @Override
-        public CleanupPreferences getDefaultCleanupPreset() {
-            return null;
-        }
-
-        @Override
-        public LibraryPreferences getLibraryPreferences() {
-            return null;
-        }
-
-        @Override
-        public DOIPreferences getDOIPreferences() {
-            return null;
-        }
-
-        @Override
-        public OwnerPreferences getOwnerPreferences() {
-            return null;
-        }
-
-        @Override
-        public TimestampPreferences getTimestampPreferences() {
-            return null;
-        }
-
-        @Override
-        public RemotePreferences getRemotePreferences() {
-            return null;
-        }
-
-        @Override
-        public ProxyPreferences getProxyPreferences() {
-            return null;
-        }
-
-        @Override
-        public SSLPreferences getSSLPreferences() {
-            return null;
-        }
-
-        @Override
-        public CitationKeyPatternPreferences getCitationKeyPatternPreferences() {
-            return null;
-        }
-
-        @Override
-        public AutoLinkPreferences getAutoLinkPreferences() {
-            return null;
-        }
-
-        @Override
-        public ExportPreferences getExportPreferences() {
-            return null;
-        }
-
-        @Override
-        public ImporterPreferences getImporterPreferences() {
-            return null;
-        }
-
-        @Override
-        public GrobidPreferences getGrobidPreferences() {
-            return null;
-        }
-
-        @Override
-        public XmpPreferences getXmpPreferences() {
-            return null;
-        }
-
-        @Override
-        public NameFormatterPreferences getNameFormatterPreferences() {
-            return null;
-        }
-
-        @Override
-        public SearchPreferences getSearchPreferences() {
-            return null;
-        }
-
-        @Override
-        public MrDlibPreferences getMrDlibPreferences() {
-            return null;
-        }
-
-        @Override
-        public ProtectedTermsPreferences getProtectedTermsPreferences() {
-            return null;
-        }
-
-        @Override
-        public AiPreferences getAiPreferences() {
-            return null;
-        }
-
-        @Override
-        public LastFilesOpenedPreferences getLastFilesOpenedPreferences() {
-            return null;
-        }
-
-        @Override
-        public OpenOfficePreferences getOpenOfficePreferences(JournalAbbreviationRepository journalAbbreviationRepository) {
-            return null;
-        }
-
-        @Override
-        public PushToApplicationPreferences getPushToApplicationPreferences() {
-            return null;
-        }
-
-        @Override
-        public GitPreferences getGitPreferences() {
-            return null;
-        }
-    };
 
     public static void main(String[] args) {
         initLogging(args);
@@ -306,7 +69,7 @@ public class JabKit {
             BuildInfo buildInfo = new BuildInfo();
             Injector.setModelOrService(BuildInfo.class, buildInfo);
 
-            BibEntryTypesManager entryTypesManager = preferences.getCustomEntryTypesRepository(cliPreferences, bibEntryTypesManager);
+            BibEntryTypesManager entryTypesManager = preferences.getCustomEntryTypesRepository(bibEntryTypesManager);
             Injector.setModelOrService(BibEntryTypesManager.class, entryTypesManager);
 
             ArgumentProcessor argumentProcessor = new ArgumentProcessor(preferences, entryTypesManager);
