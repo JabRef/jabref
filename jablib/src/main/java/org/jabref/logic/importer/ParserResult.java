58180f7b2206b10e4f9b7567c29706861094c97b
Philip <me@phlp.eu>
2025-09-11 22:45:19 +0200

    remove unused import

--- jablib/src/main/java/org/jabref/logic/importer/ParserResult.java
+++ jablib/src/main/java/org/jabref/logic/importer/ParserResult.java
@@ -3,7 +3,6 @@ package org.jabref.logic.importer;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.List;
