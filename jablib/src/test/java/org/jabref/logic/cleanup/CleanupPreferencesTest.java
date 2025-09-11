43c981bc712aeb614d634339366f819a21b896a1
Alex Sukhin <alex@sukhin.net>
2025-09-11 17:19:14 +0100

    Converted tests to assertEquals

--- jablib/src/test/java/org/jabref/logic/cleanup/CleanupPreferencesTest.java
+++ jablib/src/test/java/org/jabref/logic/cleanup/CleanupPreferencesTest.java
@@ -1,13 +1,11 @@
 package org.jabref.logic.cleanup;
 
 import java.util.EnumSet;
-import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CleanupPreferencesTest {
 
@@ -19,38 +17,23 @@ class CleanupPreferencesTest {
     }
 
     @Test
-    void updateWithReplacesStepsInSameCategory_RemovesOldStep() {
+    void updateWithReplacesStepsInSameCategory() {
         CleanupPreferences update = new CleanupPreferences(EnumSet.of(CleanupPreferences.CleanupStep.CLEANUP_EPRINT));
         CleanupPreferences result = cleanupPreferences.updateWith(update);
-        Set<CleanupPreferences.CleanupStep> activeJobs = result.getActiveJobs();
 
-        assertFalse(activeJobs.contains(CleanupPreferences.CleanupStep.CLEAN_UP_DOI));
+        EnumSet<CleanupPreferences.CleanupStep> expected = EnumSet.of(CleanupPreferences.CleanupStep.CLEANUP_EPRINT);
+        assertEquals(expected, result.getActiveJobs());
     }
 
     @Test
-    void updateWithReplacesStepsInSameCategory_AddsNewStep() {
-        CleanupPreferences update = new CleanupPreferences(EnumSet.of(CleanupPreferences.CleanupStep.CLEANUP_EPRINT));
-        CleanupPreferences result = cleanupPreferences.updateWith(update);
-        Set<CleanupPreferences.CleanupStep> activeJobs = result.getActiveJobs();
-
-        assertTrue(activeJobs.contains(CleanupPreferences.CleanupStep.CLEANUP_EPRINT));
-    }
-
-    @Test
-    void updateWithDifferentCategory_KeepsOldStep() {
+    void updateWithAddsStepInDifferentCategory() {
         CleanupPreferences update = new CleanupPreferences(EnumSet.of(CleanupPreferences.CleanupStep.MOVE_PDF));
         CleanupPreferences result = cleanupPreferences.updateWith(update);
-        Set<CleanupPreferences.CleanupStep> activeJobs = result.getActiveJobs();
 
-        assertTrue(activeJobs.contains(CleanupPreferences.CleanupStep.CLEAN_UP_DOI));
-    }
-
-    @Test
-    void updateWithDifferentCategory_AddsNewStep() {
-        CleanupPreferences update = new CleanupPreferences(EnumSet.of(CleanupPreferences.CleanupStep.MOVE_PDF));
-        CleanupPreferences result = cleanupPreferences.updateWith(update);
-        Set<CleanupPreferences.CleanupStep> activeJobs = result.getActiveJobs();
-
-        assertTrue(activeJobs.contains(CleanupPreferences.CleanupStep.MOVE_PDF));
+        EnumSet<CleanupPreferences.CleanupStep> expected = EnumSet.of(
+                CleanupPreferences.CleanupStep.CLEAN_UP_DOI,
+                CleanupPreferences.CleanupStep.MOVE_PDF
+        );
+        assertEquals(expected, result.getActiveJobs());
     }
 }
