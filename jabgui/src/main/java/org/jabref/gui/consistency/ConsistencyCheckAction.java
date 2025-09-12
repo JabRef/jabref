6c38c9d9a785ab447493377d08ccce6fef1b526a
Houssem Nasri <houssem.nasri@360t.com>
2025-09-12 18:41:30 +0200

    Fix unresponsive "Check Consistency" dialog

--- jabgui/src/main/java/org/jabref/gui/consistency/ConsistencyCheckAction.java
+++ jabgui/src/main/java/org/jabref/gui/consistency/ConsistencyCheckAction.java
@@ -77,7 +77,7 @@ public class ConsistencyCheckAction extends SimpleCommand {
         });
         taskExecutor.execute(task);
 
-        dialogService.showProgressDialogAndWait(
+        dialogService.showProgressDialog(
                 Localization.lang("Check consistency"),
                 Localization.lang("Checking consistency..."),
                 task);
