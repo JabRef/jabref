58151bbe3479b72cf74cdc29e45b6e05e8dc730f
InAnYan <ruslanpopov1512@gmail.com>
2025-09-13 22:19:28 +0300

    chore(test): better format comments in TransliterationTest

--- jablib/src/test/java/org/jabref/logic/util/strings/TransliterationTest.java
+++ jablib/src/test/java/org/jabref/logic/util/strings/TransliterationTest.java
@@ -9,15 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TransliterationTest {
     @ParameterizedTest(name = "string={0}, expected={1}")
     @CsvSource({
-            "Hello, Hello",                     // English
-            "Grüße, Grusse",                   // German
-            "संस्कृतम्, Sanskrtam",             // Sanskrit
-            "नमस्ते, Namaste",                  // Hindi
-            "Привет, Privet",                   // Russian
-            "Привіт, Privit",                   // Ukrainian, though "Pryvit" is better for expected result.
-            "你好, Ni Hao",                      // Chinese
-            "안녕하세요, Annyeonghaseyo",         // Korean
-            "مرحبا, Mrhba"                   // Arabic
+            "Hello, Hello",            // English
+            "Grüße, Grusse",           // German
+            "संस्कृतम्, Sanskrtam",        // Sanskrit
+            "नमस्ते, Namaste",            // Hindi
+            "Привет, Privet",          // Russian
+            "Привіт, Privit",          // Ukrainian, though "Pryvit" is better for expected result.
+            "你好, Ni Hao",             // Chinese
+            "안녕하세요, Annyeonghaseyo", // Korean
+            "مرحبا, Mrhba"             // Arabic
     })
     void transliterates(String string, String expected) {
         assertEquals(expected, Transliteration.transliterate(string, false));
