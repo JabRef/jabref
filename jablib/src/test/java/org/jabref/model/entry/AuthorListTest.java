e761fd7f96d4facce1b8b4884d29bb09868fc55a
Yizhang Cao <caoyizhang1202@gmail.com>
2025-09-23 21:31:44 -0700

    Added import org.junit.jupiter.params.ParameterizedTest; and import org.junit.jupiter.params.provider.CsvSource

--- jablib/src/test/java/org/jabref/model/entry/AuthorListTest.java
+++ jablib/src/test/java/org/jabref/model/entry/AuthorListTest.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
