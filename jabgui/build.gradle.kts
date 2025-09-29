42ac8805e2da3da2f5d2ed1b1cd7a6fef5767eeb
Siedlerchr <siedlerkiller@gmail.com>
2025-09-29 21:55:39 +0200

    try without icon

--- jabgui/build.gradle.kts
+++ jabgui/build.gradle.kts
@@ -190,8 +190,7 @@ javaModulePackaging {
         })
     }
     targetsWithOs("macos") {
-        options.addAll(
-            "--icon", "$projectDir/src/main/resources/icons/jabref.icns",
+        options.addAll( // --icon", "$projectDir/src/main/resources/icons/jabref.icns",
             "--mac-package-identifier", "JabRef",
             "--mac-package-name", "JabRef",
             "--file-associations", "$projectDir/buildres/macos/bibtexAssociations.properties",
