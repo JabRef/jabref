8d91aa7c7461e46a9f235b8a171c73ba0b95aceb
Kemal Yönet <kemalyoenet@Kemal-MacBook-Air.local>
2025-09-30 11:20:35 +0200

    uebung3: Add package-info for org.jabref.gui.help

---
+++ jabgui/src/main/java/org/jabref/gui/help/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Provides classes related to the "About" dialog and other help screens of the JabRef GUI.
+ * <p>Responsibilities:
+ * <ul>
+ *   <li>Showing application metadata (version, license, contributors).</li>
+ *   <li>Opening documentation and community links in the system browser.</li>
+ *   <li>Encapsulating small actions used by help-related views.</li>
+ * </ul>
+ *
+ * <p>Design notes:
+ * <ul>
+ *   <li>Classes in this package are JavaFX UI components or their view models.</li>
+ *   <li>They may depend on services from {@code org.jabref.gui} and {@code org.jabref.logic}.</li>
+ *   <li>No business logic should be added here; keep code presentation-centric.</li>
+ * </ul>
+ */
+package org.jabref.gui.help;
