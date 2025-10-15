ecb28184b3ac836996a3544f3482c4d344d8e601
Philip <37398281+palukku@users.noreply.github.com>
2025-10-16 01:09:32 +0200

    fix modernizer

--- jabls/src/main/java/org/jabref/languageserver/util/definition/DefinitionProviderFactory.java
+++ jabls/src/main/java/org/jabref/languageserver/util/definition/DefinitionProviderFactory.java
@@ -1,15 +1,14 @@
 package org.jabref.languageserver.util.definition;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import org.jabref.languageserver.util.LspParserHandler;
 
-import com.google.common.collect.Maps;
-
 public class DefinitionProviderFactory {
 
-    private static final Map<String, DefinitionProvider> PROVIDER_MAP = Maps.newHashMap();
+    private static final Map<String, DefinitionProvider> PROVIDER_MAP = new HashMap<>();
 
     public static Optional<DefinitionProvider> getDefinitionProvider(LspParserHandler parserHandler, String languageId) {
         return Optional.ofNullable(PROVIDER_MAP.computeIfAbsent(languageId.toLowerCase(), key -> switch (key) {
