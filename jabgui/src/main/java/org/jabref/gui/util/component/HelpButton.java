65e7566c725fa8d69a7e805690b05303b97929c1
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2025-10-20 21:37:41 +0200

    Small fixes

--- jabgui/src/main/java/org/jabref/gui/util/component/HelpButton.java
+++ jabgui/src/main/java/org/jabref/gui/util/component/HelpButton.java
@@ -18,6 +18,7 @@ import org.jspecify.annotations.NonNull;
 
 public class HelpButton extends Button {
     private final StringProperty helpUrl = new SimpleStringProperty("");
+    private final StringProperty tooltip = new SimpleStringProperty("");
     private boolean isHelpFileSet = false;
 
     public HelpButton() {
