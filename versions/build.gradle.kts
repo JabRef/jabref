546e041187beb26f24a325735c31c01d9c5127e3
Oliver Kopp <kopp.dev@gmail.com>
2025-10-30 20:07:50 +0100

    Revert "New study.yml format (#13844)" (#14204)
    This reverts commit db1e65195f87d0ea7a376b85f6b3bbb0710245cd.

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies.constraints {
     api("com.ibm.icu:icu4j:72.0.1!!")
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.0")
-    api("com.fasterxml.jackson-datatype-jdk8:2.20.0")
     api("com.fasterxml:aalto-xml:1.3.3")
     api("org.eclipse.lsp4j:org.eclipse.lsp4j:0.24.0")
     api("org.eclipse.lsp4j:org.eclipse.lsp4j.websocket:0.24.0")
