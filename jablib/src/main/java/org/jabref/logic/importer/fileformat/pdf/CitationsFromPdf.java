e8006ebd87eb2300c22c9564f66a5342b484505c
Oliver Kopp <kopp.dev@gmail.com>
2025-11-05 14:44:26 +0100

    Wire all availble importers

--- jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/CitationsFromPdf.java
+++ jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/CitationsFromPdf.java
@@ -1,4 +1,39 @@
 package org.jabref.logic.importer.fileformat.pdf;
 
+import java.nio.file.Path;
+
+import org.jabref.logic.ai.AiService;
+import org.jabref.logic.importer.ParserResult;
+import org.jabref.logic.importer.plaincitation.LlmPlainCitationParser;
+import org.jabref.logic.preferences.JabRefCliPreferences;
+import org.jabref.logic.util.CurrentThreadTaskExecutor;
+import org.jabref.logic.util.NotificationService;
+
+/// Wrapper around the different options to extract citations from a PDF
+///
+/// We deliberately opt for passing whole [JabRefCliPreferences] to ease calling this helper methods
 public class CitationsFromPdf {
+
+    public static ParserResult extractCitationsUsingTextMatching(JabRefCliPreferences preferences, Path path) {
+        BibliographyFromPdfImporter importer = new BibliographyFromPdfImporter(preferences.getCitationKeyPatternPreferences());
+        return importer.importDatabase(path);
+    }
+
+    public static ParserResult extractCitationsUsingGrobid(JabRefCliPreferences preferences, Path path) {
+        PdfGrobidImporter importer = new PdfGrobidImporter(preferences.getImportFormatPreferences());
+        return importer.importDatabase(path);
+    }
+
+    /// As NoticationService, one can pass `LOGGER::info`
+    public static ParserResult extractCitationsUsingLLM(JabRefCliPreferences preferences, NotificationService notificationService, Path path) {
+        try (AiService aiService = new AiService(
+                preferences.getAiPreferences(),
+                preferences.getFilePreferences(),
+                preferences.getCitationKeyPatternPreferences(),
+                notificationService,
+                new CurrentThreadTaskExecutor())) {
+            LlmPlainCitationParser importer = new LlmPlainCitationParser(aiService.getTemplatesService(), preferences.getImportFormatPreferences(), aiService.getChatLanguageModel());
+            return importer.importDatabase(path);
+        }
+    }
 }
