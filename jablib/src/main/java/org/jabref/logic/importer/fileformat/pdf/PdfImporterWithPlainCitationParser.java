7dc7437417752d68921d7bcc9506f22a949d9f65
Oliver Kopp <kopp.dev@gmail.com>
2025-11-05 14:29:55 +0100

    Add missing class

---
+++ jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfImporterWithPlainCitationParser.java
@@ -0,0 +1,25 @@
+package org.jabref.logic.importer.fileformat.pdf;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import org.jabref.logic.importer.ParserResult;
+import org.jabref.logic.importer.plaincitation.PlainCitationParser;
+import org.jabref.logic.importer.plaincitation.ReferencesBlockFromPdfFinder;
+import org.jabref.model.entry.BibEntry;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public abstract class PdfImporterWithPlainCitationParser extends PdfImporter implements PlainCitationParser {
+
+    @Override
+    public ParserResult importDatabase(Path filePath, PDDocument document) {
+        try {
+            String pagesText = ReferencesBlockFromPdfFinder.getReferencesPagesText(document);
+            List<BibEntry> entries = parseMultiplePlainCitations(pagesText);
+            return new ParserResult(entries);
+        } catch (Exception e) {
+            return ParserResult.fromError(e);
+        }
+    }
+}
