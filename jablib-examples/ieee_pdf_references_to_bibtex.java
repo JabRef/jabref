e9714df0ada443890564792d0f0aee14b059534e
Oliver Kopp <kopp.dev@gmail.com>
2025-11-06 10:22:27 +0100

    First version of to BibTeX conversion

--- jablib-examples/ieee_pdf_references_to_bibtex.java
+++ jablib-examples/ieee_pdf_references_to_bibtex.java
@@ -2,7 +2,7 @@
 
 import org.jabref.logic.exporter.BibWriter;
 import org.jabref.logic.exporter.BibDatabaseWriter;
-import org.jabref.logic.importer.fileformat.BibliographyFromPdfImporter;
+import org.jabref.logic.importer.fileformat.pdf.CitationsFromPdf;
 import org.jabref.logic.preferences.CliPreferences;
 import org.jabref.logic.preferences.JabRefCliPreferences;
 import org.tinylog.Logger;
@@ -16,45 +16,24 @@ import org.tinylog.Logger;
 //DEPS org.jabref:jablib:6.0-SNAPSHOT
 //REPOS mavencentral,mavencentralsnapshots=https://central.sonatype.com/repository/maven-snapshots/,s01oss=https://s01.oss.sonatype.org/content/repositories/snapshots/,oss=https://oss.sonatype.org/content/repositories,jitpack=https://jitpack.io,oss2=https://oss.sonatype.org/content/groups/public,ossrh=https://oss.sonatype.org/content/repositories/snapshots,raw=https://raw.githubusercontent.com/JabRef/jabref/refs/heads/main/jablib/lib/
 
-// generate with
-//     git diff --name-only main | grep jablib/src/main | sed "s#\(.*\)#//SOURCES ../\1#" | grep -v module-info | grep -v .properties
-//SOURCES ../jablib/src/main/java/org/jabref/logic/ai/AiDefaultPreferences.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/bibtex/BibEntryWriter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/citation/repository/BibEntrySerializer.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/crawler/StudyRepository.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/exporter/BibDatabaseWriter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/git/io/GitFileWriter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/Importer.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/ParserResult.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/BibliographyFromPdfImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/BibtexParser.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/PdfMergeMetadataImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfContentImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfEmbeddedBibFileImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfGrobidImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfVerbatimBibtexImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/PdfXmpImporter.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/plaincitation/LlmPlainCitationParser.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/plaincitation/MultiplePlainCitationsParser.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/plaincitation/PlainCitationParserChoice.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/plaincitation/ReferencesBlockFromPdfFinder.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/importer/util/GrobidService.java
-//SOURCES ../jablib/src/main/java/org/jabref/logic/preferences/JabRefCliPreferences.jav
-
 void main() throws Exception {
     var preferences = JabRefCliPreferences.getInstance();
 
-    extractEntriesUsingBibliographyFromPdfImporter()
+    // This is PDF to BibTeX
+    // Choose one
+    var result = CitationsFromPdf.extractCitationsUsingTextMatching(preferences, Path.of("pdfs", "test.pdf"));
+    // var result = CitationsFromPdf.extractCitationsUsingGrobid(preferences, Path.of("pdfs", "test.pdf"));
+    // var result = CitationsFromPdf.extractCitationsUsingLLM(preferences, Logger::info, Path.of("pdfs", "test.pdf"));
 
-    var importer = new BibliographyFromPdfImporter(preferences.getCitationKeyPatternPreferences());
-    var result = importer.importDatabase(Path.of("pdfs", "test.pdf"));
     if (result.hasWarnings()) {
         Logger.warn("There were warnings during the import. {}", result.getWarningsMap());
     }
+
+    // Write the result as BibTeX file
     var context = result.getDatabaseContext();
-    var writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8);
-    var entries = result.getDatabase().getEntries();
-    var bibWriter = new BibDatabaseWriter(writer, context, preferences);
-    bibWriter.writeDatabase(context);
+    try (var writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8)) {
+        var entries = result.getDatabase().getEntries();
+        var bibWriter = new BibDatabaseWriter(writer, context, preferences);
+        bibWriter.writeDatabase(context);
+    }
 }
