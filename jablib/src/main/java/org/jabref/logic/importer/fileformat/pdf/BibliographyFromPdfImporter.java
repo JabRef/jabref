0f4811d5bea7ab99c97a6c877eb30eadc415e544
Oliver Kopp <kopp.dev@gmail.com>
2025-11-06 15:29:25 +0100

    Fix checkstyle

--- jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/BibliographyFromPdfImporter.java
+++ jablib/src/main/java/org/jabref/logic/importer/fileformat/pdf/BibliographyFromPdfImporter.java
@@ -1,5 +1,5 @@
 package org.jabref.logic.importer.fileformat.pdf;
 
 /// This class groups all PDF importers which extract the reference section (also called "citations" or "bibliography") from a PDF
-public abstract class BibliographyFromPdfImporter extends PdfImporter{
+public abstract class BibliographyFromPdfImporter extends PdfImporter {
 }
