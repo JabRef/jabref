c07d71c2863cd98fbeb85d7c71ab6bb36ca3005d
Siedlerchr <siedlerkiller@gmail.com>
2025-11-09 15:04:42 +0100

    fix jbang

--- jabsrv/src/main/java/module-info.java
+++ jabsrv/src/main/java/module-info.java
@@ -10,7 +10,7 @@ module org.jabref.jabsrv {
     exports org.jabref.http.server.command to jersey.server;
     opens org.jabref.http.server.cayw to com.google.gson, org.glassfish.hk2.locator, org.glassfish.hk2.utilities;
     opens org.jabref.http.dto to com.google.gson;
-    opens org.jabref.http.server.command to com.google.gson, org.glassfish.hk2.locator, org.glassfish.hk2.utilities, com.fasterxml.jackson.databind;
+    opens org.jabref.http.server.command to com.google.gson, org.glassfish.hk2.locator, org.glassfish.hk2.utilities, tools.jackson.databind;
     exports org.jabref.http.server.services;
     exports org.jabref.http;
     opens org.jabref.http.server.resources to org.glassfish.hk2.locator, org.glassfish.hk2.utilities;
