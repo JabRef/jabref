b8282623e167b5f5a9a2bf0feaefb0b7fc3161e0
Siedlerchr <siedlerkiller@gmail.com>
2025-11-10 20:48:25 +0100

    fix imports

--- .jbang/JabSrvLauncher.java
+++ .jbang/JabSrvLauncher.java
@@ -91,6 +91,7 @@
 //DEPS com.github.ben-manes.caffeine:caffeine:3.2.3
 //DEPS tools.jackson.core:jackson-core:3.0.2
 //DEPS tools.jackson.core:jackson-databind:3.0.2
+//DEPS tools.jackson.dataformat:jackson-dataformat-yaml:3.0.2
 
 /// This class is required for [jbang](https://www.jbang.dev/)
 public class JabSrvLauncher {
