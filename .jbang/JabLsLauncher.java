104b0c7ed82efe925831bb134a19242c8db01a5b
Oliver Kopp <kopp.dev@gmail.com>
2025-11-13 12:27:50 +0100

    Fix lsp4j dependency for JabLS

--- .jbang/JabLsLauncher.java
+++ .jbang/JabLsLauncher.java
@@ -37,9 +37,9 @@
 
 // from jabls
 //DEPS com.fasterxml.jackson.core:jackson-databind:2.20.1
-//DEPS org.eclipse:lsp4j:0.24.0
 //DEPS info.picocli:picocli:4.7.7
 //DEPS org.apache.logging.log4j:log4j-to-slf4j:2.25.2
+//DEPS org.eclipse.lsp4j:org.eclipse.lsp4j:0.24.0
 //DEPS org.jabref:afterburner.fx:2.0.0
 //DEPS org.slf4j:jul-to-slf4j:2.0.17
 //DEPS org.slf4j:slf4j-api:2.0.17
