ccd4a871cacdcb9b86de9d8a8ff73f0c0247782e
Oliver Kopp <kopp.dev@gmail.com>
2025-11-14 23:33:54 +0100

    Fix JavaLanguageVersion

--- build-logic/src/main/kotlin/org.jabref.gradle.feature.compile.gradle.kts
+++ build-logic/src/main/kotlin/org.jabref.gradle.feature.compile.gradle.kts
@@ -19,7 +19,7 @@ java {
         // - docs/getting-into-the-code/guidelines-for-setting-up-a-local-workspace/intellij-12-build.md
         // - jablib-examples/jbang/*.java
         // - jablib-examples/maven3/*/pom.xml
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
         // See https://docs.gradle.org/current/javadoc/org/gradle/jvm/toolchain/JvmVendorSpec.html for a full list
         // Temurin does not ship jmods, thus we need to use another JDK -- see https://github.com/actions/setup-java/issues/804
         // We also need a JDK without JavaFX, because we patch JavaFX due to modularity issues
