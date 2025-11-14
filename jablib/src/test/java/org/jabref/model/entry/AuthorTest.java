494d5bd5b07f9fd5d694c194c4a7f2d0849efea6
Oliver Kopp <kopp.dev@gmail.com>
2025-11-14 13:38:42 +0100

    Use CsvSource and TextBlock in AuthorTest

--- jablib/src/test/java/org/jabref/model/entry/AuthorTest.java
+++ jablib/src/test/java/org/jabref/model/entry/AuthorTest.java
@@ -6,54 +6,73 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AuthorTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "AO, 'A. O.'",
-            "AO., 'A. O.'",
-            "A.O., 'A. O.'",
-            "A-O, 'A.-O.'"
-    })
+    @CsvSource(
+            """
+                    AO, A. O.
+                    AO., A. O.
+                    A.O., A. O.
+                    A-O, A.-O.
+                    """
+    )
     void addDotIfAbbreviationAddsDot(String input, String expected) {
         assertEquals(expected, Author.addDotIfAbbreviation(input));
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "'A O', 'A. O.'",
-            "'A-melia', 'A.-melia'",
-            "'AmeliA', 'AmeliA'",
-            "'Ameli A', 'Ameli A.'",
-            "'Ameli ', 'Ameli'",
-            "'Ameli AA', 'Ameli A. A.'"
-    })
+    @CsvSource("""
+            A O, A. O.
+            A-melia, A.-melia
+            AmeliA, AmeliA
+            Ameli A, Ameli A.
+            Ameli , Ameli
+            Ameli AA, Ameli A. A.
+            """
+    )
     void addDotIfAbbreviationEdgeCases(String input, String expected) {
         assertEquals(expected, Author.addDotIfAbbreviation(input));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"O.", "A. O.", "A.-O.",
-            "O. Moore", "A. O. Moore", "O. von Moore", "A.-O. Moore",
-            "Moore, O.", "Moore, O., Jr.", "Moore, A. O.", "Moore, A.-O.",
-            "MEmre", "{\\'{E}}douard", "J{\\\"o}rg", "Moore, O. and O. Moore",
-            "Moore, O. and O. Moore and Moore, O. O."})
+    @CsvSource(delimiterString = "->", textBlock = """
+            O.
+            A. O.
+            A.-O.
+            O. Moore
+            A. O. Moore
+            O. von Moore
+            A.-O. Moore
+            Moore, O.
+            Moore, O., Jr.
+            Moore, A. O.
+            Moore, A.-O.
+            MEmre
+            {\\\\'{E}}douard
+            J{\\\\\\"o}rg
+            Moore, O. and O. Moore
+            Moore, O. and O. Moore and Moore, O. O.
+            """)
     void addDotIfAbbreviationDoNotAddDot(String input) {
         assertEquals(input, Author.addDotIfAbbreviation(input));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    @ValueSource(strings = {
-            // Lower-case letters
-            "asdf", "a",
-            // Numbers
-            "1", "1 23"
-    })
+    @CsvSource(
+            """
+                    # Lower-case letters
+                    asdf
+                    a
+                    # Numbers
+                    1
+                    1 23
+                    """
+    )
     void addDotIfAbbreviation(String input) {
         assertEquals(input, Author.addDotIfAbbreviation(input));
     }
