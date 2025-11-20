d449518918e86027d734f70ca08835778682f759
Alex1034 <alexandra.petrulova@student.tuke.sk>
2025-11-20 21:44:57 +0100

    Add date range tests and changelog entry

--- jabgui/src/test/java/org/jabref/gui/fieldeditors/DateEditorViewModelTest.java
+++ jabgui/src/test/java/org/jabref/gui/fieldeditors/DateEditorViewModelTest.java
@@ -1,27 +1,25 @@
 package org.jabref.gui.fieldeditors;
 
+import java.time.LocalDate;
+import javax.swing.undo.UndoManager;
 import javafx.util.StringConverter;
 
 import org.jabref.gui.autocompleter.SuggestionProvider;
 import org.jabref.logic.integrity.FieldCheckers;
-import org.jabref.model.entry.Date;
 import org.jabref.model.entry.field.Field;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import javax.swing.undo.UndoManager;
-
-import java.time.LocalDate;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DateEditorViewModelTest {
-
     private DateEditorViewModel viewModel;
     private StringConverter<TemporalAccessor> dateToStringConverter;
 
