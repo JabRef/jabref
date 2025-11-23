7e081f70a80fd99b0de7d24652e91249d296af42
Alex1034 <alexandra.petrulova@student.tuke.sk>
2025-11-23 21:33:42 +0100

    Repair

--- jablib/src/test/java/org/jabref/model/entry/DateTest.java
+++ jablib/src/test/java/org/jabref/model/entry/DateTest.java
@@ -22,8 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-
 class DateTest {
 
     private static Stream<Arguments> validDates() {
