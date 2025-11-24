f67286bd1028d04215394ea9f72769819c965475
Christoph <siedlerkiller@gmail.com>
2025-11-24 22:07:42 +0100

    Apply suggestions from code review

--- jabgui/src/main/java/org/jabref/gui/clipboard/JabRefClipboardData.java
+++ jabgui/src/main/java/org/jabref/gui/clipboard/JabRefClipboardData.java
@@ -6,7 +6,7 @@ import org.jabref.gui.StateManager;
 import org.jabref.model.TransferMode;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.jspecify.annotations.NullMarked;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
