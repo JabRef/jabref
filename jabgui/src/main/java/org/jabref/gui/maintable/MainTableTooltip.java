5dd02dd2f6b11752bcab078066c85a1c724d8e56
Assemien Olivia <tanoh.assemien@imt-atlantique.net>
2025-11-26 10:16:12 +0100

    resolution issue v1

--- jabgui/src/main/java/org/jabref/gui/maintable/MainTableTooltip.java
+++ jabgui/src/main/java/org/jabref/gui/maintable/MainTableTooltip.java
@@ -3,6 +3,7 @@ package org.jabref.gui.maintable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import org.jabref.gui.DialogService;
 import org.jabref.gui.preferences.GuiPreferences;
@@ -11,6 +12,7 @@ import org.jabref.gui.theme.ThemeManager;
 import org.jabref.logic.util.TaskExecutor;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
+
 
 public class MainTableTooltip extends Tooltip {
 
@@ -22,15 +24,36 @@ public class MainTableTooltip extends Tooltip {
     public MainTableTooltip(DialogService dialogService, GuiPreferences preferences, ThemeManager themeManager, TaskExecutor taskExecutor) {
         this.preferences = preferences;
         this.preview = new PreviewViewer(dialogService, preferences, themeManager, taskExecutor);
-        this.tooltipContent.getChildren().addAll(fieldValueLabel, preview);
+
+        // Configuration simple
+        fieldValueLabel.setWrapText(true);
+        fieldValueLabel.setMaxWidth(400);
+
+        tooltipContent.getChildren().addAll(fieldValueLabel, preview);
+        tooltipContent.setSpacing(5);
+        tooltipContent.setStyle("-fx-padding: 8;");
+
+        // Tooltip configuré pour le scroll
+        this.setMaxWidth(400);
+        this.setWrapText(true);
+        this.setShowDelay(Duration.millis(100));
+        this.setHideDelay(Duration.seconds(10));
+        //this.setShowDuration(Duration.seconds(30));
+        //this.setAutoHide(false);
+        //this.setHideOnEscape(true);
+        //this.setConsumeAutoHidingEvents(false);
     }
 
     public Tooltip createTooltip(BibDatabaseContext databaseContext, BibEntry entry, String fieldValue) {
-        fieldValueLabel.setText(fieldValue + "\n");
+        fieldValueLabel.setText(fieldValue);
+
         if (preferences.getPreviewPreferences().shouldShowPreviewEntryTableTooltip()) {
             preview.setLayout(preferences.getPreviewPreferences().getSelectedPreviewLayout());
             preview.setDatabaseContext(databaseContext);
             preview.setEntry(entry);
+
+            // Taille fixe simple et efficace
+            preview.setMaxHeight(200); // Hauteur constante
             this.setGraphic(tooltipContent);
         } else {
             this.setGraphic(fieldValueLabel);
