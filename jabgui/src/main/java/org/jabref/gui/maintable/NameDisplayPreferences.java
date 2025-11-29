b60aa9a1221eb09c02d0442e26a3bd48ffce6176
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-11-29 03:59:01 -0600

    Code complet de NameDisplayPreferences.java et JabRefGuiPreferences.java

--- jabgui/src/main/java/org/jabref/gui/maintable/NameDisplayPreferences.java
+++ jabgui/src/main/java/org/jabref/gui/maintable/NameDisplayPreferences.java
@@ -1,48 +1,73 @@
 package org.jabref.gui.maintable;
 
+
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+
 public class NameDisplayPreferences {
+
 
     public enum DisplayStyle {
         NATBIB, AS_IS, FIRSTNAME_LASTNAME, LASTNAME_FIRSTNAME
     }
 
+
     public enum AbbreviationStyle {
         NONE, LASTNAME_ONLY, FULL
     }
 
+
     private final ObjectProperty<DisplayStyle> displayStyle = new SimpleObjectProperty<>();
     private final ObjectProperty<AbbreviationStyle> abbreviationStyle = new SimpleObjectProperty<>();
+
+
+
 
     public NameDisplayPreferences(DisplayStyle displayStyle,
                                   AbbreviationStyle abbreviationStyle) {
         this.displayStyle.set(displayStyle);
         this.abbreviationStyle.set(abbreviationStyle);
     }
+    private NameDisplayPreferences() {
+        this(DisplayStyle.LASTNAME_FIRSTNAME, AbbreviationStyle.NONE);
+    }
+    public static NameDisplayPreferences getDefault() {
+        return new NameDisplayPreferences();
+    }
+
 
     public DisplayStyle getDisplayStyle() {
         return displayStyle.get();
     }
 
+
     public ObjectProperty<DisplayStyle> displayStyleProperty() {
         return displayStyle;
     }
+
 
     public void setDisplayStyle(DisplayStyle displayStyle) {
         this.displayStyle.set(displayStyle);
     }
 
+
     public AbbreviationStyle getAbbreviationStyle() {
         return abbreviationStyle.get();
     }
+
 
     public ObjectProperty<AbbreviationStyle> abbreviationStyleProperty() {
         return abbreviationStyle;
     }
 
+
     public void setAbbreviationStyle(AbbreviationStyle abbreviationStyle) {
         this.abbreviationStyle.set(abbreviationStyle);
     }
+    public void setAll(NameDisplayPreferences other) {
+        setDisplayStyle(other.getDisplayStyle());
+        setAbbreviationStyle(other.getAbbreviationStyle());
+    }
 }
+
