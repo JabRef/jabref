7474d770e2d75baad4cc332bbdd1eec8aa8197f0
Om Shripad Kapale <omshripadkapale@gmail.com>
2025-11-30 22:23:48 +0530

    Fix checkstyle violations - separate variable declarations

--- jablib/src/main/java/org/jabref/model/entry/identifier/ArXivIdentifier.java
+++ jablib/src/main/java/org/jabref/model/entry/identifier/ArXivIdentifier.java
@@ -20,7 +20,8 @@ import org.slf4j.LoggerFactory;
 public class ArXivIdentifier extends EprintIdentifier {
     private static final Logger LOGGER = LoggerFactory.getLogger(ArXivIdentifier.class);
 
-    private static final String ARXIV_PREFIX = "http(s)?://arxiv.org/(abs|pdf|html)/|arxiv|arXiv";    private final String identifier;
+    private static final String ARXIV_PREFIX = "http(s)?://arxiv.org/(abs|pdf|html)/|arxiv|arXiv";
+    private final String identifier;
     private final String classification;
     private final String version;
 
