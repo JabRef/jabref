1293aa5e78c51e6d914240ef336cf2162229ed6b
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2025-11-30 19:37:31 +0100

    Undo changes to KeyCollisionException

--- jablib/src/main/java/org/jabref/model/database/KeyCollisionException.java
+++ jablib/src/main/java/org/jabref/model/database/KeyCollisionException.java
@@ -1,36 +1,27 @@
 package org.jabref.model.database;
 
-import java.util.Objects;
-
-import org.jspecify.annotations.NonNull;
-
 public class KeyCollisionException extends RuntimeException {
 
-    private static final String UNKNOWN_ID = "<unknown>";
-
-    private final @NonNull String id;
+    private String id;
 
     public KeyCollisionException() {
         super();
-        this.id = UNKNOWN_ID;
     }
 
     public KeyCollisionException(String msg, String id) {
         super(msg);
-        this.id = Objects.requireNonNullElse(id, UNKNOWN_ID);
+        this.id = id;
     }
 
     public KeyCollisionException(String msg, Throwable exception) {
         super(msg, exception);
-        this.id = UNKNOWN_ID;
     }
 
     public KeyCollisionException(Throwable exception) {
         super(exception);
-        this.id = UNKNOWN_ID;
     }
 
-    public @NonNull String getId() {
+    public String getId() {
         return id;
     }
 }
