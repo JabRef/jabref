05df76e54454e71f2de92ac744eb05c0e0ac51ca
Oliver Kopp <kopp.dev@gmail.com>
2025-12-01 23:10:36 +0100

    Add initial check for token

--- jabgui/src/test/java/org/jabref/gui/citedrive/CiteDriveOAuthServiceTest.java
+++ jabgui/src/test/java/org/jabref/gui/citedrive/CiteDriveOAuthServiceTest.java
@@ -1,28 +1,62 @@
 package org.jabref.gui.citedrive;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+import javafx.collections.FXCollections;
+
+import org.jabref.gui.externalfiletype.ExternalFileTypes;
 import org.jabref.gui.frame.ExternalApplicationsPreferences;
+import org.jabref.http.SrvStateManager;
+import org.jabref.http.manager.HttpServerManager;
 import org.jabref.logic.citedrive.OAuthSessionRegistry;
+import org.jabref.logic.preferences.CliPreferences;
 import org.jabref.logic.remote.RemotePreferences;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class CiteDriveOAuthServiceTest {
 
+    private static HttpServerManager HTTP_SERVER_MANAGER = new HttpServerManager();
+
+    @BeforeAll
+    static void startServer() {
+        CliPreferences cliPreferences = mock(CliPreferences.class);
+        SrvStateManager srvStateManager = mock(SrvStateManager.class);
+        URI uri = URI.create("http://localhost:23119");
+        HTTP_SERVER_MANAGER.start(cliPreferences, srvStateManager, uri);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        HTTP_SERVER_MANAGER.stop();
+    }
+
     @Test
-    // @Disabled
-    void getToken() {
+    @Disabled
+    // @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    void getToken() throws ExecutionException, InterruptedException {
         ExternalApplicationsPreferences externalApplicationsPreferences = mock(ExternalApplicationsPreferences.class);
+        when(externalApplicationsPreferences.getExternalFileTypes()).thenReturn(FXCollections.observableSet(new TreeSet<>(ExternalFileTypes.getDefaultExternalFileTypes())));
+
         RemotePreferences remotePreferences = mock(RemotePreferences.class);
         when(remotePreferences.getHttpServerUri()).thenReturn(URI.create("http://localhost:23119"));
+
         OAuthSessionRegistry oAuthSessionRegistry = new OAuthSessionRegistry();
         CiteDriveOAuthService citeDriveOAuthService = new CiteDriveOAuthService(externalApplicationsPreferences, remotePreferences, oAuthSessionRegistry);
-        citeDriveOAuthService.authorizeInteractive();
+
+        Optional<Tokens> actual = citeDriveOAuthService.authorizeInteractive().get();
+        assertTrue(actual.isPresent());
     }
 }
