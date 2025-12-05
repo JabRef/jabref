7bf71ea7723e2123864e0013bbd4dbe0dcacd7ea
Enzo Hirotani <enzoyhirotani@gmail.com>
2025-12-04 21:59:17 -0300

    SonarCloud Config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("org.jabref.gradle.base.repositories")
     id("org.jabref.gradle.feature.compile") // for openrewrite
-    id("org.openrewrite.rewrite") version "7.21.0"
+    id("org.openrewrite.rewrite") version "7.17.0"
     id("org.itsallcode.openfasttrace") version "3.1.0"
-    id("org.cyclonedx.bom") version "3.1.0"
+    id("org.cyclonedx.bom") version "3.0.0-alpha-1"
     id("org.sonarqube") version "7.1.0.6387"
+
 }
 
-sonar {
+sonarqube {
     properties {
-        property("sonar.projectKey", "EnzoHirotani_ES2_2025-2_JabRef2")
+        // Kotlin DSL requires parentheses for function calls with multiple arguments
+        property("sonar.projectKey", "EnzoHirotani_ES2_2025-2_JabRef")
         property("sonar.organization", "enzohirotani")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
 
@@ -18,7 +21,7 @@ sonar {
 // This is the behavior when applied in the root project (https://docs.openrewrite.org/reference/gradle-plugin-configuration#multi-module-gradle-projects)
 
 dependencies {
-    rewrite(platform("org.openrewrite.recipe:rewrite-recipe-bom:3.19.0"))
+    rewrite(platform("org.openrewrite.recipe:rewrite-recipe-bom:3.15.0"))
     rewrite("org.openrewrite.recipe:rewrite-static-analysis")
     rewrite("org.openrewrite.recipe:rewrite-logging-frameworks")
     rewrite("org.openrewrite.recipe:rewrite-testing-frameworks")
@@ -45,12 +48,12 @@ rewrite {
 
 requirementTracing {
     inputDirectories.setFrom(files("docs",
-            "jablib/src/main/java", "jablib/src/test/java",
-            "jabls/src/main/java", "jabls/src/test/java",
-            "jabkit/src/main/java", "jabkit/src/test/java",
-            "jabgui/src/main/java", "jabgui/src/test/java",
-            "jabsrv/src/main/java", "jabsrv/src/test/java"
-        )
+        "jablib/src/main/java", "jablib/src/test/java",
+        "jabls/src/main/java", "jabls/src/test/java",
+        "jabkit/src/main/java", "jabkit/src/test/java",
+        "jabgui/src/main/java", "jabgui/src/test/java",
+        "jabsrv/src/main/java", "jabsrv/src/test/java"
+    )
     )
     // TODO: Short Tag Importer: https://github.com/itsallcode/openfasttrace-gradle#configuring-the-short-tag-importer
 }
