c8662fc1b84ab3c71ef1f8ee1b942512da08021d
Prasanth <prasanth530001@gmail.com>
2025-12-08 02:28:11 +0530

    Fix import order for Checkstyle

--- jablib/src/test/java/org/jabref/logic/bibtex/comparator/plausibility/PersonNamesPlausibilityComparatorTest.java
+++ jablib/src/test/java/org/jabref/logic/bibtex/comparator/plausibility/PersonNamesPlausibilityComparatorTest.java
@@ -1,6 +1,7 @@
 package org.jabref.logic.bibtex.comparator.plausibility;
 
 import org.jabref.logic.bibtex.comparator.ComparisonResult;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
