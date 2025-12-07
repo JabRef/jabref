f089f2a4b9a06d1c608677de7f6d398a5c0c68e7
Prasanth <prasanth530001@gmail.com>
2025-12-07 23:55:38 +0530

    Add PersonNamesPlausibilityComparator to improve merge dialog selection for author fields

---
+++ jablib/src/main/java/org/jabref/logic/bibtex/comparator/plausibility/PersonNamesPlausibilityComparator.java
@@ -0,0 +1,40 @@
+package org.jabref.logic.bibtex.comparator.plausibility;
+
+import org.jabref.logic.bibtex.comparator.ComparisonResult;
+import org.jabref.model.entry.AuthorList;
+
+public class PersonNamesPlausibilityComparator implements FieldValuePlausibilityComparator {
+
+    @Override
+    public ComparisonResult compare(String left, String right) {
+
+        if (left == null || right == null) {
+            return ComparisonResult.UNDETERMINED;
+        }
+
+        var leftAuthors = AuthorList.parse(left);
+        var rightAuthors = AuthorList.parse(right);
+
+        int leftCount = leftAuthors.getNumberOfAuthors();
+        int rightCount = rightAuthors.getNumberOfAuthors();
+
+        if (leftCount > rightCount) {
+            return ComparisonResult.LEFT_BETTER;
+        }
+        if (rightCount > leftCount) {
+            return ComparisonResult.RIGHT_BETTER;
+        }
+
+        int leftLength = left.length();
+        int rightLength = right.length();
+
+        if (leftLength > rightLength) {
+            return ComparisonResult.LEFT_BETTER;
+        }
+        if (rightLength > leftLength) {
+            return ComparisonResult.RIGHT_BETTER;
+        }
+
+        return ComparisonResult.UNDETERMINED;
+    }
+}
