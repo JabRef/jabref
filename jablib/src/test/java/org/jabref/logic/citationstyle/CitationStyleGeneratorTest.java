969d66076aa95c8f6c7cb43697ed0bc7c7222f90
Oliver Kopp <kopp.dev@gmail.com>
2025-12-11 15:57:58 +0100

    More conservative

--- jablib/src/test/java/org/jabref/logic/citationstyle/CitationStyleGeneratorTest.java
+++ jablib/src/test/java/org/jabref/logic/citationstyle/CitationStyleGeneratorTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -24,6 +25,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Execution(ExecutionMode.SAME_THREAD)
+@ResourceLock("Localization.lang")
 class CitationStyleGeneratorTest {
 
     private static final List<CitationStyle> STYLE_LIST = CSLStyleLoader.getInternalStyles();
