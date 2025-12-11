f3c09a95e617c90da0c3c2ae1d347fd4d7b926ac
Oliver Kopp <kopp.dev@gmail.com>
2025-12-11 12:30:11 +0100

    Fix JabKit tests for parallel execution

--- jabkit/src/test/java/org/jabref/toolkit/commands/JabKitTest.java
+++ jabkit/src/test/java/org/jabref/toolkit/commands/JabKitTest.java
@@ -21,11 +21,14 @@ import org.jabref.support.BibEntryAssert;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+@Execution(ExecutionMode.SAME_THREAD) // Embedded postgres is started per test class and causes conflicts for file "libicuuc.so"
 class JabKitTest extends AbstractJabKitTest {
 
     @Test
