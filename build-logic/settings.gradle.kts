8678fc04d6f0b0812013b809e0b7b7e697436c95
Oliver Kopp <kopp.dev@gmail.com>
2025-12-26 16:09:10 +0100

    More config

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,10 +1,2 @@
 rootProject.name = "build-logic"
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
