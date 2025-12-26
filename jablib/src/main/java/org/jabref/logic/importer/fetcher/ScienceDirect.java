20e308fc17c4bd69e74a27be96915d4212b82a31
Kollakota Siva Sai <133692412+Siva-Sai22@users.noreply.github.com>
2025-12-26 17:32:54 +0530

    Update jablib/src/main/java/org/jabref/logic/importer/fetcher/ScienceDirect.java
    Co-authored-by: Oliver Kopp <kopp.dev@gmail.com>

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/ScienceDirect.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/ScienceDirect.java
@@ -137,7 +137,7 @@ public class ScienceDirect implements FulltextFetcher, CustomizableKeyFetcher {
         try {
             String request = API_URL + doi;
             HttpResponse<JsonNode> jsonResponse = Unirest.get(request)
-                                                         // Share the same key as Scopus (https://devdocs.jabref.org/code-howtos/fetchers.html#fetchers)
+                                                         // Shares the same key as Scopus, because both are offered by Elsevier (https://devdocs.jabref.org/code-howtos/fetchers.html#fetchers)
                                                          .header("X-ELS-APIKey", importerPreferences.getApiKey("Scopus").orElse(""))
                                                          .queryString("httpAccept", "application/json")
                                                          .asJson();
