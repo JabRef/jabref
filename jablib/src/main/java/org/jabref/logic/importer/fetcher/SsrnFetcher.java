5bded80f2dd83087178964d063e44c56b90f9920
Oliver Kopp <kopp.dev@gmail.com>
2025-12-30 23:34:18 +0100

    Add simple route

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/SsrnFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/SsrnFetcher.java
@@ -1,9 +1,25 @@
 package org.jabref.logic.importer.fetcher;
 
+import java.util.Optional;
+
+import org.jabref.logic.importer.FetcherException;
 import org.jabref.logic.importer.ImportFormatPreferences;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.identifier.SSRN;
 
 public class SsrnFetcher extends DoiFetcher {
     public SsrnFetcher(ImportFormatPreferences preferences) {
         super(preferences);
+    }
+
+    @Override
+    public String getName() {
+        return "SSRN";
+    }
+
+    @Override
+    public Optional<BibEntry> performSearchById(String identifier) throws FetcherException {
+        String doi = SSRN.parse(identifier).map(ssrn -> ssrn.toDoi().asString()).orElse(identifier);
+        return super.performSearchById(doi);
     }
 }
