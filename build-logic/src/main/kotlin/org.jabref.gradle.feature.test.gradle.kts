ef484d542e74c91c257bd6faf4c5da964e462b60
Oliver Kopp <kopp.dev@gmail.com>
2026-01-02 22:02:10 +0100

    Remove JUnit parallelism (and rely on Gradle only)

--- build-logic/src/main/kotlin/org.jabref.gradle.feature.test.gradle.kts
+++ build-logic/src/main/kotlin/org.jabref.gradle.feature.test.gradle.kts
@@ -13,13 +13,21 @@ testing {
 }
 
 tasks.withType<Test>().configureEach {
-    // Enable parallel tests (on desktop).
-    if (!providers.environmentVariable("CI").map { it.toBoolean() }.getOrElse(false)) {
+    // Note: For now, parallel handling with gradle is enough
+    //       How to enable parallel JUnit tests **in addition**: https://docs.junit.org/6.0.1/writing-tests/parallel-execution.html
+
+    if (providers.environmentVariable("CI").map { it.toBoolean() }.getOrElse(false) ||
+        providers.environmentVariable("GITHUB_ACTIONS").map { it.toBoolean() }.getOrElse(false)) {
+        maxParallelForks = 1
+    } else {
+       // Enable parallel tests (on desktop).
         // See https://docs.gradle.org/8.1/userguide/performance.html#execute_tests_in_parallel for details.
         maxParallelForks = (Runtime.getRuntime().availableProcessors() - 1).coerceAtLeast(1)
-        // See https://docs.gradle.org/current/userguide/performance.html#b_fork_tests_into_multiple_processes for details.
-        forkEvery = 100
     }
+
+    // Even in sequential tests, to a "force" cleanup
+    // See https://docs.gradle.org/current/userguide/performance.html#b_fork_tests_into_multiple_processes for details.
+    forkEvery = 100
 }
 
 testlogger {
