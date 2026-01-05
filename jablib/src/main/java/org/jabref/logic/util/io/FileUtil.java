a84bf7898abca57addbbb10bf8da6244e128ddf1
Oliver Kopp <kopp.dev@gmail.com>
2026-01-05 11:48:37 +0100

    Fix checkstyle

--- jablib/src/main/java/org/jabref/logic/util/io/FileUtil.java
+++ jablib/src/main/java/org/jabref/logic/util/io/FileUtil.java
@@ -9,7 +9,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
