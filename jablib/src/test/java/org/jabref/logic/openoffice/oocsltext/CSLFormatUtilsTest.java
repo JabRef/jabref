b8d06a7a17bea1cac213b4ffa43ed543d5df2b0f
subhramit <subhramit.bb@live.in>
2026-01-13 02:46:56 +0530

    One more dot
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- jablib/src/test/java/org/jabref/logic/openoffice/oocsltext/CSLFormatUtilsTest.java
+++ jablib/src/test/java/org/jabref/logic/openoffice/oocsltext/CSLFormatUtilsTest.java
@@ -168,7 +168,7 @@ class CSLFormatUtilsTest {
 
                 // Numeric type "[1]", brackets, newlines
                 Arguments.of(
-                        "[1] B. Smith, B. Jones, and J. Williams, “Title of the test entry,” <i>BibTeX Journal</i>, vol. 34, no. 3, pp. 45–67, July 2016, doi: 10.1001/bla.blubb.<p></p>",
+                        "[1] B. Smith, B. Jones, and J. Williams, “Title of the test entry,” <i>BibTeX Journal</i>, vol. 34, no. 3, pp. 45–67, Jul. 2016, doi: 10.1001/bla.blubb.<p></p>",
                         STYLE_LIST.stream().filter(e -> "IEEE Reference Guide version 11.29.2023".equals(e.getTitle())).findAny().get()
                 ),
 
