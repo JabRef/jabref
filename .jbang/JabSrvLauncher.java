4a8f9c17b3c2908559b0a18361dd9ececda6b40e
Oliver Kopp <kopp.dev@gmail.com>
2026-01-12 23:14:36 +0100

    Fix JBang

--- .jbang/JabSrvLauncher.java
+++ .jbang/JabSrvLauncher.java
@@ -14,6 +14,7 @@
 //DEPS info.picocli:picocli:4.7.7
 
 // from jabsrv
+//DEPS com.fasterxml.jackson.core:jackson-annotations:2.20
 //DEPS com.github.ben-manes.caffeine:caffeine:3.2.3
 //DEPS com.google.guava:guava:33.5.0-jre
 //DEPS com.konghq:unirest-modules-gson:4.7.1
