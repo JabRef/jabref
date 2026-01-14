63bf74435c2cbe141c50ab2f19ed103237cca508
Oliver Kopp <kopp.dev@gmail.com>
2026-01-14 22:22:38 +0100

    Fix response

--- jabsrv/src/main/java/org/jabref/http/server/command/CommandResource.java
+++ jabsrv/src/main/java/org/jabref/http/server/command/CommandResource.java
@@ -4,7 +4,6 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.glassfish.hk2.api.ServiceLocator;
@@ -20,7 +19,6 @@ public class CommandResource {
 
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
     public Response dispatchCommand(String jsonCommand) {
         ObjectMapper objectMapper = new JsonMapper();
         try {
