ec52d49344926aa9d947200bcfc3ebdeb0fd2201
Oliver Kopp <kopp.dev@gmail.com>
2026-01-20 15:32:35 +0100

    Fix checkstyle

--- jablib/src/test/java/org/jabref/logic/importer/fetcher/ScopusTest.java
+++ jablib/src/test/java/org/jabref/logic/importer/fetcher/ScopusTest.java
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
