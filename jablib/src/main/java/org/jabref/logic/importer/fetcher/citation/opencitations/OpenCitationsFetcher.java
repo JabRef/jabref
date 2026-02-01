4e5f01ee3f80fec31812c88b990ea755bc10c257
Oliver Kopp <kopp.dev@gmail.com>
2026-02-01 13:08:43 +0100

    Begin of OpenCitationsFetcher

---
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/citation/opencitations/OpenCitationsFetcher.java
@@ -0,0 +1,13 @@
+package org.jabref.logic.importer.fetcher.citation.opencitations;
+
+import org.jabref.logic.importer.fetcher.citation.CitationFetcher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenCitationsFetcher implements CitationFetcher {
+
+    public static final String FETCHER_NAME = "OpenCitations";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OpenCitationsFetcher.class);
+}
