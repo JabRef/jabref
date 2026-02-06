6c76d073304eafc51fc22dc1492440ebbe67b025
BOgdAnSAM-sudo <samojlenkobogdandmitrovic@gmail.com>
2026-02-06 20:24:54 +0100

    make field FieldFormatterCleanupActions in CleanupSingleField always enabled

--- jabgui/src/main/java/org/jabref/gui/cleanup/CleanupSingleFieldViewModel.java
+++ jabgui/src/main/java/org/jabref/gui/cleanup/CleanupSingleFieldViewModel.java
@@ -1,8 +1,6 @@
 package org.jabref.gui.cleanup;
 
-import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 
@@ -10,15 +8,13 @@ import org.jabref.logic.cleanup.FieldFormatterCleanup;
 import org.jabref.logic.cleanup.FieldFormatterCleanupActions;
 
 public class CleanupSingleFieldViewModel {
-    public final BooleanProperty cleanupsEnabled = new SimpleBooleanProperty(true);
     public final ListProperty<FieldFormatterCleanup> cleanups = new SimpleListProperty<>(FXCollections.observableArrayList());
 
     public CleanupSingleFieldViewModel(FieldFormatterCleanupActions initialCleanups) {
-        cleanupsEnabled.set(initialCleanups.isEnabled());
         cleanups.setAll(initialCleanups.getConfiguredActions());
     }
 
     public FieldFormatterCleanupActions getSelectedFormatters() {
-        return new FieldFormatterCleanupActions(cleanupsEnabled.get(), cleanups.get());
+        return new FieldFormatterCleanupActions(true, cleanups.get());
     }
 }
