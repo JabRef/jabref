baa1ea6c1135a59c53f77aecdfdd2eee39ef5381
Oliver Kopp <kopp.dev@gmail.com>
2026-02-10 13:16:51 +0100

    Re-add rpm

--- build-logic/src/main/kotlin/org.jabref.gradle.base.targets.gradle.kts
+++ build-logic/src/main/kotlin/org.jabref.gradle.base.targets.gradle.kts
@@ -29,12 +29,12 @@ javaModulePackaging {
     target("ubuntu-22.04") {
         operatingSystem = OperatingSystemFamily.LINUX
         architecture = MachineArchitecture.X86_64
-        packageTypes = listOf("app-image", "deb")
+        packageTypes = listOf("app-image", "deb", "rpm")
     }
     target("ubuntu-22.04-arm") {
         operatingSystem = OperatingSystemFamily.LINUX
         architecture = MachineArchitecture.ARM64
-        packageTypes = listOf("app-image", "deb")
+        packageTypes = listOf("app-image", "deb", "rpm")
     }
     target("macos-15-intel") {
         operatingSystem = OperatingSystemFamily.MACOS
