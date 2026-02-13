3e6ee340d9263b84ff00157afcafc38e0f7ed950
Siedlerchr <siedlerkiller@gmail.com>
2026-02-13 10:54:05 +0100

    fix unused import

--- jablib/src/test/java/org/jabref/logic/externalfiles/LinkedFileTransferHelperTest.java
+++ jablib/src/test/java/org/jabref/logic/externalfiles/LinkedFileTransferHelperTest.java
@@ -1,7 +1,6 @@
 package org.jabref.logic.externalfiles;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
