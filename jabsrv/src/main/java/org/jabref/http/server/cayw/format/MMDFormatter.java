38abdcfb954e2eca6ac819b422e32dca87e335e3
Loay Elnoamani <loaytareq44@gmail.com>
2026-02-16 14:57:49 +0200

    enhance citation formatting

--- jabsrv/src/main/java/org/jabref/http/server/cayw/format/MMDFormatter.java
+++ jabsrv/src/main/java/org/jabref/http/server/cayw/format/MMDFormatter.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 import org.jabref.http.server.cayw.CAYWQueryParams;
 import org.jabref.http.server.cayw.gui.CAYWEntry;
-import org.jabref.model.entry.BibEntry;
+import org.jabref.http.server.cayw.gui.CitationProperties;
 
 import jakarta.ws.rs.core.MediaType;
 
@@ -19,13 +19,24 @@ public class MMDFormatter implements CAYWFormatter {
 
     @Override
     public String format(CAYWQueryParams queryParams, List<CAYWEntry> caywEntries) {
-        List<BibEntry> bibEntries = caywEntries.stream()
-                                               .map(CAYWEntry::bibEntry)
-                                               .toList();
+        return caywEntries.stream()
+                          .map(this::formatEntry)
+                          .flatMap(Optional::stream)
+                          .collect(Collectors.joining(""));
+    }
 
-        return bibEntries.stream()
-                         .map(entry -> entry.getCitationKey().map("[#%s][]"::formatted))
-                         .flatMap(Optional::stream)
-                         .collect(Collectors.joining(""));
+    private Optional<String> formatEntry(CAYWEntry entry) {
+        return entry.bibEntry().getCitationKey().map(key -> {
+            CitationProperties props = entry.citationProperties();
+
+            StringBuilder keyPart = new StringBuilder("#").append(key);
+            props.getFormattedLocator().ifPresent(l -> keyPart.append(", ").append(l));
+            props.getSuffix().ifPresent(s -> keyPart.append(", ").append(s));
+
+            if (props.getPrefix().isPresent()) {
+                return "[%s][%s]".formatted(props.getPrefix().get(), keyPart);
+            }
+            return "[%s][]".formatted(keyPart);
+        });
     }
 }
