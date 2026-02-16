e8404768c05b8bd2ff2c38e410f706dd7befcf9e
Oliver Kopp <kopp.dev@gmail.com>
2026-02-16 22:50:35 +0100

    Extract interface

---
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/citation/CitationCountFetcher.java
@@ -0,0 +1,14 @@
+package org.jabref.logic.importer.fetcher.citation;
+
+import java.util.Optional;
+
+import org.jabref.logic.importer.FetcherException;
+import org.jabref.model.entry.BibEntry;
+
+public interface CitationCountFetcher {
+    /// Get the paper details that includes citation count field for a given {@link BibEntry}.
+    ///
+    /// @param entry entry to search citation count field
+    /// @return returns a {@link Integer} for citation count field (may be empty)
+    Optional<Integer> getCitationCount(BibEntry entry) throws FetcherException;
+}
