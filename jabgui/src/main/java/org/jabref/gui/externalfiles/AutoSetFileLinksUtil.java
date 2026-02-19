1b62746dbf0713c03831ad65ee316eda6b76844c
Shubh Singhal <shubhsinghal@Shubhs-MacBook-Air.local>
2026-02-19 10:47:54 +0530

    Fix auto-link logic for moved linked files

--- jabgui/src/main/java/org/jabref/gui/externalfiles/AutoSetFileLinksUtil.java
+++ jabgui/src/main/java/org/jabref/gui/externalfiles/AutoSetFileLinksUtil.java
@@ -146,21 +146,21 @@ public class AutoSetFileLinksUtil {
         }
     }
 
-    private List<LinkedFile> autoLinkBrokenLinkedFiles(List<LinkedFile> linkedFiles,Map<String,LinkedFile> candidateFiles) {
-        List<LinkedFile> updated=new ArrayList<>();
-        for(LinkedFile linkedFile:linkedFiles) {
-            Path linkedPath=Path.of(linkedFile.getLink());
-            String brokenFileName=linkedPath.getFileName().toString();
-            boolean exists=false;
-            for(Path directory:directories) {
-                if(Files.exists(directory.resolve(linkedPath))) {
-                    exists=true;
+    private List<LinkedFile> autoLinkBrokenLinkedFiles(List<LinkedFile> linkedFiles, Map<String, LinkedFile> candidateFiles) {
+        List<LinkedFile> updated = new ArrayList<>();
+        for (LinkedFile linkedFile : linkedFiles) {
+            Path linkedPath = Path.of(linkedFile.getLink());
+            String brokenFileName = linkedPath.getFileName().toString();
+            boolean exists = false;
+            for (Path directory : directories) {
+                if (Files.exists(directory.resolve(linkedPath))) {
+                    exists = true;
                     break;
                 }
             }
-            if(!exists) {
-                LinkedFile replacement=candidateFiles.get(FileUtil.getBaseName(brokenFileName));
-                if(replacement!=null) {
+            if (!exists) {
+                LinkedFile replacement = candidateFiles.get(FileUtil.getBaseName(brokenFileName));
+                if (replacement != null) {
                     linkedFile.setLink(replacement.getLink());
                     linkedFile.setFileType(replacement.getFileType());
                     candidateFiles.remove(FileUtil.getBaseName(brokenFileName));
