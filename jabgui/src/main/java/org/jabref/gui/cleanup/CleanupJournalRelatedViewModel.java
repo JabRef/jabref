9afd4bfd223e70382bf53a190d1ec34024308c6c
BOgdAnSAM-sudo <samojlenkobogdandmitrovic@gmail.com>
2026-02-19 18:13:27 +0100

    refactor CleanupJournalRelatedPanel to use JournalAbbreviationPanel

--- jabgui/src/main/java/org/jabref/gui/cleanup/CleanupJournalRelatedViewModel.java
+++ jabgui/src/main/java/org/jabref/gui/cleanup/CleanupJournalRelatedViewModel.java
@@ -5,29 +5,25 @@ import java.util.EnumSet;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+import org.jabref.gui.commonfxcontrols.JournalAbbreviationPanel;
 import org.jabref.logic.cleanup.CleanupPreferences;
 
 public class CleanupJournalRelatedViewModel {
-    public static final EnumSet<CleanupPreferences.CleanupStep> CLEANUP_JOURNAL_METHODS = EnumSet.of(
-            CleanupPreferences.CleanupStep.ABBREVIATE_DEFAULT,
-            CleanupPreferences.CleanupStep.ABBREVIATE_DOTLESS,
-            CleanupPreferences.CleanupStep.ABBREVIATE_SHORTEST_UNIQUE,
-            CleanupPreferences.CleanupStep.ABBREVIATE_LTWA,
-            CleanupPreferences.CleanupStep.UNABBREVIATE
-    );
-
     public final ObjectProperty<CleanupPreferences.CleanupStep> selectedJournalCleanupOption = new SimpleObjectProperty<>();
+    public final EnumSet<CleanupPreferences.CleanupStep> allSupportedJobs;
     public final CleanupPreferences preferences;
 
     public CleanupJournalRelatedViewModel(CleanupPreferences preferences) {
         this.preferences = preferences;
+
+        allSupportedJobs = JournalAbbreviationPanel.getAllCleanupOptions();
         selectedJournalCleanupOption.set(getInitialMethod());
     }
 
     private CleanupPreferences.CleanupStep getInitialMethod() {
-        return CLEANUP_JOURNAL_METHODS.stream()
-                                      .filter(preferences::isActive)
-                                      .findFirst()
-                                      .orElse(CleanupPreferences.CleanupStep.ABBREVIATE_DEFAULT);
+        return allSupportedJobs.stream()
+                               .filter(preferences::isActive)
+                               .findFirst()
+                               .orElse(CleanupPreferences.CleanupStep.ABBREVIATE_DEFAULT);
     }
 }
