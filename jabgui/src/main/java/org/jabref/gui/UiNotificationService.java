bdf31880b9834127d98e72a95d5d8815c8fc6d3d
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2026-02-26 17:43:17 +0100

    Convert to markdown comments

--- jabgui/src/main/java/org/jabref/gui/UiNotificationService.java
+++ jabgui/src/main/java/org/jabref/gui/UiNotificationService.java
@@ -6,10 +6,8 @@ import com.dlsc.gemsfx.infocenter.Notification;
 
 public interface UiNotificationService extends NotificationService {
 
-    /**
-     * Notify the user in a non-blocking way (e.g. a toast).
-     *
-     * @param notification the message to show.
-     */
+    /// Notify the user in a non-blocking way (e.g. a toast).
+    ///
+    /// @param notification the message to show.
     void notify(Notification<Object> notification);
 }
