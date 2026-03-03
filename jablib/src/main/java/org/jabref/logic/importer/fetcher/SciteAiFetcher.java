033d0f0b49824095388ed1ded452fd3be5176081
aliyasirnac <aliyasirnac@gmail.com>
2026-03-03 18:32:14 +0300

    Add tests for SciteAiFetcher citation count functionality

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/SciteAiFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/SciteAiFetcher.java
@@ -14,6 +14,7 @@ import org.jabref.model.entry.identifier.DOI;
 import org.jabref.model.sciteTallies.TalliesResponse;
 
 import kong.unirest.core.json.JSONObject;
+import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
