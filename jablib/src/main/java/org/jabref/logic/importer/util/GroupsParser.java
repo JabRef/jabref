f4be20623f628298fefbaef3e4ab2b6b5c640fd3
StardustEnigma <atharvamandle19@gmail.com>
2026-03-06 13:26:21 +0000

    Revert "Apply formatting"
    This reverts commit 978528d36a2359817730221f050b0068f87b5304.

--- jablib/src/main/java/org/jabref/logic/importer/util/GroupsParser.java
+++ jablib/src/main/java/org/jabref/logic/importer/util/GroupsParser.java
@@ -44,20 +44,14 @@ public class GroupsParser {
     /// Identifier for SmartGroup (deprecated, replaced by {@link ExplicitGroup}).
     ///
     /// @deprecated Kept for backward compatibility during migration.
-    @Deprecated
-    private static final String SMART_GROUP_ID_FOR_MIGRATION = "SmartGroup:";
+    @Deprecated private static final String SMART_GROUP_ID_FOR_MIGRATION = "SmartGroup:";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GroupsParser.class);
 
     private GroupsParser() {
     }
 
-    public static GroupTreeNode importGroups(List<String> orderedData,
-                                             Character keywordSeparator,
-                                             FileUpdateMonitor fileMonitor,
-                                             MetaData metaData,
-                                             String userAndHost)
-            throws ParseException {
+    public static GroupTreeNode importGroups(List<String> orderedData, Character keywordSeparator, FileUpdateMonitor fileMonitor, MetaData metaData, String userAndHost) throws ParseException {
         try {
             GroupTreeNode cursor = null;
             GroupTreeNode root = null;
@@ -74,10 +68,12 @@ public class GroupsParser {
                 }
                 int level = Integer.parseInt(string.substring(0, spaceIndex));
                 AbstractGroup group = GroupsParser.fromString(string.substring(spaceIndex + 1), keywordSeparator, fileMonitor, metaData, userAndHost);
+
                 if (group == null) {
                     LOGGER.warn("Skipping unknown group in metadata: {}", string);
                     continue;
                 }
+
                 GroupTreeNode newNode = GroupTreeNode.fromGroup(group);
                 if (cursor == null) {
                     // create new root
@@ -97,9 +93,7 @@ public class GroupsParser {
             }
             return root;
         } catch (ParseException e) {
-            throw new ParseException(Localization
-                    .lang("Group tree could not be parsed. If you save the BibTeX library, all groups will be lost."),
-                    e);
+            throw new ParseException(Localization.lang("Group tree could not be parsed. If you save the BibTeX library, all groups will be lost."), e);
         }
     }
 
@@ -109,8 +103,7 @@ public class GroupsParser {
     /// @return New instance of the encoded group.
     /// @throws ParseException If an error occurred and a group could not be created, e.g. due to a malformed regular expression.
     @Nullable
-    public static AbstractGroup fromString(String input, Character keywordSeparator, FileUpdateMonitor fileMonitor, MetaData metaData, String userAndHost)
-            throws ParseException {
+    public static AbstractGroup fromString(String input, Character keywordSeparator, FileUpdateMonitor fileMonitor, MetaData metaData, String userAndHost) throws ParseException {
         if (input.startsWith(MetadataSerializationConfiguration.KEYWORD_GROUP_ID)) {
             return keywordGroupFromString(input, keywordSeparator);
         }
@@ -145,15 +138,14 @@ public class GroupsParser {
         if (input.startsWith(MetadataSerializationConfiguration.TEX_GROUP_ID)) {
             return texGroupFromString(input, fileMonitor, metaData, userAndHost);
         }
-
+        
         return null;
     }
 
     private static AbstractGroup texGroupFromString(String input, FileUpdateMonitor fileMonitor, MetaData metaData, String userAndHost) throws ParseException {
         assert input.startsWith(MetadataSerializationConfiguration.TEX_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.TEX_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.TEX_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -179,8 +171,7 @@ public class GroupsParser {
     private static AbstractGroup automaticPersonsGroupFromString(String input) {
         assert input.startsWith(MetadataSerializationConfiguration.AUTOMATIC_PERSONS_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_PERSONS_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_PERSONS_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -193,8 +184,7 @@ public class GroupsParser {
     private static AbstractGroup automaticDateGroupFromString(String input) {
         assert input.startsWith(MetadataSerializationConfiguration.AUTOMATIC_DATE_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_DATE_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_DATE_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -209,8 +199,7 @@ public class GroupsParser {
     private static AbstractGroup automaticEntryTypeGroupFromString(String input) {
         assert input.startsWith(MetadataSerializationConfiguration.AUTOMATIC_ENTRY_TYPE_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_ENTRY_TYPE_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_ENTRY_TYPE_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -222,8 +211,7 @@ public class GroupsParser {
     private static AbstractGroup automaticKeywordGroupFromString(String input) {
         assert input.startsWith(MetadataSerializationConfiguration.AUTOMATIC_KEYWORD_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_KEYWORD_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.AUTOMATIC_KEYWORD_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -241,8 +229,7 @@ public class GroupsParser {
     private static KeywordGroup keywordGroupFromString(String input, Character keywordSeparator) {
         assert input.startsWith(MetadataSerializationConfiguration.KEYWORD_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.KEYWORD_GROUP_ID
-                .length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.KEYWORD_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         GroupHierarchyType context = GroupHierarchyType.getByNumberOrDefault(Integer.parseInt(token.nextToken()));
@@ -272,8 +259,7 @@ public class GroupsParser {
     private static ExplicitGroup explicitGroupFromString(String input, Character keywordSeparator) throws ParseException {
         assert input.startsWith(MetadataSerializationConfiguration.EXPLICIT_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.EXPLICIT_GROUP_ID.length()),
-                MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.EXPLICIT_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         try {
@@ -289,8 +275,7 @@ public class GroupsParser {
     private static ExplicitGroup legacyExplicitGroupFromString(String input, Character keywordSeparator) throws ParseException {
         assert input.startsWith(MetadataSerializationConfiguration.LEGACY_EXPLICIT_GROUP_ID);
 
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.LEGACY_EXPLICIT_GROUP_ID.length()),
-                MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.LEGACY_EXPLICIT_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         try {
@@ -325,8 +310,7 @@ public class GroupsParser {
     /// @param input The String representation obtained from SearchGroup.toString(), or null if incompatible
     private static AbstractGroup searchGroupFromString(String input) {
         assert input.startsWith(MetadataSerializationConfiguration.SEARCH_GROUP_ID);
-        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.SEARCH_GROUP_ID.length()),
-                MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
+        QuotedStringTokenizer token = new QuotedStringTokenizer(input.substring(MetadataSerializationConfiguration.SEARCH_GROUP_ID.length()), MetadataSerializationConfiguration.GROUP_UNIT_SEPARATOR, MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
 
         String name = StringUtil.unquote(token.nextToken(), MetadataSerializationConfiguration.GROUP_QUOTE_CHAR);
         int context = Integer.parseInt(token.nextToken());
@@ -340,9 +324,7 @@ public class GroupsParser {
         }
         // version 0 contained 4 additional booleans to specify search
         // fields; these are ignored now, all fields are always searched
-        SearchGroup searchGroup = new SearchGroup(name,
-                GroupHierarchyType.getByNumberOrDefault(context), expression, searchFlags
-        );
+        SearchGroup searchGroup = new SearchGroup(name, GroupHierarchyType.getByNumberOrDefault(context), expression, searchFlags);
         addGroupDetails(token, searchGroup);
         return searchGroup;
     }
