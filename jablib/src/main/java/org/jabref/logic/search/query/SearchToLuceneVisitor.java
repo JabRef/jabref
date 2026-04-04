192394870c87b8e614b9fb8b4028050b200f7e4c
Jasper McLennan <jaspermclennan@uvic.ca>
2026-04-03 19:23:56 -0700

    more formatting

--- jablib/src/main/java/org/jabref/logic/search/query/SearchToLuceneVisitor.java
+++ jablib/src/main/java/org/jabref/logic/search/query/SearchToLuceneVisitor.java
@@ -94,10 +94,14 @@ public class SearchToLuceneVisitor extends SearchBaseVisitor<String> {
     private String buildFieldExpression(String field, String term, int operator, boolean isQuoted) {
 
         // --- ADDED FOR #14249: DATE/RANGE SUPPORT ---
-        if (operator == SearchParser.GEQUAL) return field + "[" + term + " TO *]";
-        if (operator == SearchParser.LEQUAL) return field + "[* TO " + term + "]";
-        if (operator == SearchParser.GT) return field + "{" + term + " TO *}";
-        if (operator == SearchParser.LT) return field + "{* TO " + term + "}";
+        if (operator == SearchParser.GEQUAL)
+            return field + "[" + term + " TO *]";
+        if (operator == SearchParser.LEQUAL)
+            return field + "[* TO " + term + "]";
+        if (operator == SearchParser.GT)
+            return field + "{" + term + " TO *}";
+        if (operator == SearchParser.LT)
+            return field + "{* TO " + term + "}";
         // ----------------------------------------------
         boolean isRegexOp = isRegexOperator(operator);
         boolean isNegationOp = isNegationOperator(operator);
