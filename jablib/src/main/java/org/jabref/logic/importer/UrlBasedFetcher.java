16f3673390ee913210501630ed6538367dc40290
sudeep <kulkarnisudeep2005@gmail.com>
2026-04-06 17:06:00 +0530

    Delete jablib/src/main/java/org/jabref/logic/importer/UrlBasedFetcher.java

--- jablib/src/main/java/org/jabref/logic/importer/UrlBasedFetcher.java
+++
@@ -1,9 +0,0 @@
-package org.jabref.logic.importer;
-
-import java.util.List;
-
-import org.jabref.model.entry.BibEntry;
-
-public interface UrlBasedFetcher extends WebFetcher {
-    List<BibEntry> performSearch(String url) throws FetcherException;
-}
