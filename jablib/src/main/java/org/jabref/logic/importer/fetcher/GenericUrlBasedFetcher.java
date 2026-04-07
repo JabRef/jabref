0dd0056e42ae28a224ead68d8e5d7ad77e496228
sudeep <kulkarnisudeep2005@gmail.com>
2026-04-07 09:14:01 +0530

    Update GenericUrlBasedFetcher.java

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/GenericUrlBasedFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/GenericUrlBasedFetcher.java
@@ -3,7 +3,6 @@ package org.jabref.logic.importer.fetcher;
 import java.util.List;
 
 import org.jabref.logic.importer.FetcherException;
-import org.jabref.logic.importer.fetcher.WebFetcher;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.types.StandardEntryType;
