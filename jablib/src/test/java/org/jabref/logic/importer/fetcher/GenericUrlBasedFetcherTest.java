cc86e6d9a4564bc9316e0bb4246844cd509d1802
sudeep <kulkarnisudeep2005@gmail.com>
2026-04-07 09:50:03 +0530

    Update GenericUrlBasedFetcherTest.java

--- jablib/src/test/java/org/jabref/logic/importer/fetcher/GenericUrlBasedFetcherTest.java
+++ jablib/src/test/java/org/jabref/logic/importer/fetcher/GenericUrlBasedFetcherTest.java
@@ -10,7 +10,8 @@ import org.jabref.model.entry.types.StandardEntryType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GenericUrlBasedFetcherTest {
 
