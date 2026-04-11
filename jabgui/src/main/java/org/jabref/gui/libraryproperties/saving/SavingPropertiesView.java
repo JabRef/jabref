d3cb9a53d19ee5850177dbf28f0065df3d8a05be
Faneesh Juneja <willowstration@gmail.com>
2026-04-11 15:13:03 -0700

    add journal abbreviation preference to library properties

--- jabgui/src/main/java/org/jabref/gui/libraryproperties/saving/SavingPropertiesView.java
+++ jabgui/src/main/java/org/jabref/gui/libraryproperties/saving/SavingPropertiesView.java
@@ -1,12 +1,16 @@
 package org.jabref.gui.libraryproperties.saving;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.util.StringConverter;
 
 import org.jabref.gui.commonfxcontrols.FieldFormatterCleanupsPanel;
 import org.jabref.gui.commonfxcontrols.SaveOrderConfigPanel;
 import org.jabref.gui.libraryproperties.AbstractPropertiesTabView;
 import org.jabref.gui.libraryproperties.PropertiesTab;
+import org.jabref.logic.journals.AbbreviationType;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.preferences.CliPreferences;
 import org.jabref.model.database.BibDatabaseContext;
@@ -19,6 +23,7 @@ public class SavingPropertiesView extends AbstractPropertiesTabView<SavingProper
     @FXML private CheckBox protect;
     @FXML private SaveOrderConfigPanel saveOrderConfigPanel;
     @FXML private FieldFormatterCleanupsPanel fieldFormatterCleanupsPanel;
+    @FXML private ComboBox<AbbreviationType> journalAbbreviationOnSave;
 
     @Inject private CliPreferences preferences;
 
@@ -49,5 +54,33 @@ public class SavingPropertiesView extends AbstractPropertiesTabView<SavingProper
 
         fieldFormatterCleanupsPanel.cleanupsDisableProperty().bindBidirectional(viewModel.cleanupsDisableProperty());
         fieldFormatterCleanupsPanel.cleanupsProperty().bindBidirectional(viewModel.cleanupsProperty());
+
+        journalAbbreviationOnSave.setItems(FXCollections.observableArrayList(
+                null, AbbreviationType.DEFAULT, AbbreviationType.DOTLESS,
+                AbbreviationType.SHORTEST_UNIQUE, AbbreviationType.LTWA));
+        journalAbbreviationOnSave.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(AbbreviationType type) {
+                if (type == null) {
+                    return Localization.lang("None (use global setting)");
+                }
+                return switch (type) {
+                    case DEFAULT ->
+                            Localization.lang("Abbreviate (default)");
+                    case DOTLESS ->
+                            Localization.lang("Abbreviate (dotless)");
+                    case SHORTEST_UNIQUE ->
+                            Localization.lang("Abbreviate (shortest unique)");
+                    case LTWA ->
+                            Localization.lang("Abbreviate (LTWA)");
+                };
+            }
+
+            @Override
+            public AbbreviationType fromString(String string) {
+                return null;
+            }
+        });
+        journalAbbreviationOnSave.valueProperty().bindBidirectional(viewModel.journalAbbreviationOnSaveProperty());
     }
 }
