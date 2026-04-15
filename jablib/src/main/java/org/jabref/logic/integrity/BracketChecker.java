df06692c79ddf487e057d3778d461383bba315c8
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2026-04-15 17:42:39 +0200

    Simplify BracketChecker

--- jablib/src/main/java/org/jabref/logic/integrity/BracketChecker.java
+++ jablib/src/main/java/org/jabref/logic/integrity/BracketChecker.java
@@ -1,34 +1,17 @@
 package org.jabref.logic.integrity;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.jabref.logic.l10n.Localization;
-import org.jabref.logic.util.strings.StringUtil;
+import org.jabref.logic.bibtex.FieldWriter;
 
 public class BracketChecker implements ValueChecker {
 
     @Override
     public Optional<String> checkValue(String value) {
-        if (StringUtil.isBlank(value)) {
-            return Optional.empty();
-        }
-
-        // metaphor: integer-based stack (push + / pop -)
-        int counter = 0;
-        for (char a : value.trim().toCharArray()) {
-            if (a == '{') {
-                counter++;
-            } else if (a == '}') {
-                if (counter == 0) {
-                    return Optional.of(Localization.lang("unexpected closing curly bracket"));
-                } else {
-                    counter--;
-                }
-            }
-        }
-
-        if (counter > 0) {
-            return Optional.of(Localization.lang("unexpected opening curly bracket"));
+        List<String> errors = FieldWriter.checkBalancedBraces(value);
+        if (!errors.isEmpty()) {
+            return Optional.of(String.join("\n", errors));
         }
 
         return Optional.empty();
