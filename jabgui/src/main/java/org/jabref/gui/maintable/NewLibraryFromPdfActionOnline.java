e24799655595b3c891508234a192fd86a971dc60
Faneesh Juneja <willowstration@gmail.com>
2026-04-18 16:24:56 -0700

    Merge fix-12700 with origin

--- jabgui/src/main/java/org/jabref/gui/maintable/NewLibraryFromPdfActionOnline.java
+++ jabgui/src/main/java/org/jabref/gui/maintable/NewLibraryFromPdfActionOnline.java
@@ -15,6 +15,7 @@ import org.jabref.logic.preferences.CliPreferences;
 import org.jabref.logic.util.TaskExecutor;
 import org.jabref.model.entry.BibEntry;
 
+import jakarta.ws.rs.HEAD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,13 +23,19 @@ public class NewLibraryFromPdfActionOnline extends NewLibraryFromPdfAction {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NewLibraryFromPdfActionOnline.class);
 
+<<<<<<< HEAD
     public NewLibraryFromPdfActionOnline(LibraryTabContainer libraryTabContainer, StateManager stateManager, DialogService dialogService, CliPreferences preferences, TaskExecutor taskExecutor, AiService aiService) {
         super(libraryTabContainer, stateManager, dialogService, preferences, taskExecutor, aiService);
+=======
+    public NewLibraryFromPdfActionOnline(LibraryTabContainer libraryTabContainer, StateManager stateManager, DialogService dialogService, CliPreferences preferences, TaskExecutor taskExecutor) {
+        super(libraryTabContainer, stateManager, dialogService, preferences, taskExecutor);
+>>>>>>> f362373cef372f3280cb57aa433998bc17fc53bf
     }
 
     @Override
     protected Callable<ParserResult> getParserResultCallable(Path path) {
         return () -> {
+<<<<<<<HEAD
             List<BibEntry> entries;
             try {
                 entries = new GrobidService(this.preferences.getGrobidPreferences())
@@ -40,8 +47,14 @@ public class NewLibraryFromPdfActionOnline extends NewLibraryFromPdfAction {
             if (entries.isEmpty()) {
                 entries = CitationsFromPdf.extractCitationsUsingLLM(
                         this.aiService, this.preferences.getImportFormatPreferences(), path).getDatabase().getEntries();
+=======
+            List<BibEntry> entries = new GrobidService(this.preferences.getGrobidPreferences()).processReferences(path, preferences.getImportFormatPreferences());
+            if (entries.isEmpty()) {
+                entries = CitationsFromPdf.extractCitationsUsingLLM(this.preferences, LOGGER::info, path).getDatabase().getEntries();
+>>>>>>> f362373cef372f3280cb57aa433998bc17fc53bf
             }
             return new ParserResult(entries);
         };
     }
 }
+
