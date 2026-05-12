349cee59b9225463c1a213598484f31ebbd7a261
Zeyad Abdelfattah <ziadabdelfattah68@gmail.com>
2026-05-12 10:33:02 +0300

    Reformated the code to meet JabRef's code guidelines

--- jablib/src/main/java/org/jabref/logic/ocr/OcrFailureReason.java
+++ jablib/src/main/java/org/jabref/logic/ocr/OcrFailureReason.java
@@ -2,9 +2,5 @@ package org.jabref.logic.ocr;
 
 /// Enums for the reasons that can lead the OCR process to fail.
 public enum OcrFailureReason {
-    NOT_AVAILABLE,
-    TIMEOUT,
-    NON_ZERO_EXIT,
-    IO_ERROR,
-    INTERRUPTED
+    NOT_AVAILABLE, TIMEOUT, NON_ZERO_EXIT, IO_ERROR, INTERRUPTED
 }
