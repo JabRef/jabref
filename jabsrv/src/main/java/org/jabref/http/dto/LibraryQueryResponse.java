4df2e6e19e3fc615dffc0e0ece828038ea25a747
Oliver Kopp <kopp.dev@gmail.com>
2026-05-18 02:02:07 +0200

    Remove "query" sub resource

--- jabsrv/src/main/java/org/jabref/http/dto/LibraryQueryResponse.java
+++
@@ -1,9 +0,0 @@
-package org.jabref.http.dto;
-
-import java.util.List;
-
-import org.jspecify.annotations.NullMarked;
-
-@NullMarked
-public record LibraryQueryResponse(List<LibraryQueryMatch> matches) {
-}
