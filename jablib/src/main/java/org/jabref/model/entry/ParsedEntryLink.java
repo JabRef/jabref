534abfc2b088b6e9d6f6e9e79d66050e3a39f207
InAnYan <ruslanpopov1512@gmail.com>
2026-05-20 21:26:25 +0200

    fix(null): fix nullable in parsed entry link

--- jablib/src/main/java/org/jabref/model/entry/ParsedEntryLink.java
+++ jablib/src/main/java/org/jabref/model/entry/ParsedEntryLink.java
@@ -9,19 +9,15 @@ import org.jspecify.annotations.Nullable;
 
 public class ParsedEntryLink {
 
+    private final BibDatabase database;
+    
     private String key;
     @Nullable private BibEntry linkedEntry;
-    private BibDatabase database;
 
     public ParsedEntryLink(String key, BibDatabase database) {
         this.key = key;
         this.linkedEntry = database.getEntryByCitationKey(this.key).orElse(null);
         this.database = database;
-    }
-
-    public ParsedEntryLink(BibEntry bibEntry) {
-        this.key = bibEntry.getCitationKey().orElse("");
-        this.linkedEntry = bibEntry;
     }
 
     public String getKey() {
