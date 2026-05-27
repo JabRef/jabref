b99565d1edddcead6c641f9a3a9e17e429b204e5
Siedlerchr <siedlerkiller@gmail.com>
2026-05-27 21:07:57 +0200

    add dependency on unitfx

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies.constraints {
     api("cc.jilt:jilt:1.9.1")
 
     api("com.dlsc.gemsfx:gemsfx:4.1.0")
+    api("com.dlsc.unitfx:unitfx:1.0.10")
     api("com.dlsc.pdfviewfx:pdfviewfx:3.4.2")
     api("com.ibm.icu:icu4j:72.0.1!!")
     api("com.fasterxml:aalto-xml:1.4.0")
