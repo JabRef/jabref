bf9169d7ebee99e117696521ab7959762a51b60e
Siedlerchr <siedlerkiller@gmail.com>
2026-05-31 20:01:35 +0200

    Add user dialog

--- jabsrv/src/test/java/org/jabref/http/server/cayw/CAYWResourceTest.java
+++ jabsrv/src/test/java/org/jabref/http/server/cayw/CAYWResourceTest.java
@@ -5,22 +5,32 @@ import javafx.collections.FXCollections;
 import org.jabref.http.SrvStateManager;
 import org.jabref.http.server.ServerTest;
 import org.jabref.http.server.TestBibFile;
+import org.jabref.http.server.services.FilesToServe;
 import org.jabref.model.entry.BibEntry;
 
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Response;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CAYWResourceTest extends ServerTest {
+    private final FilesToServe filesToServe = new FilesToServe();
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        filesToServe.setFilesToServe(FXCollections.observableArrayList(TestBibFile.GENERAL_SERVER_TEST.path));
+    }
+
     @Override
     protected Application configure() {
         ResourceConfig resourceConfig = new ResourceConfig(CAYWResource.class);
-        addFilesToServeToResourceConfig(resourceConfig);
         addPreferencesToResourceConfig(resourceConfig);
         addGsonToResourceConfig(resourceConfig);
         addFormatterServiceToResourceConfig(resourceConfig);
@@ -28,9 +38,11 @@ class CAYWResourceTest extends ServerTest {
         resourceConfig.register(new AbstractBinder() {
             @Override
             protected void configure() {
+                bind(filesToServe).to(FilesToServe.class);
                 SrvStateManager mockSrv = Mockito.mock(SrvStateManager.class);
                 BibEntry bibEntry = new BibEntry().withCitationKey("Author2023test");
                 Mockito.when(mockSrv.getSelectedEntries()).thenReturn(FXCollections.observableArrayList(bibEntry));
+                Mockito.when(mockSrv.getOpenDatabases()).thenReturn(FXCollections.observableArrayList());
                 bind(mockSrv).to(SrvStateManager.class);
             }
         });
@@ -93,6 +105,20 @@ class CAYWResourceTest extends ServerTest {
 
     @Test
     void unknownLibraryPathReturnsBadRequest() {
+        Response response = target("/better-bibtex/cayw")
+                .queryParam("format", "biblatex")
+                .queryParam("selected", "1")
+                .queryParam("librarypath", "/tmp/not-served-library.bib")
+                .request()
+                .get();
+
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void nullFilesToServeListDoesNotCrash() {
+        filesToServe.setFilesToServe(null);
+
         Response response = target("/better-bibtex/cayw")
                 .queryParam("format", "biblatex")
                 .queryParam("selected", "1")
