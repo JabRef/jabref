9702aabbe9de98a4064a7361e30237e855ce960d
InAnYan <ruslanpopov1512@gmail.com>
2026-06-08 22:01:01 +0200

    refactor(null-away): fix BibWarning

--- jablib/src/main/java/org/jabref/model/biblog/BibWarning.java
+++ jablib/src/main/java/org/jabref/model/biblog/BibWarning.java
@@ -2,12 +2,14 @@ package org.jabref.model.biblog;
 
 import java.util.Optional;
 
+import org.jspecify.annotations.Nullable;
+
 /// Represents a warning message parsed from a BibTeX .blg file.
 /// e.g. [SeverityType] -- [Message] in [EntryKey]
 public record BibWarning(
         SeverityType severityType,
         String message,
-        String fieldName,
+        @Nullable String fieldName,
         String entryKey) {
     public Optional<String> getFieldName() {
         return Optional.ofNullable(fieldName);
