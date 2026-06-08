55a06cd4633a64b2687a410a9e8a08993e72bba9
InAnYan <ruslanpopov1512@gmail.com>
2026-06-08 21:35:10 +0200

    refactor(null-away): clean-up KeyCollisionException (because no one uses the fields)

--- jablib/src/main/java/org/jabref/model/database/KeyCollisionException.java
+++ jablib/src/main/java/org/jabref/model/database/KeyCollisionException.java
@@ -1,27 +1,11 @@
 package org.jabref.model.database;
 
 public class KeyCollisionException extends RuntimeException {
-
-    private String id;
-
     public KeyCollisionException() {
         super();
     }
 
-    public KeyCollisionException(String msg, String id) {
+    public KeyCollisionException(String msg) {
         super(msg);
-        this.id = id;
-    }
-
-    public KeyCollisionException(String msg, Throwable exception) {
-        super(msg, exception);
-    }
-
-    public KeyCollisionException(Throwable exception) {
-        super(exception);
-    }
-
-    public String getId() {
-        return id;
     }
 }
