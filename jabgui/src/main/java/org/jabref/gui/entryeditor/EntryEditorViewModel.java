8458d06b10ed04ee98ec8ef1b0d22d2002012047
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2026-06-15 17:57:09 +0200

    Introduce EntryEditorViewModel

---
+++ jabgui/src/main/java/org/jabref/gui/entryeditor/EntryEditorViewModel.java
@@ -0,0 +1,36 @@
+package org.jabref.gui.entryeditor;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import org.jabref.gui.AbstractViewModel;
+import org.jabref.gui.StateManager;
+import org.jabref.model.entry.BibEntry;
+
+import org.jspecify.annotations.Nullable;
+
+public class EntryEditorViewModel extends AbstractViewModel {
+
+    private final ObjectProperty<@Nullable BibEntry> currentlyEditedEntry = new SimpleObjectProperty<>();
+    private final StateManager stateManager;
+
+    public EntryEditorViewModel(StateManager stateManager) {
+        this.stateManager = stateManager;
+
+        // [impl->req~entry-editor.keep-showing~1] — when selection becomes empty, keep the old entry showing
+        stateManager.getSelectedEntries().addListener((InvalidationListener) _ -> {
+            if (!stateManager.getSelectedEntries().isEmpty()) {
+                currentlyEditedEntry.set(stateManager.getSelectedEntries().getFirst());
+            }
+        });
+    }
+
+    public ObjectProperty<@Nullable BibEntry> currentlyEditedEntryProperty() {
+        return currentlyEditedEntry;
+    }
+
+    public @Nullable BibEntry getCurrentlyEditedEntry() {
+        return currentlyEditedEntry.get();
+    }
+}
