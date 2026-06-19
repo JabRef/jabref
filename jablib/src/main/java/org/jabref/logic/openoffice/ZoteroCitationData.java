9d77273882e306ba39eed7a1bb07b991a9b34395
Dark Blue <h.zhang6@student.tue.nl>
2026-06-20 00:46:10 +0200

    Update entry type/field mapping (#15994)
    * Update entry type mapping
    
    * Update test
    
    * Checkstyle
    
    * Refactor mapping
    
    * Refactor mapping
    
    * Remove redundant code
    
    * Fix Qodo

--- jablib/src/main/java/org/jabref/logic/openoffice/ZoteroCitationData.java
+++ jablib/src/main/java/org/jabref/logic/openoffice/ZoteroCitationData.java
@@ -16,6 +16,15 @@ class ZoteroCitationData {
     public static class ItemData {
         String type = "";
 
+        @SerializedName("abstract")
+        String abstractText = "";
+
+        @SerializedName("call-number")
+        String callNumber = "";
+
+        @SerializedName("chapter-number")
+        String chapterNumber = "";
+
         @SerializedName("container-title")
         String containerTitle = "";
 
@@ -30,6 +39,9 @@ class ZoteroCitationData {
 
         String edition = "";
 
+        @SerializedName("event-place")
+        String eventPlace = "";
+
         @SerializedName("ISBN")
         String isbn = "";
 
@@ -38,21 +50,41 @@ class ZoteroCitationData {
 
         String issue = "";
 
+        String keyword = "";
+
+        String language = "";
+
         String number = "";
+
+        @SerializedName("number-of-pages")
+        String numberOfPages = "";
+
+        @SerializedName("number-of-volumes")
+        String numberOfVolumes = "";
 
         String page = "";
 
         String publisher = "";
 
-        @SerializedName(value = "publisher-place", alternate = "event-place")
-        String location = "";
+        @SerializedName("publisher-place")
+        String publisherPlace = "";
+
+        String status = "";
 
         String title = "";
 
+        @SerializedName(value = "title-short", alternate = "shortTitle")
+        String shortTitle = "";
+
         String volume = "";
+
+        @SerializedName("volume-title")
+        String volumeTitle = "";
 
         @SerializedName("URL")
         String url = "";
+
+        String version = "";
 
         String note = "";
 
@@ -64,6 +96,12 @@ class ZoteroCitationData {
 
         String getFieldValue(String cslField) {
             return switch (cslField) {
+                case "abstract" ->
+                        abstractText;
+                case "call-number" ->
+                        callNumber;
+                case "chapter-number" ->
+                        chapterNumber;
                 case "collection-title" ->
                         collectionTitle;
                 case "container-title" ->
@@ -72,9 +110,10 @@ class ZoteroCitationData {
                         doi;
                 case "edition" ->
                         edition;
-                case "event-place",
-                     "publisher-place" ->
-                        location;
+                case "event-place" ->
+                        eventPlace;
+                case "publisher-place" ->
+                        publisherPlace;
                 case "collection-number" ->
                         collectionNumber;
                 case "ISBN" ->
@@ -83,18 +122,35 @@ class ZoteroCitationData {
                         issn;
                 case "issue" ->
                         issue;
+                case "keyword" ->
+                        keyword;
+                case "language" ->
+                        language;
                 case "number" ->
                         number;
+                case "number-of-pages" ->
+                        numberOfPages;
+                case "number-of-volumes" ->
+                        numberOfVolumes;
                 case "page" ->
                         page;
                 case "publisher" ->
                         publisher;
+                case "status" ->
+                        status;
+                case "title-short",
+                     "shortTitle" ->
+                        shortTitle;
                 case "title" ->
                         title;
                 case "URL" ->
                         url;
                 case "volume" ->
                         volume;
+                case "volume-title" ->
+                        volumeTitle;
+                case "version" ->
+                        version;
                 case "note" ->
                         note;
                 case "section" ->
