09e42cac3c0ec104dba427b0214a71d3c9e57548
Zeyad Abdelfattah <ziadabdelfattah68@gmail.com>
2026-06-20 16:05:30 +0300

    Add browsing and auto detection of path functionalities

--- jabgui/src/main/java/org/jabref/gui/preferences/ocr/OcrTab.java
+++ jabgui/src/main/java/org/jabref/gui/preferences/ocr/OcrTab.java
@@ -1,12 +1,17 @@
 package org.jabref.gui.preferences.ocr;
 
+import java.nio.file.Path;
+import java.util.Optional;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import org.jabref.gui.preferences.AbstractPreferenceTabView;
 import org.jabref.gui.preferences.PreferencesTab;
+import org.jabref.gui.util.FileDialogConfiguration;
 import org.jabref.logic.l10n.Localization;
+import org.jabref.logic.util.BackgroundTask;
 
 import com.airhacks.afterburner.views.ViewLoader;
 
@@ -30,5 +35,32 @@ public class OcrTab extends AbstractPreferenceTabView<OcrTabViewModel> implement
         this.viewModel = new OcrTabViewModel(preferences.getOcrPreferences());
 
         ocrPath.textProperty().bindBidirectional(viewModel.ocrPathProperty());
+    }
+
+    @FXML
+    private void browseEnginePath() {
+        Optional<Path> selectedPath = dialogService.showFileOpenDialog(
+                new FileDialogConfiguration.Builder()
+                        .withInitialDirectory(preferences.getFilePreferences().getWorkingDirectory())
+                        .build());
+        selectedPath.ifPresent(path -> viewModel.ocrPathProperty().set(path.toString()));
+    }
+
+    @FXML
+    private void autoDetectEnginePath() {
+        BackgroundTask<Optional<String>> autoDetectTask = BackgroundTask.wrap(() -> viewModel.autoDetectEnginePath());
+
+        autoDetectTask.titleProperty().set(Localization.lang("Auto detection of engine path"));
+        autoDetectTask.showToUser(true);
+        autoDetectTask.onSuccess(result -> {
+            if (result.isPresent()) {
+                viewModel.ocrPathProperty().set(result.get());
+                dialogService.notify(Localization.lang("OCRmyPDF detected at: %0", result.get()));
+            } else {
+                dialogService.notify(Localization.lang("OCRmyPDF could not be detected automatically"));
+            }
+        });
+        autoDetectTask.onFailure(_ -> dialogService.notify(Localization.lang("Auto detect engine path failed")));
+        taskExecutor.execute(autoDetectTask);
     }
 }
--- jabgui/src/main/java/org/jabref/gui/preferences/ocr/OcrTabViewModel.java
+++ jabgui/src/main/java/org/jabref/gui/preferences/ocr/OcrTabViewModel.java
@@ -1,12 +1,23 @@
 package org.jabref.gui.preferences.ocr;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 import org.jabref.gui.preferences.PreferenceTabViewModel;
 import org.jabref.logic.ocr.OcrPreferences;
+import org.jabref.logic.util.HeadlessExecutorService;
+import org.jabref.logic.util.StreamGobbler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OcrTabViewModel implements PreferenceTabViewModel {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OcrTabViewModel.class);
+    private static final int CHECKING_TIMEOUT = 10;
     private final StringProperty ocrPath = new SimpleStringProperty();
 
     private final OcrPreferences ocrPreferences;
@@ -27,5 +38,69 @@ public class OcrTabViewModel implements PreferenceTabViewModel {
 
     public StringProperty ocrPathProperty() {
         return ocrPath;
+    }
+
+    Optional<String> autoDetectEnginePath() {
+        if (ocrmypdfPathExists()) {
+            return Optional.of("ocrmypdf");
+        } else if (pythonOcrPathExists()) {
+            return Optional.of("python -m ocrmypdf");
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private boolean ocrmypdfPathExists() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("ocrmypdf", "--version");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            // Get the output and the errors of the process
+            StreamGobbler streamGobblerInput = new StreamGobbler(process.getInputStream(), LOGGER::debug);
+            HeadlessExecutorService.INSTANCE.execute(streamGobblerInput);
+
+            boolean finished = process.waitFor(CHECKING_TIMEOUT, TimeUnit.SECONDS);
+            if (!finished) {
+                process.destroyForcibly();
+                LOGGER.debug("Auto-detecting ocrmypdf as path timed out");
+                return false;
+            }
+            return process.exitValue() == 0;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOGGER.debug("Auto detection of ocrmpdf as engine's path was interrupted", e);
+            return false;
+        } catch (IOException e) {
+            LOGGER.debug("ocrmypdf is not available as engine's path: IOException occurred", e);
+            return false;
+        }
+    }
+
+    private boolean pythonOcrPathExists() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", "-m", "ocrmypdf", "--version");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            // Get the output and the errors of the process
+            StreamGobbler streamGobblerInput = new StreamGobbler(process.getInputStream(), LOGGER::debug);
+            HeadlessExecutorService.INSTANCE.execute(streamGobblerInput);
+
+            boolean finished = process.waitFor(CHECKING_TIMEOUT, TimeUnit.SECONDS);
+            if (!finished) {
+                process.destroyForcibly();
+                LOGGER.debug("Auto-detecting python -m ocrmypdf as path timed out");
+                return false;
+            }
+            return process.exitValue() == 0;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOGGER.debug("Auto detection of python -m ocrmpdf as engine's path was interrupted", e);
+            return false;
+        } catch (IOException e) {
+            LOGGER.debug("python -m ocrmpdf is not available as engine's path: IOException occurred", e);
+            return false;
+        }
     }
 }
