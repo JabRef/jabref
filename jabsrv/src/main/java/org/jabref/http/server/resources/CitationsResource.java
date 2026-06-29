JabRef/jabref-koppor

Cache /citations/lookup by text hash and add batch endpoint
The SumatraPDF page-scan flow needs one HTTP round-trip per visible page
to paint per-citation marker dots, not one per link. Two server-side
changes make that affordable:

1. CitationCacheService gains a second Caffeine map keyed by SHA-256 of
   the raw citation text (max 1024 entries, 24h TTL — longer than the
   CUID2 token cache because the parsed BibEntry stays valid as long as
   the citation text does). /citations/lookup now checks this map first;
   on hit it reuses the prior parse and skips the LLM / fetcher call
   entirely. Each lookup still mints a fresh CUID2 token so the
   add-from-cache flow remains unchanged.

2. New POST /libraries/{id}/citations/lookup:batch accepts a JSON array
   of citation texts and returns a parallel array of LookupResponse,
   aligned by index. Blank slots and per-citation parse failures yield
   a "none" response so one bad entry can't fail the whole batch. The
   shared body is factored into a private doLookup helper so single and
   batched paths stay in lock-step.

Token cache TTL stays at 1h: tokens are short-lived by design (lookup →
add round-trip). The text-hash cache picks up the slack for repeat
lookups across longer reading sessions.

jabsrv/src/main/java/org/jabref/http/server/resources/CitationsResource.java
@@ -98,18 +98,95 @@ public class CitationsResource {
         // The target library is the one Ctrl+J would add to: "current" -> active
         // library, any other id -> that open library (404 if unknown/closed).
         BibDatabaseContext targetContext = resolveTargetContext(id);
+        return doLookup(citationText, targetContext,
+                        srvStateManager.getOpenDatabases(),
+                        new DuplicateCheck(new BibEntryTypesManager()));
+    }
 
-        BibEntry parsed = ServerUtils.parsePlainCitation(preferences, citationText)
-                .orElseThrow(() -> new BadRequestException("Could not parse a bibliography entry from the given text."));
+    /// Wrapper for the batched lookup payload. Plain `List<String>` would work
+    /// too, but a named record makes the JSON self-describing and leaves room
+    /// to add per-request options (e.g. include-snippets) without breaking the
+    /// schema.
+    public record BatchLookupRequest(List<String> citations) {
+    }
+
+    /// Aligned-by-index with the request: `results[i]` corresponds to
+    /// `citations[i]`. Blank input slots yield an empty `LookupResponse`
+    /// (matches=[], matchScope="none") rather than failing the whole batch.
+    public record BatchLookupResponse(List<LookupResponse> results) {
+    }
+
+    /// Batched variant of [#lookup] for the SumatraPDF page-scan flow: when
+    /// the reader lands on a new page we extract every citation link's
+    /// extracted bibliography text in one shot, fire one POST, and paint the
+    /// returned per-citation `matchScope` as in-PDF dots. Single round-trip
+    /// + JabRef-side [CitationCacheService#getByTextHash] hits mean repeat
+    /// scans of the same page are O(1) per citation with no LLM cost.
+    @POST
+    @Path("lookup:batch")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public BatchLookupResponse lookupBatch(@PathParam("id") String id, BatchLookupRequest request) throws FetcherException {
+        if (request == null || request.citations() == null || request.citations().isEmpty()) {
+            throw new BadRequestException("Citations list must not be empty.");
+        }
+        BibDatabaseContext targetContext = resolveTargetContext(id);
+        // Resolve once and reuse across every citation in the batch: the
+        // open-database list and DuplicateCheck are independent of the
+        // individual citation text, so re-creating per iteration would burn
+        // CPU for no benefit.
+        List<BibDatabaseContext> openDatabases = srvStateManager.getOpenDatabases();
+        DuplicateCheck duplicateCheck = new DuplicateCheck(new BibEntryTypesManager());
+
+        List<LookupResponse> results = new ArrayList<>(request.citations().size());
+        for (String citationText : request.citations()) {
+            if (StringUtil.isBlank(citationText)) {
+                // Empty slot — return a "none" response so client indexing
+                // stays aligned. Skipping would shift downstream results.
+                results.add(new LookupResponse(List.of(), "none", null, null));
+                continue;
+            }
+            try {
+                results.add(doLookup(citationText, targetContext, openDatabases, duplicateCheck));
+            } catch (FetcherException e) {
+                // One citation failing to parse shouldn't fail the batch.
+                // Surface as an empty response so the client paints "no match"
+                // (= gray ring) for that slot; the others still resolve.
+                results.add(new LookupResponse(List.of(), "none", null, null));
+            }
+        }
+        return new BatchLookupResponse(results);
+    }
+
+    /// Shared body of single + batched lookup. Resolves the citation text to
+    /// a `BibEntry` (via the text-hash cache when possible, else the
+    /// LLM/fetcher), walks every open library for duplicates, mints a fresh
+    /// token for the add-from-cache flow, and assembles the response.
+    private LookupResponse doLookup(String citationText,
+                                    BibDatabaseContext targetContext,
+                                    List<BibDatabaseContext> openDatabases,
+                                    DuplicateCheck duplicateCheck) throws FetcherException {
+        // Text-hash cache first: lets a re-scan of the same PDF page (or two
+        // distinct PDFs that cite the same paper) reuse the prior parse and
+        // skip the LLM call entirely.
+        String textHash = CitationCacheService.sha256Hex(citationText);
+        Optional<CitationCacheService.CachedCitation> cachedByText = citationCacheService.getByTextHash(textHash);
+        BibEntry parsed;
+        if (cachedByText.isPresent()) {
+            parsed = cachedByText.get().parsed();
+        } else {
+            parsed = ServerUtils.parsePlainCitation(preferences, citationText)
+                    .orElseThrow(() -> new BadRequestException("Could not parse a bibliography entry from the given text."));
+            citationCacheService.putByTextHash(textHash, parsed, citationText);
+        }
 
         // Cross-library lookup: walk every open library so we can tell the
         // client whether the citation is in the *target* library (Ctrl+J
         // would create a duplicate → mint) vs an *other* open library (still
         // worth surfacing — the user may want to switch libraries or copy
         // across → olive, AnchorHub's related-match colour).
-        DuplicateCheck duplicateCheck = new DuplicateCheck(new BibEntryTypesManager());
         List<LookupMatch> matches = new ArrayList<>();
-        for (BibDatabaseContext ctx : srvStateManager.getOpenDatabases()) {
+        for (BibDatabaseContext ctx : openDatabases) {
             boolean isActive = ctx == targetContext;
             duplicateCheck.containsDuplicate(ctx.getDatabase(), parsed, ctx.getMode())
                           .ifPresent(existing -> matches.add(new LookupMatch(

jabsrv/src/main/java/org/jabref/http/server/services/CitationCacheService.java
@@ -1,5 +1,8 @@
 package org.jabref.http.server.services;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
@@ -13,17 +16,26 @@ import jakarta.inject.Singleton;
 
 /// Caches the result of a plain-citation → BibEntry conversion so a hover
 /// existence check and a subsequent add-to-library don't both pay the LLM /
-/// fetcher cost. Cache keys are server-issued CUID2 strings returned from the
-/// lookup endpoint; the matching add endpoint consumes the key.
+/// fetcher cost.
 ///
-/// Bounded to keep memory tight (256 entries, ~5 KB each ≈ 1.25 MB worst case)
-/// and time-bounded to 1 hour so a stale token left over from a previous
-/// session can't resurrect an unrelated citation.
+/// Two complementary maps:
+///
+/// 1. **Token cache** — keyed by a server-issued CUID2 returned from the
+///    lookup endpoint. The matching add endpoint consumes the key. Short TTL
+///    so a stale token can't resurrect an unrelated citation across sessions.
+/// 2. **Text-hash cache** — keyed by SHA-256 of the raw citation text. Lets a
+///    repeat `/lookup` (or batched `/lookup:batch`) skip the LLM / fetcher
+///    parse entirely when the same citation string has been parsed recently
+///    (e.g. SumatraPDF re-scans a visible PDF page after the user jumps back
+///    to it). Longer TTL than the token cache because the parsed BibEntry
+///    stays valid as long as the citation text does.
 @Singleton
 public class CitationCacheService {
 
-    private static final int MAX_ENTRIES = 256;
-    private static final Duration TTL = Duration.ofHours(1);
+    private static final int TOKEN_MAX_ENTRIES = 256;
+    private static final Duration TOKEN_TTL = Duration.ofHours(1);
+    private static final int TEXT_MAX_ENTRIES = 1024;
+    private static final Duration TEXT_TTL = Duration.ofHours(24);
     /// Token length picked to match the CUID2 size already used by
     /// [BibFieldsIndexer]: long enough to be collision-resistant in a 256-slot
     /// cache without being noisy in HTTP responses.
@@ -32,16 +44,21 @@ public class CitationCacheService {
     public record CachedCitation(BibEntry parsed, String rawCitation, Instant createdAt) {
     }
 
-    private final Cache<String, CachedCitation> cache = Caffeine.newBuilder()
-                                                                .maximumSize(MAX_ENTRIES)
-                                                                .expireAfterWrite(TTL)
-                                                                .build();
+    private final Cache<String, CachedCitation> tokenCache = Caffeine.newBuilder()
+                                                                     .maximumSize(TOKEN_MAX_ENTRIES)
+                                                                     .expireAfterWrite(TOKEN_TTL)
+                                                                     .build();
+
+    private final Cache<String, CachedCitation> textCache = Caffeine.newBuilder()
+                                                                    .maximumSize(TEXT_MAX_ENTRIES)
+                                                                    .expireAfterWrite(TEXT_TTL)
+                                                                    .build();
 
     /// Stores a parsed citation and returns the token the client should send
     /// back to the add endpoint.
     public String put(BibEntry parsed, String rawCitation) {
         String key = CUID.randomCUID2(CUID_LENGTH).toString();
-        cache.put(key, new CachedCitation(parsed, rawCitation, Instant.now()));
+        tokenCache.put(key, new CachedCitation(parsed, rawCitation, Instant.now()));
         return key;
     }
 
@@ -49,12 +66,44 @@ public class CitationCacheService {
     /// was never issued. Does **not** evict on read so the same lookup result
     /// can be added more than once (idempotent across retries).
     public Optional<CachedCitation> get(String key) {
-        return Optional.ofNullable(cache.getIfPresent(key));
+        return Optional.ofNullable(tokenCache.getIfPresent(key));
     }
 
     /// Drops the cache entry; called by the add endpoint after a successful
     /// import so a stale token can't be re-used to add a duplicate.
     public void invalidate(String key) {
-        cache.invalidate(key);
+        tokenCache.invalidate(key);
+    }
+
+    /// Stashes a parsed citation under its text-hash so a later `/lookup` for
+    /// the same text can skip the LLM parse. The token-keyed cache is left
+    /// untouched — callers mint a fresh token via [#put] for each lookup.
+    public void putByTextHash(String textHash, BibEntry parsed, String rawCitation) {
+        textCache.put(textHash, new CachedCitation(parsed, rawCitation, Instant.now()));
+    }
+
+    /// Returns the cached parse for the given text-hash, or empty on miss.
+    public Optional<CachedCitation> getByTextHash(String textHash) {
+        return Optional.ofNullable(textCache.getIfPresent(textHash));
+    }
+
+    /// SHA-256 of the UTF-8 bytes of the citation text, lower-case hex. Used
+    /// as the text-cache key. Stable across processes / restarts so a reader
+    /// hitting the same PDF twice in a row gets cache hits even after a
+    /// JabRef restart that lost the in-memory cache (clients re-derive the
+    /// same hash from the same citation text).
+    public static String sha256Hex(String citationText) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(citationText.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder(hash.length * 2);
+            for (byte b : hash) {
+                sb.append(Character.forDigit((b >> 4) & 0xF, 16));
+                sb.append(Character.forDigit(b & 0xF, 16));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-256 unavailable on this JVM", e);
+        }
     }
 }

JabRef/jabref

jabsrv/src/main/java/org/jabref/http/server/resources/CitationsResource.java
@@ -98,18 +98,94 @@ public class CitationsResource {
         // The target library is the one Ctrl+J would add to: "current" -> active
         // library, any other id -> that open library (404 if unknown/closed).
         BibDatabaseContext targetContext = resolveTargetContext(id);
+        return doLookup(citationText, targetContext,
+                        srvStateManager.getOpenDatabases(),
+                        new DuplicateCheck(new BibEntryTypesManager()));
+    }
 
-        BibEntry parsed = ServerUtils.parsePlainCitation(preferences, citationText)
-                                     .orElseThrow(() -> new BadRequestException("Could not parse a bibliography entry from the given text."));
+    /// Wrapper for the batched lookup payload. Plain `List<String>` would work
+    /// too, but a named record makes the JSON self-describing and leaves room
+    /// to add per-request options (e.g. include-snippets) without breaking the
+    /// schema.
+    public record BatchLookupRequest(List<String> citations) {
+    }
+
+    /// Aligned-by-index with the request: `results[i]` corresponds to
+    /// `citations[i]`. Blank input slots yield an empty `LookupResponse`
+    /// (matches=[], matchScope="none") rather than failing the whole batch.
+    public record BatchLookupResponse(List<LookupResponse> results) {
+    }
+
+    /// Batched variant of [#lookup] for the SumatraPDF page-scan flow: when
+    /// the reader lands on a new page we extract every citation link's
+    /// extracted bibliography text in one shot, fire one POST, and paint the
+    /// returned per-citation `matchScope` as in-PDF dots. Single round-trip
+    /// + JabRef-side [CitationCacheService#getByText] hits mean repeat
+    /// scans of the same page are O(1) per citation with no LLM cost.
+    @POST
+    @Path("lookup:batch")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public BatchLookupResponse lookupBatch(@PathParam("id") String id, BatchLookupRequest request) throws FetcherException {
+        if (request == null || request.citations() == null || request.citations().isEmpty()) {
+            throw new BadRequestException("Citations list must not be empty.");
+        }
+        BibDatabaseContext targetContext = resolveTargetContext(id);
+        // Resolve once and reuse across every citation in the batch: the
+        // open-database list and DuplicateCheck are independent of the
+        // individual citation text, so re-creating per iteration would burn
+        // CPU for no benefit.
+        List<BibDatabaseContext> openDatabases = srvStateManager.getOpenDatabases();
+        DuplicateCheck duplicateCheck = new DuplicateCheck(new BibEntryTypesManager());
+
+        List<LookupResponse> results = new ArrayList<>(request.citations().size());
+        for (String citationText : request.citations()) {
+            if (StringUtil.isBlank(citationText)) {
+                // Empty slot — return a "none" response so client indexing
+                // stays aligned. Skipping would shift downstream results.
+                results.add(new LookupResponse(List.of(), "none", null, null));
+                continue;
+            }
+            try {
+                results.add(doLookup(citationText, targetContext, openDatabases, duplicateCheck));
+            } catch (FetcherException e) {
+                // One citation failing to parse shouldn't fail the batch.
+                // Surface as an empty response so the client paints "no match"
+                // (= gray ring) for that slot; the others still resolve.
+                results.add(new LookupResponse(List.of(), "none", null, null));
+            }
+        }
+        return new BatchLookupResponse(results);
+    }
+
+    /// Shared body of single + batched lookup. Resolves the citation text to
+    /// a `BibEntry` (via the text-hash cache when possible, else the
+    /// LLM/fetcher), walks every open library for duplicates, mints a fresh
+    /// token for the add-from-cache flow, and assembles the response.
+    private LookupResponse doLookup(String citationText,
+                                    BibDatabaseContext targetContext,
+                                    List<BibDatabaseContext> openDatabases,
+                                    DuplicateCheck duplicateCheck) throws FetcherException {
+        // Text cache first: lets a re-scan of the same PDF page (or two
+        // distinct PDFs that cite the same paper) reuse the prior parse and
+        // skip the LLM call entirely.
+        Optional<CitationCacheService.CachedCitation> cachedByText = citationCacheService.getByText(citationText);
+        BibEntry parsed;
+        if (cachedByText.isPresent()) {
+            parsed = cachedByText.get().parsed();
+        } else {
+            parsed = ServerUtils.parsePlainCitation(preferences, citationText)
+                    .orElseThrow(() -> new BadRequestException("Could not parse a bibliography entry from the given text."));
+            citationCacheService.putByText(parsed, citationText);
+        }
 
         // Cross-library lookup: walk every open library so we can tell the
         // client whether the citation is in the *target* library (Ctrl+J
         // would create a duplicate → mint) vs an *other* open library (still
         // worth surfacing — the user may want to switch libraries or copy
         // across → olive, AnchorHub's related-match colour).
-        DuplicateCheck duplicateCheck = new DuplicateCheck(new BibEntryTypesManager());
         List<LookupMatch> matches = new ArrayList<>();
-        for (BibDatabaseContext ctx : srvStateManager.getOpenDatabases()) {
+        for (BibDatabaseContext ctx : openDatabases) {
             boolean isActive = ctx == targetContext;
             duplicateCheck.containsDuplicate(ctx.getDatabase(), parsed, ctx.getMode())
                           .ifPresent(existing -> matches.add(new LookupMatch(

jabsrv/src/main/java/org/jabref/http/server/services/CitationCacheService.java
@@ -13,17 +13,26 @@ import jakarta.inject.Singleton;
 
 /// Caches the result of a plain-citation → BibEntry conversion so a hover
 /// existence check and a subsequent add-to-library don't both pay the LLM /
-/// fetcher cost. Cache keys are server-issued CUID2 strings returned from the
-/// lookup endpoint; the matching add endpoint consumes the key.
+/// fetcher cost.
 ///
-/// Bounded to keep memory tight (256 entries, ~5 KB each ≈ 1.25 MB worst case)
-/// and time-bounded to 1 hour so a stale token left over from a previous
-/// session can't resurrect an unrelated citation.
+/// Two complementary maps:
+///
+/// 1. **Token cache** — keyed by a server-issued CUID2 returned from the
+///    lookup endpoint. The matching add endpoint consumes the key. Short TTL
+///    so a stale token can't resurrect an unrelated citation across sessions.
+/// 2. **Text cache** — keyed by the raw citation text itself. Lets a repeat
+///    `/lookup` (or batched `/lookup:batch`) skip the LLM / fetcher parse
+///    entirely when the same citation string has been parsed recently (e.g.
+///    SumatraPDF re-scans a visible PDF page after the user jumps back to it).
+///    Longer TTL than the token cache because the parsed BibEntry stays valid
+///    as long as the citation text does.
 @Singleton
 public class CitationCacheService {
 
-    private static final int MAX_ENTRIES = 256;
-    private static final Duration TTL = Duration.ofHours(1);
+    private static final int TOKEN_MAX_ENTRIES = 256;
+    private static final Duration TOKEN_TTL = Duration.ofHours(1);
+    private static final int TEXT_MAX_ENTRIES = 1024;
+    private static final Duration TEXT_TTL = Duration.ofHours(24);
     /// Token length picked to match the CUID2 size already used by
     /// [BibFieldsIndexer]: long enough to be collision-resistant in a 256-slot
     /// cache without being noisy in HTTP responses.
@@ -32,16 +41,21 @@ public class CitationCacheService {
     public record CachedCitation(BibEntry parsed, String rawCitation, Instant createdAt) {
     }
 
-    private final Cache<String, CachedCitation> cache = Caffeine.newBuilder()
-                                                                .maximumSize(MAX_ENTRIES)
-                                                                .expireAfterWrite(TTL)
-                                                                .build();
+    private final Cache<String, CachedCitation> tokenCache = Caffeine.newBuilder()
+                                                                     .maximumSize(TOKEN_MAX_ENTRIES)
+                                                                     .expireAfterWrite(TOKEN_TTL)
+                                                                     .build();
+
+    private final Cache<String, CachedCitation> textCache = Caffeine.newBuilder()
+                                                                    .maximumSize(TEXT_MAX_ENTRIES)
+                                                                    .expireAfterWrite(TEXT_TTL)
+                                                                    .build();
 
     /// Stores a parsed citation and returns the token the client should send
     /// back to the add endpoint.
     public String put(BibEntry parsed, String rawCitation) {
         String key = CUID.randomCUID2(CUID_LENGTH).toString();
-        cache.put(key, new CachedCitation(parsed, rawCitation, Instant.now()));
+        tokenCache.put(key, new CachedCitation(parsed, rawCitation, Instant.now()));
         return key;
     }
 
@@ -49,12 +63,24 @@ public class CitationCacheService {
     /// was never issued. Does **not** evict on read so the same lookup result
     /// can be added more than once (idempotent across retries).
     public Optional<CachedCitation> get(String key) {
-        return Optional.ofNullable(cache.getIfPresent(key));
+        return Optional.ofNullable(tokenCache.getIfPresent(key));
     }
 
     /// Drops the cache entry; called by the add endpoint after a successful
     /// import so a stale token can't be re-used to add a duplicate.
     public void invalidate(String key) {
-        cache.invalidate(key);
+        tokenCache.invalidate(key);
+    }
+
+    /// Stashes a parsed citation under its raw text so a later `/lookup` for
+    /// the same text can skip the LLM parse. The token-keyed cache is left
+    /// untouched — callers mint a fresh token via [#put] for each lookup.
+    public void putByText(BibEntry parsed, String rawCitation) {
+        textCache.put(rawCitation, new CachedCitation(parsed, rawCitation, Instant.now()));
+    }
+
+    /// Returns the cached parse for the given citation text, or empty on miss.
+    public Optional<CachedCitation> getByText(String citationText) {
+        return Optional.ofNullable(textCache.getIfPresent(citationText));
     }
 }