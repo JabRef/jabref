01c7486b49741953bbd21c604dae5b37cf7d87a5
thesauravpoddar <thesauravpoddar@gmail.com>
2026-07-11 12:13:47 +0530

    removed unused dependencies

--- jablib/src/main/java/org/jabref/model/paging/Page.java
+++ jablib/src/main/java/org/jabref/model/paging/Page.java
@@ -1,7 +1,6 @@
 package org.jabref.model.paging;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
--- jablib/src/test/java/org/jabref/model/paging/PageTest.java
+++ jablib/src/test/java/org/jabref/model/paging/PageTest.java
@@ -2,7 +2,6 @@ package org.jabref.model.paging;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
