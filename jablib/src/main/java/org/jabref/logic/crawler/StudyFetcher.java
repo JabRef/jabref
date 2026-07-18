d5ea5c362fbca7083ad843a4c61addc92e3cd306
Faneesh Juneja <willowstration@gmail.com>
2026-07-18 09:38:21 -0700

    Update jablib/src/main/java/org/jabref/logic/crawler/StudyFetcher.java
    Co-authored-by: Loay Tarek Mostafa <142718879+LoayTarek5@users.noreply.github.com>

--- jablib/src/main/java/org/jabref/logic/crawler/StudyFetcher.java
+++ jablib/src/main/java/org/jabref/logic/crawler/StudyFetcher.java
@@ -3,7 +3,6 @@ package org.jabref.logic.crawler;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.jabref.logic.importer.FetcherException;
