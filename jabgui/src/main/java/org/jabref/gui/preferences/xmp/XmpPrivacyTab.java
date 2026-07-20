1712346345d559b06e648590321ccc5a4ebd6788
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2026-07-20 23:28:26 +0200

    Implement XMP tab

--- jabgui/src/main/java/org/jabref/gui/preferences/xmp/XmpPrivacyTab.java
+++ jabgui/src/main/java/org/jabref/gui/preferences/xmp/XmpPrivacyTab.java
@@ -3,49 +3,43 @@ package org.jabref.gui.preferences.xmp;
 import javax.swing.undo.UndoManager;
 
 import javafx.application.Platform;
-import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 import org.jabref.gui.icon.IconTheme;
-import org.jabref.gui.preferences.AbstractPreferenceTabView;
-import org.jabref.gui.preferences.PreferencesTab;
+import org.jabref.gui.icon.JabRefIconView;
+import org.jabref.gui.preferences.forms.AbstractFormTabView;
 import org.jabref.gui.util.BindingsHelper;
 import org.jabref.gui.util.FieldsUtil;
 import org.jabref.gui.util.IconValidationDecorator;
 import org.jabref.gui.util.ValueTableCellFactory;
 import org.jabref.gui.util.ViewModelListCellFactory;
 import org.jabref.logic.l10n.Localization;
-import org.jabref.logic.preferences.CliPreferences;
 import org.jabref.model.entry.field.Field;
 
-import com.airhacks.afterburner.views.ViewLoader;
+import com.airhacks.afterburner.injection.Injector;
 import de.saxsys.mvvmfx.utils.validation.visualization.ControlsFxVisualizer;
-import jakarta.inject.Inject;
 
-public class XmpPrivacyTab extends AbstractPreferenceTabView<XmpPrivacyTabViewModel> implements PreferencesTab {
+public class XmpPrivacyTab extends AbstractFormTabView<XmpPrivacyTabViewModel> {
 
-    @FXML private CheckBox enableXmpFilter;
-    @FXML private TableView<Field> filterList;
-    @FXML private TableColumn<Field, Field> fieldColumn;
-    @FXML private TableColumn<Field, Field> actionsColumn;
-    @FXML private ComboBox<Field> addFieldName;
-    @FXML private Button addField;
-
-    @Inject private CliPreferences preferences;
-    @Inject private UndoManager undoManager;
-
+    private final UndoManager undoManager = Injector.instantiateModelOrService(UndoManager.class);
     private final ControlsFxVisualizer validationVisualizer = new ControlsFxVisualizer();
 
+    private TableView<Field> filterList;
+
     public XmpPrivacyTab() {
-        ViewLoader.view(this)
-                  .root(this)
-                  .load();
+        this.viewModel = new XmpPrivacyTabViewModel(dialogService, preferences.getXmpPreferences());
+        buildView();
     }
 
     @Override
@@ -53,14 +47,27 @@ public class XmpPrivacyTab extends AbstractPreferenceTabView<XmpPrivacyTabViewMo
         return Localization.lang("XMP metadata");
     }
 
-    public void initialize() {
-        this.viewModel = new XmpPrivacyTabViewModel(dialogService, preferences.getXmpPreferences());
+    private void buildView() {
+        getChildren().add(form()
+                .title(Localization.lang("XMP export privacy settings"))
+                .checkbox(Localization.lang("Do not write the following fields to XMP Metadata"), viewModel.xmpFilterEnabledProperty())
+                .custom(buildFilterRegion())
+                .build());
 
-        enableXmpFilter.selectedProperty().bindBidirectional(viewModel.xmpFilterEnabledProperty());
+        validationVisualizer.setDecoration(new IconValidationDecorator());
+        Platform.runLater(() -> validationVisualizer.initVisualization(viewModel.xmpFilterListValidationStatus(), filterList));
+    }
+
+    private Node buildFilterRegion() {
+        filterList = new TableView<>();
+        filterList.setPrefHeight(300.0);
+        filterList.setPrefWidth(200.0);
+        filterList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         filterList.disableProperty().bind(viewModel.xmpFilterEnabledProperty().not());
-        addFieldName.disableProperty().bind(viewModel.xmpFilterEnabledProperty().not());
-        addField.disableProperty().bind(viewModel.xmpFilterEnabledProperty().not());
+        filterList.itemsProperty().bind(viewModel.filterListProperty());
 
+        TableColumn<Field, Field> fieldColumn = new TableColumn<>(Localization.lang("Field name"));
+        fieldColumn.setMinWidth(100.0);
         fieldColumn.setSortable(true);
         fieldColumn.setReorderable(false);
         fieldColumn.setCellValueFactory(cellData -> BindingsHelper.constantOf(cellData.getValue()));
@@ -68,16 +75,23 @@ public class XmpPrivacyTab extends AbstractPreferenceTabView<XmpPrivacyTabViewMo
                 .withText(item -> FieldsUtil.getNameWithType(item, preferences, undoManager))
                 .install(fieldColumn);
 
+        TableColumn<Field, Field> actionsColumn = new TableColumn<>();
+        actionsColumn.setMinWidth(40.0);
+        actionsColumn.setPrefWidth(40.0);
+        actionsColumn.setMaxWidth(39.0);
+        actionsColumn.setResizable(false);
         actionsColumn.setSortable(false);
         actionsColumn.setReorderable(false);
+        actionsColumn.getStyleClass().add("actions-column");
         actionsColumn.setCellValueFactory(cellData -> BindingsHelper.constantOf(cellData.getValue()));
         new ValueTableCellFactory<Field, Field>()
                 .withGraphic(item -> IconTheme.JabRefIcons.DELETE_ENTRY.getGraphicNode())
                 .withTooltip(item -> Localization.lang("Remove") + " " + item.getName())
-                .withOnMouseClickedEvent(
-                        item -> evt -> viewModel.removeFilter(filterList.getFocusModel().getFocusedItem()))
+                .withOnMouseClickedEvent(item -> _ -> viewModel.removeFilter(filterList.getFocusModel().getFocusedItem()))
                 .install(actionsColumn);
 
+        filterList.getColumns().add(fieldColumn);
+        filterList.getColumns().add(actionsColumn);
         filterList.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             if (event.getCode() == KeyCode.DELETE) {
                 viewModel.removeFilter(filterList.getSelectionModel().getSelectedItem());
@@ -85,9 +99,10 @@ public class XmpPrivacyTab extends AbstractPreferenceTabView<XmpPrivacyTabViewMo
             }
         });
 
-        filterList.itemsProperty().bind(viewModel.filterListProperty());
-
+        ComboBox<Field> addFieldName = new ComboBox<>();
+        addFieldName.setPrefWidth(200.0);
         addFieldName.setEditable(true);
+        addFieldName.disableProperty().bind(viewModel.xmpFilterEnabledProperty().not());
         new ViewModelListCellFactory<Field>()
                 .withText(item -> FieldsUtil.getNameWithType(item, preferences, undoManager))
                 .install(addFieldName);
@@ -100,12 +115,20 @@ public class XmpPrivacyTab extends AbstractPreferenceTabView<XmpPrivacyTabViewMo
                 event.consume();
             }
         });
+        VBox.setVgrow(addFieldName, Priority.ALWAYS);
 
-        validationVisualizer.setDecoration(new IconValidationDecorator());
-        Platform.runLater(() -> validationVisualizer.initVisualization(viewModel.xmpFilterListValidationStatus(), filterList));
-    }
+        VBox listColumn = new VBox(4.0, filterList, addFieldName);
 
-    public void addField() {
-        viewModel.addField();
+        Button addField = new Button();
+        addField.setGraphic(new JabRefIconView(IconTheme.JabRefIcons.ADD_NOBOX));
+        addField.getStyleClass().addAll("icon-button", "narrow");
+        addField.setPrefSize(25.0, 25.0);
+        addField.setTooltip(new Tooltip(Localization.lang("Add field to filter list")));
+        addField.disableProperty().bind(viewModel.xmpFilterEnabledProperty().not());
+        addField.setOnAction(_ -> viewModel.addField());
+
+        HBox region = new HBox(4.0, listColumn, addField);
+        region.setAlignment(Pos.BOTTOM_LEFT);
+        return region;
     }
 }
