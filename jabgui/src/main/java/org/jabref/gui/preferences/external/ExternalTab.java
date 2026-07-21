90fe4c405a8461e32ba485331b86f70eb5e6164a
Carl Christian Snethlage <calixtus@users.noreply.github.com>
2026-07-21 08:09:02 +0200

    Implement External tab

--- jabgui/src/main/java/org/jabref/gui/preferences/external/ExternalTab.java
+++ jabgui/src/main/java/org/jabref/gui/preferences/external/ExternalTab.java
@@ -1,48 +1,44 @@
 package org.jabref.gui.preferences.external;
 
 import javafx.application.Platform;
-import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 import org.jabref.gui.actions.ActionFactory;
 import org.jabref.gui.actions.StandardActions;
 import org.jabref.gui.help.HelpAction;
-import org.jabref.gui.preferences.AbstractPreferenceTabView;
-import org.jabref.gui.preferences.PreferencesTab;
+import org.jabref.gui.icon.IconTheme;
+import org.jabref.gui.icon.JabRefIconView;
+import org.jabref.gui.preferences.forms.AbstractFormTabView;
 import org.jabref.gui.push.GuiPushToApplication;
 import org.jabref.gui.util.IconValidationDecorator;
 import org.jabref.gui.util.ViewModelListCellFactory;
 import org.jabref.logic.help.HelpFile;
 import org.jabref.logic.l10n.Localization;
 
-import com.airhacks.afterburner.views.ViewLoader;
 import de.saxsys.mvvmfx.utils.validation.visualization.ControlsFxVisualizer;
 import org.controlsfx.control.SearchableComboBox;
 
-public class ExternalTab extends AbstractPreferenceTabView<ExternalTabViewModel> implements PreferencesTab {
-
-    @FXML private TextField eMailReferenceSubject;
-    @FXML private CheckBox autoOpenAttachedFolders;
-    @FXML private SearchableComboBox<GuiPushToApplication> pushToApplicationCombo;
-    @FXML private TextField citeCommand;
-    @FXML private Button autolinkExternalHelp;
-
-    @FXML private CheckBox useCustomTerminal;
-    @FXML private TextField customTerminalCommand;
-    @FXML private Button customTerminalBrowse;
-    @FXML private CheckBox useCustomFileBrowser;
-    @FXML private TextField customFileBrowserCommand;
-    @FXML private Button customFileBrowserBrowse;
-    @FXML private TextField kindleEmail;
+public class ExternalTab extends AbstractFormTabView<ExternalTabViewModel> {
 
     private final ControlsFxVisualizer validationVisualizer = new ControlsFxVisualizer();
 
+    // Kept as fields so validation can be attached after the grids are assembled.
+    private TextField citeCommand;
+    private TextField customTerminalCommand;
+    private TextField customFileBrowserCommand;
+
     public ExternalTab() {
-        ViewLoader.view(this)
-                  .root(this)
-                  .load();
+        this.viewModel = new ExternalTabViewModel(dialogService, preferences);
+        buildView();
     }
 
     @Override
@@ -50,32 +46,22 @@ public class ExternalTab extends AbstractPreferenceTabView<ExternalTabViewModel>
         return Localization.lang("External programs");
     }
 
-    public void initialize() {
-        this.viewModel = new ExternalTabViewModel(dialogService, preferences);
+    private void buildView() {
+        getChildren().add(form()
+                .title(Localization.lang("External programs"))
 
-        new ViewModelListCellFactory<GuiPushToApplication>()
-                .withText(GuiPushToApplication::getDisplayName)
-                .withIcon(GuiPushToApplication::getApplicationIcon)
-                .install(pushToApplicationCombo);
+                .section(Localization.lang("Sending of emails"))
+                .stringField(Localization.lang("Subject for sending an email with references"), viewModel.eMailReferenceSubjectProperty())
+                .stringField(Localization.lang("Email address for sending to Kindle"), viewModel.kindleEmailProperty())
+                .checkbox(Localization.lang("Automatically open folders of attached files"), viewModel.autoOpenAttachedFoldersProperty())
 
-        eMailReferenceSubject.textProperty().bindBidirectional(viewModel.eMailReferenceSubjectProperty());
-        autoOpenAttachedFolders.selectedProperty().bindBidirectional(viewModel.autoOpenAttachedFoldersProperty());
+                .section(Localization.lang("Push applications"))
+                .custom(buildPushGrid())
 
-        pushToApplicationCombo.itemsProperty().bind(viewModel.pushToApplicationsListProperty());
-        pushToApplicationCombo.valueProperty().bindBidirectional(viewModel.selectedPushToApplication());
-        citeCommand.textProperty().bindBidirectional(viewModel.citeCommandProperty());
+                .section(Localization.lang("Custom applications"))
+                .custom(buildCustomApplicationsGrid())
 
-        useCustomTerminal.selectedProperty().bindBidirectional(viewModel.useCustomTerminalProperty());
-        customTerminalCommand.textProperty().bindBidirectional(viewModel.customTerminalCommandProperty());
-        customTerminalCommand.disableProperty().bind(useCustomTerminal.selectedProperty().not());
-        customTerminalBrowse.disableProperty().bind(useCustomTerminal.selectedProperty().not());
-
-        useCustomFileBrowser.selectedProperty().bindBidirectional(viewModel.useCustomFileBrowserProperty());
-        customFileBrowserCommand.textProperty().bindBidirectional(viewModel.customFileBrowserCommandProperty());
-        customFileBrowserCommand.disableProperty().bind(useCustomFileBrowser.selectedProperty().not());
-        customFileBrowserBrowse.disableProperty().bind(useCustomFileBrowser.selectedProperty().not());
-
-        kindleEmail.textProperty().bindBidirectional(viewModel.kindleEmailProperty());
+                .build());
 
         validationVisualizer.setDecoration(new IconValidationDecorator());
         Platform.runLater(() -> {
@@ -83,28 +69,101 @@ public class ExternalTab extends AbstractPreferenceTabView<ExternalTabViewModel>
             validationVisualizer.initVisualization(viewModel.fileBrowserCommandValidationStatus(), customFileBrowserCommand);
             validationVisualizer.initVisualization(viewModel.citeCommandValidationStatus(), citeCommand);
         });
-
-        ActionFactory actionFactory = new ActionFactory();
-        actionFactory.configureIconButton(StandardActions.HELP_PUSH_TO_APPLICATION, new HelpAction(HelpFile.PUSH_TO_APPLICATION, dialogService, preferences.getExternalApplicationsPreferences()), autolinkExternalHelp);
     }
 
-    @FXML
-    void pushToApplicationSettings() {
-        viewModel.pushToApplicationSettings();
+    private Node buildPushGrid() {
+        SearchableComboBox<GuiPushToApplication> pushToApplicationCombo = new SearchableComboBox<>();
+        new ViewModelListCellFactory<GuiPushToApplication>()
+                .withText(GuiPushToApplication::getDisplayName)
+                .withIcon(GuiPushToApplication::getApplicationIcon)
+                .install(pushToApplicationCombo);
+        pushToApplicationCombo.setPrefWidth(200.0);
+        pushToApplicationCombo.itemsProperty().bind(viewModel.pushToApplicationsListProperty());
+        pushToApplicationCombo.valueProperty().bindBidirectional(viewModel.selectedPushToApplication());
+
+        Button applicationSettings = iconButton(IconTheme.JabRefIcons.PREFERENCES, Localization.lang("Application settings"), viewModel::pushToApplicationSettings);
+
+        citeCommand = new TextField();
+        citeCommand.setPrefWidth(300.0);
+        citeCommand.textProperty().bindBidirectional(viewModel.citeCommandProperty());
+
+        Button citeHelp = new Button();
+        citeHelp.getStyleClass().addAll("icon-button", "narrow");
+        citeHelp.setPrefSize(20.0, 20.0);
+        new ActionFactory().configureIconButton(
+                StandardActions.HELP_PUSH_TO_APPLICATION,
+                new HelpAction(HelpFile.PUSH_TO_APPLICATION, dialogService, preferences.getExternalApplicationsPreferences()),
+                citeHelp);
+        Button resetCite = iconButton(IconTheme.JabRefIcons.REFRESH, Localization.lang("Reset to default"), viewModel::resetCiteCommandToDefault);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(4.0);
+        grid.setVgap(4.0);
+        grid.setAlignment(Pos.CENTER_LEFT);
+        grid.add(new Label(Localization.lang("Application to push entries to")), 0, 0);
+        grid.add(pushToApplicationCombo, 1, 0);
+        grid.add(applicationSettings, 2, 0);
+        grid.add(new Label(Localization.lang("Cite command")), 0, 1);
+        grid.add(citeCommand, 1, 1);
+        grid.add(citeHelp, 2, 1);
+        grid.add(resetCite, 3, 1);
+        return grid;
     }
 
-    @FXML
-    void useTerminalCommandBrowse() {
-        viewModel.customTerminalBrowse();
+    private Node buildCustomApplicationsGrid() {
+        String dirPlaceholderHint = Localization.lang("Note: Use the placeholder %DIR% for the location of the opened library file.");
+
+        CheckBox useCustomTerminal = new CheckBox(Localization.lang("Use custom terminal emulator"));
+        useCustomTerminal.selectedProperty().bindBidirectional(viewModel.useCustomTerminalProperty());
+        customTerminalCommand = commandField(dirPlaceholderHint, viewModel.customTerminalCommandProperty(), useCustomTerminal);
+        Button terminalBrowse = browseButton(useCustomTerminal, viewModel::customTerminalBrowse);
+
+        CheckBox useCustomFileBrowser = new CheckBox(Localization.lang("Use custom file browser"));
+        useCustomFileBrowser.selectedProperty().bindBidirectional(viewModel.useCustomFileBrowserProperty());
+        customFileBrowserCommand = commandField(dirPlaceholderHint, viewModel.customFileBrowserCommandProperty(), useCustomFileBrowser);
+        Button fileBrowserBrowse = browseButton(useCustomFileBrowser, viewModel::customFileBrowserBrowse);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(4.0);
+        grid.setVgap(4.0);
+        grid.setAlignment(Pos.CENTER_LEFT);
+        ColumnConstraints checkColumn = new ColumnConstraints();
+        checkColumn.setMinWidth(50.0);
+        checkColumn.setPrefWidth(200.0);
+        ColumnConstraints commandColumn = new ColumnConstraints();
+        commandColumn.setHgrow(Priority.ALWAYS);
+        commandColumn.setMinWidth(100.0);
+        grid.getColumnConstraints().addAll(checkColumn, commandColumn, new ColumnConstraints());
+        grid.add(useCustomTerminal, 0, 0);
+        grid.add(customTerminalCommand, 1, 0);
+        grid.add(terminalBrowse, 2, 0);
+        grid.add(useCustomFileBrowser, 0, 1);
+        grid.add(customFileBrowserCommand, 1, 1);
+        grid.add(fileBrowserBrowse, 2, 1);
+        return grid;
     }
 
-    @FXML
-    void useFileBrowserSpecialCommandBrowse() {
-        viewModel.customFileBrowserBrowse();
+    private TextField commandField(String tooltip, javafx.beans.property.StringProperty value, CheckBox enabler) {
+        TextField field = new TextField();
+        field.textProperty().bindBidirectional(value);
+        field.setTooltip(new Tooltip(tooltip));
+        field.disableProperty().bind(enabler.selectedProperty().not());
+        return field;
     }
 
-    @FXML
-    void resetCiteCommandToDefault() {
-        viewModel.resetCiteCommandToDefault();
+    private Button browseButton(CheckBox enabler, Runnable action) {
+        Button button = iconButton(IconTheme.JabRefIcons.OPEN, Localization.lang("Browse"), action);
+        button.disableProperty().bind(enabler.selectedProperty().not());
+        return button;
+    }
+
+    private Button iconButton(IconTheme.JabRefIcons icon, String tooltip, Runnable action) {
+        Button button = new Button();
+        button.setGraphic(new JabRefIconView(icon));
+        button.getStyleClass().addAll("icon-button", "narrow");
+        button.setPrefSize(20.0, 20.0);
+        button.setTooltip(new Tooltip(tooltip));
+        button.setOnAction(_ -> action.run());
+        return button;
     }
 }
