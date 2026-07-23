29aabdab4e85b257ae2831b3b2208dd7412199d8
subhramit <subhramit.bb@live.in>
2026-07-23 22:58:29 +0530

    Expand abbreviation
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- jablib/src/test/java/org/jabref/logic/openoffice/style/BstStyleTest.java
+++ jablib/src/test/java/org/jabref/logic/openoffice/style/BstStyleTest.java
@@ -10,31 +10,31 @@ class BstStyleTest {
 
     @Test
     void internalNameStripsBstExtensionIeee() {
-        BstStyle s = BstStyle.createInternal(BstStyle.INTERNAL_IEEETRAN_PATH);
-        assertEquals("IEEEtran", s.getName());
+        BstStyle style = BstStyle.createInternal(BstStyle.INTERNAL_IEEETRAN_PATH);
+        assertEquals("IEEEtran", style.getName());
     }
 
     @Test
     void internalNameStripsBstExtensionAbbrv() {
-        BstStyle s = BstStyle.createInternal(BstStyle.INTERNAL_ABBRV_PATH);
-        assertEquals("abbrv", s.getName());
+        BstStyle style = BstStyle.createInternal(BstStyle.INTERNAL_ABBRV_PATH);
+        assertEquals("abbrv", style.getName());
     }
 
     @Test
     void externalLowercaseExtensionIsStripped() {
-        BstStyle s = new BstStyle(Path.of("apa.bst"));
-        assertEquals("apa", s.getName());
+        BstStyle style = new BstStyle(Path.of("apa.bst"));
+        assertEquals("apa", style.getName());
     }
 
     @Test
     void externalUppercaseExtensionIsStripped() {
-        BstStyle s = new BstStyle(Path.of("FOO.BST"));
-        assertEquals("FOO", s.getName());
+        BstStyle style = new BstStyle(Path.of("FOO.BST"));
+        assertEquals("FOO", style.getName());
     }
 
     @Test
     void externalNoExtensionIsUnchanged() {
-        BstStyle s = new BstStyle(Path.of("customstyle"));
-        assertEquals("customstyle", s.getName());
+        BstStyle style = new BstStyle(Path.of("customstyle"));
+        assertEquals("customstyle", style.getName());
     }
 }
