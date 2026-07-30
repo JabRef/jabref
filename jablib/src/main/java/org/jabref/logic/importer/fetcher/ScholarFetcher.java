7d11565198937ebe7d2e63987e085196f4a96d1f
thesauravpoddar <thesauravpoddar@gmail.com>
2026-07-30 08:05:10 +0530

    fixes checkstyle errors

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/ScholarFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/ScholarFetcher.java
@@ -7,7 +7,6 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
