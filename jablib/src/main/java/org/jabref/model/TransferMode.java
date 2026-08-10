c673f0a0a08c74a7dcce7d1a21c332db0c6a1608
Eng-YasminKotb <ysysys141516@gmail.com>
2026-08-10 23:33:15 +0300

    Refactor: move transfer mode conversion to enum

--- jablib/src/main/java/org/jabref/model/TransferMode.java
+++ jablib/src/main/java/org/jabref/model/TransferMode.java
@@ -3,5 +3,22 @@ package org.jabref.model;
 public enum TransferMode {
     COPY,
     MOVE,
-    NONE
+    NONE;
+
+    public static TransferMode from(javafx.scene.input.TransferMode javafxTransferMode) {
+        if (javafxTransferMode == null) {
+            return NONE;
+        }
+        switch (javafxTransferMode) {
+            case COPY -> {
+                return COPY;
+            }
+            case MOVE -> {
+                return MOVE;
+            }
+            default ->
+                    throw new IllegalStateException("Unexpected transfer mode: " + javafxTransferMode);
+        }
+    }
 }
+
