f3356d6fa30339ef8701dc8057602452f6fb6b00
bgyoo970 <bbap970@gmail.com>
2026-08-15 00:01:09 -0400

    [#16075] fixed comment

--- jablib/src/main/java/org/jabref/logic/preview/CustomizedPreviewStyle.java
+++ jablib/src/main/java/org/jabref/logic/preview/CustomizedPreviewStyle.java
@@ -2,7 +2,7 @@ package org.jabref.logic.preview;
 
 // A persisted, record of a custom citation preview style.
 // id introduced as an uuid to help with keeping track of renaming custom citations
-// CustomizedTextPreviewLayout doesn't implement PreviewLayout
+// CustomizedPreviewStyle doesn't implement PreviewLayout
 // Meant to act as a persisted snapshot of the data needed to reconstruct a TextBasedPreviewLayout
 public record CustomizedPreviewStyle(String id, String name, String text) {
 }
