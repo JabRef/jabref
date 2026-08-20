7c2921d8ed6e74acefb98acb045a5855ae05d4ef
thesauravpoddar <thesauravpoddar@gmail.com>
2026-08-20 12:40:00 +0530

    fixed all checkstyle changes

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/ScholarFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/ScholarFetcher.java
@@ -26,7 +26,6 @@ import org.jabref.logic.util.URLUtil;
 import org.jabref.logic.util.strings.StringUtil;
 import org.jabref.model.entry.AuthorList;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.field.UnknownField;
 import org.jabref.model.entry.types.StandardEntryType;
