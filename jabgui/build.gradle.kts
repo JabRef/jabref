8734470ac01200d57affa6f84722566b678b951f
Siedlerchr <siedlerkiller@gmail.com>
2026-08-28 21:58:18 +0200

    add test and changelog

--- jabgui/build.gradle.kts
+++ jabgui/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.gradlex.javamodule.packaging.tasks.Jpackage
 import org.jabref.gradle.EmbeddedPostgresBinaries
+import org.jabref.gradle.VerifyJpackageJavaOptions
 import org.jabref.gradle.useLibericaJdkFull
 
 plugins {
@@ -261,6 +262,24 @@ embeddedPostgresBinaryByJpackageTask.forEach { (taskName, binary) ->
         javaOptions.set(application.applicationDefaultJvmArgs + "--add-modules=${binary.moduleName}")
         addModules.addAll(sharedJpackageImageModules)
     }
+}
+
+val verifyJpackageJavaOptions = tasks.register<VerifyJpackageJavaOptions>("verifyJpackageJavaOptions") {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Verifies that packaged launchers retain application JVM options"
+    mismatches.set(embeddedPostgresBinaryByJpackageTask.mapNotNull { (taskName, binary) ->
+        val actualOptions = tasks.named<Jpackage>(taskName).get().javaOptions.get()
+        val expectedOptions = application.applicationDefaultJvmArgs + "--add-modules=${binary.moduleName}"
+        if (actualOptions == expectedOptions) {
+            null
+        } else {
+            "$taskName expected $expectedOptions, but got $actualOptions"
+        }
+    })
+}
+
+tasks.named("check") {
+    dependsOn(verifyJpackageJavaOptions)
 }
 
 tasks.test {
