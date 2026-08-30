38a917f7ab9e4fe3a0e826f77b05410cec1848aa
Anmol <dipanshisharma906@gmail.com>
2026-08-30 18:18:33 +0530

    Add platform-conditional tests for default key bindings

--- jabgui/src/test/java/org/jabref/gui/keyboard/KeyBindingTest.java
+++ jabgui/src/test/java/org/jabref/gui/keyboard/KeyBindingTest.java
@@ -1,10 +1,27 @@
 package org.jabref.gui.keyboard;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KeyBindingTest {
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void defaultKeyBindingUsesMacSpecificDefaultOnMacOs() {
+        assertEquals("shortcut+alt+F", KeyBinding.LOOKUP_DOC_IDENTIFIER.getDefaultKeyBinding());
+        assertEquals("shortcut+alt+G", KeyBinding.FOCUS_GROUP_LIST.getDefaultKeyBinding());
+    }
+
+    @Test
+    @DisabledOnOs(OS.MAC)
+    void defaultKeyBindingUsesCrossPlatformDefaultOnOtherPlatforms() {
+        assertEquals("alt+F", KeyBinding.LOOKUP_DOC_IDENTIFIER.getDefaultKeyBinding());
+        assertEquals("alt+s", KeyBinding.FOCUS_GROUP_LIST.getDefaultKeyBinding());
+    }
 
     @Test
     void bindingsAffectedByOptionKeyCharacterInsertionHaveMacSpecificDefaults() {
