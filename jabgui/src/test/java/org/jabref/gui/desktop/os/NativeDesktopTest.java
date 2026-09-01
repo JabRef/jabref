3e881b683db0295449f42c2c67094cc0692f89d1
vsmottin <vsmottin@outlook.com>
2026-09-01 11:35:51 -0300

    test(NativeDesktop): change to parameterized test

--- jabgui/src/test/java/org/jabref/gui/desktop/os/NativeDesktopTest.java
+++ jabgui/src/test/java/org/jabref/gui/desktop/os/NativeDesktopTest.java
@@ -1,51 +1,45 @@
 package org.jabref.gui.desktop.os;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NativeDesktopTest {
 
-    @Test
-    void splitCommandLineWithSpacesNoQuotes() {
-        String command = "gnome-terminal --working-directory=/tmp";
-        List<String> expected = List.of("gnome-terminal", "--working-directory=/tmp");
-        assertEquals(expected, NativeDesktop.splitCommandLine(command));
+    private static Stream<Arguments> splitCommandLine() {
+        return Stream.of(
+                Arguments.of(List.of("gnome-terminal", "--working-directory=/tmp"),
+                        "gnome-terminal --working-directory=/tmp"),
+
+                Arguments.of(
+                        List.of("/usr/bin/gnome-terminal", "--working-directory=%DIR%"),
+                        "/usr/bin/gnome-terminal --working-directory='%DIR%'"),
+
+                Arguments.of(
+                        List.of("/usr/bin/gnome-terminal", "--working-directory=%DIR"),
+                        "/usr/bin/gnome-terminal --working-directory=\"%DIR\""),
+
+                Arguments.of(
+                        List.of("konsole", "--workdir", "/home/user/My Documents"),
+                        "konsole --workdir '/home/user/My Documents'"),
+
+                Arguments.of(List.of("xfce4-terminal", "--working-directory=/home/user/My Documents"),
+                        "xfce4-terminal --working-directory=/home/user/My\\ Documents"),
+
+                Arguments.of(List.of("C:\\Program Files\\ConEmu\\ConEmu64.exe", "/single", "/dir", "%DIR%"),
+                        "\"C:\\Program Files\\ConEmu\\ConEmu64.exe\" /single /dir \"%DIR%\""),
+
+                Arguments.of(List.of(), "   "));
     }
 
-    @Test
-    void splitCommandLineWithSingleQuotedDir() {
-        String command = "/usr/bin/gnome-terminal --working-directory='%DIR'";
-        List<String> expected = List.of("/usr/bin/gnome-terminal", "--working-directory=%DIR");
-        assertEquals(expected, NativeDesktop.splitCommandLine(command));
-    }
-
-    @Test
-    void splitCommandLineWithDoubleQuotedDir() {
-        String command = "/usr/bin/gnome-terminal --working-directory=\"%DIR\"";
-        List<String> expected = List.of("/usr/bin/gnome-terminal", "--working-directory=%DIR");
-        assertEquals(expected, NativeDesktop.splitCommandLine(command));
-    }
-
-    @Test
-    void splitCommandLineWithQuotedSpacesInDir() {
-        String command = "konsole --workdir '/home/user/My Documents'";
-        List<String> expected = List.of("konsole", "--workdir", "/home/user/My Documents");
-        assertEquals(expected, NativeDesktop.splitCommandLine(command));
-    }
-
-    @Test
-    void splitCommandLineWithEscapedSpaces() {
-        String command = "xfce4-terminal --working-directory=/home/user/My\\ Documents";
-        List<String> expected = List.of("xfce4-terminal", "--working-directory=/home/user/My Documents");
-        assertEquals(expected, NativeDesktop.splitCommandLine(command));
-    }
-
-    @Test
-    void splitCommandLineNullOrBlank() {
-        assertEquals(List.of(), NativeDesktop.splitCommandLine(null));
-        assertEquals(List.of(), NativeDesktop.splitCommandLine("  "));
+    @ParameterizedTest
+    @MethodSource
+    void splitCommandLine(List<String> expected, String commandLine) {
+        assertEquals(expected, NativeDesktop.splitCommandLine(commandLine));
     }
 }
