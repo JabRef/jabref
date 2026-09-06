da4fbf2f9d33094b5f771c0322c42fa058df37a0
Ahmed Ashour <a8087027@gmail.com>
2026-09-06 22:34:07 +0300

    Move rate-limit retry requirement tag closer to implementation

--- jablib/src/main/java/org/jabref/logic/importer/fetcher/SwhidFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/fetcher/SwhidFetcher.java
@@ -65,7 +65,6 @@ public class SwhidFetcher implements IdBasedFetcher {
     }
 
     @Override
-    // [impl->req~fetchers.identifier-rate-limit-retries~1]
     public Optional<BibEntry> performSearchById(String identifier) throws FetcherException {
         Optional<SWHID> parsedSwhid = SWHID.parse(identifier);
         if (parsedSwhid.isEmpty()) {
@@ -82,6 +81,7 @@ public class SwhidFetcher implements IdBasedFetcher {
             throw new FetcherException("Invalid URL constructed for SWHID: " + canonicalSwhid, e);
         }
 
+        // [impl->req~fetchers.identifier-rate-limit-retries~1]
         return FetcherRetry.executeWithRateLimitRetry(() -> {
             try {
                 URLDownload urlDownload = new URLDownload(url);
