fd71793ffed74ddaf0abd76b8dfbd6034e50b9a8
Curtis Izuchukwu <izuchukwucur@gmail.com>
2026-09-18 23:58:56 +0100

    Fixed GroupTreeView import order.

--- jabgui/src/main/java/org/jabref/gui/groups/GroupTreeView.java
+++ jabgui/src/main/java/org/jabref/gui/groups/GroupTreeView.java
@@ -1,5 +1,4 @@
 package org.jabref.gui.groups;
-import org.jabref.logic.util.io.FileUtil;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -68,6 +67,7 @@ import org.jabref.gui.util.ViewModelTreeTableRowFactory;
 import org.jabref.logic.ai.AiService;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.util.TaskExecutor;
+import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibEntryTypesManager;
