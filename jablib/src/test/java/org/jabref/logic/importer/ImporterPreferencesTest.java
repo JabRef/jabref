660506ba09a0eab4eba140310efd4a244d3cef28
Curtis Izuchukwu <izuchukwucur@gmail.com>
2026-09-18 23:39:46 +0100

    Added tests for the BibTeX drag import confirmation

--- jablib/src/test/java/org/jabref/logic/importer/ImporterPreferencesTest.java
+++ jablib/src/test/java/org/jabref/logic/importer/ImporterPreferencesTest.java
@@ -24,7 +24,7 @@ class ImporterPreferencesTest {
                 false,
                 Path.of(""),
                 false,
-                false,
+                true,
                 Set.of(),
                 Set.of(new FetcherApiKey(FETCHER, true, key)),
                 false,
