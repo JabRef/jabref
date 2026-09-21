76be8757723235a54b08b8ef65fc509a7964887b
adibapathan07 <adibapathan248@gmail.com>
2026-09-21 07:11:00 -0700

    Fix formatting in GrobidPreferences constructor

--- jablib/src/main/java/org/jabref/logic/importer/util/GrobidPreferences.java
+++ jablib/src/main/java/org/jabref/logic/importer/util/GrobidPreferences.java
@@ -15,7 +15,7 @@ public class GrobidPreferences {
         this(
                 false,                          // Grobid enabled
                 false,                          // Grobid use asked
-               " "
+                " "
         );
     }
 
